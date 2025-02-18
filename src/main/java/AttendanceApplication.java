f5da5f7a1efb5254071910b9f1d89eb69ff66465
ppparkta <pppppark2@gmail.com>
2025-02-18 15:11:38 +0900

    chore: 프로젝트 초기화
    feat: 휴일일 때 출석일자 생성 예외처리
    
    feat: AttendanceDate 출석 여부 확인
    
    fix: AttendanceTime isAbsence 조건 수정
    
    feat: Attendance 생성 시 빈 날 자동으로 결석처리
    
    refactor: Attendance try-catch 제거
    
    feat: Attendance 대상자 계산
    
    feat: has 오버로딩
    
    feat: CrewGroup Crew 조회 기능 추가
    
    refactor: 열거형 패키지 분리
    
    feat: LocalDate/Time 한국어 변환 기능 추가
    
    refactor: 코드 정렬 맞춤 및 import 정리
    
    feat: String->LocalDateTime 컨버터 추가
    
    feat: 컨트롤러 연결
    
    feat: file 읽어서 map으로 변환
    
    feat: String -> LocalDateTime 컨버터 추가
    
    refactor: file 파싱 InputParser에게 위임
    
    refactor: 사용하지 않는 기능 제거
    
    feat: CrewGroup 초기화 로직 추가
    
    feat: 2,3번 기능 구현
    
    test: 테스트코드 잘못 비교한 내용 수정
    
    refactor: 안 쓰는 import, method 제거, 매직넘버상수화
    
    refactor: 예외 메시지 추가
    
    refactor: 예외 메시지 테스트 추가
    
    refactor: 테스트명 상황 명확하게 수정한다
    
    refactor: 인덴트 조절
    
    refactor: 요일 파싱 방법 변경
    
    refactor: Service 계층 수정
    
    변경사항
    - AttendanceService -> CrewService
    - 각 크루의 출석부 초기화 책임 분리
    
    refactor: FileInputView 이름 변경
    
    refactor: 제어자 순서 변경, 상수 컨벤션 수정
    
    refactor: 테스트 DisplayName 수정
    
    변경내역
    - 데이터 의미에 맞게 테스트명 수정
    
    refactor: 서비스 삭제 및 도메인 이동
    
    refactor: 테스트 클래스명 수정
    
    fix: pattern 오타 수정
    
    refactor: 테스트명 클래스 수정에 맞게 변경
    
    refactor: util 패키지 삭제
    
    chore: 프로젝트 초기화

--- src/main/java/AttendanceApplication.java
+++ src/main/java/AttendanceApplication.java
@@ -1,12 +1,5 @@
-import presentation.AttendanceController;
-import presentation.view.AttendanceFileInputView;
-import domain.CrewFactory;
 
 public class AttendanceApplication {
     public static void main(String[] args) {
-        AttendanceFileInputView fileInputView = new AttendanceFileInputView();
-        CrewFactory crewFactory = new CrewFactory();
-        AttendanceController attendanceController = new AttendanceController(fileInputView, crewFactory);
-        attendanceController.run();
     }
 }
