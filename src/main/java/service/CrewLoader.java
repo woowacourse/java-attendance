d7b99839442a5cb7724ec95c8143b1d430a6e6fd
gabean13 <choegabin171@gmail.com>
2025-02-18 17:11:57 +0900

    feat: 출석 파일 읽어서 CrewGroup 생성 구현

---
+++ src/main/java/service/CrewLoader.java
@@ -0,0 +1,28 @@
+package service;
+
+import domain.Crew;
+import domain.CrewGroup;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import util.FileReader;
+
+public class CrewLoader {
+
+    public CrewGroup loadCrews() {
+        FileReader fileReader = new FileReader();
+        List<String> lines = fileReader.readFile();
+        CrewGroup crewGroup = new CrewGroup();
+
+        for (String line : lines) {
+            String[] splittedLine = line.split(",");
+            String nickname = splittedLine[0];
+            String rawDate = splittedLine[1];
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            LocalDateTime dateTime = LocalDateTime.parse(rawDate, formatter);
+            crewGroup.addCrew(nickname, dateTime);
+        }
+
+        return crewGroup;
+    }
+}
---
+++ src/main/java/util/FileReader.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FileReader {
+    public List<String> readFile() {
+        Path path = Paths.get("src", "main", "resources", "attendances.csv");
+        return convertFile(path);
+    }
+
+    protected List<String> convertFile(Path path) {
+        try (Stream<String> lines = Files.lines(path, StandardCharsets.UTF_8)) {
+            return lines.skip(1).toList();
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+}
