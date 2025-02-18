f1d4480e1ec26f85f2b6cced385d0295a7232c93
Minuring <mw9505@gmail.com>
2025-02-18 15:44:04 +0900

    feat: 출석 상태 기능 추가
    요일 별 교육 운영시간에 따른 출석 상태를 결정합니다.

--- src/main/java/domain/Attendance.java
+++ src/main/java/domain/Attendance.java
@@ -2,20 +2,23 @@ package domain;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Attendance {
 
     private Crew crew;
-    private LocalDateTime time;
+    private LocalDateTime dateTime;
+    private String status;
 
-    private Attendance(Crew crew, LocalDateTime time) {
-        validateDayOfWeek(time);
+    private Attendance(Crew crew, LocalDateTime dateTime) {
+        validateDayOfWeek(dateTime);
+        setStatus(dateTime);
         this.crew = crew;
-        this.time = time;
+        this.dateTime = dateTime;
     }
 
-    public static Attendance of(Crew crew, LocalDateTime time) {
-        return new Attendance(crew, time);
+    public static Attendance of(Crew crew, LocalDateTime dateTime) {
+        return new Attendance(crew, dateTime);
     }
 
     private void validateDayOfWeek(LocalDateTime dateTime) {
@@ -23,5 +26,33 @@ public class Attendance {
         if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void setStatus(LocalDateTime dateTime) {
+        LocalTime time = dateTime.toLocalTime();
+
+        if (dateTime.getDayOfWeek() == DayOfWeek.MONDAY) {
+            determineStatus(time, 13);
+            return;
+        }
+
+        determineStatus(time, 10);
+    }
+
+    private void determineStatus(LocalTime time, int hour) {
+        if (time.isBefore(LocalTime.of(hour, 5)) || time.equals(LocalTime.of(hour, 5))) {
+            status = "출석";
+            return;
+        }
+        if (time.isAfter(LocalTime.of(hour, 5)) && time.isBefore(LocalTime.of(hour, 30))
+            || time.equals(LocalTime.of(hour, 30))) {
+            status = "지각";
+            return;
+        }
+        status = "결석";
+    }
+
+    public String getStatus() {
+        return status;
     }
 }
--- src/test/java/domain/AttendanceTest.java
+++ src/test/java/domain/AttendanceTest.java
@@ -1,18 +1,27 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class AttendanceTest {
+    private Crew crew;
+
+    @BeforeEach
+    void setUp() {
+        crew = new Crew("이든");
+    }
 
     @Test
     void 닉네임과_등교시간으로_출석을_한다() {
-        Crew crew = new Crew("이든");
         LocalTime time = LocalTime.of(9, 59);
         LocalDateTime localDateTime = LocalDateTime.of(LocalDate.now(), time);
 
@@ -22,13 +31,47 @@ public class AttendanceTest {
 
     @Test
     void 등교날짜가_주말이면_예외가_발생한다() {
-        Crew crew = new Crew("이든");
         LocalTime time = LocalTime.of(9, 59);
 
-        LocalDate saturday = LocalDate.of(2025, 2, 22);
+        LocalDate saturday = LocalDate.of(2024, 12, 14);
         LocalDateTime localDateTime = LocalDateTime.of(saturday, time);
 
         assertThatThrownBy(() -> Attendance.of(crew, localDateTime))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 등교날짜가_주말이아니면_출석을_한다() {
+        LocalTime time = LocalTime.of(13, 4);
+
+        LocalDate monday = LocalDate.of(2024, 12, 9);
+        LocalDateTime localDateTime = LocalDateTime.of(monday, time);
+
+        assertThatCode(() -> Attendance.of(crew, localDateTime))
+            .doesNotThrowAnyException();
+    }
+
+    @CsvSource(value = {"13:5:출석", "13:6:지각", "13:31:결석"}, delimiterString = ":")
+    @ParameterizedTest
+    void 월요일의_시간에따라_다른출석상태를_반환한다(int hour, int minute, String expectedStatus) {
+        LocalTime time = LocalTime.of(hour, minute);
+
+        LocalDate monday = LocalDate.of(2024, 12, 9);
+        LocalDateTime localDateTime = LocalDateTime.of(monday, time);
+        Attendance attendance = Attendance.of(crew, localDateTime);
+
+        assertThat(attendance.getStatus()).isEqualTo(expectedStatus);
+    }
+
+    @CsvSource(value = {"10:5:출석", "10:6:지각", "10:31:결석"}, delimiterString = ":")
+    @ParameterizedTest
+    void 화요일의_시간에따라_다른출석상태를_반환한다(int hour, int minute, String expectedStatus) {
+        LocalTime time = LocalTime.of(hour, minute);
+
+        LocalDate tuesday = LocalDate.of(2024, 12, 10);
+        LocalDateTime localDateTime = LocalDateTime.of(tuesday, time);
+        Attendance attendance = Attendance.of(crew, localDateTime);
+
+        assertThat(attendance.getStatus()).isEqualTo(expectedStatus);
     }
 }
