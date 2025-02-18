844e181680d1719bc9d175bc78ed7e080884278f
DEV_WOONG <ywcsuwon@gmail.com>
2025-02-18 15:11:53 +0900

    feat: 출석 기능 예외 상황 처리

--- src/main/java/domain/Attendance.java
+++ src/main/java/domain/Attendance.java
@@ -1,14 +1,36 @@
 package domain;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 public class Attendance {
+    public static final LocalTime MAX_ATTENDANCE_TIME = LocalTime.of(23, 0);
+    public static final LocalTime MIN_ATTENDANCE_TIME = LocalTime.of(8, 0);
+    public static final List<DayOfWeek> WEEKEND = List.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+
     private String name;
     private LocalDateTime attendanceTime;
 
     public Attendance(String name, LocalDateTime attendanceTime) {
+        validateDate(attendanceTime.toLocalDate());
+        validateTime(attendanceTime.toLocalTime());
         this.name = name;
         this.attendanceTime = attendanceTime;
     }
 
+    private void validateDate(LocalDate date) {
+        // TODO : 주말, 공휴일이면 '출석 가능한 날짜가 아닙니다.'
+        if (WEEKEND.contains(date.getDayOfWeek())) {
+            throw new IllegalArgumentException("출석 가능한 날짜가 아닙니다.");
+        }
+    }
+
+    private void validateTime(LocalTime time) {
+        if (time.isAfter(MAX_ATTENDANCE_TIME) || time.isBefore(MIN_ATTENDANCE_TIME)) {
+            throw new IllegalArgumentException("출석 가능한 시간이 아닙니다.");
+        }
+    }
 }
--- src/test/java/AttendanceTest.java
+++ src/test/java/AttendanceTest.java
@@ -9,13 +9,37 @@ public class AttendanceTest {
     void 닉네임과_등교_시간을_입력하면_출석() {
         // given
         String inputName = "Lemon";
-        LocalDateTime localDateTime = LocalDateTime.of(2024,12,1,10,30);
+        LocalDateTime localDateTime = LocalDateTime.of(2024,12,3,10,30);
 
         // when
         Attendance attendance = new Attendance(inputName,localDateTime);
 
         // then
         Assertions.assertThat(attendance).extracting("name").isEqualTo("Lemon");
-        Assertions.assertThat(attendance).extracting("attendanceTime").isEqualTo(LocalDateTime.of(2024,12,1,10,30));
+        Assertions.assertThat(attendance).extracting("attendanceTime").isEqualTo(LocalDateTime.of(2024,12,3,10,30));
+    }
+
+    @Test
+    void 주말에_출석하면_예외가_발생한다() {
+        // given
+        String inputName = "Lemon";
+        LocalDateTime localDateTime = LocalDateTime.of(2024,12,8,10,30);
+
+        // expected
+        Assertions.assertThatThrownBy(() -> new Attendance(inputName,localDateTime))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("출석 가능한 날짜가 아닙니다.");
+    }
+
+    @Test
+    void _8시와_23시_사이가_아니면_예외가_발생한다() {
+        // given
+        String inputName = "Lemon";
+        LocalDateTime localDateTime = LocalDateTime.of(2024,12,3,7,59);
+
+        // expected
+        Assertions.assertThatThrownBy(() -> new Attendance(inputName,localDateTime))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("출석 가능한 시간이 아닙니다.");
     }
 }
