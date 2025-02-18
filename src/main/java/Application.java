103040d06b0dd64de3778d1b554c75eeda8e6469
Hacanna42 <dtmdgus901@gmail.com>
2025-02-18 16:26:22 +0900

    test, feat: 클래스 분리 및 테스트 코드 이동

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,32 +3,5 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
     }
-
-    public static boolean lateCheck(int dayOfWeek, int hour, int minute) {
-        if (dayOfWeek == 1) {
-            return hour > 13 || (hour == 13 && minute > 5);
-        }
-
-        return hour > 10 || (hour == 10 && minute > 5);
-    }
-
-    public static boolean absenceCheck(int dayOfWeek, int hour, int minute) {
-        if (dayOfWeek == 1) {
-            return hour > 13 || (hour == 13 && minute > 30);
-        }
-        return hour > 10 || (hour == 10 && minute > 30);
-    }
-
-    public static boolean openTimeCheck(int hour, int minute) {
-        if (hour == 23 && minute > 0) {
-            return false;
-        }
-        return hour >= 8 && hour <= 23;
-    }
-
-    public static boolean restDayCheck(int day) {
-        List<Integer> restDays = List.of(1, 7, 8, 14, 15, 21, 22, 25, 28, 29);
-        return restDays.contains(day);
-    }
 }
 
--- src/main/java/domain/AttendanceDate.java
+++ src/main/java/domain/AttendanceDate.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.List;
+
 public class AttendanceDate {
     private final int month = 12;
     private final int day;
@@ -10,5 +12,10 @@ public class AttendanceDate {
 
     public int getDayOfWeek() {
         return ((day + 5) % 7) + 1;
+    }
+
+    public boolean isRestDay() {
+        List<Integer> restDays = List.of(1, 7, 8, 14, 15, 21, 22, 25, 28, 29);
+        return restDays.contains(day);
     }
 }
