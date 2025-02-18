b58988ce61e5dfbdc5a56f7bf9b63bf22896e9a2
saera <yook.saera@gmail.com>
2025-02-18 20:44:21 +0900

    feat: FileLoader의 Attendance 여러 개 생성 테스트 통과

--- src/main/java/attendance/util/FileLoader.java
+++ src/main/java/attendance/util/FileLoader.java
@@ -2,16 +2,35 @@ package attendance.util;
 
 import attendance.domain.Attendance;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class FileLoader {
 
-    public static Attendance load(final String data) {
-        List<String> seperatedData = Arrays.stream(data.split(",")).toList();
-        LocalDateTime localDateTime = LocalDateTime.parse(seperatedData.get(1).replace(" ", "T"));
-        Attendance attendance = new Attendance(seperatedData.get(0));
+    public static Attendance createAttendance(final String name, final LocalDateTime localDateTime) {
+        Attendance attendance = new Attendance(name);
         attendance.add(localDateTime);
         return attendance;
+    }
+
+    public static List<Attendance> loadAll(final List<String> datas) {
+        List<Attendance> attendances = new ArrayList<>();
+        datas.forEach(data -> {
+            List<String> seperatedData = Arrays.stream(data.split(",")).toList();
+            LocalDateTime localDateTime = LocalDateTime.parse(seperatedData.get(1).replace(" ", "T"));
+
+            Optional<Attendance> crewAttendance = attendances.stream()
+                    .filter(a -> a.isNameMatch(seperatedData.get(0)))
+                    .findFirst();
+
+            if (crewAttendance.isEmpty()) {
+                attendances.add(createAttendance(seperatedData.get(0), localDateTime));
+                return;
+            }
+            crewAttendance.get().add(localDateTime);
+        });
+        return attendances;
     }
 }
--- src/test/java/attendance/util/FileLoaderTest.java
+++ src/test/java/attendance/util/FileLoaderTest.java
@@ -1,6 +1,8 @@
 package attendance.util;
 
 import attendance.domain.Attendance;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -12,8 +14,10 @@ public class FileLoaderTest {
     @Test
     void test1(){
         String data = "쿠키,2024-12-13 10:08";
+        List<String> seperatedData = Arrays.stream(data.split(",")).toList();
+        LocalDateTime localDateTime = LocalDateTime.parse(seperatedData.getLast().replace(" ", "T"));
 
-        Assertions.assertThat(FileLoader.load(data))
+        Assertions.assertThat(FileLoader.createAttendance(seperatedData.getFirst(), localDateTime))
                 .isNotNull()
                 .isInstanceOf(Attendance.class);
     }
@@ -32,6 +36,6 @@ public class FileLoaderTest {
 
         Assertions.assertThat(attendances)
                 .hasSize(4)
-                .hasExactlyElementsOfTypes(Attendance.class);
+                .hasOnlyElementsOfType(Attendance.class);
     }
 }
