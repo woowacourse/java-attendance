37fe0be1b047b1d62ba47cc581605d8dc2bbf546
geonwoo <kysub99@naver.com>
2025-02-18 18:16:03 +0900

    feat(CheckInTimesTest): CheckInTimes 도메인 구현

--- src/main/java/domain/CheckInTimes.java
+++ src/main/java/domain/CheckInTimes.java
@@ -1,4 +1,41 @@
 package domain;
 
+import java.util.List;
+
+import static domain.AttendanceStatus.*;
+
 public class CheckInTimes {
+    private final List<CheckInTime> checkInTimes;
+
+    private CheckInTimes(List<CheckInTime> checkInTimes) {
+        this.checkInTimes = checkInTimes;
+    }
+
+    public static CheckInTimes of (List<CheckInTime> checkInTimes) {
+        return new CheckInTimes(checkInTimes);
+    }
+
+    public int countPresence() {
+        return Math.toIntExact(
+                checkInTimes.stream()
+                .filter(time -> time.getAttendanceStatus() == PRESENCE)
+                .count()
+        );
+    }
+
+    public int countLate() {
+        return Math.toIntExact(
+                checkInTimes.stream()
+                        .filter(time -> time.getAttendanceStatus() == LATE)
+                        .count()
+        );
+    }
+
+    public int countAbsence() {
+        return Math.toIntExact(
+                checkInTimes.stream()
+                        .filter(time -> time.getAttendanceStatus() == ABSENCE)
+                        .count()
+        );
+    }
 }
