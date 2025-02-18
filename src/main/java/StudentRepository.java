e89aa2eaa00096068a2ab1b2679a19e2047ea471
(minjae8563) <(minjae8563@naver.com)>
2025-02-18 16:47:33 +0900

    feat : 이름 기반으로 Student 객체 찾는 기능 구현, add 기능 구현

--- src/main/java/StudentRepository.java
+++ src/main/java/StudentRepository.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StudentRepository {
-    List<String> studentNames;
+    List<Student> students = new ArrayList<>();
 
     public boolean notExistStudent(String studentName) {
         return false;
     }
 
+    public Student findStudentByName(String name) {
+        for (Student student : students){
+            if (student.name.equals(name)){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
 }
