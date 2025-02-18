039dac022b8409bd85412339a7fa35fe40021425
saera <yook.saera@gmail.com>
2025-02-18 17:23:26 +0900

    feat: 닉네임, 등교 시간 입력 받는 기능 구현

---
+++ src/main/java/attendance/view/InputView.java
@@ -0,0 +1,25 @@
+package attendance.view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+
+public class InputView {
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public String readNickName() throws IOException {
+        System.out.println("닉네임을 입력해 주세요.");
+        return bufferedReader.readLine();
+    }
+
+    public LocalTime readAttendanceTime() throws IOException {
+        System.out.println("등교 시간을 입력해 주세요.");
+        try {
+            return LocalTime.parse(bufferedReader.readLine());
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("잘못된 시간 형식입니다. HH:mm 형식으로 입력해주세요.");
+        }
+    }
+}
