9c01e11ffcdd64546ecca7dac910e0b4fbe401a1
BWY <quantumjump10@gmail.com>
2025-02-18 19:21:00 +0900

    feat(attendanceCheck): 날짜에 따라 해당 요일을 반환하는 기능 구현

--- src/main/java/AttendanceCheck.java
+++ src/main/java/AttendanceCheck.java
@@ -1,4 +1,5 @@
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class AttendanceCheck {
@@ -15,4 +16,31 @@ public class AttendanceCheck {
 
         return "결석";
     }
+
+    public static String convertKorean(LocalDate localDate) {
+        if (localDate.getDayOfWeek().getValue() == 1) {
+            return "월요일";
+        }
+        if (localDate.getDayOfWeek().getValue() == 2) {
+            return "화요일";
+        }
+        if (localDate.getDayOfWeek().getValue() == 3) {
+            return "수요일";
+        }
+        if (localDate.getDayOfWeek().getValue() == 4) {
+            return "목요일";
+        }
+
+        if (localDate.getDayOfWeek().getValue() == 5) {
+            return "금요일";
+        }
+
+        if (localDate.getDayOfWeek().getValue() == 6) {
+            return "토요일";
+        }
+
+        return "일요일";
+
+    }
+
 }
