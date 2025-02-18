2669c728f0f023fcf234efe95ff5581b332a701d
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-18 16:22:24 +0900

    test(domain) : 출석 상태 테스트 작성

---
+++ src/test/java/domain/AttendanceTest.java
@@ -0,0 +1,104 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class AttendanceTest {
+    @Nested
+    @DisplayName("화수목금")
+    class NotMonday {
+        @DisplayName("출석한 경우")
+        @Test
+        void test2() {
+            Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 3, 9, 55));
+            assertThat(attendance.getState()).isEqualTo("출석");
+        }
+
+        @DisplayName("지각한 경우")
+        @Test
+        void test3() {
+            Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 3, 10, 6));
+            assertThat(attendance.getState()).isEqualTo("지각");
+        }
+
+        @DisplayName("결석한 경우")
+        @Test
+        void test4() {
+            Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 3, 10, 31));
+            assertThat(attendance.getState()).isEqualTo("결석");
+        }
+
+        @DisplayName("1시 이후인 경우")
+        @Test
+        void test5() {
+            Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 3, 13, 0));
+            assertThat(attendance.getState()).isEqualTo("결석");
+        }
+    }
+
+    @Nested
+    @DisplayName("월")
+    class Monday {
+        @DisplayName("출석한 경우")
+        @Test
+        void test2() {
+            Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 2, 9, 55));
+            assertThat(attendance.getState()).isEqualTo("출석");
+        }
+
+        @DisplayName("지각한 경우")
+        @Test
+        void test3() {
+            Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 2, 13, 6));
+            assertThat(attendance.getState()).isEqualTo("지각");
+        }
+
+        @DisplayName("결석한 경우")
+        @Test
+        void test4() {
+            Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 2, 13, 31));
+            assertThat(attendance.getState()).isEqualTo("결석");
+        }
+    }
+
+    @Nested
+    @DisplayName("주말 및 공휴일 출석")
+    class HollyDays{
+        @Test
+        @DisplayName("성탄절")
+        void test5() {
+            assertThatThrownBy(() -> new Attendance(LocalDateTime.of(2024, 12, 25, 9, 55)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+        @Test
+        @DisplayName("주말")
+        void test6() {
+            assertThatThrownBy(() -> new Attendance(LocalDateTime.of(2024, 12, 8, 9, 55)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+    }
+
+
+    @Nested
+    @DisplayName("캠퍼스 운영 시간이 아닌 경우")
+    class WrongTime{
+        @Test
+        @DisplayName("8시 전인 경우")
+        void test5() {
+            assertThatThrownBy(() -> new Attendance(LocalDateTime.of(2024, 12, 3, 7, 59)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+        @Test
+        @DisplayName("23시를 초과한 경우")
+        void test6() {
+            assertThatThrownBy(() -> new Attendance(LocalDateTime.of(2024, 12, 3, 23, 1)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+    }
+}
