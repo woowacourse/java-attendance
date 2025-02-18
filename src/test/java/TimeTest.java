ca37598fdfb11333b320679dd506ade7a03a3623
Hacanna42 <dtmdgus901@gmail.com>
2025-02-18 15:20:53 +0900

    test, feat: 휴일 체크 TDD 사이클 완료

--- src/test/java/TimeTest.java
+++ src/test/java/TimeTest.java
@@ -3,62 +3,110 @@ import org.junit.jupiter.api.Test;
 
 public class TimeTest {
     @Test
-    void test() {
+    void lateTest() {
         boolean isLate = Application.lateCheck(1, 13, 0);
         Assertions.assertThat(isLate).isFalse();
     }
 
     @Test
-    void test1() {
+    void lateTest1() {
         boolean isLate = Application.lateCheck(1, 14, 0);
         Assertions.assertThat(isLate).isTrue();
     }
 
     @Test
-    void test2() {
+    void lateTest2() {
         boolean isLate = Application.lateCheck(1, 13, 6);
         Assertions.assertThat(isLate).isTrue();
     }
 
     @Test
-    void test3() {
+    void lateTest3() {
         boolean isLate = Application.lateCheck(2, 13, 0);
         Assertions.assertThat(isLate).isTrue();
     }
 
     @Test
-    void test4() {
+    void lateTest4() {
         boolean isLate = Application.lateCheck(2, 10, 0);
         Assertions.assertThat(isLate).isFalse();
     }
 
     @Test
-    void test5() {
+    void lateTest5() {
         boolean isLate = Application.lateCheck(2, 10, 6);
         Assertions.assertThat(isLate).isTrue();
     }
 
     @Test
-    void test6() {
+    void absence1() {
         boolean isAbsence = Application.absenceCheck(1, 13, 31);
         Assertions.assertThat(isAbsence).isTrue();
     }
 
     @Test
-    void test7() {
+    void absence2() {
         boolean isAbsence = Application.absenceCheck(1, 13, 0);
         Assertions.assertThat(isAbsence).isFalse();
     }
 
     @Test
-    void test8() {
+    void absence3() {
         boolean isAbsence = Application.absenceCheck(2, 13, 0);
         Assertions.assertThat(isAbsence).isTrue();
     }
 
     @Test
-    void test9() {
+    void absence4() {
         boolean isAbsence = Application.absenceCheck(1, 14, 0);
         Assertions.assertThat(isAbsence).isTrue();
+    }
+
+    @Test
+    void openTimeTest() {
+        boolean isOpenTime = Application.openTimeCheck(13, 0);
+        Assertions.assertThat(isOpenTime).isTrue();
+    }
+
+    @Test
+    void openTimeTest2() {
+        boolean isOpenTime = Application.openTimeCheck(7, 0);
+        Assertions.assertThat(isOpenTime).isFalse();
+    }
+
+    @Test
+    void openTimeTest3() {
+        boolean isOpenTime = Application.openTimeCheck(24, 0);
+        Assertions.assertThat(isOpenTime).isFalse();
+    }
+
+    @Test
+    void openTimeTest4() {
+        boolean isOpenTime = Application.openTimeCheck(23, 30);
+        Assertions.assertThat(isOpenTime).isFalse();
+    }
+
+    @Test
+    void restDayTest() {
+        boolean isRestDay = Application.restDayCheck(1);
+        Assertions.assertThat(isRestDay).isTrue();
+    }
+
+    @Test
+    void restDayTest1() {
+        boolean isRestDay = Application.restDayCheck(2);
+        Assertions.assertThat(isRestDay).isFalse();
+    }
+
+    @Test
+    void restDayTest2() {
+        boolean isRestDay = Application.restDayCheck(8);
+        Assertions.assertThat(isRestDay).isTrue();
+    }
+
+    @Test
+    void restDayTest3() {
+        boolean isRestDay = Application.restDayCheck(25);
+        Assertions.assertThat(isRestDay).isTrue();
     }
 }
