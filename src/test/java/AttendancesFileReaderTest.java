c4ed10556debdcb961a3eb9d1ef1700ea1547ff2
ChoiDongJoon <adong0808@naver.com>
2025-02-18 20:41:28 +0900

    test: 파일 읽기 테스트 추가

---
+++ src/test/java/AttendancesFileReaderTest.java
@@ -0,0 +1,15 @@
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class AttendancesFileReaderTest {
+
+    @Test
+    void 전체_파일_읽기() {
+        AttendancesFileReader attendancesFileReader = new AttendancesFileReader();
+        assertThat(attendancesFileReader.read()).isEqualTo("쿠키,2024-12-13 10:08\n"
+                + "빙봉,2024-12-13 10:07\n"
+                + "빙티,2024-12-13 10:07\n"
+                + "이든,2024-12-13 10:07\n");
+    }
+}
