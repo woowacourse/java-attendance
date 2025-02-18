396125ab3c27f6c0720aa3935d42acd281642690
JO-eusan <goodsan0330@gmail.com>
2025-02-18 20:04:51 +0900

    feat: 출석 기능 구현

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import repository.AttendanceRepository;
 import view.InputView;
 import view.OutputView;
@@ -18,6 +20,19 @@ public class AttendanceController {
     }
 
     public void run() {
+        function_1();
+    }
 
+    private void function_1() {
+        String name = inputView.readName();
+        String time = inputView.readTime();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String date = LocalDateTime.now().format(formatter);
+        formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(date + " " + time, formatter);
+
+        attendanceRepository.attend(name,dateTime);
+        outputView.printAttendanceRecord(dateTime);
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,13 +1,16 @@
 package view;
 
 import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class OutputView {
     private final String RECORD_FORMAT = "%d월 %02d일 %s %02d:%02d (출석)%n";
 
     public void printAttendanceRecord(LocalDateTime dateAndTime){
-        System.out.printf(RECORD_FORMAT,dateAndTime.getMonth(),dateAndTime.getDayOfMonth(),
-            dateAndTime.getDayOfWeek(), dateAndTime.getHour(), dateAndTime.getMinute());
+        System.out.printf(RECORD_FORMAT,dateAndTime.getMonthValue(), dateAndTime.getDayOfMonth(),
+        dateAndTime.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN),
+            dateAndTime.getHour(), dateAndTime.getMinute());
     }
 
 }
