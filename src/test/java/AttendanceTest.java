5e7d394f004a3be388adba3b1740854ad665092f
songsunkook <songsunkook@gmail.com>
2025-02-18 15:32:25 +0900

    red: 이미 출석한 경우 예외를 던진다

--- src/test/java/AttendanceTest.java
+++ src/test/java/AttendanceTest.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AttendanceTest {
@@ -12,5 +13,13 @@ public class AttendanceTest {
         crews.add(new Crew("pobi"));
         Crew crew = crews.get("pobi");
         crew.attendance(LocalDate.now(), LocalTime.of(10, 1));
+    }
+
+    @Test
+    void 이미_출석한_경우_예외를_던진다() {
+        Crew crew = new Crew("pobi");
+        Assertions.assertThatThrownBy(() -> {
+            crew.attendance(LocalDate.now(), LocalTime.of(10, 01));
+        }).isInstanceOf(AlreadyAttendanceException.class);
     }
 }
