bc2a121432890d9d0fe3cac4895bab6bf23ae263
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-18 18:05:01 +0900

    feat(AttendanceType): 출석 유형 기능 구현

--- src/main/java/attendance/domain/AttendanceType.java
+++ src/main/java/attendance/domain/AttendanceType.java
@@ -1,9 +1,14 @@
 package attendance.domain;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public enum AttendanceType {
     SAFE("출석"),
     LATE("지각"),
-    ABSENT("결석");
+    ABSENT("결석"),
+    FREE("자유출근");
 
     private final String type;
 
