0c2bd44d216585651bbbefdcf0a2c662f014efee
BWY <quantumjump10@gmail.com>
2025-02-18 19:19:01 +0900

    test: 등교 시간에 따른 출석 상태 변동 테스트 구현

--- src/test/java/AttendanceCheckTest.java
+++ src/test/java/AttendanceCheckTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AttendanceCheckTest {
@@ -10,6 +12,30 @@ public class AttendanceCheckTest {
 
         final var actual = "12월 05일 화요일 " + attendanceTime + " (출석)";
         final var expected = "12월 05일 화요일 09:59 (출석)";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 출석시간이_지각범위내라면_지각으로_표시된다() {
+        LocalTime standardTime = LocalTime.of(10, 0);
+        LocalTime attendanceTime = LocalTime.of(10, 6);
+
+        final var attendanceCheck = AttendanceCheck.checkAttendanceStatus(standardTime, attendanceTime);
+
+        final var actual = "12월 05일 화요일 " + attendanceTime + " (" + attendanceCheck + ")";
+        final var expected = "12월 05일 화요일 10:06 (지각)";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 출석시간이_결석범위라면_결석으로_표시된다() {
+        LocalTime standardTime = LocalTime.of(10, 0);
+        LocalTime attendanceTime = LocalTime.of(10, 31);
+
+        final var attendanceCheck = AttendanceCheck.checkAttendanceStatus(standardTime, attendanceTime);
+
+        final var actual = "12월 05일 화요일 " + attendanceTime + " (" + attendanceCheck + ")";
+        final var expected = "12월 05일 화요일 10:31 (결석)";
         assertThat(actual).isEqualTo(expected);
     }
 
