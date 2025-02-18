ffc79efe5b2e7f94e30240375c63ac3284228e18
rladmstn <oz33177351@gmail.com>
2025-02-18 16:04:08 +0900

    feat(AttendanceChecker): 출석 상태 지정 로직 구현

---
+++ src/main/java/attendance/domain/AttendanceChecker.java
@@ -0,0 +1,35 @@
+package attendance.domain;
+
+import java.time.LocalDateTime;
+
+public class AttendanceChecker {
+
+    public String check(LocalDateTime dateTime) {
+        // 평일로 가정
+
+        String day = dateTime.getDayOfWeek().name();
+        if (day.equals("MONDAY")) { // 월요일
+
+            return checkStatusWithCondition(dateTime, 13, 0, 6);
+        }
+
+        return checkStatusWithCondition(dateTime, 10, 0, 6);
+    }
+
+    private String checkStatusWithCondition(LocalDateTime dateTime, int hour, int attendanceMinute, int lateMinute) {
+
+        if (!dateTime.isAfter(
+                LocalDateTime.of(dateTime.getYear(), dateTime.getMonth(), dateTime.getDayOfMonth(), hour,
+                        attendanceMinute))) {
+            return "출석";
+        }
+
+        if (!dateTime.isAfter(
+                LocalDateTime.of(dateTime.getYear(), dateTime.getMonth(), dateTime.getDayOfMonth(), hour,
+                        lateMinute))) {
+            return "지각";
+        }
+
+        return "결석";
+    }
+}
---
+++ src/test/java/attendance/domain/AttendanceCheckerTest.java
@@ -0,0 +1,23 @@
+package attendance.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class AttendanceCheckerTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"2024,12,13,10,0,출석", "2024,12,13,10,6,지각", "2024,12,13,10,31,결석",
+            "2024,12,9,12,30,출석", "2024,12,9,13,6,지각", "2024,12,9,13,31,결석"})
+    void 시간에_맞는_출결_상태를_갖는다(int year, int month, int day, int hour, int minute, String result) {
+
+        // given
+        LocalDateTime dateTime = LocalDateTime.of(year, month, day, hour, minute);
+        AttendanceChecker attendance = new AttendanceChecker();
+
+        // when && then
+        assertThat(attendance.check(dateTime)).isEqualTo(result);
+    }
+}
