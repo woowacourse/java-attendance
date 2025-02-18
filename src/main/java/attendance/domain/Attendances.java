eeb22b3a73387a586763ae25ba9c5fdd5466cb0e
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-18 18:03:33 +0900

    feat(Attendances): 출석 명단 객체 구현

---
+++ src/main/java/attendance/domain/Attendances.java
@@ -0,0 +1,38 @@
+package attendance.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Attendances {
+    private final List<Attendance> attendances;
+
+    public Attendances() {
+        this.attendances = new ArrayList<>();
+    }
+
+    public void initAttendances(Crews crews, List<List<String>> attendanceRecords) {
+        for (List<String> attendanceRecord : attendanceRecords) {
+            String crewName = attendanceRecord.getFirst();
+            Crew crew = findCrew(crews, crewName);
+
+            List<String> dateInfo = List.of(attendanceRecord.getLast().split(" "));
+            LocalDateTime localDateTime = LocalDateTime.of(LocalDate.parse(dateInfo.getFirst()),
+                    LocalTime.parse(dateInfo.getLast()));
+
+            AttendanceType status = AttendanceType.of(localDateTime);
+
+            attendances.add(new Attendance(crew, localDateTime, status));
+        }
+
+    }
+
+    private Crew findCrew(Crews crews, String crewName) {
+        return crews.getCrews().stream()
+                .filter(crew -> crew.getName().equals(crewName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("No such crew name exists"));
+    }
+}
