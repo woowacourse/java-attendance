5705552d0ec2f451bca7e43bce434d04b8aa5624
GiSeok Kang <ime.rokz@gmail.com>
2025-02-18 21:16:43 +0900

    refactor: 출석 확인 책임 분리

--- src/main/java/Attendance.java
+++ src/main/java/Attendance.java
@@ -2,45 +2,51 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.TextStyle;
-import java.util.Locale;
+import java.time.MonthDay;
 
 public class Attendance {
 
-    public String check(LocalDateTime today) {
-        LocalDate date = today.toLocalDate();
-        LocalTime time = today.toLocalTime();
+    private LocalDateTime attendanceDateTime;
 
-        int month = date.getMonthValue();
-        int day = date.getDayOfMonth();
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        int hour = time.getHour();
-        int minute = time.getMinute();
-
-        if(month == 12 && day == 25){
-            throw new IllegalArgumentException("[ERROR] 공휴일에는 출석할 수 없습니다.");
-        }
-
-        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
-            throw new IllegalArgumentException("[ERROR] 주말에는 출석할 수 없습니다.");
-        }
-
-        String koreanDayOfWeek = dayOfWeek.getDisplayName(TextStyle.FULL, Locale.KOREAN);
-
-        String attendanceState = decisionAttendance(dayOfWeek, hour, minute).description;
-
-        return String.format("%d월 %02d일 %s %d:%d (%s)", month, day, koreanDayOfWeek, hour, minute, attendanceState);
+    private Attendance(LocalDateTime attendanceDateTime) {
+        this.attendanceDateTime = attendanceDateTime;
     }
 
-    private AttendanceState decisionAttendance(DayOfWeek dayOfWeek, int hour, int minute) {
-        if (hour < 8 || hour >= 23) {
-            throw new IllegalArgumentException("[ERROR] 출석 시간이 아닙니다.");
-        }
+    public static Attendance from(LocalDateTime attendanceDateTime) {
+        validate(attendanceDateTime);
+        return new Attendance(attendanceDateTime);
+    }
+
+    public static void validate(LocalDateTime attendanceDateTime) {
+        LocalDate date = attendanceDateTime.toLocalDate();
+        LocalTime time = attendanceDateTime.toLocalTime();
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+
+        validateHoliday(date);
+        validateWeekend(dayOfWeek);
+        validateOperatingTime(time);
+    }
+
+    public void update(LocalDateTime updateDateTime) {
+        validate(updateDateTime);
+        this.attendanceDateTime = updateDateTime;
+    }
+
+    public AttendanceState check() {
+        DayOfWeek dayOfWeek = attendanceDateTime.getDayOfWeek();
+        int hour = attendanceDateTime.getHour();
+        int minute = attendanceDateTime.getMinute();
 
         if (dayOfWeek == DayOfWeek.MONDAY) {
             return decisionByHour(hour, minute, 13);
         }
         return decisionByHour(hour, minute, 10);
+    }
+
+    private static void validateOperatingTime(LocalTime time) {
+        if (time.getHour() < 8 || time.getHour() == 23) {
+            throw new IllegalArgumentException("[ERROR] 출석 시간이 아닙니다.");
+        }
     }
 
     private AttendanceState decisionByHour(int hour, int minute, int standardHour) {
@@ -65,5 +71,17 @@ public class Attendance {
         }
 
         return AttendanceState.ATTEND;
+    }
+
+    private static void validateWeekend(DayOfWeek dayOfWeek) {
+        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+            throw new IllegalArgumentException("[ERROR] 주말에는 출석할 수 없습니다.");
+        }
+    }
+
+    private static void validateHoliday(LocalDate date) {
+        if (MonthDay.from(date).equals(MonthDay.of(12, 25))) {
+            throw new IllegalArgumentException("[ERROR] 공휴일에는 출석할 수 없습니다.");
+        }
     }
 }
--- src/test/java/AttendanceTest.java
+++ src/test/java/AttendanceTest.java
@@ -16,62 +16,57 @@ public class AttendanceTest {
     @DisplayName("출석 시간을 입력하면 출석할 수 있다")
     void input_enterTime_then_attendance() {
         // given
-        LocalDateTime today = LocalDateTime.of(2024, 12, 9, 10, 10);
-        Attendance attendance = new Attendance();
+        LocalDateTime attendanceDateTime = LocalDateTime.of(2024, 12, 9, 10, 10);
+        Attendance attendance = Attendance.from(attendanceDateTime);
 
         // when
-        String message = attendance.check(today);
+        AttendanceState attendanceState = attendance.check();
 
         // then
-        assertThat(message).isEqualTo("12월 09일 월요일 10:10 (출석)");
+        assertThat(attendanceState).isEqualTo(AttendanceState.ATTEND);
     }
 
     @ParameterizedTest
     @DisplayName("출석 시간으로부터 30분 초과는 결석이다")
     @MethodSource("provideDateTimeForAbsent")
-    void over_enterTime_then_absent(LocalDateTime today, String result) {
+    void over_enterTime_then_absent(LocalDateTime attendanceDateTime) {
         // given
-        Attendance attendance = new Attendance();
+        Attendance attendance = Attendance.from(attendanceDateTime);
 
         // when
-        String message = attendance.check(today);
+        AttendanceState attendanceState = attendance.check();
 
         // then
-        assertThat(message).isEqualTo(result);
+        assertThat(attendanceState).isEqualTo(AttendanceState.ABSENT);
     }
 
     private static Stream<Arguments> provideDateTimeForAbsent() {
         return Stream.of(
                 Arguments.of(
-                        LocalDateTime.of(2024, 12, 10, 10, 31),
-                        "12월 10일 화요일 10:31 (결석)"
-                        ),
-                Arguments.of(
-                        LocalDateTime.of(2024, 12, 9, 13, 31),
-                        "12월 09일 월요일 13:31 (결석)"
+                        LocalDateTime.of(2024, 12, 10, 10, 31)
                 ),
                 Arguments.of(
-                        LocalDateTime.of(2024, 12, 9, 14, 31),
-                        "12월 09일 월요일 14:31 (결석)"
+                        LocalDateTime.of(2024, 12, 9, 13, 31)
                 ),
                 Arguments.of(
-                        LocalDateTime.of(2024, 12, 10, 11, 31),
-                        "12월 10일 화요일 11:31 (결석)"
+                        LocalDateTime.of(2024, 12, 9, 14, 31)
+                ),
+                Arguments.of(
+                        LocalDateTime.of(2024, 12, 10, 11, 31)
                 )
         );
     }
 
     @Nested
-    class 출석_예외_테스트{
+    class AttendanceExceptionTest {
         @Test
         @DisplayName("캠퍼스 운영시간 전에 출석하면 예외가 발생한다")
         void under_operating_time_then_exception() {
             // given
-            LocalDateTime today = LocalDateTime.of(2024, 12, 9, 7, 10);
-            Attendance attendance = new Attendance();
+            LocalDateTime attendanceDateTime = LocalDateTime.of(2024, 12, 9, 7, 10);
 
             // when-then
-            assertThatThrownBy(() -> attendance.check(today))
+            assertThatThrownBy(() -> Attendance.from(attendanceDateTime))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("[ERROR] 출석 시간이 아닙니다.");
         }
@@ -80,11 +75,10 @@ public class AttendanceTest {
         @DisplayName("캠퍼스 운영시간 후에 출석하면 예외가 발생한다")
         void over_operating_time_then_exception() {
             // given
-            LocalDateTime today = LocalDateTime.of(2024, 12, 9, 23, 10);
-            Attendance attendance = new Attendance();
+            LocalDateTime attendanceDateTime = LocalDateTime.of(2024, 12, 9, 23, 10);
 
             // when-then
-            assertThatThrownBy(() -> attendance.check(today))
+            assertThatThrownBy(() -> Attendance.from(attendanceDateTime))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("[ERROR] 출석 시간이 아닙니다.");
         }
@@ -93,11 +87,10 @@ public class AttendanceTest {
         @DisplayName("주말에 출석하면 예외가 발생한다")
         void attendance_weekend_then_exception() {
             // given
-            LocalDateTime today = LocalDateTime.of(2024, 12, 8, 10, 10);
-            Attendance attendance = new Attendance();
+            LocalDateTime attendanceDateTime = LocalDateTime.of(2024, 12, 8, 10, 10);
 
             // when-then
-            assertThatThrownBy(() -> attendance.check(today))
+            assertThatThrownBy(() -> Attendance.from(attendanceDateTime))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("[ERROR] 주말에는 출석할 수 없습니다.");
         }
@@ -106,14 +99,53 @@ public class AttendanceTest {
         @DisplayName("공휴일에 출석하면 예외가 발생한다")
         void attendance_holiday_then_exception() {
             // given
-            LocalDateTime today = LocalDateTime.of(2024, 12, 25, 10, 10);
-            Attendance attendance = new Attendance();
+            LocalDateTime attendanceDateTime = LocalDateTime.of(2024, 12, 25, 10, 10);
 
             // when-then
-            assertThatThrownBy(() -> attendance.check(today))
+            assertThatThrownBy(() -> Attendance.from(attendanceDateTime))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("[ERROR] 공휴일에는 출석할 수 없습니다.");
         }
     }
+
+//    @Nested
+//    class Update {
+//        @ParameterizedTest
+//        @DisplayName("출석 기록을 수정할 수 있다")
+//        @MethodSource("provideDateAndTimeForUpdate")
+//        void updateTest(LocalTime originTime, LocalDate updateDate, LocalTime updateTime, String result) {
+//            // given
+//            Attendance attendance = Attendance.from(attendanceDateTime);
+//
+//            // when
+//            String expectedResult = attendance.update(originTime, nickname, updateDate, updateTime);
+//
+//            // then
+//            assertThat(expectedResult).isEqualTo(result);
+//        }
+//
+//        private static Stream<Arguments> provideDateAndTimeForUpdate() {
+//            return Stream.of(
+//                    Arguments.of(
+//                            LocalTime.of(10, 7),
+//                            LocalDate.of(2024, 12, 3),
+//                            LocalTime.of(9, 59),
+//                            "12월 03일 화요일 10:07 (지각) -> 09:59 (출석) 수정 완료!"
+//                    ),
+//                    Arguments.of(
+//                            LocalTime.of(10, 7),
+//                            LocalDate.of(2024, 12, 3),
+//                            LocalTime.of(10, 12),
+//                            "12월 03일 화요일 10:07 (지각) -> 10:12 (지각) 수정 완료!"
+//                    ),
+//                    Arguments.of(
+//                            LocalTime.of(10, 7),
+//                            LocalDate.of(2024, 12, 4),
+//                            LocalTime.of(9, 59),
+//                            "12월 04일 수요일 10:07 (지각) -> 09:59 (출석) 수정 완료!"
+//                    )
+//            );
+//        }
+//    }
 
 }
