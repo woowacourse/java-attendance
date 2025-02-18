ff55b4ff0714777680c4973a830d2ddbd2e3d820
saera <yook.saera@gmail.com>
2025-02-18 14:59:09 +0900

    test: 월요일 출석, 지각, 결석 확인 실패하는 테스트 작성

--- src/test/java/attendance/AttendanceTest.java
+++ src/test/java/attendance/AttendanceTest.java
@@ -9,21 +9,42 @@ public class AttendanceTest {
     @DisplayName("출석")
     @Test
     void test1() {
-        int result = Attendance.method(10, 5);
+        int result = Attendance.checkAttendance(10, 5);
         assertThat(result).isEqualTo(1);
     }
 
     @DisplayName("지각")
     @Test
     void test2() {
-        int result = Attendance.method(10, 6);
+        int result = Attendance.checkAttendance(10, 6);
         assertThat(result).isEqualTo(0);
     }
 
     @DisplayName("결석")
     @Test
     void test3() {
-        int result = Attendance.method(10, 31);
+        int result = Attendance.checkAttendance(10, 31);
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @DisplayName("월요일 출석")
+    @Test
+    void test4() {
+        int result = Attendance.checkMondayAttendance(13, 5);
         assertThat(result).isEqualTo(1);
+    }
+
+    @DisplayName("월요일 지각")
+    @Test
+    void test5() {
+        int result = Attendance.checkMondayAttendance(13, 6);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @DisplayName("월요일 결석")
+    @Test
+    void test6() {
+        int result = Attendance.checkMondayAttendance(13, 31);
+        assertThat(result).isEqualTo(-1);
     }
 }
