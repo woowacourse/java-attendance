5b39a473b79634a261a8c6a01de97d26fe4f858a
YuyoungRhee <bbclara@naver.com>
2025-02-18 14:38:00 +0900

    feat: 요일 시작 시간 model 작성

---
+++ src/main/java/attendance/model/WoowaDayOfWeek.java
@@ -0,0 +1,26 @@
+package attendance.model;
+
+import java.time.LocalTime;
+
+public enum WoowaDayOfWeek {
+    월요일(LocalTime.of(13, 0), LocalTime.of(18, 0)),
+    화요일(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+    수요일(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+    목요일(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+    금요일(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+    토요일(null, null),
+    일요일(null, null),
+    ;
+
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+
+    WoowaDayOfWeek(LocalTime startTime, LocalTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+}
