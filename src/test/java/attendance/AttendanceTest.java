2270de92c333b82e8e1b1a58c51de97a159d6f33
saera <yook.saera@gmail.com>
2025-02-18 15:14:00 +0900

    test: 캠퍼스 운영 시작 시간 확인 실패하는 테스트 작성

--- src/test/java/attendance/AttendanceTest.java
+++ src/test/java/attendance/AttendanceTest.java
@@ -1,6 +1,8 @@
 package attendance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,5 +48,19 @@ public class AttendanceTest {
     void test6() {
         int result = Attendance.checkMondayAttendance(13, 31);
         assertThat(result).isEqualTo(-1);
+    }
+
+    @DisplayName("캠퍼스 운영 시간 외 예외 발생")
+    @Test
+    void test7() {
+        assertThatThrownBy(Attendance.method1(7, 59))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 현재 캠퍼스 운영시간이 아닙니다.");
+    }
+
+    @DisplayName("캠퍼스 운영 시간 내 통과")
+    @Test
+    void test8() {
+        assertThatCode(Attendance.method1(8, 0)).doesNotThrowAnyException();
     }
 }
