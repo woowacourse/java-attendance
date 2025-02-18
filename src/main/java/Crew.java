934a67d672699e392d6eddf42b8bbffecd112e6a
DongYeop <s01054830455@gmail.com>
2025-02-18 14:55:54 +0900

    test: 크루 생성 테스트 추가

---
+++ src/main/java/Crew.java
@@ -0,0 +1,12 @@
+public class Crew {
+
+    private final String name;
+
+    public Crew(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
