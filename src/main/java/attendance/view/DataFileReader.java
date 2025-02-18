342930dededbbd26dc23e2e5aadd0c38b69fdd7d
saera <yook.saera@gmail.com>
2025-02-18 21:20:39 +0900

    feat: 프로그램 시작 시 데이터를 attendances.csv에서 읽어오는 기능 구현

---
+++ src/main/java/attendance/view/DataFileReader.java
@@ -0,0 +1,25 @@
+package attendance.view;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataFileReader {
+    private final String FILE_PATH = "src/main/resources/attendances.csv";
+
+    public List<String> read() {
+        List<String> datas = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_PATH))) {
+            bufferedReader.readLine();
+            String input;
+            while((input = bufferedReader.readLine()) != null){
+                datas.add(input);
+            }
+        } catch (IOException e) {
+            System.out.println("입출력 예외 발생: " + e.getMessage());
+        }
+        return datas;
+    }
+}
