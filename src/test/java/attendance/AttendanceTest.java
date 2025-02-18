bbf2b96b1e85b5ff5d183227a59f2dbb94f1d706
saera <yook.saera@gmail.com>
2025-02-18 15:41:40 +0900

    test: 주말 및 공휴일 출석 가능 여부 확인 실패 테스트 작성

--- src/test/java/attendance/AttendanceTest.java
+++ src/test/java/attendance/AttendanceTest.java
@@ -76,5 +76,27 @@ public class AttendanceTest {
     @Test
     void test10() {
         assertThatCode(() -> Attendance.method1(23, 0)).doesNotThrowAnyException();
+    }   
+
+    @DisplayName("공휴일 출석 실패")
+    @Test
+    void test11() {
+        assertThatThrownBy(() -> Attendance.method2(25))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 12월 25일 수요일은 등교일이 아닙니다.");
+    }
+
+    @DisplayName("주말 출석 실패")
+    @Test
+    void test11() {
+        assertThatThrownBy(() -> Attendance.method2(22))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 12월 22일 일요일은 등교일이 아닙니다.");
+    }
+
+    @DisplayName("평일 출석 통과")
+    @Test
+    void test12() {
+        assertThatCode(() -> Attendance.method2(24)).doesNotThrowAnyException();
     }
 }
