1c5191c4d860b564415668a69a961c529635b2ee
junho <wnsgh732@gmail.com>
2025-02-18 17:52:58 +0900

    feat: 결석 횟수 계산 로직 추가
    - 공휴일 확인, LocalDate와 LocalTime을 합치기 위한 DateUtil 추가
    -  주말을 확인하는 값을 enum으로 변경
    - 결석 횟수 확인 테스트 코드 추가

--- src/main/java/domain/Crew.java
+++ src/main/java/domain/Crew.java
@@ -1,10 +1,14 @@
 package domain;
 
+import domain.util.DateUtil;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
+
+import static domain.util.DateUtil.assembleDateAndTime;
 
 public class Crew {
     private final String name;
@@ -35,5 +39,23 @@ public class Crew {
 
     public LocalTime getAttendanceTime(final LocalDate date) {
         return attendanceBook.getOrDefault(date, LocalTime.of(0, 0));
+    }
+
+    public int calculateAbsence(int now) {
+        LocalDate localDate = LocalDate.of(2024, 12, 1);
+        int absenceCount = 0, tardyCount = 0;
+        for (int day = 0; day < now; day++) {
+            if (!attendanceBook.containsKey(localDate) && !DateUtil.isWeekend(localDate)) {
+                absenceCount++;
+            }
+            if (attendanceBook.containsKey(localDate)) {
+                LocalTime localTime = attendanceBook.get(localDate);
+                AttendanceStatus attend = AttendanceStatus.attend(assembleDateAndTime(localDate, localTime));
+                if (attend == AttendanceStatus.ABSENCE) absenceCount++;
+                if (attend == AttendanceStatus.TARDY) tardyCount++;
+            }
+            localDate = localDate.plusDays(1);
+        }
+        return absenceCount + tardyCount / 3;
     }
 }
