6f6ff473de45c1d16b818b3f5f08e005c05eec2e
BWY <quantumjump10@gmail.com>
2025-02-18 19:25:11 +0900

    test: 문자열을 LocalTime으로 변환하는 테스트 구현

--- src/test/java/AttendanceCheckTest.java
+++ src/test/java/AttendanceCheckTest.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class AttendanceCheckTest {
 
@@ -55,4 +58,39 @@ public class AttendanceCheckTest {
         final var expected = "2월 18일 화요일 10:31 (결석)";
         assertThat(actual).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"10:31"})
+    void 입력받은_문자열이_LocalTime_객체로_정상변환된다(String value) {
+        LocalTime standardTime = LocalTime.of(10, 0);
+
+        LocalTime attendanceTime = AttendanceCheck.convertToLocalTime(value);
+
+        final var attendanceCheck = AttendanceCheck.checkAttendanceStatus(standardTime, attendanceTime);
+        final var today = LocalDate.now();
+        final var month = today.getMonth().getValue();
+        final var date = today.getDayOfMonth();
+        final var dayOfWeek = AttendanceCheck.convertKorean(today);
+
+        final var actual = month + "월 " + date + "일 " + dayOfWeek + " " + attendanceTime + " (" + attendanceCheck + ")";
+        final var expected = "2월 18일 화요일 10:31 (결석)";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"10:61"})
+    void 존재하지않는_시간을_입력하면_예외가_발생한다(String value) {
+        assertThatThrownBy(() -> AttendanceCheck.convertToLocalTime(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"테스트:00"})
+    void 시간형식이_아닌값을_입력하면_예외가_발생한다(String value) {
+        assertThatThrownBy(() -> AttendanceCheck.convertToLocalTime(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
 }
