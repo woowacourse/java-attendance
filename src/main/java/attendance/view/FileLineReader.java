d3a070ccd8c20b6c283e0727a473b1902d97b487
ykmxxi <ykmxxi97@gmail.com>
2025-02-18 15:58:30 +0900

    refactor: 입출력 관련 패키지로 이동

--- src/main/java/attendance/view/FileLineReader.java
+++ src/main/java/attendance/view/FileLineReader.java
@@ -1,4 +1,4 @@
-package attendance;
+package attendance.view;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
--- src/test/java/attendance/view/FileReaderTest.java
+++ src/test/java/attendance/view/FileReaderTest.java
@@ -1,4 +1,4 @@
-package attendance;
+package attendance.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
