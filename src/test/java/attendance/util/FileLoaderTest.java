c33a04de28bcd2bda946c08a136bb7c9d9cdf15a
saera <yook.saera@gmail.com>
2025-02-18 20:00:23 +0900

    test: FileLoader의 Attendance 생성 실패 테스트 작성

---
+++ src/test/java/attendance/util/FileLoaderTest.java
@@ -0,0 +1,18 @@
+package attendance.util;
+
+import attendance.domain.Attendance;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FileLoaderTest {
+
+    @DisplayName("Attendance 생성 성공")
+    @Test
+    void test1(){
+        String data = "쿠키,2024-12-13 10:08";
+        Assertions.assertThatCode(() -> FileLoader.load(data))
+                .isNotNull()
+                .isInstanceOf(Attendance.class);
+    }
+}
