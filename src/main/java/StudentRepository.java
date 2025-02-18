ba58a73bae5284f0945da9d405cc7b872cb44831
(minjae8563) <(minjae8563@naver.com)>
2025-02-18 15:15:21 +0900

    feat : Student 객체를 담고있는 StudentRepository 클래스 및 학생 유무 검증 로직 구현

---
+++ src/main/java/StudentRepository.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StudentRepository {
+    List<String> studentNames;
+
+    public boolean notExistStudent(String studentName) {
+        return false;
+    }
+
+}
