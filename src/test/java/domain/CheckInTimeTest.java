4bc3f5608f733ded0fb6b4ad2f2ec59ecc9c1237
geonwoo <kysub99@naver.com>
2025-02-18 16:56:55 +0900

    test(CheckInTime): CheckInTime 도메인 테스트 코드 작성

---
+++ src/test/java/domain/CheckInTimeTest.java
@@ -0,0 +1,63 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class CheckInTimeTest {
+
+    @Test
+    @DisplayName("주말에 출석할 경우 예외 발생")
+    void weekendCheckInThrowException() {
+        LocalDateTime time = LocalDateTime.of(2024, 12, 28, 11, 11, 11);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> CheckInTime.of(time))
+                .withMessageContaining("[ERROR]");
+    }
+
+    @Test
+    @DisplayName("공휴일에 출석할 경우 예외 발생")
+    void publicHolidayCheckInThrowException() {
+        LocalDateTime time = LocalDateTime.of(2024, 12, 25, 11, 11, 11);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> CheckInTime.of(time))
+                .withMessageContaining("[ERROR]");
+    }
+
+    @Test
+    @DisplayName("출석 시간으로 출석 판단")
+    void decidePresenceTest() {
+        LocalDateTime time = LocalDateTime.of(2024, 12, 10, 9, 50, 10);
+        CheckInTime checkInTime = CheckInTime.of(time);
+
+        AttendanceStatus status = checkInTime.getAttendanceStatus(workingTime);
+
+        assertThat(status).isEqualTo(AttendanceStatus.PRESENCE);
+    }
+
+    @Test
+    @DisplayName("출석 시간으로 지각 판단")
+    void decideLateTest() {
+        LocalDateTime time = LocalDateTime.of(2024, 12, 10, 10, 5, 10);
+        CheckInTime checkInTime = CheckInTime.of(time);
+
+        AttendanceStatus status = checkInTime.getAttendanceStatus(workingTime);
+
+        assertThat(status).isEqualTo(AttendanceStatus.LATE);
+    }
+
+    @Test
+    @DisplayName("출석 시간으로 결석 판단")
+    void decideAbsenceTest() {
+        LocalDateTime time = LocalDateTime.of(2024, 12, 10, 10, 30, 10);
+        CheckInTime checkInTime = CheckInTime.of(time);
+
+        AttendanceStatus status = checkInTime.getAttendanceStatus(workingTime);
+
+        assertThat(status).isEqualTo(AttendanceStatus.ABSENCE);
+    }
+}
