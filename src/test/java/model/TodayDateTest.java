b6cb82beb4c215301900b02350e8f50dc5a4475b
(minjae8563) <(minjae8563@naver.com)>
2025-02-19 15:47:25 +0900

    test : String 을 입력 받고, 검증하는 테스트와 LocalDateTime 으로 반환하는 테스트 생성

--- src/test/java/model/TodayDateTest.java
+++ src/test/java/model/TodayDateTest.java
@@ -2,9 +2,30 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TodayDateTest {
+    @Test
+    @DisplayName("시간을 입력받아 LocalDateTime 으로 변환하는 메서드 테스트")
+    void test1(){
+        String time = "09:59";
+        TodayDate todayDate = new TodayDate(LocalDate.of(2024, 12, 14));
+        LocalDateTime localDateTime = todayDate.makeLocalDateToLocalDateTime(time);
+        LocalDateTime expect = LocalDateTime.of(2024, 12, 14, 9, 59);
+        Assertions.assertTrue(localDateTime.isEqual(expect));
+    }
 
+    @Test
+    @DisplayName("형식이 잘못된 시간을 입력하면 예외 발생 테스트")
+    void test2() {
+        String time = "09:71";
+        TodayDate todayDate = new TodayDate(LocalDate.of(2024, 12, 14));
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> todayDate.makeLocalDateToLocalDateTime(time))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 시간 형식에 맞지 않습니다.");
+    }
 }
