14e7d02df619f7d744a528bc5155c582a9776ae6
egaeng09 <egaeng09@gmail.com>
2025-02-19 15:34:41 +0900

    feat: 실행 날짜 custom 지정 기능 추가
    - 요구사항에서 필요한 날짜가 실제 기준으로 과거이기 때문에 날짜 유틸 클래스를 통해 실행 기준일을 별도로 지정하였습니다.

--- src/main/java/domain/util/DateUtil.java
+++ src/main/java/domain/util/DateUtil.java
@@ -6,6 +6,14 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class DateUtil {
+    public static LocalDateTime TODAY = LocalDateTime.of(LocalDate.of(2024, 12, 07), LocalTime.of(10, 30, 00));
+
+    public static LocalDate getFirstDateOfMonth() {
+        return TODAY.toLocalDate()
+                .atStartOfDay()
+                .toLocalDate();
+    }
+
     public static boolean isWeekend(LocalDate localDate) {
         //TODO: 공휴일 체크
         return localDate.getDayOfWeek() == DayOfWeek.SATURDAY || localDate.getDayOfWeek() == DayOfWeek.SUNDAY;
