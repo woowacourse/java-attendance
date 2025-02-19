8c45f4c5efa9bb564054f9f6b043f867291499e1
jhpark1227 <junhyeok0246@naver.com>
2025-02-19 13:50:38 +0900

    feat: Attendance 클래스 구현

---
+++ src/main/java/model/Attendance.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Attendance {
+
+    private final Crew crew;
+    private final LocalDateTime checkInTime;
+
+    private Attendance(Crew crew, LocalDateTime checkInTime) {
+        this.crew = crew;
+        this.checkInTime = checkInTime;
+    }
+
+    public static Attendance of(Crew crew, LocalDateTime checkInTime) {
+        return new Attendance(crew, checkInTime);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Attendance that = (Attendance) o;
+        return Objects.equals(crew, that.crew) && Objects.equals(checkInTime, that.checkInTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(crew, checkInTime);
+    }
+}
