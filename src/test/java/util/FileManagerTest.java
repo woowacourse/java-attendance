2d6942e3ad49eb7d66fa5723b023646f555d1430
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-19 14:54:15 +0900

    test(FileManagerTest): csv 파일 입출력을 그린 테스트하라
    - csv 파일 전체를 읽어온다.

--- src/test/java/util/FileManagerTest.java
+++ src/test/java/util/FileManagerTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,23 +16,31 @@ public class FileManagerTest {
 
     @Test
     @DisplayName("csv 파일 전체를 읽어온다")
-    void readTest1() throws URISyntaxException, IOException {
+    void readTest1() {
         //given
-        final String path = "src/main/resources/";
-        final String fileName = "attendance.csv";
+        final String fileName = "attendances.csv";
+        final int expectedSize = 42;
+        final String expectedHeader = "nickname,datetime";
 
         //when
         final Queue<String> data = FileManger.readFileLines(fileName);
 
         //then
-        assertThat(data).isNotEmpty();
-
+        assertThat(data)
+                .isNotEmpty()
+                .hasSize(expectedSize)
+                .startsWith(expectedHeader);
     }
 
     private class FileManger {
 
         public static Queue<String> readFileLines(final String fileName) {
-            return null;
+            final String path = "src/main/resources/";
+            try {
+                return new ArrayDeque<>(Files.readAllLines(Path.of(path + fileName)));
+            } catch (final IOException e) {
+                throw new IllegalStateException(e);
+            }
         }
     }
 }
