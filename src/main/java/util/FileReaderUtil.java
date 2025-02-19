7c50295a4c8e66cef9abdb2cee3e517825423952
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-19 17:09:32 +0900

    feat: 파일 불러오기 기능 추가

---
+++ src/main/java/util/FileReaderUtil.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+
+public class FileReaderUtil {
+
+    // TODO 인자로 받아서 생성자에서 사용하도록...
+    private static final String ATTENDANCE_DATA_PATH = "src/main/resources/attendances.csv";
+    private static final int HEADER = 1;
+
+    public List<String> read() {
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(ATTENDANCE_DATA_PATH))) {
+
+            return reader.lines()
+                    .skip(HEADER)
+                    .toList();
+
+        } catch (IOException e) {
+            throw new IllegalArgumentException("[ERROR] 출석 데이터를 읽어오는데 실패했습니다");
+        }
+    }
+}
