395a8fc797dadf3d2b999d85a07d5a77820b682d
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-19 17:16:32 +0900

    feat(Attendances): 출석 확인 및 수정 기능 관련 메서드 추가

--- src/main/java/attendance/domain/Attendances.java
+++ src/main/java/attendance/domain/Attendances.java
@@ -13,7 +13,7 @@ public class Attendances {
         this.attendances = new ArrayList<>();
     }
 
-    public void initAttendances(Crews crews, List<List<String>> attendanceRecords) {
+    public void initAttendances(final Crews crews, final List<List<String>> attendanceRecords) {
         for (List<String> attendanceRecord : attendanceRecords) {
             String crewName = attendanceRecord.getFirst();
             Crew crew = crews.findCrew(crewName);
@@ -27,5 +27,39 @@ public class Attendances {
             attendances.add(new Attendance(crew, localDateTime, status));
         }
 
+    }
+
+    public void add(Attendance attendance) {
+        attendances.add(attendance);
+    }
+
+    public Attendance findMatchCrewDate(Crew crew, LocalDate localDate) {
+        return attendances.stream()
+                .filter(attendance -> attendance.isSameCrewDate(crew, localDate))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 날짜에 해당 크루의 출석 기록이 존재하지 않습니다."));
+    }
+
+    public void modifyAttendances(final Crew crew, final LocalDateTime localDateTime) {
+        for (Attendance attendance : attendances) {
+            modifyAttendance(crew, localDateTime, attendance);
+        }
+    }
+
+    private static void modifyAttendance(final Crew crew, final LocalDateTime localDateTime,
+                                         final Attendance attendance) {
+        LocalDate localDate = localDateTime.toLocalDate();
+        if (attendance.isSameCrewDate(crew, localDate)) {
+            attendance.modifyLocalDateTime(localDateTime);
+            attendance.modifyAttendanceType(localDateTime);
+        }
+    }
+
+    public String findOriginalTime(final Crew crew, final LocalDate localDate) {
+        return findMatchCrewDate(crew, localDate).getTime();
+    }
+
+    public String findOriginalType(final Crew crew, final LocalDate localDate) {
+        return findMatchCrewDate(crew, localDate).getType();
     }
 }
