9b2912d1a4429835e60c39832fc66863948ebae2
saera <yook.saera@gmail.com>
2025-02-19 16:54:40 +0900

    feat: 출석, 지각, 결석 상태를 나타내는 Enum 생성

---
+++ src/main/java/attendance/domain/AttendanceStatus.java
@@ -0,0 +1,12 @@
+package attendance.domain;
+
+public enum AttendanceStatus {
+    PRESENT("출석"), LATENESS("지각"), ABSENCE("결석");
+
+    private String status;
+    AttendanceStatus(String status) {}
+
+    public String getStatus() {
+        return status;
+    }
+}
