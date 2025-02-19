b76c6bb661b4dc8224850b68e5b5aff13a440574
jhan0121 <jhan0121@gmail.com>
2025-02-19 18:33:38 +0900

    feat: csv 타입의 파일을 읽어서 AttendanceBook 을 반환하는 기능

---
+++ src/main/java/AttendanceController.java
@@ -0,0 +1,40 @@
+import java.net.URL;
+import java.util.List;
+
+public class AttendanceController {
+
+    private static final String CSV_PATH = "attendances.csv";
+
+    public void run() {
+        List<String> rows = readCsv();
+        AttendanceBook attendanceBook = loadAttendanceBook(rows);
+
+        for (String row : rows) {
+            System.out.println(row);
+        }
+    }
+
+    private AttendanceBook loadAttendanceBook(List<String> data) {
+        AttendanceBook attendanceBook = new AttendanceBook();
+        for (String row : data) {
+            attendanceBook.attend(parseName(row), parseAttend(row));
+        }
+        return attendanceBook;
+    }
+
+    private Attend parseAttend(String row) {
+        String dateTime = row.split(",")[1];
+        String day = dateTime.substring(8, 10);
+        String time = dateTime.substring(11);
+        return Attend.of(day, time);
+    }
+
+    private String parseName(String row) {
+        return row.split(",")[0];
+    }
+
+    private List<String> readCsv() {
+        URL fileURL = AttendanceController.class.getResource(CSV_PATH);
+        return FileUtil.readFile(fileURL);
+    }
+}
