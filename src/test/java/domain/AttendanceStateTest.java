b6fd13ae8e7bbb2fc8f85ffceba09ee83c831766
DongYeop <s01054830455@gmail.com>
2025-02-19 18:30:56 +0900

    test: 출석 확인 테스트 생성(월요일/나머지 구분)

---
+++ src/test/java/domain/AttendanceStateTest.java
@@ -0,0 +1,32 @@
+package domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class AttendanceStateTest {
+
+    @DisplayName("출석 시간과 요일로 출석 상태를 계산하여 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "13:05,2,출석",
+            "13:06,2,지각",
+            "13:30,2,지각",
+            "13:31,2,결석",
+            "10:05,4,출석",
+            "10:06,4,지각",
+            "10:30,4,지각",
+            "10:31,4,결석"
+    }, delimiter = ',')
+    void statusReturn(LocalTime localTime, int dayOfMonth, String expected) {
+        // given
+        String actual = AttendanceState.findStateBy(localTime, dayOfMonth);
+
+        // when & then
+        assertThat(actual).isEqualTo(expected);
+    }
+}
