0c2a3a70b8c20a76d870da301f21c04c760c47fd
(minjae8563) <(minjae8563@naver.com)>
2025-02-19 13:50:30 +0900

    feat : 파일을 읽는 FileInput 객체 생성

---
+++ src/main/java/util/FileInput.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileInput {
+
+    private final String filePath = "src/main/resources/attendances.csv";
+    private final BufferedReader fileBr;
+
+    public FileInput() throws IOException {
+        fileBr = new BufferedReader(new FileReader(filePath));
+    }
+
+    public ArrayList<String> readAttendanceFile() throws IOException{
+        ArrayList<String> attendenceFile = new ArrayList<>();
+        fileBr.readLine();
+        while(true) {
+            String information = fileBr.readLine();
+            if (information == null) {
+                break;
+            }
+            attendenceFile.add(information);
+        }
+        return attendenceFile;
+    }
+
+}
