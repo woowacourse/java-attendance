bcd9c0647a3434696d247bfce83eb9c96f25f009
RaZel713 <razel7130@gmail.com>
2025-02-19 17:28:39 +0900

    red: 크루별 출석 기록 확인 구현
    - 닉네임을 입력하면 전날까지의 크루 출석 기록을 출력해야 한다
    - 등교하지 않아 출석 기록이 없는 날은 결석으로 간주한다

---
+++ src/test/java/CheckAttendanceRecordTest.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class CheckAttendanceRecordTest {
+    @Test
+    void 닉네임을_입력하면_전날까지의_크루_출석_기록을_출력해야_한다() {
+        AttendanceBook attendanceBook = new AttendanceBook();
+
+        Crew crew1 = Crew.createByName("쿠키");
+        crew1.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 3), LocalTime.of(10, 6)));
+        attendanceBook.addNewCrew(crew1);
+
+        attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 4), LocalTime.of(9, 7)));
+        attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 5), LocalTime.of(14, 7)));
+
+        crew1.getAttendanceRecords();
+    }
+}
