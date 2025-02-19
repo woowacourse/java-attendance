ad1f63248658dde498ec5f589ccd132c160c550e
rladmstn <oz33177351@gmail.com>
2025-02-19 15:10:10 +0900

    feat(AttendanceRepository): 출결 기록 추가 로직 구현

--- src/main/java/attendance/repository/AttendanceRepository.java
+++ src/main/java/attendance/repository/AttendanceRepository.java
@@ -1,6 +1,7 @@
 package attendance.repository;
 
 import attendance.domain.Attendance;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class AttendanceRepository {
@@ -8,5 +9,18 @@ public class AttendanceRepository {
 
     public AttendanceRepository(List<Attendance> attendances) {
         this.attendances = attendances;
+    }
+
+    public void add(Attendance currentAttendance) {
+        for (Attendance attendance : attendances) {
+            if (attendance.isAlreadyAttendance(currentAttendance)) {
+                throw new IllegalArgumentException("[ERROR] 오늘은 이미 출석하셨습니다. 수정 기능을 이용해 주세요.");
+            }
+        }
+        attendances.add(currentAttendance);
+    }
+
+    public List<Attendance> findAttendanceByName() {
+        return List.of(new Attendance("체체", LocalDateTime.now()));
     }
 }
---
+++ src/test/java/attendance/repository/AttendanceRepositoryTest.java
@@ -0,0 +1,44 @@
+package attendance.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import attendance.domain.Attendance;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AttendanceRepositoryTest {
+
+    @DisplayName("출결 기록을 추가한다.")
+    @Test
+    void 출결_기록을_추가한다() {
+
+        // given
+        Attendance attendance = new Attendance("피글렛", LocalDateTime.now());
+        AttendanceRepository attendanceRepository = new AttendanceRepository(new ArrayList<>());
+
+        // when
+        attendanceRepository.add(attendance);
+
+        // then
+        assertThat(attendanceRepository.findAttendanceByName().size()).isEqualTo(1);
+    }
+
+    @DisplayName("해당 날짜에 출결 기록이 있는 크루를 추가할 시 예외가 발생한다.")
+    @Test
+    void 해당_날짜에_출결_기록이_있는_크루를_추가할_시_예외가_발생한다() {
+
+        // given
+        Attendance attendance = new Attendance("체체", LocalDateTime.now());
+        AttendanceRepository attendanceRepository = new AttendanceRepository(new ArrayList<>());
+        attendanceRepository.add(attendance);
+
+        // when & then
+        assertThatThrownBy(() -> attendanceRepository.add(attendance))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 오늘은 이미 출석하셨습니다. 수정 기능을 이용해 주세요.");
+    }
+
+}
