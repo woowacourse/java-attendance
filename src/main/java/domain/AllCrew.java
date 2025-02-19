76641af8abd196eba56aa729d21dc84df9519a24
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-19 17:40:45 +0900

    feat(domain) : 제적 위험자 출력 기능 작성
    - 모든 제적 위험자 조회 결과 출력

---
+++ src/main/java/domain/AllCrew.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllCrew {
+    private final List<Crew> allCrew;
+
+    public AllCrew() {
+        this.allCrew = new ArrayList<>();
+    }
+
+    public void addCrew(Crew crew) {
+        allCrew.add(crew);
+    }
+
+    public void addCrewAttendanceByName(String name, LocalDateTime dateTime) {
+        Crew findCrew = allCrew.stream()
+                .filter(crew -> crew.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Crew not found"));
+
+        findCrew.addAttendance(dateTime);
+    }
+
+    public String printAllCrewWarningInfo(LocalDate date) {
+        String result = "";
+        for(Crew crew : allCrew) {
+            result += crew.printWarningInfo(date) + "\n";
+        }
+        return result;
+    }
+}
