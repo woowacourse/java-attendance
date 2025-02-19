4ac81053047361a791687b631989bf7782c967bf
JO-eusan <goodsan0330@gmail.com>
2025-02-19 20:47:59 +0900

    test: 출석 기록 조회 및 통계 계산

---
+++ src/test/java/AttendanceRecordCheckTest.java
@@ -0,0 +1,52 @@
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import domain.AttendanceManager;
+import domain.Records;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AttendanceRecordCheckTest {
+
+    AttendanceManager attendanceManager = new AttendanceManager();
+    List<LocalDateTime> records = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        records = List.of(
+            LocalDateTime.of(2024, 12, 2, 13, 0), // 출석
+            LocalDateTime.of(2024, 12, 3, 9, 59), // 출석
+            LocalDateTime.of(2024, 12, 4, 10, 6), // 지각
+            LocalDateTime.of(2024, 12, 5, 10, 31),// 결석
+            LocalDateTime.of(2024, 12, 6, 10, 40)); // 결석
+
+        String name = "빙티"; // 경고 대상자
+        attendanceManager.createCrew(name, records);
+    }
+
+    @DisplayName("크루 출석 기록을 전날까지 조회한다.")
+    @Test
+    void 출석_기록_조회() {
+        String name = "빙티";
+        Records expectedRecords = attendanceManager.findByName(name);
+        assertThat(expectedRecords.getAttendanceCount()).isEqualTo(records.size());
+    }
+
+    @DisplayName("출석 통계를 정확하게 계산한다.")
+    @Test
+    void 출석_통계_계산() {
+        String name = "빙티";
+        Records records = attendanceManager.findByName(name);
+        StatisticsResult statisticsResult = AttendanceStatistics.countStatus(records);
+        int attendanceCount = statisticsResult.getAttendanceCount();
+        int latenessCount = statisticsResult.getLatenessCount();
+        int absenceCount = statisticsResult.getAbsenceCount();
+
+        assertThat(attendanceCount).isEqualTo(2);
+        assertThat(latenessCount).isEqualTo(1);
+        assertThat(absenceCount).isEqualTo(2);
+    }
+}
