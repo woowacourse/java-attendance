2d735e80661d011b31b91b50606bfa7bc309c4cb
jhpark1227 <junhyeok0246@naver.com>
2025-02-19 13:50:42 +0900

    feat: Attendances 클래스 구현

---
+++ src/main/java/model/Attendances.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.List;
+
+public class Attendances {
+
+    private final List<Attendance> attendances;
+
+    private Attendances(List<Attendance> attendances) {
+        this.attendances = attendances;
+    }
+
+    public static Attendances of(List<Attendance> attendances) {
+        return new Attendances(attendances);
+    }
+
+    public boolean contains(Attendance attendance) {
+        return attendances.stream().anyMatch(attendance::equals);
+    }
+
+    public void checkIn(Attendance attendance) {
+        attendances.add(attendance);
+    }
+}
