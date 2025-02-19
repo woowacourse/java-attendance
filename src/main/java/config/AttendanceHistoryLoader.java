c02c692426cf4aa4bf8fc1e7f9f21f9cf6b940ed
BWY <quantumjump10@gmail.com>
2025-02-19 19:48:44 +0900

    feat: 출석 기록으로부터 크루원 초기 데이터 세팅
    - 출석 기록을 csv 파일에서 가져오는 기능 구현

---
+++ src/main/java/config/AttendanceHistoryLoader.java
@@ -0,0 +1,53 @@
+package config;
+
+import domain.Attendance;
+import domain.Crew;
+import domain.Crews;
+import domain.Day;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AttendanceHistoryLoader {
+
+    public Crews loadCrews() {
+        List<Crew> crews = new ArrayList<>();
+        Map<String, Crew> crewMap = new HashMap<>();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/attendance2.csv"))) {
+            reader.readLine();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                String nickname = values[0];
+                String datetime = values[1];
+                String[] datetimeValues = datetime.split(" ");
+                String date = datetimeValues[0];
+                String time = datetimeValues[1];
+
+                Crew crew = crewMap.computeIfAbsent(nickname, key -> {
+                    Crew newCrew = new Crew(nickname);
+                    crews.add(newCrew);
+                    return newCrew;
+                });
+
+                crew.addAttendance(new Attendance(new Day(LocalDate.parse(date, dateFormatter)), LocalTime.parse(time, timeFormatter)));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Crews(crews);
+    }
+}
---
+++ src/main/java/domain/Crews.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.List;
+
+public class Crews {
+
+    private final List<Crew> crews;
+
+    public Crews(List<Crew> crews) {
+        this.crews = crews;
+    }
+}
