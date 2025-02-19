f9bcad1bba23c447cde18dd1912bc1c7daa6705d
yeonnhuu <yeonnhuu@gmail.com>
2025-02-19 15:02:12 +0900

    :sparkles: feat(InputView): 입력 기능 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,47 @@
+package view;
+
+import domain.MenuOption;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public String readOption(List<MenuOption> options) {
+        StringBuilder sb = new StringBuilder();
+        for (MenuOption option : options) {
+            sb.append(System.lineSeparator())
+                    .append(option.getCommand())
+                    .append(". ")
+                    .append(option.getOption());
+        }
+        return prompt(sb.toString());
+    }
+
+    public String readNickname() {
+        return prompt("닉네임을 입력해 주세요.");
+    }
+
+    public String readArrivalTime() {
+        return prompt("등교 시간을 입력해 주세요.");
+    }
+
+    public String readEditNickname() {
+        return prompt("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+    }
+
+    public String readEditArrivalDate() {
+        return prompt("수정하려는 날짜(일)를 입력해 주세요.");
+    }
+
+    public String readEditArrivalTime() {
+        return prompt("언제로 변경하겠습니까?");
+    }
+
+
+    private String prompt(String message) {
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+}
