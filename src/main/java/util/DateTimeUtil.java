dc6dcfca305bc7e07fdb9350d40c4374d9936d4d
ppparkta <pppppark2@gmail.com>
2025-02-19 17:54:10 +0900

    feat: LocalDate/Time 한국어 변환 기능 추가

--- src/main/java/util/DateTimeUtil.java
+++ src/main/java/util/DateTimeUtil.java
@@ -3,6 +3,9 @@ package util;
 import java.time.LocalDateTime;
 
 public class DateTimeUtil {
+    private final static DateTimeFormatter localDateFormatter = DateTimeFormatter.ofPattern("MM월 dd일");
+    private final static DateTimeFormatter localTimeFormatter = DateTimeFormatter.ofPattern("hh시 mm분");
+
     public static int getDayOfWeek(LocalDateTime localDateTime) {
         return localDateTime.getDayOfWeek().getValue();
     }
