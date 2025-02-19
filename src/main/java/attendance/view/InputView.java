e1d646d6a317ec3f2f9dd607878925f28fa6070c
kimdev <kim__dev@hufs.ac.kr>
2025-02-19 19:21:46 +0900

    feat: 기능 선택 입력을 받는 기능 구현

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -1,4 +1,15 @@
 package attendance.view;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class InputView {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public OperationCommand readOperationCommand() {
+        String commandText = scanner.nextLine();
+        return OperationCommand.from(commandText);
+    }
 }
