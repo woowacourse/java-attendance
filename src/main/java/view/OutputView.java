ca3a0184a51cbd9f2485230ee5adf4ed3cdd3596
(minjae8563) <(minjae8563@naver.com)>
2025-02-19 20:49:29 +0900

    feat : 각 메뉴에 맞는 출력 형식 OutputView 에 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,13 @@
 package view;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import model.AttendanceStatus;
 import model.Student;
+import model.StudentPunishment;
+import model.StudentRepository;
 import util.LocalDateTimePrintFormatter;
 
 public class OutputView {
@@ -12,6 +18,56 @@ public class OutputView {
                 String dateAndTime = LocalDateTimePrintFormatter.LocalDateTimeToLocalTime(localDateTimeIn);
                 String state = student.getRecord().get(localDateTimeIn).getState();
                 System.out.println(dateAndTime + "(" + state +")");
+            }
+        }
+    }
+
+    public static void printSecondMenu(String recordBeforeModify, String localDateTimeFormat3) {
+        System.out.println(recordBeforeModify + " -> " + localDateTimeFormat3);
+    }
+
+    public static void printAttendanceRecord(LinkedHashMap<LocalDateTime, AttendanceStatus> record) {
+        List<Map.Entry<LocalDateTime, AttendanceStatus>> entries =
+                record.entrySet().stream()
+                        .sorted(Map.Entry.comparingByKey())
+                        .toList();
+        for (Map.Entry<LocalDateTime,AttendanceStatus> entry : entries) {
+            System.out.println(LocalDateTimePrintFormatter.LocalDateTimeToLocalTime(entry.getKey()) + "(" +entry.getValue().getState() + ")");
+        }
+    }
+
+    public static void printStudentState(Student student) {
+        System.out.println("출석: " + student.attendance + "회");
+        System.out.println("지각" + student.late + "회");
+        System.out.println("결석" + student.absent + "회");
+    }
+
+    public static void printStudentPunishmentLabel(Student student) {
+        if (student.calculateAbsent() > StudentPunishment.DISMISSAL.getStandard()) {
+            System.out.println("제적 대상자입니다.");
+            return;
+        }
+        if (student.calculateAbsent() >= StudentPunishment.INTERVIEW.getStandard()) {
+            System.out.println("면담 대상자입니다.");
+            return;
+        }
+        if (student.calculateAbsent() >= StudentPunishment.WARNING.getStandard()) {
+            System.out.println("경고 대상자입니다.");
+        }
+    }
+
+    public static void printEveryStudentPunishmentLabel(StudentRepository studentRepository) {
+        for (Student student : studentRepository.getStudents()) {
+            if (student.calculateAbsent() > StudentPunishment.DISMISSAL.getStandard()) {
+                System.out.println("- " + student.getName() + ": 결석" + student.getAbsent() + "회, 지각 " + student.getLate() + "회 (" + StudentPunishment.DISMISSAL.getPunishmentLabel() + ")" );
+                return;
+            }
+            if (student.calculateAbsent() >= StudentPunishment.INTERVIEW.getStandard()) {
+                System.out.println("- " + student.getName() + ": 결석" + student.getAbsent() + "회, 지각 " + student.getLate() + "회 (" + StudentPunishment.INTERVIEW.getPunishmentLabel() + ")" );
+                return;
+            }
+            if (student.calculateAbsent() >= StudentPunishment.WARNING.getStandard()) {
+                System.out.println("- " + student.getName() + ": 결석" + student.getAbsent() + "회, 지각 " + student.getLate() + "회 (" + StudentPunishment.WARNING.getPunishmentLabel() + ")" );
             }
         }
     }
