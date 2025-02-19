4c03ef9c8805cb88e593612091242264f49dd1d2
(minjae8563) <(minjae8563@naver.com)>
2025-02-19 20:46:12 +0900

    feat : getter 메서드 생성, 입력 받은 날짜를 기준으로 수정할 때 로직 구현, LocalDateTime 두 개를 일자까지 비교하여 같은지 판단하는 메서드 구현

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -3,6 +3,7 @@ package model;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.LinkedHashMap;
 
 public class Student {
@@ -12,6 +13,18 @@ public class Student {
     public int absent;
     public int attendance;
     public int late;
+
+    public int getLate() {
+        return late;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAbsent() {
+        return absent;
+    }
 
     public Student(String name) {
         this.name = name;
@@ -33,6 +46,31 @@ public class Student {
         AttendanceStatus attendanceStatus = AttendanceCalculatorByDay.
                 attendanceCalculator(day, LocalTime.from(localDateTime));
 
+        for (LocalDateTime localDateTime1 : record.keySet()) {
+            if (compareDayIsSame(localDateTime1, localDateTime)) {
+                if (record.get(localDateTime1).equals(AttendanceStatus.ATTENDANCE)) {
+                    attendance --;
+                    record.remove(localDateTime1);
+                    record.putIfAbsent(localDateTime, attendanceStatus);
+                    return;
+                }
+
+                if (record.get(localDateTime1).equals(AttendanceStatus.LATE)) {
+                    late --;
+                    record.remove(localDateTime1);
+                    record.putIfAbsent(localDateTime, attendanceStatus);
+                    return;
+                }
+
+                if (record.get(localDateTime1).equals(AttendanceStatus.ABSENT)) {
+                    absent --;
+                    record.remove(localDateTime1);
+                    record.putIfAbsent(localDateTime, attendanceStatus);
+                    return;
+                }
+            }
+        }
+
         record.putIfAbsent(localDateTime, attendanceStatus);
 
         assert attendanceStatus != null;
@@ -49,6 +87,35 @@ public class Student {
 
     public LinkedHashMap<LocalDateTime, AttendanceStatus> getRecord() {
         return record;
+    }
+
+    public boolean compareDayIsSame(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
+        LocalDateTime dayDate1 = localDateTime1.truncatedTo(ChronoUnit.DAYS);
+        LocalDateTime dayDate2 = localDateTime2.truncatedTo(ChronoUnit.DAYS);
+
+        return (dayDate1.compareTo(dayDate2) == 0);
+    }
+
+    public LocalDateTime findLocalDateTime(LocalDateTime localDateTime) {
+        for (LocalDateTime localDateTime1 : record.keySet()) {
+            if (compareDayIsSame(localDateTime1,localDateTime)) {
+                return localDateTime1;
+            }
+        }
+        return null;
+    }
+
+    public String findStateByLocalDateTime(LocalDateTime localDateTime) {
+        for (LocalDateTime localDateTime1 : record.keySet()) {
+            if (compareDayIsSame(localDateTime1,localDateTime)) {
+                return record.get(localDateTime1).getState();
+            }
+        }
+        return null;
+    }
+
+    public int calculateAbsent() {
+        return absent + late/3;
     }
 
 }
