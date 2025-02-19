7b336baea4df46b76af86d4f96974ca3418f1755
yeonnhuu <yeonnhuu@gmail.com>
2025-02-19 15:00:58 +0900

    :sparkles: feat(MenuOption): 메뉴 옵션 추가

---
+++ src/main/java/domain/MenuOption.java
@@ -0,0 +1,26 @@
+package domain;
+
+public enum MenuOption {
+
+    ATTENDANCE_CHECK("1", "출석 확인"),
+    ATTENDANCE_CORRECTION("2", "출석 수정"),
+    CREW_ATTENDANCE_CHECK("3", "크루별 출석 기록 확인"),
+    CHECK_EXPELLED_CREW("4", "제적 위험자 확인"),
+    QUIT("Q", "종료");
+
+    private final String command;
+    private final String option;
+
+    MenuOption(String command, String option) {
+        this.command = command;
+        this.option = option;
+    }
+
+    public String getCommand() {
+        return this.command;
+    }
+
+    public String getOption() {
+        return this.option;
+    }
+}
