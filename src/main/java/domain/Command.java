83c4bb33629046f8c32de6e2fa810b304e0411b7
ryan-dia <cjfdnjs158@naver.com>
2025-02-19 17:34:36 +0900

    feat: 기능 명령 enum 관리 추가
    Co-authored-by: 2Jin1031 <2jin2.1031@gmail.com>

---
+++ src/main/java/domain/Command.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum Command {
+    CHECK_ATTENDEES("출석 확인", "1"),
+    EDIT_ATTENDANCE("출석 수정", "2"),
+    CHECK_THE_ATTENDANCE_RECORD_BY_CREW("크루별 출석 기록 확인", "3"),
+    CONFIRMATION_OF_THOSE_AT_RISK_OF_EXPULSION("제적 위험자 확인", "4"),
+    QUIT("종료", "Q");
+
+    private String commandName;
+    private String commandNumber;
+
+    Command(final String commandName, final String commandNumber) {
+        this.commandName = commandName;
+        this.commandNumber = commandNumber;
+    }
+
+    public static Command findByCommandNumber(final String commandNumber) {
+        return Arrays.stream(Command.values())
+                .filter(c -> c.commandNumber.equals(commandNumber))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("알맞은 명령어를 입력하세요."));
+    }
+}
