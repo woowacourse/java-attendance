779cd2d0586dc52ecddb6ce927e9c5d003ab3fa2
ChoiDongJoon <adong0808@naver.com>
2025-02-19 18:15:27 +0900

    feat: 경고/면담/제적 대상자 확인 기능 구현

--- src/main/java/attendance/domain/Crew.java
+++ src/main/java/attendance/domain/Crew.java
@@ -25,7 +25,7 @@ public class Crew {
 
     public void existInAttendances(LocalDate date) {
         for (Attendance attendance : attendances) {
-            if(attendance.isEqualToDate(date)) {
+            if (attendance.isEqualToDate(date)) {
                 throw new IllegalArgumentException("[ERROR] 이미 오늘 출석을 하셨습니다. 출석 수정을 이용해주세요.");
             }
         }
@@ -49,5 +49,11 @@ public class Crew {
 
     public Map<AttendanceStatus, Integer> getStatusCount() {
         return statusCount;
+    }
+
+    public Warning checkWarning() {
+        int absenceCount = statusCount.getOrDefault(AttendanceStatus.ABSENCE, 0) + statusCount.getOrDefault(AttendanceStatus.LATE_ABSENCE, 0);
+        absenceCount += statusCount.getOrDefault(AttendanceStatus.LATE, 0) / 3;
+        return Warning.check(absenceCount);
     }
 }
--- src/test/java/attendance/CrewTest.java
+++ src/test/java/attendance/CrewTest.java
@@ -2,20 +2,20 @@ package attendance;
 
 import attendance.domain.Attendance;
 import attendance.domain.Crew;
+import attendance.domain.Warning;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class CrewTest {
 
     @Test
     void 이미_출석한_경우_예외_반환() {
-        Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 16, 11, 00));
+        Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 16, 11, 0));
         Crew crew = new Crew("훌라", List.of(attendance));
 
         assertThatThrownBy(() -> crew.existInAttendances(LocalDate.of(2024,12,16)))
@@ -24,9 +24,64 @@ class CrewTest {
 
     @Test
     void 이미_출석하지않은_경우_예외_반환X() {
-        Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 16, 11, 00));
+        Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 16, 11, 0));
         Crew crew = new Crew("훌라", List.of(attendance));
 
         assertThatCode(() -> crew.existInAttendances(LocalDate.of(2024,12,17))).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 결석_2회_이상_경고_대상자() {
+        List<Attendance> attendances = List.of(new Attendance(LocalDateTime.of(2024, 12, 16, 14, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 17, 11, 0)));
+        Crew crew = new Crew("훌라", attendances);
+
+        assertThat(crew.checkWarning()).isEqualTo(Warning.WARN);
+    }
+
+    @Test
+    void 결석_3회_이상_면담_대상자() {
+        List<Attendance> attendances = List.of(new Attendance(LocalDateTime.of(2024, 12, 16, 14, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 17, 11, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 18, 11, 0)));
+        Crew crew = new Crew("훌라", attendances);
+
+        assertThat(crew.checkWarning()).isEqualTo(Warning.INTERVIEW);
+    }
+
+    @Test
+    void 결석_5회_이상_제적_대상자() {
+        List<Attendance> attendances = List.of(new Attendance(LocalDateTime.of(2024, 12, 16, 14, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 17, 11, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 18, 11, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 19, 11, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 20, 11, 0)));
+        Crew crew = new Crew("훌라", attendances);
+
+        assertThat(crew.checkWarning()).isEqualTo(Warning.EXPULSION);
+    }
+
+    @Test
+    void 정상_출석이면_대상자가_아니다() {
+        List<Attendance> attendances = List.of(new Attendance(LocalDateTime.of(2024, 12, 16, 9, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 17, 10, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 18, 10, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 19, 10, 0)),
+                new Attendance(LocalDateTime.of(2024, 12, 20, 10, 0)));
+        Crew crew = new Crew("훌라", attendances);
+
+        assertThat(crew.checkWarning()).isEqualTo(Warning.NONE);
+    }
+
+    @Test
+    void 지각_3회_결석_1회는_경고_대상자() {
+        List<Attendance> attendances = List.of(new Attendance(LocalDateTime.of(2024, 12, 16, 13, 7)),
+                new Attendance(LocalDateTime.of(2024, 12, 17, 10, 7)),
+                new Attendance(LocalDateTime.of(2024, 12, 18, 10, 7)),
+                new Attendance(LocalDateTime.of(2024, 12, 20, 11, 7)));
+
+        Crew crew = new Crew("훌라", attendances);
+
+        assertThat(crew.checkWarning()).isEqualTo(Warning.WARN);
     }
 }
