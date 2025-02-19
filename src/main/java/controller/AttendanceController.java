d32e8dc02a7ff0ea499c3d63ff8529e8341bedc8
julie <7jujubuilds@gmail.com>
2025-02-19 17:20:56 +0900

    feat(menu): 메뉴 기능
    - 사용자가 입력한 값에 따라 해당 기능 실행한다.

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -13,5 +13,33 @@ public class AttendanceController {
         if (menuInput.matches("[Qq]")) {
             return;
         }
+        if (menuInput.equals("1")) {
+            checkIn();
+        }
+        if (menuInput.equals("2")) {
+            updateAttendance();
+        }
+        if (menuInput.equals("3")) {
+            checkAttendanceRecords();
+        }
+        if (menuInput.equals("4")) {
+            checkDisciplinaryStatus();
+        }
+    }
+
+    public void checkIn() {
+        System.out.println("출석 확인");
+    }
+
+    public void updateAttendance() {
+        System.out.println("출석 수정");
+    }
+
+    public void checkAttendanceRecords() {
+        System.out.println("출석 기록 확인");
+    }
+
+    public void checkDisciplinaryStatus() {
+        System.out.println("제적 위험자 확인");
     }
 }
