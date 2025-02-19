9709c0838c32aa80b57548d0d6c3b35e803c2836
kimdev <kim__dev@hufs.ac.kr>
2025-02-19 15:57:20 +0900

    feat: 전날까지의 모든 출석을 조회하는 기능 구현

--- src/test/java/attendance/domain/AttendancesTest.java
+++ src/test/java/attendance/domain/AttendancesTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +13,23 @@ class AttendancesTest {
     @Test
     void 찾으려는_날짜를_입력하면_출석_기록을_찾아준다() {
         // given
-        Attendance attendance = new Attendance(LocalDateTime.of(2025, 2, 19, 10, 0));
-        Attendances attendances = new Attendances();
-        attendances.addAttendance(attendance);
+        List<LocalDateTime> attendanceDateTimes = List.of(LocalDateTime.of(2025, 2, 3, 10, 0));
+        LocalDateTime today = LocalDateTime.of(2025, 2, 4, 10, 0);
+        Attendances attendances = new Attendances(attendanceDateTimes, today);
 
         // when & then
-        assertThat(attendances.findAttendanceByLocalDate(LocalDate.of(2025, 2, 19))).isNotNull();
+        assertThat(attendances.findAttendanceByLocalDate(LocalDate.of(2025, 2, 3))).isNotNull();
+    }
+
+    @Test
+    void 오늘_날짜를_알려주면_전날까지의_해당_크루의_출석_기록을_알려준다() {
+        // Given
+        List<LocalDateTime> attendanceDateTimes = List.of(LocalDateTime.of(2025, 2, 3, 10, 0));
+        LocalDateTime today = LocalDateTime.of(2025, 2, 6, 10, 0);
+        Attendances attendances = new Attendances(attendanceDateTimes, today);
+
+        // When & Then
+        assertThat(attendances.findAllBeforeToday(today)).hasSize(3);
     }
 
 }
