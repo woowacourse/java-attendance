0844ccc9c3723b991f935388e4d385c0784b74e9
saera <yook.saera@gmail.com>
2025-02-19 14:57:17 +0900

    test: 특정 크루의 출석 기록 수정 테스트 작성

--- src/test/java/attendance/domain/AttendanceRepositoryTest.java
+++ src/test/java/attendance/domain/AttendanceRepositoryTest.java
@@ -49,4 +49,56 @@ public class AttendanceRepositoryTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 유효하지 않은 닉네임입니다.");
     }
+
+    @DisplayName("크루 출석 정보 수정 성공")
+    @Test
+    void test3() {
+        List<Attendance> attendances = new ArrayList<>();
+        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 23, 13, 3);
+        Attendance attendance = new Attendance("빙봉");
+        attendance.add(localDateTime);
+
+        AttendanceRepository attendanceRepository = new AttendanceRepository(attendances);
+        String name = "빙봉";
+        LocalDateTime newLocalDateTime = LocalDateTime.of(2024, 12, 23, 13, 1);
+
+        assertThatCode(() -> attendanceRepository.update(name, newLocalDateTime))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("유효하지 않은 닉네임 크루 출석 정보 수정 실패")
+    @Test
+    void test4() {
+        List<Attendance> attendances = new ArrayList<>();
+        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 23, 13, 3);
+        Attendance attendance = new Attendance("빙봉");
+        attendance.add(localDateTime);
+
+        AttendanceRepository attendanceRepository = new AttendanceRepository(attendances);
+        String name = "빙티";
+        LocalDateTime newLocalDateTime = LocalDateTime.of(2024, 12, 22, 13, 1);
+
+        assertThatThrownBy(() -> attendanceRepository.update(name, newLocalDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 날짜에 출석 기록이 없습니다.");
+
+    }
+
+    @DisplayName("기록이 없는 날짜 크루 출석 정보 수정 실패")
+    @Test
+    void test6() {
+        List<Attendance> attendances = new ArrayList<>();
+        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 23, 13, 3);
+        Attendance attendance = new Attendance("빙봉");
+        attendance.add(localDateTime);
+
+        AttendanceRepository attendanceRepository = new AttendanceRepository(attendances);
+        String name = "빙봉";
+        LocalDateTime newLocalDateTime = LocalDateTime.of(2024, 12, 22, 13, 1);
+
+        assertThatThrownBy(() -> attendanceRepository.update(name, newLocalDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 날짜에 출석 기록이 없습니다.");
+
+    }
 }
