c963b61d2b3e778d76b644e1421ea04c5fcef0d4
jhpark1227 <junhyeok0246@naver.com>
2025-02-19 15:58:34 +0900

    test: 운영 시작 시간이 아닌 경우 출석을 할 수 없는 기능 단위테스트 구현

--- src/test/java/model/AttendanceTest.java
+++ src/test/java/model/AttendanceTest.java
@@ -9,7 +9,7 @@ public class AttendanceTest {
 
     @Test
     @DisplayName("주말 및 공휴일에는 출석할 수 없다.")
-    void test2() {
+    void test1() {
         //given
         Crew crew = Crew.of("쿠키");
         LocalDateTime checkInTime = LocalDateTime.of(2024, 12, 14, 9, 35);
@@ -18,5 +18,31 @@ public class AttendanceTest {
         Assertions.assertThatThrownBy(() -> Attendance.of(crew, checkInTime))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("주말 및 공휴일에는 출석할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("운영 시작 시간 이전에는 출석을 체크할 수 없다.")
+    void test2() {
+        //given
+        Crew crew = Crew.of("쿠키");
+        LocalDateTime checkInTime = LocalDateTime.of(2024, 12, 3, 7, 0);
+
+        //when & then
+        Assertions.assertThatThrownBy(() -> Attendance.of(crew, checkInTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("지금은 운영 시간이 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("운영 종료 시간 이후에는 출석을 체크할 수 없다.")
+    void test3() {
+        //given
+        Crew crew = Crew.of("쿠키");
+        LocalDateTime checkInTime = LocalDateTime.of(2024, 12, 3, 24, 0);
+
+        //when & then
+        Assertions.assertThatThrownBy(() -> Attendance.of(crew, checkInTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("지금은 운영 시간이 아닙니다.");
     }
 }
