d54e405af4afa23748463794d0df20243575b45a
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-19 17:13:47 +0900

    feat(Crew): equals 메서드 재정의

--- src/main/java/attendance/domain/AttendanceType.java
+++ src/main/java/attendance/domain/AttendanceType.java
@@ -16,7 +16,7 @@ public enum AttendanceType {
         this.type = type;
     }
 
-    public static AttendanceType of(LocalDateTime localDateTime) {
+    public static AttendanceType of(final LocalDateTime localDateTime) {
         DayOfWeek dayOfWeek = localDateTime.getDayOfWeek();
         LocalTime localTime = localDateTime.toLocalTime();
         if (isMonday(dayOfWeek)) {
@@ -28,7 +28,7 @@ public enum AttendanceType {
         return FREE;
     }
 
-    private static AttendanceType calculateAttendanceType(LocalTime localTime, int hour, int minute) {
+    private static AttendanceType calculateAttendanceType(final LocalTime localTime, final int hour, final int minute) {
         if (localTime.isAfter(LocalTime.of(hour, minute + 30))) {
             return ABSENT;
         }
@@ -42,13 +42,17 @@ public enum AttendanceType {
         return dayOfWeek.equals(DayOfWeek.MONDAY);
     }
 
-    private static boolean isOtherWeekday(DayOfWeek dayOfWeek) {
+    private static boolean isOtherWeekday(final DayOfWeek dayOfWeek) {
         return dayOfWeek.equals(DayOfWeek.TUESDAY) ||
                 dayOfWeek.equals(DayOfWeek.WEDNESDAY) ||
                 dayOfWeek.equals(DayOfWeek.THURSDAY) ||
                 dayOfWeek.equals(DayOfWeek.FRIDAY);
     }
 
+    @Override
+    public String toString() {
+        return type;
+    }
     // 해당 요일의 시작 시각으로부터 5분 초과는 지각으로 간주한다.
     // 해당 요일의 시작 시각으로부터 30분 초과는 결석으로 간주한다.
     // 시간은 월요일은 13:00~18:00
--- src/main/java/attendance/domain/Crew.java
+++ src/main/java/attendance/domain/Crew.java
@@ -18,7 +18,20 @@ public class Crew {
     public void countAbsent() {
     }
 
-    public boolean isSameCrewName(String crewName) {
+    public boolean isSameCrewName(final String crewName) {
         return this.crewName.equals(crewName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true; // 만일 현 객체 this와 매개변수 객체가 같을 경우 true
+        }
+        if (!(o instanceof Crew)) {
+            return false; // 만일 매개변수 객체가 Crew 타입과 호환되지 않으면 false
+        }
+        Crew crew = (Crew) o; // 만일 매개변수 객체가 Crew 타입과 호환된다면 다운캐스팅(down casting) 진행
+        return Objects.equals(this.crewName, crew.crewName);
+        // this 객체 이름과 매개변수 객체 이름이 같을경우 true, 다를 경우 false
     }
 }
