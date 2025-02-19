40e26ef90f9bb546f18b6978c44e2341290e8719
(minjae8563) <(minjae8563@naver.com)>
2025-02-19 16:53:37 +0900

    feat : 컨트롤러 메서드 구현
    createStudentRepository : 잘못 된 파일을 입력시 재귀 함수를 통해 반복하는 메서드 구현
    getStudentNameUntilExist : 존재하지 않는 학생을 입력시 재귀 함수를 통해 반복하여 입력받는 메서드 구현
    getStartTimeUntilValidate : 잘못된 형식의 시간을 입력시 재귀 함수를 통해 반복하여 입력받는 메서드 구현
    attendanceStart : 1번 메뉴에 대한 기능 구현

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,8 +1,15 @@
 package controller;
 
 import java.io.IOException;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import model.Student;
 import model.StudentRepository;
+import model.TodayDate;
 import util.FileInput;
+import view.InputView;
+import view.OutputView;
 
 public class Controller {
 
@@ -13,6 +20,67 @@ public class Controller {
         return studentRepository;
     }
 
+    private StudentRepository createStudentRepository(){
+        try{
+            return readFileAndCreateStudentRepository();
+        } catch (IOException e){
+            return createStudentRepository();
+        }
+    }
+
     public void attendanceStart() {
+        TodayDate todayDate = new TodayDate(LocalDate.of(2024, 12, 13));
+        InputView.printTodayAndSelectFunction(todayDate.getTodayDate());
+        String selectFunction = InputView.getUserInputString();
+        StudentRepository studentRepository = createStudentRepository();
+
+        if (selectFunction.equals("Q")){
+            return;
+        }
+        if (Integer.parseInt(selectFunction) == 1) {
+            String studentName = getStudentNameUntilExist(studentRepository);
+            LocalDateTime localDateTime = getStartTimeUntilValidate(todayDate);
+            System.out.println(localDateTime);
+            Student student = studentRepository.findStudentByName(studentName);
+            student.updateState(localDateTime);
+            OutputView.printTodayAttendanceResult(student, localDateTime);
+            System.out.println(student.getRecord());
+        }
+
+        if (Integer.parseInt(selectFunction) == 2) {
+
+        }
+        if (Integer.parseInt(selectFunction) == 3) {
+
+        }
+        if (Integer.parseInt(selectFunction) == 4) {
+
+        }
+    }
+
+    private String getStudentNameUntilExist(StudentRepository studentRepository) {
+        String userName = InputView.printInputNicName();
+        try{
+            studentRepository.notExistStudent(userName);
+            return userName;
+        }catch (IllegalArgumentException e){
+            return getStudentNameUntilExist(studentRepository);
+        }
+    }
+
+    private LocalDateTime getStartTimeUntilValidate(TodayDate todayDate) {
+        String startTime = InputView.printStartTime();
+        try{
+            return todayDate.makeLocalDateToLocalDateTime(startTime);
+        }catch (DateTimeException e){
+            return getStartTimeUntilValidate(todayDate);
+        }
+    }
+
+
+
+
+    private void attendanceFunctionOne(){
+
     }
 }
