dad76ef2d1e752039b033419fccc054ee303601b
ryan-dia <cjfdnjs158@naver.com>
2025-02-19 17:44:08 +0900

    feat: 처벌 관리 enum 추가
    Co-authored-by: 2Jin1031 <2jin2.1031@gmail.com>

---
+++ src/main/java/domain/Punishment.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum Punishment {
+    EXPULSION("제적", 5),
+    INTERVIEW("면담", 2),
+    WARNING("경고", 1),
+    NONE("없음", 0);
+
+    private String punishmentName;
+    private int absenceCount;
+
+    Punishment(final String punishmentName, final int absenceCount) {
+        this.punishmentName = punishmentName;
+        this.absenceCount = absenceCount;
+    }
+
+
+    public static Punishment findByAbsenceCount(int absenceCount) {
+        return Arrays.stream(Punishment.values())
+                .filter(punishment -> punishment.absenceCount < absenceCount)
+                .findFirst()
+                .orElse(NONE);
+    }
+}
