09ea7513876c8eb37ba1b5837bd58e46c751c7c6
(minjae8563) <(minjae8563@naver.com)>
2025-02-19 20:47:24 +0900

    fix : todayDate 에서 makeLocalDateToLocalDateTime 메서드 InputView 로 이동

--- src/main/java/model/TodayDate.java
+++ src/main/java/model/TodayDate.java
@@ -1,11 +1,6 @@
 package model;
 
-import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 public class TodayDate {
 
     private final LocalDate todayDate;
@@ -17,16 +12,4 @@ public class TodayDate {
     public LocalDate getTodayDate() {
         return todayDate;
     }
-
-    public LocalDateTime makeLocalDateToLocalDateTime(String time) {
-        try {
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-            LocalTime localTime = LocalTime.parse(time,dateTimeFormatter);
-            return todayDate.atTime(localTime);
-        } catch (DateTimeException e) {
-            throw new IllegalArgumentException("[ERROR] 시간 형식에 맞지 않습니다.");
-        }
-
-    }
-
 }
