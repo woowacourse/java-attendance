3cc4b2b7b2ded9a502f3e661f6e6d55f2fe33040
kimdev <kim__dev@hufs.ac.kr>
2025-02-19 15:57:20 +0900

    feat: 전날까지의 모든 출석을 조회하는 기능 구현

--- src/main/java/attendance/domain/Attendances.java
+++ src/main/java/attendance/domain/Attendances.java
@@ -1,12 +1,33 @@
 package attendance.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Attendances {
 
     private final List<Attendance> attendances = new ArrayList<>();
+
+    public Attendances(List<LocalDateTime> attendanceDateTimes, LocalDateTime today) {
+        for(int day=1; day<today.getDayOfMonth(); day++) {
+            LocalDate localDate = LocalDate.of(today.getYear(), today.getMonth(), day);
+            DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+            if (dayOfWeek.equals(DayOfWeek.SUNDAY) || dayOfWeek.equals(DayOfWeek.SATURDAY) || Holiday.isExists(localDate)) {
+                continue;
+            }
+            this.attendances.add(initializeAttendance(attendanceDateTimes, today, day));
+        }
+    }
+
+    private Attendance initializeAttendance(List<LocalDateTime> attendanceDateTimes, LocalDateTime today, int day) {
+        return attendanceDateTimes.stream()
+                .filter(dateTime -> dateTime.toLocalDate().isEqual(LocalDate.of(today.getYear(), today.getMonth(), day)))
+                .findAny()
+                .map(Attendance::new)
+                .orElse(Attendance.absence(LocalDate.of(today.getYear(), today.getMonth(), day)));
+    }
 
     public void addAttendance(final Attendance attendance) {
         attendances.add(attendance);
@@ -19,4 +40,13 @@ public class Attendances {
                 .orElse(Attendance.absence(findDate));
     }
 
+    public List<Attendance> findAllBeforeToday(LocalDateTime today) {
+        boolean hasTodayAttendance = attendances.stream()
+                .anyMatch(attendance -> attendance.isSameDate(today.toLocalDate()));
+        if (hasTodayAttendance) {
+            return attendances.subList(0, attendances.size());
+        }
+        return attendances.stream()
+                .toList();
+    }
 }
