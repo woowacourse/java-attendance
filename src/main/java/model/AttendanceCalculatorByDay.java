c613e56dfb26307517b6e9f3a6420eef8fffc9b9
(minjae8563) <(minjae8563@naver.com)>
2025-02-19 13:50:48 +0900

    feat : 출력을 위한 멤버 변수 추가

--- src/main/java/model/AttendanceCalculatorByDay.java
+++ src/main/java/model/AttendanceCalculatorByDay.java
@@ -4,20 +4,22 @@ import java.time.LocalTime;
 
 public enum AttendanceCalculatorByDay {
 
-    MONDAY(LocalTime.of(13,5),LocalTime.of(13,30),1),
-    TUESDAY(LocalTime.of(10,5),LocalTime.of(10,30),2),
-    WEDNESDAY(LocalTime.of(10,5), LocalTime.of(10,30),3),
-    THURSDAY(LocalTime.of(10,5), LocalTime.of(10,30),4),
-    FRIDAY(LocalTime.of(10,5), LocalTime.of(10,30),5);
+    MONDAY(LocalTime.of(13,5),LocalTime.of(13,30),1,"월요일"),
+    TUESDAY(LocalTime.of(10,5),LocalTime.of(10,30),2,"화요일"),
+    WEDNESDAY(LocalTime.of(10,5), LocalTime.of(10,30),3,"수요일"),
+    THURSDAY(LocalTime.of(10,5), LocalTime.of(10,30),4,"목요일"),
+    FRIDAY(LocalTime.of(10,5), LocalTime.of(10,30),5,"금요일");
 
     private LocalTime lateTime;
     private LocalTime absentTime;
     private int dayOfWeekValue;
+    private String day;
 
-    AttendanceCalculatorByDay(LocalTime lateTime, LocalTime absentTime, int dayOfWeekValue) {
+    AttendanceCalculatorByDay(LocalTime lateTime, LocalTime absentTime, int dayOfWeekValue, String day) {
         this.lateTime = lateTime;
         this.absentTime = absentTime;
         this.dayOfWeekValue = dayOfWeekValue;
+        this.day = day;
     }
 
     public static AttendanceStatus attendanceCalculator(int day, LocalTime localTime) {
