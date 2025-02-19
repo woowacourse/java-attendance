619031214eefa2ac2c40dff14fbbc9678f74c3af
goohong <goohong.chung@gmail.com>
2025-02-19 15:11:30 +0900

    red: 이미 출석한 경우, 다시 출석할 수 없으며 수정 기능을 이용하도록 안내해야 한다.

--- src/test/java/CheckAttendanceTest.java
+++ src/test/java/CheckAttendanceTest.java
@@ -1,3 +1,5 @@
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Map;
@@ -12,13 +14,31 @@ public class CheckAttendanceTest {
         AttendanceBook attendanceBook = new AttendanceBook();
 
         Crew crew1 = Crew.createByName("쿠키");
-        crew1.addDailyAttendance(Map.of(LocalDate.of(2024,12,1),LocalTime.of(10, 6)));
+        crew1.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 1), LocalTime.of(10, 6)));
         attendanceBook.addNewCrew(crew1);
 
         Crew crew2 = Crew.createByName("우유");
-        crew2.addDailyAttendance(Map.of(LocalDate.of(2024,12,1), LocalTime.of(10, 7)));
+        crew2.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 1), LocalTime.of(10, 7)));
         attendanceBook.addNewCrew(crew2);
 
-        attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024,12,1), LocalTime.of(10, 7))); // 정상
+        attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 2), LocalTime.of(10, 7))); // 정상
+    }
+
+    @Test
+    void 이미_출석한_경우_다시_출석할_수_없으며_수정_기능을_이용하도록_안내해야_한다() {
+        AttendanceBook attendanceBook = new AttendanceBook();
+
+        Crew crew1 = Crew.createByName("쿠키");
+        crew1.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 1), LocalTime.of(10, 6)));
+        attendanceBook.addNewCrew(crew1);
+
+        Crew crew2 = Crew.createByName("우유");
+        crew2.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 1), LocalTime.of(10, 7)));
+        attendanceBook.addNewCrew(crew2);
+
+        assertThatThrownBy(
+                () -> attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 1), LocalTime.of(10, 7))))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 }
