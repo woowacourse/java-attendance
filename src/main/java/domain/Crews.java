4e3c80c5a6ae86e926ec3b57f9e2a01e1e29a85e
Hacanna42 <dtmdgus901@gmail.com>
2025-02-19 17:40:09 +0900

    test, feat: Crew 구현 TDD 사이클 완료

---
+++ src/main/java/domain/Crews.java
@@ -0,0 +1,28 @@
+package domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Crews {
+    private final Set<Crew> crews;
+
+    public Crews() {
+        crews = new HashSet<>();
+    }
+
+    public void add(String nickname) {
+        crews.add(Crew.of(nickname));
+    }
+
+    public Crew findCrewBy(String nickname) {
+        return crews.stream()
+                .filter(crew -> crew.hasSame(nickname))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 크루입니다"));
+    }
+
+//    private boolean isRegistered(String nickname) {
+//        return crews.stream()
+//                .anyMatch(crew -> crew.hasSame(nickname));
+//    }
+}
