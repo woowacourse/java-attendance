961e9b04719efabaebfc13d7b7f1bb0df3b2ed27
JO-eusan <goodsan0330@gmail.com>
2025-02-19 15:05:48 +0900

    refactor: 출석 정보 저장 구조 변경

--- src/main/java/repository/AttendanceRepository.java
+++ src/main/java/repository/AttendanceRepository.java
@@ -1,49 +1,5 @@
 package repository;
 
-import domain.CrewRecord;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-
 public class AttendanceRepository {
 
-    private List<CrewRecord> crewRecords = new ArrayList<>();
-
-    public CrewRecord attend(String inputName, LocalDateTime inputDateAndTime) {
-        for (CrewRecord crewRecord : crewRecords) {
-            if (crewRecord.isAttend(inputName, inputDateAndTime)) {
-                throw new IllegalArgumentException("이미 출석한 경우 수정만 가능합니다.");
-            }
-        }
-        CrewRecord newRecord = new CrewRecord(inputName, inputDateAndTime);
-        crewRecords.add(newRecord);
-        return newRecord;
-    }
-
-    public CrewRecord edit(String name, int dayOfMonth, String time) {
-        for(CrewRecord crewRecord : crewRecords) {
-            if(crewRecord.getName().equals(name) && crewRecord.getDateTime().getDayOfMonth() == dayOfMonth) {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                String date = LocalDate.of(2024, 12, dayOfMonth).atStartOfDay().format(formatter);
-                formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-                LocalDateTime newDateTime = LocalDateTime.parse(date + " " + time, formatter);
-
-                crewRecord.editRecord(newDateTime);
-                return crewRecord;
-            }
-        }
-        return null;
-    }
-
-    public List<CrewRecord> findByName(String name) {
-        List<CrewRecord> records = new ArrayList<>();
-        for (CrewRecord crewRecord : crewRecords) {
-            if (name.equals(crewRecord.getName())) {
-                records.add(crewRecord);
-            }
-        }
-        return records;
-    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,18 +3,40 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
+
     private final String INPUT_NAME_MESSAGE = "닉네임을 입력해 주세요.";
     private final String INPUT_TIME_MESSAGE = "등교 시간을 입력해 주세요.";
+    private final String INPUT_EDIT_NAME_MESSAGE ="출석을 수정하려는 크루의 닉네임을 입력해 주세요.";
+    private final String INPUT_EDIT_DAY_MESSAGE = "수정하려는 날짜(일)를 입력해 주세요.";
+    private final String INPUT_EDIT_TIME_MESSAGE = "언제로 변경하겠습니까?";
 
-    public String readName(){
+    public String readName() {
+        return basicInput(INPUT_NAME_MESSAGE);
+    }
+
+    public String readTime() {
+        return basicInput(INPUT_TIME_MESSAGE);
+    }
+
+    public String readEditName() {
+        return basicInput(INPUT_EDIT_NAME_MESSAGE);
+    }
+
+    public String readEditDayOfMonth(){
+        return basicInput(INPUT_EDIT_DAY_MESSAGE);
+    }
+
+    public String readEditTime(){
+        return basicInput(INPUT_EDIT_TIME);
+    }
+
+    private String basicInput(String message) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(INPUT_NAME_MESSAGE);
+        printMessage(message);
         return sc.nextLine();
     }
 
-    public String readTime(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println(INPUT_TIME_MESSAGE);
-        return sc.nextLine();
+    private void printMessage(String message) {
+        System.out.println(message);
     }
 }
