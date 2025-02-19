1c501af6d4e882281a7b23864156103981dfa7c9
ryan-dia <cjfdnjs158@naver.com>
2025-02-19 17:37:05 +0900

    feat: 출석 개수 카운팅 메서드 추가
    Co-authored-by: 2Jin1031 <2jin2.1031@gmail.com>

--- src/main/java/domain/Attendances.java
+++ src/main/java/domain/Attendances.java
@@ -9,4 +9,20 @@ public class Attendances {
     public Attendances(final List<Attendance> attendances) {
         this.attendances = attendances;
     }
+
+    public AttendanceDto calculateAttendanceCount() {
+        final long attendanceCount = attendances.stream()
+                .filter(attendance -> attendance.attendanceStatus.equals(AttendanceStatus.ATTENDANCE))
+                .count();
+        final long tardinessCount = attendances.stream()
+                .filter(attendance -> attendance.attendanceStatus.equals(AttendanceStatus.TARDINESS))
+                .count();
+        final long absence = attendances.stream()
+                .filter(attendance -> attendance.attendanceStatus.equals(AttendanceStatus.ABSENCE))
+                .count();
+
+        return new AttendanceDto((int) attendanceCount, (int) tardinessCount, (int) absence);
+    }
+
+
 }
---
+++ src/test/java/domain/AttendancesTest.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AttendancesTest {
+
+    @Test
+    void 출석_상태_갯수_측정() {
+        // given (출석 : 2, 지각 : 1, 결석: 3)
+        List<Attendance> attendanceList = new ArrayList<>();
+        attendanceList.add(Attendance.of("2024-12-13 10:00"));
+        attendanceList.add(Attendance.of("2024-12-16 13:00"));
+        attendanceList.add(Attendance.of("2024-12-17 10:08"));
+        attendanceList.add(Attendance.of("2024-12-18 10:31"));
+        attendanceList.add(Attendance.of("2024-12-19 10:31"));
+        attendanceList.add(Attendance.of("2024-12-20 10:31"));
+        Attendances attendances = new Attendances(attendanceList);
+
+        // when
+
+        AttendanceDto attendanceDto = attendances.calculateAttendanceCount();
+        int expectedAttendance = 2;
+        int expectedTardiness = 1;
+        int expectedAbsence = 3;
+
+        AttendanceDto expectedDto = new AttendanceDto(expectedAttendance, expectedTardiness, expectedAbsence);
+
+        // then
+        Assertions.assertThat(attendanceDto).isEqualTo(expectedDto);
+    }
+
+}
