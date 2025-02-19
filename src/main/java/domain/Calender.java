c2b388d25ea6c6d7109af3a00a8b32ea6c498d24
DongYeop <s01054830455@gmail.com>
2025-02-19 15:44:33 +0900

    feat: Calender 클래스 생성

---
+++ src/main/java/domain/Calender.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Calender {
+
+    MON("월요일", List.of(2, 9, 16, 23, 30)),
+    TUE("화요일", List.of(3, 10, 17, 24, 31)),
+    WED("수요일", List.of(4, 11, 18)),
+    THU("목요일", List.of(5, 12, 19, 26)),
+    FRI("금요일", List.of(6, 13, 20, 27)),
+    HOLY("공휴일", List.of(1, 7, 8, 14, 15, 21, 22, 25, 28, 29));
+
+    private final String description;
+    private final List<Integer> date;
+
+    Calender(final String description, final List<Integer> date) {
+        this.description = description;
+        this.date = date;
+    }
+
+    public static String findBy(final int dayOfMonth) {
+        return Arrays.stream(Calender.values())
+                .filter(calender -> calender.date.contains(dayOfMonth))
+                .map(calender -> calender.description)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하는 요일이 없습니다."));
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
