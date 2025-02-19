2f9d9f6ac3dfe7bc229d592cdb463dd20379dfdc
(minjae8563) <(minjae8563@naver.com)>
2025-02-19 16:50:37 +0900

    feat : 출력 양식을 만드는 클래스 구현

---
+++ src/main/java/util/LocalDateTimePrintFormatter.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import model.AttendanceCalculatorByDay;
+
+public class LocalDateTimePrintFormatter {
+    public static String LocalDateTimeToLocalTime(LocalDateTime localDateTime) {
+        int month = localDateTime.getMonthValue();
+        int date = localDateTime.getDayOfMonth();
+        int dayOfWeek = localDateTime.getDayOfWeek().getValue();
+        String day = AttendanceCalculatorByDay.findDayByDayOfWeekValue(dayOfWeek);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        String time = localDateTime.format(dateTimeFormatter);
+        return month + "월 " + date + "일 " + day +" "+ time + " ";
+    }
+}
