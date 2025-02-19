2d7420bfecf115a58a2a3cdf7f46434b7ac26349
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-19 20:56:18 +0900

    feat(CrewStatistics): 크루 출석 통계 정보 구현

---
+++ src/main/java/attendance/domain/CrewStatistic.java
@@ -0,0 +1,76 @@
+package attendance.domain;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+
+public class CrewStatistic {
+    private final Crew crew;
+    private final List<Attendance> crewAttendances;
+    private CrewStatus status;
+
+    public CrewStatistic(Crew crew, List<Attendance> crewAttendances) {
+        this.crew = crew;
+        this.crewAttendances = crewAttendances;
+        this.status = CrewStatus.NONE;
+    }
+
+    public void initCrewStatus() {
+        for (Attendance crewAttendance : crewAttendances) {
+            if (crewAttendance.getType() == AttendanceType.SAFE) {
+                crew.plusSafeCount();
+            }
+            if (crewAttendance.getType() == AttendanceType.LATE) {
+                crew.plusLateCount();
+            }
+            if (crewAttendance.getType() == AttendanceType.ABSENT) {
+                crew.plusAbsentCount();
+            }
+        }
+    }
+
+    public void calculatePenalty() {
+        int lateCount = crew.getLateCount();
+        int absentCount = crew.getAbsentCount();
+
+        int plusAbsentCount = lateCount / 3;
+        absentCount += plusAbsentCount;
+
+        if (absentCount > 5) {
+            status = CrewStatus.EXPEL;
+        }
+        if (absentCount >= 3) {
+            status = CrewStatus.MEETING;
+        }
+        if (absentCount >= 2) {
+            status = CrewStatus.WARNING;
+        }
+    }
+
+    public List<List<String>> getStatisticInfo() {
+        List<Attendance> sortedAttendances = crewAttendances.stream()
+                .sorted(Comparator.comparing(Attendance::getDate))
+                .toList();
+
+        List<List<String>> crewStatisticInfo = new ArrayList<>();
+
+        for (Attendance crewAttendance : sortedAttendances) {
+            LocalDate crewLocalDate = crewAttendance.getDate();
+
+            crewStatisticInfo.add(
+                    List.of(
+                            String.valueOf(crewLocalDate.getMonthValue()),
+                            String.format("%02d", crewLocalDate.getDayOfMonth()),
+                            crewLocalDate.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN),
+                            crewAttendance.getTimeValue(),
+                            crewAttendance.getType().toString()
+                    )
+            );
+        }
+
+        return crewStatisticInfo;
+    }
+}
