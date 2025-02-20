6afeab12331672c79ec958460118eba4080b08f4
threepebbles <threepebbles@naver.com>
2025-02-20 17:18:02 +0900

    fix: 캠퍼스 운영시간 상수 수정

--- src/main/java/constant/CampusConstant.java
+++ src/main/java/constant/CampusConstant.java
@@ -3,6 +3,6 @@ package constant;
 import java.time.LocalTime;
 
 public class CampusConstant {
-    public static final LocalTime startTime = LocalTime.of(11, 00);
+    public static final LocalTime startTime = LocalTime.of(8, 00);
     public static final LocalTime endTime = LocalTime.of(23, 00);
 }
