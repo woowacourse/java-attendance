80b90d312586031e2502eb63bf27bcf3fc30627e
BWY <quantumjump10@gmail.com>
2025-02-20 14:35:58 +0900

    chore: 상수명 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
     private static final String NICKNAME_INPUT_MESSAGE = "닉네임을 입력해 주세요.";
     private static final String ATTENDANCE_INPUT_MESSAGE = "등교 시간을 입력해 주세요.";
     private static final String EDIT_NICKNAME_INPUT_MESSAGE = "출석을 수정하려는 크루의 닉네임을 입력해 주세요.";
-    private static final String EDIT_DATE_INPUT_MESSAGE = "수정하려는 날짜(일)를 입력해 주세요.";
+    private static final String EDIT_DAY_OF_MONTH_INPUT_MESSAGE = "수정하려는 날짜(일)를 입력해 주세요.";
     private static final String ASK_NEW_TIME_FOR_CHANGE = "언제로 변경하겠습니까?";
 
 
@@ -27,8 +27,8 @@ public class InputView {
         return scanner.nextLine();
     }
 
-    public String getEditDate() {
-        System.out.println(EDIT_DATE_INPUT_MESSAGE);
+    public String getEditDayOfMonth() {
+        System.out.println(EDIT_DAY_OF_MONTH_INPUT_MESSAGE);
         return scanner.nextLine();
     }
 
