ac048eb86aa149d04b87fc526b57599ee567cff2
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-20 15:49:44 +0900

    feat: 전체 기능 구현
    - view 분리
    - 메시지 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,12 @@
 import controller.AttendanceController;
+import view.InputView;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        AttendanceController attendanceController = new AttendanceController();
+        AttendanceController attendanceController = new AttendanceController(new InputView(new Scanner(System.in)));
         attendanceController.run();
     }
 }
