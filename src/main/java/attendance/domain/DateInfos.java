8b66599d8868796dcf2d47bae86be3838a241ba8
bowook <mbosx316@naver.com>
2025-02-20 17:01:57 +0900

    feat(DateInfos) : DateInfos 출석부 역할 기능 추가

---
+++ src/main/java/attendance/domain/DateInfos.java
@@ -0,0 +1,92 @@
+package attendance.domain;
+
+import attendance.domain.constant.AttendanceStatus;
+import attendance.domain.constant.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DateInfos {
+
+    private final List<DateInfo> dateInfos;
+    private int absence;
+    private int late;
+    private int attendance;
+
+    private DateInfos(List<DateInfo> dateInfos) {
+        this.dateInfos = dateInfos;
+    }
+
+    public static DateInfos fromDefaultValue(LocalDate now) {
+        List<DateInfo> dateInfos = makeDefaultDateInfos(now);
+        return new DateInfos(dateInfos);
+    }
+
+    public static DateInfos from(List<DateInfo> dateInfos) {
+        return new DateInfos(dateInfos);
+    }
+
+    public void calculateAttendanceHistory() {
+        int absence = 0;
+        int late = 0;
+        int attendance = 0;
+        for (DateInfo dateInfo : dateInfos) {
+            absence += dateInfo.checkAbsenceStatus();
+            late += dateInfo.checkLateStatus();
+            attendance += dateInfo.checkAttendanceStatus();
+        }
+        this.absence = absence;
+        this.late = late;
+        this.attendance = attendance;
+    }
+
+    private static List<DateInfo> makeDefaultDateInfos(LocalDate now) {
+        List<DateInfo> dateInfos = new ArrayList<>();
+        for (int day = 1; day <= now.getDayOfMonth(); day++) {
+            LocalDate currentDay = LocalDate.of(now.getYear(), now.getMonthValue(), day);
+            addWeekdayDateInfo(currentDay, dateInfos);
+        }
+        return dateInfos;
+    }
+
+    private static void addWeekdayDateInfo(LocalDate currentDay, List<DateInfo> dateInfos) {
+        if (checkHoliday(currentDay)) return;
+
+        dateInfos.add(DateInfo.makeDefaultValue(currentDay.getMonthValue(),
+                currentDay.getDayOfMonth(),
+                DayOfWeek.from(currentDay.getDayOfWeek().getValue())));
+    }
+
+    private static boolean checkHoliday(LocalDate currentDay) {
+        return currentDay.getDayOfWeek().getValue() >= 6;
+    }
+
+    public DateInfo findByDate(int date) {
+        return dateInfos.stream().filter(dateInfo -> Integer.parseInt(dateInfo.getDay()) == date)
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public int findStatusCounts(AttendanceStatus attendanceStatus) {
+        return (int) dateInfos.stream()
+                .filter(dateInfo -> dateInfo.getAttendanceStatus().equals(attendanceStatus.getName()))
+                .count();
+    }
+
+    public List<DateInfo> getDateInfos() {
+        return dateInfos;
+    }
+
+    public int getAbsence() {
+        return absence;
+    }
+
+    public int getLate() {
+        return late;
+    }
+
+    public int getAttendance() {
+        return attendance;
+    }
+
+}
