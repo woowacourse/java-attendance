4dc4a5e53152a9a6273de41470d7a9564ed32e1b
BWY <quantumjump10@gmail.com>
2025-02-20 16:35:28 +0900

    feat: Application 추가

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import config.AttendanceHistoryLoader;
+import controller.AttendanceController;
+import domain.Crews;
+import view.InputView;
+import view.OutputView;
+
+public class Application {
+    public static void main(String[] args) {
+        AttendanceHistoryLoader attendanceHistoryLoader = new AttendanceHistoryLoader();
+        Crews crews = attendanceHistoryLoader.loadCrews();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        AttendanceController attendanceController = new AttendanceController(crews, inputView, outputView);
+        attendanceController.run();
+    }
+}
