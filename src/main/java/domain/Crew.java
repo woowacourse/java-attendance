90610bfdf20a4ebce37184a758130f578fca8ef3
ryan-dia <cjfdnjs158@naver.com>
2025-02-20 15:37:27 +0900

    feat: 크루 출석 관리 기능들 추가
    Co-authored-by: 2Jin1031 <2jin2.1031@gmail.com>

--- src/main/java/domain/Crew.java
+++ src/main/java/domain/Crew.java
@@ -1,5 +1,8 @@
 package domain;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Crew {
     Nickname nickname;
     Attendances attendances;
@@ -13,5 +16,45 @@ public class Crew {
 
     public Nickname getNickname() {
         return nickname;
+    }
+
+    public boolean isAttended(final LocalDateTime dateTime) {
+        return attendances.isAttended(dateTime);
+    }
+
+    public void add(Attendance attendance) {
+        attendances.add(attendance);
+    }
+
+    public Attendance getAttendance(final LocalDateTime dateTime) {
+        return attendances.findAttendance(dateTime);
+    }
+
+    public void updateAttendance(final Attendance oldAttendance, final Attendance attendance) {
+        attendances.remove(oldAttendance);
+        attendances.add(attendance);
+
+    }
+
+    public Attendances getAttendances() {
+        return attendances;
+    }
+
+    public AttendanceCounter getAttendanceCounter() {
+        return attendanceCounter;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Crew crew = (Crew) o;
+        return Objects.equals(nickname, crew.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nickname);
     }
 }
