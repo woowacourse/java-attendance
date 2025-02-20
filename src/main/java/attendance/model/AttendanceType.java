53c238f265ae03482ac457b71525ff6b2a53905c
Yehyeok Bang <qkddpgur318@gmail.com>
2025-02-20 14:37:23 +0900

    refactor: 출석 유형 스스로 판단할 수 있도록 수정
    Co-authored-by: abc5259 <dlwogns3413@naver.com>

--- src/main/java/attendance/model/AttendanceType.java
+++ src/main/java/attendance/model/AttendanceType.java
@@ -2,34 +2,32 @@ package attendance.model;
 
 import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.function.Function;
 
 public enum AttendanceType {
 
-    OK(Integer.MIN_VALUE, 5),
-    LATE(6, 30),
-    ABSENCE(31, Integer.MAX_VALUE),
+    OK((difMinutes) -> difMinutes <= 5),
+    LATE((difMinutes) -> difMinutes > 5 && difMinutes <= 30),
+    ABSENCE((difMinutes) -> difMinutes > 30),
     ;
 
-    private final int openMinutes;
-    private final int closeMinutes;
+    private static final int MINUTE_SCALE = 60;
 
-    AttendanceType(int openMinutes, int closeMinutes) {
-        this.openMinutes = openMinutes;
-        this.closeMinutes = closeMinutes;
+    private final Function<Integer, Boolean> isMatch;
+
+    AttendanceType(Function<Integer, Boolean> isMatch) {
+        this.isMatch = isMatch;
     }
 
     public static AttendanceType judge(LocalTime startTime, LocalTime attendanceTime) {
         return Arrays.stream(values())
-                .filter(attendanceType -> {
-                    int difSecond = attendanceTime.toSecondOfDay() - startTime.toSecondOfDay();
-                    int difMinutes = difSecond / 60;
-                    return attendanceType.contains(difMinutes);
-                })
+                .filter(attendanceType -> attendanceType.isMatch.apply(calculateDifMinutes(startTime, attendanceTime)))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("판단할 수 없습니다."));
     }
 
-    public boolean contains(int minutes) {
-        return openMinutes <= minutes && minutes <= closeMinutes;
+    private static int calculateDifMinutes(LocalTime startTime, LocalTime attendanceTime) {
+        int difSecond = attendanceTime.toSecondOfDay() - startTime.toSecondOfDay();
+        return difSecond / MINUTE_SCALE;
     }
 }
