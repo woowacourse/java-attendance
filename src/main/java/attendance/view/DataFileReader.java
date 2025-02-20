d36ecc270ecfd4be2f7c2b1d599f830a59d6b2ab
saera <yook.saera@gmail.com>
2025-02-20 16:56:47 +0900

    refactor: DataFileReader 메서드 분리 및 indent 감소

--- src/main/java/attendance/view/DataFileReader.java
+++ src/main/java/attendance/view/DataFileReader.java
@@ -1,6 +1,7 @@
 package attendance.view;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,15 +12,28 @@ public class DataFileReader {
 
     public static List<String> read() {
         List<String> datas = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_PATH))) {
-            bufferedReader.readLine();
-            String input;
-            while((input = bufferedReader.readLine()) != null){
-                datas.add(input);
-            }
-        } catch (IOException e) {
-            System.out.println("입출력 예외 발생: " + e.getMessage());
+        BufferedReader bufferedReader = createReader();
+        readLine(bufferedReader);
+        String input;
+        while ((input = readLine(bufferedReader)) != null) {
+            datas.add(input);
         }
         return datas;
+    }
+
+    private static BufferedReader createReader() {
+        try {
+            return new BufferedReader(new FileReader(FILE_PATH));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("파일을 찾을 수 없습니다.", e);
+        }
+    }
+
+    private static String readLine(BufferedReader bufferedReader) {
+        try {
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException("입출력 오류 발생", e);
+        }
     }
 }
