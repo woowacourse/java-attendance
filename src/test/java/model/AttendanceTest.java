6a05c00559f29bb8b2b7c54a6306cbdeb188e2dc
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-20 15:51:11 +0900

    test: 테스트 이름 작성

--- src/test/java/model/AttendanceTest.java
+++ src/test/java/model/AttendanceTest.java
@@ -5,30 +5,26 @@ import java.time.LocalDateTime;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class AttendanceTest {
+class AttendanceTest {
 
     @Test
-    void test7() {
+    void 정시에_도착한_경우_출석이다() {
         LocalDateTime dateTime = LocalDateTime.of(2024, 12, 10, 10, 0);
-
         Attendance attendance = Attendance.from(dateTime);
         Assertions.assertThat(attendance).isEqualTo(Attendance.출석);
     }
 
     @Test
-    void test8() {
+    void _5분_초과로_늦게온_경우_지각이다() {
         LocalDateTime dateTime = LocalDateTime.of(2024, 12, 10, 10, 6);
-
         Attendance attendance = Attendance.from(dateTime);
         Assertions.assertThat(attendance).isEqualTo(Attendance.지각);
     }
 
     @Test
-    void test9() {
+    void _31분_초과로_늦게온_경우_결석이다() {
         LocalDateTime dateTime = LocalDateTime.of(2024, 12, 10, 10, 31);
-
         Attendance attendance = Attendance.from(dateTime);
         Assertions.assertThat(attendance).isEqualTo(Attendance.결석);
     }
-
 }
