217ff510ccb3a1c4ce61f15d92fb13beb0155155
bowook <mbosx316@naver.com>
2025-02-20 16:58:50 +0900

    feat(Crew) : Crew 일급 컬렉션 사용 추가

---
+++ src/main/java/attendance/domain/Crews.java
@@ -0,0 +1,44 @@
+package attendance.domain;
+
+import attendance.exception.CustomException;
+import attendance.exception.ErrorMessage;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Crews {
+
+    private final Set<Crew> crews;
+
+    private Crews(Set<Crew> crews) {
+        this.crews = crews;
+    }
+
+    public static Crews fromCrewsFile(Set<String> crews) {
+        Set<Crew> crewNames = new HashSet<>();
+        for (String crewName : crews) {
+            crewNames.add(Crew.from(crewName));
+        }
+        return new Crews(crewNames);
+    }
+
+    public Crew findCrew(String crewName) {
+        return crews.stream().filter(crew ->
+                crew.checkSameName(crewName))
+                .findFirst()
+                .orElseThrow(() -> CustomException.from(ErrorMessage.NICKNAME_NOT_PRESENCE));
+    }
+
+    public Set<Crew> getCrews() {
+        return this.crews;
+    }
+
+    public void register(Map<Crew, DateInfos> register, DateInfos dateInfos) {
+        for (Crew crew : crews) {
+            register.put(crew, dateInfos);
+        }
+    }
+}
+
+
