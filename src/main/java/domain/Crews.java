32545aa62be25ec0406d538cad336c9e92c3f363
ryan-dia <cjfdnjs158@naver.com>
2025-02-20 15:32:58 +0900

    feat: 크루 정렬 기능 추가(처벌 기준에 따른)
    Co-authored-by: 2Jin1031 <2jin2.1031@gmail.com>

--- src/main/java/domain/Crews.java
+++ src/main/java/domain/Crews.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Crews {
@@ -11,8 +12,36 @@ public class Crews {
 
     public Crew findByNickname(final Nickname nickname) {
         return crews.stream()
-                .filter(o -> nickname.equals(o.nickname))
+                .filter(o -> o.nickname.equals(nickname))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("크루가 존재하지 않습니다."));
     }
+
+    public List<Crew> getSortedCrews() {
+        return crews.stream().
+                sorted(
+                        punishmentOrder
+                                .thenComparing(punishmentOrder1)
+                                .thenComparing(punishmentOrder2)
+                )
+                .toList();
+    }
+
+    private final Comparator<Crew> punishmentOrder = Comparator
+            .comparingInt((Crew crew) -> Punishment.findByAbsenceCount(getPunishment(crew)).getAbsenceCount())
+            .reversed();
+
+    private static int getPunishment(final Crew crew) {
+        final AttendanceCounter attendanceCounter = crew.getAttendanceCounter();
+        return (attendanceCounter.getTardiness() * 3) + attendanceCounter.getAbsence();
+    }
+
+    private final Comparator<Crew> punishmentOrder1 = Comparator
+            .comparingInt((Crew crew) -> crew.getAttendanceCounter().getAbsence())
+            .reversed();
+
+    private final Comparator<Crew> punishmentOrder2 = Comparator
+            .comparingInt((Crew crew) -> crew.getAttendanceCounter().getTardiness())
+            .reversed();
+
 }
