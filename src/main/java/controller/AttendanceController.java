f8e84ccdfcf0e4044408a1bfdbe257a955d1726c
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-20 15:57:28 +0900

    feat: 출석 확인 기능 호출 구현

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -1,22 +1,43 @@
 package controller;
 
+import domain.AbsentPolicy;
+import domain.AttendanceDateTime;
 import domain.AttendanceSheet;
 import domain.AttendanceSheets;
 import domain.AttendanceState;
 import util.FileReaderUtil;
+import view.InputView;
+import view.OutputView;
 
-import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AttendanceController {
 
     public void run() throws IOException {
+    private final InputView inputView;
+
+    public AttendanceController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        LocalDate date = LocalDate.of(2024, 12, 13);
+
         AttendanceSheetsFactory attendanceSheetsFactory = new AttendanceSheetsFactory(new FileReaderUtil());
         AttendanceSheets attendanceSheets = attendanceSheetsFactory.create();
 
-        List<AttendanceSheet> foundAttendanceSheet = attendanceSheets.findAttendanceByNickname("쿠키");
-        foundAttendanceSheet.stream()
-                .forEach(attendanceSheet -> System.out.println(attendanceSheet.getNickname() + ", " + attendanceSheet.getAttendanceDateTime()));
+        while (true) {
+            String select = inputView.inputMenu(LocalDate.now());
+
+            if (select.equals("1")) {
+                attend(date, attendanceSheets);
+                continue;
+            }
 
         List<String> allNames = attendanceSheets.findAllNames();
         System.out.println(allNames);
@@ -26,6 +47,21 @@ public class AttendanceController {
 
             int lateCount = 0;
             int absentCount = 0;
+            if (select.equals("Q")) {
+                return;
+            }
+        }
+    }
+
+    private void attend(LocalDate date, AttendanceSheets attendanceSheets) {
+        String nickname = inputView.inputNickname();
+        String time = inputView.inputTime();
+        int hour = Integer.parseInt(time.split(":")[0]);
+        int minute = Integer.parseInt(time.split(":")[1]);
+
+        LocalDateTime localDateTime = LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(), hour, minute);
+        attendanceSheets.add(new AttendanceSheet(nickname, AttendanceDateTime.from(localDateTime)));
+    }
 
             for (AttendanceSheet attendanceSheet : attendanceByNickname) {
                 AttendanceState state = attendanceSheet.getAttendanceDateTime().check();
