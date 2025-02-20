202d06ce50225f3cef8a8c261261c74e0eff7605
(minjae8563) <(minjae8563@naver.com)>
2025-02-20 14:11:05 +0900

    refactor : 가독성을 위해 String 문자열을 멤버 변수로 사용

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -15,6 +15,13 @@ public class InputView {
     private final static String RECORD_PRINT_FOR_EACH_CREW_MENU = "3. 크루별 출석 기록 확인";
     private final static String DISMISSAL_CREW_CHECK_MENU = "4. 제적 위험자 확인";
     private final static String QUICK = "Q";
+    private final static String TIME_FORMATTER = "HH:mm";
+    private final static String PROMPT_TIME_INPUT_TO_MODIFY = "언제로 변경하겠습니까?";
+    private final static String PROMPT_DAY_INPUT_TO_MODIFY = "수정하려는 날짜(일)를 입력해 주세요.";
+    private final static String PROMPT_STUDENT_NAME_INPUT_TO_MODIFY = "출석을 수정하려는 크루의 닉네임을 입력해 주세요.";
+    private final static String PROMPT_STUDENT_NAME_INPUT = "닉네임을 입력해 주세요.";
+    private final static String PROMPT_START_TIME_INPUT = "등교 시간을 입력해 주세요.";
+    private final static String MENU_OPTION = "[1-4]|Q";
 
     private final static Scanner scanner = new Scanner(System.in);
 
@@ -49,27 +56,26 @@ public class InputView {
     }
 
     private static String isQOrOneOrTwoOrThreeOrFour(String input){
-        String regex = "[1-4]|Q";
-        if (!input.matches(regex)) {
+        if (!input.matches(MENU_OPTION)) {
             throw new IllegalArgumentException("[ERROR] 메뉴에 없는 선택지 입니다.");
         }
         return input;
     }
 
     public static void printInputNicName(){
-        System.out.println("닉네임을 입력해 주세요.");
+        System.out.println(PROMPT_STUDENT_NAME_INPUT);
     }
 
     public static void printStartTime(){
-        System.out.println("등교 시간을 입력해 주세요.");
+        System.out.println(PROMPT_START_TIME_INPUT);
     }
 
     public static void printStudentNameForModify(){
-        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        System.out.println(PROMPT_STUDENT_NAME_INPUT_TO_MODIFY);
     }
 
     public static int inputDateForModify(){
-        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        System.out.println(PROMPT_DAY_INPUT_TO_MODIFY);
         try{
             int date = Integer.parseInt(userInput());
             if(date < 1 || date > 31){
@@ -83,13 +89,13 @@ public class InputView {
     }
 
     public static void printTimeForModify(){
-        System.out.println("언제로 변경하겠습니까?");
+        System.out.println(PROMPT_TIME_INPUT_TO_MODIFY);
     }
 
     public static LocalDateTime makeLocalDateToLocalDateTime(LocalDate localDate) {
         String time = userInput();
         try {
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(TIME_FORMATTER);
             LocalTime localTime = LocalTime.parse(time,dateTimeFormatter);
             return localDate.atTime(localTime);
         } catch (DateTimeException e) {
