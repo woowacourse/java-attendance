56c8f7a12f95d75165c074f6b0cf4c8550bd310f
ryan-dia <cjfdnjs158@naver.com>
2025-02-20 15:44:57 +0900

    chore: application 추가
    Co-authored-by: 2Jin1031 <2jin2.1031@gmail.com>

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import controller.AttendanceController;
+
+public class Application {
+
+    public static void main(String[] args) {
+        AttendanceController attendanceController = new AttendanceController();
+        attendanceController.start();
+    }
+}
