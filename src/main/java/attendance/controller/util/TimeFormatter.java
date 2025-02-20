9283069efaf1a069143f354ec590348042819bfb
ChoiDongJoon <adong0808@naver.com>
2025-02-20 14:28:12 +0900

    feat: 등교 시간 입력 포맷 검증

--- src/main/java/attendance/controller/util/TimeFormatter.java
+++ src/main/java/attendance/controller/util/TimeFormatter.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class TimeFormatter {
     private static final String TIME_FORMAT = "HH:mm";
@@ -12,8 +13,13 @@ public class TimeFormatter {
     }
 
     public static LocalDateTime format(final LocalDate now, final String inputDateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIME_FORMAT);
-        LocalTime time = LocalTime.parse(inputDateTime, formatter);
-        return LocalDateTime.of(now, time);
+        try{
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIME_FORMAT);
+            LocalTime time = LocalTime.parse(inputDateTime, formatter);
+            return LocalDateTime.of(now, time);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(String.format("[ERROR] 시간은 %s 형식으로 입력해주세요.", TIME_FORMAT));
+        }
+
     }
 }
