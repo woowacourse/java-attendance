b7341767411af070ae5e4b8526e1d4e91f631929
RaZel713 <razel7130@gmail.com>
2025-02-20 13:57:30 +0900

    test: 변경된 자료형 반영

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,9 +1,11 @@
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import domain.Parser;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,7 +32,9 @@ public class ParserTest {
     @Test
     public void csv파일파서_이름분리_테스트() {
         List<String> removedData = List.of("쿠키,2024-12-13 10:08", "빙봉,2024-12-13 10:07");
-        List<List<String>> expectedNameSeperatedData = List.of(List.of("쿠키", "2024-12-13 10:08"), List.of("빙봉", "2024-12-13 10:07"));
+        List<List<String>> expectedNameSeperatedData = List.of(
+                List.of("쿠키", "2024-12-13 10:08"),
+                List.of("빙봉", "2024-12-13 10:07"));
 
         assertThat(Parser.parseName(removedData)).isEqualTo(expectedNameSeperatedData);
     }
@@ -39,7 +43,8 @@ public class ParserTest {
     @Test
     public void csv파일파서_크루정보분리_테스트() {
         String rawDate = "2024-12-13 10:08";
-        List<String> expectedSeperatedData = List.of("2024-12-13", "10:08");
+        Map<LocalDate, LocalTime> expectedSeperatedData =
+                Map.of(LocalDate.parse("2024-12-13"), LocalTime.parse("10:08")); // 중간에 map으로 변경한 것 반영되지 않아 추가
 
         assertThat(Parser.parseDate(rawDate)).isEqualTo(expectedSeperatedData);
     }
