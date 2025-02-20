0b4bfb108711f727e836bd33aa4ff386cea4e248
ryan-dia <cjfdnjs158@naver.com>
2025-02-20 16:10:54 +0900

    refactor: controller 메서드 분리
    Co-authored-by: 2Jin1031 <2jin2.1031@gmail.com>

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -8,6 +8,7 @@ import domain.Nickname;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import util.Constants;
 import util.CrewGenerator;
 import util.CsvReader;
 import view.InputView;
@@ -15,53 +16,75 @@ import view.OutputView;
 
 public class AttendanceController {
 
+    private static String CSV_PATH = "src/main/resources/attendances.csv";
+
     public void start() {
-        LocalDateTime fixDateTime = LocalDateTime.of(2024, 12, 14, 0, 0, 0, 0);
+        LocalDateTime fixDateTime = LocalDateTime.of(2024, Constants.MONTH, 14, 0, 0, 0, 0);
         final String input = InputView.readCommand(fixDateTime);
         Command command = Command.findByCommandNumber(input);
-        Crews crews = CrewGenerator.generate(CsvReader.readFile("src/main/resources/attendances.csv"),
+        Crews crews = CrewGenerator.generate(CsvReader.readFile(CSV_PATH),
                 fixDateTime.toLocalDate());
 
-        if (command.equals(Command.CHECK_ATTENDEES)) {
-            String inputNickName = InputView.readNickName();
-            Nickname nickname = new Nickname(inputNickName);
-            Crew crew = crews.findByNickname(nickname);
-            String inputDateTime = InputView.readDateTime();
-            final LocalDate localDate = fixDateTime.toLocalDate();
-            final LocalTime dateTime = LocalTime.parse(inputDateTime);
-            if (crew.isAttended(LocalDateTime.of(localDate, dateTime))) {
-                throw new IllegalArgumentException("이미 출석했습니다. 다음에는 수정기능을 이용해주세요.");
+        while (true) {
+            if (command.equals(Command.QUIT)) {
+                break;
             }
-            final Attendance attendance = new Attendance(LocalDateTime.of(localDate, dateTime));
-            crew.add(attendance);
-            OutputView.printAttendance(attendance);
-
-        } else if (command.equals(Command.EDIT_ATTENDANCE)) {
-            String inputNickName = InputView.readUpdateNickName();
-            Nickname nickname = new Nickname(inputNickName);
-            Crew crew = crews.findByNickname(nickname);
-            String inputUpdateDate = InputView.readUpdateDate();
-            String inputUpdateTime = InputView.readUpdateDateTime();
-
-            final LocalDate localDate = LocalDate.of(2024, 12, Integer.parseInt(inputUpdateDate));
-            final LocalTime oldTime = LocalTime.parse(inputUpdateTime);
-            LocalDateTime oldDateTime = LocalDateTime.of(localDate, oldTime);
-
-            Attendance oldAttendance = crew.getAttendance(oldDateTime);
-            Attendance newAttendance = new Attendance(oldDateTime);
-            crew.updateAttendance(oldAttendance, newAttendance);
-            OutputView.printUpdateAttendance(oldAttendance, newAttendance);
-
-        } else if (command.equals(Command.CHECK_THE_ATTENDANCE_RECORD_BY_CREW)) {
-            String inputNickName = InputView.readNickName();
-            Nickname nickname = new Nickname(inputNickName);
-            Crew crew = crews.findByNickname(nickname);
-
-            OutputView.printCrewAttendances(crew);
-
-        } else if (command.equals(Command.CONFIRMATION_OF_THOSE_AT_RISK_OF_EXPULSION)) {
-            OutputView.printAllExpulsion(crews);
-
+            if (command.equals(Command.CHECK_ATTENDEES)) {
+                processCheckAttendees(crews, fixDateTime);
+                continue;
+            }
+            if (command.equals(Command.EDIT_ATTENDANCE)) {
+                processEditAttendance(crews);
+                continue;
+            }
+            if (command.equals(Command.CHECK_THE_ATTENDANCE_RECORD_BY_CREW)) {
+                processAttendanceRecordByCrew(crews);
+                continue;
+            }
+            if (command.equals(Command.CONFIRMATION_OF_THOSE_AT_RISK_OF_EXPULSION)) {
+                OutputView.printAllExpulsion(crews);
+            }
         }
+
+    }
+
+    private static void processAttendanceRecordByCrew(final Crews crews) {
+        String inputNickName = InputView.readNickName();
+        Nickname nickname = new Nickname(inputNickName);
+        Crew crew = crews.findByNickname(nickname);
+
+        OutputView.printCrewAttendances(crew);
+    }
+
+    private static void processEditAttendance(final Crews crews) {
+        String inputNickName = InputView.readUpdateNickName();
+        Nickname nickname = new Nickname(inputNickName);
+        Crew crew = crews.findByNickname(nickname);
+        String inputUpdateDate = InputView.readUpdateDate();
+        String inputUpdateTime = InputView.readUpdateDateTime();
+
+        final LocalDate localDate = LocalDate.of(2024, Constants.MONTH, Integer.parseInt(inputUpdateDate));
+        final LocalTime oldTime = LocalTime.parse(inputUpdateTime);
+        LocalDateTime oldDateTime = LocalDateTime.of(localDate, oldTime);
+
+        Attendance oldAttendance = crew.getAttendance(oldDateTime);
+        Attendance newAttendance = new Attendance(oldDateTime);
+        crew.updateAttendance(oldAttendance, newAttendance);
+        OutputView.printUpdateAttendance(oldAttendance, newAttendance);
+    }
+
+    private static void processCheckAttendees(final Crews crews, final LocalDateTime fixDateTime) {
+        String inputNickName = InputView.readNickName();
+        Nickname nickname = new Nickname(inputNickName);
+        Crew crew = crews.findByNickname(nickname);
+        String inputDateTime = InputView.readDateTime();
+        final LocalDate localDate = fixDateTime.toLocalDate();
+        final LocalTime dateTime = LocalTime.parse(inputDateTime);
+        if (crew.isAttended(LocalDateTime.of(localDate, dateTime))) {
+            throw new IllegalArgumentException("이미 출석했습니다. 다음에는 수정기능을 이용해주세요.");
+        }
+        final Attendance attendance = new Attendance(LocalDateTime.of(localDate, dateTime));
+        crew.add(attendance);
+        OutputView.printAttendance(attendance);
     }
 }
