e40bce41f65ef3717b79cd16c253b784f3a5b45d
BWY <quantumjump10@gmail.com>
2025-02-20 15:55:28 +0900

    refactor: 공휴일에 Attendance 객체가 생성되는 경우, 예외 처리 추가

--- src/main/java/domain/Attendance.java
+++ src/main/java/domain/Attendance.java
@@ -10,9 +10,16 @@ public class Attendance {
     private LocalTime attendanceTime;
 
     public Attendance(Day day, LocalTime attendanceTime) {
+        validateDay(day);
         this.day = day;
         this.attendanceTime = attendanceTime;
         updateStatus();
+    }
+
+    private void validateDay(Day day) {
+        if (day.checkHoliday()) {
+            throw new IllegalArgumentException("[ERROR] 등교일이 아닙니다.");
+        }
     }
 
     public Boolean isEqualTo(LocalDate date) {
