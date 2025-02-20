4d5bc2d0cbb5b1cff547a4e2ec66f7815c6270f2
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-20 11:22:10 +0900

    feat(OutputView): 크루별 출석 기록 조회 및 제적 위험자 확인 출력 구현

--- src/main/java/attendance/view/OutputView.java
+++ src/main/java/attendance/view/OutputView.java
@@ -7,6 +7,14 @@ public class OutputView {
     private static final String ATTENDANCE_STRING = "%s월 %s일 %s요일 %s (%s)";
     private static final String MODIFIED_ATTENDANCE_STRING = " -> %s (%s) 수정 완료!";
     private static final String CREW_ATTENDANCE_HISTORY_STRING = "이번 달 %s의 출석 기록입니다.";
+    private static final String CREW_STATISTICS_STRING = """
+            출석: %s회
+            지각: %s회
+            결석: %s회
+            """;
+    private static final String CREW_STATUS_STRING = "%s 대상자입니다.";
+    private static final String EXPEL_CREWS_HEAD_STRING = "제적 위험자 조회 결과";
+    private static final String EXPEL_CREW_BODY_STRING = "- %s: 결석 %s회, 지각 %s회 (%s)";
 
     // print methods
     // TODO 출석 확인 기능 출력:
@@ -45,6 +53,15 @@ public class OutputView {
         System.out.println();
     }
 
+    // TODO 크루별 출석 기록 확인 기능 출력:
+    // 2월 13일 목요일 10:07 (지각)
+    // 2월 14일 금요일 --:-- (결석)
+    //
+    // 출석: 3회
+    // 지각: 0회
+    // 결석: 3회
+    //
+    // 면담 대상자입니다.
     public void printCrewAttendanceHistory(String crewName, List<List<String>> crewAttendanceHistory) {
         System.out.println(
                 CREW_ATTENDANCE_HISTORY_STRING.formatted(crewName)
@@ -66,28 +83,41 @@ public class OutputView {
                     )
             );
         }
+        System.out.println();
     }
 
-    // TODO 크루별 출석 기록 확인 기능 출력:
-    // 이번 달 빙티의 출석 기록입니다.
-    //
-    // (주말 출력 X)
-    // 02월 02일 월요일 13:00 (출석)
-    // 02월 03일 화요일 10:07 (지각)
-    // 02월 04일 수요일 10:02 (출석)
-    // 02월 05일 목요일 10:06 (지각)
-    // 02월 06일 금요일 10:01 (출석)
-    // 02월 09일 월요일 --:-- (결석)
-    // (전날까지의 기록 출력)
-    // TODO >>>>>>
-    // 출석: 3회
-    // 지각: 0회
-    // 결석: 3회
-    //
-    // 면담 대상자입니다.
+    public void printCrewStatisticStatus(List<String> crewStatisticStatus) {
+        System.out.println(
+                CREW_STATISTICS_STRING.formatted(
+                        crewStatisticStatus.get(0),
+                        crewStatisticStatus.get(1),
+                        crewStatisticStatus.get(2)
+                )
+        );
+
+        System.out.println(
+                CREW_STATUS_STRING.formatted(crewStatisticStatus.get(3))
+        );
+        System.out.println();
+    }
 
     // TODO 제적 위험자 확인 기능 출력:
     // 제적 위험자 조회 결과
     // - 빙티: 결석 3회, 지각 4회 (면담)
     // (해당 되지 않는 사람은 출력 X)
+    public void printExpelCrewHead() {
+        System.out.println(EXPEL_CREWS_HEAD_STRING);
+    }
+
+    public void printExpelCrew(List<String> crewExpelExpectedInfo) {
+        System.out.println(
+                EXPEL_CREW_BODY_STRING.formatted(
+                        crewExpelExpectedInfo.get(0),
+                        crewExpelExpectedInfo.get(1),
+                        crewExpelExpectedInfo.get(2),
+                        crewExpelExpectedInfo.get(3)
+                )
+        );
+        System.out.println();
+    }
 }
