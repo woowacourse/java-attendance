37a4e4865caa87229605c83a3f9970d27390f030
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-20 17:27:36 +0900

    test(AttendanceTest): Nested로 성공 테스트를 분리하라

--- src/test/java/domain/AttendanceTest.java
+++ src/test/java/domain/AttendanceTest.java
@@ -5,37 +5,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class AttendanceTest {
 
-    @Test
-    @DisplayName("출석 객체 생성 테스트")
-    void test1() {
-        //given
-        final String time = "2024-12-13 10:08";
-        final LocalDateTime expectedTime = LocalDateTime.parse(time, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
 
-        //when
-        final Attendance attendance = new Attendance(time);
+    @Nested
+    @DisplayName("성공 테스트")
+    class SuccessCases {
+        @Test
+        @DisplayName("출석 객체 생성 테스트")
+        void test1() {
+            //given
+            final String time = "2024-12-13 10:08";
+            final LocalDateTime expectedTime = LocalDateTime.parse(time,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
 
-        //then
-        assertThat(attendance.getDateTime()).isEqualTo(expectedTime);
+            //when
+            final Attendance attendance = new Attendance(time);
+
+            //then
+            assertThat(attendance.getDateTime()).isEqualTo(expectedTime);
+
+        }
+
+        @Test
+        @DisplayName("출석 상태 계산 테스트")
+        void test2() {
+            //given
+            final String time = "2024-12-13 10:31";
+            final Attendance attendance = new Attendance(time);
+
+            //when
+            final AttendanceStatus actual = attendance.calculateStatus();
+
+            //then
+            assertThat(actual).isEqualTo(AttendanceStatus.ABSENCE);
+        }
 
     }
-
-    @Test
-    @DisplayName("출석 상태 계산 테스트")
-    void test2() {
-        //given
-        final String time = "2024-12-13 10:31";
-        final Attendance attendance = new Attendance(time);
-
-        //when
-        final AttendanceStatus actual = attendance.calculateStatus();
-
-        //then
-        assertThat(actual).isEqualTo(AttendanceStatus.ABSENCE);
-    }
-
 }
