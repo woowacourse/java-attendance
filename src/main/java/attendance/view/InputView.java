be7c5b35c46412269612bf32187b6d7e96418499
saera <yook.saera@gmail.com>
2025-02-20 16:47:47 +0900

    refactor: InputView 문자열 상수화

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -8,40 +8,37 @@ import java.time.format.DateTimeParseException;
 
 public class InputView {
     private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    public static final String ENTER_NAME = "\n닉네임을 입력해 주세요.\n";
+    public static final String ENTER_NAME_FOR_MODIFY = "\n출석을 수정하려는 크루의 닉네임을 입력해 주세요.\n";
+    public static final String ENTER_DAY_FOR_MODIFY = "수정하려는 날짜(일)를 입력해 주세요.\n";
+    public static final String ENTER_NEW_TIME = "언제로 변경하겠습니까?\n";
+    private static final String ENTER_ATTENDANCE_TIME = "등교 시간을 입력해 주세요.\n";
 
     public static String readOption() {
         return readLine();
     }
 
     public static String readNickName() {
-        System.out.println("닉네임을 입력해 주세요.");
+        System.out.print(ENTER_NAME);
         return readLine();
     }
 
-    private static String readLine(){
-        try {
-            return bufferedReader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException("입출력 오류 발생");
-        }
-    }
-
     public static LocalTime readAttendanceTime() {
-        System.out.println("등교 시간을 입력해 주세요.");
+        System.out.print(ENTER_ATTENDANCE_TIME);
         try {
             return LocalTime.parse(readLine());
         } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("잘못된 시간 형식입니다. HH:mm 형식으로 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] 잘못된 시간 형식입니다. HH:mm 형식으로 입력해주세요.");
         }
     }
 
     public static String readModifyNickName() {
-        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        System.out.print(ENTER_NAME_FOR_MODIFY);
         return readLine();
     }
 
     public static int readModifyDay() {
-        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        System.out.print(ENTER_DAY_FOR_MODIFY);
         try{
             return Integer.parseInt(readLine());
         } catch (NumberFormatException e){
@@ -50,11 +47,19 @@ public class InputView {
     }
 
     public static LocalTime readModifyTime() {
-        System.out.println("언제로 변경하겠습니까?");
+        System.out.print(ENTER_NEW_TIME);
         try{
             return LocalTime.parse(readLine());
         } catch (DateTimeParseException e){
             throw new IllegalArgumentException("[ERROR] 시간을 HH:mm 형식으로 입력해주세요.");
+        }
+    }
+
+    private static String readLine(){
+        try {
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException("입출력 오류 발생");
         }
     }
 }
