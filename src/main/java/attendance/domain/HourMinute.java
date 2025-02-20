0d02c64893dba8c7b6b5ef75ebfef3ca6b698024
saera <yook.saera@gmail.com>
2025-02-20 16:49:06 +0900

    refactor: HourMinute 시간 값이 없음을 의미하는 상수 추가

--- src/main/java/attendance/domain/HourMinute.java
+++ src/main/java/attendance/domain/HourMinute.java
@@ -3,6 +3,8 @@ package attendance.domain;
 import java.time.LocalDateTime;
 
 public record HourMinute(int hour, int minute, AttendanceStatus attendanceStatus) {
+    public static final int NULL_TIME = -1;
+
     public HourMinute(LocalDateTime localDateTime) {
         this(localDateTime.getHour(), localDateTime.getMinute(), AttendanceChecker.checkAttendance(localDateTime));
     }
