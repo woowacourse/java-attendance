3138826797f02871ca365bc589ed66e3c407590f
BWY <quantumjump10@gmail.com>
2025-02-20 16:35:13 +0900

    refactor: AttendanceController 메서드 분리

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -23,66 +23,76 @@ public class AttendanceController {
     }
 
     public void run() {
-        String option;
+        String option = "";
         crews.recordAllAbsence();
-        do {
+        while (!option.equals("Q")) {
             outputView.printOptionMessage();
             option = inputView.getOption();
 
-            if (option.equals("1")) {
-                checkHoliday(LocalDate.now());
-
-                String nickname = inputView.getNickname();
-                Crew crew = crews.findByNickname(nickname);
-
-                if (crew.isAlreadyAttend(LocalDate.now())) {
-                    System.out.println("이미 출석 완료되었습니다. 수정 기능을 이용해주세요.");
-                    continue;
-                }
-
-                String attendanceTimeInput = inputView.getAttendanceTime();
-                LocalTime attendanceTime = Converter.convertStringToLocalTime(attendanceTimeInput);
-                Attendance attendance = new Attendance(new Day(LocalDate.now()), attendanceTime);
-                crew.addAttendance(attendance);
-                outputView.printAttendanceInformation(attendance.toDto());
-            }
-
-
-            if (option.equals("2")) {
-                String editNickname = inputView.getEditNickname();
-                Integer dayOfMonth = Converter.convertStringToInteger(inputView.getEditDayOfMonth());
-                LocalTime attendanceTime = Converter.convertStringToLocalTime(inputView.getNewTime());
-
-                Crew crew = crews.findByNickname(editNickname);
-                Attendance attendance = crew.findByDate(dayOfMonth);
-                AttendanceDto originalAttendanceDto = attendance.toDto();
-
-                attendance.updateAttendanceTime(attendanceTime);
-                AttendanceDto editedAttendanceDto = attendance.toDto();
-
-                outputView.printUpdatedAttendanceHistory(originalAttendanceDto, editedAttendanceDto);
-            }
-
-
-            if (option.equals("3")) {
-                String nickname = inputView.getNickname();
-                Crew crew = crews.findByNickname(nickname);
-
-                outputView.printCrewAttendanceHistoryMessage(nickname);
-                outputView.printAttendanceHistoryWithCrew(crew);
-            }
-
-
-            if (option.equals("4")) {
-                List<CrewDto> crewDtos = crews.createCrewDtos();
-                List<CrewDto> penaltyCrewDtos = crewDtos.stream()
-                        .filter(crewDto -> crewDto.getPenaltyStatus() != PenaltyStatus.NONE)
-                        .collect(Collectors.toCollection(ArrayList::new));
-
-                outputView.printPenaltyCrews(new CrewDtos(penaltyCrewDtos));
-            }
+            processAttendanceTasks(option);
         }
-        while (!option.equals("Q"));
+    }
+
+    private void processAttendanceTasks(String option) {
+        processAttendance(option);
+        processAttendanceEdit(option);
+        processAttendanceHistory(option);
+        processPenaltyCheck(option);
+    }
+
+    public void processAttendance(String option) {
+        if (!option.equals("1")) return;
+        checkHoliday(LocalDate.now());
+        Crew crew = crews.findByNickname(inputView.getNickname());
+
+        if (crew.isAlreadyAttend(LocalDate.now())) {
+            System.out.println("이미 출석 완료되었습니다. 수정 기능을 이용해주세요.");
+            return;
+        }
+
+        registerAttendance(crew);
+    }
+
+    private void registerAttendance(Crew crew) {
+        LocalTime attendanceTime = Converter.convertStringToLocalTime(inputView.getAttendanceTime());
+        Attendance attendance = new Attendance(new Day(LocalDate.now()), attendanceTime);
+        crew.addAttendance(attendance);
+        outputView.printAttendanceInformation(attendance.toDto());
+    }
+
+    public void processAttendanceEdit(String option) {
+        if (!option.equals("2")) return;
+
+        Crew crew = crews.findByNickname(inputView.getEditNickname());
+        Attendance attendance = crew.findByDate(Converter.convertStringToInteger(inputView.getEditDayOfMonth()));
+        AttendanceDto originalAttendanceDto = attendance.toDto();
+
+        attendance.updateAttendanceTime(Converter.convertStringToLocalTime(inputView.getNewTime()));
+        AttendanceDto editedAttendanceDto = attendance.toDto();
+
+        outputView.printUpdatedAttendanceHistory(originalAttendanceDto, editedAttendanceDto);
+    }
+
+    public void processAttendanceHistory(String option) {
+        if (!option.equals("3")) return;
+
+        String nickname = inputView.getNickname();
+        Crew crew = crews.findByNickname(nickname);
+
+        outputView.printCrewAttendanceHistoryMessage(nickname);
+        outputView.printAttendanceHistoryWithCrew(crew);
+    }
+
+    public void processPenaltyCheck(String option) {
+        if (!option.equals("4")) return;
+
+        List<CrewDto> crewDtos = crews.createCrewDtos();
+        List<CrewDto> penaltyCrewDtos = crewDtos.stream()
+                .filter(crewDto -> crewDto.getPenaltyStatus() != PenaltyStatus.NONE)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        outputView.printPenaltyCrews(new CrewDtos(penaltyCrewDtos));
+
     }
 
     private void checkHoliday(LocalDate todayDate) {
@@ -90,8 +100,12 @@ public class AttendanceController {
         int month = todayDate.getMonth().getValue();
         int dayOfMonth = todayDate.getDayOfMonth();
         String dayOfWeekName = DayOfWeek.getNameById(todayDate.getDayOfWeek().getValue());
+        
         if (today.checkHoliday()) {
             throw new IllegalArgumentException("[ERROR] " + month + "월 " + dayOfMonth + "일 " + dayOfWeekName + "은 등교일이 아닙니다.");
         }
     }
 }
+
+
+
