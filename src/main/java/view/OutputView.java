286740821946a24dbc99d4b6f785e0704bc7ad4e
BWY <quantumjump10@gmail.com>
2025-02-20 15:54:17 +0900

    feat: OutputView 클래스 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,10 +1,10 @@
 package view;
 
-import domain.AttendanceDto;
-import domain.DayOfWeek;
+import domain.*;
 import util.Converter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class OutputView {
     private static final String ATTENDANCE_HISTORY_MESSAGE = "이번 달 %s의 출석 기록입니다.";
@@ -14,7 +14,14 @@ public class OutputView {
             "3. 크루별 출석 기록 확인\n" +
             "4. 제적 위험자 확인\n" +
             "Q. 종료\n";
-
+    private static final String UPDATE_COMPLETE_MESSAGE = "%d월 %02d일 %s %s (%s) -> %s (%s) 수정 완료!\n";
+    private static final String CREW_ATTENDANCE_HISTORY_MESSAGE = "이번 달 %s의 출석 기록입니다.\n\n";
+    private static final String ATTENDANCE_HISTORY_WITH_DATE = "%d월 %02d일 %s %s (%s)\n";
+    private static final String ATTENDANCE_COUNT = "출석: %d회\n";
+    private static final String LATE_COUNT = "지각: %d회\n";
+    private static final String ABSENT_COUNT = "결석: %d회\n\n";
+    private static final String PENALTY_CREW_READ_RESULT_PREFIX = "제적 위험자 조회 결과";
+    private static final String PENALTY_CREW_READ_RESULT = "- %s: 결석 %d회, 지각 %d회 (%s)\n";
 
     public void printOptionMessage() {
         LocalDate today = LocalDate.now();
@@ -26,7 +33,62 @@ public class OutputView {
         LocalDate today = LocalDate.now();
         String dayOfWeekName = DayOfWeek.getNameById(today.getDayOfWeek().getValue());
         String attendanceTime = Converter.covertLocalTimeToString(attendanceDto.getAttendanceTime());
+        String attendanceStatusName = getAttendanceStatusName(attendanceDto);
+        System.out.printf("%d월 %02d일 %s %s (%s)\n", today.getMonth().getValue(), today.getDayOfMonth(), dayOfWeekName, attendanceTime, attendanceStatusName);
+    }
 
+    public void printUpdatedAttendanceHistory(AttendanceDto originalAttendanceDto, AttendanceDto editedAttendanceDto) {
+
+        LocalDate date = editedAttendanceDto.getDate();
+        String dayOfWeekName = DayOfWeek.getNameById(date.getDayOfWeek().getValue());
+
+        String originalAttendanceTime = Converter.covertLocalTimeToString(originalAttendanceDto.getAttendanceTime());
+        String editedAttendanceTime = Converter.covertLocalTimeToString(editedAttendanceDto.getAttendanceTime());
+
+        String originAttendanceStatusName = getAttendanceStatusName(originalAttendanceDto);
+        String editedAttendanceStatusName = getAttendanceStatusName(editedAttendanceDto);
+
+        System.out.printf(UPDATE_COMPLETE_MESSAGE,
+                date.getMonth().getValue(), date.getDayOfMonth(), dayOfWeekName,
+                originalAttendanceTime, originAttendanceStatusName,
+                editedAttendanceTime, editedAttendanceStatusName);
+    }
+
+    public void printCrewAttendanceHistoryMessage(String nickname) {
+        System.out.printf(CREW_ATTENDANCE_HISTORY_MESSAGE, nickname);
+    }
+
+    public void printAttendanceHistoryWithCrew(Crew crew) {
+        CrewDto crewDto = crew.toDto();
+        int lateCount = crewDto.getLateCount();
+        int absentCount = crewDto.getAbsentCount();
+
+        int attendanceCount = crew.getAttendances().size() - lateCount - absentCount;
+
+        for (Attendance attendance : crew.getAttendances()) {
+            AttendanceDto dto = attendance.toDto();
+            LocalDate date = dto.getDate();
+            String attendanceTime = "--:--";
+            String dayOfWeekName = DayOfWeek.getNameById(date.getDayOfWeek().getValue());
+            String attendanceStatusName = getAttendanceStatusName(dto);
+
+            if (dto.getAttendanceTime() != null) {
+                attendanceTime = Converter.covertLocalTimeToString(dto.getAttendanceTime());
+            }
+
+            System.out.printf(ATTENDANCE_HISTORY_WITH_DATE, date.getMonth().getValue(), date.getDayOfMonth(), dayOfWeekName, attendanceTime, attendanceStatusName);
+        }
+
+        System.out.println();
+
+        System.out.printf(ATTENDANCE_COUNT, attendanceCount);
+        System.out.printf(LATE_COUNT, lateCount);
+        System.out.printf(ABSENT_COUNT, absentCount);
+
+        System.out.println(crewDto.getPenaltyStatus().getName() + " 대상자입니다.\n");
+    }
+
+    private String getAttendanceStatusName(AttendanceDto attendanceDto) {
         String attendanceStatusName = "출석";
         if (attendanceDto.getAbsent()) {
             attendanceStatusName = "결석";
@@ -35,7 +97,17 @@ public class OutputView {
         if (attendanceDto.getLate()) {
             attendanceStatusName = "지각";
         }
-        System.out.printf("%d월 %02d일 %s %s (%s)\n", today.getMonth().getValue(), today.getDayOfMonth(), dayOfWeekName, attendanceTime, attendanceStatusName);
+        return attendanceStatusName;
+    }
+
+    public void printPenaltyCrews(CrewDtos crewDtos) {
+        System.out.println(PENALTY_CREW_READ_RESULT_PREFIX);
+        List<CrewDto> dtos = crewDtos.getCrewDtos();
+        for (CrewDto dto : dtos) {
+            System.out.printf(PENALTY_CREW_READ_RESULT, dto.getNickName(), dto.getAbsentCount(), dto.getLateCount(), dto.getPenaltyStatus().getName());
+        }
+        System.out.println();
+
     }
 
 }
