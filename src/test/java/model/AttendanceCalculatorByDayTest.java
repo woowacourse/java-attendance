cb597f8490d43518a630e2e0cc1e67ef9f80b53b
LJM\eueo8 <eueo8259@gmail.com>
2025-02-20 17:24:32 +0900

    refactor : test 클래스들도 model 패키지로 이동

--- src/test/java/model/AttendanceCalculatorByDayTest.java
+++ src/test/java/model/AttendanceCalculatorByDayTest.java
@@ -1,6 +1,6 @@
+package model;
+
 import java.time.LocalTime;
-import model.AttendanceCalculatorByDay;
-import model.AttendanceStatus;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
--- src/test/java/model/StudentRepositoryTest.java
+++ src/test/java/model/StudentRepositoryTest.java
@@ -1,12 +1,10 @@
+package model;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import controller.Controller;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import model.AttendanceStatus;
-import model.Student;
-import model.StudentRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,14 +19,7 @@ public class StudentRepositoryTest {
     @DisplayName("존재하지 않는 학생을 입력시 예외처리 한다.")
     void test1() {
         String input = "포비";
-
-        /*InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));*/
-
-        assertThatThrownBy(() -> studentRepository.notExistStudent("포비"))
+        assertThatThrownBy(() -> studentRepository.notExistStudent(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 등록되지 않은 닉네임입니다.");
     }
@@ -45,22 +36,21 @@ public class StudentRepositoryTest {
     @DisplayName("이름 기준으로 학생 객체 찾는 기능")
     void test3() {
         String name = "짱수";
-        Student student6 = studentRepository.findStudentByName(name);
-        assertThat(student6).isEqualTo(student);
+        Student student = studentRepository.findStudentByName(name);
+        String expectStudentName = student.getName();
+        assertThat(name).isEqualTo(expectStudentName);
+
     }
 
     @Test
     @DisplayName("등교 시간을 바탕으로 출석 기록 업데이트하는 기능")
     void test4() {
         String name = "짱수";
-        int month = LocalDateTime.now().getMonthValue();
-        int day = LocalDateTime.now().getDayOfMonth();
-        LocalDateTime localDateTime = LocalDateTime.of(2025, month, day,9,59);
+        LocalDateTime localDateTime = LocalDateTime.of(2025, 12, 13,9,59);
 
-        Student student6 = studentRepository.findStudentByName(name);
-        student6.updateState(localDateTime);
-        assertThat(student6.getAttendance()).isEqualTo(1);
-
+        Student student = studentRepository.findStudentByName(name);
+        student.updateState(localDateTime);
+        assertThat(student.getAttendance()).isEqualTo(1);
     }
 
     @Test
