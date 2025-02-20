409c2ec77af10ec8a0e766cdf80e7caa098c691d
songsunkook <songsunkook@gmail.com>
2025-02-20 17:47:46 +0900

    refactor: 불필요한 catch문 제거

--- src/main/java/util/FileParser.java
+++ src/main/java/util/FileParser.java
@@ -2,7 +2,6 @@ package util;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -26,8 +25,6 @@ public class FileParser {
             while ((line = br.readLine()) != null) {
                 result.add(parseAttendanceHistory(line));
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
