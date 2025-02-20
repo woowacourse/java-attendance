cc0216f5423a87c701981baf71fa8ca61d028c35
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-20 15:50:12 +0900

    refactor: 패키지명 수정

--- src/test/java/AttendanceTimeTest.java
+++ src/test/java/AttendanceTimeTest.java
@@ -1,19 +1,19 @@
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.stream.Stream;
-
-import domain.AttendanceState;
 import domain.AttendanceDateTime;
+import domain.AttendanceState;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AttendanceTimeTest {
 
