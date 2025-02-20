1b2ad65b858cbf30a03e15ba942c5be6baa106f4
Yehyeok Bang <qkddpgur318@gmail.com>
2025-02-20 14:50:07 +0900

    feat: 입력에 문제가 생긴 경우 예외 처리 구현
    Co-authored-by: abc5259 <dlwogns3413@naver.com>

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -13,32 +13,32 @@ public class InputView {
         System.out.printf("오늘은 %02d월 %02d일 %s요일입니다. 기능을 선택해 주세요.%n", today.getMonth().getValue(), today.getDayOfMonth(),
                 today.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN));
         System.out.println("1. 출석 확인\n2. 출석 수정\n3. 크루별 출석 기록 확인\n4. 제적 위험자 확인\nQ. 종료");
-        return trim(scanner.nextLine());
+        return trim(readLine());
     }
 
     public String inputNickname() {
         System.out.println("닉네임을 입력해 주세요.");
-        return trim(scanner.nextLine());
+        return trim(readLine());
     }
 
     public String inputAttendanceTime() {
         System.out.println("등교 시간을 입력해 주세요.");
-        return trim(scanner.nextLine());
+        return trim(readLine());
     }
 
     public String inputNicknameForUpdateAttendance() {
         System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
-        return trim(scanner.nextLine());
+        return trim(readLine());
     }
 
     public int inputDateForUpdateAttendance() {
         System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
-        return parseInt(trim(scanner.nextLine()));
+        return parseInt(trim(readLine()));
     }
 
     public String inputTimeForUpdateAttendance() {
         System.out.println("언제로 변경하시겠습니끼?");
-        return trim(scanner.nextLine());
+        return trim(readLine());
     }
 
     private String trim(String input) {
@@ -50,6 +50,14 @@ public class InputView {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private String readLine() {
+        try {
+            return scanner.nextLine();
+        } catch (RuntimeException e) {
+            throw new IllegalStateException("입력을 받는 중 문제가 발생했습니다.");
         }
     }
 }
