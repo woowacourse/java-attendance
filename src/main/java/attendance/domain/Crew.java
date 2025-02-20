28cdf4bd4e3410c3cf6a8cdcf828a467ff066282
ChoiDongJoon <adong0808@naver.com>
2025-02-20 17:05:27 +0900

    refactor: Map 필드 제거

--- src/main/java/attendance/domain/Crew.java
+++ src/main/java/attendance/domain/Crew.java
@@ -2,21 +2,18 @@ package attendance.domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 public class Crew implements Comparable<Crew> {
+    public static final int LATE_TO_ABSENCE_UNIT = 3;
 
     private final String nickname;
     private final List<Attendance> attendances;
-    private final Map<AttendanceStatus, Integer> statusCount;
 
     public Crew(final String nickname, final List<Attendance> attendances) {
         this.nickname = nickname;
         this.attendances = attendances;
-        statusCount = new HashMap<>();
-        attendances.forEach((attendance) -> addStatusCount(attendance, 1));
     }
 
     public boolean isEqualToNickname(final String nickname) {
@@ -34,39 +31,45 @@ public class Crew implements Comparable<Crew> {
 
     public void addAttendance(final Attendance attendance) {
         attendances.add(attendance);
-        statusCount.put(attendance.getStatus(), statusCount.getOrDefault(attendance.getStatus(), 0) + 1);
-    }
-
-    public void addStatusCount(Attendance attendance, int amount) {
-        statusCount.put(attendance.getStatus(), statusCount.getOrDefault(attendance.getStatus(), 0) + amount);
     }
 
     public Warning checkWarning() {
         return Warning.check(calculateTotalAbsenceCount());
     }
 
-    private int calculateTotalAbsenceCount() {
-        return statusCount.getOrDefault(AttendanceStatus.ABSENCE, 0)
-                + statusCount.getOrDefault(AttendanceStatus.LATE_ABSENCE, 0)
-                + statusCount.getOrDefault(AttendanceStatus.LATE, 0) / 3;
+    private long calculateTotalAbsenceCount() {
+        return countAbsence() + (countLate() / LATE_TO_ABSENCE_UNIT);
+    }
+
+    public long countAttend() {
+        return attendances.stream()
+                .filter(attendance -> attendance.getStatus().equals(AttendanceStatus.ATTEND))
+                .count();
+    }
+
+    public long countAbsence() {
+        return attendances.stream()
+                .filter(attendance -> {
+                    AttendanceStatus status = attendance.getStatus();
+                    return status.equals(AttendanceStatus.ABSENCE) || status.equals(AttendanceStatus.LATE_ABSENCE);
+                })
+                .count();
+    }
+
+    public long countLate() {
+        return attendances.stream()
+                .filter(attendance -> attendance.getStatus().equals(AttendanceStatus.LATE))
+                .count();
     }
 
     public Attendance updateAttendance(final LocalDateTime dateTime) {
-        return attendances.stream()
+        Attendance after = attendances.stream()
                 .filter(attendance -> attendance.isEqualToDate(LocalDate.from(dateTime)))
                 .findFirst()
-                .map(attendance -> {
-                    AttendanceStatus before = attendance.getStatus();
-                    AttendanceStatus after = attendance.updateDateTime(dateTime);
-                    updateStatusCount(before, after);
-                    return attendance;
-                })
+                .map(attendance -> attendance.updateDateTime(dateTime))
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 출석 기록이 없습니다."));
-    }
 
-    private void updateStatusCount(AttendanceStatus before, AttendanceStatus after) {
-        statusCount.put(before, statusCount.getOrDefault(before, 0) - 1);
-        statusCount.put(after, statusCount.getOrDefault(after, 0) + 1);
+        return after;
     }
 
     public Attendance findAttendanceByDate(final LocalDate updateDate) {
@@ -78,12 +81,11 @@ public class Crew implements Comparable<Crew> {
 
     @Override
     public int compareTo(Crew o) {
-        int targetAbsenceCount = o.calculateTotalAbsenceCount();
-        int targetLateCount = o.statusCount
-                .getOrDefault(AttendanceStatus.LATE, 0) % 3;
+        long targetAbsenceCount = o.calculateTotalAbsenceCount();
+        long targetLateCount = o.countLate() % LATE_TO_ABSENCE_UNIT;
 
-        int absenceCount = calculateTotalAbsenceCount();
-        int lateCount = statusCount.getOrDefault(AttendanceStatus.LATE, 0) % 3;
+        long absenceCount = this.calculateTotalAbsenceCount();
+        long lateCount = this.countLate() % LATE_TO_ABSENCE_UNIT;
 
         if (targetAbsenceCount > absenceCount) {
             return 1;
@@ -107,9 +109,5 @@ public class Crew implements Comparable<Crew> {
 
     public List<Attendance> getAttendances() {
         return attendances;
-    }
-
-    public Map<AttendanceStatus, Integer> getStatusCount() {
-        return statusCount;
     }
 }
