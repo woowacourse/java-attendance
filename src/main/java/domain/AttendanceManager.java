facf7417bc76153429507aa5ee8822ddd27337d5
JO-eusan <goodsan0330@gmail.com>
2025-02-20 16:51:01 +0900

    refactor: 예외 처리 방식 변경 및 불필요한 메서드 제거

--- src/main/java/domain/AttendanceManager.java
+++ src/main/java/domain/AttendanceManager.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class AttendanceManager {
 
-    private Map<String, Records> crews;
+    private final Map<String, Records> crews;
 
     public AttendanceManager() {
         this.crews = new HashMap<>();
@@ -21,11 +21,7 @@ public class AttendanceManager {
     }
 
     public TimeAndStatus attendCrew(String name, LocalDateTime localDateTime) {
-        Records records = crews.get(name);
-
-        if (records == null) {
-            throw new IllegalArgumentException("존재하지 않는 크루입니다.");
-        }
+        Records records = findByName(name);
 
         if (records.isSameDate(localDateTime)) {
             throw new IllegalArgumentException("이미 출석한 경우 수정 기능을 사용하세요.");
@@ -35,7 +31,7 @@ public class AttendanceManager {
     }
 
     public TimeAndStatus editCrew(String name, LocalDateTime newLocalDateTime) {
-        Records records = crews.get(name);
+        Records records = findByName(name);
 
         if (!records.isSameDate(newLocalDateTime)) {
             throw new IllegalArgumentException("수정 기능은 출석 후 이용 가능합니다.");
@@ -44,13 +40,8 @@ public class AttendanceManager {
         return records.edit(newLocalDateTime);
     }
 
-    public Map<String, StatisticsResult> findWarningCrews(LocalDate nowDate) {
-        return AttendanceStatistics.calculateExpelledWarning(nowDate, crews);
-    }
-
     public Map<String, StatisticsResult> sortCrew(LocalDate nowDate) {
-        Map<String, StatisticsResult> sortedResult = AttendanceStatistics.calculateExpelledWarning(
-            nowDate, crews);
+        Map<String, StatisticsResult> sortedResult = findWarningCrews(nowDate);
 
         return sortedResult
             .entrySet().stream().sorted(Comparator.comparing(
@@ -63,8 +54,14 @@ public class AttendanceManager {
                 Map::putAll);
     }
 
+    public Map<String, StatisticsResult> findWarningCrews(LocalDate nowDate) {
+        return AttendanceStatistics.calculateExpelledWarning(nowDate, crews);
+    }
 
     public Records findByName(String name) {
+        if(crews.get(name) == null){
+            throw new IllegalArgumentException("존재하지 않는 크루입니다.");
+        }
         return crews.get(name);
     }
 }
