9ff7d5d9d6a235a83fc9c629afd50034e63c2619
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-21 16:06:23 +0900

    refactor: Predicate 를 사용해 관리 상태를 도출하도록 변경

--- src/main/java/attendance/model/domain/attendance/ManagementStatus.java
+++ src/main/java/attendance/model/domain/attendance/ManagementStatus.java
@@ -1,32 +1,31 @@
 package attendance.model.domain.attendance;
 
 import attendance.model.domain.attendance.vo.WarningCount;
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 public enum ManagementStatus {
 
-    EXPULSION("제적"),
-    COUNSELING("면담"),
-    WARNING("경고"),
-    NONE("일반");
+    EXPULSION("제적", count -> count > 5),
+    COUNSELING("면담", count -> count > 2),
+    WARNING("경고", count -> count > 1),
+    NONE("일반", count -> count <= 1);
 
     private final String name;
+    private final Predicate<Integer> judgement;
 
-    ManagementStatus(final String name) {
+    ManagementStatus(String name, Predicate<Integer> judgement) {
         this.name = name;
+        this.judgement = judgement;
     }
 
     public static ManagementStatus from(final WarningCount warningCount) {
         final int policyAppliedAbsenceCount = warningCount.getPolicyAppliedAbsenceCount();
-        if (policyAppliedAbsenceCount > 5) {
-            return EXPULSION;
-        }
-        if (policyAppliedAbsenceCount > 2) {
-            return COUNSELING;
-        }
-        if (policyAppliedAbsenceCount > 1) {
-            return WARNING;
-        }
-        return NONE;
+
+        return Arrays.stream(values())
+                .filter(status -> status.judgement.test(policyAppliedAbsenceCount))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당되는 관리 상태가 없습니다."));
     }
 
     public boolean requiresManagement() {
