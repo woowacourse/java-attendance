b728496a195a50437d6bb5da794751fa4949c45f
threepebbles <threepebbles@naver.com>
2025-02-21 18:57:33 +0900

    fix: 사용자 입력에 대해 예외 발생 시 재입력 받도록 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,12 @@
 package view;
 
+import dto.AttendanceModifyRequest;
+import dto.AttendanceRequest;
+import dto.OptionRequest;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
 import java.util.Locale;
 import java.util.Scanner;
-
-import dto.AttendanceModifyRequest;
-import dto.AttendanceRequest;
-import dto.OptionRequest;
 import util.DateTimeUtil;
 import util.RetryHandler;
 
@@ -19,16 +18,16 @@ public class InputView {
         return RetryHandler.retryUntilSuccessWithReturn(() -> {
             LocalDate now = DateTimeUtil.nowDate();
             System.out.printf("""
-                오늘은 %d월 %d일 %s요일입니다. 기능을 선택해 주세요.
-                1. 출석 확인
-                2. 출석 수정
-                3. 크루별 출석 기록 확인
-                4. 제적 위험자 확인
-                Q. 종료 
-                """,
-                now.getMonth().getValue(),
-                now.getDayOfMonth(),
-                now.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN)
+                            오늘은 %d월 %d일 %s요일입니다. 기능을 선택해 주세요.
+                            1. 출석 확인
+                            2. 출석 수정
+                            3. 크루별 출석 기록 확인
+                            4. 제적 위험자 확인
+                            Q. 종료 
+                            """,
+                    now.getMonth().getValue(),
+                    now.getDayOfMonth(),
+                    now.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN)
             );
             String option = scanner.nextLine();
             System.out.println();
@@ -37,29 +36,35 @@ public class InputView {
     }
 
     public static AttendanceRequest scanAttendance() {
-        System.out.println("닉네임을 입력해 주세요.");
-        String nickname = scanner.nextLine();
-        System.out.println("등교 시간을 입력해 주세요.");
-        String time = scanner.nextLine();
-        System.out.println();
-        return AttendanceRequest.of(nickname, time);
+        return RetryHandler.retryUntilSuccessWithReturn(() -> {
+            System.out.println("닉네임을 입력해 주세요.");
+            String nickname = scanner.nextLine();
+            System.out.println("등교 시간을 입력해 주세요.");
+            String time = scanner.nextLine();
+            System.out.println();
+            return AttendanceRequest.of(nickname, time);
+        });
     }
 
     public static AttendanceModifyRequest scanModify() {
-        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
-        String nickname = scanner.nextLine();
-        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
-        String day = scanner.nextLine();
-        System.out.println("언제로 변경하겠습니까?");
-        String time = scanner.nextLine();
-        System.out.println();
-        return AttendanceModifyRequest.of(nickname, day, time);
+        return RetryHandler.retryUntilSuccessWithReturn(() -> {
+            System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+            String nickname = scanner.nextLine();
+            System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+            String day = scanner.nextLine();
+            System.out.println("언제로 변경하겠습니까?");
+            String time = scanner.nextLine();
+            System.out.println();
+            return AttendanceModifyRequest.of(nickname, day, time);
+        });
     }
 
     public static String scanNickname() {
-        System.out.println("닉네임을 입력해 주세요.");
-        String nickname = scanner.nextLine();
-        System.out.println();
-        return nickname;
+        return RetryHandler.retryUntilSuccessWithReturn(() -> {
+            System.out.println("닉네임을 입력해 주세요.");
+            String nickname = scanner.nextLine();
+            System.out.println();
+            return nickname;
+        });
     }
 }
