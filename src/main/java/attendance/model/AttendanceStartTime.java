4e4577bd06141c0a031898441f7b81ab92cdb6f3
Yehyeok Bang <qkddpgur318@gmail.com>
2025-02-21 18:08:25 +0900

    refactor: 축약했던 이름을 수정합니다.
    - 축약하지 않는다는 요구사항을 지키도록 수정합니다.

--- src/main/java/attendance/model/AttendanceStartTime.java
+++ src/main/java/attendance/model/AttendanceStartTime.java
@@ -6,11 +6,11 @@ import java.util.Arrays;
 
 public enum AttendanceStartTime {
 
-    MON(DayOfWeek.MONDAY, LocalTime.of(13, 0)),
-    TUE(DayOfWeek.TUESDAY, LocalTime.of(10, 0)),
-    WED(DayOfWeek.WEDNESDAY, LocalTime.of(10, 0)),
-    THR(DayOfWeek.THURSDAY, LocalTime.of(10, 0)),
-    FRI(DayOfWeek.FRIDAY, LocalTime.of(10, 0)),
+    MONDAY(DayOfWeek.MONDAY, LocalTime.of(13, 0)),
+    TUESDAY(DayOfWeek.TUESDAY, LocalTime.of(10, 0)),
+    WEDNESDAY(DayOfWeek.WEDNESDAY, LocalTime.of(10, 0)),
+    THURSDAY(DayOfWeek.THURSDAY, LocalTime.of(10, 0)),
+    FRIDAY(DayOfWeek.FRIDAY, LocalTime.of(10, 0)),
     ;
 
     private final DayOfWeek dayOfWeek;
