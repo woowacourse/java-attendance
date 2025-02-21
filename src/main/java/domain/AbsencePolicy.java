69c7dc0b87021229d94d2c1c2f5727d62e847304
dltmddus <tmddus4671@gmail.com>
2025-02-21 23:17:00 +0900

    refactor: getAbsensePolicy 리턴 타입 수정(String -> AbsencePolicy)

--- src/main/java/domain/AbsencePolicy.java
+++ src/main/java/domain/AbsencePolicy.java
@@ -7,7 +7,7 @@ public enum AbsencePolicy {
     WARNING(2, "경고"),
     INTERVIEW(3, "면담"),
     DISMISSED(6, "제적"),
-    PASS(Integer.MAX_VALUE, "통과");
+    PASS(Integer.MAX_VALUE, "대상자 아님");
 
     private final int value;
     private final String description;
@@ -21,13 +21,16 @@ public enum AbsencePolicy {
         return value;
     }
 
-    public static String getAbsencePolicy(int absentCount, int lateCount) {
+    public String getDescription() {
+        return description;
+    }
+
+    public static AbsencePolicy getAbsencePolicy(int absentCount, int lateCount) {
         int totalAbsentCount = absentCount + (lateCount / 3);
 
         return Stream.of(DISMISSED, INTERVIEW, WARNING)
                 .filter(policy -> totalAbsentCount >= policy.value)
                 .findFirst()
-                .map(policy -> policy.description)
-                .orElse(PASS.description);
+                .orElse(PASS);
     }
 }
