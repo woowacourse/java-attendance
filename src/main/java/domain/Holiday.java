a8470ecb2c944705f1685bb8e0ae01e2bad487af
threepebbles <threepebbles@naver.com>
2025-02-21 16:48:38 +0900

    refactor(Holiday): description 필드 추가

--- src/main/java/domain/Holiday.java
+++ src/main/java/domain/Holiday.java
@@ -4,30 +4,24 @@ import java.time.LocalDate;
 import java.util.Arrays;
 
 public enum Holiday {
-    CHRISTMAS(12, 25),
-    NONE(0, 0),
+    CHRISTMAS("크리스마스", 12, 25),
+    NONE("공휴일이 아닌 경우", 0, 0),
     ;
 
+    private final String description;
     private final int month;
     private final int day;
 
-    Holiday(int month, int day) {
+    Holiday(String description, int month, int day) {
+        this.description = description;
         this.month = month;
         this.day = day;
     }
 
-    public int getMonth() {
-        return month;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
     public static Holiday from(LocalDate date) {
         return Arrays.stream(values())
-            .filter(holiday -> holiday.month == date.getMonthValue() && holiday.day == date.getDayOfMonth())
-            .findAny()
-            .orElse(NONE);
+                .filter(holiday -> holiday.month == date.getMonthValue() && holiday.day == date.getDayOfMonth())
+                .findAny()
+                .orElse(NONE);
     }
 }
