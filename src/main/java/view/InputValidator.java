ccb325eb47a909c6c5565633f090beeb620ba065
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-21 15:31:55 +0900

    test(view): 메뉴 입력 예외 테스트 작성

---
+++ src/main/java/view/InputValidator.java
@@ -0,0 +1,7 @@
+package view;
+
+public class InputValidator {
+    public static void validateMenuInput(String menuInput) {
+        //
+    }
+}
---
+++ src/test/java/view/InputValidatorTest.java
@@ -0,0 +1,26 @@
+package view;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class InputValidatorTest {
+    @DisplayName("기능 선택을 위해 [1, 2, 3, 4, Q(q)]의 문자를 입력할 시 정상 동작한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "4", "Q", "q"})
+    void test1(String menuInput) {
+        assertDoesNotThrow(() -> InputValidator.validateMenuInput(menuInput));;
+    }
+
+    @DisplayName("기능 선택을 위해 [1, 2, 3, 4, Q(q)]의 문자를 입력할 시 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"5", "a", "2200000000"})
+    void test2(String menuInput) {
+        assertThatThrownBy(() -> InputValidator.validateMenuInput(menuInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
