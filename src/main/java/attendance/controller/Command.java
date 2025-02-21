fd3b6a8b8117b54591033f12364ad5ce9a3af7ea
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-21 15:30:56 +0900

    refactor: Command 가 Controller 의 Consumer 를 가지도록 변경

--- src/main/java/attendance/controller/Command.java
+++ src/main/java/attendance/controller/Command.java
@@ -1,21 +1,24 @@
 package attendance.controller;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
 
 public enum Command {
 
-    ATTENDANCE("1", "출석 확인"),
-    UPDATE_ATTENDANCE("2", "출석 수정"),
-    GET_ATTENDANCE_LOG("3", "크루별 출석 기록 확인"),
-    GET_REQUIRES_MANAGEMENT_CREWS("4", "제적 위험자 확인"),
-    QUIT("Q", "종료");
+    ATTENDANCE("1", "출석 확인", Controller::attendance),
+    UPDATE_ATTENDANCE("2", "출석 수정", Controller::updateAttendance),
+    GET_ATTENDANCE_LOG("3", "크루별 출석 기록 확인", Controller::checkCrewAttendance),
+    GET_REQUIRES_MANAGEMENT_CREWS("4", "제적 위험자 확인", Controller::printRequiresManagementCrews),
+    QUIT("Q", "종료", Controller::quit);
 
     private final String option;
     private final String description;
+    private final Consumer<Controller> action;
 
-    Command(String option, String description) {
+    Command(String option, String description, Consumer<Controller> action) {
         this.option = option;
         this.description = description;
+        this.action = action;
     }
 
     public static Command from(String option) {
@@ -25,11 +28,11 @@ public enum Command {
                 .orElseThrow(() -> new IllegalArgumentException("해당하는 기능은 존재하지 않습니다."));
     }
 
-    public String getOption() {
-        return option;
+    public String getMenu() {
+        return option + ". " + description;
     }
 
-    public String getDescription() {
-        return description;
+    public void run(Controller controller) {
+        action.accept(controller);
     }
 }
