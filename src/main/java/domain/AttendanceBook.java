94e4d9e5b9a541ca29b78c69460294d5ef175ab0
RaZel713 <razel7130@gmail.com>
2025-02-21 16:35:16 +0900

    refactor: AttendanceBook 객체 코드 순서 변경

--- src/main/java/domain/AttendanceBook.java
+++ src/main/java/domain/AttendanceBook.java
@@ -20,61 +20,6 @@ public class AttendanceBook {
         this.crews = new ArrayList<>();
     }
 
-    public List<CrewPenaltyResponse> checkPenaltyCrew() {
-        List<CrewPenaltyResponse> crewPenaltyResponses = new ArrayList<>();
-
-        for (Crew crew : crews) {
-            List<AttendanceRecordResponse> attendanceRecords = crew.getAttendanceRecords();
-            TotalRecordsResponse totalRecords = TotalRecordsResponse.fromAttendanceRecords(attendanceRecords);
-
-            int penaltyCount = getPenaltyCount(totalRecords);
-
-            crewPenaltyResponses.add(
-                    new CrewPenaltyResponse(
-                            crew.getName(),
-                            totalRecords.absentCount(),
-                            totalRecords.lateCount(),
-                            PenaltyStatus.getByPenaltyCount(penaltyCount)
-                    )
-            );
-        }
-
-        return crewPenaltyResponses;
-    }
-
-    public boolean checkCrewAlreadyExists(String name) {
-        boolean result = false;
-
-        for (Crew crew : crews) {
-            if (crew.hasName(name)) {
-                result = true;
-            }
-        }
-
-        return result;
-    }
-
-    public void validateDateAlreadyExistsByCrewName(String name, LocalDate date) {
-        Crew foundCrew = getCrewByName(name);
-        foundCrew.validateDateAlreadyExists(date);
-    }
-
-    public void initialize(String name, Map<LocalDate, LocalTime> dateAndTime) {
-        if (!checkCrewAlreadyExists(name)) {
-            addNewCrew(Crew.createByName(name));
-        }
-        addDailyAttendanceByName(name, dateAndTime);
-    }
-
-    public void addNewCrew(Crew newCrew) {
-        crews.add(newCrew);
-    }
-
-    public void addDailyAttendanceByName(String name, Map<LocalDate, LocalTime> dateAndTime) {
-        Crew suitableCrew = getCrewByName(name);
-        suitableCrew.addDailyAttendance(dateAndTime);
-    }
-
     public Crew getCrewByName(String name) {
         return crews.stream()
                 .filter(crew -> crew.hasName(name))
@@ -82,6 +27,43 @@ public class AttendanceBook {
                 .orElseThrow(() -> new IllegalArgumentException(ErrorCode.NICKNAME_NOT_FOUND.getMessage()));
     }
 
+    // 데이터 유효성 검사
+    public void validateNameAlreadyExists(String name) {
+        if (!checkCrewAlreadyExists(name)) {
+            throw new IllegalArgumentException(ErrorCode.NICKNAME_NOT_FOUND.getMessage());
+        }
+    }
+
+    public void validateDateAlreadyExistsByCrewName(String name, LocalDate date) {
+        Crew foundCrew = getCrewByName(name);
+        foundCrew.validateDateAlreadyExists(date);
+    }
+
+    public void validateIsInOperationHour(LocalTime time) {
+        if (!time.isAfter(OPERATION_TIME_START) || !time.isBefore(OPERATION_TIME_END)) {
+            throw new IllegalArgumentException(ErrorCode.TIME_NOT_IN_OPERATION_HOUR.getMessage());
+        }
+    }
+
+    // 출석부 초기화: csv 파일 데이터를 적용
+    public void initialize(String name, Map<LocalDate, LocalTime> dateAndTime) {
+        if (!checkCrewAlreadyExists(name)) {
+            addNewCrew(Crew.createByName(name));
+        }
+        Crew crew = getCrewByName(name);
+        crew.addDailyAttendance(dateAndTime);
+    }
+
+    public boolean checkCrewAlreadyExists(String name) {
+        return crews.stream()
+                .anyMatch(crew -> crew.hasName(name));
+    }
+
+    public void addNewCrew(Crew newCrew) {
+        crews.add(newCrew);
+    }
+
+    // 기능 1. 출석 확인
     public AttendanceRecordResponse checkAttendance(String name, Map<LocalDate, LocalTime> dateAndTime) {
         Crew foundCrew = getCrewByName(name);
 
@@ -101,6 +83,7 @@ public class AttendanceBook {
         return new AttendanceRecordResponse(date, time, AttendanceStatus.judgeStatus(date, time));
     }
 
+    // 기능 2. 출석 수정
     public ModifyAttendanceResponse modifyAttendance(String name, Map<LocalDate, LocalTime> dateAndTimeToModify) {
 
         Crew foundCrew = getCrewByName(name);
@@ -128,19 +111,30 @@ public class AttendanceBook {
         );
     }
 
+    // 기능 4. 제적 위험자 확인
+    public List<CrewPenaltyResponse> checkPenaltyCrew() {
+        List<CrewPenaltyResponse> crewPenaltyResponses = new ArrayList<>();
+
+        for (Crew crew : crews) {
+            List<AttendanceRecordResponse> attendanceRecords = crew.getAttendanceRecords();
+            TotalRecordsResponse totalRecords = TotalRecordsResponse.fromAttendanceRecords(attendanceRecords);
+
+            int penaltyCount = getPenaltyCount(totalRecords);
+
+            crewPenaltyResponses.add(
+                    new CrewPenaltyResponse(
+                            crew.getName(),
+                            totalRecords.absentCount(),
+                            totalRecords.lateCount(),
+                            PenaltyStatus.getByPenaltyCount(penaltyCount)
+                    )
+            );
+        }
+
+        return crewPenaltyResponses;
+    }
+
     public int getPenaltyCount(TotalRecordsResponse totalRecords) {
         return totalRecords.absentCount() + (totalRecords.lateCount() / 3);
-    }
-
-    public void validateIsInOperationHour(LocalTime time) {
-        if (!time.isAfter(OPERATION_TIME_START) || !time.isBefore(OPERATION_TIME_END)) {
-            throw new IllegalArgumentException(ErrorCode.TIME_NOT_IN_OPERATION_HOUR.getMessage());
-        }
-    }
-
-    public void validateNameAlreadyExists(String name) {
-        if (!checkCrewAlreadyExists(name)) {
-            throw new IllegalArgumentException(ErrorCode.NICKNAME_NOT_FOUND.getMessage());
-        }
     }
 }
