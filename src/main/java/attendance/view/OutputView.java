547f43dc685d3ae49594125c653f1fda63c10b76
kimdev <kim__dev@hufs.ac.kr>
2025-02-21 14:17:30 +0900

    fix: 사용자 환경에 따라 다르게 출력되던 요일 출력 문구 수정

--- src/main/java/attendance/view/OutputView.java
+++ src/main/java/attendance/view/OutputView.java
@@ -16,9 +16,9 @@ public class OutputView {
 
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
     private static final DateTimeFormatter ATTENDANCE_DATE_TIME_FORMATTER =
-            DateTimeFormatter.ofPattern("MM월 dd일 E요일 HH:mm");
+            DateTimeFormatter.ofPattern("M월 dd일 E요일 HH:mm").withLocale(Locale.KOREA);
     private static final DateTimeFormatter ABSENT_DATE_TIME__FORMATTER =
-            DateTimeFormatter.ofPattern("MM월 dd일 E요일 --:--");
+            DateTimeFormatter.ofPattern("M월 dd일 E요일 --:--").withLocale(Locale.KOREA);
 
     public void printOperations(LocalDate today) {
         System.out.printf("오늘은 %d월 %d일 %s입니다. 기능을 선택해 주세요.%n", today.getMonthValue(), today.getDayOfMonth(),
