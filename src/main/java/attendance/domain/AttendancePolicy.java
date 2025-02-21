2f6d4cec23b3b876f317366d92824be7d700aaae
kjyyjk <rlawnsdud920@gmail.com>
2025-02-21 15:42:21 +0900

    refactor: 캠퍼스 관련된 시간을 CampusRule로 Enum 상수화
    - 기존 AttendancePolicy내에 있는 캠퍼스 관련된 시간 상수들을
    CampusRule이라는 Enum 상수로 추출한다.

--- src/main/java/attendance/domain/AttendancePolicy.java
+++ src/main/java/attendance/domain/AttendancePolicy.java
@@ -3,24 +3,24 @@ package attendance.domain;
 import static attendance.domain.AttendanceType.ABSENCE;
 import static attendance.domain.AttendanceType.ATTENDANCE;
 import static attendance.domain.AttendanceType.LATE;
+import static attendance.domain.CampusRule.CLOSE_TIME;
+import static attendance.domain.CampusRule.GENERAL_ABSENCE_TIME;
+import static attendance.domain.CampusRule.GENERAL_LATE_TIME;
+import static attendance.domain.CampusRule.MONDAY_ABSENCE_TIME;
+import static attendance.domain.CampusRule.MONDAY_LATE_TIME;
+import static attendance.domain.CampusRule.OPEN_TIME;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class AttendancePolicy {
-    private static final LocalTime OPEN_TIME = LocalTime.of(8, 0);
-    private static final LocalTime CLOSE_TIME = LocalTime.of(23, 0);
-    private static final LocalTime MONDAY_LATE_TIME = LocalTime.of(13, 6);
-    private static final LocalTime MONDAY_ABSENCE_TIME = LocalTime.of(13, 31);
-    private static final LocalTime GENERAL_LATE_TIME = LocalTime.of(10, 6);
-    private static final LocalTime GENERAL_ABSENCE_TIME = LocalTime.of(10, 31);
     private static final LocalDate CHRISTMAS = LocalDate.of(2024, 12, 25);
 
     private AttendancePolicy() {}
 
     public static AttendanceType checkAttendanceType(LocalDate localDate, LocalTime localTime) {
-        if (localTime.isBefore(OPEN_TIME) || localTime.isAfter(CLOSE_TIME)) {
+        if (localTime.isBefore(OPEN_TIME.getTime()) || localTime.isAfter(CLOSE_TIME.getTime())) {
             throw new IllegalArgumentException("캠퍼스 운영 시간이 아닙니다.");
         }
         DayOfWeek dayOfWeek = localDate.getDayOfWeek();
@@ -40,11 +40,11 @@ public class AttendancePolicy {
     }
 
     private static AttendanceType checkMondayAttendanceType(LocalTime localTime) {
-        if (localTime.isBefore(MONDAY_LATE_TIME)) {
+        if (localTime.isBefore(MONDAY_LATE_TIME.getTime())) {
             return ATTENDANCE;
         }
 
-        if (localTime.isBefore(MONDAY_ABSENCE_TIME)) {
+        if (localTime.isBefore(MONDAY_ABSENCE_TIME.getTime())) {
             return LATE;
         }
         return ABSENCE;
@@ -54,11 +54,11 @@ public class AttendancePolicy {
      * 월요일이 아닌 목 ~ 금의 출석 결과를 반환
      */
     private static AttendanceType checkGeneralAttendanceType(LocalTime localTime) {
-        if (localTime.isBefore(GENERAL_LATE_TIME)) {
+        if (localTime.isBefore(GENERAL_LATE_TIME.getTime())) {
             return ATTENDANCE;
         }
 
-        if (localTime.isBefore(GENERAL_ABSENCE_TIME)) {
+        if (localTime.isBefore(GENERAL_ABSENCE_TIME.getTime())) {
             return LATE;
         }
         return ABSENCE;
