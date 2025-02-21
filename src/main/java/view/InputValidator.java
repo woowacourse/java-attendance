21bd9819ead2d099a3d7c105e1f727bb9b86536a
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-21 15:50:57 +0900

    test(view): 시간 형식 예외 처리 테스트 작성

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -3,7 +3,7 @@ package view;
 import domain.AllCrew;
 
 public class InputValidator {
-    public static void validateMenuInput(String menu) {
+    public static void validateMenu(String menu) {
         if(!menu.matches("[1234Qq]")) {
             throw new IllegalArgumentException("존재하는 메뉴 번호를 입력해주세요.");
         }
@@ -13,5 +13,9 @@ public class InputValidator {
         if(!allCrew.containsCrewName(name)) {
             throw new IllegalArgumentException("존재하지 않는 닉네임입니다.");
         }
+    }
+
+    public static void validateTimeFormat(String time) {
+        //
     }
 }
--- src/test/java/view/InputValidatorTest.java
+++ src/test/java/view/InputValidatorTest.java
@@ -14,15 +14,15 @@ public class InputValidatorTest {
     @DisplayName("기능 선택을 위해 [1, 2, 3, 4, Q(q)]의 문자를 입력할 시 정상 동작한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1", "2", "3", "4", "Q", "q"})
-    void test1(String menuInput) {
-        assertDoesNotThrow(() -> InputValidator.validateMenuInput(menuInput));
+    void test1(String menu) {
+        assertDoesNotThrow(() -> InputValidator.validateMenu(menu));
     }
 
     @DisplayName("기능 선택을 위해 [1, 2, 3, 4, Q(q)]의 문자를 입력할 시 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"5", "a", "2200000000"})
-    void test2(String menuInput) {
-        assertThatThrownBy(() -> InputValidator.validateMenuInput(menuInput))
+    @ValueSource(strings = {"5", "a", "2200000000", "", " "})
+    void test2(String menu) {
+        assertThatThrownBy(() -> InputValidator.validateMenu(menu))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -38,10 +38,25 @@ public class InputValidatorTest {
     }
 
     @DisplayName("없는 닉네임을 입력할 시 예외가 발생한다.")
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"없는이름", "", " "})
     void test4() {
         AllCrew allCrew = new AllCrew();
 
         assertThatThrownBy(() -> InputValidator.validateName("없는 이름", allCrew));
+    }
+
+    @DisplayName("등교 시간을 hh:mm의 포맷에 맞게 입력한 경우 정상 동작한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"09:59", "13:00"})
+    void test5(String time) {
+        assertDoesNotThrow(() -> InputValidator.validateTimeFormat(time));
+    }
+
+    @DisplayName("등교 시간을 hh:mm의 포맷에 맞지 않게 입력한 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"9:59", "", " "})
+    void test6(String time) {
+        assertThatThrownBy(() -> InputValidator.validateTimeFormat(time));
     }
 }
