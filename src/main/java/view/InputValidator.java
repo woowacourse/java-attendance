7137c519571395f3663562d4ed3b448abe9b47c9
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-21 15:33:36 +0900

    feat(view): 메뉴 입력 예외 처리 기능 작성

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -2,6 +2,8 @@ package view;
 
 public class InputValidator {
     public static void validateMenuInput(String menuInput) {
-        //
+        if(!menuInput.matches("[1234Qq]")) {
+            throw new IllegalArgumentException("존재하는 메뉴 번호를 입력해주세요.");
+        }
     }
 }
--- src/test/java/view/InputValidatorTest.java
+++ src/test/java/view/InputValidatorTest.java
@@ -3,7 +3,6 @@ package view;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
