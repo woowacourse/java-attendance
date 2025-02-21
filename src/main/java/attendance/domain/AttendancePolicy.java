0b2f4a411d1fd36e1f200f48d6f11ba695f5b358
kjyyjk <rlawnsdud920@gmail.com>
2025-02-21 15:39:38 +0900

    refactor: 출석/지각/결석 기준 시간 상수화
    - 기존에는 AttendancePolicy 내부에서 출석/지각/결석을 판별하기 위해 메서드 내에서
    LocalTime.of를 사용하고 있었다. 하지만 단순히 코드를 보았을 때 이게 어떤 시간인지
    알기가 쉽지가 않다. 따라서 static final 상수로 추출하여 적절한 네이밍을 해주고
    가독성을 증진시킨다.

--- src/main/java/attendance/domain/AttendancePolicy.java
+++ src/main/java/attendance/domain/AttendancePolicy.java
@@ -11,6 +11,11 @@ import java.time.LocalTime;
 public class AttendancePolicy {
     private static final LocalTime OPEN_TIME = LocalTime.of(8, 0);
     private static final LocalTime CLOSE_TIME = LocalTime.of(23, 0);
+    private static final LocalTime MONDAY_LATE_TIME = LocalTime.of(13, 6);
+    private static final LocalTime MONDAY_ABSENCE_TIME = LocalTime.of(13, 31);
+    private static final LocalTime GENERAL_LATE_TIME = LocalTime.of(10, 6);
+    private static final LocalTime GENERAL_ABSENCE_TIME = LocalTime.of(10, 31);
+    private static final LocalDate CHRISTMAS = LocalDate.of(2024, 12, 25);
 
     private AttendancePolicy() {}
 
@@ -28,32 +33,32 @@ public class AttendancePolicy {
     public static void checkHoliday(LocalDate localDate) {
         DayOfWeek dayOfWeek = localDate.getDayOfWeek();
         if (dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY &&
-                !localDate.equals(LocalDate.of(2024, 12, 25))) {
+                !localDate.equals(CHRISTMAS)) {
             return;
         }
         throw new IllegalArgumentException("등교일이 아닙니다");
     }
 
     private static AttendanceType checkMondayAttendanceType(LocalTime localTime) {
-        if (localTime.isBefore(LocalTime.of(13, 6))) {
+        if (localTime.isBefore(MONDAY_LATE_TIME)) {
             return ATTENDANCE;
         }
 
-        if (localTime.isBefore(LocalTime.of(13, 31))) {
+        if (localTime.isBefore(MONDAY_ABSENCE_TIME)) {
             return LATE;
         }
         return ABSENCE;
     }
 
     /**
-     * 월요일이 아닌 목 ~ 금의 출석 타입을 반환
+     * 월요일이 아닌 목 ~ 금의 출석 결과를 반환
      */
     private static AttendanceType checkGeneralAttendanceType(LocalTime localTime) {
-        if (localTime.isBefore(LocalTime.of(10, 6))) {
+        if (localTime.isBefore(GENERAL_LATE_TIME)) {
             return ATTENDANCE;
         }
 
-        if (localTime.isBefore(LocalTime.of(10, 31))) {
+        if (localTime.isBefore(GENERAL_ABSENCE_TIME)) {
             return LATE;
         }
         return ABSENCE;
