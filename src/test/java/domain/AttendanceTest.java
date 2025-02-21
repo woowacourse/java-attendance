488c78a05acaf595ce608ba43c9326a2b8952a5f
yeonnhuu <yeonnhuu@gmail.com>
2025-02-21 14:59:16 +0900

    :sparkles: feat(AttendanceTest): Attendance 테스트 추가

--- src/test/java/domain/AttendanceTest.java
+++ src/test/java/domain/AttendanceTest.java
@@ -30,11 +30,28 @@ public class AttendanceTest {
     }
 
     @Test
+    void 크루의_존재_예외() {
+        String crewName = "행성";
+
+        assertThatThrownBy(() -> attendance.getAttendanceTimes(crewName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void 이미_출석한_경우_예외() {
         String crewName = "이든";
         LocalDateTime attendanceTime = LocalDateTime.of(2024, 12, 10, 9, 59, 0);
 
         assertThatThrownBy(() -> attendance.attend(crewName, attendanceTime))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 수정할_출석이_없는_예외() {
+        String crewName = "리버";
+        LocalDateTime attendanceTime = LocalDateTime.of(2024, 12, 16, 9, 59, 0);
+
+        assertThatThrownBy(() -> attendance.edit(crewName, attendanceTime.getDayOfMonth(), attendanceTime.toLocalTime()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -58,6 +75,37 @@ public class AttendanceTest {
     }
 
     @Test
+    void 출석_날짜_예외() {
+        String crewName = "리버";
+        LocalDateTime attendanceTime = LocalDateTime.of(2024, 12, 16, 9, 59, 0);
+
+        assertThatThrownBy(() -> attendance.findAttendanceTime(crewName, attendanceTime.toLocalDate()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 닉네임_존재_예외() {
+        String crewName = "행성";
+
+        assertThatThrownBy(() -> attendance.validateNickName(crewName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 지각_3회_포함_결석_계산_기능() {
+        String crewName = "빙티";
+
+        assertThat(attendance.getAbsentCount(crewName)).isEqualTo(5);
+    }
+
+    @Test
+    void 결석으로_계산되지_않는_지각_계산_기능() {
+        String crewName = "빙티";
+
+        assertThat(attendance.getLateCount(crewName)).isEqualTo(1);
+    }
+
+    @Test
     void 크루의_출석_기록_통계_확인_기능() {
         Map<AttendanceStatus, Integer> attendanceStatuses = attendance.getCrewAttendanceStatus("빙티");
 
@@ -69,6 +117,8 @@ public class AttendanceTest {
     @Test
     void 제적_위험자_확인_기능() {
         List<String> expelledCrew = attendance.checkExpelledCrew();
+
         assertTrue(expelledCrew.contains("빙티"));
     }
+
 }
