824290671ef98acef00656c424ed4d2fdd4505d4
jhan0121 <jhan0121@gmail.com>
2025-02-22 22:42:30 +0900

    test: AttendResults 테스트 코드 수정

--- src/test/java/AttendResultsTest.java
+++ src/test/java/AttendResultsTest.java
@@ -2,37 +2,38 @@ import domain.Attend;
 import domain.AttendCount;
 import domain.AttendanceBook;
 import domain.AttendanceResults;
+import domain.Current;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import util.DateUtil;
 
 public class AttendResultsTest {
 
     @Test
     @DisplayName("출석, 지각, 결석 횟수를 파악하는 기능")
-    void test() throws Exception {
+    void countAttendResult() {
         //given
         AttendanceBook attendanceBook = new AttendanceBook();
         String name = "플린트";
-        int day = 5;
         List<Attend> attends = List.of(
-                Attend.of("2", "10:00"),
-                Attend.of("3", "10:06"),
-                Attend.of("4", "10:31")
+                Attend.of(LocalDate.of(2024, 12, 2), LocalTime.of(10, 0)),
+                Attend.of(LocalDate.of(2024, 12, 3), LocalTime.of(10, 6)),
+                Attend.of(LocalDate.of(2024, 12, 4), LocalTime.of(10, 31))
         );
         attendanceBook.registerName(name);
         for (Attend attend : attends) {
             attendanceBook.attend(name, attend);
         }
-        AttendanceResults attendanceResults = attendanceBook.checkAttendance(name, DateUtil.getAttendUntilDay(day));
+        AttendanceResults attendanceResults = attendanceBook.checkAttendance(name, Current.TODAY.getAttendUntilDay());
 
         //when
         AttendCount attendCount = attendanceResults.countAttendStatus();
 
         //then
-        AttendCount expected = new AttendCount(1, 1, 2);
+        AttendCount expected = new AttendCount(1, 1, 7);
         Assertions.assertThat(attendCount).isEqualTo(expected);
     }
 }
