be79b0b64b8a05f564efc96c82c33be8f80656c4
ryan-dia <cjfdnjs158@naver.com>
2025-02-22 17:03:22 +0900

    refactor: readLocalTime 메서드 단순화
    기존에 시간을 파싱하는 책임을 AttendanceTime으로 이관함

--- src/main/java/controller/command/CheckAttendanceCommand.java
+++ src/main/java/controller/command/CheckAttendanceCommand.java
@@ -3,14 +3,11 @@ package controller.command;
 import controller.AttendanceCommand;
 import domain.Attendance;
 import domain.AttendanceDateTime;
+import domain.AttendanceTime;
 import domain.Crew;
 import domain.Crews;
 import domain.Nickname;
 import error.CustomIllegalArgumentException;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import util.Constants;
 import view.InputView;
 import view.OutputView;
 
@@ -18,13 +15,9 @@ public class CheckAttendanceCommand implements AttendanceCommand {
 
     @Override
     public void execute(final Crews crews) {
-        final LocalDateTime fixDateTime = LocalDateTime.of(Constants.FIXED_YEAR, Constants.FIXED_MONTH,
-                Constants.FIXED_DAY, 0, 0,
-                0, 0);
         final Nickname nickname = readNickname();
         final Crew crew = crews.findByNickname(nickname);
-        final AttendanceDateTime attendanceDateTime = AttendanceDateTime.of(
-                LocalDateTime.of(fixDateTime.toLocalDate(), readLocalTime()));
+        final AttendanceDateTime attendanceDateTime = AttendanceDateTime.generateTodayAttendance(readLocalTime());
 
         if (crew.isAttended(attendanceDateTime.getLocalDateTime())) {
             throw new CustomIllegalArgumentException("이미 출석했습니다. 다음에는 수정기능을 이용해주세요.");
@@ -40,10 +33,8 @@ public class CheckAttendanceCommand implements AttendanceCommand {
         return new Nickname(inputNickName);
     }
 
-    private LocalTime readLocalTime() {
-        final String inputTime = InputView.readDateTime();
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(Constants.TIME_FORMAT);
-        return LocalTime.parse(inputTime, formatter);
+    private AttendanceTime readLocalTime() {
+        return AttendanceTime.of(InputView.readDateTime());
     }
 
     private Attendance attend(Crew crew, AttendanceDateTime attendedDateTime) {
