22dbd814b27f106dd3cb9062c90ce6444540dd4d
jhan0121 <jhan0121@gmail.com>
2025-02-22 23:02:45 +0900

    refactor: 컨트롤러 인덴트 및 메서드 길이 요구사항 반영

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -28,22 +28,7 @@ public class AttendanceController {
         String command = "";
         while (!command.equals("Q")) {
             command = inputView.inputCommand(Current.TODAY.getLocalDate());
-            try {
-                if (command.equals("1")) {
-                    registerAttend(attendanceBook);
-                }
-                if (command.equals("2")) {
-                    editAttend(attendanceBook);
-                }
-                if (command.equals("3")) {
-                    searchAttendance(attendanceBook);
-                }
-                if (command.equals("4")) {
-                    searchWarningCrews(attendanceBook);
-                }
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+            runCommand(command, attendanceBook);
         }
     }
 
@@ -52,35 +37,62 @@ public class AttendanceController {
         return attendReader.loadAttendanceBook();
     }
 
-    private void searchWarningCrews(AttendanceBook attendanceBook) {
-        List<WarningCrew> warningCrews = attendanceBook.checkWarningCrews(Current.TODAY.getAttendUntilDay());
-        outputView.printWarningCrews(warningCrews);
+    private void runCommand(String command, AttendanceBook attendanceBook) {
+        try {
+            registerAttend(command, attendanceBook);
+            editAttend(command, attendanceBook);
+            searchAttendance(command, attendanceBook);
+            searchWarningCrews(command, attendanceBook);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private void searchAttendance(AttendanceBook attendanceBook) {
-        String nickName = inputView.inputNickName();
-        AttendanceResults attendResult = attendanceBook.checkAttendance(nickName, Current.TODAY.getAttendUntilDay());
-        outputView.printAttendanceResult(nickName, attendResult);
-    }
-
-    private void editAttend(AttendanceBook attendanceBook) {
-        String nickName = inputView.inputEditNickName();
-        LocalDate date = inputView.inputDate();
-        LocalTime time = inputView.inputEditTime();
-        Attend after = Attend.of(date, time);
-        Attend before = attendanceBook.findByNameAndDay(nickName, date.getDayOfMonth());
-        attendanceBook.edit(nickName, after);
-        AttendStatus beforeStatus = attendanceBook.checkAttendance(before);
-        AttendStatus afterStatus = attendanceBook.checkAttendance(after);
-        outputView.printEditResult(before, after, beforeStatus, afterStatus);
-    }
-
-    private void registerAttend(AttendanceBook attendanceBook) {
+    private void registerAttend(String command, AttendanceBook attendanceBook) {
+        if (!command.equals("1")) {
+            return;
+        }
         String nickName = inputView.inputNickName();
         LocalTime time = inputView.inputTime();
         Attend attend = Attend.fromTime(time);
         attendanceBook.attend(nickName, attend);
         AttendStatus attendStatus = attendanceBook.checkAttendance(attend);
         outputView.printAttendResult(attend, attendStatus);
+    }
+
+    private void editAttend(String command, AttendanceBook attendanceBook) {
+        if (!command.equals("2")) {
+            return;
+        }
+        String nickName = inputView.inputEditNickName();
+        LocalDate date = inputView.inputDate();
+        LocalTime time = inputView.inputEditTime();
+        Attend after = Attend.of(date, time);
+        Attend before = attendanceBook.findByNameAndDay(nickName, date.getDayOfMonth());
+        attendanceBook.edit(nickName, after);
+        printChangeStatus(attendanceBook, before, after);
+    }
+
+    private void printChangeStatus(AttendanceBook attendanceBook, Attend before, Attend after) {
+        AttendStatus beforeStatus = attendanceBook.checkAttendance(before);
+        AttendStatus afterStatus = attendanceBook.checkAttendance(after);
+        outputView.printEditResult(before, after, beforeStatus, afterStatus);
+    }
+
+    private void searchAttendance(String command, AttendanceBook attendanceBook) {
+        if (!command.equals("3")) {
+            return;
+        }
+        String nickName = inputView.inputNickName();
+        AttendanceResults attendResult = attendanceBook.checkAttendance(nickName, Current.TODAY.getAttendUntilDay());
+        outputView.printAttendanceResult(nickName, attendResult);
+    }
+
+    private void searchWarningCrews(String command, AttendanceBook attendanceBook) {
+        if (!command.equals("4")) {
+            return;
+        }
+        List<WarningCrew> warningCrews = attendanceBook.checkWarningCrews(Current.TODAY.getAttendUntilDay());
+        outputView.printWarningCrews(warningCrews);
     }
 }
