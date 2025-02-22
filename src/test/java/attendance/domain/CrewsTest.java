0f2ad6c667ab785162230c6ab6de7273f2e0f9ad
MinJun Choi <mj043000@naver.com>
2025-02-22 12:17:29 +0900

    test: 출석 기록이 없는 크루 조회 경우 테스트 추가

--- src/test/java/attendance/domain/CrewsTest.java
+++ src/test/java/attendance/domain/CrewsTest.java
@@ -1,6 +1,7 @@
 package attendance.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import attendance.util.FileReader;
 import java.util.List;
@@ -24,5 +25,13 @@ class CrewsTest {
     @Test
     void findCrewByName() {
         assertThat(crews.findCrew("빙티")).isEqualTo(new Crew("빙티"));
+    }
+
+    @DisplayName("출석 기록이 없는 크루를 조회하는 경우 에러를 발생힌다.")
+    @Test
+    void findWrongCrewByName() {
+        assertThatThrownBy(() -> crews.findCrew("엠제이"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력하신 이름의 크루가 존재하지 않습니다.");
     }
 }
