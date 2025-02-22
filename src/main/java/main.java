9b0364132fab501660f75b91951a05364b181195
(minjae8563) <(minjae8563@naver.com)>
2025-02-23 00:02:40 +0900

    feat : 프로그램이 실행되는 main 클래스 구현

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,6 +3,6 @@ import controller.Controller;
 public class main {
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.attendanceStart();
+        controller.start();
     }
 }
