d1e0c5d2f29cbc365c8100bb6467063cb116473c
(minjae8563) <(minjae8563@naver.com)>
2025-02-22 23:43:44 +0900

    refactor : 파일을 읽고, split 하여 StudentRecordRepository 를 생성하는 역할을 FileInput 에서 처리

--- src/main/java/util/FileInput.java
+++ src/main/java/util/FileInput.java
@@ -3,28 +3,54 @@ package util;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import model.StudentRecordRepository;
 
 public class FileInput {
-
-    private final String filePath = "src/main/resources/attendances.csv";
-    private final BufferedReader fileBr;
+    private static final String filePath = "src/main/resources/attendances.csv";
+    private static BufferedReader fileBr = null;
 
     public FileInput() throws IOException {
         fileBr = new BufferedReader(new FileReader(filePath));
     }
 
-    public ArrayList<String> readAttendanceFile() throws IOException{
-        ArrayList<String> attendenceFile = new ArrayList<>();
+    public static ArrayList<String> readAttendanceFile() throws IOException{
+        ArrayList<String> attendanceFile = new ArrayList<>();
         fileBr.readLine();
         while(true) {
             String information = fileBr.readLine();
             if (information == null) {
                 break;
             }
-            attendenceFile.add(information);
+            attendanceFile.add(information);
         }
-        return attendenceFile;
+        return attendanceFile;
+    }
+
+    public static StudentRecordRepository readFileAndCreateStudentRepository() throws IOException {
+        FileInput fileInput = new FileInput();
+        StudentRecordRepository studentRecordRepository = new StudentRecordRepository();
+        for (String information : readAttendanceFile()) {
+            String[] nameAndTimeInformation = information.split(",");
+            String name = nameAndTimeInformation[0];
+            String timeInformation = nameAndTimeInformation[1];
+            String localDateTimeFormatter = "yyyy-MM-dd HH:mm";
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(localDateTimeFormatter);
+            LocalDateTime localDateTime = LocalDateTime.parse(timeInformation, dateTimeFormatter);
+            studentRecordRepository.addRecord(name,localDateTime);
+        }
+        return studentRecordRepository;
+    }
+
+    public static StudentRecordRepository createStudentRepository(){
+        try{
+            return readFileAndCreateStudentRepository();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+            return createStudentRepository();
+        }
     }
 
 }
