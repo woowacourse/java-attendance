1c15da463699a996d98731dd55ef13ef55d1c051
RaZel713 <razel7130@gmail.com>
2025-02-22 15:02:37 +0900

    refactor: Dto 공백 수정

--- src/main/java/dto/AttendanceRecordResponse.java
+++ src/main/java/dto/AttendanceRecordResponse.java
@@ -4,5 +4,9 @@ import domain.AttendanceStatus;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record AttendanceRecordResponse(LocalDate date, LocalTime time, AttendanceStatus attendanceStatus) {
+public record AttendanceRecordResponse(
+        LocalDate date,
+        LocalTime time,
+        AttendanceStatus attendanceStatus
+) {
 }
