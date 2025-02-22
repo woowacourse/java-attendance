de919e0865bd2b3aaf4574a5ace9366caec76fb8
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-22 17:15:01 +0900

    feat(MenuCommand): 상수 설정 및 널 값 입력에 대한 처리

--- src/main/java/attendance/domain/MenuCommand.java
+++ src/main/java/attendance/domain/MenuCommand.java
@@ -9,7 +9,8 @@ public enum MenuCommand {
     EXPEL("4"),
     QUIT("Q");
 
-    private static final String MENU_ERROR = "[ERROR] 지원하지 않는 기능입니다. 시스템을 종료합니다.";
+    private static final String MENU_COMMAND_ERROR_MESSAGE = "[ERROR] 지원하지 않는 기능 값입니다.";
+
     private final String command;
 
     MenuCommand(String command) {
@@ -17,10 +18,13 @@ public enum MenuCommand {
     }
 
     public static MenuCommand toCommand(final String input) {
+        if (input == null) {
+            throw new IllegalArgumentException(MENU_COMMAND_ERROR_MESSAGE);
+        }
         return Arrays.stream(values())
                 .filter(command -> command.getCommand().equals(input))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(MENU_ERROR));
+                .orElseThrow(() -> new IllegalArgumentException(MENU_COMMAND_ERROR_MESSAGE));
     }
 
     private String getCommand() {
