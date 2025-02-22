3c4c572e7da08cb815f48dff5f8898ce2f8951e0
threepebbles <threepebbles@naver.com>
2025-02-22 16:44:33 +0900

    feat(AttendanceTime): null 방지 조건 추가

--- src/main/java/domain/AttendanceTime.java
+++ src/main/java/domain/AttendanceTime.java
@@ -6,5 +6,9 @@ public record AttendanceTime(
         LocalTime time,
         AttendanceStatus status
 ) {
-
+    public AttendanceTime {
+        if (status == null) {
+            throw new IllegalArgumentException("출석 상태는 null일 수 없습니다.");
+        }
+    }
 }
