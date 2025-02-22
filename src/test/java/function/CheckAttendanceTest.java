a7d91c88ca914d71ba3b931cf86743664423fad9
RaZel713 <razel7130@gmail.com>
2025-02-22 20:11:26 +0900

    test: 출석 확인 기능 테스트 수정
    - 중복되는 코드 분리

--- src/test/java/function/CheckAttendanceTest.java
+++ src/test/java/function/CheckAttendanceTest.java
@@ -7,94 +7,76 @@ import static constants.TestTimeMaker.NON_OPERATING_TIME;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import domain.AttendanceBook;
-import domain.Crew;
 import java.time.LocalDate;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import view.ErrorCode;
 
 public class CheckAttendanceTest {
+    private AttendanceBook attendanceBook;
 
-    @DisplayName("출석하기 위해서는 닉네임과 등교 시간을 입력 받아야 한다.")
-    @Test
-    void 출석하기_위해서는_닉네임과_등교_시간을_입력_받아야_한다() {
-        AttendanceBook attendanceBook = new AttendanceBook();
+    @BeforeEach
+    void setup() {
+        attendanceBook = new AttendanceBook();
+        attendanceBook.initialize("우유", Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND));
+        attendanceBook.initialize("우유", Map.of(LocalDate.of(2024, 12, 3), EXCEPT_MONDAY_ATTEND));
+        attendanceBook.initialize("우유", Map.of(LocalDate.of(2024, 12, 4), EXCEPT_MONDAY_ATTEND));
 
-        Crew crew1 = Crew.createByName("쿠키");
-        crew1.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND));
-        attendanceBook.addNewCrew(crew1);
-
-        Crew crew2 = Crew.createByName("우유");
-        crew2.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND));
-        attendanceBook.addNewCrew(crew2);
-
-        attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 3), EXCEPT_MONDAY_ATTEND));
+        attendanceBook.initialize("쿠키", Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND));
+        attendanceBook.initialize("쿠키", Map.of(LocalDate.of(2024, 12, 3), EXCEPT_MONDAY_ATTEND));
+        attendanceBook.initialize("쿠키", Map.of(LocalDate.of(2024, 12, 4), EXCEPT_MONDAY_ATTEND));
     }
 
     @Test
+    @DisplayName("출석하기_위해서는_닉네임과_등교_시간을_입력_받아야_한다")
+    void 출석하기_위해서는_닉네임과_등교_시간을_입력_받아야_한다() {
+        attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 5), EXCEPT_MONDAY_ATTEND));
+    }
+
+    @Test
+    @DisplayName("등록되지_않는_닉네임의_경우_예외를_출력한다")
+    void 등록되지_않는_닉네임의_경우_예외를_출력한다() {
+        assertThatThrownBy(
+                () -> attendanceBook.checkAttendance("없음", Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.NICKNAME_NOT_FOUND.getFormat());
+
+    }
+
+    @Test
+    @DisplayName("이미_출석한_경우_다시_출석할_수_없으며_수정_기능을_이용하도록_안내해야_한다")
     void 이미_출석한_경우_다시_출석할_수_없으며_수정_기능을_이용하도록_안내해야_한다() {
-        AttendanceBook attendanceBook = new AttendanceBook();
-
-        Crew crew1 = Crew.createByName("쿠키");
-        crew1.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND));
-        attendanceBook.addNewCrew(crew1);
-
-        Crew crew2 = Crew.createByName("우유");
-        crew2.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND));
-        attendanceBook.addNewCrew(crew2);
-
         assertThatThrownBy(
                 () -> attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 2), MONDAY_LATE)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.CHECK_ATTENDANCE_ALREADY_EXISTS.getFormat());
     }
 
     @Test
-    @DisplayName("주말 및 공휴일에는 출석을 받지 않는다.")
+    @DisplayName("주말_및_공휴일에는_출석을_받지_않는다")
     void 주말_및_공휴일에는_출석을_받지_않는다() {
-        AttendanceBook attendanceBook = new AttendanceBook();
-        Crew crew1 = Crew.createByName("쿠키");
-        crew1.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND));
-        attendanceBook.addNewCrew(crew1);
-
         assertThatThrownBy(
                 () -> attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 25), EXCEPT_MONDAY_ATTEND)))
                 .isInstanceOf(IllegalArgumentException.class) // 공휴일
-                .hasMessage("[ERROR] 12월 25일 공휴일은 등교일이 아닙니다.");
+                .hasMessage(ErrorCode.HOLIDAY_NOT_WORKING_DAY_FORMAT.format(25));
         assertThatThrownBy(
                 () -> attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 1), EXCEPT_MONDAY_ATTEND)))
                 .isInstanceOf(IllegalArgumentException.class) // 일요일
-                .hasMessage("[ERROR] 12월 01일 일요일은 등교일이 아닙니다.");
+                .hasMessage(ErrorCode.SUNDAY_NOT_WORKING_DAY_FORMAT.format(1));
         assertThatThrownBy(
                 () -> attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 7), EXCEPT_MONDAY_ATTEND)))
                 .isInstanceOf(IllegalArgumentException.class) // 토요일
-                .hasMessage("[ERROR] 12월 07일 토요일은 등교일이 아닙니다.");
+                .hasMessage(ErrorCode.SATURDAY_NOT_WORKING_DAY_FORMAT.format(7));
     }
 
     @Test
-    @DisplayName("등록되지 않는 닉네임의 경우 예외를 출력한다.")
-    void 등록되지_않는_닉네임의_경우_예외를_출력한다() {
-        AttendanceBook attendanceBook = new AttendanceBook();
-        Crew crew1 = Crew.createByName("쿠키");
-        crew1.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND));
-        attendanceBook.addNewCrew(crew1);
-
-        assertThatThrownBy(
-                () -> attendanceBook.checkAttendance("우유", Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 등록되지 않은 닉네임입니다.");
-
-    }
-
-    @Test
+    @DisplayName("출석_확인시_캠퍼스_운영_시간이_아닌_경우_예외를_출력한다")
     void 출석_확인시_캠퍼스_운영_시간이_아닌_경우_예외를_출력한다() {
-        AttendanceBook attendanceBook = new AttendanceBook();
-        Crew crew1 = Crew.createByName("쿠키");
-        crew1.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 2), EXCEPT_MONDAY_ATTEND));
-        attendanceBook.addNewCrew(crew1);
-
         assertThatThrownBy(
-                () -> attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 3), NON_OPERATING_TIME)))
+                () -> attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 5), NON_OPERATING_TIME)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 캠퍼스 운영 시간은 08:00~23:00 입니다.");
+                .hasMessage(ErrorCode.TIME_NOT_IN_OPERATION_HOUR.getFormat());
     }
 }
