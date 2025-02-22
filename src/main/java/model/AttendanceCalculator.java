aa86ef9ecfb1a31ca120f908859beae43946db8c
(minjae8563) <(minjae8563@naver.com)>
2025-02-23 00:01:35 +0900

    feat : 학생들의 결과를 산출하고, 이에 대한 정보를 Map 에 저장하여 리턴해주는 Enum 클래스 구현

---
+++ src/main/java/model/AttendanceCalculator.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.List;
+
+public enum AttendanceCalculator {
+
+    MONDAY(LocalTime.of(13,5),LocalTime.of(13,30),1),
+    TUESDAY(LocalTime.of(10,5),LocalTime.of(10,30),2),
+    WEDNESDAY(LocalTime.of(10,5), LocalTime.of(10,30),3),
+    THURSDAY(LocalTime.of(10,5), LocalTime.of(10,30),4),
+    FRIDAY(LocalTime.of(10,5), LocalTime.of(10,30),5);
+
+    private final LocalTime lateTime;
+    private final LocalTime absentTime;
+    private final int dayOfWeekValue;
+
+    AttendanceCalculator(LocalTime lateTime, LocalTime absentTime, int dayOfWeekValue) {
+        this.lateTime = lateTime;
+        this.absentTime = absentTime;
+        this.dayOfWeekValue = dayOfWeekValue;
+    }
+
+    public static boolean checkHoliday(LocalDateTime localDateTime) {
+        return (localDateTime.getDayOfWeek().getValue() == 6 || localDateTime.getDayOfWeek().getValue() == 7 || localDateTime.getDayOfMonth() == 25);
+    }
+
+    public static HashMap<String, Integer> recordAttendanceResult(List<LocalDateTime> record) {
+        HashMap<String, Integer> attendanceRecord = new HashMap<>();
+        attendanceRecord.put("결석",0);
+        attendanceRecord.put("출석",0);
+        attendanceRecord.put("지각",0);
+        for (LocalDateTime localDateTime :record) {
+            int day = localDateTime.getDayOfWeek().getValue();
+            attendanceRecord.put(calculateAttendance(day, LocalTime.from(localDateTime)).getState(),
+                    attendanceRecord.get(calculateAttendance(day, LocalTime.from(localDateTime)).getState()) + 1);
+        }
+        return attendanceRecord;
+    }
+
+    public static AttendanceStatus calculateAttendance(int day, LocalTime localTime) {
+        if (localTime.equals(LocalTime.of(0,0))) {
+            return AttendanceStatus.ABSENT;
+        }
+        for (AttendanceCalculator attendanceCalculatorByDay : AttendanceCalculator.values()) {
+            if (attendanceCalculatorByDay.dayOfWeekValue == day) {
+                return getAttendanceStatus(localTime, attendanceCalculatorByDay);
+            }
+        }
+        return null;
+    }
+
+    private static AttendanceStatus getAttendanceStatus(LocalTime localTime,
+                                                        AttendanceCalculator attendanceCalculatorByDay) {
+        if (localTime.isBefore(attendanceCalculatorByDay.lateTime)) {
+            return AttendanceStatus.ATTENDANCE;
+        }
+        if (localTime.isBefore(attendanceCalculatorByDay.absentTime)) {
+            return AttendanceStatus.LATE;
+        }
+        return AttendanceStatus.ABSENT;
+    }
+}
