f0aac45f4c3605235b5ee1ebb23f770778c54c1b
supernovaMK <minkikim2002@gmail.com>
2025-02-22 20:46:52 +0900

    refactor(AttendanceFileReader): try with resource 를 사용하도록 수정

--- src/main/java/domain/AttendanceFileReader.java
+++ src/main/java/domain/AttendanceFileReader.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,14 +11,14 @@ public class AttendanceFileReader {
 
     public static List<String> readFile(String fileName) {
         List<String> studentList = new ArrayList<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             br.readLine();
             while ((line = br.readLine()) != null) {
                 studentList.add(line);
             }
         } catch (IOException e) {
+            throw new RuntimeException("파일을 읽어오는 과정에서 문제가 생겼습니다.");
         }
         return studentList;
     }
