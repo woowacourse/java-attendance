73a4f58d01658a51362399f027bf46bd305c3d82
DongYeop <s01054830455@gmail.com>
2025-02-22 19:39:54 +0900

    refactor: 숫자 검증 로직 중복메서드 제거
    - 동일한 기능을 하는 중복된 메서드를 하나의 메서드로 리팩터링
    - 날짜 매직넘버 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,10 @@ import java.util.regex.Pattern;
 import util.Console;
 
 public class InputView {
+
+    private static final int START_DATE = 1;
+    private static final int END_DATE = 31;
+
     public static String inputFeature(int month, int day, String dayOfWeek) {
         System.out.printf("오늘은 %d월 %02d일 %s입니다. 기능을 선택해 주세요.\n" + "1. 출석 확인\n" + "2. 출석 수정\n" + "3. 크루별 출석 기록 확인\n"
                 + "4. 제적 위험자 확인\n" + "Q. 종료\n", month, day, dayOfWeek);
@@ -63,24 +67,8 @@ public class InputView {
     public static String inputUpdateDate() {
         System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
         String input = Console.readLine();
-        validateDateSize(input);
-        validateInteger(input);
+        validateParseDate(input);
         return input;
-    }
-
-    private static void validateInteger(final String input) {
-        try {
-            Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("0이상의 숫자만 입력해 주세요.");
-        }
-    }
-
-    private static void validateDateSize(final String input) {
-        int date = Integer.parseInt(input);
-        if (date > 31 || date < 1) {
-            throw new IllegalArgumentException("날짜는 1부터 31일까지만 입력할 수 있습니다.");
-        }
     }
 
     public static String inputUpdateTime() {
@@ -88,5 +76,20 @@ public class InputView {
         String input = Console.readLine();
         validateNullOrEmpty(input);
         return input;
+    }
+
+    private static void validateParseDate(final String inputDate) {
+        try {
+            int date = Integer.parseInt(inputDate);
+            validateSize(date);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("날짜는 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private static void validateSize(final int date) {
+        if (date < START_DATE || date > END_DATE) {
+            throw new IllegalArgumentException("날짜는 1부터 31일까지의 숫자만 입력할 수 있습니다.");
+        }
     }
 }
