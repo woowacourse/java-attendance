4de1df87f7b77f22b741d8cff40f0a331f6ed5f6
GiSeok Kang <ime.rokz@gmail.com>
2025-02-22 23:50:29 +0900

    refactor: AttendanceSheets 테스트 .csv 파일 의존 제거

--- src/test/java/domain/AttendanceSheetsTest.java
+++ src/test/java/domain/AttendanceSheetsTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import util.FileReaderUtil;
 
 class AttendanceSheetsTest {
 
@@ -16,13 +17,22 @@ class AttendanceSheetsTest {
 
     @BeforeAll
     static void beforeAll() {
-        AttendanceSheetsFactory attendanceSheetsFactory = new AttendanceSheetsFactory(new FileReaderUtil());
-        attendanceSheets = attendanceSheetsFactory.create();
+        List<AttendanceSheet> testSheets = new ArrayList<>();
+        testSheets.add(new AttendanceSheet("율무", AttendanceDateTime.from(LocalDateTime.of(2024, 12, 2, 13, 9))));
+        testSheets.add(new AttendanceSheet("강산", AttendanceDateTime.from(LocalDateTime.of(2024, 12, 2, 13, 0))));
+        testSheets.add(new AttendanceSheet("링크", AttendanceDateTime.from(LocalDateTime.of(2024, 12, 2, 13, 31))));
+        testSheets.add(new AttendanceSheet("링크", AttendanceDateTime.from(LocalDateTime.of(2024, 12, 3, 10, 0))));
+        testSheets.add(new AttendanceSheet("율무", AttendanceDateTime.from(LocalDateTime.of(2024, 12, 3, 10, 9))));
+        testSheets.add(new AttendanceSheet("강산", AttendanceDateTime.from(LocalDateTime.of(2024, 12, 4, 10, 0))));
+        testSheets.add(new AttendanceSheet("링크", AttendanceDateTime.from(LocalDateTime.of(2024, 12, 4, 9, 59))));
+        testSheets.add(new AttendanceSheet("율무", AttendanceDateTime.from(LocalDateTime.of(2024, 12, 6, 10, 3))));
+
+        attendanceSheets = new AttendanceSheets(testSheets);
     }
 
     @ParameterizedTest
     @DisplayName("닉네임을 통해 출석 기록을 가져올 수 있다.")
-    @ValueSource(strings = {"쿠키", "빙봉", "빙티", "이든", "짱수"})
+    @ValueSource(strings = {"율무", "강산", "링크"})
     void attendance_sheets_find_by_Nickname(String nickname) {
         // when
         List<AttendanceSheet> foundByNickname = attendanceSheets.findAttendanceByNickname(nickname);
@@ -41,7 +51,7 @@ class AttendanceSheetsTest {
 
         // then
         Assertions.assertThat(allNames)
-                .containsExactlyInAnyOrder("쿠키", "빙봉", "빙티", "이든", "짱수");
+                .containsExactlyInAnyOrder("율무", "강산", "링크");
     }
 
     @ParameterizedTest
@@ -57,7 +67,7 @@ class AttendanceSheetsTest {
 
     @ParameterizedTest
     @DisplayName("없는 닉네임 입력 시 예외를 발생한다.")
-    @ValueSource(strings = {"율무", "링크", "강산"})
+    @ValueSource(strings = {"벡터", "제프", "에드"})
     void not_contains_nickname(String nickname) {
         // when
         // then
