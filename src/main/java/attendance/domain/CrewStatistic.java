5ce16bf81eacbec94cdac2665c07df86d067f470
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-22 17:43:30 +0900

    refactor(CrewStatistic): 상수 설정 및 출석 횟수 객체 사용 추가

--- src/main/java/attendance/domain/CrewStatistic.java
+++ src/main/java/attendance/domain/CrewStatistic.java
@@ -8,18 +8,22 @@ import java.util.List;
 import java.util.Locale;
 
 public class CrewStatistic {
+    private static final int WARNING_COUNT_LIMIT = 2;
+    private static final int MEETING_COUNT_LIMIT = 3;
+    private static final int EXPEL_COUNT_LIMIT = 5;
+
+    private static final String MONTH_FORMAT = "%02d";
+
     private final Crew crew;
+    private final AttendanceCount attendanceCount;
     private final List<Attendance> crewAttendances;
     private CrewStatus status;
 
     public CrewStatistic(Crew crew, List<Attendance> crewAttendances) {
         this.crew = crew;
+        this.attendanceCount = new AttendanceCount();
         this.crewAttendances = crewAttendances;
         this.status = CrewStatus.NONE;
-    }
-
-    public void resetCrewStatus() {
-        crew.resetCount();
     }
 
     public void initCrewsStatus() {
@@ -30,83 +34,91 @@ public class CrewStatistic {
 
     private void initCrewStatus(Attendance crewAttendance) {
         if (crewAttendance.getType() == AttendanceType.SAFE) {
-            crew.plusSafeCount();
+            attendanceCount.incrementSafeCount();
         }
         if (crewAttendance.getType() == AttendanceType.LATE) {
-            crew.plusLateCount();
+            attendanceCount.incrementLateCount();
         }
         if (crewAttendance.getType() == AttendanceType.ABSENT) {
-            crew.plusAbsentCount();
+            attendanceCount.incrementAbsentCount();
         }
     }
 
+    public void resetCrewStatus() {
+        attendanceCount.resetAttendanceCount();
+    }
+
     public void calculatePenalty() {
-        int lateCount = crew.getLateCount();
-        int absentCount = crew.getAbsentCount();
+        int absentCount = attendanceCount.calculatePenalty();
+        checkCrewStatus(absentCount);
+    }
 
-        int plusAbsentCount = lateCount / 3;
-        absentCount += plusAbsentCount;
-
-        if (absentCount > 5) {
+    private void checkCrewStatus(int absentCount) {
+        if (absentCount > EXPEL_COUNT_LIMIT) {
             status = CrewStatus.EXPEL;
+            return;
         }
-        if (absentCount >= 3) {
+        if (absentCount >= MEETING_COUNT_LIMIT) {
             status = CrewStatus.MEETING;
+            return;
         }
-        if (absentCount >= 2) {
+        if (absentCount == WARNING_COUNT_LIMIT) {
             status = CrewStatus.WARNING;
         }
     }
 
-    public List<List<String>> getCrewAttendanceHistory() {
-        List<Attendance> sortedAttendances = crewAttendances.stream()
-                .sorted(Comparator.comparing(Attendance::getDate))
-                .toList();
-
+    public List<List<String>> crewAttendanceHistoryInfo() {
+        List<Attendance> sortedAttendances = sortCrewAttendances();
         List<List<String>> crewStatisticInfo = new ArrayList<>();
 
         for (Attendance crewAttendance : sortedAttendances) {
             LocalDate crewLocalDate = crewAttendance.getDate();
-
             crewStatisticInfo.add(
-                    List.of(
-                            String.valueOf(crewLocalDate.getMonthValue()),
-                            String.format("%02d", crewLocalDate.getDayOfMonth()),
-                            crewLocalDate.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN),
-                            crewAttendance.getTimeValue(),
-                            crewAttendance.getType().toString()
-                    )
+                    createCrewAttendanceInfo(crewLocalDate, crewAttendance)
             );
         }
-
         return crewStatisticInfo;
     }
 
-    public List<String> getCrewStatisticStatus() {
+    private List<Attendance> sortCrewAttendances() {
+        return crewAttendances.stream()
+                .sorted(Comparator.comparing(Attendance::getDate))
+                .toList();
+    }
+
+    private List<String> createCrewAttendanceInfo(LocalDate crewLocalDate, Attendance crewAttendance) {
         return List.of(
-                String.valueOf(crew.getSafeCount()),
-                String.valueOf(crew.getLateCount()),
-                String.valueOf(crew.getAbsentCount()),
-                status.toString()
+                String.valueOf(crewLocalDate.getMonthValue()),
+                String.format(MONTH_FORMAT, crewLocalDate.getDayOfMonth()),
+                crewLocalDate.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN),
+                crewAttendance.getTimeValue(),
+                crewAttendance.getType().toString()
         );
     }
 
-    public int getPenaltyCount() {
-        int absentCount = crew.getAbsentCount();
-        int lateCount = crew.getLateCount();
-        return absentCount + lateCount / 3;
-    }
-
-    public String getCrewName() {
-        return crew.getName();
+    public List<String> crewStatisticStatusInfo() {
+        return List.of(
+                String.valueOf(attendanceCount.getSafeCount()),
+                String.valueOf(attendanceCount.getLateCount()),
+                String.valueOf(attendanceCount.getAbsentCount()),
+                status.toString()
+        );
     }
 
     public List<String> crewExpelExpectedInfo() {
         return List.of(
                 crew.getName(),
-                String.valueOf(crew.getAbsentCount()),
-                String.valueOf(crew.getLateCount()),
+                String.valueOf(attendanceCount.getAbsentCount()),
+                String.valueOf(attendanceCount.getLateCount()),
                 status.toString()
         );
+    }
+
+    public int getPenaltyCount() {
+        return attendanceCount.checkPenaltyCount();
+    }
+
+    public String getCrewName() {
+        return crew.getName();
     }
 }
