fe905e39e82ee5d7b93d906ebb2ae0627dce82dc
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-22 17:34:29 +0900

    refactor(Crews): 메서드 내 자료구조 변경 및 에러 메시지 상수 처리

--- src/main/java/attendance/domain/Crews.java
+++ src/main/java/attendance/domain/Crews.java
@@ -3,8 +3,11 @@ package attendance.domain;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Crews {
+    private static final String NO_SUCH_CREW_ERROR_MESSAGE = "[ERROR] 입력하신 이름의 크루가 존재하지 않습니다.";
+
     private final List<Crew> crews;
 
     public Crews() {
@@ -12,14 +15,11 @@ public class Crews {
     }
 
     public void initCrews(final List<List<String>> attendanceRecords) {
-        List<String> crewNames = new ArrayList<>();
+        Set<String> uniqueCrewNames = new HashSet<>();
 
         for (List<String> attendanceRecord : attendanceRecords) {
-            crewNames.add(attendanceRecord.getFirst());
+            uniqueCrewNames.add(attendanceRecord.getFirst());
         }
-
-        HashSet<String> uniqueCrewNames = new HashSet<>(crewNames);
-
         for (String uniqueCrewName : uniqueCrewNames) {
             crews.add(new Crew(uniqueCrewName));
         }
@@ -29,7 +29,7 @@ public class Crews {
         return crews.stream()
                 .filter(crew -> crew.isSameCrewName(crewName))
                 .findFirst()
-                .orElseThrow(() -> new IllegalStateException("[ERROR] 입력하신 이름의 크루가 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalStateException(NO_SUCH_CREW_ERROR_MESSAGE));
     }
 
     public List<Crew> getCrews() {
