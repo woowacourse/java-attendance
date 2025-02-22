0101b7de3394db3a4d01bd2a4617120011af851c
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-22 17:16:39 +0900

    test(AttendanceTest): 출석 확인과 수정 및 문자열 변환 기능 검사

--- src/test/java/attendance/domain/AttendanceTest.java
+++ src/test/java/attendance/domain/AttendanceTest.java
@@ -3,6 +3,7 @@ package attendance.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Stream;
@@ -13,13 +14,34 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class AttendanceTest {
-    @DisplayName("기능: 이름이 동일한 Crew 객체로 해당 Attendance 확인")
+    @DisplayName("기능: 출석 시간이 동일한 날짜 객체로 해당 Attendance 비교")
     @MethodSource("provideAttendanceInstances")
     @ParameterizedTest
-    void findCrewInfoFromAttendance(List<Attendance> attendances) {
+    void compareSameTimeFromAttendance(List<Attendance> attendances) {
+        Attendance attendance = attendances.getFirst();
+
+        assertThat(attendance.isSameTime(LocalDateTime.of(2025, 2, 19, 9, 50, 0))).isTrue();
+        assertThat(attendance.isSameTime(LocalDateTime.of(2025, 2, 19, 9, 51, 0))).isFalse();
+    }
+
+    @DisplayName("기능: 이름이 동일한 Crew 객체로 해당 Attendance 비교")
+    @MethodSource("provideAttendanceInstances")
+    @ParameterizedTest
+    void compareSameCrewFromAttendance(List<Attendance> attendances) {
         for (Attendance attendance : attendances) {
             assertThatCode(() -> attendance.isSameCrew(new Crew("리원"))).doesNotThrowAnyException();
+            assertThat(attendance.isSameCrew(new Crew("리원"))).isTrue();
         }
+    }
+
+    @DisplayName("기능: 이름이 동일한 Crew 및 날짜 객체로 해당 Attendance 비교")
+    @MethodSource("provideAttendanceInstances")
+    @ParameterizedTest
+    void compareSameCrewDateFromAttendance(List<Attendance> attendances) {
+        Attendance attendance = attendances.getFirst();
+
+        assertThat(attendance.isSameCrewDate(new Crew("리원"), LocalDate.of(2025, 2, 19))).isTrue();
+        assertThat(attendance.isSameCrewDate(new Crew("리원"), LocalDate.of(2025, 2, 20))).isFalse();
     }
 
     private static Stream<Arguments> provideAttendanceInstances() {
@@ -41,7 +63,7 @@ public class AttendanceTest {
         );
     }
 
-    @DisplayName("기능: 출석 시간이 바뀌면 그에 맞게 AttendanceType 갱신")
+    @DisplayName("기능: 변경된 출석 시간으로 헤당 Attendance의 AttendanceType 수정")
     @Test
     void changeAttendanceTypeForModifiedTime() {
         Attendance attendance = new Attendance(
@@ -55,5 +77,19 @@ public class AttendanceTest {
         );
 
         assertThat(attendance.getType()).isEqualTo(AttendanceType.LATE);
+    }
+
+    @DisplayName("기능: 해당 Attendance의 날짜와 시간 및 출석 유형 정보 문자열로 반환")
+    @Test
+    void returnDateTimeAttendanceTypeFromAttendance() {
+        Attendance attendance = new Attendance(
+                new Crew("엠제이"),
+                LocalDateTime.of(2025, 2, 20, 9, 50, 0),
+                AttendanceType.SAFE
+        );
+
+        List.of("2", "20", "목", "09:50", "출석").forEach(
+                info -> assertThat(attendance.getInfo()).contains(info)
+        );
     }
 }
