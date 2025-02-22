343f33185f97a88101ce68c7f862117713a1f043
(minjae8563) <(minjae8563@naver.com)>
2025-02-22 23:47:40 +0900

    feat : 학생의 출석 기록을 LocalDateTime 형으로 저장한 List 를 가진 Record 클래스 구현

---
+++ src/main/java/model/Record.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Record {
+
+    List<LocalDateTime> timeRecords = new ArrayList<>();
+
+    public Record(LocalDateTime localDateTime) {
+        timeRecords.add(localDateTime);
+    }
+
+    public ArrayList<LocalDateTime> getTimeRecords() {
+        return (ArrayList<LocalDateTime>) timeRecords;
+    }
+
+    public void addTime(LocalDateTime localDateTime) {
+        timeRecords.add(localDateTime);
+        Collections.sort(timeRecords);
+    }
+
+    public LocalDateTime compareDayIsSame(LocalDateTime wantToFindLocalDateTime) {
+        for (LocalDateTime localDateTime : timeRecords) {
+            LocalDateTime dayDate1 = localDateTime.truncatedTo(ChronoUnit.DAYS);
+            LocalDateTime dayDate2 = wantToFindLocalDateTime.truncatedTo(ChronoUnit.DAYS);
+            if (dayDate1.compareTo(dayDate2) == 0) {
+                return localDateTime;
+            }
+        }
+        return null;
+    }
+
+    public void modifyRecord(LocalDateTime wantToModifyLocalDateTime) {
+        timeRecords.remove(compareDayIsSame(wantToModifyLocalDateTime));
+        addTime(wantToModifyLocalDateTime);
+        Collections.sort(timeRecords);
+    }
+
+    public boolean compareDayIsSame(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
+        LocalDateTime dayDate1 = localDateTime1.truncatedTo(ChronoUnit.DAYS);
+        LocalDateTime dayDate2 = localDateTime2.truncatedTo(ChronoUnit.DAYS);
+
+        return (dayDate1.compareTo(dayDate2) == 0);
+    }
+
+    public void updateNoInformationInFile(LocalDateTime todayDate) {
+        LocalDateTime standard = LocalDateTime.of(2024,12,1,0,0);
+        while (!compareDayIsSame(standard,todayDate)) {
+            if (standard.getDayOfWeek().getValue() == 6 || standard.getDayOfWeek().getValue() == 7) {
+                standard = standard.plusDays(1);
+                continue;
+            }
+            if (compareDayIsSame(standard)==null) {
+                timeRecords.add(standard);
+                standard = standard.plusDays(1);
+                continue;
+            }
+            standard = standard.plusDays(1);
+        }
+        Collections.sort(timeRecords);
+    }
+
+}
