e21bbe903cf5055ea0dc30ce6c792c5972db1751
junhaa <2171326@hansung.ac.kr>
2025-02-22 23:23:10 +0900

    refactor: 코드 가독성 개선
    - BufferedReader의 lines()를 활용하여 코드 가독성 개선

--- src/main/java/attendance/view/DataFileReader.java
+++ src/main/java/attendance/view/DataFileReader.java
@@ -3,7 +3,6 @@ package attendance.view;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,11 +12,10 @@ public class DataFileReader {
     public static List<String> read() {
         List<String> datas = new ArrayList<>();
         BufferedReader bufferedReader = createReader();
-        readLine(bufferedReader);
-        String input;
-        while ((input = readLine(bufferedReader)) != null) {
-            datas.add(input);
-        }
+
+        bufferedReader.lines()
+                .skip(1)
+                .forEach(data -> datas.add(data));
         return datas;
     }
 
@@ -26,14 +24,6 @@ public class DataFileReader {
             return new BufferedReader(new FileReader(FILE_PATH));
         } catch (FileNotFoundException e) {
             throw new RuntimeException("파일을 찾을 수 없습니다.", e);
-        }
-    }
-
-    private static String readLine(BufferedReader bufferedReader) {
-        try {
-            return bufferedReader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException("입출력 오류 발생", e);
         }
     }
 }
