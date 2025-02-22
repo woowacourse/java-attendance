5aa3a9e821465996f9d6c9c935dabd4d5dda7f5a
jhan0121 <jhan0121@gmail.com>
2025-02-22 22:31:54 +0900

    refactor: 경고 판정 객체 내 대상 판정 메서드 구현

--- src/main/java/domain/WarningStatus.java
+++ src/main/java/domain/WarningStatus.java
@@ -1,5 +1,30 @@
 package domain;
 
 public enum WarningStatus {
-    CLEAR, WARNING, INTERVIEW, EXPEL
+    CLEAR {
+        @Override
+        public boolean match(long totalAbsenceCount) {
+            return totalAbsenceCount < 2;
+        }
+    },
+    WARNING {
+        @Override
+        public boolean match(long totalAbsenceCount) {
+            return totalAbsenceCount == 2;
+        }
+    },
+    INTERVIEW {
+        @Override
+        public boolean match(long totalAbsenceCount) {
+            return totalAbsenceCount >= 3 && totalAbsenceCount <= 5;
+        }
+    },
+    EXPEL {
+        @Override
+        public boolean match(long totalAbsenceCount) {
+            return totalAbsenceCount > 5;
+        }
+    };
+
+    abstract public boolean match(long totalAbsenceCount);
 }
