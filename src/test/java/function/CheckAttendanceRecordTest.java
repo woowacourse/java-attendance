6e957fa6c3b3d43a34af556874706dc81aaf1b2d
RaZel713 <razel7130@gmail.com>
2025-02-22 19:46:30 +0900

    test: 출석 기록 확인 기능 테스트 수정

--- src/test/java/function/CheckAttendanceRecordTest.java
+++ src/test/java/function/CheckAttendanceRecordTest.java
@@ -2,19 +2,22 @@ package function;
 
 import static constants.TestTimeMaker.EXCEPT_MONDAY_ATTEND;
 import static constants.TestTimeMaker.MONDAY_ATTEND;
-import static domain.AttendanceBook.fromAttendanceRecords;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.AttendanceBook;
+import domain.AttendanceStatus;
 import domain.Crew;
 import dto.AttendanceRecordResponse;
 import dto.TotalRecordsResponse;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CheckAttendanceRecordTest {
     @Test
+    @DisplayName("닉네임을_입력하면_전날까지의_크루_출석_기록을_출력해야_한다")
     void 닉네임을_입력하면_전날까지의_크루_출석_기록을_출력해야_한다() {
         AttendanceBook attendanceBook = new AttendanceBook();
 
@@ -22,11 +25,22 @@ public class CheckAttendanceRecordTest {
         crew1.addDailyAttendance(Map.of(LocalDate.of(2024, 12, 2), MONDAY_ATTEND));
         attendanceBook.addNewCrew(crew1);
 
-        attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 3), EXCEPT_MONDAY_ATTEND));
-        attendanceBook.checkAttendance("쿠키", Map.of(LocalDate.of(2024, 12, 4), EXCEPT_MONDAY_ATTEND));
+        attendanceBook.initialize("쿠키", Map.of(LocalDate.of(2024, 12, 3), EXCEPT_MONDAY_ATTEND));
+        attendanceBook.initialize("쿠키", Map.of(LocalDate.of(2024, 12, 4), EXCEPT_MONDAY_ATTEND));
 
-        List<AttendanceRecordResponse> attendanceRecords = crew1.getAttendanceRecords();
+        List<AttendanceRecordResponse> record = attendanceBook.checkAttendanceHistoryByCrew("쿠키");
+        TotalRecordsResponse count = attendanceBook.checkAttendanceCountByCrew(record);
 
-        TotalRecordsResponse totalRecordsResponse = fromAttendanceRecords(attendanceRecords);
+        assertThat(record.getFirst().date()).isEqualTo(LocalDate.of(2024, 12, 2));
+        assertThat(record.get(1).date()).isEqualTo(LocalDate.of(2024, 12, 3));
+        assertThat(record.get(2).date()).isEqualTo(LocalDate.of(2024, 12, 4));
+
+        assertThat(record.getFirst().time()).isEqualTo(MONDAY_ATTEND);
+        assertThat(record.get(1).time()).isEqualTo(EXCEPT_MONDAY_ATTEND);
+        assertThat(record.get(2).time()).isEqualTo(EXCEPT_MONDAY_ATTEND);
+
+        assertThat(record.getFirst().attendanceStatus()).isEqualTo(AttendanceStatus.ATTEND);
+        assertThat(record.get(1).attendanceStatus()).isEqualTo(AttendanceStatus.ATTEND);
+        assertThat(record.get(2).attendanceStatus()).isEqualTo(AttendanceStatus.ATTEND);
     }
 }
