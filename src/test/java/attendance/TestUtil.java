3afaeedf7a8e72af446050e955ff8dd8208cc79d
YuyoungRhee <bbclara@naver.com>
2025-02-23 05:57:29 +0900

    test(TestUtil): 테스트를 위한 CustomClock 설정 유틸 추가

--- src/test/java/attendance/TestUtil.java
+++ src/test/java/attendance/TestUtil.java
@@ -5,11 +5,13 @@ import attendance.model.CustomClock;
 import attendance.model.FixedCustomClock;
 import attendance.model.WoowaDate;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Set;
 
 public class TestUtil {
     private static final CustomClock FIXED_CUSTOM_CLOCK = new FixedCustomClock(
+            LocalDate.of(2024, 12, 16),
             Set.of(LocalDate.of(2024, 12, 25))
     );
     private static final LocalDate startDate = LocalDate.of(2024, 12, 1);
@@ -25,6 +27,25 @@ public class TestUtil {
 
     public static CustomClock getClock() {
         return FIXED_CUSTOM_CLOCK;
+    }
+
+    public static CustomClock clockOf(int todayDate) {
+        return new CustomClock() {
+            @Override
+            public LocalDateTime now() {
+                return LocalDateTime.of(2024, 12, todayDate, 10, 0);
+            }
+
+            @Override
+            public LocalDate nowDate() {
+                return LocalDate.of(2024, 12, todayDate);
+            }
+
+            @Override
+            public boolean isHoliday(LocalDate date) {
+                return false;
+            }
+        };
     }
 
     public static LocalDate getTrainingStartDate() {
