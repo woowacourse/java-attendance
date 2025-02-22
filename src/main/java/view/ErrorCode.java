de193907f87460ca6cd20d3a7183edeac0f3bf25
RaZel713 <razel7130@gmail.com>
2025-02-22 20:12:07 +0900

    refactor: ErrorCode 공백 관리

--- src/main/java/view/ErrorCode.java
+++ src/main/java/view/ErrorCode.java
@@ -7,11 +7,13 @@ public enum ErrorCode {
 
     NICKNAME_NOT_FOUND("등록되지 않은 닉네임입니다."),
     TIME_NOT_IN_OPERATION_HOUR("캠퍼스 운영 시간은 08:00~23:00 입니다."),
+
     SATURDAY_NOT_WORKING_DAY_FORMAT("12월 %02d일 토요일은 등교일이 아닙니다."),
     SUNDAY_NOT_WORKING_DAY_FORMAT("12월 %02d일 일요일은 등교일이 아닙니다."),
     HOLIDAY_NOT_WORKING_DAY_FORMAT("12월 %02d일 공휴일은 등교일이 아닙니다."),
-    CHECK_ATTENDANCE_ALREADY_EXISTS("이미 출석한 날짜입니다. 수정 기능을 이용해주세요."),
     ATTENDANCE_RECORD_NOT_EXISTS_FORMAT("%02d일 기록이 존재하지 않습니다."),
+
+    CHECK_ATTENDANCE_ALREADY_EXISTS("이미 출석한 날짜입니다. 수정 기능을 이용해주세요."),
 
     DAY_INPUT_NOT_VALID("날짜(일) 입력이 올바르지 않습니다."),
     TIME_INPUT_NOT_VALID("시간 입력이 올바르지 않습니다.");
