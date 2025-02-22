037dc3679e3638b9d92b6a5f2868e75d735ef46e
junhaa <2171326@hansung.ac.kr>
2025-02-22 22:58:46 +0900

    fix: FileLoader에서 데이터가 추가되지 않는 오류 수정

--- src/main/java/attendance/util/FileLoader.java
+++ src/main/java/attendance/util/FileLoader.java
@@ -8,31 +8,24 @@ import java.util.List;
 
 public class FileLoader {
 
-    public static Attendance createAttendance(final String name, final LocalDateTime localDateTime) {
-        Attendance attendance = new Attendance(name);
-        attendance.add(localDateTime);
-        return attendance;
-    }
-
     public static List<Attendance> loadAll(final List<String> datas) {
         List<Attendance> attendances = new ArrayList<>();
         datas.forEach(data -> {
             List<String> seperatedData = Arrays.stream(data.split(",")).toList();
             String name = seperatedData.get(0);
             LocalDateTime localDateTime = LocalDateTime.parse(seperatedData.get(1).replace(" ", "T"));
-
-            findAttendanceByName(attendances, name, localDateTime).add(localDateTime);
+            findAttendanceByName(attendances, name).add(localDateTime);
         });
         return attendances;
     }
 
-    private static Attendance findAttendanceByName(List<Attendance> attendances, String name,
-                                                   LocalDateTime localDateTime) {
+    private static Attendance findAttendanceByName(List<Attendance> attendances, String name) {
         return attendances.stream()
                 .filter(attendance -> attendance.isNameMatch(name))
                 .findFirst()
                 .orElseGet(() -> {
-                    Attendance newAttendance = createAttendance(name, localDateTime);
+                    Attendance newAttendance = new Attendance(name);
+                    attendances.add(newAttendance);
                     return newAttendance;
                 });
     }
