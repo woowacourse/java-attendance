53d9888eca98704742ae38f6972a8fd3d9ba1c26
GiSeok Kang <ime.rokz@gmail.com>
2025-02-23 00:59:35 +0900

    style: 에외 메시지 수정

--- src/main/java/domain/AttendanceSheets.java
+++ src/main/java/domain/AttendanceSheets.java
@@ -19,7 +19,7 @@ public class AttendanceSheets {
     private void validateIsAlreadyAttendance(AttendanceSheet attendanceSheet) {
         if (findAttendanceByNickname(attendanceSheet.getNickname()).stream()
                 .anyMatch(attendanceSheet1 -> attendanceSheet1.isSame(attendanceSheet))) {
-            throw new IllegalArgumentException("[ERROR] 이미 출석했습니다. 수정 기능을 이용하세요");
+            throw new IllegalArgumentException("[ERROR] 이미 출석했습니다. 수정 기능을 이용하세요.");
         }
 
     }
