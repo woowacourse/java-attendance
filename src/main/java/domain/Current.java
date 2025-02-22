a402ec7e2065e612f94aa7f0cc9de4e956aa1dcd
jhan0121 <jhan0121@gmail.com>
2025-02-22 22:33:19 +0900

    refactor: DateUtil 내 주말 판정 기능 및 평일 일수 계산 로직 분리

--- src/main/java/domain/Current.java
+++ src/main/java/domain/Current.java
@@ -1,9 +1,14 @@
 package domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public enum Current {
     TODAY(2024, 12, 13);
+
+    public static final int CHRISTMAS = 25;
 
     private final int year;
     private final int month;
@@ -13,6 +18,19 @@ public enum Current {
         this.year = year;
         this.month = month;
         this.day = day;
+    }
+
+    public static boolean isDayOff(int day) {
+        LocalDate targetDate = LocalDate.of(2024, 12, day);
+        return targetDate.getDayOfWeek().getValue() >= DayOfWeek.SATURDAY.getValue()
+                || targetDate.getDayOfMonth() == CHRISTMAS;
+    }
+
+    public List<Integer> getAttendUntilDay() {
+        return IntStream.range(1, day)
+                .filter(day -> !isDayOff(day))
+                .boxed()
+                .toList();
     }
 
     public LocalDate getLocalDate() {
