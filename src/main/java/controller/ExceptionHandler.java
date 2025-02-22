63746ad9e5c6709cad45eff3a49433d200b25e98
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-22 12:37:58 +0900

    feat(RetryHandler) : 잘못된 입력 시 재입력받는 로직 객체 생성

---
+++ src/main/java/controller/ExceptionHandler.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.util.function.Supplier;
+
+public class ExceptionHandler {
+
+    public static <T> T retryIfIllegalArgumentAndReturn(Supplier<T> task) {
+        while (true) {
+            try {
+                return task.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage()); //TODO : outputView로 넘길 수 없을까?
+            }
+        }
+    }
+
+    public static void retryIfIllegalArgument(Runnable task) {
+        while (true) {
+            try {
+                task.run();
+                return;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage()); //TODO : outputView로 넘길 수 없을까?
+            }
+        }
+    }
+}
