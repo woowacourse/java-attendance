ed55d27a863c56581854e236b8c5d335cf5e190a
Jin409 <sualng123@gmail.com>
2025-02-22 18:14:01 +0900

    test: 기능 옵션을 찾는 테스트 추가

--- src/test/java/view/FeatureOptionTest.java
+++ src/test/java/view/FeatureOptionTest.java
@@ -1,26 +1,41 @@
 package view;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class FeatureOptionTest {
-    @Test
-    void functionTest1() {
-        String input = "1";
-        FeatureOption featureOption = FeatureOption.getFunction(input);
-        Assertions.assertThat(featureOption).isEqualTo(FeatureOption.APPLY_ATTENDANCE);
+    private static Stream<Arguments> testCasesForGetFunction() {
+        return Stream.of(
+                Arguments.of("Q", FeatureOption.QUIT),
+                Arguments.of("1", FeatureOption.APPLY_ATTENDANCE),
+                Arguments.of("2", FeatureOption.EDIT_ATTENDANCE),
+                Arguments.of("3", FeatureOption.CHECK_ATTENDANCE_OF_CREW),
+                Arguments.of("4", FeatureOption.CHECK_WARNING_CREW)
+        );
     }
 
-    @Test
-    void functionTest2() {
-        String input = "Q";
-        FeatureOption featureOption = FeatureOption.getFunction(input);
-        Assertions.assertThat(featureOption).isEqualTo(FeatureOption.QUIT);
+    @ParameterizedTest
+    @MethodSource("testCasesForGetFunction")
+    @DisplayName("조건에 따라 알맞은 기능 옵션을 가져온다")
+    void testGetFunction(String option, FeatureOption expected) {
+        // when & then
+        assertThat(FeatureOption.getFunction(option)).isEqualTo(expected);
     }
 
-    @Test
-    void functionTest3() {
-        String input = "7";
-        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> FeatureOption.getFunction(input));
+    @ParameterizedTest
+    @ValueSource(strings = {"FFFF", "-1", "333", "**"})
+    @DisplayName("잘못된 기능 옵션에 대한 입력값인 경우 예외를 던진다")
+    void testGetFunction(String input) {
+        // when & then
+        assertThatThrownBy(() -> FeatureOption.getFunction(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
+
 }
