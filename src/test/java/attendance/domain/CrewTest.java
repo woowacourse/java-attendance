a58410dac5c5ea0d11616cc2ee2f336f3f99ccda
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-22 17:31:04 +0900

    test(CrewTest): 크루 이름 및 객체 비교 검사 추가

--- src/test/java/attendance/domain/CrewTest.java
+++ src/test/java/attendance/domain/CrewTest.java
@@ -1,25 +1,28 @@
 package attendance.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CrewTest {
-    @DisplayName("기능: 이름이 같은 다른 크루 객체는 같은 크루로 확인")
+    @DisplayName("기능: 이름으로 Crew 인스턴스의 일치 여부를 비교")
+    @Test
+    void checkSameNameCrewInstance() {
+        Crew crew1 = new Crew("엠제이");
+
+        assertThatCode(() -> crew1.isSameCrewName("엠제이")).doesNotThrowAnyException();
+        assertThat(crew1.isSameCrewName("엠제이")).isTrue();
+    }
+
+    @DisplayName("기능: 이름이 동일한 다른 크루 객체는 같은 크루로 비교")
     @Test
     void checkSameNameCrew() {
         Crew crew1 = new Crew("리원");
         Crew crew2 = new Crew("리원");
 
         assertThatCode(() -> crew1.equals(crew2)).doesNotThrowAnyException();
-    }
-
-    @DisplayName("기능: 이름으로 Crew 인스턴스의 일치 여부를 확인")
-    @Test
-    void checkMatchingInstance() {
-        Crew crew1 = new Crew("엠제이");
-
-        assertThatCode(() -> crew1.isSameCrewName("엠제이")).doesNotThrowAnyException();
+        assertThat(crew1).isEqualTo(crew2);
     }
 }
