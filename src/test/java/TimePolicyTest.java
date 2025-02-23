b4b8fb92eb4e6e0b77f36e5179e1ae5d65a65ffe
sonjh919 <kingjh1125@gmail.com>
2025-02-23 20:23:54 +0900

    test(green): 시간 정책 테스트 추가
    - 분의 범위를 벗어나면 예외가 발생한다.
    ex) "a"

--- src/test/java/TimePolicyTest.java
+++ src/test/java/TimePolicyTest.java
@@ -18,7 +18,7 @@ public class TimePolicyTest {
 
     @ParameterizedTest
     @DisplayName("분의 범위를 벗어나면 예외가 발생한다")
-    @ValueSource(strings = {"60", "-1"})
+    @ValueSource(strings = {"60", "-1", "a"})
     void validateMinuteTest(String minute){
         assertThatThrownBy(() -> TimePolicy.validateMinute(minute))
                 .isInstanceOf(IllegalArgumentException.class);
