dc8e0279ff074408d33b9a5cfe162b9b4a536d90
LJM\eueo8 <eueo8259@gmail.com>
2025-02-23 23:10:47 +0900

    refactor: 출석 시간을 통해 AttendanceStatus를 가져오는 메서드 구현

--- src/main/java/model/AttendanceStatus.java
+++ src/main/java/model/AttendanceStatus.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public enum AttendanceStatus {
 
     ABSENT("결석"),
@@ -15,4 +18,10 @@ public enum AttendanceStatus {
     public String getState() {
         return state;
     }
+
+    public AttendanceStatus getAttendanceStatusForTime (LocalDateTime updateLocalDateTime){
+        int day = updateLocalDateTime.getDayOfWeek().getValue();
+        return AttendanceRuleByDay.calculateAttendance(day, LocalTime.from(updateLocalDateTime));
+    }
+    //AttendanceStatus 시간에 따라서 가져오는 메서드
 }
