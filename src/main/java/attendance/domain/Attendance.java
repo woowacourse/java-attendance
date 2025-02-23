95ffdf82ef515dbddf69a0bfffdaa606b29f5fca
황재웅 <wodnd0131@naver.com>
2025-02-23 14:40:36 +0900

    refactor: 출석 상태 결정 로직 가독성 개선

--- src/main/java/attendance/domain/Attendance.java
+++ src/main/java/attendance/domain/Attendance.java
@@ -6,6 +6,8 @@ import java.time.LocalTime;
 public record Attendance(LocalDateTime dateTime, AttendanceStatus attendanceStatus) {
 
     private static final LocalTime defaultSchedule = LocalTime.of(10, 0);
+    private static final int ABSENCE = 30;
+    private static final int LATE = 5;
 
     public Attendance(LocalDateTime dateTime) {
         this(dateTime, decideAttendanceStatus(dateTime));
@@ -13,14 +15,17 @@ public record Attendance(LocalDateTime dateTime, AttendanceStatus attendanceStat
 
     static AttendanceStatus decideAttendanceStatus(LocalDateTime dateTime) {
         var time = dateTime.toLocalTime();
-        if (time.isAfter(defaultSchedule.plusMinutes(30))) {
+        if (isAfter(time, ABSENCE)) {
             return AttendanceStatus.ABSENCE;
         }
-
-        if (time.isAfter(defaultSchedule.plusMinutes(5))) {
+        if (isAfter(time, LATE)) {
             return AttendanceStatus.LATE;
         }
 
         return AttendanceStatus.ATTENDANCE;
+    }
+
+    private static boolean isAfter(LocalTime time, int minutesToAdd) {
+        return time.isAfter(Attendance.defaultSchedule.plusMinutes(minutesToAdd));
     }
 }
