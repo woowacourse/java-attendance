dae5d18ecefa38a775729ffb6d23683bbf622b37
LJM\eueo8 <eueo8259@gmail.com>
2025-02-24 02:06:26 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/model/AttendanceStatus.java
+++ src/main/java/model/AttendanceStatus.java
@@ -1,8 +1,5 @@
 package model;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
 public enum AttendanceStatus {
 
     ABSENT("결석"),
@@ -18,10 +15,4 @@ public enum AttendanceStatus {
     public String getState() {
         return state;
     }
-
-    public AttendanceStatus getAttendanceStatusForTime (LocalDateTime updateLocalDateTime){
-        int day = updateLocalDateTime.getDayOfWeek().getValue();
-        return AttendanceRuleByDay.calculateAttendance(day, LocalTime.from(updateLocalDateTime));
-    }
-    //AttendanceStatus 시간에 따라서 가져오는 메서드
 }
