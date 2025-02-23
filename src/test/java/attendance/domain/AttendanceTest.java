90425ea23c7aab903251760f982bef007b279de4
taek2222 <oot309@daum.net>
2025-02-24 01:16:57 +0900

    test(AttendanceTest): 부족한 테스트 보완
    - 이미 출석, 출석 상태 동일 테스트를 새로 추가했습니다.
    
    - 다른 부분에 부족한 파라미터 테스트를 추가해 설계했습니다.

--- src/test/java/attendance/domain/AttendanceTest.java
+++ src/test/java/attendance/domain/AttendanceTest.java
@@ -1,46 +1,67 @@
 package attendance.domain;
 
-import attendance.utility.DateGenerator;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 class AttendanceTest {
 
-    @DisplayName("기본 날짜와 시간으로 출석을 생성할 수 있다.")
-    @Test
-    void 기본_날짜와_시간으로_출석을_생성할_수_있다() {
-        DateGenerator dateGenerator = new MockingDateGenerator();
-        LocalDateTime givenDateTime = LocalDateTime.of(
-                dateGenerator.now(),
-                LocalTime.of(10, 0)
-        );
+    @ParameterizedTest
+    @MethodSource
+    void 날짜와_시간으로_출석을_생성한다(LocalDateTime dateTime, AttendanceStateType excepted) {
+        // when
+        Attendance result = new Attendance(dateTime);
 
-        assertThatCode(() -> new Attendance(givenDateTime))
-                .doesNotThrowAnyException();
+        // than
+        assertThat(result.getDateTime()).isEqualTo(dateTime);
+        assertThat(result.getState()).isEqualTo(excepted);
     }
 
-    @DisplayName("동일 날짜에 여부를 확인한다")
     @ParameterizedTest
     @CsvSource({
             "2024-12-05T10:00:00, 2024-12-05, true",
             "2024-12-05T10:00:00, 2024-12-06, false"
     })
-    void 동일_날짜에_여부를_확인한다(LocalDateTime origin, LocalDate compare, boolean expected) {
+    void 동일_날짜_여부를_확인한다(LocalDateTime dateTime, LocalDate compare, boolean expected) {
         // given
-        Attendance attendance = new Attendance(origin);
+        Attendance attendance = new Attendance(dateTime);
 
         // then
-        assertThat(attendance.isEqualDate(compare))
+        assertThat(attendance.isSameDate(compare))
                 .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void 이미_출석_여부를_검사한다(LocalDateTime dateTime, boolean expected) {
+        // given
+        Attendance attendance = new Attendance(dateTime);
+
+        // then
+        assertThat(attendance.isAlreadyChecked())
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void 출석_상태가_동일한지_검사한다(AttendanceStateType state, boolean expected) {
+        // given
+        LocalDateTime dateTime = LocalDateTime.of(2024, 12, 2, 13, 0);
+        Attendance attendance = new Attendance(dateTime);
+
+        // when
+        boolean result = attendance.hasState(state);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest
@@ -58,15 +79,28 @@ class AttendanceTest {
         int result = firstAttendance.compareTo(secondAttendance);
 
         // then
-        assertThat(result)
-                .isEqualTo(excepted);
+        assertThat(result).isEqualTo(excepted);
     }
 
-    private static class MockingDateGenerator implements DateGenerator {
+    static Stream<Arguments> 날짜와_시간으로_출석을_생성한다() {
+        return Stream.of(
+                Arguments.of(LocalDateTime.of(2024, 12, 3, 10, 0), AttendanceStateType.ATTENDANCE),
+                Arguments.of(LocalDateTime.of(2024, 12, 3, 10, 6), AttendanceStateType.LATE),
+                Arguments.of(LocalDateTime.of(2024, 12, 3, 10, 31), AttendanceStateType.EXPULSION)
+        );
+    }
 
-        @Override
-        public LocalDate now() {
-            return LocalDate.of(2024, 12, 5);
-        }
+    static Stream<Arguments> 이미_출석_여부를_검사한다() {
+        return Stream.of(
+                Arguments.of(LocalDateTime.of(LocalDate.of(2024, 12, 3), LocalTime.MAX), false),
+                Arguments.of(LocalDateTime.of(LocalDate.of(2024, 12, 3), LocalTime.MIDNIGHT), true)
+        );
+    }
+
+    static Stream<Arguments> 출석_상태가_동일한지_검사한다() {
+        return Stream.of(
+                Arguments.of(AttendanceStateType.ATTENDANCE, true),
+                Arguments.of(AttendanceStateType.EXPULSION, false)
+        );
     }
 }
