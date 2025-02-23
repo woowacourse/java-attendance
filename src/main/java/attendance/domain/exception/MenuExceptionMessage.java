62985ecd56a6bf56f2787ad8fa7e8e10ab0c9985
ChoiDongJoon <adong0808@naver.com>
2025-02-24 00:48:55 +0900

    step2 시작을 위한 코드 삭제

--- src/main/java/attendance/domain/exception/MenuExceptionMessage.java
+++
@@ -1,7 +0,0 @@
-package attendance.domain.exception;
-
-public class MenuExceptionMessage {
-    private static final String PREFIX = "[ERROR] ";
-
-    public static final String NOT_IN_MENU = PREFIX + "메뉴를 잘못 선택하셨습니다.";
-}
