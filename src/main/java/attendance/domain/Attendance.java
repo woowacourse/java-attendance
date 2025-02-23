a7603275e1ee72b21ee77157bf49c8109a9fc958
황재웅 <wodnd0131@naver.com>
2025-02-23 17:28:28 +0900

    refactor: Attendance validate 및 상수 관리 개선

--- src/main/java/attendance/domain/Attendance.java
+++ src/main/java/attendance/domain/Attendance.java
@@ -7,15 +7,8 @@ import java.time.LocalTime;
 import attendance.common.exception.AttendanceArgumentException;
 
 public record Attendance(LocalDateTime dateTime, AttendanceStatus attendanceStatus) {
-    private static final LocalTime DEFAULT_SCHEDULE = LocalTime.of(10, 0);
-    private static final LocalTime MONDAY_SCHEDULE = LocalTime.of(13, 0);
-
     private static final String CANNOT_ATTENDANCE_WEEKEND_FORMAT = "MM월 dd일 E요일은 등교일이 아닙니다.";
     private static final String OUT_OF_SCHOOL_SCHEDULE = "등교시간에만 출석 가능합니다.";
-
-    private static final int ABSENCE = 30;
-    private static final int LATE = 5;
-    private static final int WEEKENDER = 6;
 
     public Attendance(LocalDateTime dateTime) {
         this(dateTime, decideAttendanceStatus(dateTime));
@@ -28,27 +21,39 @@ public record Attendance(LocalDateTime dateTime, AttendanceStatus attendanceStat
     }
 
     private static void validate(LocalDateTime dateTime) {
-        if (dateTime.getDayOfWeek().getValue() >= WEEKENDER) {
+        validateIsWeekend(dateTime);
+        validateIsDuringCampusSchedule(dateTime);
+    }
+
+    private static void validateIsWeekend(LocalDateTime dateTime) {
+        if (dateTime.getDayOfWeek().getValue() >= DayOfWeek.SATURDAY.getValue()) {
             throw new AttendanceArgumentException(CANNOT_ATTENDANCE_WEEKEND_FORMAT, dateTime);
         }
-        if (dateTime.toLocalTime().isBefore(LocalTime.of(8, 0))
-            || dateTime.toLocalTime().isAfter(LocalTime.of(23, 0))) {
+    }
+
+    private static void validateIsDuringCampusSchedule(LocalDateTime dateTime) {
+        LocalTime time = dateTime.toLocalTime();
+        if (isDuringCampusSchedule(time)) {
             throw new AttendanceArgumentException(OUT_OF_SCHOOL_SCHEDULE);
         }
     }
 
+    private static boolean isDuringCampusSchedule(LocalTime time) {
+        return time.isBefore(Schedule.CAMPUS_OPEN) || time.isAfter(Schedule.CAMPUS_CLOSE);
+    }
+
     private static LocalTime getScheduleForDay(LocalDateTime dateTime) {
         if (dateTime.getDayOfWeek() == DayOfWeek.MONDAY) {
-            return MONDAY_SCHEDULE;
+            return Schedule.MONDAY;
         }
-        return DEFAULT_SCHEDULE;
+        return Schedule.DEFAULT;
     }
 
     private static AttendanceStatus decideOnDefaultDay(LocalTime time, LocalTime baseSchedule) {
-        if (isAfter(time, ABSENCE, baseSchedule)) {
+        if (isAfter(time, Penalty.ABSENCE, baseSchedule)) {
             return AttendanceStatus.ABSENCE;
         }
-        if (isAfter(time, LATE, baseSchedule)) {
+        if (isAfter(time, Penalty.LATE, baseSchedule)) {
             return AttendanceStatus.LATE;
         }
         return AttendanceStatus.ATTENDANCE;
@@ -57,4 +62,17 @@ public record Attendance(LocalDateTime dateTime, AttendanceStatus attendanceStat
     private static boolean isAfter(LocalTime time, int minutesToAdd, LocalTime baseSchedule) {
         return time.isAfter(baseSchedule.plusMinutes(minutesToAdd));
     }
+
+    private static class Schedule {
+        private static final LocalTime DEFAULT = LocalTime.of(10, 0);
+        private static final LocalTime MONDAY = LocalTime.of(13, 0);
+        private static final LocalTime CAMPUS_OPEN = LocalTime.of(8, 0);
+        private static final LocalTime CAMPUS_CLOSE = LocalTime.of(23, 0);
+    }
+
+    private static class Penalty {
+        private static final int ABSENCE = 30;
+        private static final int LATE = 5;
+    }
+
 }
