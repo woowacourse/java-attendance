41d28e85d46c697f766608a3cff9874e87005376
MinJun Choi <mj043000@naver.com>
2025-02-23 20:19:42 +0900

    remove: 모든 파일 제거

--- src/main/java/attendance/Application.java
+++ src/main/java/attendance/Application.java
@@ -1,10 +1,6 @@
 package attendance;
 
-import attendance.controller.AttendanceController;
-
 public class Application {
     public static void main(String[] args) {
-        AttendanceController attendanceController = new AttendanceController();
-        attendanceController.run();
     }
 }
