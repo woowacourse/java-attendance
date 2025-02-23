fea1cb70f4b7466581262906caa8ad6f0b13fded
rladmstn <oz33177351@gmail.com>
2025-02-23 17:09:13 +0900

    refactor(FileReader): 메서드 분리
    indent 1 유지를 위해 파일을 읽어오는 부분의 메서드를 분리했습니다.

--- src/main/java/attendance/utils/FileReader.java
+++ src/main/java/attendance/utils/FileReader.java
@@ -6,23 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class FileReader {
-    
+
     private FileReader() {
     }
 
     public static List<String> parseToFile(final String filePath) {
 
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(filePath))) {
-
-            List<String> readingFileResult = new ArrayList<>();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                readingFileResult.add(line);
-            }
-
-            return readingFileResult;
+            return getFileLines(reader);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static List<String> getFileLines(BufferedReader reader) throws IOException {
+        List<String> readingFileResult = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            readingFileResult.add(line);
+        }
+        return readingFileResult;
     }
 }
