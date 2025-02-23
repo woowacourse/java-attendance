d078a65f1d5425b27808c8aeeec92368622a1a2b
goohong <goohong.chung@gmail.com>
2025-02-23 21:03:06 +0900

    refactor: Controller의 start 메서드 분리 및 Runnable Map 적용

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -32,37 +32,47 @@ public class AttendanceController {
     }
 
     public void start() {
-        List<String> fileData = fileReader.readFile();
-        List<String> removedData = Parser.parse(fileData);
-        List<NameParsedData> seperatedData = Parser.parseName(removedData);
-        AttendanceBook attendanceBook = new AttendanceBook();
-
-        attendanceBook.initializeAttendanceBook(seperatedData);
+        AttendanceBook attendanceBook = initializeAttendanceBook();
 
         while (true) {
             outputView.displayPrompt();
             UserInput selection = retryUntilValid(this::getUserInput);
 
-            try {
-                if (selection == UserInput.CHECK_ATTENDANCE) {
-                    checkAttendance(attendanceBook);
-                }
-                if (selection == UserInput.MODIFY_ATTENDANCE) {
-                    modifyAttendance(attendanceBook);
-                }
-                if (selection == UserInput.TOTAL_RECORDS_BY_CREW) {
-                    getTotalRecordsByCrew(attendanceBook);
-                }
-                if (selection == UserInput.CHECK_PENALTY) {
-                    outputView.displayPenaltyCrew(attendanceBook.checkPenaltyCrew());
-                }
-                if (selection == UserInput.QUIT) {
-                    break;
-                }
-            } catch (IllegalArgumentException e) {
-                outputView.displayErrorMessage(e.getMessage());
+            if (processUserSelection(selection, attendanceBook)) {
+                break;
             }
         }
+    }
+
+    private AttendanceBook initializeAttendanceBook() {
+        List<String> fileData = fileReader.readFile();
+        List<String> removedData = Parser.parse(fileData);
+        List<NameParsedData> separatedData = Parser.parseName(removedData);
+
+        AttendanceBook attendanceBook = new AttendanceBook();
+        attendanceBook.initializeAttendanceBook(separatedData);
+        return attendanceBook;
+    }
+
+    private boolean processUserSelection(UserInput selection, AttendanceBook attendanceBook) {
+        try {
+            Map<UserInput, Runnable> actions = Map.of(
+                    UserInput.CHECK_ATTENDANCE, () -> checkAttendance(attendanceBook),
+                    UserInput.MODIFY_ATTENDANCE, () -> modifyAttendance(attendanceBook),
+                    UserInput.TOTAL_RECORDS_BY_CREW, () -> getTotalRecordsByCrew(attendanceBook),
+                    UserInput.CHECK_PENALTY, () -> outputView.displayPenaltyCrew(attendanceBook.checkPenaltyCrew())
+            );
+
+            if (selection == UserInput.QUIT) {
+                return true;
+            }
+
+            actions.getOrDefault(selection,
+                    () -> outputView.displayErrorMessage(ErrorCode.INPUT_NOT_VALID.getMessage())).run();
+        } catch (IllegalArgumentException e) {
+            outputView.displayErrorMessage(e.getMessage());
+        }
+        return false;
     }
 
     private void getTotalRecordsByCrew(AttendanceBook attendanceBook) {
--- src/main/java/view/OutputMessages.java
+++ src/main/java/view/OutputMessages.java
@@ -4,11 +4,11 @@ import domain.UserInput;
 
 public enum OutputMessages {
     DATE_PROMPT("오늘은 %02d월 %02d일 %s입니다. 기능을 선택해 주세요."),
-    FIRST_FUNCTION_PROMPT(UserInput.CHECK_ATTENDANCE.getInput() + " 출석 확인"),
-    SECOND_FUNCTION_PROMPT(UserInput.MODIFY_ATTENDANCE.getInput() + " 출석 수정"),
-    THIRD_FUNCTION_PROMPT(UserInput.TOTAL_RECORDS_BY_CREW.getInput() + " 크루별 출석 기록 확인"),
-    FOURTH_FUNCTION_PROMPT(UserInput.CHECK_PENALTY.getInput() + " 제적 위험자 확인"),
-    QUIT_FUNCTION_PROMPT(UserInput.QUIT.getInput() + " 종료"),
+    FIRST_FUNCTION_PROMPT(UserInput.CHECK_ATTENDANCE.getInput() + ". 출석 확인"),
+    SECOND_FUNCTION_PROMPT(UserInput.MODIFY_ATTENDANCE.getInput() + ". 출석 수정"),
+    THIRD_FUNCTION_PROMPT(UserInput.TOTAL_RECORDS_BY_CREW.getInput() + ". 크루별 출석 기록 확인"),
+    FOURTH_FUNCTION_PROMPT(UserInput.CHECK_PENALTY.getInput() + ". 제적 위험자 확인"),
+    QUIT_FUNCTION_PROMPT(UserInput.QUIT.getInput() + ". 종료"),
     GUIDE_PROMPT("(원하는 기능의 번호 입력)"),
 
     DISPLAY_CHECK_ATTENDANCE_RESULT("%02d월 %02d일 %s %s %s"),
