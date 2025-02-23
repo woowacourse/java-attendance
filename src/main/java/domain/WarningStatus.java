3612b962255d91ba7bce3db56f8df773859cc98e
jhan0121 <jhan0121@gmail.com>
2025-02-23 19:01:28 +0900

    refactor: 매직 넘버 상수화

--- src/main/java/domain/WarningStatus.java
+++ src/main/java/domain/WarningStatus.java
@@ -4,10 +4,14 @@ import java.util.Arrays;
 import java.util.function.Predicate;
 
 public enum WarningStatus {
-    CLEAR((totalAbsenceCount) -> totalAbsenceCount < 2),
-    WARNING((totalAbsenceCount) -> totalAbsenceCount == 2),
-    INTERVIEW((totalAbsenceCount) -> totalAbsenceCount >= 3 && totalAbsenceCount <= 5),
-    EXPEL((totalAbsenceCount) -> totalAbsenceCount > 5);
+    CLEAR((totalAbsenceCount) -> totalAbsenceCount < WarningStatus.LATE_BOUNDARY),
+    WARNING((totalAbsenceCount) -> totalAbsenceCount == WarningStatus.LATE_BOUNDARY),
+    INTERVIEW((totalAbsenceCount) -> totalAbsenceCount > WarningStatus.LATE_BOUNDARY
+            && totalAbsenceCount <= WarningStatus.ABSENCE_BOUNDARY),
+    EXPEL((totalAbsenceCount) -> totalAbsenceCount > WarningStatus.ABSENCE_BOUNDARY);
+
+    private static final int LATE_BOUNDARY = 2;
+    private static final int ABSENCE_BOUNDARY = 5;
 
     private final Predicate<Long> matchCondition;
 
