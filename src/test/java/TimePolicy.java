381b5d24236a5d0fc06e57c0fb2d76de0fd74c76
sonjh919 <kingjh1125@gmail.com>
2025-02-23 21:13:29 +0900

    test(green): 시간 정책 테스트 수정
    - 캠퍼스 운영 시간이 아니면 예외가 발생한다
    ex) "07:59, 23:01"

--- src/test/java/TimePolicy.java
+++ src/test/java/TimePolicy.java
@@ -1,3 +1,5 @@
+import java.time.LocalTime;
+
 public class TimePolicy {
 
     private static final int MAX_HOUR = 23;
@@ -9,28 +11,43 @@ public class TimePolicy {
     private static final String TIME_SEPARATOR = ":";
     private static final int TIME_FORMAT_LENGTH = 2;
 
-    private static final String TIME_FORMAT_ERROR = "[ERROR] 시간 입력 형식이 잘못되었습니다.";
+    private static final String TIME_FORMAT_ERROR = "[ERROR] 시간 입력 형식이 잘못되었습니다";
 
-    public static void validateHour(String hour){
+    public static int validateHour(String hour){
         if(Integer.parseInt(hour)> MAX_HOUR || Integer.parseInt(hour)< MIN_HOUR){
             throw new IllegalArgumentException(TIME_FORMAT_ERROR);
         }
+        return Integer.parseInt(hour);
     }
 
-    public static void validateMinute(String minute) {
+    public static int validateMinute(String minute) {
         if(Integer.parseInt(minute) > MAX_MINUTE || Integer.parseInt(minute) < MIN_MINUTE){
             throw new IllegalArgumentException(TIME_FORMAT_ERROR);
         }
+        return Integer.parseInt(minute);
     }
 
-    public static void validateTimeFormat(String time) {
+    public static String[] validateTimeFormat(String time) {
         String[] splitTime = time.split(TIME_SEPARATOR);
 
         if(splitTime.length != TIME_FORMAT_LENGTH){
             throw new IllegalArgumentException(TIME_FORMAT_ERROR);
         }
+
+        return splitTime;
     }
 
     public static void validateOperatingTime(String time) {
+        String[] splitTime = validateTimeFormat(time);
+
+        int hour = validateHour(splitTime[0]);
+        int minute = validateMinute(splitTime[1]);
+
+        if(LocalTime.of(hour,minute).isBefore(LocalTime.of(8,0))
+        || LocalTime.of(hour,minute).isAfter(LocalTime.of(23,0)))
+        {
+            throw new IllegalArgumentException("[ERROR] 캠퍼스 운영 시간이 아닙니다");
+        }
+
     }
 }
