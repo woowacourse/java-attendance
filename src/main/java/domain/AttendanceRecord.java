6c77d7ff77958baea2342c208f908a265cb140b9
julie <7jujubuilds@gmail.com>
2025-02-23 10:09:59 +0900

    refactor(attendance_record): AttendanceRecord 클래스 리팩터링
    - LocalDate과 LocalTime의 상태를 분리해서 저장하던 부분을 하나의 LocalDateTime으로 저장

--- src/main/java/domain/AttendanceRecord.java
+++ src/main/java/domain/AttendanceRecord.java
@@ -1,48 +1,45 @@
 package domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class AttendanceRecord {
-    private static final int DATE_INDEX = 0;
-    private static final int TIME_INDEX = 1;
     public static final LocalTime ABSENT_TIME = LocalTime.of(14, 0);
 
-    private final LocalDate date;
-    private final LocalTime time;
+    private final LocalDateTime dateTime;
     private final Attendance attendance;
 
-    private AttendanceRecord(LocalDate date, LocalTime time) {
-        this.date = date;
-        validateDate(date);
-        this.time = time;
-        this.attendance = Attendance.getAttendanceStatus(Day.getDay(date), time);
+    private AttendanceRecord(LocalDateTime dateTime) {
+        validateDate(dateTime);
+        this.dateTime = dateTime;
+        this.attendance = Attendance.getAttendanceStatus(Day.getDay(dateTime.toLocalDate()), dateTime.toLocalTime());
     }
 
     public static AttendanceRecord parse(String dateTime) {
-        return new AttendanceRecord(LocalDate.parse(dateTime.split(" ")[DATE_INDEX]),
-                LocalTime.parse(dateTime.split(" ")[TIME_INDEX]));
+        return new AttendanceRecord(LocalDateTime.parse(dateTime.replace(" ", "T")));
     }
 
     public static AttendanceRecord of(LocalDate date, LocalTime time) {
-        return new AttendanceRecord(date, time);
+        return new AttendanceRecord(LocalDateTime.of(date, time));
     }
 
     public static AttendanceRecord checkIn(LocalTime time, DateGenerator dateGenerator) {
-        return new AttendanceRecord(dateGenerator.generate(), time);
+        return new AttendanceRecord(LocalDateTime.of(dateGenerator.generate(), time));
     }
 
     public static AttendanceRecord asAbsent(LocalDate date) {
-        return new AttendanceRecord(date, ABSENT_TIME);
+        return new AttendanceRecord(LocalDateTime.of(date, ABSENT_TIME));
     }
 
     public LocalDate getDate() {
-        return date;
+        return dateTime.toLocalDate();
     }
 
     public LocalTime getTime() {
-        return time;
+        return dateTime.toLocalTime();
     }
 
     public Attendance getAttendance() {
@@ -57,12 +54,11 @@ public class AttendanceRecord {
         return attendance.equals(Attendance.ABSENT);
     }
 
-    private void validateDate(LocalDate date) {
-        if (Day.checkHoliday(date)) {
-            throw new IllegalArgumentException(String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.",
-                    date.getMonthValue(),
-                    date.getDayOfMonth(),
-                    Day.getDay(date).getName()));
+    private void validateDate(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM월 dd일 E요일");
+        String formattedDateTime = dateTime.format(formatter);
+        if (Day.checkHoliday(dateTime.toLocalDate())) {
+            throw new IllegalArgumentException(String.format("[ERROR] %s은 등교일이 아닙니다.", formattedDateTime));
         }
     }
 
@@ -70,11 +66,11 @@ public class AttendanceRecord {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         AttendanceRecord that = (AttendanceRecord) o;
-        return Objects.equals(date, that.date) && Objects.equals(time, that.time) && attendance == that.attendance;
+        return Objects.equals(dateTime, that.dateTime) && attendance == that.attendance;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(date, time, attendance);
+        return Objects.hash(dateTime, attendance);
     }
 }
