c895c3fedfb8af25305c24b2daa0156341370034
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-23 23:40:03 +0900

    feat(OptionLookupExpulsion): 제적 위험 옵션 관련 프로세스 객체 분리

---
+++ src/main/java/attendance/domain/OptionLookupExpulsion.java
@@ -0,0 +1,38 @@
+package attendance.domain;
+
+import attendance.view.InputView;
+import attendance.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OptionLookupExpulsion extends MenuOption {
+    public OptionLookupExpulsion(InputView inputView, OutputView outputView, Crews crews, Attendances attendances) {
+        super(inputView, outputView, crews, attendances);
+    }
+
+    @Override
+    public void executeMenuOption(MenuCommand command) {
+        if (!isCorrectCommand(command)) {
+            return;
+        }
+        executeLookupExpulsion();
+    }
+
+    @Override
+    public boolean isCorrectCommand(MenuCommand command) {
+        return command.equals(MenuCommand.EXPEL);
+    }
+
+    private void executeLookupExpulsion() {
+        List<CrewStatistic> crewsStatistics = new ArrayList<>();
+        for (Crew crew : crews.getCrews()) {
+            List<Attendance> crewAttendances = attendances.findCrewAttendances(crew);
+            CrewStatistic crewStatistic = new CrewStatistic(crew, crewAttendances);
+            crewStatistic.checkCrewStatistic();
+            crewsStatistics.add(crewStatistic);
+        }
+        CrewStatistics crewStatistics = new CrewStatistics(crewsStatistics);
+        CrewStatistics sortedCrewStatistics = crewStatistics.sortCrewStatistics();
+        outputView.printExpelExpectedCrews(sortedCrewStatistics.crewsExpelExpectedInfo());
+    }
+}
