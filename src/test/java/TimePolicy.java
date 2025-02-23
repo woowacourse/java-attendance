2ceae5832f34cb63a83414b70424011a00c2eda5
sonjh919 <kingjh1125@gmail.com>
2025-02-23 20:02:51 +0900

    test(green): 시간 정책 테스트 수정
    - 시의 범위를 벗어나면 예외가 발생한다.
    ex) -1:00

--- src/test/java/TimePolicy.java
+++ src/test/java/TimePolicy.java
@@ -4,7 +4,7 @@ public class TimePolicy {
     private static final String TIME_FORMAT_ERROR = "[ERROR] 시간 입력 형식이 잘못되었습니다.";
 
     public static void validateHour(String hour){
-        if(Integer.parseInt(hour)> MAX_HOUR){
+        if(Integer.parseInt(hour)> MAX_HOUR || Integer.parseInt(hour)<0){
             throw new IllegalArgumentException(TIME_FORMAT_ERROR);
         }
     }
