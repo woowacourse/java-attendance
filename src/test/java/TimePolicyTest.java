eb09ebbdee790de785ed52627c70592f3ffa3509
sonjh919 <kingjh1125@gmail.com>
2025-02-23 20:58:01 +0900

    test(green): 시간 정책 테스트 추가
    - 시간 입력 형식이 다르면 예외가 발생한다.
    ex) "12:12:12"

--- src/test/java/TimePolicyTest.java
+++ src/test/java/TimePolicyTest.java
@@ -27,7 +27,7 @@ public class TimePolicyTest {
 
     @ParameterizedTest
     @DisplayName("시간 입력 형식이 다르면 예외가 발생한다")
-    @ValueSource(strings = {"12/34"})
+    @ValueSource(strings = {"12/34", "12:12:12"})
     void validateTimeFormatTest(String time) {
         assertThatThrownBy(() -> TimePolicy.validateTimeFormat(time))
                 .isInstanceOf(IllegalArgumentException.class);
