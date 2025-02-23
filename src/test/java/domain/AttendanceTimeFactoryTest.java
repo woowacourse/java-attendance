171033742dcd8041988fb5ba33062a1569fadef4
GiSeok Kang <ime.rokz@gmail.com>
2025-02-23 14:24:37 +0900

    refactor: 테스트 코드 상수 제거

--- src/test/java/domain/AttendanceTimeFactoryTest.java
+++ src/test/java/domain/AttendanceTimeFactoryTest.java
@@ -9,14 +9,14 @@ import util.FileReaderUtil;
 
 class AttendanceTimeFactoryTest {
 
-    private static final String filePath = "src/test/resources/attendance.csv";
+    private static final String FILE_PATH = "src/test/resources/attendance.csv";
 
     @Test
     @DisplayName("파일을 읽어 출석 기록 객체를 만들 수 있다.")
     void read_file_then_make_attendance_sheet() {
         // given
         AttendanceSheetsFactory attendanceSheetsFactory = new AttendanceSheetsFactory(
-                new FileReaderUtil("src/test/resources/attendance.csv"));
+                new FileReaderUtil(FILE_PATH));
         List<AttendanceSheet> expected = List.of(new AttendanceSheet("테스트",
                 AttendanceDateTime.from(LocalDateTime.of(2024, 12, 13, 10, 8))));
         // when
