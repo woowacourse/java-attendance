8251c9fe292bd17b07869ffbf7e309faee5d1489
sonjh919 <kingjh1125@gmail.com>
2025-02-23 20:21:30 +0900

    test(refactor): 시간 정책 테스트 리팩터링
    - 분의 범위를 벗어나면 예외가 발생한다.
    ex) "-1"

--- src/test/java/TimePolicy.java
+++ src/test/java/TimePolicy.java
@@ -3,8 +3,10 @@ public class TimePolicy {
     private static final int MAX_HOUR = 23;
     private static final int MIN_HOUR = 0;
 
-    private static final String TIME_FORMAT_ERROR = "[ERROR] 시간 입력 형식이 잘못되었습니다.";
     private static final int MAX_MINUTE = 59;
+    private static final int MIN_MINUTE = 0;
+
+    private static final String TIME_FORMAT_ERROR = "[ERROR] 시간 입력 형식이 잘못되었습니다.";
 
     public static void validateHour(String hour){
         if(Integer.parseInt(hour)> MAX_HOUR || Integer.parseInt(hour)< MIN_HOUR){
@@ -13,7 +15,7 @@ public class TimePolicy {
     }
 
     public static void validateMinute(String minute) {
-        if(Integer.parseInt(minute) > MAX_MINUTE || Integer.parseInt(minute) < 0){
+        if(Integer.parseInt(minute) > MAX_MINUTE || Integer.parseInt(minute) < MIN_MINUTE){
             throw new IllegalArgumentException(TIME_FORMAT_ERROR);
         }
     }
