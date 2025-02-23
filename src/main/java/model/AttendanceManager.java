aafed65f7dbc9df1fbf75546a2449ab8e3cabcf2
LJM\eueo8 <eueo8259@gmail.com>
2025-02-23 23:29:24 +0900

    refactor: 출석 정보 업데이트 하는 메서드 구현

--- src/main/java/model/AttendanceManager.java
+++ src/main/java/model/AttendanceManager.java
@@ -7,91 +7,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AttendanceManager {
-    private final Map<Student, Map<LocalDate, AttendanceRecord>> record = new HashMap<>();
+    private Map<Student, Map<LocalDate, AttendanceRecord>> record = new HashMap<>();
 
-
-    public void findAttendanceStatusByLocalDate(LocalDateTime updateLocalDateTime, Student student) {
+    public void updateAttendanceStatusByLocalDate(LocalDateTime updateLocalDateTime, Student student) {
+        //updateLocalDateTime을 통해서 만약 기존 정보에 LocalDate에 관한 정보가 있다면 업데이트하고 없다면 결석처리
         int day = updateLocalDateTime.getDayOfWeek().getValue();
-        if (checkHoliday(updateLocalDateTime, day)) {
-            throw new IllegalArgumentException("[주말 및 공휴일에는 등교일이 아닙니다]");
-        }
+        checkHoliday(updateLocalDateTime, day);
         AttendanceStatus newAttendanceStatus = AttendanceRuleByDay.
                 calculateAttendance(day, LocalTime.from(updateLocalDateTime));
         Map<LocalDate, AttendanceRecord> studentAttendanceRecord = record.get(student);
         for (LocalDate recordLocalDate : studentAttendanceRecord.keySet()) {
-            if (compareDayAndModify(updateLocalDateTime, recordLocalDate, newAttendanceStatus, student)) {
+            if (compareDayIsSame(updateLocalDateTime, recordLocalDate)) {
+                modifyAttendanceRecord(updateLocalDateTime, recordLocalDate, newAttendanceStatus, student);
                 return;
             }
         }
-
-        updateRecordForNoLocalDateTime(updateLocalDateTime, newAttendanceStatus);
+        record.get(student).put(LocalDate.from(updateLocalDateTime), new AttendanceRecord(null, AttendanceStatus.ABSENT));
     }
 
-    private static boolean checkHoliday(LocalDateTime localDateTime, int day) {
-        return day == 6 || day == 7 || localDateTime.getDayOfMonth() == 25;
+
+    private void modifyAttendanceRecord(LocalDateTime updateLocalDateTime, LocalDate recordLocalDate,
+                                           AttendanceStatus newAttendanceStatus, Student student) {
+        LocalTime localTime = LocalTime.from(updateLocalDateTime);
+        record.get(student).put(recordLocalDate, new AttendanceRecord(localTime, newAttendanceStatus));
     }
 
-    private boolean compareDayAndModify(LocalDateTime updateLocalDateTime, LocalDate recordLocalDate,
-                                        AttendanceStatus newAttendanceStatus, Student student) {
-        if (compareDayIsSame(updateLocalDateTime, recordLocalDate)) {
-            return modifyAttendanceRecord(updateLocalDateTime, recordLocalDate, newAttendanceStatus, student);
-        }
-        return false;
+    private void checkHoliday(LocalDateTime localDateTime, int day) {
+        if (day == 6 || day == 7 || localDateTime.getDayOfMonth() == 25){
+            throw new IllegalArgumentException("[주말 및 공휴일에는 등교일이 아닙니다]");
+        };
     }
 
     public boolean compareDayIsSame(LocalDateTime localDateTime, LocalDate localDate) {
         return LocalDate.from(localDateTime).equals(localDate);
     }
-
-    private boolean modifyAttendanceRecord(LocalDateTime updateLocalDateTime, LocalDate recordLocalDate,
-                                           AttendanceStatus newAttendanceStatus, Student student) {
-        //Map에 대한 기록 새롭게 하고 Student에는 oldStatus와 newStatus를 보내서 Student에서 oldStatus의 경우 -를 newStatus는 +를 하도록 처리
-        Map<LocalDate, AttendanceRecord> studentAttendanceRecord = record.get(student);
-        AttendanceStatus oldAttendanceStatus = studentAttendanceRecord.get(recordLocalDate).getAttendanceStatus();
-
-        if (oldAttendanceStatus.equals(AttendanceStatus.ATTENDANCE)) {
-            if (modifyRecordAndUpdateStudentForAttendance(updateLocalDateTime, recordLocalDate, attendanceStatus)) {
-                return true;
-            }
-        }
-
-        if (oldAttendanceStatus.equals(AttendanceStatus.LATE)) {
-            if (modifyRecordAndUpdateStudentForLate(updateLocalDateTime, localDateTime1, attendanceStatus)) {
-                return true;
-            }
-        }
-
-        if (oldAttendanceStatus.equals(AttendanceStatus.ABSENT)) {
-            return modifyRecordAndUpdateStudentForAbsent(updateLocalDateTime, localDateTime1, attendanceStatus);
-        }
-        return false;
-    }
-    private boolean modifyRecordAndUpdateStudentForAbsent(LocalDateTime localDateTime, LocalDateTime localDateTime1,
-                                                          AttendanceStatus attendanceStatus) {
-        absent --;
-        return modifyRecordAndUpdateStudentForState(localDateTime, localDateTime1, attendanceStatus);
-    }
-
-    private boolean modifyRecordAndUpdateStudentForLate(LocalDateTime localDateTime, LocalDateTime localDateTime1,
-                                                        AttendanceStatus attendanceStatus) {
-        late --;
-        return modifyRecordAndUpdateStudentForState(localDateTime, localDateTime1, attendanceStatus);
-    }
-
-    private boolean modifyRecordAndUpdateStudentForState(LocalDateTime localDateTime, LocalDateTime localDateTime1,
-                                                         AttendanceStatus attendanceStatus) {
-        modifyRecord(localDateTime, localDateTime1, attendanceStatus);
-        assert attendanceStatus != null;
-        return updateStudentAfterModify(attendanceStatus);
-    }
-
-    private boolean modifyRecordAndUpdateStudentForAttendance(LocalDateTime localDateTime, LocalDateTime localDateTime1,
-                                                              AttendanceStatus attendanceStatus) {
-        attendance --;
-        return modifyRecordAndUpdateStudentForState(localDateTime, localDateTime1, attendanceStatus);
-    }
-
-
-
-
 }
