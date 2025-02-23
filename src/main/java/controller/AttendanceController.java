165c6b0cda4e640b06f30942947062337bdf6a05
eunseongu <rndmstjs75@gmail.com>
2025-02-23 22:20:01 +0900

    refactor: 기능 실행 로직을 Function 객체로 추출
    - indent 1 이하, 메서드 10줄이하 제한 조건을 만족하기 위해 수정한다.

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -2,6 +2,7 @@ package controller;
 
 import domain.AttendanceManager;
 import domain.AttendanceStatistics;
+import domain.Function;
 import domain.Penalty;
 import domain.Records;
 import domain.StatisticsResult;
@@ -37,79 +38,70 @@ public class AttendanceController {
     }
 
     public void run() {
-        LocalDate nowDate = formatNowDate();
-        String functionNumber = "";
+        try {
+            processUserCommand();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    private void processUserCommand() {
+        LocalDate nowDate = formatNowDate();
+        String functionNumber;
         do {
             functionNumber = inputView.printFunction(nowDate);
-            switch (functionNumber) {
-                case "1" -> attend();
-                case "2" -> edit();
-                case "3" -> check();
-                case "4" -> checkExpelledWarning();
-                default -> {
-                    if (!List.of("1", "2", "3", "4", "q", "Q").contains(functionNumber)) {
-                        System.out.println("유효하지 않은 번호입니다.");
-                    }
-                }
-            }
-        } while (!functionNumber.equals("q"));
+            Function function = Function.from(functionNumber);
+            executeFunction(function, nowDate);
+        } while (Function.from(functionNumber) != Function.QUIT);
+    }
+
+    private void executeFunction(Function function, LocalDate nowDate) {
+        switch (function) {
+            case ATTEND -> attend();
+            case EDIT -> edit();
+            case CHECK -> check(nowDate);
+            case CHECK_EXPELLED_WARNING -> checkExpelledWarning(nowDate);
+        }
     }
 
     private void attend() {
-        try {
-            String name = inputView.readName();
-            attendanceManager.findByName(name);
-            List<String> attendTime = List.of(inputView.readTime().split(TIME_DELIMITER));
-            LocalDateTime attendDateTime = formatDateTime(ATTENDANCE_DAY_OF_MONTH, attendTime);
+        String name = inputView.readName();
+        attendanceManager.findByName(name);
+        List<String> attendTime = List.of(inputView.readTime().split(TIME_DELIMITER));
+        LocalDateTime attendDateTime = formatDateTime(ATTENDANCE_DAY_OF_MONTH, attendTime);
 
-            TimeAndStatus timeAndStatus = attendanceManager.attendCrew(name, attendDateTime);
-            outputView.printAttendanceRecord(attendDateTime.toLocalDate(), timeAndStatus);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        TimeAndStatus timeAndStatus = attendanceManager.attendCrew(name, attendDateTime);
+        outputView.printAttendanceRecord(attendDateTime.toLocalDate(), timeAndStatus);
     }
 
     private void edit() {
-        try {
-            String name = inputView.readEditName();
-            attendanceManager.findByName(name);
-            int editDayOfMonth = Integer.parseInt(inputView.readEditDayOfMonth());
-            List<String> editTime = List.of(inputView.readEditTime().split(TIME_DELIMITER));
-            LocalDateTime editDateTime = formatDateTime(editDayOfMonth, editTime);
-            LocalDate editDate = editDateTime.toLocalDate();
+        String name = inputView.readEditName();
+        attendanceManager.findByName(name);
+        int editDayOfMonth = Integer.parseInt(inputView.readEditDayOfMonth());
+        List<String> editTime = List.of(inputView.readEditTime().split(TIME_DELIMITER));
+        LocalDateTime editDateTime = formatDateTime(editDayOfMonth, editTime);
+        LocalDate editDate = editDateTime.toLocalDate();
 
-            TimeAndStatus oldTimeAndStatus = attendanceManager.findByName(name).findByDate(editDate);
-            TimeAndStatus newTimeAndStatus = attendanceManager.editCrew(name, editDateTime);
-            outputView.printEditResult(editDate, oldTimeAndStatus, newTimeAndStatus);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        TimeAndStatus oldTimeAndStatus = attendanceManager.findByName(name).findByDate(editDate);
+        TimeAndStatus newTimeAndStatus = attendanceManager.editCrew(name, editDateTime);
+        outputView.printEditResult(editDate, oldTimeAndStatus, newTimeAndStatus);
     }
 
-    private void check() {
-        try {
-            String name = inputView.readName();
-            attendanceManager.findByName(name);
-            LocalDate nowDate = formatNowDate();
-            Records records = attendanceManager.findByName(name);
-            StatisticsResult statisticsResult = AttendanceStatistics.countStatus(nowDate, records);
+    private void check(LocalDate nowDate) {
+        String name = inputView.readName();
+        Records records = attendanceManager.findByName(name);
+        StatisticsResult statisticsResult = AttendanceStatistics.countStatus(nowDate, records);
 
-            outputView.printRecords(name, nowDate, records);
-            outputView.printStatistics(
-                    statisticsResult.getAttendanceCount(),
-                    statisticsResult.getLatenessCount(),
-                    statisticsResult.getAbsenceCount(),
-                    statisticsResult.getPenalty()
-            );
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        outputView.printRecords(name, nowDate, records);
+        outputView.printStatistics(
+                statisticsResult.getAttendanceCount(),
+                statisticsResult.getLatenessCount(),
+                statisticsResult.getAbsenceCount(),
+                statisticsResult.getPenalty()
+        );
     }
 
-    private void checkExpelledWarning() {
-        LocalDate nowDate = formatNowDate();
-
+    private void checkExpelledWarning(LocalDate nowDate) {
         Map<String, StatisticsResult> sortedResult = attendanceManager.sortCrew(nowDate);
         outputView.printExpelledWarningResult(sortedResult);
     }
