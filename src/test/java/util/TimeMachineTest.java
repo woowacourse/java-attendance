11858e3ce6b12285e8b3f7ee4d02539e4546b2a7
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-23 19:01:18 +0900

    feat: 타임 머신
    - 24년 12월의 어떤 날로 이동하는 기능 추가

---
+++ src/test/java/util/TimeMachineTest.java
@@ -0,0 +1,26 @@
+package util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TimeMachineTest {
+
+    @Test
+    @DisplayName("타임머신으로 2024년 12월의 특정 날짜로 이동할 수 있다")
+    void timeTravelAt() {
+        // given
+        LocalDate timeBeforeTimeTravel = TimeMachine.dateOfNow();
+
+        // when
+        TimeMachine.timeTravelAt(29);
+
+        // then
+        assertThat(TimeMachine.dateOfNow())
+                .isEqualTo(LocalDate.of(2024, 12, 29))
+                .isNotEqualTo(timeBeforeTimeTravel);
+    }
+}
