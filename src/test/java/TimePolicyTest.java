2776f7ac2e10e40323e24c0e4609bce94e7748e3
sonjh919 <kingjh1125@gmail.com>
2025-02-23 20:12:31 +0900

    test(green): 시간 정책 테스트 추가
    - 시의 범위를 벗어나면 예외가 발생한다.
    ex) "a"

--- src/test/java/TimePolicyTest.java
+++ src/test/java/TimePolicyTest.java
@@ -1,9 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,7 +9,7 @@ public class TimePolicyTest {
 
     @ParameterizedTest
     @DisplayName("시의 범위를 벗어나면 예외가 발생한다")
-    @ValueSource(strings = {"-1", "24"})
+    @ValueSource(strings = {"-1", "24", "a"})
     void validateHourTest(String hour){
         assertThatThrownBy(() -> TimePolicy.validateHour(hour))
                 .isInstanceOf(IllegalArgumentException.class);
