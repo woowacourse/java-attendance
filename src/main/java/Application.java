6aa610d0972afb6b4aa4e2d01a5bab60665b3b30
sonjh919 <kingjh1125@gmail.com>
2025-02-23 17:55:47 +0900

    init: 모든 코드 지우기(tdd setup)

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,4 @@
-import controller.AttendanceController;
-import view.InputView;
-
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Application {
     public static void main(String[] args){
-        AttendanceController attendanceController = new AttendanceController(new InputView(new Scanner(System.in)));
-        attendanceController.start();
     }
 }
