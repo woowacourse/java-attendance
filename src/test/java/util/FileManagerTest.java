3c3a0e78ca931498f75cdd1f069477dc1c88ed60
DongYeop <s01054830455@gmail.com>
2025-02-23 15:31:42 +0900

    test: FileManage 파일 읽어오기 테스트 코드 검증 결과 추가

--- src/test/java/util/FileManagerTest.java
+++ src/test/java/util/FileManagerTest.java
@@ -4,20 +4,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.Attendance;
+import domain.Crew;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class FileManagerTest {
 
-    @DisplayName("csv파일을 가져올 수 있다.")
+    @DisplayName("csv파일을 읽어와 Attendance 객체를 생성할 수 있다.")
     @Test
     void getReadFile() {
         //given
         String filePath = "src/main/resources/attendances.csv";
+
+        //when
         Attendance attendance = FileManager.readFile(filePath);
 
-        //when & then
+        //then
+        Crew crew = Crew.from("이든");
+
         assertThat(attendance).isNotNull();
+        assertThat(attendance.getAttendances())
+                .containsKey(crew)
+                .isNotEmpty();
     }
 
     @DisplayName("파일 경로가 잘못된 경우 예외를 발생한다.")
