c0de38cbc55131860e9c0209f523d96ab5fc88d2
RaZel713 <razel7130@gmail.com>
2025-02-24 20:36:01 +0900

    green: csv 파일을 읽어서 크루 등록을 진행한다.

--- src/main/java/service/CrewRegistration.java
+++ src/main/java/service/CrewRegistration.java
@@ -1,5 +1,30 @@
 package service;
 
+import domain.AttendanceBook;
+import domain.Crew;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import utils.CsvReader;
+import utils.ParsingUtils;
+
 public class CrewRegistration {
     // TODO: CsvReader 와 ParsingUtils 에서 메서드 호출
+    public AttendanceBook registerCrews(String filePath) {
+        List<Crew> crews = new ArrayList<>();
+
+        List<String> existedRecords = CsvReader.readExistedRecords(filePath);
+        existedRecords.removeFirst();
+        for (String existedRecord : existedRecords) {
+            String name = ParsingUtils.parseRecordToNameAndDate(existedRecord).getFirst();
+            String timeLog = ParsingUtils.parseRecordToNameAndDate(existedRecord).getLast();
+
+            String date = ParsingUtils.parseDateToDayAndTime(timeLog).getFirst();
+            String time = ParsingUtils.parseDateToDayAndTime(timeLog).getLast();
+
+            crews.add(new Crew(name, LocalDate.parse(date), LocalTime.parse(time)));
+        }
+        return new AttendanceBook(crews);
+    }
 }
