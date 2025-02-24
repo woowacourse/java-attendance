b0bbfb6cb2cc81eb1dda43953e59d38ac903df38
ChoiDongJoon <adong0808@naver.com>
2025-02-24 16:42:35 +0900

    feat: 요일에 따라 달라지는 시작 시간 구현

--- src/main/java/attendance/Attendance.java
+++ src/main/java/attendance/Attendance.java
@@ -1,19 +1,33 @@
 package attendance;
 
+import java.time.DayOfWeek;
 import java.util.Objects;
 
 public class Attendance {
 
-    public String attend(String nickname, String timeInput) {
+    public String attend(String timeInput, DayOfWeek dayOfWeek) {
         if(Objects.equals("--:--", timeInput)) {
             return "결석";
         }
-        String[] time = timeInput.split(":");
+        int startHour = checkStartHour(dayOfWeek);
 
+        String[] time = timeInput.split(":");
         int hour = Integer.parseInt(time[0]);
         int minute = Integer.parseInt(time[1]);
 
-        if(hour > 10 || minute > 30) {
+        return checkAttendanceStatus(hour, startHour, minute);
+    }
+
+    private static int checkStartHour(DayOfWeek dayOfWeek) {
+        int startHour = 10;
+        if(dayOfWeek == DayOfWeek.MONDAY) {
+            startHour = 13;
+        }
+        return startHour;
+    }
+
+    private static String checkAttendanceStatus(int hour, int startHour, int minute) {
+        if(hour > startHour || minute > 30) {
             return "결석";
         }
         if(minute > 5) {
