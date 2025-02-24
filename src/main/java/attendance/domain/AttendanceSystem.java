29568ff51c8b9d4be9812c63ea7ee5bacb7cc8eb
KJungW <sagakkb0707@naver.com>
2025-02-24 19:29:56 +0900

    feat(add_record): 이미 등교한 경우 처리 (구현)
    작업 내용
    - 이미 출석한 경우, 다시 출석할 수 없으며 수정 기능을 이용하도록 안내한다

--- src/main/java/attendance/domain/AttendanceSystem.java
+++ src/main/java/attendance/domain/AttendanceSystem.java
@@ -1,5 +1,6 @@
 package attendance.domain;
 
+import attendance.exception.ExceptionMessage;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -13,6 +14,11 @@ public class AttendanceSystem {
     private List<AttendanceRecord> records = new ArrayList<>();
 
     public void addAttendanceRecord(String crewNickname, LocalDateTime arrivalDateTime) {
+        Optional<AttendanceRecord> originRecord = findAttendanceRecord(crewNickname, arrivalDateTime.toLocalDate());
+        if (originRecord.isPresent()) {
+            throw new IllegalArgumentException(ExceptionMessage.ALREADY_ATTENDANCE.getMessage());
+        }
+
         if (arrivalDateTime.getDayOfWeek() == DayOfWeek.MONDAY) {
             AttendanceType type = AttendanceType.parse(LocalTime.of(13, 0, 0), arrivalDateTime.toLocalTime());
             AttendanceRecord newRecord = new AttendanceRecord(crewNickname, arrivalDateTime, type);
