a133a7504f661c7912d932aae421113b9b92084f
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-24 11:15:02 +0900

    refactor(CrewStatusTest): 상태별 문자열 반환 테스트 케이스 분리

--- src/test/java/attendance/domain/CrewStatusTest.java
+++ src/test/java/attendance/domain/CrewStatusTest.java
@@ -6,11 +6,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CrewStatusTest {
-    @DisplayName("기능: 크루 출석 상태에 대응하는 문자열 반환")
+    @DisplayName("기능: 제적 상태일 때 '제적' 문자열 반환")
     @Test
-    void returnCrewStatus() {
+    void returnExpelStatus() {
         assertThat(CrewStatus.EXPEL.toString()).isEqualTo("제적");
+    }
+
+    @DisplayName("기능: 면담 상태일 때 '면담' 문자열 반환")
+    @Test
+    void returnMeetingStatus() {
         assertThat(CrewStatus.MEETING.toString()).isEqualTo("면담");
+    }
+
+    @DisplayName("기능: 경고 상태일 때 '경고' 문자열 반환")
+    @Test
+    void returnWarningStatus() {
         assertThat(CrewStatus.WARNING.toString()).isEqualTo("경고");
     }
 }
