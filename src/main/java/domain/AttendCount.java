0dcd62be86028c9290d7f114642da84400285dd0
eoehd1ek <eoehd2ek@gmail.com>
2025-02-24 17:19:47 +0900

    refector: 제적 위험 구하기 로직을 WarningStatus 클래스가 가지도록 수정

--- src/main/java/domain/AttendCount.java
+++ src/main/java/domain/AttendCount.java
@@ -2,24 +2,10 @@ package domain;
 
 public record AttendCount(long attend, long late, long absence) {
     public WarningStatus judgeWarning() {
-        long totalAbsenceCount = calculateTotalAbsenceCount();
-        if (totalAbsenceCount > 5) {
-            return WarningStatus.EXPEL;
-        }
-        if (totalAbsenceCount >= 3) {
-            return WarningStatus.INTERVIEW;
-        }
-        if (totalAbsenceCount >= 2) {
-            return WarningStatus.WARNING;
-        }
-        return WarningStatus.CLEAR;
+        return WarningStatus.judgeWarningStatus(this);
     }
 
     public long calculateRank() {
-        return absence * 3 + late;
-    }
-
-    private long calculateTotalAbsenceCount() {
-        return late / 3 + absence;
+        return absence * WarningStatus.LATE_FER_ABSENCE + late;
     }
 }
