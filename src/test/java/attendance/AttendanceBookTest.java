4f8b4ae1d697bc4b51f0fa5e6acd9a386fb8a584
Sung-june27 <kmymy27@gmail.com>
2025-02-24 20:28:15 +0900

    feat: 닉네임과 시간을 입력하면 출석하는 기능 구현

---
+++ src/test/java/attendance/AttendanceBookTest.java
@@ -0,0 +1,31 @@
+package attendance;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class AttendanceBookTest {
+
+    @Test
+    void 닉네임이_존재하지_않으면_예외를_던진다() {
+        List<LocalDateTime> attendances = List.of(LocalDateTime.of(2024, 12, 13, 9, 59));
+        AttendanceBook attendanceBook = new AttendanceBook(Map.of("훌라", attendances));
+
+        assertThatThrownBy(() -> attendanceBook.attend("모루", LocalDateTime.of(2024, 12, 16, 12, 59)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 닉네임과_시간을_입력하면_출석한다() {
+        List<LocalDateTime> attendances = List.of(LocalDateTime.of(2024, 12, 13, 9, 59));
+        AttendanceBook attendanceBook = new AttendanceBook(Map.of("훌라", attendances));
+
+        final var result = attendanceBook.attend("훌라", LocalDateTime.of(2024, 12, 16, 12, 59));
+
+        assertThat(result).isEqualTo(LocalDateTime.of(2024, 12, 16, 12, 59));
+    }
+}
