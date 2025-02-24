5396f0f446af8e72fc112a24ea83d1083f86631e
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-24 11:39:14 +0900

    feat : 사용자 기능 선택 구현

---
+++ src/main/java/AttendanceSystem.java
@@ -0,0 +1,13 @@
+import controller.AttendanceController;
+import view.InputView;
+import view.OutputView;
+
+public class AttendanceSystem {
+    public static void main(String[] args) {
+        AttendanceController attendanceController = new AttendanceController(
+                new OutputView(),
+                new InputView()
+        );
+        attendanceController.start();
+    }
+}
