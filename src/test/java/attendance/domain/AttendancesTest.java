73b69eef384510a31669deb3f2787dc1eb995a2d
taek2222 <oot309@daum.net>
2025-02-24 19:44:57 +0900

    test(AttendancesTest): 테스트 추가
    - 출석을 삭제하는 테스트를 추가로 검증하도록 추가했습니다.
    
    - 특정 일자까지의 출석 기록을 반환하는 메서드 검증도 추가했습니다.

--- src/test/java/attendance/domain/AttendancesTest.java
+++ src/test/java/attendance/domain/AttendancesTest.java
@@ -3,7 +3,9 @@ package attendance.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -12,7 +14,7 @@ class AttendancesTest {
 
     @DisplayName("출석을 추가할 수 있다.")
     @Test
-    void 출석을_추가할_수_있다() {
+    void 출석을_추가한다() {
         // given
         Attendances attendances = new Attendances();
         LocalDateTime dateTime = LocalDateTime.of(2024, 12, 5, 10, 0);
@@ -23,6 +25,22 @@ class AttendancesTest {
         // than
         assertThat(result.getDateTime()).isEqualTo(dateTime);
         assertThat(result.getState()).isEqualTo(AttendanceStateType.ATTENDANCE);
+    }
+
+    @Test
+    void 출석을_삭제한다() {
+        // given
+        LocalDateTime dateTime = LocalDateTime.of(2024, 12, 5, 10, 0);
+
+        Attendances attendances = new Attendances();
+        attendances.addAttendance(dateTime);
+
+        // when
+        Attendance attendance = attendances.deleteAttendance(dateTime.toLocalDate());
+
+        // then
+        assertThat(attendance.getDateTime())
+                .isEqualTo(dateTime);
     }
 
     @DisplayName("해당 날짜의 출석을 찾을 수 있다.")
@@ -53,5 +71,26 @@ class AttendancesTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> attendances.validateAlreadyAttendance(dateTime.toLocalDate()))
                 .withMessage("[ERROR] 이미 출석을 완료하셨습니다. 수정 기능을 이용해주세요.");
+    }
+
+    @Test
+    void 특정_일자_이전_출석을_반환한다() {
+        // given
+        Attendances attendances = new Attendances();
+
+        LocalDate date = LocalDate.of(2024, 12, 3);
+        List<LocalDateTime> dateTimes = List.of(
+                LocalDateTime.of(2024, 12, 1, 10, 0),
+                LocalDateTime.of(2024, 12, 2, 10, 0),
+                LocalDateTime.of(2024, 12, 3, 10, 0)
+        );
+
+        dateTimes.forEach(attendances::addAttendance);
+
+        // when
+        List<Attendance> result = attendances.getAttendancesBefore(date);
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
     }
 }
