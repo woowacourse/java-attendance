4c6c719029a03432d4274f48160bbc673218e349
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-24 21:16:21 +0900

    feat : 출석 확인 기본 기능 엔드투엔드 구현

---
+++ src/main/java/common/Common.java
@@ -0,0 +1,17 @@
+package common;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Common {
+    public static final TextStyle textStyle = TextStyle.FULL;
+    public static final Locale locale = Locale.of("ko", "KR");
+    public static final LocalDate nowDate = LocalDate.of(2024, 12, 13);
+
+    public static final DateTimeFormatter monthDateDayFormatter = DateTimeFormatter.ofPattern(
+            "MM월 dd일 E요일")
+            .withLocale(Locale.forLanguageTag("ko"));
+    public static final DateTimeFormatter hourMinuteFormatter = DateTimeFormatter.ofPattern("HH:mm");
+}
