6631a17b195342724799dc83249536840b5b111b
AppleMint98 <applemint9821@gmail.com>
2025-02-24 22:06:33 +0900

    fix(FileReader) : 기존 오류 수정 및 안쓰는 메서드 제거

--- src/main/java/attendance/util/FileReader.java
+++ src/main/java/attendance/util/FileReader.java
@@ -6,21 +6,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class FileReader {
-
-    public static Set<String> fileReadCrewNames(String fileName) throws IOException {
-        List<String> fileLines = fileReadLine(fileName);
-        Set<String> crewNames = new HashSet<>();
-        for (String fileLine : fileLines) {
-            String crewName = List.of(fileLine.split(",")).get(0);
-            crewNames.add(crewName);
-        }
-        return crewNames;
-    }
 
     public static List<String> fileReadLine(String fileName) throws IOException {
         try (BufferedReader br = loadFile(fileName)) {
@@ -34,12 +22,11 @@ public class FileReader {
         }
     }
 
-    private static BufferedReader loadFile(String fileName) throws IOException {
-        try (InputStream inputStream = FileReader.class.getClassLoader().getResourceAsStream(fileName)) {
-            if (inputStream == null) {
-                throw new IllegalArgumentException(ErrorMessage.FILE_NOT_PRESENCE.getMessage());
-            }
-            return new BufferedReader(new InputStreamReader(inputStream));
+    private static BufferedReader loadFile(String fileName) {
+        InputStream inputStream = FileReader.class.getClassLoader().getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalArgumentException(ErrorMessage.FILE_NOT_PRESENCE.getMessage());
         }
+        return new BufferedReader(new InputStreamReader(inputStream));
     }
 }
