ce999d7457be355bad5e1198dad2012d6e36cd68
taek2222 <oot309@daum.net>
2025-02-24 20:01:14 +0900

    refactor(AttendanceStatus): 메서드 분리 및 로직 최적화

--- src/main/java/attendance/domain/AttendanceStatus.java
+++ src/main/java/attendance/domain/AttendanceStatus.java
@@ -16,18 +16,8 @@ public class AttendanceStatus implements Comparable<AttendanceStatus> {
     private final AttendanceWarningType warningType;
 
     public AttendanceStatus(final List<Attendance> attendances) {
-        status = new EnumMap<>(AttendanceStateType.class);
-
-        Arrays.stream(values())
-                .forEach(stateType -> {
-                    int stateCount = calculateStateCount(attendances, stateType);
-                    status.put(stateType, stateCount);
-                });
-
-        int expulsion = status.get(EXPULSION);
-        int late = status.get(LATE);
-
-        warningType = find(expulsion, late);
+        status = calculateAttendanceStatus(attendances);
+        warningType = calculateAttendanceRisk();
     }
 
     public static AttendanceStatus of(final List<Attendance> attendances) {
@@ -38,6 +28,17 @@ public class AttendanceStatus implements Comparable<AttendanceStatus> {
         return warningType != NONE;
     }
 
+    @Override
+    public int compareTo(final AttendanceStatus other) {
+        if (warningType == other.warningType) {
+            int thisScore = calculateScore();
+            int otherScore = other.calculateScore();
+
+            return Integer.compare(thisScore, otherScore);
+        }
+        return this.warningType.compareTo(other.warningType);
+    }
+
     public EnumMap<AttendanceStateType, Integer> getStatus() {
         return status;
     }
@@ -46,24 +47,29 @@ public class AttendanceStatus implements Comparable<AttendanceStatus> {
         return warningType;
     }
 
-    @Override
-    public int compareTo(final AttendanceStatus other) {
-        if (warningType == other.warningType) {
-            if (this.calculateScore() == other.calculateScore()) {
-                return 0;
-            }
-            return Integer.compare(other.calculateScore(), this.calculateScore());
-        }
-        return this.warningType.compareTo(other.warningType);
-    }
-
-    private int calculateScore() {
-        return status.get(EXPULSION) * 3 + status.get(LATE);
+    private EnumMap<AttendanceStateType, Integer> calculateAttendanceStatus(final List<Attendance> attendances) {
+        EnumMap<AttendanceStateType, Integer> status = new EnumMap<>(AttendanceStateType.class);
+        Arrays.stream(values())
+                .forEach(stateType -> {
+                    int stateCount = calculateStateCount(attendances, stateType);
+                    status.put(stateType, stateCount);
+                });
+        return status;
     }
 
     private int calculateStateCount(final List<Attendance> attendances, final AttendanceStateType status) {
         return (int) attendances.stream()
                 .filter(attendance -> attendance.hasState(status))
                 .count();
+    }
+
+    private AttendanceWarningType calculateAttendanceRisk() {
+        int expulsion = status.get(EXPULSION);
+        int late = status.get(LATE);
+        return find(expulsion, late);
+    }
+
+    private int calculateScore() {
+        return status.get(EXPULSION) * 3 + status.get(LATE);
     }
 }
