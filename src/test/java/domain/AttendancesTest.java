631bdeaac0e79b152daeb42dad61f1dc7e9a33e0
geonwoo <kysub99@naver.com>
2025-02-24 16:54:38 +0900

    test(AttendancesTest): findDangerCrews 테스트 작성

--- src/test/java/domain/AttendancesTest.java
+++ src/test/java/domain/AttendancesTest.java
@@ -3,10 +3,12 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AttendancesTest {
     @Test
@@ -36,5 +38,42 @@ class AttendancesTest {
         assertThatThrownBy(() -> attendances.findAttendanceByName("차니"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    void findDangerCrewsTest() {
+        //given
+        Crew crew1 = Crew.of("경고");
+        Crew crew2 = Crew.of("면담");
+        Crew crew3 = Crew.of("정상");
+
+        CheckInTimes checkInTimes1 = CheckInTimes.of(List.of());
+        CheckInTimes checkInTimes2 = CheckInTimes.of(List.of());
+        CheckInTimes checkInTimes3 = CheckInTimes.of(List.of());
+
+        // 2 결석
+        Attendance attendance1 = Attendance.of(crew1, checkInTimes1);
+        attendance1.checkIn(LocalDateTime.of(2024, 12, 3, 15, 30));
+        attendance1.checkIn(LocalDateTime.of(2024, 12, 4, 16, 30));
+        // 3 결석
+        Attendance attendance2 = Attendance.of(crew2, checkInTimes2);
+        attendance2.checkIn(LocalDateTime.of(2024, 12, 3, 15, 30));
+        attendance2.checkIn(LocalDateTime.of(2024, 12, 4, 16, 30));
+        attendance2.checkIn(LocalDateTime.of(2024, 12, 5, 16, 30));
+        // 0 지각, 0 결석
+        Attendance attendance3 = Attendance.of(crew3, checkInTimes3);
+        attendance3.checkIn(LocalDateTime.of(2024, 12, 3, 9, 30));
+
+        Attendances attendances = Attendances.of(List.of(attendance1, attendance2, attendance3));
+
+        //when
+        List<Attendance> dangerCrews = attendances.findDangerCrews();
+
+        //then
+        // assertThat(dangerCrews).hasSize(3);
+        assertTrue(dangerCrews.contains(attendance1));
+        assertTrue(dangerCrews.contains(attendance2));
+        assertTrue(dangerCrews.contains(attendance3));
+        //assertFalse(dangerCrews.contains(attendance2));
     }
 }
