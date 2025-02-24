59e347d02acaeea0fdb0021433278efc0a1065a6
Jin409 <sualng123@gmail.com>
2025-02-24 20:40:05 +0900

    refactor: Scanner 를 매 요청마다 선언하지 않도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,31 +5,27 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static FeatureOption readOption() {
-        // TODO: 날짜 출력
         System.out.println("기능을 선택해 주세요.");
         Arrays.stream(FeatureOption.values()).forEach(featureOption -> System.out.println(featureOption.toString()));
-        return FeatureOption.getFunction(scan());
+        return FeatureOption.getFunction(SCANNER.next());
     }
 
     public static String readNickname() {
         System.out.println("닉네임을 입력해 주세요.");
-        return scan();
+        return SCANNER.next();
     }
 
     public static AttendanceTimeDto readAttendanceTime() {
         System.out.println("등교 시간을 입력해 주세요.");
-        return Parser.parseAttendanceTime(scan());
+        return Parser.parseAttendanceTime(SCANNER.next());
     }
 
     public static int readToday() {
         System.out.println("오늘 날짜를 입력해주세요.");
-        return Parser.parseInteger(scan());
-    }
-
-
-    private static String scan() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.next();
+        return Parser.parseInteger(SCANNER.next());
     }
 }
