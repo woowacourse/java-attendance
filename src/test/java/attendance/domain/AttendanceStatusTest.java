503447b18da86b52c7bb88c92a811d8665a1b4af
taek2222 <oot309@daum.net>
2025-02-24 20:39:04 +0900

    test(AttendanceStatusTest): 테스트 추가
    - 비교 결과를 반환하는 값을 테스트 하는 것을 추가했습니다.

--- src/test/java/attendance/domain/AttendanceStatusTest.java
+++ src/test/java/attendance/domain/AttendanceStatusTest.java
@@ -1,6 +1,5 @@
 package attendance.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -11,16 +10,17 @@ import java.util.List;
 import static attendance.domain.AttendanceStateType.ABSENCE;
 import static attendance.domain.AttendanceStateType.ATTENDANCE;
 import static attendance.domain.AttendanceStateType.LATE;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AttendanceStatusTest {
 
     @Test
-    void 전날까지의_출석_기록으로_객체를_생성한다() {
+    void 출석_기록으로_객체를_생성한다() {
         // given
         List<Attendance> attendances = createAttendances(List.of(
                 LocalDateTime.of(2024, 12, 3, 10, 0),
-                LocalDateTime.of(2024, 12, 3, 10, 6),
-                LocalDateTime.of(2024, 12, 3, 10, 31)
+                LocalDateTime.of(2024, 12, 4, 10, 6),
+                LocalDateTime.of(2024, 12, 5, 10, 31)
         ));
 
         // when
@@ -28,9 +28,129 @@ class AttendanceStatusTest {
         EnumMap<AttendanceStateType, Integer> result = attendanceStatus.getStatus();
 
         // then
-        Assertions.assertThat(result.get(ABSENCE)).isEqualTo(1);
-        Assertions.assertThat(result.get(LATE)).isEqualTo(1);
-        Assertions.assertThat(result.get(ATTENDANCE)).isEqualTo(1);
+        assertThat(result.get(ABSENCE)).isEqualTo(1);
+        assertThat(result.get(LATE)).isEqualTo(1);
+        assertThat(result.get(ATTENDANCE)).isEqualTo(1);
+    }
+
+    @Test
+    void 위험자_상태가_높은_경우_양수를_반환한다() {
+        // given
+        List<Attendance> origin = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 18, 0),
+                LocalDateTime.of(2024, 12, 4, 18, 0),
+                LocalDateTime.of(2024, 12, 5, 18, 0)
+        ));
+
+        List<Attendance> compared = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 10, 0),
+                LocalDateTime.of(2024, 12, 4, 10, 0),
+                LocalDateTime.of(2024, 12, 5, 10, 0)
+        ));
+
+        // when
+        AttendanceStatus originStatus = new AttendanceStatus(origin);
+        AttendanceStatus comparedStatus = new AttendanceStatus(compared);
+
+        // then
+        assertThat(originStatus.compareTo(comparedStatus))
+                .isPositive();
+    }
+
+    @Test
+    void 위험자_상태가_낮은_경우_음수를_반환한다() {
+        // given
+        List<Attendance> origin = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 10, 0),
+                LocalDateTime.of(2024, 12, 4, 10, 0),
+                LocalDateTime.of(2024, 12, 5, 10, 0)
+        ));
+
+        List<Attendance> compared = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 18, 0),
+                LocalDateTime.of(2024, 12, 4, 18, 0),
+                LocalDateTime.of(2024, 12, 5, 18, 0)
+        ));
+
+        // when
+        AttendanceStatus originStatus = new AttendanceStatus(origin);
+        AttendanceStatus comparedStatus = new AttendanceStatus(compared);
+
+        // then
+        assertThat(originStatus.compareTo(comparedStatus))
+                .isNegative();
+    }
+
+    @Test
+    void 출결에_결석_및_지각이_더_적은_경우_양수를_반환한다() {
+        // given
+        List<Attendance> origin = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 18, 0),
+                LocalDateTime.of(2024, 12, 4, 18, 0),
+                LocalDateTime.of(2024, 12, 5, 10, 0)
+        ));
+
+        List<Attendance> compared = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 18, 0),
+                LocalDateTime.of(2024, 12, 4, 18, 0),
+                LocalDateTime.of(2024, 12, 5, 10, 6)
+        ));
+
+        // when
+        AttendanceStatus originStatus = new AttendanceStatus(origin);
+        AttendanceStatus comparedStatus = new AttendanceStatus(compared);
+
+        // then
+        assertThat(originStatus.compareTo(comparedStatus))
+                .isPositive();
+    }
+
+    @Test
+    void 출결에_결석_및_지각이_더_많은_경우_음수를_반환한다() {
+        // given
+        List<Attendance> origin = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 18, 0),
+                LocalDateTime.of(2024, 12, 4, 18, 0),
+                LocalDateTime.of(2024, 12, 5, 10, 6)
+        ));
+
+        List<Attendance> compared = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 18, 0),
+                LocalDateTime.of(2024, 12, 4, 18, 0),
+                LocalDateTime.of(2024, 12, 5, 10, 0)
+        ));
+
+        // when
+        AttendanceStatus originStatus = new AttendanceStatus(origin);
+        AttendanceStatus comparedStatus = new AttendanceStatus(compared);
+
+        // then
+        assertThat(originStatus.compareTo(comparedStatus))
+                .isNegative();
+    }
+
+    @Test
+    void 출결_상황이_동일한_경우_0을_반환한다() {
+        // given
+        List<Attendance> origin = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 10, 0),
+                LocalDateTime.of(2024, 12, 4, 10, 0),
+                LocalDateTime.of(2024, 12, 5, 10, 0)
+        ));
+
+        List<Attendance> compared = createAttendances(List.of(
+                LocalDateTime.of(2024, 12, 3, 10, 0),
+                LocalDateTime.of(2024, 12, 4, 10, 0),
+                LocalDateTime.of(2024, 12, 5, 10, 0)
+        ));
+
+        // when
+        AttendanceStatus originStatus = new AttendanceStatus(origin);
+        AttendanceStatus comparedStatus = new AttendanceStatus(compared);
+
+        // then
+        assertThat(originStatus.compareTo(comparedStatus))
+                .isZero();
     }
 
     private List<Attendance> createAttendances(List<LocalDateTime> dateTimes) {
