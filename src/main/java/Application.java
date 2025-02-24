f418c946fe11c797f58e68f172a2592008581cf6
gabean13 <choegabin171@gmail.com>
2025-02-24 19:20:07 +0900

    remove : 전체 파일 삭제

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,17 +1,4 @@
-import controller.Controller;
-import service.DateValidator;
-import view.InputValidator;
-import view.InputView;
-import view.OutputView;
-
 public class Application {
     public static void main(String[] args) {
-        InputValidator inputValidator = new InputValidator();
-        InputView inputView = new InputView(inputValidator);
-        OutputView outputView = new OutputView();
-        DateValidator dateValidator = new DateValidator();
-        Controller controller = new Controller(dateValidator, inputView, outputView);
-
-        controller.run();
     }
 }
