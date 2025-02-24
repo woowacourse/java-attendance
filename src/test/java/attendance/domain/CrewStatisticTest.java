fe8bfca34e4944330ba5ccfebb3b3007ad218f14
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-24 11:09:41 +0900

    refactor(CrewStatisticTest): 상수와 초기화 설정 추가 및 테스트 케이스 분리

--- src/test/java/attendance/domain/CrewStatisticTest.java
+++ src/test/java/attendance/domain/CrewStatisticTest.java
@@ -2,75 +2,82 @@ package attendance.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 public class CrewStatisticTest {
-    private static final int ATTENDANCE_TOTAL_DAYS = 15;
+    private static final LocalDate START_DATE = LocalDate.of(2025, 2, 1);
+    private static final LocalDate END_DATE = LocalDate.of(2025, 2, LocalDate.now().getDayOfMonth());
+    private static final Set<DayOfWeek> WEEKEND = Set.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+    private static final int HOLIDAY_COUNT = 0;
+
+    private static final List<List<String>> ATTENDANCE_RECORDS_NO_ABSENT = List.of(
+            List.of("쿠키", "2025-02-19 10:08"),
+            List.of("쿠키", "2025-02-18 10:00"),
+            List.of("쿠키", "2025-02-17 13:03"),
+            List.of("쿠키", "2025-02-14 10:02"),
+            List.of("쿠키", "2025-02-13 10:07"),
+            List.of("쿠키", "2025-02-12 10:01"),
+            List.of("쿠키", "2025-02-11 10:00"),
+            List.of("쿠키", "2025-02-10 13:09")
+    );
 
     private final Crews crews = new Crews();
     private final Attendances attendances = new Attendances();
     private final Crew crew = new Crew("쿠키");
     private CrewStatistic crewStatistic;
+    private int workingDays;
 
-    void setUp(List<List<String>> attendanceRecords) {
-        crews.initCrews(attendanceRecords);
-        attendances.initAttendances(crews, attendanceRecords);
+    @BeforeEach
+    void setUp() {
+        crews.initCrews(ATTENDANCE_RECORDS_NO_ABSENT);
+        attendances.initAttendances(crews, ATTENDANCE_RECORDS_NO_ABSENT);
 
         List<Attendance> crewAttendances = attendances.findCrewAttendances(crew);
-
         crewStatistic = new CrewStatistic(crew, crewAttendances);
-        crewStatistic.initCrewsStatus();
-        crewStatistic.calculatePenalty();
+        crewStatistic.checkCrewStatistic();
+
+        calculateWorkingDays();
     }
 
-    @DisplayName("기능: 크루별 출석 기록 및 출결 횟수 정보 확인")
-    @MethodSource("provideAttendanceRecords")
-    @ParameterizedTest
-    void checkCrewAttendanceStatistics(List<List<String>> attendanceRecords) {
-        setUp(attendanceRecords);
+    private void calculateWorkingDays() {
+        workingDays = (int) START_DATE.datesUntil(END_DATE)
+                .map(LocalDate::getDayOfWeek)
+                .filter(date -> !WEEKEND.contains(date))
+                .count();
+        workingDays -= HOLIDAY_COUNT;
+    }
 
-        assertThat(crewStatistic.crewAttendanceHistoryInfo().size()).isEqualTo(ATTENDANCE_TOTAL_DAYS);
+    @Test
+    @DisplayName("기능: 크루의 전체 출석 기록 개수를 확인")
+    void checkTotalAttendanceDays() {
+        assertThat(crewStatistic.crewAttendanceHistoryInfo().size())
+                .isEqualTo(workingDays);
+    }
 
+    @Test
+    @DisplayName("기능: 크루의 출결 횟수 및 상태 정보 계산 확인")
+    void calculateCrewStatisticStatusInfo() {
         String safeCount = String.valueOf(5);
         String lateCount = String.valueOf(3);
-        String absentCount = String.valueOf(ATTENDANCE_TOTAL_DAYS - attendanceRecords.size() + 1);
-        List<String> expectedStatisticInfo = List.of(safeCount, lateCount, absentCount, "제적");
+        String absentCount = String.valueOf(workingDays - ATTENDANCE_RECORDS_NO_ABSENT.size() + 1);
 
-        assertThat(crewStatistic.crewStatisticStatusInfo()).isEqualTo(expectedStatisticInfo);
+        assertThat(crewStatistic.crewStatisticStatusInfo())
+                .isEqualTo(List.of(safeCount, lateCount, absentCount, "제적"));
     }
 
-    @DisplayName("기능: 제적 위험자 정보 확인")
-    @MethodSource("provideAttendanceRecords")
-    @ParameterizedTest
-    void test(List<List<String>> attendanceRecords) {
-        setUp(attendanceRecords);
-
-        String absentCount = String.valueOf(ATTENDANCE_TOTAL_DAYS - attendanceRecords.size() + 1);
+    @Test
+    @DisplayName("기능: 제적 위험자인 크루들 정보 계산 확인")
+    void calculateExpelExpectedCrewsInfo() {
+        String absentCount = String.valueOf(workingDays - ATTENDANCE_RECORDS_NO_ABSENT.size() + 1);
         String lateCount = String.valueOf(3);
-        List<String> expectedExpelInfo = List.of("쿠키", absentCount, lateCount, "제적");
 
-        assertThat(crewStatistic.crewExpelExpectedInfo()).isEqualTo(expectedExpelInfo);
-    }
-
-    private static Stream<Arguments> provideAttendanceRecords() {
-        return Stream.of(
-                Arguments.of(
-                        List.of(
-                                List.of("쿠키", "2025-02-19 10:08"),
-                                List.of("쿠키", "2025-02-18 10:00"),
-                                List.of("쿠키", "2025-02-17 13:03"),
-                                List.of("쿠키", "2025-02-14 10:02"),
-                                List.of("쿠키", "2025-02-13 10:07"),
-                                List.of("쿠키", "2025-02-12 10:01"),
-                                List.of("쿠키", "2025-02-11 10:00"),
-                                List.of("쿠키", "2025-02-10 13:09")
-                        )
-                )
-        );
+        assertThat(crewStatistic.crewExpelExpectedInfo())
+                .isEqualTo(List.of("쿠키", absentCount, lateCount, "제적"));
     }
 }
