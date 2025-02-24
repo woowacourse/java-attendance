e196bb312d416520ffba2c606b334e73cfa6cf0d
ChoiDongJoon <adong0808@naver.com>
2025-02-24 22:58:21 +0900

    feat: 출석하면 Attendance를 리턴

--- src/main/java/attendance/Attendance.java
+++ src/main/java/attendance/Attendance.java
@@ -2,33 +2,48 @@ package attendance;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 
 public class Attendance {
 
-    public static final int OPEN_HOUR = 8;
-    public static final int CLOSE_HOUR = 23;
+    private static final int OPEN_HOUR = 8;
+    private static final int CLOSE_HOUR = 23;
+    private static final List<DayOfWeek> WEEKEND = List.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+    private static final int CHRISTMAS = 25;
 
-    public String attend(String timeInput, LocalDate today) {
-        List<DayOfWeek> weekend = List.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
-        checkHoliday(today, weekend);
+    private LocalDateTime attendanceDateTime;
+    private String attendanceStatus;
+
+    public Attendance() {
+    }
+
+    public Attendance(LocalDateTime attendanceDateTime, String attendanceStatus) {
+        this.attendanceDateTime = attendanceDateTime;
+        this.attendanceStatus = attendanceStatus;
+    }
+
+    public Attendance attend(String timeInput, LocalDate today) {
+
+        checkHoliday(today);
         if(Objects.equals("--:--", timeInput)) {
-            return "결석";
+            String attendanceStatus = "결석";
+            return new Attendance(today.atStartOfDay(), attendanceStatus);
         }
         int startHour = checkStartHour(today);
 
-        String[] time = timeInput.split(":");
-        int hour = Integer.parseInt(time[0]);
-        int minute = Integer.parseInt(time[1]);
+        LocalTime attendanceTime = formatTimeInput(timeInput);
 
-        checkCampusOpen(hour);
+        checkCampusOpen(attendanceTime.getHour());
 
-        return checkAttendanceStatus(hour, minute, startHour);
+        return new Attendance(LocalDateTime.of(today, attendanceTime), checkAttendanceStatus(attendanceTime, startHour));
     }
 
-    private void checkHoliday(LocalDate today, List<DayOfWeek> weekend) {
-        if(weekend.contains(today.getDayOfWeek()) || today.getDayOfMonth() == 25) {
+    private void checkHoliday(LocalDate today) {
+        if(WEEKEND.contains(today.getDayOfWeek()) || today.getDayOfMonth() == CHRISTMAS) {
             throw new IllegalArgumentException("등교일이 아닙니다.");
         }
     }
@@ -41,17 +56,22 @@ public class Attendance {
         return startHour;
     }
 
+    private LocalTime formatTimeInput(String timeInput) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("[HH:mm][HH:m][H:mm][H:m]");
+        return LocalTime.parse(timeInput, formatter);
+    }
+
     private void checkCampusOpen(int hour) {
         if(hour < OPEN_HOUR || hour >= CLOSE_HOUR) {
             throw new IllegalArgumentException("캠퍼스 운영 시간은 8:00 ~ 23:00입니다.");
         }
     }
 
-    private String checkAttendanceStatus(int hour, int minute, int startHour) {
-        if(hour > startHour || minute > 30) {
+    private String checkAttendanceStatus(LocalTime attendanceTime, int startHour) {
+        if(attendanceTime.getHour() > startHour || attendanceTime.getMinute() > 30) {
             return "결석";
         }
-        if(minute > 5) {
+        if(attendanceTime.getMinute() > 5) {
             return "지각";
         }
         return "출석";
