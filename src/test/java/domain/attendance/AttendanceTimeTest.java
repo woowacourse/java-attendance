4bc0954d2ef7e2fd51aaa0d51339caac49ed9f38
ppparkta <pppppark2@gmail.com>
2025-02-24 16:29:53 +0900

    refactor: 한 번에 하나의 테스트만 확인한다

--- src/test/java/domain/attendance/AttendanceTimeTest.java
+++ src/test/java/domain/attendance/AttendanceTimeTest.java
@@ -4,23 +4,29 @@ import java.time.LocalDateTime;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AttendanceTimeTest {
     @DisplayName("월요일은 13시 5분까지 출석해야 출석으로 인정된다")
     @Test
     void test() {
         boolean isAttendanceSuccess = AttendanceTime.isAttendance(1, LocalDateTime.of(2025, 2, 17, 13, 5));
-        boolean isAttendanceFail = AttendanceTime.isAttendance(1, LocalDateTime.of(2025, 2, 17, 13, 6));
         Assertions.assertThat(isAttendanceSuccess).isTrue();
+    }
+
+    @DisplayName("월요일은 13시 6분부터 13시 30분까지 출석하면 지각이다")
+    @ParameterizedTest
+    @ValueSource(ints = {6, 30})
+    void test(int minute) {
+        boolean isAttendanceFail = AttendanceTime.isAttendance(1, LocalDateTime.of(2025, 2, 17, 13, minute));
         Assertions.assertThat(isAttendanceFail).isFalse();
     }
 
     @DisplayName("월요일은 13시 30분 초과 출석하면 결석이다")
     @Test
     void test2() {
-        boolean isAbsenceFalse = AttendanceTime.isAbsence(1, LocalDateTime.of(2025, 2, 17, 13, 30));
         boolean isAbsenceTrue = AttendanceTime.isAbsence(1, LocalDateTime.of(2025, 2, 17, 13, 31));
-        Assertions.assertThat(isAbsenceFalse).isFalse();
         Assertions.assertThat(isAbsenceTrue).isTrue();
     }
 
@@ -28,17 +34,13 @@ class AttendanceTimeTest {
     @Test
     void test3() {
         boolean isAttendanceSuccess = AttendanceTime.isAttendance(2, LocalDateTime.of(2025, 2, 18, 10, 5));
-        boolean isAttendanceFail = AttendanceTime.isAttendance(2, LocalDateTime.of(2025, 2, 18, 10, 6));
         Assertions.assertThat(isAttendanceSuccess).isTrue();
-        Assertions.assertThat(isAttendanceFail).isFalse();
     }
 
     @DisplayName("월요일을 제외한 평일은 10시 30분 초과 출석하면 결석이다")
     @Test
     void test4() {
-        boolean isAbsenceFalse = AttendanceTime.isAbsence(2, LocalDateTime.of(2025, 2, 18, 10, 30));
         boolean isAbsenceTrue = AttendanceTime.isAbsence(2, LocalDateTime.of(2025, 2, 18, 10, 31));
-        Assertions.assertThat(isAbsenceFalse).isFalse();
         Assertions.assertThat(isAbsenceTrue).isTrue();
     }
 
