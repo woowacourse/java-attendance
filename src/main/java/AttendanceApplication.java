f3d4a4b9b63672e7d1e1934450d8669dbd1da091
ppparkta <pppppark2@gmail.com>
2025-02-24 20:56:23 +0900

    refactor: 변수명 클래스에 맞게 수정

--- src/main/java/AttendanceApplication.java
+++ src/main/java/AttendanceApplication.java
@@ -5,8 +5,8 @@ import domain.CrewFactory;
 public class AttendanceApplication {
     public static void main(String[] args) {
         AttendanceFileInputView fileInputView = new AttendanceFileInputView();
-        CrewFactory attendanceService = new CrewFactory();
-        AttendanceController attendanceController = new AttendanceController(fileInputView, attendanceService);
+        CrewFactory crewFactory = new CrewFactory();
+        AttendanceController attendanceController = new AttendanceController(fileInputView, crewFactory);
         attendanceController.run();
     }
 }
