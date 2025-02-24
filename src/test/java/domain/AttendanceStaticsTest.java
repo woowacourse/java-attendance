67a25f3659eeef513f77a4ffaff18d912978e50a
GiSeok Kang <ime.rokz@gmail.com>
2025-02-24 15:53:21 +0900

    test: AbsentPolicy 테스트 AttendanceStatics 테스트로 수정

--- src/test/java/domain/AttendanceStaticsTest.java
+++ src/test/java/domain/AttendanceStaticsTest.java
@@ -3,12 +3,12 @@ package domain;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import view.AbsentPolicy;
 
-class AbsentPolicyTest {
+class AttendanceStaticsTest {
 
     public static Stream<Arguments> getNoneCounts() {
         return Stream.of(
@@ -50,13 +50,34 @@ class AbsentPolicyTest {
         );
     }
 
+    private static Stream<Arguments> getAttendLateAbsentCount() {
+        return Stream.of(
+                Arguments.of(3, 0, 0),
+                Arguments.of(3, 0, 1),
+                Arguments.of(3, 3, 0),
+                Arguments.of(3, 2, 1)
+        );
+    }
+
+    private static Stream<Arguments> getAttendLateAbsentCountForRiskOfExpulsion() {
+        return Stream.of(
+                Arguments.of(3, 0, 2),
+                Arguments.of(3, 3, 1),
+                Arguments.of(3, 3, 2),
+                Arguments.of(3, 2, 4)
+        );
+    }
+
     @ParameterizedTest
     @DisplayName("결석 횟수와 지각 횟수에 따라 대상자가 아님을 확인한다.")
     @MethodSource("getNoneCounts")
     void absent_count_late_count_then_none(int absentCount, int lateCount) {
+        // given
+        AttendanceStatics attendanceStatics = new AttendanceStatics(0, lateCount, absentCount);
+
         // when
         // then
-        Assertions.assertThat(AbsentPolicy.calculateAbsentPolicy(absentCount, lateCount))
+        Assertions.assertThat(attendanceStatics.calculateAbsentPolicy())
                 .isEqualTo(AbsentPolicy.NONE);
     }
 
@@ -64,9 +85,12 @@ class AbsentPolicyTest {
     @DisplayName("결석 횟수와 지각 횟수에 따라 경고 대상자임을 계산한다.")
     @MethodSource("getWarningCounts")
     void absent_count_late_count_then_warning(int absentCount, int lateCount) {
+        // given
+        AttendanceStatics attendanceStatics = new AttendanceStatics(0, lateCount, absentCount);
+
         // when
         // then
-        Assertions.assertThat(AbsentPolicy.calculateAbsentPolicy(absentCount, lateCount))
+        Assertions.assertThat(attendanceStatics.calculateAbsentPolicy())
                 .isEqualTo(AbsentPolicy.WARNING);
     }
 
@@ -74,9 +98,12 @@ class AbsentPolicyTest {
     @DisplayName("결석 횟수와 지각 횟수에 따라 면담 대상자임을 계산한다.")
     @MethodSource("getInterviewCounts")
     void absent_count_late_count_then_interview(int absentCount, int lateCount) {
+        // given
+        AttendanceStatics attendanceStatics = new AttendanceStatics(0, lateCount, absentCount);
+
         // when
         // then
-        Assertions.assertThat(AbsentPolicy.calculateAbsentPolicy(absentCount, lateCount))
+        Assertions.assertThat(attendanceStatics.calculateAbsentPolicy())
                 .isEqualTo(AbsentPolicy.INTERVIEW);
     }
 
@@ -84,29 +111,38 @@ class AbsentPolicyTest {
     @DisplayName("결석 횟수와 지각 횟수에 따라 제적 대상자임을 계산한다.")
     @MethodSource("getExpulsionCounts")
     void absent_count_late_count_then_expulsion(int absentCount, int lateCount) {
+        // given
+        AttendanceStatics attendanceStatics = new AttendanceStatics(0, lateCount, absentCount);
+
         // when
         // then
-        Assertions.assertThat(AbsentPolicy.calculateAbsentPolicy(absentCount, lateCount))
+        Assertions.assertThat(attendanceStatics.calculateAbsentPolicy())
                 .isEqualTo(AbsentPolicy.EXPULSION);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("제적 위험자가 아님을 확인한다.")
-    void check_not_risk_of_expulsion() {
-        Assertions.assertThat(AbsentPolicy.isRiskOfExpulsion(AbsentPolicy.EXPULSION))
-                .isFalse();
-        Assertions.assertThat(AbsentPolicy.isRiskOfExpulsion(AbsentPolicy.NONE))
+    @MethodSource("getAttendLateAbsentCount")
+    void check_not_risk_of_expulsion(int attendCount, int lateCount, int absentCount) {
+        // given
+        AttendanceStatics attendanceStatics = new AttendanceStatics(attendCount, lateCount, absentCount);
+
+        // when
+        // then
+        Assertions.assertThat(attendanceStatics.isRiskOfExpulsion())
                 .isFalse();
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("제적 위험자임을 확인한다.")
-    void check_risk_of_expulsion() {
+    @MethodSource("getAttendLateAbsentCountForRiskOfExpulsion")
+    void check_risk_of_expulsion(int attendCount, int lateCount, int absentCount) {
+        // given
+        AttendanceStatics attendanceStatics = new AttendanceStatics(attendCount, lateCount, absentCount);
+
         // when
         // then
-        Assertions.assertThat(AbsentPolicy.isRiskOfExpulsion(AbsentPolicy.INTERVIEW))
-                .isTrue();
-        Assertions.assertThat(AbsentPolicy.isRiskOfExpulsion(AbsentPolicy.WARNING))
+        Assertions.assertThat(attendanceStatics.isRiskOfExpulsion())
                 .isTrue();
     }
 }
