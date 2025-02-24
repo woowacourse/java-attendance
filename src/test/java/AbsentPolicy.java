57ac67baae9ad57fee2cdf92853e5b25bc013849
sonjh919 <kingjh1125@gmail.com>
2025-02-24 16:41:01 +0900

    test(refactor): 결석 정책 테스트 리팩터링
    - 공휴일에 출석하려고 하는 경우 예외가 발생한다

--- src/test/java/AbsentPolicy.java
+++ src/test/java/AbsentPolicy.java
@@ -5,6 +5,8 @@ import java.time.LocalTime;
 
 public class AbsentPolicy {
 
+    private static final LocalDate CHRISTMAS = LocalDate.of(2024, 12, 25);
+
     public void validateIsWeekend(LocalDate attendanceDate) {
         DayOfWeek attendanceDayOfWeek = attendanceDate.getDayOfWeek();
 
@@ -14,7 +16,7 @@ public class AbsentPolicy {
     }
 
     public void validateIsHoliday(LocalDate attendanceDate) {
-        if(attendanceDate.equals(LocalDate.of(2024, 12, 25))){
+        if(attendanceDate.equals(CHRISTMAS)){
             throw new IllegalArgumentException("[ERROR] 공휴일에는 출석할 수 없습니다");
         }
     }
