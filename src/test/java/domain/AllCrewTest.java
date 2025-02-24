eee449136a7938b60ccb868d00260be6ffd11e2c
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-24 17:44:00 +0900

    test(domain) : AllCrew 정렬 기능 테스트
    - 결석+3회지각 기준으로 내림차순 정렬
    - 이름 오름차순 정렬

--- src/test/java/domain/AllCrewTest.java
+++ src/test/java/domain/AllCrewTest.java
@@ -8,29 +8,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AllCrewTest {
-    @DisplayName("모든 제적 위험자 조회 결과 출력")
+
+    @DisplayName("결석 내림차순 정렬")
     @Test
-    void test7() {
+    void test1() {
+        // given
         AllCrew allCrew = new AllCrew();
         Crew crew1 = new Crew("빙티");
         Crew crew2 = new Crew("이든");
-
-        allCrew.addCrew(crew1);
+        // 이든, 빙티 순으로 삽입
         allCrew.addCrew(crew2);
+        allCrew.addCrew(crew1);
 
-
-        allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 2, 13,0));  // 출석
+        // 2일 결석
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 3, 9,58));  // 출석
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 4, 10,2));  // 출석
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 5, 10,6));  // 지각
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 6, 10,1));  //  출석
-        // 9일 결석
+        // 7일 결석
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 10, 10,8)); // 지각
         // 11, 12 결석
-        // 결석 3회, 지각 2회
+        // 결석 4회, 지각 2회 => 4결석
 
-
-        // 2 => 결석
+        allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 2, 10,31)); // 출석 (월요일)
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 3, 10,7)); // 지각
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 4, 10,8)); // 지각
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 5, 10,29)); // 지각
@@ -38,41 +38,38 @@ class AllCrewTest {
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 9, 10,31)); // 출석 (월요일)
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 10, 10,2));//출석
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 11, 10,1));//출석
-        // 12 => 결석
-        // 12일까지 : 2결석 4지각
+        // 12 일 결석
+        // 1결석 4지각 => 2결석
+        allCrew.updateAbsentHistory(LocalDate.of(2024, 12, 12));
 
+        // when
+        allCrew.sortAllCrewOrderByWarningInfo();
 
-        // 결과
-        assertThat(allCrew.printAllCrewWarningInfo(LocalDate.of(2024, 12, 12)))
-                .isEqualTo("""
-                        - 빙티: 결석 3회, 지각 2회 (면담)
-                        - 이든: 결석 2회, 지각 4회 (면담)
-                        """);
+        // then
+        assertThat(allCrew.getAllWarningCrew()).extracting(Crew::getName).containsExactly("빙티", "이든");
     }
 
-    @DisplayName("모든 제적 위험자 조회 결과 출력2 - 정렬")
+    @DisplayName("이름 오름차순 정렬")
     @Test
-    void test8() {
+    void test2() {
+        // given
         AllCrew allCrew = new AllCrew();
         Crew crew1 = new Crew("빙티");
         Crew crew2 = new Crew("이든");
-
+        // 이든, 빙티 순으로 삽입
         allCrew.addCrew(crew2);
         allCrew.addCrew(crew1);
-
-
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 2, 13,0));  // 출석
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 3, 9,58));  // 출석
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 4, 10,2));  // 출석
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 5, 10,6));  // 지각
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 6, 10,1));  //  출석
-        // 9일 결석
+        // 7일 결석
         allCrew.addCrewAttendanceByName("빙티", LocalDateTime.of(2024, 12, 10, 10,8)); // 지각
         // 11, 12 결석
         // 결석 3회, 지각 2회
 
-
-        // 2 => 결석
+        // 2일 결석
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 3, 10,7)); // 지각
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 4, 10,8)); // 지각
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 5, 10,29)); // 지각
@@ -80,15 +77,15 @@ class AllCrewTest {
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 9, 10,31)); // 출석 (월요일)
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 10, 10,2));//출석
         allCrew.addCrewAttendanceByName("이든", LocalDateTime.of(2024, 12, 11, 10,1));//출석
-        // 12 => 결석
-        // 12일까지 : 2결석 4지각
+        // 12 일 결석
+        // 2결석 4지각
+        allCrew.updateAbsentHistory(LocalDate.of(2024, 12, 12));
 
+        // when
+        allCrew.sortAllCrewOrderByWarningInfo();
 
-        // 결과
-        assertThat(allCrew.printAllCrewWarningInfo(LocalDate.of(2024, 12, 12)))
-                .isEqualTo("""
-                        - 빙티: 결석 3회, 지각 2회 (면담)
-                        - 이든: 결석 2회, 지각 4회 (면담)
-                        """);
+        // then
+        assertThat(allCrew.getAllWarningCrew()).extracting(Crew::getName).containsExactly("빙티", "이든");
     }
+
 }
