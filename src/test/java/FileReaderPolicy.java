8c947aa5477c48cee8cf8932494e032bf9090da6
sonjh919 <kingjh1125@gmail.com>
2025-02-24 12:56:07 +0900

    test(refactor): 파일 입출력 테스트 리팩터링
    - 파일 위치가 올바르지 않으면 예외가 발생한다

---
+++ src/test/java/FileReaderPolicy.java
@@ -0,0 +1,20 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileReaderPolicy {
+    private static final String FILE_PATH = "src/main/resources/attendances.csv";
+
+    FileReader fileReader;
+
+    public FileReaderPolicy() {
+        this.fileReader = readFile();
+    }
+
+    private FileReader readFile() {
+        try {
+            return new FileReader(FILE_PATH);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("[ERROR] 파일 위치가 올바르지 않습니다");
+        }
+    }
+}
