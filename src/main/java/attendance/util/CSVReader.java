8527bcbdc22dee78c17ce9caec1db4de3d00f1c9
MinJun Choi <mj043000@naver.com>
2025-02-24 17:53:41 +0900

    refactor: 메소드 구분 및 구분자 구분

--- src/main/java/attendance/util/CSVReader.java
+++ src/main/java/attendance/util/CSVReader.java
@@ -8,18 +8,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CSVReader {
+    public static final String COMMA = ",";
+
     public static List<List<String>> readCSV(Path path) {
         List<List<String>> csvData = new ArrayList<>();
         try {
-            List<String> records = Files.readAllLines(path);
-            for (int i = 1; i < records.size(); i++) {
-                csvData.add(List.of(records.get(i).split(",")));
-            }
+            addData(path, csvData);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         return csvData;
+    }
+
+    private static void addData(Path path, List<List<String>> csvData) throws IOException {
+        List<String> records = Files.readAllLines(path);
+        for (int i = 1; i < records.size(); i++) {
+            csvData.add(List.of(records.get(i).split(COMMA)));
+        }
     }
 
 
