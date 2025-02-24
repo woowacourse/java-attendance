14bac438618589e9c8b7d6d34b3b477f4c1ba15c
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-24 09:57:39 +0900

    refactor(AttendanceTypeTest): 테스트 케이스 세분화 적용

--- src/test/java/attendance/domain/AttendanceTypeTest.java
+++ src/test/java/attendance/domain/AttendanceTypeTest.java
@@ -3,33 +3,35 @@ package attendance.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AttendanceTypeTest {
-    @DisplayName("기능: 출석 날짜와 시간에 대한 알맞은 AttendanceType 반환")
+    @DisplayName("기능: 출석 날짜와 시간에 대한 출석 출결 유형 반환")
     @Test
-    void createAttendanceTypeOfLocalDateTime() {
-        List<LocalDateTime> localDateTime =
-                List.of(
-                        LocalDateTime.of(
-                                2025, 2, 20, 9, 0, 0
-                        ),
-                        LocalDateTime.of(
-                                2025, 2, 20, 10, 10, 0
-                        ),
-                        LocalDateTime.of(
-                                2025, 2, 20, 10, 40, 0
-                        ),
-                        LocalDateTime.of(
-                                2025, 2, 22, 10, 0, 0
-                        )
-                );
+    void checkAttendanceTypeSafe() {
+        LocalDateTime localDateTime = LocalDateTime.of(2025, 2, 20, 10, 0);
+        assertThat(AttendanceType.of(localDateTime)).isEqualTo(AttendanceType.SAFE);
+    }
 
-        assertThat(AttendanceType.of(localDateTime.get(0))).isEqualTo(AttendanceType.SAFE);
-        assertThat(AttendanceType.of(localDateTime.get(1))).isEqualTo(AttendanceType.LATE);
-        assertThat(AttendanceType.of(localDateTime.get(2))).isEqualTo(AttendanceType.ABSENT);
-        assertThat(AttendanceType.of(localDateTime.get(3))).isEqualTo(AttendanceType.FREE);
+    @DisplayName("기능: 출석 날짜와 시간에 대한 지각 출결 유형 반환")
+    @Test
+    void checkAttendanceTypeLate() {
+        LocalDateTime localDateTime = LocalDateTime.of(2025, 2, 20, 10, 10);
+        assertThat(AttendanceType.of(localDateTime)).isEqualTo(AttendanceType.LATE);
+    }
+
+    @DisplayName("기능: 출석 날짜와 시간에 대한 결석 출결 유형 반환")
+    @Test
+    void checkAttendanceTypeAbsent() {
+        LocalDateTime localDateTime = LocalDateTime.of(2025, 2, 20, 10, 40);
+        assertThat(AttendanceType.of(localDateTime)).isEqualTo(AttendanceType.ABSENT);
+    }
+
+    @DisplayName("기능: 출석 날짜와 시간에 대한 자유 출결 유형 반환")
+    @Test
+    void checkAttendanceTypeFree() {
+        LocalDateTime localDateTime = LocalDateTime.of(2025, 2, 22, 10, 0);
+        assertThat(AttendanceType.of(localDateTime)).isEqualTo(AttendanceType.FREE);
     }
 }
