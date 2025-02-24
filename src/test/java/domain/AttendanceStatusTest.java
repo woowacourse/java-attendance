1fd0ad1c65d5b99e7bf13c0f677d645fff644362
jeyong <joon6093@naver.com>
2025-02-25 00:03:10 +0900

    test: 출석 상태에 대한 테스트 코드 작성

--- src/test/java/domain/AttendanceStatusTest.java
+++ src/test/java/domain/AttendanceStatusTest.java
@@ -1,11 +1,11 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import org.junit.jupiter.api.Test;
 
 class AttendanceStatusTest {
-
     @Test
     void 출석_여부를_판단한다() {
         // given & when
@@ -31,11 +31,17 @@ class AttendanceStatusTest {
     @Test
     void 결석_여부를_판단한다() {
         // given & when
-        AttendanceStatus attendanceStatus = AttendanceStatus.from(
+        AttendanceStatus attendanceStatus1 = AttendanceStatus.from(
                 new WorkDateTime(new WorkDate(2024, 12, 13), new WorkTime(10, 35))
+        );
+        AttendanceStatus attendanceStatus2 = AttendanceStatus.from(
+                new WorkDateTime(new WorkDate(2024, 12, 13), new WorkTime(null, null))
         );
 
         // then
-        assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ABSENCE);
+        assertSoftly(softly -> {
+            softly.assertThat(attendanceStatus1).isEqualTo(AttendanceStatus.ABSENCE);
+            softly.assertThat(attendanceStatus2).isEqualTo(AttendanceStatus.ABSENCE);
+        });
     }
 }
