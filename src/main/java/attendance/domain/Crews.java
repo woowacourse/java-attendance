9709078f68c011929bb4b7a97d02bec8ae9ce70c
bowook <mbosx316@naver.com>
2025-02-25 01:09:27 +0900

    refactor(Crews) : 불변성 보장

--- src/main/java/attendance/domain/Crews.java
+++ src/main/java/attendance/domain/Crews.java
@@ -3,6 +3,7 @@ package attendance.domain;
 import attendance.exception.CustomException;
 import attendance.exception.ErrorMessage;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,12 +11,12 @@ public class Crews {
 
     private final Set<Crew> crews;
     public Crews(final Set<Crew> crews) {
-        this.crews = crews;
+        this.crews = Collections.unmodifiableSet(crews);
     }
 
     public Crew findCrew(String crewName) {
         return crews.stream().filter(crew ->
-                crew.checkSameName(crewName))
+                        crew.checkSameName(crewName))
                 .findFirst()
                 .orElseThrow(() -> CustomException.from(ErrorMessage.NICKNAME_NOT_PRESENCE));
     }
