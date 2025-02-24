0d368a6974d3836db20c4dba8ff9e0edffdf1f1d
황재웅 <wodnd0131@naver.com>
2025-02-24 11:22:55 +0900

    test: 출석이 수정될 경우, 출석 상태도 수정한다
    -test: 출석 상태를, 지각으로 수정한다
    -test: 출석 상태를, 결석으로 수정한다
    -test: 월요일일 경우에 맞게 출석을 수정한다

--- src/test/java/attendance/AttendanceModifierTest.java
+++ src/test/java/attendance/AttendanceModifierTest.java
@@ -9,10 +9,13 @@ import java.time.LocalTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import attendance.common.exception.AttendanceArgumentException;
 import attendance.common.exception.AttendanceFileException;
 import attendance.domain.AttendanceFileReader;
+import attendance.domain.AttendanceStatus;
 import attendance.domain.attendanceBook.Attendance;
 import attendance.domain.attendanceBook.AttendanceBook;
 import attendance.domain.attendanceManager.AttendanceManager;
@@ -81,22 +84,57 @@ public class AttendanceModifierTest {
         assertThat(attendanceBook.findAttendance(nickname, attendance)).isEqualTo(attendance);
     }
 
-    @Test
-    @DisplayName("출석 상태를, 지각으로 수정한다.")
-    void test_modifyStateToLate() {
+    // 해당 테스트는 attendance에 의해 출석 상태가 결정되기에, modifier의 핵심 기능과는 다소 거리가 있다.
+    // 때문에, 기존의 단위 테스트와 다른 면이 있어, 테스트 케이스를 크게 세분화하진 않겠다.
+    @ParameterizedTest
+    @DisplayName("출석이 수정될 경우, 출석 상태도 수정한다")
+    @CsvSource({
+        "2,13,5, LATE",
+        "2,13,30, ABSENCE",
+        "10,10,5, LATE",
+        "10,10,30, ABSENCE"
+    })
+    void test_modifyAttendanceState(int dayOfMonth, int hour, int addMinutes, AttendanceStatus expectedStatus) {
+        var nickname = "이든";
+        var date = LocalDate.of(2024, 12, dayOfMonth);
+        var time = LocalTime.of(hour, 2);
+        var attendance = new Attendance(LocalDateTime.of(date, time));
 
-    }
+        assertThat(attendanceBook.findAttendance(nickname, attendance).attendanceStatus())
+            .isEqualTo(AttendanceStatus.ATTENDANCE);
 
-    @Test
-    @DisplayName("출석 상태를, 결석으로 수정한다.")
-    void test_modifyStateToAbsence() {
+        var modifiedTime = time.plusMinutes(addMinutes);
+        var modifiedAttendance = new Attendance(LocalDateTime.of(date, modifiedTime));
+        attendanceModifier.manage(nickname, date, modifiedTime);
 
+        assertThatThrownBy(() -> attendanceBook.findAttendance(nickname, attendance))
+            .isInstanceOf(AttendanceArgumentException.class)
+            .hasMessageContaining("출석 정보를 찾을 수 없습니다.");
+        assertThat(attendanceBook.findAttendance(nickname, modifiedAttendance).attendanceStatus())
+            .isEqualTo(expectedStatus);
     }
 
     @Test
     @DisplayName("수정 시간이 캠퍼스 운영 시간 이외일 경우, 예외를 발생한다.")
     void error_outOfRangeOnCampusSchedule() {
+        var nickname = "이든";
+        var date = LocalDate.of(2024, 12, 2);
+        var time = LocalTime.of(13, 2);
 
+        var attendance = new Attendance(LocalDateTime.of(date, time));
+
+        assertThat(attendanceBook.findAttendance(nickname, attendance).attendanceStatus())
+            .isEqualTo(AttendanceStatus.ATTENDANCE);
+
+        var modifiedTime = LocalTime.of(7, 2);
+        var modifiedAttendance = new Attendance(LocalDateTime.of(date, modifiedTime));
+        attendanceModifier.manage(nickname, date, modifiedTime);
+
+        assertThatThrownBy(() -> attendanceBook.findAttendance(nickname, attendance))
+            .isInstanceOf(AttendanceArgumentException.class)
+            .hasMessageContaining("출석 정보를 찾을 수 없습니다.");
+        assertThat(attendanceBook.findAttendance(nickname, modifiedAttendance).attendanceStatus())
+            .isEqualTo(AttendanceStatus.ABSENCE);
     }
 
     @Test
@@ -108,18 +146,6 @@ public class AttendanceModifierTest {
     @Test
     @DisplayName("미래 날짜에 대해 수정할 경우, 예외가 발생한다.")
     void error_modifyFutureAttendance() {
-
-    }
-
-    @Test
-    @DisplayName("월요일의 출석 상태를, 지각으로 수정한다.")
-    void error_modifyToLateOnMonday() {
-
-    }
-
-    @Test
-    @DisplayName("월요일의 출석 상태를, 결석으로 수정한다.")
-    void error_modifyToAbsenceOnMonday() {
 
     }
 
