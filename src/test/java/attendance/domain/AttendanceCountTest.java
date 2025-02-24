6feadfcfda358bcff43e2930d0df12dad6f0326f
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-24 09:10:38 +0900

    refactor(AttendanceCountTest): 테스트 케이스 세부 분리

--- src/test/java/attendance/domain/AttendanceCountTest.java
+++ src/test/java/attendance/domain/AttendanceCountTest.java
@@ -6,23 +6,66 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AttendanceCountTest {
-    @DisplayName("기능: 해당 AttendanceCount 출석 횟수 증가 및 초기화")
+    @DisplayName("기능: 출석 횟수 초기화 테스트")
     @Test
-    void updateAttendanceCount() {
+    void resetSafeCount() {
         AttendanceCount attendanceCount = new AttendanceCount();
 
         attendanceCount.incrementSafeCount();
-        attendanceCount.incrementLateCount();
-        attendanceCount.incrementAbsentCount();
-
-        assertThat(attendanceCount.getSafeCount()).isEqualTo(1);
-        assertThat(attendanceCount.getLateCount()).isEqualTo(1);
-        assertThat(attendanceCount.getAbsentCount()).isEqualTo(1);
-
         attendanceCount.resetAttendanceCount();
 
         assertThat(attendanceCount.getSafeCount()).isZero();
+    }
+
+    @DisplayName("기능: 지각 횟수 초기화 테스트")
+    @Test
+    void resetLateCount() {
+        AttendanceCount attendanceCount = new AttendanceCount();
+
+        attendanceCount.incrementLateCount();
+        attendanceCount.resetAttendanceCount();
+
         assertThat(attendanceCount.getLateCount()).isZero();
+    }
+
+    @DisplayName("기능: 결석 횟수 초기화 테스트")
+    @Test
+    void resetAbsentCount() {
+        AttendanceCount attendanceCount = new AttendanceCount();
+
+        attendanceCount.incrementAbsentCount();
+        attendanceCount.resetAttendanceCount();
+
         assertThat(attendanceCount.getAbsentCount()).isZero();
+    }
+
+    @DisplayName("기능: 출석 횟수 증가 테스트")
+    @Test
+    void incrementSafeCount() {
+        AttendanceCount attendanceCount = new AttendanceCount();
+
+        attendanceCount.incrementSafeCount();
+
+        assertThat(attendanceCount.getSafeCount()).isEqualTo(1);
+    }
+
+    @DisplayName("기능: 지각 횟수 증가 테스트")
+    @Test
+    void incrementLateCount() {
+        AttendanceCount attendanceCount = new AttendanceCount();
+
+        attendanceCount.incrementLateCount();
+
+        assertThat(attendanceCount.getLateCount()).isEqualTo(1);
+    }
+
+    @DisplayName("기능: 결석 횟수 증가 테스트")
+    @Test
+    void incrementAbsentCount() {
+        AttendanceCount attendanceCount = new AttendanceCount();
+
+        attendanceCount.incrementAbsentCount();
+
+        assertThat(attendanceCount.getAbsentCount()).isEqualTo(1);
     }
 }
