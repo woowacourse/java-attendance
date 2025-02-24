75e3ddcb8045317e3ab9603425d1b0ec7420d9af
sonjh919 <kingjh1125@gmail.com>
2025-02-24 15:47:21 +0900

    test(green): 결석 정책 테스트 수정
    - 교육 시작 시간으로부터 5분 초과는 지각으로 간주한다
    ex) 10:06

--- src/test/java/AbsentPolicy.java
+++ src/test/java/AbsentPolicy.java
@@ -1,7 +1,13 @@
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class AbsentPolicy {
-    public String checkAttendanceStatus(LocalTime educationTime) {
-        return null;
+    public String checkAttendanceStatus(LocalDateTime educationDateTime) {
+        LocalTime educationTime = educationDateTime.toLocalTime();
+
+        if(educationTime.isAfter(LocalTime.of(10,0).plusMinutes(5))){
+            return "지각";
+        }
+        return "출석";
     }
 }
--- src/test/java/AbsentPolicyTest.java
+++ src/test/java/AbsentPolicyTest.java
@@ -1,20 +1,20 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AbsentPolicyTest {
     @Test
     @DisplayName("교육 시작 시간으로부터 5분 초과는 지각으로 간주한다")
-    public void validateEducationTimeTest() {
+    public void checkAttendanceStatusTest() {
         //given
         AbsentPolicy absentPolicy = new AbsentPolicy();
-        LocalTime educationTime = LocalTime.of(10,6);
+        LocalDateTime educationDateTime = LocalDateTime.of(2024,12,10,10,6);
 
         //when-then
-        assertThat(absentPolicy.checkAttendanceStatus(educationTime)).isEqualTo("지각");
+        assertThat(absentPolicy.checkAttendanceStatus(educationDateTime)).isEqualTo("지각");
     }
+
 }
