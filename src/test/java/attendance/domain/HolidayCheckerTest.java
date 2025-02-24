7b20e09d4c6bf84e2aedb393d53ec35308496089
Firedrago95 <yonghwa95@naver.com>
2025-02-24 17:19:07 +0900

    test: 등교일 예외검사 테스트 작성

--- src/test/java/attendance/domain/HolidayCheckerTest.java
+++ src/test/java/attendance/domain/HolidayCheckerTest.java
@@ -1,6 +1,7 @@
 package attendance.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
@@ -26,5 +27,14 @@ class HolidayCheckerTest {
         LocalDate localDate = LocalDate.of(2024, 12, 2);
 
         assertThat(HolidayChecker.check(localDate)).isFalse();
+    }
+
+    @Test
+    void 등교일이_아니면_예외를_반생한다() {
+        LocalDate localDate = LocalDate.of(2024, 12, 25);
+
+        assertThatThrownBy(() -> HolidayChecker.validWeekDay(localDate))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 12월 25일 수요일은 등교일이 아닙니다.");
     }
 }
