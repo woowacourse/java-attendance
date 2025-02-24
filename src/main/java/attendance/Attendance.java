33e0d49dd3b65b00426e58ffa27c26e2ef62175e
ChoiDongJoon <adong0808@naver.com>
2025-02-24 17:21:42 +0900

    feat: 출석 날짜가 캠퍼스 운영일이 아니면 예외

--- src/main/java/attendance/Attendance.java
+++ src/main/java/attendance/Attendance.java
@@ -1,6 +1,8 @@
 package attendance;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
 
 public class Attendance {
@@ -8,11 +10,13 @@ public class Attendance {
     public static final int OPEN_HOUR = 8;
     public static final int CLOSE_HOUR = 23;
 
-    public String attend(String timeInput, DayOfWeek dayOfWeek) {
+    public String attend(String timeInput, LocalDate today) {
+        List<DayOfWeek> weekend = List.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+        checkHoliday(today, weekend);
         if(Objects.equals("--:--", timeInput)) {
             return "결석";
         }
-        int startHour = checkStartHour(dayOfWeek);
+        int startHour = checkStartHour(today);
 
         String[] time = timeInput.split(":");
         int hour = Integer.parseInt(time[0]);
@@ -20,24 +24,30 @@ public class Attendance {
 
         checkCampusOpen(hour);
 
-        return checkAttendanceStatus(hour, startHour, minute);
+        return checkAttendanceStatus(hour, minute, startHour);
     }
 
-    private static int checkStartHour(DayOfWeek dayOfWeek) {
+    private void checkHoliday(LocalDate today, List<DayOfWeek> weekend) {
+        if(weekend.contains(today.getDayOfWeek()) || today.getDayOfMonth() == 25) {
+            throw new IllegalArgumentException("등교일이 아닙니다.");
+        }
+    }
+
+    private int checkStartHour(LocalDate today) {
         int startHour = 10;
-        if(dayOfWeek == DayOfWeek.MONDAY) {
+        if(today.getDayOfWeek() == DayOfWeek.MONDAY) {
             startHour = 13;
         }
         return startHour;
     }
 
-    private static void checkCampusOpen(int hour) {
+    private void checkCampusOpen(int hour) {
         if(hour < OPEN_HOUR || hour >= CLOSE_HOUR) {
             throw new IllegalArgumentException("캠퍼스 운영 시간은 8:00 ~ 23:00입니다.");
         }
     }
 
-    private static String checkAttendanceStatus(int hour, int startHour, int minute) {
+    private String checkAttendanceStatus(int hour, int minute, int startHour) {
         if(hour > startHour || minute > 30) {
             return "결석";
         }
--- src/test/java/attendance/AttendanceTest.java
+++ src/test/java/attendance/AttendanceTest.java
@@ -3,7 +3,7 @@ package attendance;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,64 +13,79 @@ public class AttendanceTest {
     @Test
     @DisplayName("등교 시간이 입력되면 출석")
     void attendance_basic_test1() {
+        LocalDate today = LocalDate.of(2024, 12, 3);
+
         Attendance attendance = new Attendance();
-        assertThat(attendance.attend("10:00", DayOfWeek.TUESDAY)).isEqualTo("출석");
+        assertThat(attendance.attend("10:00", today)).isEqualTo("출석");
     }
 
     @Test
     @DisplayName("5분 초과는 지각이다")
     void attendance_late_test1() {
+        LocalDate today = LocalDate.of(2024, 12, 3);
+
         Attendance attendance = new Attendance();
-        assertThat(attendance.attend("10:06", DayOfWeek.TUESDAY)).isEqualTo("지각");
+        assertThat(attendance.attend("10:06", today)).isEqualTo("지각");
     }
 
     @Test
     @DisplayName("30분 초과는 결석이다")
     void attendance_absence_test1() {
+        LocalDate today = LocalDate.of(2024, 12, 3);
+
         Attendance attendance = new Attendance();
-        assertThat(attendance.attend("10:31", DayOfWeek.TUESDAY)).isEqualTo("결석");
+        assertThat(attendance.attend("10:31", today)).isEqualTo("결석");
     }
 
     @Test
     @DisplayName("출석 기록이 없으면 결석")
     void attendance_absence_test2() {
+        LocalDate today = LocalDate.of(2024, 12, 3);
+
         Attendance attendance = new Attendance();
-        assertThat(attendance.attend("--:--", DayOfWeek.TUESDAY)).isEqualTo("결석");
+        assertThat(attendance.attend("--:--", today)).isEqualTo("결석");
     }
 
     @Test
     @DisplayName("1시간 이상 늦어도 결석")
     void attendance_absence_test3() {
+        LocalDate today = LocalDate.of(2024, 12, 3);
+
         Attendance attendance = new Attendance();
-        assertThat(attendance.attend("11:10", DayOfWeek.TUESDAY)).isEqualTo("결석");
+        assertThat(attendance.attend("11:10", today)).isEqualTo("결석");
     }
 
     @Test
     @DisplayName("월요일은 13시부터 시작한다. - 출석 테스트")
     void attendance_monday_test1() {
+        LocalDate monday = LocalDate.of(2024, 12, 2);
+
         Attendance attendance = new Attendance();
-        assertThat(attendance.attend("13:00", DayOfWeek.MONDAY)).isEqualTo("출석");
+        assertThat(attendance.attend("13:00", monday)).isEqualTo("출석");
     }
 
     @Test
     @DisplayName("월요일은 13시부터 시작한다. - 지각 테스트")
     void attendance_monday_test2() {
+        LocalDate monday = LocalDate.of(2024, 12, 2);
         Attendance attendance = new Attendance();
-        assertThat(attendance.attend("13:06", DayOfWeek.MONDAY)).isEqualTo("지각");
+        assertThat(attendance.attend("13:06", monday)).isEqualTo("지각");
     }
 
     @Test
     @DisplayName("월요일은 13시부터 시작한다. - 결석 테스트")
     void attendance_monday_test3() {
+        LocalDate monday = LocalDate.of(2024, 12, 2);
         Attendance attendance = new Attendance();
-        assertThat(attendance.attend("13:36", DayOfWeek.MONDAY)).isEqualTo("결석");
+        assertThat(attendance.attend("13:36", monday)).isEqualTo("결석");
     }
 
     @Test
     @DisplayName("출석 시간이 8시 이전이면 예외")
     void attendance_not_open_test1() {
+        LocalDate today = LocalDate.of(2024, 12, 2);
         Attendance attendance = new Attendance();
-        assertThatThrownBy(() -> attendance.attend("7:36", DayOfWeek.MONDAY))
+        assertThatThrownBy(() -> attendance.attend("7:36", today))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("캠퍼스 운영 시간은 8:00 ~ 23:00입니다.");
     }
@@ -78,9 +93,30 @@ public class AttendanceTest {
     @Test
     @DisplayName("출석 시간이 11시 이후면 예외")
     void attendance_not_open_test2() {
+        LocalDate today = LocalDate.of(2024, 12, 2);
         Attendance attendance = new Attendance();
-        assertThatThrownBy(() -> attendance.attend("23:36", DayOfWeek.MONDAY))
+        assertThatThrownBy(() -> attendance.attend("23:36", today))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("캠퍼스 운영 시간은 8:00 ~ 23:00입니다.");
+    }
+
+    @Test
+    @DisplayName("출석일이 주말이면 예외")
+    void attendance_not_open_test3() {
+        LocalDate today = LocalDate.of(2024, 12, 1);
+        Attendance attendance = new Attendance();
+        assertThatThrownBy(() -> attendance.attend("10:00", today))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("등교일이 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("출석일이 크리스마스면 예외")
+    void attendance_not_open_test4() {
+        LocalDate today = LocalDate.of(2024, 12, 25);
+        Attendance attendance = new Attendance();
+        assertThatThrownBy(() -> attendance.attend("10:00", today))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("등교일이 아닙니다.");
     }
 }
