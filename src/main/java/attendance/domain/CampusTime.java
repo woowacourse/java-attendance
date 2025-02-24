52a79543274762623210262d7e929bea3f27f6a4
taek2222 <oot309@daum.net>
2025-02-24 17:19:41 +0900

    refactor(CampusTime): 파라미터 네이밍 및 로직 수정

--- src/main/java/attendance/domain/CampusTime.java
+++ src/main/java/attendance/domain/CampusTime.java
@@ -12,9 +12,8 @@ public enum CampusTime {
         this.time = time;
     }
 
-    public static void validateOperationTime(LocalTime target) { // todo : 파라미터 네이밍, 내부 코드 개선, 에러 분리
-        boolean isNotOperationTime = target.isBefore(START_TIME.time) || target.isAfter(END_TIME.time);
-        if (isNotOperationTime) {
+    public static void validateOperationTime(LocalTime time) {
+        if (time.isBefore(START_TIME.time) || time.isAfter(END_TIME.time)) {
             throw new IllegalArgumentException("[ERROR] 캠퍼스 운영시간이 아닙니다.");
         }
     }
