058b7a62564543b3f0462c5fb30ece8195677c4e
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-24 11:54:29 +0900

    red test : 중복 없이 크루 이름을 읽어오는 기능 TDD

--- src/test/java/test/AttendanceTest.java
+++ src/test/java/test/AttendanceTest.java
@@ -1,4 +1,34 @@
 package test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import model.CrewGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class AttendanceTest {
+
+    @DisplayName("중복 없이 크루 이름을 읽어온다.")
+    @Test
+    void test1() {
+        String crewInput = """
+                쿠키,2024-12-13 10:08
+                빙봉,2024-12-13 10:07
+                이든,2024-12-13 10:07
+                빙봉,2024-12-12 11:11
+                빙티,2024-12-12 10:07
+                이든,2024-12-12 10:06
+                이든,2024-12-11 10:10
+                """;
+        List<String> crews = CrewGenerator.findCrewNames(crewInput);
+        assertThat(crews).containsExactly("쿠키", "빙봉", "빙티", "이든");
+    }
+
+//    @DisplayName("입력한 닉네임에 맞는 크루 정보를 가져온다.")
+//    @Test
+//    void test1() {
+//        String name = "빙티";
+//        Crews crews = new C
+//    }
 }
