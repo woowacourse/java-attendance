8141de7867afbfa2bff3868e609803e2e86fe8db
eoehd1ek <eoehd2ek@gmail.com>
2025-02-24 15:39:42 +0900

    refector(Attends): 로직 상 필요 없는 코드를 제거하도록 수정
    주어진 Attend에서 날짜만 비교하므로, 날짜로 비교해 동일 출석 가져올 필요가 없다

--- src/main/java/domain/Attends.java
+++ src/main/java/domain/Attends.java
@@ -36,24 +36,11 @@ public class Attends {
     }
 
     public void edit(Attend attend) {
-        if (hasDayEqualsAttend(attend)) {
-            // 똑같은 거 찾기
-            Attend before = findByAttend(attend);
-
-            // 찾은 객체를 토대로 list에서 삭제
-            removeByAttend(before);
-        }
+        removeByDayEqualAttend(attend);
         addAttend(attend);
     }
 
-    private Attend findByAttend(Attend target) {
-        return attends.stream()
-                .filter(target::isDayEqual)
-                .findAny()
-                .orElseThrow(IllegalArgumentException::new);
-    }
-
-    private void removeByAttend(Attend target) {
+    private void removeByDayEqualAttend(Attend target) {
         attends = attends.stream()
                 .filter(before -> !before.isDayEqual(target))
                 .collect(Collectors.toList());
