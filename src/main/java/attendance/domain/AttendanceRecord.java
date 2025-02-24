4b2e04e56f2c5f52ba006f485fc0fd2c51dd99e9
KJungW <sagakkb0707@naver.com>
2025-02-24 18:43:22 +0900

    feat(add_record): 출석 추가 (구현)
    작업 내용
    - 닉네임과 출석 시간으로 출석 기록을 추가할 수 있다

--- src/main/java/attendance/domain/AttendanceRecord.java
+++ src/main/java/attendance/domain/AttendanceRecord.java
@@ -1,9 +1,38 @@
 package attendance.domain;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public class AttendanceRecord {
+public final class AttendanceRecord {
+
+    private final String nickname;
+    private final LocalDateTime arrivalDateTime;
 
     public AttendanceRecord(String nickname, LocalDateTime arrivalDateTime) {
+        this.nickname = nickname;
+        this.arrivalDateTime = arrivalDateTime;
+    }
+
+    public boolean isSame(String name, LocalDate date) {
+        return nickname.equals(name) && arrivalDateTime.toLocalDate().equals(date);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        AttendanceRecord that = (AttendanceRecord) object;
+        return Objects.equals(nickname, that.nickname) && Objects.equals(arrivalDateTime,
+                that.arrivalDateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nickname, arrivalDateTime);
     }
 }
--- src/main/java/attendance/domain/AttendanceSystem.java
+++ src/main/java/attendance/domain/AttendanceSystem.java
@@ -2,15 +2,22 @@ package attendance.domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class AttendanceSystem {
 
-    public void addAttendanceRecord(String crewNickname, LocalDateTime arrivalDateTime) {
+    private List<AttendanceRecord> records = new ArrayList<>();
 
+    public void addAttendanceRecord(String crewNickname, LocalDateTime arrivalDateTime) {
+        AttendanceRecord newRecord = new AttendanceRecord(crewNickname, arrivalDateTime);
+        records.add(newRecord);
     }
 
-    public Optional<AttendanceRecord> findAttendanceRecord(String crewNickname, LocalDate localDate) {
-        return Optional.empty();
+    public Optional<AttendanceRecord> findAttendanceRecord(String crewNickname, LocalDate date) {
+        return records.stream()
+                .filter(record -> record.isSame(crewNickname, date))
+                .findAny();
     }
 }
