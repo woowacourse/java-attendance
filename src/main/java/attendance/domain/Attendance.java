491e89d16cd3f11559db1aaeccd41e1e70b274ad
DEV_WOONG <ywcsuwon@gmail.com>
2025-02-25 22:11:11 +0900

    feat(green): 월요일 출석 기능

--- src/main/java/attendance/domain/Attendance.java
+++ src/main/java/attendance/domain/Attendance.java
@@ -1,5 +1,6 @@
 package attendance.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
@@ -14,6 +15,18 @@ public class Attendance {
     }
 
     private String checkAttendanceStatus() {
+        if (attendanceDateTime.toLocalDate().getDayOfWeek().equals(DayOfWeek.MONDAY)) {
+            if (attendanceDateTime.toLocalTime().isBefore(LocalTime.of(13, 5)) || attendanceDateTime.toLocalTime()
+                    .equals(LocalTime.of(13, 5))) {
+                return "출석";
+            }
+            if (attendanceDateTime.toLocalTime().isBefore(LocalTime.of(13, 30)) || attendanceDateTime.toLocalTime()
+                    .equals(LocalTime.of(13, 30))) {
+                return "지각";
+            }
+            return "결석";
+        }
+
         if (attendanceDateTime.toLocalTime().isBefore(LocalTime.of(10, 5)) || attendanceDateTime.toLocalTime()
                 .equals(LocalTime.of(10, 5))) {
             return "출석";
