305cc9e48b15238e39c33b99c6f57e35434e1dc5
Firedrago95 <yonghwa95@naver.com>
2025-02-25 19:00:04 +0900

    test: 출석 기록 추가 테스트 코드 작성

--- src/test/java/attendance/domain/AttendancesTest.java
+++ src/test/java/attendance/domain/AttendancesTest.java
@@ -13,6 +13,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AttendancesTest {
 
     @Test
+    void 출석_기록을_추가한다() {
+        // given
+        String name = "빙봉";
+        LocalDate attendanceDate = LocalDate.of(2024, 12, 13);
+        LocalTime attendanceTime = LocalTime.of(10, 8);
+        Attendances attendances = new Attendances();
+
+        // when
+        attendances.addAttendance(name, new Attendance(attendanceDate, attendanceTime));
+
+        // then
+        assertThat(attendances.hasAttendance(name, attendanceDate, attendanceTime)).isTrue();
+    }
+
+    @Test
     void 기존_출석_기록_있다면_true_반환한다() {
         // given
         String name = "빙봉";
@@ -32,13 +47,15 @@ public class AttendancesTest {
         // given
         String name = "빙봉";
         LocalDate today = LocalDate.of(2024, 12, 13);
-        LocalTime attendanceTime = LocalTime.of(10, 8);
+        LocalTime absenceTime = LocalTime.of(10, 31);
+        LocalTime lateTime = LocalTime.of(10, 6);
 
         Attendances attendances = new Attendances();
-        attendances.addAttendance(name, new Attendance(today, attendanceTime));
+        attendances.addAttendance(name, new Attendance(today, absenceTime));
+        attendances.addAttendance("빙티", new Attendance(today, lateTime));
 
         // when & then
-        assertThat(attendances.hasAttendance("빙티", today, attendanceTime)).isFalse();
+        assertThat(attendances.hasAttendance("빙티", today, absenceTime)).isFalse();
 
     }
 }
