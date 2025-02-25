328bdf2d0d9204a8763d62335df73ce3e6899f5c
Sung-june27 <kmymy27@gmail.com>
2025-02-25 22:52:32 +0900

    feat: 닉네임과 시간을 이용해 출석을 수정한다

--- src/main/java/attendance/AttendanceDate.java
+++ src/main/java/attendance/AttendanceDate.java
@@ -6,15 +6,20 @@ import java.time.LocalDate;
 public record AttendanceDate(
         LocalDate date
 ) {
-    public AttendanceDate {
+    public static AttendanceDate from(LocalDate date) {
+        validate(date);
+        return new AttendanceDate(date);
+    }
+
+    public boolean isEqualToDate(LocalDate date) {
+        return this.date.isEqual(date);
+    }
+
+    private static void validate(LocalDate date) {
         DayOfWeek dayOfWeek = date.getDayOfWeek();
         if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY ||
                 date.isEqual(LocalDate.of(2024, 12, 25))) {
             throw new IllegalArgumentException();
         }
-    }
-
-    public boolean isEqualToDate(LocalDate date) {
-        return this.date.isEqual(date);
     }
 }
