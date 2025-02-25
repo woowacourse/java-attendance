1269409acb8d0396f708b71a543b3eb4ee6582fe
ryan-dia <cjfdnjs158@naver.com>
2025-02-25 22:25:26 +0900

    feat: 출석 상태 도메인 추가

---
+++ src/main/java/domain/AttendanceStatus.java
@@ -0,0 +1,57 @@
+package domain;
+
+import java.time.LocalTime;
+
+public enum AttendanceStatus {
+    PRESENT("출석"),
+    LATE("지각"),
+    ABSENCE("결석");
+
+    private static final int LATE_CUTOFF = 30;
+    private static final int ATTENDANCE_CUTOFF = 5;
+
+    private final String displayName;
+
+    AttendanceStatus(final String displayName) {
+        this.displayName = displayName;
+    }
+
+    public static AttendanceStatus findByAttendanceDateTime(final AttendanceDateTime attendanceDateTime,
+                                                            final AttendanceTimePolicy attendanceTimePolicy) {
+        final LocalTime attendanceTime = attendanceTimePolicy.getAttendanceTime();
+        final LocalTime entranceTime = attendanceDateTime.getDateTime()
+                .toLocalTime();
+
+        if (isNotWithinOperatingHours(entranceTime)) {
+            return ABSENCE;
+        }
+        if (isPresent(entranceTime, attendanceTime)) {
+            return PRESENT;
+        }
+        if (isLate(entranceTime, attendanceTime)) {
+            return LATE;
+        }
+        return ABSENCE;
+    }
+
+    private static boolean isNotWithinOperatingHours(final LocalTime entranceTime) {
+        return entranceTime.isBefore(AttendanceTimePolicy.START_TIME) || entranceTime.isAfter(
+                AttendanceTimePolicy.END_TIME);
+    }
+
+    private static boolean isPresent(final LocalTime entranceTime, final LocalTime attendanceTime) {
+        return !entranceTime.isAfter(getCutoffTime(attendanceTime, ATTENDANCE_CUTOFF));
+    }
+
+    private static boolean isLate(final LocalTime entranceTime, final LocalTime attendanceTime) {
+        return !entranceTime.isAfter(getCutoffTime(attendanceTime, LATE_CUTOFF));
+    }
+
+    private static boolean isAbsence(final LocalTime entranceTime, final LocalTime attendanceTime) {
+        return entranceTime.isAfter(getCutoffTime(attendanceTime, LATE_CUTOFF));
+    }
+
+    private static LocalTime getCutoffTime(final LocalTime attendanceTime, final int maxMinutes) {
+        return attendanceTime.plusMinutes(maxMinutes);
+    }
+}
---
+++ src/test/java/domain/AttendanceStatusTest.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class AttendanceStatusTest {
+
+    @DisplayName("출석시간에 맞는 상태를 반환한다.")
+    @ParameterizedTest
+    @MethodSource("methodSources")
+    void validAttendanceStatus(final AttendanceDateTime attendanceDateTime, final AttendanceStatus expectedStatus) {
+        // given
+        // when
+        final AttendanceTimePolicy attendanceTimePolicy = AttendanceTimePolicy.findByAttendanceDateTime(
+                attendanceDateTime);
+        final AttendanceStatus attendanceStatus = AttendanceStatus.findByAttendanceDateTime(attendanceDateTime,
+                attendanceTimePolicy);
+
+        // then
+        Assertions.assertThat(attendanceStatus).isEqualTo(expectedStatus);
+    }
+
+    private static Stream<Arguments> methodSources() {
+        return Stream.of(
+                Arguments.arguments(AttendanceDateTime.from("2024-12-11 10:00"), AttendanceStatus.PRESENT),
+                Arguments.arguments(AttendanceDateTime.from("2024-12-13 10:06"), AttendanceStatus.LATE),
+                Arguments.arguments(AttendanceDateTime.from("2024-12-16 13:00"), AttendanceStatus.PRESENT),
+                Arguments.arguments(AttendanceDateTime.from("2024-12-17 11:00"), AttendanceStatus.ABSENCE)
+        );
+    }
+}
