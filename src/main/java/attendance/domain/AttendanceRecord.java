1a0821c79c8c988cc73380fa1fbb80aeb1ca9fbd
YuyoungRhee <bbclara@naver.com>
2025-02-25 15:00:18 +0900

    feat(AttendanceRecord): 시작시간을 EducationTime을 통해 가져오도록 변경
    - 현재 출석 LocalDateTime의 dayOfWeek를 통해 시작시간을 가져오도록 변경

--- src/main/java/attendance/domain/AttendanceRecord.java
+++ src/main/java/attendance/domain/AttendanceRecord.java
@@ -1,6 +1,5 @@
 package attendance.domain;
 
-import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -13,12 +12,8 @@ public class AttendanceRecord {
     }
 
     public AttendanceStatus getAttendanceStatus() {
-        LocalTime startTime;
-        if (attendanceDateTime.getDayOfWeek() == DayOfWeek.MONDAY) {
-            startTime = LocalTime.of(13, 0);
-        } else {
-            startTime = LocalTime.of(10, 0);
-        }
+        EducationTime educationTime = EducationTime.from(attendanceDateTime.getDayOfWeek());
+        LocalTime startTime = educationTime.getStartTime();
         LocalTime attendanceTime = attendanceDateTime.toLocalTime();
         Duration duration = Duration.between(startTime, attendanceTime);
         long minutes = duration.toMinutes();
