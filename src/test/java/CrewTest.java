65448525e23af6a84c68a41845558c5bc41e6021
supernovaMK <minkikim2002@gmail.com>
2025-02-25 17:31:37 +0900

    test: 크루의 주말,휴일 출석 경우 테스트 추가

--- src/test/java/CrewTest.java
+++ src/test/java/CrewTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CrewTest {
 
@@ -14,5 +15,13 @@ public class CrewTest {
         LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 13, 10, 8);
         crew.attend(localDateTime);
         assertThat(crew.getAttendTimes().size()).isEqualTo(1);
+    }
+    @DisplayName("크루는 주말이나 휴일에 출석을 할 수 없다")
+    @Test
+    void 크루는_주말이나_휴일에_출석을_할_수_없다() {
+        Crew crew = new Crew("슬링키");
+        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 15, 10, 8);
+        assertThatThrownBy(()->crew.attend(localDateTime)).isInstanceOf(IllegalArgumentException.class);
+
     }
 }
