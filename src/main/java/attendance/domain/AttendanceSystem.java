617624b48583895a49f3c095dc0bb8fe7b29455f
KJungW <sagakkb0707@naver.com>
2025-02-25 13:25:00 +0900

    feat(add_record): 등교일 체크 (구현)
    구현내용
    - 등교일이 아닌 경우 예외 메세지를 출력한다

--- src/main/java/attendance/domain/AttendanceSystem.java
+++ src/main/java/attendance/domain/AttendanceSystem.java
@@ -5,24 +5,36 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 public class AttendanceSystem {
 
     private final CrewStorage crewStorage;
+    private final HolidayChecker holidayChecker;
     private final List<AttendanceRecord> records = new ArrayList<>();
 
     public AttendanceSystem(CrewStorage crewStorage, HolidayChecker holidayChecker) {
         this.crewStorage = crewStorage;
+        this.holidayChecker = holidayChecker;
     }
 
     public void addAttendanceRecord(String crewNickname, LocalDateTime arrivalDateTime) {
         crewStorage.validateIsNotContained(crewNickname);
+
         Optional<AttendanceRecord> originRecord = findAttendanceRecord(crewNickname, arrivalDateTime.toLocalDate());
         if (originRecord.isPresent()) {
             throw new IllegalArgumentException(ExceptionMessage.ALREADY_ATTENDANCE.getMessage());
+        }
+
+        if (holidayChecker.checkHoliday(arrivalDateTime.toLocalDate())) {
+            String exceptionMessage = String.format(ExceptionMessage.HOLIDAY_ATTENDANCE.getMessage(),
+                    arrivalDateTime.getMonth().getValue(), arrivalDateTime.getDayOfMonth(),
+                    arrivalDateTime.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREA));
+            throw new IllegalArgumentException(exceptionMessage);
         }
 
         if (arrivalDateTime.getDayOfWeek() == DayOfWeek.MONDAY) {
--- src/main/java/attendance/domain/HolidayChecker.java
+++ src/main/java/attendance/domain/HolidayChecker.java
@@ -1,10 +1,30 @@
 package attendance.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HolidayChecker {
 
-    public void addPublicHoliday(LocalDate publicHoliday) {
+    private Set<LocalDate> publicHolidays = new HashSet<>();
 
+    public void addPublicHoliday(LocalDate publicHoliday) {
+        publicHolidays.add(publicHoliday);
+    }
+
+
+    public boolean checkHoliday(LocalDate date) {
+        return isWeekend(date) || isPublicHoliday(date);
+    }
+
+    private boolean isWeekend(LocalDate date) {
+        return date.getDayOfWeek() == DayOfWeek.SATURDAY ||
+                date.getDayOfWeek() == DayOfWeek.SUNDAY;
+    }
+
+    private boolean isPublicHoliday(LocalDate date) {
+        return publicHolidays.contains(date);
     }
 }
+
