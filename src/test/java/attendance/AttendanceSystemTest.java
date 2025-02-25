bc5f8e0651ca70b09eefe9ae8111cb65b3fa8892
KJungW <sagakkb0707@naver.com>
2025-02-25 14:28:02 +0900

    test(add_record): 테스트명 정리
    작업 내용
    - 다른 주요 기능과 테스트명이 겹치지 않도록
      테스트명에 컨벤션 추가

--- src/test/java/attendance/AttendanceSystemTest.java
+++ src/test/java/attendance/AttendanceSystemTest.java
@@ -48,9 +48,9 @@ class AttendanceSystemTest {
         attendanceSystem = new AttendanceSystem(crewStorage, attendanceChecker);
     }
 
-    @DisplayName("닉네임과 출석 시간으로 출석 기록을 추가할 수 있다")
+    @DisplayName("출석 확인 - 닉네임과 출석 시간으로 출석 기록을 추가할 수 있다")
     @Test
-    void 닉네임과_출석_시간으로_출석_기록을_추가할_수_있다() {
+    void 출석_확인_닉네임과_출석_시간으로_출석_기록을_추가할_수_있다() {
         String crewNickname = VALID_CREW_NICKNAME;
         LocalDateTime arrivalDateTime = LocalDateTime.of(2025, 2, 4, 8, 50, 0);
 
@@ -63,10 +63,10 @@ class AttendanceSystemTest {
         assertThat(actualRecord).isEqualTo(expectedRecord);
     }
 
-    @DisplayName("교육시간과 출석정책을 기준으로 월요일의 출석 상태를 결정한다")
+    @DisplayName("출석 확인 - 교육시간과 출석정책을 기준으로 월요일의 출석 상태를 결정한다")
     @ParameterizedTest
     @MethodSource()
-    void 교육시간과_출석정책을_기준으로_월요일의_출석_상태를_결정한다(LocalDateTime arrivalDateTime, AttendanceType attendanceType) {
+    void 출석_확인_교육시간과_출석정책을_기준으로_월요일의_출석_상태를_결정한다(LocalDateTime arrivalDateTime, AttendanceType attendanceType) {
         String crewNickname = VALID_CREW_NICKNAME;
         attendanceSystem.addAttendanceRecord(crewNickname, arrivalDateTime);
 
@@ -75,7 +75,7 @@ class AttendanceSystemTest {
         assertThat(actualRecord.getAttendanceType()).isEqualTo(attendanceType);
     }
 
-    static Stream<Arguments> 교육시간과_출석정책을_기준으로_월요일의_출석_상태를_결정한다() {
+    static Stream<Arguments> 출석_확인_교육시간과_출석정책을_기준으로_월요일의_출석_상태를_결정한다() {
         return Stream.of(
                 Arguments.of(LocalDateTime.of(2025, 2, 3, 12, 0, 0), AttendanceType.ATTENDANCE),
                 Arguments.of(LocalDateTime.of(2025, 2, 3, 13, 4, 59), AttendanceType.ATTENDANCE),
@@ -87,10 +87,10 @@ class AttendanceSystemTest {
         );
     }
 
-    @DisplayName("교육시간과 출석정책을 기준으로 화요일에서 금요일의 출석 상태를 결정한다")
+    @DisplayName("출석 확인 - 교육시간과 출석정책을 기준으로 화요일에서 금요일의 출석 상태를 결정한다")
     @ParameterizedTest
     @MethodSource()
-    void 교육시간과_출석정책을_기준으로_화요일에서_금요일의_출석_상태를_결정한다(LocalDateTime arrivalDateTime, AttendanceType attendanceType) {
+    void 출석_확인_교육시간과_출석정책을_기준으로_화요일에서_금요일의_출석_상태를_결정한다(LocalDateTime arrivalDateTime, AttendanceType attendanceType) {
         String crewNickname = VALID_CREW_NICKNAME;
         attendanceSystem.addAttendanceRecord(crewNickname, arrivalDateTime);
 
@@ -99,7 +99,7 @@ class AttendanceSystemTest {
         assertThat(actualRecord.getAttendanceType()).isEqualTo(attendanceType);
     }
 
-    static Stream<Arguments> 교육시간과_출석정책을_기준으로_화요일에서_금요일의_출석_상태를_결정한다() {
+    static Stream<Arguments> 출석_확인_교육시간과_출석정책을_기준으로_화요일에서_금요일의_출석_상태를_결정한다() {
         return Stream.of(
                 Arguments.of(LocalDateTime.of(2025, 2, 7, 9, 0, 0), AttendanceType.ATTENDANCE),
                 Arguments.of(LocalDateTime.of(2025, 2, 7, 10, 4, 59), AttendanceType.ATTENDANCE),
@@ -111,9 +111,9 @@ class AttendanceSystemTest {
         );
     }
 
-    @DisplayName("이미 출석한 경우, 다시 출석할 수 없으며 수정 기능을 이용하도록 안내한다")
+    @DisplayName("출석 확인 - 이미 출석한 경우, 다시 출석할 수 없으며 수정 기능을 이용하도록 안내한다")
     @Test
-    void 이미_출석한_경우_다시_출석할_수_없으며_수정_기능을_이용하도록_안내한다() {
+    void 출석_확인_이미_출석한_경우_다시_출석할_수_없으며_수정_기능을_이용하도록_안내한다() {
         String crewNickname = VALID_CREW_NICKNAME;
         LocalDate arrivalDate = LocalDate.of(2025, 2, 4);
         LocalDateTime arrivalDateTime = LocalDateTime.of(arrivalDate, LocalTime.of(8, 50, 0));
@@ -124,9 +124,9 @@ class AttendanceSystemTest {
                 .withMessage(ExceptionMessage.ALREADY_ATTENDANCE.getMessage());
     }
 
-    @DisplayName("네임이 등록되지 않은 경우 예외 메세지를 출력한다")
+    @DisplayName("출석 확인 - 네임이 등록되지 않은 경우 예외 메세지를 출력한다")
     @Test
-    void 네임이_등록되지_않은_경우_예외_메세지를_출력한다() {
+    void 출석_확인_네임이_등록되지_않은_경우_예외_메세지를_출력한다() {
         String crewNickname = INVALID_CREW_NICKNAME;
         LocalDateTime arrivalDateTime = LocalDateTime.of(2025, 2, 4, 8, 50, 0);
 
@@ -135,10 +135,10 @@ class AttendanceSystemTest {
                 .withMessage(ExceptionMessage.INVALID_CREW.getMessage());
     }
 
-    @DisplayName("등교일이 아닌 경우 예외 메세지를 출력한다")
+    @DisplayName("출석 확인 - 등교일이 아닌 경우 예외 메세지를 출력한다")
     @ParameterizedTest
     @MethodSource()
-    void 등교일이_아닌_경우_예외_메세지를_출력한다(LocalDateTime arrivalDateTime) {
+    void 출석_확인_등교일이_아닌_경우_예외_메세지를_출력한다(LocalDateTime arrivalDateTime) {
         String crewNickname = VALID_CREW_NICKNAME;
 
         String exceptionMessage = String.format(ExceptionMessage.HOLIDAY_ATTENDANCE.getMessage(),
@@ -150,7 +150,7 @@ class AttendanceSystemTest {
                 .withMessage(exceptionMessage);
     }
 
-    static Stream<Arguments> 등교일이_아닌_경우_예외_메세지를_출력한다() {
+    static Stream<Arguments> 출석_확인_등교일이_아닌_경우_예외_메세지를_출력한다() {
         return Stream.of(
                 Arguments.of(SATURDAY),
                 Arguments.of(SUNDAY),
@@ -158,10 +158,10 @@ class AttendanceSystemTest {
         );
     }
 
-    @DisplayName("캠퍼스 운영 시간이 아닌 경우 예외 메세지를 출력한다")
+    @DisplayName("출석 확인 - 캠퍼스 운영 시간이 아닌 경우 예외 메세지를 출력한다")
     @ParameterizedTest
     @ValueSource(strings = {"07:59:59", "23:00:00"})
-    void 캠퍼스_운영_시간이_아닌_경우_예외_메세지를_출력한다(LocalTime arrivalTime) {
+    void 출석_확인_캠퍼스_운영_시간이_아닌_경우_예외_메세지를_출력한다(LocalTime arrivalTime) {
         LocalDateTime arrivalDateTime = LocalDateTime.of(LocalDate.of(2025, 2, 10), arrivalTime);
 
         assertThatIllegalArgumentException()
@@ -169,10 +169,10 @@ class AttendanceSystemTest {
                 .withMessage(ExceptionMessage.OUT_OF_CAMPUS_TIME.getMessage());
     }
 
-    @DisplayName("캠퍼스 운영 시간인 경우 예외 메세지를 발생시키지 않는다")
+    @DisplayName("출석 확인 - 캠퍼스 운영 시간인 경우 예외 메세지를 발생시키지 않는다")
     @ParameterizedTest
     @ValueSource(strings = {"08:00", "22:59"})
-    void 캠퍼스_운영_시간인_경우_예외_메세지를_발생시키지_않는다(LocalTime arrivalTime) {
+    void 출석_확인_캠퍼스_운영_시간인_경우_예외_메세지를_발생시키지_않는다(LocalTime arrivalTime) {
         LocalDateTime arrivalDateTime = LocalDateTime.of(LocalDate.of(2025, 2, 10), arrivalTime);
 
         assertThatCode(() -> attendanceSystem.addAttendanceRecord(VALID_CREW_NICKNAME, arrivalDateTime))
