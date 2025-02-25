824538145bafaabebbd9ff4403afc885e9bc3a5b
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-26 00:01:13 +0900

    feat: 출석 일자 클래스 작성

---
+++ src/main/java/attendance/model/attendance/datetime/date/AttendanceDate.java
@@ -0,0 +1,54 @@
+package attendance.model.attendance.datetime.date;
+
+import attendance.model.campus.CampusOperationPolicy;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class AttendanceDate {
+
+    private final LocalDate value;
+
+    private AttendanceDate(LocalDate value) {
+        this.value = value;
+    }
+
+    public static AttendanceDate policyApplied(
+            final LocalDate value,
+            final CampusOperationPolicy campusOperationPolicy
+    ) {
+
+        validateCampusOperationTime(value, campusOperationPolicy);
+        return new AttendanceDate(value);
+    }
+
+    private static void validateCampusOperationTime(
+            final LocalDate value,
+            final CampusOperationPolicy campusOperationPolicy
+    ) {
+
+        if (!campusOperationPolicy.isOpenDate(value)) {
+            throw new IllegalArgumentException("캠퍼스 운영 일자가 아닙니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AttendanceDate that = (AttendanceDate) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public String toString() {
+        return "AttendanceDate{" +
+                "value=" + value +
+                '}';
+    }
+}
