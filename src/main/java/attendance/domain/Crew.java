6f314e07bb7d914bafd512a9386186e2de34d29a
ykmxxi <ykmxxi97@gmail.com>
2025-02-25 19:54:38 +0900

    feat: 크루를 생성하는 기능 구현

---
+++ src/main/java/attendance/domain/Crew.java
@@ -0,0 +1,41 @@
+package attendance.domain;
+
+import java.util.Objects;
+
+public class Crew {
+
+    private static final int MIN_NICKNAME_LENGTH = 2;
+    private static final int MAX_NICKNAME_LENGTH = 4;
+
+    private final String nickname;
+
+    public Crew(final String nickname) {
+        validateLength(nickname);
+        this.nickname = nickname;
+    }
+
+    private void validateLength(final String nickname) {
+        if (nickname.length() < MIN_NICKNAME_LENGTH || nickname.length() > MAX_NICKNAME_LENGTH) {
+            throw new IllegalArgumentException("닉네임은 %d글자 이상 %d글자 이하만 가능합니다.".formatted(
+                    MIN_NICKNAME_LENGTH, MAX_NICKNAME_LENGTH
+            ));
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Crew crew)) {
+            return false;
+        }
+        return Objects.equals(nickname, crew.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nickname);
+    }
+
+}
---
+++ src/test/java/attendance/domain/CrewTest.java
@@ -0,0 +1,26 @@
+package attendance.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CrewTest {
+
+    @ValueSource(strings = {"빙", "빙봉빙봉빙"})
+    @ParameterizedTest
+    void 크루_닉네임_길이_범위를_벗어난_경우_크루_생성이_불가능하다(String wrongRangeName) {
+        assertThatThrownBy(() -> new Crew(wrongRangeName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 닉네임을_알려주면_크루를_생성한다() {
+        Crew crew = new Crew("빙봉");
+
+        assertThat(crew).isEqualTo(new Crew("빙봉"));
+    }
+
+}
