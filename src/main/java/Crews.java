e1533d977141dba2215b644ae54a64c52a7aa7ce
supernovaMK <minkikim2002@gmail.com>
2025-02-25 14:34:40 +0900

    feat: Crews 클래스 구현 - 크루의 이름을 통해서 출석하는 기능 구현 - 이미 정보가 있는 크루가 있다면 출석정보를 추가하는 기능 구현

---
+++ src/main/java/Crews.java
@@ -0,0 +1,33 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Crews {
+    private final List<Crew> crews;
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public Crews(List<String> crewAttendanceResource) {
+        crews = new ArrayList<>();
+        crewAttendanceResource.stream().map(crew -> crew.split(",")
+        ).forEach(crew -> addCrew(crew[0], crew[1]));
+    }
+
+    private void addCrew(String nickname, String date) {
+        Crew crew = findCrewByNickname(nickname).orElse(null);
+        if (crew != null) {
+            crew.attend(LocalDateTime.parse(date, FORMATTER));
+            return;
+        }
+        crews.add(new Crew(nickname, LocalDateTime.parse(date, FORMATTER)));
+    }
+
+    public List<Crew> getCrews() {
+        return crews;
+    }
+
+    public Optional<Crew> findCrewByNickname(String nickname) {
+        return crews.stream().filter(crew -> crew.getNickname().equals(nickname)).findFirst();
+    }
+}
---
+++ src/test/java/CrewsTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CrewsTest {
+    @DisplayName("크루들의 출석 데이터를 받아와 크루를 생성할 수 있다")
+    @Test
+    void test1() {
+        List<String> crewAttendanceResource = CrewAttendanceFileReader.readFile("src/main/resources/attendances.csv");
+        Crews crews = new Crews(crewAttendanceResource);
+        assertThat(crews.getCrews().size()).isGreaterThan(0);
+    }
+
+    @DisplayName("크루들의 출석 데이터를 받아올때 한 크루가 여러번 들어가지 않는다")
+    @Test
+    void test2() {
+        List<String> crewAttendanceResource = CrewAttendanceFileReader.readFile("src/main/resources/attendances.csv");
+        Crews crews = new Crews(crewAttendanceResource);
+        assertThat(crews.getCrews().stream().filter(crew -> crew.getNickname().equals("쿠키")).toList().size()).isEqualTo(1);
+    }
+
+    @DisplayName("크루의 이름을 통해서 크루를 찾을 수 있다")
+    @Test
+    void test3(){
+        List<String> crewAttendanceResource = CrewAttendanceFileReader.readFile("src/main/resources/attendances.csv");
+        Crews crews = new Crews(crewAttendanceResource);
+        Crew crew = crews.findCrewByNickname("쿠키").orElseThrow(()->new IllegalArgumentException("없는 닉네임입니다"));
+        assertThat(crew.getNickname()).isEqualTo("쿠키");
+    }
+    @DisplayName("크루의 이름을 통해서 크루를 찾을 수 있다")
+    @Test
+    void test4(){
+        List<String> crewAttendanceResource = CrewAttendanceFileReader.readFile("src/main/resources/attendances.csv");
+        Crews crews = new Crews(crewAttendanceResource);
+        Crew crew = crews.findCrewByNickname("이든").orElseThrow(()->new IllegalArgumentException("없는 닉네임입니다"));
+        assertThat(crew.getNickname()).isEqualTo("이든");
+    }
+
+
+}
