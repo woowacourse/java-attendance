a2880e466ccdd836cc5c8c0ef284f4bc18493ee4
sonjh919 <kingjh1125@gmail.com>
2025-02-25 09:45:20 +0900

    refactor: 매직 넘버 상수화 및 메서드 분리

--- src/main/java/policy/AbsentPolicy.java
+++ src/main/java/policy/AbsentPolicy.java
@@ -9,6 +9,12 @@ public class AbsentPolicy {
 
     private static final LocalDate CHRISTMAS = LocalDate.of(2024, 12, 25);
 
+    private static final int LATE_TIME_LIMIT = 31;
+    private static final int ATTENDANCE_TIME_LIMIT = 6;
+
+    private static final LocalTime MONDAY_EDUCATION_START_TIME = LocalTime.of(13, 0);
+    private static final LocalTime STANDARD_EDUCATION_START_TIME = LocalTime.of(10, 0);
+
     public void validateIsWeekend(DayOfWeek attendanceDayOfWeek) {
         if(attendanceDayOfWeek == DayOfWeek.SATURDAY || attendanceDayOfWeek == DayOfWeek.SUNDAY){
             throw new IllegalArgumentException("[ERROR] 주말에는 출석할 수 없습니다");
@@ -35,22 +41,30 @@ public class AbsentPolicy {
     }
 
     private static String calculateAttendanceStatus(LocalTime educationTime, LocalTime attendanceTime) {
-        if(educationTime.isBefore(attendanceTime.plusMinutes(6))){
+        if(isAttendanceTime(educationTime, attendanceTime)){
             return "출석";
         }
 
-        if(educationTime.isBefore(attendanceTime.plusMinutes(31))){
+        if(isLateTime(educationTime, attendanceTime)){
             return "지각";
         }
 
         return "결석";
     }
 
+    private static boolean isLateTime(LocalTime educationTime, LocalTime attendanceTime) {
+        return educationTime.isBefore(attendanceTime.plusMinutes(LATE_TIME_LIMIT));
+    }
+
+    private static boolean isAttendanceTime(LocalTime educationTime, LocalTime attendanceTime) {
+        return educationTime.isBefore(attendanceTime.plusMinutes(ATTENDANCE_TIME_LIMIT));
+    }
+
     private static LocalTime calculateAttendanceTime(DayOfWeek attendanceDayOfWeek) {
         if(attendanceDayOfWeek == DayOfWeek.MONDAY){
-            return LocalTime.of(13, 0);
+            return MONDAY_EDUCATION_START_TIME;
         }
-        return LocalTime.of(10, 0);
+        return STANDARD_EDUCATION_START_TIME;
     }
 
 }
