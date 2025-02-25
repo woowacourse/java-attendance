c3282aab9affbcd88a732ded9a175bcf001a9963
CheChe903 <cheche903@naver.com>
2025-02-25 15:35:26 +0900

    feat(AttendanceBookTest): 크루 이름 검증 로직 실패하는 테스트 작성
    출석부에 입력으로 들어온 크루가 존재하지 않는다면 예외가 발생합니다.

---
+++ src/test/java/attendance/domain/AttendanceBookTest.java
@@ -0,0 +1,26 @@
+package attendance.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AttendanceBookTest {
+
+    @DisplayName("입력된 이름이 출석부에 없다면 예외를 발생한다.")
+    @Test
+    void 입력된_이름이_출석부에_없다면_예외를_발생한다() {
+
+        // given
+        List<String> names = new ArrayList<>();
+        names.add("체체");
+        AttendanceBook attendanceBook = new AttendanceBook(names);
+
+        // when & then
+        assertThatThrownBy(() -> attendanceBook.hasCrew("추추"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 출석부에 존재하지 않는 닉네임입니다.");
+    }
+}
