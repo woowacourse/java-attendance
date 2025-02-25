1d64482aa7022a95f0cceea956c94ab1994fd7fd
songsunkook <songsunkook@gmail.com>
2025-02-25 14:42:14 +0900

    refactor: 이미 출석한 경우 다시 출석을 시도하면 예외를 반환한다

--- src/main/java/attendance/Crew.java
+++ src/main/java/attendance/Crew.java
@@ -19,9 +19,13 @@ public class Crew {
     }
 
     public void attendance(LocalDate date, LocalTime time) {
-        if (attendanceRecords.containsKey(date)) {
-            throw new IllegalArgumentException();
-        }
+        validateAttendance(date);
         attendanceRecords.put(date, time);
+    }
+
+    private void validateAttendance(LocalDate date) {
+        if (attendanceRecords.containsKey(date)) {
+            throw new IllegalArgumentException("이미 출석한 경우 다시 출석할 수 없습니다. 출석 수정 기능을 이용해 주세요.");
+        }
     }
 }
