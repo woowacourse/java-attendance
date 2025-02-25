5010ebd7189722ef7467eb3fbdcd972e11e58d34
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-26 00:34:43 +0900

    feat(AttendanceExpelDto): 출석 제적 정보 관련 객체 도입 및 테스트 추가

---
+++ src/main/java/attendance/dto/AttendanceExpelDto.java
@@ -0,0 +1,17 @@
+package attendance.dto;
+
+import attendance.domain.CrewStatistic;
+import attendance.domain.CrewStatistics;
+import java.util.ArrayList;
+import java.util.List;
+
+public record AttendanceExpelDto(List<AttendanceExpelRecord> attendanceExpelRecords) {
+
+    public static AttendanceExpelDto fromAttendanceExpelRecord(CrewStatistics crewStatistics) {
+        List<AttendanceExpelRecord> attendanceExpelRecords = new ArrayList<>();
+        for (CrewStatistic crewStatistic : crewStatistics.getCrewStatistics()) {
+            attendanceExpelRecords.add(AttendanceExpelRecord.fromCrewStatistic(crewStatistic));
+        }
+        return new AttendanceExpelDto(attendanceExpelRecords);
+    }
+}
