30a5d3ff3381bd0d5eef3afa7243a54a0cf28104
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-26 00:34:41 +0900

    feat: 출석 시작 시간이 늦어지는 특별한 날(월요일) 규칙 추가

---
+++ src/main/java/policy/date/rule/SpecialDayOfWeekRule.java
@@ -0,0 +1,31 @@
+package policy.date.rule;
+
+import java.time.DayOfWeek;
+import java.util.stream.Stream;
+
+public enum SpecialDayOfWeekRule {
+
+    DEEP_SLEEP(DayOfWeek.MONDAY, true, "우아한 숙면을 취하리"),
+    I_WANNABE_SPECIAL_DAY(DayOfWeek.FRIDAY, false, "금요일도 늦게 등교한다면 어떨까")
+    ;
+
+    private final DayOfWeek dayOfWeek;
+    private final boolean isActive;
+    private final String description;
+
+    SpecialDayOfWeekRule(DayOfWeek dayOfWeek, boolean isActive, String description) {
+        this.dayOfWeek = dayOfWeek;
+        this.isActive = isActive;
+        this.description = description;
+    }
+
+    public static boolean isSpecialDayOfWeek(DayOfWeek dayOfWeek) {
+        return Stream.of(SpecialDayOfWeekRule.values())
+                .filter(specialDay -> specialDay.isActive)
+                .anyMatch(specialDayOfWeek -> specialDayOfWeek.dayOfWeek.equals(dayOfWeek));
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/test/java/policy/date/rule/SpecialDayOfWeekRuleTest.java
@@ -0,0 +1,37 @@
+package policy.date.rule;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpecialDayOfWeekRuleTest {
+
+    @Test
+    @DisplayName("특별한 요일은 isSpecialDayOfWeek에서 true를 반환한다 (DayOfWeek)")
+    void specialDayReturnTrue() {
+        // given
+        DayOfWeek specialDayOfWeek = DayOfWeek.MONDAY;
+
+        // when
+        boolean isSpecialDayOfWeek = SpecialDayOfWeekRule.isSpecialDayOfWeek(specialDayOfWeek);
+
+        // then
+        assertThat(isSpecialDayOfWeek).isTrue();
+    }
+
+    @Test
+    @DisplayName("특별하지 않은 요일은 isSpecialDayOfWeek에서 false를 반환한다 (DayOfWeek)")
+    void nonSpecialDayReturnFalse() {
+        // given
+        DayOfWeek nonSpecialDayOfWeek = DayOfWeek.WEDNESDAY;
+
+        // when
+        boolean isSpecialDayOfWeek = SpecialDayOfWeekRule.isSpecialDayOfWeek(nonSpecialDayOfWeek);
+
+        // then
+        assertThat(isSpecialDayOfWeek).isFalse();
+    }
+}
