524b17ca7e06ac94b2ec4dc34afac3609f79e8b8
Firedrago95 <yonghwa95@naver.com>
2025-02-25 13:59:23 +0900

    chore: 2단계 미션을 위한 모든 코드 제거

--- src/main/java/attendance/dto/FileRequestDto.java
+++
@@ -1,7 +0,0 @@
-package attendance.dto;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-
-public record FileRequestDto(String name, LocalDate date, LocalTime time) {
-}
--- src/main/java/attendance/service/DateGenerator.java
+++
@@ -1,8 +0,0 @@
-package attendance.service;
-
-import java.time.LocalDate;
-
-public interface DateGenerator {
-
-    LocalDate generate();
-}
