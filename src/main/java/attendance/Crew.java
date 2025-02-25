46fba9b7c6545a874eda44fe9c81f49ed7418fac
kjyyjk <rlawnsdud920@gmail.com>
2025-02-25 22:34:14 +0900

    feat: 닉네임이 같으면 동일한 크루
    - 크루의 닉네임은 중복을 허용하지 않는다.
    따라서 Crew의 nickname이 같으면 같은 객체라고 판단한다.

---
+++ src/main/java/attendance/Crew.java
@@ -0,0 +1,28 @@
+package attendance;
+
+import java.util.Objects;
+
+public class Crew {
+    final String nickname;
+
+    public Crew(final String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Crew crew = (Crew) o;
+        return Objects.equals(nickname, crew.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nickname);
+    }
+}
---
+++ src/test/java/attendance/CrewTest.java
@@ -0,0 +1,38 @@
+package attendance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class CrewTest {
+    @Nested
+    class equals {
+        @DisplayName("닉네임이_같으면_true_를_반환한다")
+        @Test
+        void should_ReturnTrue_WhenNicknameIsSame() {
+            //given
+            Crew crew = new Crew("레오");
+
+            //when
+            boolean result = crew.equals(new Crew("레오"));
+
+            //then
+            assertThat(result).isTrue();
+        }
+
+        @DisplayName("닉네임이_다르면_false_를_반환한다")
+        @Test
+        void should_ReturnFalse_WhenNicknameIsNotSame() {
+            //given
+            Crew crew = new Crew("레오");
+
+            //when
+            boolean result = crew.equals(new Crew("오레오"));
+
+            //then
+            assertThat(result).isFalse();
+        }
+    }
+}
