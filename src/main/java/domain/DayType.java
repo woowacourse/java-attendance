391cbcfa14540bde197cde84648849c47ba30696
goohong <goohong.chung@gmail.com>
2025-02-25 17:49:56 +0900

    refactor: Calendar -> DayType으로 이름 변경

--- src/main/java/domain/DayType.java
+++ src/main/java/domain/DayType.java
@@ -4,24 +4,28 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-public enum Calendar {
+public enum DayType {
     MONDAYS(List.of(2, 9, 16, 23, 30)),
     SATURDAYS(List.of(7, 13, 21, 28)),
     SUNDAYS(List.of(1, 8, 15, 22, 29)),
     HOLIDAYS(List.of(25)),
-    WORKING_DAYS(null);
+    WORKING_DAYS(List.of());
 
     private final List<Integer> days;
 
-    Calendar(List<Integer> days) {
+    DayType(List<Integer> days) {
         this.days = days;
     }
 
-    public Calendar findByDate(LocalDate date) {
+    public static boolean matches(LocalDate date, DayType dayType) {
+        return findByDate(date).equals(dayType);
+    }
+
+    private static DayType findByDate(LocalDate date) {
         int day = date.getDayOfMonth();
 
-        return Arrays.stream(Calendar.values())
-                .filter(calendar -> calendar.containsDay(day))
+        return Arrays.stream(DayType.values())
+                .filter(dayType -> dayType.containsDay(day))
                 .findAny()
                 .orElse(WORKING_DAYS);
     }
