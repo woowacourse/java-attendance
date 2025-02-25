6318572c9c7c04e166cd80af37b6f6bed2ca46f2
kjyyjk <rlawnsdud920@gmail.com>
2025-02-25 18:07:00 +0900

    feat: 주어진 요일과 시간을 기반으로 출석 상태를 반환
    - 출석 정책과 관련된 책임을 가지는 AttendancePolicy는 넘겨 받은 요일과
    출석 시간을 기반으로 출석 상태를 반환할 수 있다.

---
+++ src/main/java/attendance/AttendancePolicy.java
@@ -0,0 +1,30 @@
+package attendance;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class AttendancePolicy {
+    private static final LocalTime MONDAY_ABSENCE_THRESHOLD = LocalTime.of(13, 30);
+    private static final LocalTime MONDAY_LATE_THRESHOLD = LocalTime.of(13, 05);
+    private static final LocalTime NOT_MONDAY_ABSENCE_THRESHOLD = LocalTime.of(10, 30);
+    private static final LocalTime NOT_MONDAY_LATE_THRESHOLD = LocalTime.of(10, 05);
+
+    public String calculateAttendanceStatus(DayOfWeek attendanceDay, LocalTime attendanceTime) {
+        if (attendanceDay == DayOfWeek.MONDAY) {
+            if (attendanceTime.isAfter(MONDAY_ABSENCE_THRESHOLD)) {
+                return "ABSENCE";
+            }
+            if (attendanceTime.isAfter(MONDAY_LATE_THRESHOLD)) {
+                return "LATE";
+            }
+            return "ATTENDANCE";
+        }
+        if (attendanceTime.isAfter(NOT_MONDAY_ABSENCE_THRESHOLD)) {
+            return "ABSENCE";
+        }
+        if (attendanceTime.isAfter(NOT_MONDAY_LATE_THRESHOLD)) {
+            return "LATE";
+        }
+        return "ATTENDANCE";
+    }
+}
