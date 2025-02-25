256e3c472ced2a8a53a85a56646ac1efa64c2142
AppleMint98 <applemint9821@gmail.com>
2025-02-25 15:58:07 +0900

    fix(DateInfoTest) : 변경된 생성 로직에 맞춰 테스트 수정

--- src/test/java/attendance/domain/DateInfoTest.java
+++ src/test/java/attendance/domain/DateInfoTest.java
@@ -1,82 +1,86 @@
 package attendance.domain;
 
-import attendance.domain.constant.DayOfWeek;
+import attendance.domain.constant.AttendanceStatus;
+import attendance.exception.ErrorMessage;
+import java.time.LocalDate;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class DateInfoTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"2,19,3,10:31,결석",
-            "2,20,4,10:05,출석",
-            "2,21,5,10:06,지각"})
-    void 결석_출석_지각_확인(int month, int day, int dayNumber, String timeNumber, String expectedStatus) {
+    @CsvSource(value = {"2025,2,19,10:31,결석",
+            "2025,2,20,10:05,출석",
+            "2025,2,21,10:06,지각"})
+    void 결석_출석_지각_확인(int year, int month, int day, String time, String expectedStatus) {
         //given
-        DayOfWeek dayOfWeek = DayOfWeek.from(dayNumber);
-        CampusTime campusTime = CampusTime.fromHourColonMinute(timeNumber);
-        DateInfo dateInfo = DateInfo.of(month, day, dayOfWeek, campusTime);
+        LocalDate now = LocalDate.of(year, month, day);
+        CampusTime campusTime = CampusTime.fromHourColonMinute(time);
+        DateInfo dateInfo = DateInfo.fromCampusTime(now, campusTime);
 
         //when
-        String attendanceStatus = dateInfo.getAttendanceStatus();
+        AttendanceStatus attendanceStatus = dateInfo.getAttendanceStatus();
 
         //then
-        Assertions.assertThat(attendanceStatus).isEqualTo(expectedStatus);
+        Assertions.assertThat(attendanceStatus.getStatus()).isEqualTo(expectedStatus);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"2,19,3,10:31,09:59,출석",
-            "2,19,3,10:31,10:11,지각",
-            "2,19,3,10:11,10:31,결석",
-            "2,19,3,10:11,09:50,출석",
-            "2,19,3,09:59,10:11,지각",
-            "2,19,3,09:59,10:31,결석"})
-    void 결석_출석으로_수정_확인(int month, int day, int dayNumber, String beforeTime, String afterTime, String expectedStatus) {
+    @CsvSource(value = {"2025,2,19,10:31,09:59,출석",
+            "2025,2,19,10:31,10:11,지각",
+            "2025,2,19,10:11,10:31,결석",
+            "2025,2,19,10:11,09:50,출석",
+            "2025,2,19,09:59,10:11,지각",
+            "2025,2,19,09:59,10:31,결석"})
+    void 결석_출석으로_수정_확인(int year, int month, int day, String beforeTime, String afterTime, String expectedStatus) {
         //given
-        DayOfWeek dayOfWeek = DayOfWeek.from(dayNumber);
-        CampusTime prevCampusTime = CampusTime.fromHourColonMinute(beforeTime);
-        DateInfo dateInfo = DateInfo.of(month, day, dayOfWeek, prevCampusTime);
+        LocalDate now = LocalDate.of(year, month, day);
+        CampusTime beforeCampusTime = CampusTime.fromHourColonMinute(beforeTime);
+        DateInfo beforeDateInfo = DateInfo.fromCampusTime(now, beforeCampusTime);
         CampusTime modifyCampusTime = CampusTime.fromHourColonMinute(afterTime);
+
         //when
-        dateInfo.modifyAttendanceTime(modifyCampusTime);
+        beforeDateInfo.modifyAttendanceTime(modifyCampusTime);
 
         //then
-        Assertions.assertThat(dateInfo.getAttendanceStatus()).isEqualTo(expectedStatus);
+        Assertions.assertThat(beforeDateInfo.getAttendanceStatus().getStatus()).isEqualTo(expectedStatus);
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"2,19,3,10:31,1", "2,19,3,09:59,0", "2,19,3,10:11,0"})
-    void 결석_개수_반환(int month, int day, int dayNumber, String timeNumber, int expectedStatus) {
-        //given
-        DayOfWeek dayOfWeek = DayOfWeek.from(dayNumber);
-        CampusTime campusTime = CampusTime.fromHourColonMinute(timeNumber);
-        DateInfo dateInfo = DateInfo.of(month, day, dayOfWeek, campusTime);
+    @Test
+    void 주말_정보가_들어오면_예외_반환() {
+        // given
+        LocalDate now = LocalDate.of(2025, 2, 23);
+        CampusTime campusTime = CampusTime.fromHourColonMinute("10:31");
 
-        //when & then
-        Assertions.assertThat(dateInfo.checkAbsenceStatus()).isEqualTo(expectedStatus);
+        // when // then
+        Assertions.assertThatThrownBy(() -> DateInfo.fromCampusTime(now, campusTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NOT_WEEKEND.getMessage());
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"2,19,3,10:11,1", "2,19,3,09:59,0", "2,19,3,10:31,0"})
-    void 지각_개수_반환(int month, int day, int dayNumber, String timeNumber, int expectedStatus) {
-        //given
-        DayOfWeek dayOfWeek = DayOfWeek.from(dayNumber);
-        CampusTime campusTime = CampusTime.fromHourColonMinute(timeNumber);
-        DateInfo dateInfo = DateInfo.of(month, day, dayOfWeek, campusTime);
+    @Test
+    void 해당_정보가_존재하면_true_반환() {
+        // given
+        LocalDate now = LocalDate.of(2025, 2, 25);
+        CampusTime campusTime = CampusTime.fromHourColonMinute("10:31");
+        DateInfo dateInfo = DateInfo.fromCampusTime(now, campusTime);
 
-        //when & then
-        Assertions.assertThat(dateInfo.checkLateStatus()).isEqualTo(expectedStatus);
+        // when // then
+        Assertions.assertThat(dateInfo.isAttendanceDay(25)).isTrue();
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"2,19,3,10:11,0", "2,19,3,09:59,1", "2,19,3,10:31,0"})
-    void 출석_개수_반환(int month, int day, int dayNumber, String timeNumber, int expectedStatus) {
-        //given
-        DayOfWeek dayOfWeek = DayOfWeek.from(dayNumber);
-        CampusTime campusTime = CampusTime.fromHourColonMinute(timeNumber);
-        DateInfo dateInfo = DateInfo.of(month, day, dayOfWeek, campusTime);
+    @Test
+    void 해당_정보가_존재하면_false_반환() {
+        // given
+        LocalDate now = LocalDate.of(2025, 2, 25);
+        CampusTime campusTime = CampusTime.fromHourColonMinute("10:31");
+        DateInfo dateInfo = DateInfo.fromCampusTime(now, campusTime);
 
-        //when & then
-        Assertions.assertThat(dateInfo.checkAttendanceStatus()).isEqualTo(expectedStatus);
+        // when // then
+        Assertions.assertThat(dateInfo.isAttendanceDay(24)).isFalse();
     }
+
 }
+
