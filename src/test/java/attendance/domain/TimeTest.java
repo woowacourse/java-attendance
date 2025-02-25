262f9a95ef27fe6773512fc5fdc351b5dff26325
CheChe903 <cheche903@naver.com>
2025-02-25 16:40:17 +0900

    test(TimeTest): 캠퍼스 운영 시간 검증 테스트 작성

--- src/test/java/attendance/domain/TimeTest.java
+++ src/test/java/attendance/domain/TimeTest.java
@@ -1,5 +1,6 @@
 package attendance.domain;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDateTime;
@@ -7,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TimeTest {
-
 
     @DisplayName("캠퍼스 운영 시간 외의 시간이 들어올 경우 예외를 발생한다.")
     @Test
@@ -19,6 +19,18 @@ class TimeTest {
         assertThatThrownBy(() -> new Time(LocalDateTime.of(2025, 2, 25, 7, 30)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 캠퍼스 운영 시간이 아닙니다.");
+    }
+
+    @DisplayName("캠퍼스 운영 시간에 들어올 경우 예외를 발생하지 않는다.")
+    @Test
+    void 캠퍼스_운영_시간에_들어올_경우_예외를_발생하지_않는다() {
+
+        // given
+
+        // when & then
+        assertThatCode(() -> {
+            Time time = new Time(LocalDateTime.of(2025, 2, 25, 8, 0));
+        }).doesNotThrowAnyException();
     }
 
 }
