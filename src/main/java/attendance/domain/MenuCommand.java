bb43674fa19d6221ac511702c19c694fe5121b3d
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-26 00:28:27 +0900

    refactor(MenuCommand): 옵션별 객체 관련 필드 추가 및 기본값 삭제

--- src/main/java/attendance/domain/MenuCommand.java
+++ src/main/java/attendance/domain/MenuCommand.java
@@ -1,22 +1,27 @@
 package attendance.domain;
 
+import attendance.controller.OptionCheckAttendance;
+import attendance.controller.OptionLookupAttendance;
+import attendance.controller.OptionLookupExpulsion;
+import attendance.controller.OptionModifyAttendance;
 import java.util.Arrays;
 
 public enum MenuCommand {
-    ATTEND("1"),
-    MODIFY("2"),
-    LOOKUP("3"),
-    EXPEL("4"),
-    QUIT("Q"),
-    NONE("None");
+    ATTEND("1", OptionCheckAttendance.class),
+    MODIFY("2", OptionModifyAttendance.class),
+    LOOKUP("3", OptionLookupAttendance.class),
+    EXPEL("4", OptionLookupExpulsion.class),
+    QUIT("Q", null);
 
     private static final String MENU_COMMAND_ERROR_MESSAGE = "[ERROR] 지원하지 않는 기능 값입니다.";
     private static final String COMMAND_PATTERN = "^[1-4|Q]$";
 
     private final String command;
+    private final Class<?> option;
 
-    MenuCommand(String command) {
+    MenuCommand(String command, Class<?> option) {
         this.command = command;
+        this.option = option;
     }
 
     public static MenuCommand toCommand(final String input) {
@@ -31,5 +36,9 @@ public enum MenuCommand {
 
     private String getCommand() {
         return command;
+    }
+
+    public Class<?> getOption() {
+        return option;
     }
 }
