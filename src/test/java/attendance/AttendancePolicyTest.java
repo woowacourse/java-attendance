3f6ad5d138617e1d5aa789ed15f8dbcffefffa84
kjyyjk <rlawnsdud920@gmail.com>
2025-02-25 21:29:43 +0900

    refactor: Nested 중첩클래스를 활용해 테스트 그룹화

--- src/test/java/attendance/AttendancePolicyTest.java
+++ src/test/java/attendance/AttendancePolicyTest.java
@@ -5,96 +5,101 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class AttendancePolicyTest {
-    @DisplayName("월요일_교육_시작_시간으로부터_5분_이내_출석_시_ATTENDANCE_를_반환한다")
-    @Test
-    void should_ReturnAttendance_WhenMondayAttendance() {
-        //given
-        AttendancePolicy attendancePolicy = new AttendancePolicy();
-        DayOfWeek attendanceDay = DayOfWeek.MONDAY;
-        LocalTime attendanceTime = LocalTime.of(13, 05);
+    @DisplayName("출석_시간에_따른_출석_상태_반환")
+    @Nested
+    class calculateAttendanceStatus {
+        @DisplayName("월요일_교육_시작_시간으로부터_5분_이내_출석_시_ATTENDANCE_를_반환한다")
+        @Test
+        void should_ReturnAttendance_WhenMondayAttendance() {
+            //given
+            AttendancePolicy attendancePolicy = new AttendancePolicy();
+            DayOfWeek attendanceDay = DayOfWeek.MONDAY;
+            LocalTime attendanceTime = LocalTime.of(13, 05);
 
-        //when
-        String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
+            //when
+            String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
 
-        //then
-        assertThat(result).isEqualTo("ATTENDANCE");
-    }
+            //then
+            assertThat(result).isEqualTo("ATTENDANCE");
+        }
 
-    @DisplayName("월요일_출석_시간이_교육_시작_시간으로부터_5분_초과_시_LATE_를_반환한다")
-    @Test
-    void should_ReturnLate_WhenMondayLate() {
-        //given
-        AttendancePolicy attendancePolicy = new AttendancePolicy();
-        DayOfWeek attendanceDay = DayOfWeek.MONDAY;
-        LocalTime attendanceTime = LocalTime.of(13, 06);
+        @DisplayName("월요일_출석_시간이_교육_시작_시간으로부터_5분_초과_시_LATE_를_반환한다")
+        @Test
+        void should_ReturnLate_WhenMondayLate() {
+            //given
+            AttendancePolicy attendancePolicy = new AttendancePolicy();
+            DayOfWeek attendanceDay = DayOfWeek.MONDAY;
+            LocalTime attendanceTime = LocalTime.of(13, 06);
 
-        //when
-        String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
+            //when
+            String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
 
-        //then
-        assertThat(result).isEqualTo("LATE");
-    }
+            //then
+            assertThat(result).isEqualTo("LATE");
+        }
 
-    @DisplayName("월요일_출석_시간이_교육_시작_시간으로부터_30분_초과_시_ABSENCE_를_반환한다")
-    @Test
-    void should_ReturnAbsence_WhenMondayAbsence() {
-        //given
-        AttendancePolicy attendancePolicy = new AttendancePolicy();
-        DayOfWeek attendanceDay = DayOfWeek.MONDAY;
-        LocalTime attendanceTime = LocalTime.of(13, 31);
+        @DisplayName("월요일_출석_시간이_교육_시작_시간으로부터_30분_초과_시_ABSENCE_를_반환한다")
+        @Test
+        void should_ReturnAbsence_WhenMondayAbsence() {
+            //given
+            AttendancePolicy attendancePolicy = new AttendancePolicy();
+            DayOfWeek attendanceDay = DayOfWeek.MONDAY;
+            LocalTime attendanceTime = LocalTime.of(13, 31);
 
-        //when
-        String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
+            //when
+            String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
 
-        //then
-        assertThat(result).isEqualTo("ABSENCE");
-    }
+            //then
+            assertThat(result).isEqualTo("ABSENCE");
+        }
 
-    @DisplayName("월요일_아닌_출석_시간이_교육_시작_시간으로부터_5분_이내_출석_시_ATTENDANCE_를_반환한다")
-    @Test
-    void should_ReturnAttendance_WhenNotMondayAttendance() {
-        //given
-        AttendancePolicy attendancePolicy = new AttendancePolicy();
-        DayOfWeek attendanceDay = DayOfWeek.WEDNESDAY;
-        LocalTime attendanceTime = LocalTime.of(10, 05);
+        @DisplayName("월요일_아닌_출석_시간이_교육_시작_시간으로부터_5분_이내_출석_시_ATTENDANCE_를_반환한다")
+        @Test
+        void should_ReturnAttendance_WhenNotMondayAttendance() {
+            //given
+            AttendancePolicy attendancePolicy = new AttendancePolicy();
+            DayOfWeek attendanceDay = DayOfWeek.WEDNESDAY;
+            LocalTime attendanceTime = LocalTime.of(10, 05);
 
-        //when
-        String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
+            //when
+            String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
 
-        //then
-        assertThat(result).isEqualTo("ATTENDANCE");
-    }
+            //then
+            assertThat(result).isEqualTo("ATTENDANCE");
+        }
 
-    @DisplayName("월요일_아닌_출석_시간이_교육_시작_시간으로부터_5분_초과_시_LATE_를_반환한다")
-    @Test
-    void should_ReturnLate_WhenNotMondayLate() {
-        //given
-        AttendancePolicy attendancePolicy = new AttendancePolicy();
-        DayOfWeek attendanceDay = DayOfWeek.WEDNESDAY;
-        LocalTime attendanceTime = LocalTime.of(10, 06);
+        @DisplayName("월요일_아닌_출석_시간이_교육_시작_시간으로부터_5분_초과_시_LATE_를_반환한다")
+        @Test
+        void should_ReturnLate_WhenNotMondayLate() {
+            //given
+            AttendancePolicy attendancePolicy = new AttendancePolicy();
+            DayOfWeek attendanceDay = DayOfWeek.WEDNESDAY;
+            LocalTime attendanceTime = LocalTime.of(10, 06);
 
-        //when
-        String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
+            //when
+            String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
 
-        //then
-        assertThat(result).isEqualTo("LATE");
-    }
+            //then
+            assertThat(result).isEqualTo("LATE");
+        }
 
-    @DisplayName("월요일_아닌_출석_시간이_교육_시작_시간으로부터_30분_초과_시_ABSENCE_를_반환한다")
-    @Test
-    void should_ReturnAbsence_WhenNotMondayAbsence() {
-        //given
-        AttendancePolicy attendancePolicy = new AttendancePolicy();
-        DayOfWeek attendanceDay = DayOfWeek.WEDNESDAY;
-        LocalTime attendanceTime = LocalTime.of(10, 31);
+        @DisplayName("월요일_아닌_출석_시간이_교육_시작_시간으로부터_30분_초과_시_ABSENCE_를_반환한다")
+        @Test
+        void should_ReturnAbsence_WhenNotMondayAbsence() {
+            //given
+            AttendancePolicy attendancePolicy = new AttendancePolicy();
+            DayOfWeek attendanceDay = DayOfWeek.WEDNESDAY;
+            LocalTime attendanceTime = LocalTime.of(10, 31);
 
-        //when
-        String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
+            //when
+            String result = attendancePolicy.calculateAttendanceStatus(attendanceDay, attendanceTime);
 
-        //then
-        assertThat(result).isEqualTo("ABSENCE");
+            //then
+            assertThat(result).isEqualTo("ABSENCE");
+        }
     }
 }
