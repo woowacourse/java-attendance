9d2752ca11a767859138f560b04c120d3e3f16e1
LJM\eueo8 <eueo8259@gmail.com>
2025-02-25 12:51:46 +0900

    refactor : 출결 판단 시간을 AttendanceStatus 필드에 추가 후 계산하여 반환하는 메서드 추가

--- src/main/java/model/AttendanceStatus.java
+++ src/main/java/model/AttendanceStatus.java
@@ -2,17 +2,29 @@ package model;
 
 public enum AttendanceStatus {
 
-    ABSENT("결석"),
-    ATTENDANCE("출석"),
-    LATE("지각");
+    ABSENT("결석", 30),
+    ATTENDANCE("출석",0),
+    LATE("지각", 5);
 
     private final String state;
+    private final int attendanceJudgementTime;
 
-    AttendanceStatus(String state) {
+    AttendanceStatus(String state, int attendanceJudgementTime) {
         this.state = state;
+        this.attendanceJudgementTime = attendanceJudgementTime;
     }
 
     public String getState() {
         return state;
+    }
+
+    public static AttendanceStatus fromMinutesLate(int minutesLate) {
+        if (minutesLate > ABSENT.attendanceJudgementTime) {
+            return ABSENT;
+        }
+        if (minutesLate > LATE.attendanceJudgementTime) {
+            return LATE;
+        }
+        return ATTENDANCE;
     }
 }
