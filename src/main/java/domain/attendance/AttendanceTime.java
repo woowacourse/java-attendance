3340ade648fc8136b58b947c05ceb5736ac816a7
jumdo12 <jumdo12@gmail.com>
2025-02-25 16:01:02 +0900

    refactor: 매직넘버 상수 처리

--- src/main/java/domain/attendance/AttendanceTime.java
+++ src/main/java/domain/attendance/AttendanceTime.java
@@ -11,12 +11,16 @@ public enum AttendanceTime {
     FRI(10, 5, 5),
     ;
 
+    private static final int CAMPUS_OPENING_TIME = 8;
+    private static final int CAMPUS_CLOSING_TIME = 22;
+    private static final int CAMPUS_ABSENCE_OVER_TIME = 25;
+
     private final int hour;
     private final int minute;
     private final int dayOfWeek;
 
     AttendanceTime(int hour, int minute, int dayOfWeek) {
-        if (hour < 8 || hour > 22) {
+        if (hour < CAMPUS_OPENING_TIME || hour > CAMPUS_CLOSING_TIME) {
             throw new IllegalArgumentException("캠퍼스 운영 시간에만 출석이 가능합니다.");
         }
 
@@ -36,6 +40,6 @@ public enum AttendanceTime {
         return Arrays.stream(values())
                 .filter(value -> value.dayOfWeek == dayOfWeek)
                 .anyMatch(value ->value.hour < dateTime.getHour() || (value.hour == dateTime.getHour()
-                        && value.minute + 25 < dateTime.getMinute()));
+                        && value.minute + CAMPUS_ABSENCE_OVER_TIME < dateTime.getMinute()));
     }
 }
