70362c06abcbcc5d2d83035e3b600b5257a27686
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-25 17:11:36 +0900

    green test : 수정 날짜에 맞는 Attendance 객체 반환 TDD

--- src/test/java/test/AttendanceModifyTest.java
+++ src/test/java/test/AttendanceModifyTest.java
@@ -1,6 +1,17 @@
 package test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import model.Attendance;
+import model.AttendanceInitializer;
+import model.Attendances;
+import model.Crew;
+import model.Crews;
 import model.DateGenerator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +21,7 @@ public class AttendanceModifyTest {
 
     @DisplayName("수정하려는 날짜를 LocalDate 객체로 반환한다.")
     @Test
-    void test() {
+    void test() { //TODO : 좀 필요없는 테스트임 (a=a 테스트)
         //given
         int rawDate = 3;
 
@@ -18,7 +29,24 @@ public class AttendanceModifyTest {
         LocalDate date = DateGenerator.create(rawDate);
 
         //then
-        Assertions.assertThat(date).isEqualTo(LocalDate.of(2024, 12, rawDate));
+        assertThat(date).isEqualTo(LocalDate.of(2024, 12, rawDate));
+    }
+
+    //TODO : 이미 있는 메서드였음
+    @DisplayName("수정하려는 날짜에 맞는 Attendance 객체를 찾는다.")
+    @Test
+    void test1() {
+        //given
+        LocalDate modifyDate = LocalDate.of(2024, 12, 13);
+        Attendances attendances = new Attendances(List.of(
+                new Attendance(modifyDate, LocalTime.of(10, 10))
+        ));
+
+        //when
+        Attendance attendance = attendances.findByDate(modifyDate);
+
+        //then
+        assertThat(attendance.equals(new Attendance(modifyDate, LocalTime.of(10, 10))));
     }
 
 }
