faf5b5af770c54a1ce298964c97c2f0e223ee76d
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-25 17:03:43 +0900

    red test : 일을 입력하면 날짜를 생성 TDD

---
+++ src/main/java/model/DateGenerator.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.time.LocalDate;
+
+public class DateGenerator {
+    public static LocalDate create(int rawDate) {
+        return null;
+    }
+}
--- src/test/java/test/AttendanceModifyTest.java
+++ src/test/java/test/AttendanceModifyTest.java
@@ -1,7 +1,27 @@
 package test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import model.Attendance;
+import model.AttendanceInitializer;
+import model.DateGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class AttendanceModifyTest {
+
+    @DisplayName("수정하려는 날짜를 LocalDate 객체로 반환한다.")
+    @Test
+    void test() {
+        //given
+        int rawDate = 3;
+
+        //when
+        LocalDate date = DateGenerator.create(rawDate);
+
+        //then
+        Assertions.assertThat(date).isEqualTo(LocalDate.of(2024, 12, rawDate));
+    }
 
 }
