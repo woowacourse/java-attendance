d083c521a882addced3755adcf4b8425241a34fb
sonjh919 <kingjh1125@gmail.com>
2025-02-25 15:11:20 +0900

    test(refactor): 제적 정책 테스트 수정
    - 결석 회수가 5회 초과이면 제적 대상자이다. 지각 3회는 결석 1회로 간주한다.

--- src/test/java/ExpellPolicy.java
+++ src/test/java/ExpellPolicy.java
@@ -1,8 +1,12 @@
 public class ExpellPolicy {
-    public String checkExpellStatus(int lateCount, int absentCount) {
-        absentCount += lateCount/3;
 
-        if(absentCount > 5) {
+    private static final int LATE_ABSENT_RATE = 3;
+    private static final int EXPELL_LIMIT = 5;
+
+    public String checkExpellStatus(int lateCount, int absentCount) {
+        absentCount += lateCount/ LATE_ABSENT_RATE;
+
+        if(absentCount > EXPELL_LIMIT) {
             return "제적";
         }
         return null;
