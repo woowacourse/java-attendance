e9bb0e605221d9df5228ee03c8fa3abf4bb2a2a7
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-25 13:55:58 +0900

    test(red): 특정 기간 사이 출석 기록 조회 레드 테스트 작성

--- src/main/java/attendance/model/attendance/AttendanceLogs.java
+++ src/main/java/attendance/model/attendance/AttendanceLogs.java
@@ -1,5 +1,6 @@
 package attendance.model.attendance;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class AttendanceLogs {
@@ -14,4 +15,8 @@ public class AttendanceLogs {
     //LocalDate from 부터 LocalDate to 내에 등교일인데 출석하지 않은 날에 대한 AttendanceLog 들과 함께 AttendanceLog 리스트를 반환한다.
     //LocalDate from 부터 LocalDate to 내에 크루 출석 데이터를 종합하여 Map<AttendanceStatus, Integer> 를 반환한다.
     //Map<AttendanceStatus, Integer> 에는 각 AttendanceStatus 가 몇 개 있는지 저장한다.
+
+    public List<AttendanceLog> getAllAttendanceLogs(final LocalDate from, final LocalDate to) {
+        return null;
+    }
 }
---
+++ src/test/java/attendance/model/attendance/AttendanceLogsTest.java
@@ -0,0 +1,79 @@
+package attendance.model.attendance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import attendance.model.campus.CampusOperationPolicy;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class AttendanceLogsTest {
+
+    private static final CampusOperationPolicy campusOperationPolicy = new CampusOperationPolicy();
+    private static AttendanceLogs attendanceLogs;
+
+    @BeforeAll
+    static void setUp() {
+        List<AttendanceLog> values = new ArrayList<>();
+        values.add(
+                AttendanceLog.fromAttendanceDateTime(
+                        LocalDateTime.of(2024, 12, 2, 13, 0),
+                        campusOperationPolicy
+                )
+        );
+        values.add(
+                AttendanceLog.fromAttendanceDateTime(
+                        LocalDateTime.of(2024, 12, 4, 10, 5),
+                        campusOperationPolicy
+                )
+        );
+        values.add(
+                AttendanceLog.fromAttendanceDateTime(
+                        LocalDateTime.of(2024, 12, 5, 11, 0),
+                        campusOperationPolicy
+                )
+        );
+        attendanceLogs = new AttendanceLogs(values);
+    }
+
+    @Test
+    void getAllAttendanceLogs() {
+
+        // Given
+        final LocalDate from = LocalDate.of(2024, 12, 2);
+        final LocalDate to = LocalDate.of(2024, 12, 5);
+        final List<AttendanceLog> expected = new ArrayList<>();
+        expected.add(
+                AttendanceLog.fromAttendanceDateTime(
+                        LocalDateTime.of(2024, 12, 2, 13, 0),
+                        campusOperationPolicy
+                )
+        );
+        expected.add(
+                AttendanceLog.fromAbsenceDate(
+                        LocalDate.of(2024, 12, 3)
+                )
+        );
+        expected.add(
+                AttendanceLog.fromAttendanceDateTime(
+                        LocalDateTime.of(2024, 12, 4, 10, 5),
+                        campusOperationPolicy
+                )
+        );
+        expected.add(
+                AttendanceLog.fromAttendanceDateTime(
+                        LocalDateTime.of(2024, 12, 5, 11, 0),
+                        campusOperationPolicy
+                )
+        );
+
+        // When
+        final List<AttendanceLog> actual = attendanceLogs.getAllAttendanceLogs(from, to);
+
+        // Then
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+}
