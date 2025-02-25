ec59c9fa14a7fdc610f4502d8b29d14cd8857478
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-26 00:07:00 +0900

    style: AttendanceDateTest 내부 불필요한 주석 제거

--- src/test/java/attendance/model/attendance/datetime/date/AttendanceDateTest.java
+++ src/test/java/attendance/model/attendance/datetime/date/AttendanceDateTest.java
@@ -8,15 +8,6 @@ import org.junit.jupiter.api.Test;
 
 class AttendanceDateTest {
 
-    //- **상태**
-    //    - LocalDate 출석 일자
-    //- **생성**
-    //    - LocalDate 와 CampusOperationPolicy 를 받아 생성된다.
-    //- **동작**
-    //    - 생성 시 CampusOperationPolicy 를 통해 캠퍼스 운영 날짜인지 검증한다.
-    //    - LocalDate 를 받아 해당 LocalDate 와 자신이 같은 날인지 반환한다.
-    //    - DayOfWeek 를 받아 해당 요일이 자신의 요일인지 반환한다.
-
     private final CampusOperationPolicy campusOperationPolicy = new CampusOperationPolicy();
 
     @Test
