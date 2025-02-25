30f66fb7b95df1a6c219e3b6ec07041994858233
threepebbles <threepebbles@naver.com>
2025-02-25 14:30:45 +0900

    green6: 해당 날짜에 맞는 교육 시간을 계산한다

--- src/main/java/constant/LectureTime.java
+++ src/main/java/constant/LectureTime.java
@@ -3,7 +3,9 @@ package constant;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.TextStyle;
 import java.util.Arrays;
+import java.util.Locale;
 
 public enum LectureTime {
     MONDAY(DayOfWeek.MONDAY, LocalTime.of(13, 0), LocalTime.of(18, 0)),
@@ -30,6 +32,13 @@ public enum LectureTime {
 
     // TODO: 날짜에 맞는 교육 시간을 반환한다, 교육 날이 아니면 예외를 발생시킨다
     public static LectureTime from(LocalDate date) {
-        return LectureTime.MONDAY;
+        return Arrays.stream(values())
+                .filter(lectureTime -> lectureTime.dayOfWeek == date.getDayOfWeek())
+                .findAny()
+                .orElseThrow(
+                        () -> new IllegalArgumentException(date.getDayOfWeek()
+                                .getDisplayName(TextStyle.SHORT, Locale.KOREAN)
+                                + ": 교육이 없는 요일입니다.")
+                );
     }
 }
