4e09214d9884e62c134b6091a1f49a73c0b694ee
CheChe903 <cheche903@naver.com>
2025-02-25 16:37:39 +0900

    feat(Time): 캠퍼스 운영 시간 검증 로직 구현
    08:00 - 23:00 에만 운영합니다.

--- src/main/java/attendance/domain/Time.java
+++ src/main/java/attendance/domain/Time.java
@@ -7,6 +7,13 @@ public class Time {
     private final LocalDateTime attendanceTime;
 
     public Time(LocalDateTime attendanceTime) {
+        validateCampusOperationTime(attendanceTime);
         this.attendanceTime = attendanceTime;
+    }
+
+    private void validateCampusOperationTime(LocalDateTime attendanceTime) {
+        if (attendanceTime.getHour() < 8 || (attendanceTime.getHour() == 23 && attendanceTime.getMinute() > 0)) {
+            throw new IllegalArgumentException("[ERROR] 캠퍼스 운영 시간이 아닙니다.");
+        }
     }
 }
