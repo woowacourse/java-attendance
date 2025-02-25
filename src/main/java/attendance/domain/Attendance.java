6e2c1549190694609c120a8823712fd07419c1da
Firedrago95 <yonghwa95@naver.com>
2025-02-25 19:56:47 +0900

    feat: 캠퍼스 운영시간 외에 출석 예외 발생 기능 구현

--- src/main/java/attendance/domain/Attendance.java
+++ src/main/java/attendance/domain/Attendance.java
@@ -5,11 +5,15 @@ import java.time.LocalTime;
 
 public class Attendance {
 
+    private static final LocalTime CAMPUS_OPEN_TIME = LocalTime.of(8,0);
+    private static final LocalTime CAMPUS_CLOSE_TIME = LocalTime.of(23, 0);
+
     private final LocalDate attendanceDate;
     private final LocalTime attendanceTime;
 
     public Attendance(LocalDate attendanceDate, LocalTime attendanceTime) {
         Holiday.check(attendanceDate);
+        validateCampusOperatingHours(attendanceTime);
         this.attendanceDate = attendanceDate;
         this.attendanceTime = attendanceTime;
     }
@@ -17,5 +21,11 @@ public class Attendance {
     public boolean hasAttend(LocalDate attendanceDate, LocalTime attendanceTime) {
         return this.attendanceDate.isEqual(attendanceDate)
             && this.attendanceTime.equals(attendanceTime);
+    }
+
+    private void validateCampusOperatingHours(LocalTime attendanceTime) {
+        if (attendanceTime.isBefore(CAMPUS_OPEN_TIME) || attendanceTime.isAfter(CAMPUS_CLOSE_TIME)) {
+            throw new IllegalArgumentException("[ERROR] 캠퍼스 운영시간이 아닙니다.");
+        }
     }
 }
--- src/test/java/attendance/domain/AttendanceTest.java
+++ src/test/java/attendance/domain/AttendanceTest.java
@@ -2,8 +2,35 @@ package attendance.domain;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class AttendanceTest {
 
+    @Test
+    void 출석시간이_캠퍼스_오픈전이면_예외가_발생한다() {
+        // given
+        LocalDate friday = LocalDate.of(2024, 12, 6);
+        LocalTime beforeOpen = LocalTime.of(7, 59);
+
+        assertThatThrownBy(() -> new Attendance(friday, beforeOpen))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 캠퍼스 운영시간이 아닙니다.");
+    }
+
+    @Test
+    void 출석시간이_캠퍼스_종료후면_예외가_발생한다() {
+        // given
+        LocalDate friday = LocalDate.of(2024, 12, 6);
+        LocalTime afterClose = LocalTime.of(23, 1);
+
+        assertThatThrownBy(() -> new Attendance(friday, afterClose))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 캠퍼스 운영시간이 아닙니다.");
+    }
 }
