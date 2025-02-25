0b3ae056267cba74cd9c6866fee0ef6ea002daa0
threepebbles <threepebbles@naver.com>
2025-02-25 14:50:04 +0900

    green2: 날짜와 시간으로 알맞는 출석 상태를 반환한다

--- src/main/java/constant/AttendanceStatus.java
+++ src/main/java/constant/AttendanceStatus.java
@@ -2,11 +2,13 @@ package constant;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public enum AttendanceStatus {
-    ATTENDANCE("출석", "정상 등교", Integer.MIN_VALUE),
-    LATE("지각", "5분 초과", 5),
-    ABSENT_LATE("결석", "30분 초과", 30),
+    ATTENDANCE("출석", "정상 등교", 0),
+    LATE("지각", "6분 이상 늦음", 5 + 1),
+    ABSENT_LATE("결석", "31분 이상 늦음", 30 + 1),
     ABSENT("결석", "출석 기록 없음", Integer.MAX_VALUE),
     ;
 
@@ -20,8 +22,19 @@ public enum AttendanceStatus {
         this.elapsedMinutesLimit = elapsedMinutesLimit;
     }
 
-    // TODO: 날짜와 시간으로 출석 상태를 계산한다
     public static AttendanceStatus of(LocalDate date, LocalTime time) {
-        return ATTENDANCE;
+        if (Holiday.isHoliday(date) || !LectureTime.isLectureDate(date)) {
+            throw new IllegalArgumentException(date + ": 교육이 없는 날입니다.");
+        }
+        
+        int elapsedMinutes = LectureTime.calculateElapsedMinutes(date, time);
+        return Arrays.stream(values())
+                .filter(status -> status.elapsedMinutesLimit <= elapsedMinutes)
+                .max(Comparator.comparing(AttendanceStatus::getElapsedMinutesLimit))
+                .orElseThrow(() -> new IllegalArgumentException("논리적으로 발생할 수 없는 예외입니다."));
+    }
+
+    public int getElapsedMinutesLimit() {
+        return elapsedMinutesLimit;
     }
 }
--- src/test/java/constant/AttendanceStatusTest.java
+++ src/test/java/constant/AttendanceStatusTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class AttendanceStatusTest {
     @Test
     @DisplayName("날짜와 시간으로 알맞는 출석 상태를 반환한다")
-    void of() {
+    void ofTest() {
         // given
         LocalDate monday = LocalDate.of(2025, 2, 3);
         LocalTime mondayAttendanceTime = LocalTime.of(13, 1);
