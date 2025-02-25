e5fd59b36f27a10585ec6a347f742431c62a0e8e
threepebbles <threepebbles@naver.com>
2025-02-25 14:08:00 +0900

    red1: 닉네임으로 존재하는 크루인지 확인한다

---
+++ src/main/java/domain/Crew.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Crew {
+    private String nickname;
+
+    public Crew(String nickname) {
+        this.nickname = nickname;
+    }
+}
---
+++ src/main/java/repository/CrewRepository.java
@@ -0,0 +1,18 @@
+package repository;
+
+import domain.Crew;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CrewRepository {
+    private static final List<Crew> CREWS = new ArrayList<>();
+
+    // TODO: 크루 추가
+    public static void addCrew(Crew crew) {
+    }
+
+    // TODO: 크루 존재 여부 확인
+    public static boolean existsCrew(String nickname) {
+        return true;
+    }
+}
