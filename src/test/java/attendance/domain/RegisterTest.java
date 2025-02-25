bcaefe32a7e1aa17adf0a6205c53c6cb4848ae44
bowook <mbosx316@naver.com>
2025-02-25 13:26:49 +0900

    test(RegisterTest) : 캠퍼스 운영 시간 검증 테스트

--- src/test/java/attendance/domain/RegisterTest.java
+++ src/test/java/attendance/domain/RegisterTest.java
@@ -15,6 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RegisterTest {
 
