1c9a290c8904f1b8c819ef46258ed006add9cb7b
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-26 00:35:32 +0900

    feat: 공휴일 규칙 추가

---
+++ src/test/java/policy/date/rule/HolidayRuleTest.java
@@ -0,0 +1,37 @@
+package policy.date.rule;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.MonthDay;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HolidayRuleTest {
+
+    @Test
+    @DisplayName("공휴일은 isHoliday에서 true를 반환한다 (MonthDay)")
+    void holidayReturnTrue() {
+        // given
+        MonthDay christmas = MonthDay.of(12, 25);
+
+        // when
+        boolean isHoliday = HolidayRule.isHoliday(christmas);
+
+        // then
+        assertThat(isHoliday).isTrue();
+    }
+
+    @Test
+    @DisplayName("공휴일이 아닌 날은 isHoliday에서 false를 반환한다 (MonthDay)")
+    void nonHolidayReturnFalse() {
+        // given
+        MonthDay myBirthday = MonthDay.of(6, 8);
+
+        // when
+        boolean isHoliday = HolidayRule.isHoliday(myBirthday);
+
+        // then
+        assertThat(isHoliday).isFalse();
+    }
+}
