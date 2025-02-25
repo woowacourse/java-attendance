6fdf2cb3386550a873618c8f8d9fc4eff23a42ed
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-26 00:33:36 +0900

    feat: 주말 규칙 추가

---
+++ src/main/java/policy/date/rule/WeekendRule.java
@@ -0,0 +1,32 @@
+package policy.date.rule;
+
+import java.time.DayOfWeek;
+import java.util.stream.Stream;
+
+public enum WeekendRule {
+
+    FRIDAY(DayOfWeek.FRIDAY, false, "금요일이 주말이 되길"),
+    SATURDAY(DayOfWeek.SATURDAY, true, "놀토가 돌아오지 않길"),
+    SUNDAY(DayOfWeek.SUNDAY, true, "일요일")
+    ;
+
+    private final DayOfWeek dayOfWeek;
+    private final boolean isActive;
+    private final String description;
+
+    WeekendRule(DayOfWeek dayOfWeek, boolean isActive, String description) {
+        this.dayOfWeek = dayOfWeek;
+        this.isActive = isActive;
+        this.description = description;
+    }
+
+    public static boolean isWeekend(DayOfWeek dayOfWeek) {
+        return Stream.of(WeekendRule.values())
+                .filter(weekendRule -> weekendRule.isActive)
+                .anyMatch(weekendRule -> dayOfWeek.equals(weekendRule.dayOfWeek));
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/test/java/policy/date/rule/WeekendRuleTest.java
@@ -0,0 +1,57 @@
+package policy.date.rule;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class WeekendRuleTest {
+
+    @Test
+    @DisplayName("토, 일요일은 isWeekend에서 true를 반환한다 (DayOfWeek)")
+    void weekendReturnTrue() {
+        // given
+        DayOfWeek saturday = DayOfWeek.SATURDAY;
+        DayOfWeek sunday = DayOfWeek.SUNDAY;
+
+        // when
+        boolean isSaturdayWeekend = WeekendRule.isWeekend(saturday);
+        boolean isSundayWeekend = WeekendRule.isWeekend(sunday);
+
+        // then
+        assertAll(
+                () -> assertThat(isSaturdayWeekend).isTrue(),
+                () -> assertThat(isSundayWeekend).isTrue()
+        );
+    }
+
+    @Test
+    @DisplayName("월, 화, 수, 목, 금요일은 isWeekend에서 true를 반환한다 (DayOfWeek)")
+    void weekdayReturnTrue() {
+        // given
+        DayOfWeek monday = DayOfWeek.MONDAY;
+        DayOfWeek tuesday = DayOfWeek.TUESDAY;
+        DayOfWeek wednesday = DayOfWeek.WEDNESDAY;
+        DayOfWeek thursday = DayOfWeek.THURSDAY;
+        DayOfWeek friday = DayOfWeek.FRIDAY;
+
+        // when
+        boolean isMondayWeekend = WeekendRule.isWeekend(monday);
+        boolean isTuesdayWeekend = WeekendRule.isWeekend(tuesday);
+        boolean isWednesdayWeekend = WeekendRule.isWeekend(wednesday);
+        boolean isThursdayWeekend = WeekendRule.isWeekend(thursday);
+        boolean isFridayWeekend = WeekendRule.isWeekend(friday);
+
+        // then
+        assertAll(
+                () -> assertThat(isMondayWeekend).isFalse(),
+                () -> assertThat(isTuesdayWeekend).isFalse(),
+                () -> assertThat(isWednesdayWeekend).isFalse(),
+                () -> assertThat(isThursdayWeekend).isFalse(),
+                () -> assertThat(isFridayWeekend).isFalse()
+        );
+    }
+}
