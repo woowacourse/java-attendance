308614712a71fb0d4576f34dbcda6d5758c29385
jumdo12 <jumdo12@gmail.com>
2025-02-25 16:01:21 +0900

    refactor: 매직넘버 수정

--- src/main/java/presentation/InputValidator.java
+++ src/main/java/presentation/InputValidator.java
@@ -1,11 +1,16 @@
 package presentation;
 
+import static presentation.ControllerCommand.*;
+
 public class InputValidator {
     public static final String INVALID_DATETIME_ARGUMENT_EXCEPTION = "잘못된 형식을 입력하였습니다.";
 
     public static void commandValidate(String command) {
-        if (!command.equals("1") && !command.equals("2") && !command.equals("3") && !command.equals("4")
-                && !command.equals("Q") && !command.equals("q")) {
+        if (!command.equals(ATTEND_COMMAND.getCommand())
+                && !command.equals(EDIT_COMMAND.getCommand())
+                && !command.equals(CREW_QUERY_COMMAND.getCommand())
+                && !command.equals(CREWS_WARNING_COMMAND.getCommand())
+                && !command.equalsIgnoreCase(EXIT_COMMAND.getCommand())) {
             throw new IllegalArgumentException(INVALID_DATETIME_ARGUMENT_EXCEPTION);
         }
     }
