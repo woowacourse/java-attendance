cc8b1202d662e8b9e8b8b5859d07568a79b87bba
taek2222 <oot309@daum.net>
2025-02-26 21:39:27 +0900

    feat: 예외 및 메시지 추가

---
+++ src/main/java/attendance/view/Menu.java
@@ -0,0 +1,9 @@
+package attendance.view;
+
+public enum Menu {
+    ;
+
+    public static void find(final String input) {
+        throw new IllegalArgumentException("[ERROR] 잘못된 메뉴 선택의 입력입니다.");
+    }
+}
