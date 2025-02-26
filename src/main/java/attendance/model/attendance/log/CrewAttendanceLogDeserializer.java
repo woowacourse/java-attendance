2a60791729afbebf316ae84d1dd23ebc85b92cb4
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-26 15:09:59 +0900

    test(red): 크루 출석 기록 역직렬화 레드 테스트 작성

--- src/main/java/attendance/model/attendance/log/CrewAttendanceLogDeserializer.java
+++ src/main/java/attendance/model/attendance/log/CrewAttendanceLogDeserializer.java
@@ -1,11 +1,19 @@
 package attendance.model.attendance.log;
 
+import attendance.model.campus.CampusOperationPolicy;
 import java.nio.file.Path;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class CrewAttendanceLogDeserializer {
 
-    public List<CrewAttendanceLog> deserializeFromCsv(final Path csvFilePath) {
+    private static final String CREW_DATETIME_DELIMITER = ",";
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public List<CrewAttendanceLog> deserializeFromCsv(
+            final Path csvFilePath,
+            final CampusOperationPolicy campusOperationPolicy
+    ) {
         return null;
     }
 }
