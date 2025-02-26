7edde10b9b0fc6a5fe90c726bff2e0710a70d395
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-26 20:38:59 +0900

    🔴 red: 날짜로 부터 교육 시간을 구하는 로직 예외 테스트 추가

--- src/test/java/model/EducationScheduleTest.java
+++ src/test/java/model/EducationScheduleTest.java
@@ -1,6 +1,7 @@
 package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import attendance.model.EducationSchedule;
 import java.time.LocalDate;
@@ -18,5 +19,15 @@ class EducationScheduleTest {
 
         // then
         assertThat(educationSchedule).isEqualTo(EducationSchedule.MONDAY);
+    }
+
+    @Test
+    void 운영_요일이_아닌_날짜로_운영시간을_조회하면_예외가_발생한다() {
+        // given
+        LocalDate date = LocalDate.of(2024, 12, 8);
+
+        // when & then
+        assertThatThrownBy(() -> EducationSchedule.from(date))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
