9037afad3a97dd71826bb790f85fb2f9fc0273ef
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-26 22:22:35 +0900

    refactor: Day 도메인 분리
    - LocalDate가 주말 혹은 공휴일에 해당 될 경우 객체를 생성하지 않는 Day 클래스 구현
    - date 기반의 equals 오버라이딩

---
+++ src/main/java/domain/Day.java
@@ -0,0 +1,47 @@
+package domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+public class Day {
+    private final LocalDate date;
+
+    public Day(LocalDate date) {
+        validateDate(date);
+        this.date = date;
+    }
+
+    private void validateDate(LocalDate date) {
+        if (isWeekend(date) || isHoliday(date)) {
+            throw new IllegalStateException("[ERROR] 등교일이 아닙니다.");
+        }
+    }
+
+    private boolean isWeekend(LocalDate date) {
+        return DayOfWeek.SATURDAY.equals(date.getDayOfWeek()) || DayOfWeek.SUNDAY.equals(date.getDayOfWeek());
+    }
+
+    private boolean isHoliday(LocalDate date) {
+        return Holiday.isHoliday(date);
+    }
+
+    public LocalTime getCriteriaTime() {
+        return CustomDayOfWeek.getCriteriaTime(date);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Day day = (Day) o;
+        return Objects.equals(date, day.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(date);
+    }
+}
