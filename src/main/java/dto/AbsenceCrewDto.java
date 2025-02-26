673654e9064c1679090bda568c50daf5593f2ef4
byesol <bsj5967@naver.com>
2025-02-26 15:02:03 +0900

    refactor : 정렬 로직 책임 변경

--- src/main/java/dto/AbsenceCrewDto.java
+++ src/main/java/dto/AbsenceCrewDto.java
@@ -1,14 +1,11 @@
 package dto;
 
-import static domain.AbsenceLevel.LATE_TO_ABSENCE_THRESHOLD;
-import static domain.AttendanceResult.ABSENCE;
-import static domain.AttendanceResult.LATE;
 
 import domain.AbsenceLevel;
 import domain.AttendanceResult;
 import java.util.Map;
 
-public class AbsenceCrewDto implements Comparable<AbsenceCrewDto> {
+public class AbsenceCrewDto {
 
     private final String username;
     private final Map<AttendanceResult, Integer> results;
@@ -18,22 +15,6 @@ public class AbsenceCrewDto implements Comparable<AbsenceCrewDto> {
         this.username = username;
         this.results = results;
         this.classifyAbsenceLevel = classifyAbsenceLevel.getLevel();
-    }
-
-    @Override
-    public int compareTo(AbsenceCrewDto o) {
-        int myAbsenceCount = results.getOrDefault(LATE, 0);
-        myAbsenceCount += results.getOrDefault(ABSENCE, 0) * LATE_TO_ABSENCE_THRESHOLD;
-        int otherAbsenceCount = o.results.getOrDefault(LATE, 0);
-        otherAbsenceCount += o.results.getOrDefault(ABSENCE, 0) * LATE_TO_ABSENCE_THRESHOLD;
-
-        int result = Integer.compare(otherAbsenceCount, myAbsenceCount);
-
-        if (result == 0) {
-            return this.username.compareTo(o.username);
-        }
-
-        return result;
     }
 
     public String getUsername() {
