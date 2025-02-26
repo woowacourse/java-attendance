ccbc84efd030db97ecba8b808ed4d6ec57e07c47
(minjae8563) <(minjae8563@naver.com)>
2025-02-27 00:37:12 +0900

    feat : 멤버 변수를 StudentAttendanceHistory 로 변경 후, Student 에서 메서드 호출하는 형식으로 변경

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,83 +1,45 @@
 package model;
 
-import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Student {
 
-    String name;
+    private final String name;
 
-    List<LocalDateTime> timeRecords;
+    private final StudentAttendanceHistory studentAttendanceHistory;
 
-    public Student(String name, List<LocalDateTime> timeRecords) {
+    public Student(String name, StudentAttendanceHistory studentAttendanceHistory) {
         this.name = name;
-        this.timeRecords = timeRecords;
+        this.studentAttendanceHistory = studentAttendanceHistory;
     }
 
     public String getName() {
         return name;
     }
 
-    public List<LocalDateTime> getTimeRecords() {
-        return timeRecords;
-    }
-
     public void addTime(LocalDateTime localDateTime) {
-        timeRecords.add(localDateTime);
-        Collections.sort(timeRecords);
+        studentAttendanceHistory.addTime(localDateTime);
     }
 
     public LocalDateTime findSameDay(LocalDateTime wantToFindLocalDateTime) {
-        for (LocalDateTime localDateTime : timeRecords) {
-            LocalDateTime dayDate1 = localDateTime.truncatedTo(ChronoUnit.DAYS);
-            LocalDateTime dayDate2 = wantToFindLocalDateTime.truncatedTo(ChronoUnit.DAYS);
-            if (dayDate1.isEqual(dayDate2)) {
-                return localDateTime;
-            }
-        }
-        return null;
+        return studentAttendanceHistory.findSameDay(wantToFindLocalDateTime);
     }
 
     public void modifyRecord(LocalDateTime wantToModifyLocalDateTime) {
-        timeRecords.remove(findSameDay(wantToModifyLocalDateTime));
-        addTime(wantToModifyLocalDateTime);
-        Collections.sort(timeRecords);
-    }
-
-    public boolean isExistSameDay(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
-        LocalDateTime dayDate1 = localDateTime1.truncatedTo(ChronoUnit.DAYS);
-        LocalDateTime dayDate2 = localDateTime2.truncatedTo(ChronoUnit.DAYS);
-
-        return (dayDate1.isEqual(dayDate2));
+        studentAttendanceHistory.modifyRecord(wantToModifyLocalDateTime);
     }
 
     public void updateNoInformationInFile(LocalDateTime todayDate) {
-        LocalDateTime standard = LocalDateTime.of(2024, 12, 1, 0, 0);
-        while (!isExistSameDay(standard,todayDate)) {
-            addTimeRecordIfValid(standard);
-            standard = standard.plusDays(1);
-        }
-        Collections.sort(timeRecords);
-    }
-
-    private void addTimeRecordIfValid(LocalDateTime standard) {
-        if (!isWeekend(standard) && findSameDay(standard) == null) {
-            timeRecords.add(standard);
-        }
-    }
-
-    private boolean isWeekend(LocalDateTime localDateTime) {
-        return (localDateTime.getDayOfWeek().equals(DayOfWeek.SUNDAY) || localDateTime.getDayOfWeek().equals(DayOfWeek.SATURDAY));
+        studentAttendanceHistory.updateNoInformationInFile(todayDate);
     }
 
     public void isAlreadyAttendanceDate(TodayDate todayDate) {
-        if (timeRecords.contains(todayDate.getTodayDateTIme())) {
+        if (studentAttendanceHistory.isAlreadyAttendanceDate(todayDate)) {
             throw new IllegalArgumentException("[ERROR] 이미 출석한 요일입니다. 수정하고 싶으시면 수정 메뉴를 이용해 주세요.");
         }
     }
 
+    public StudentAttendanceHistory getStudentAttendanceHistory() {
+        return studentAttendanceHistory;
+    }
 }
