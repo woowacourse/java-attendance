6392001d3cd6e5100eff21d320234b6410c2a3da
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-26 13:32:23 +0900

    style: 의미 없는 주석 제거

--- src/test/java/attendance/model/attendance/datetime/time/AttendanceTimeTest.java
+++ src/test/java/attendance/model/attendance/datetime/time/AttendanceTimeTest.java
@@ -9,19 +9,6 @@ import org.junit.jupiter.api.Test;
 
 class AttendanceTimeTest {
 
-    //### AttendanceTime (출석 시간 Class)
-    //
-    //- **상태**
-    //    - LocalTime 출석 시간
-    //- **생성**
-    //    - LocalTime 과 CampusOperationPolicy 를 받아 생성된다.
-    //    - value 가 null 인 자신을 생성한다.
-    //- **동작**
-    //    - 생성 시 CampusOperationPolicy 를 통해 캠퍼스 운영 시간인지 검증한다.
-    //    - LocalTime 을 받아 해당 LocalTime 이 자신의 value 보다 이전인지 반환한다.
-    //    - 자신의 value 가 비어 있는 null 한 시간인지 반환한다.
-    //    - value 가 null 일수 있으므로, getter 는 Optional 로 반환한다.
-
     private final CampusOperationPolicy campusOperationPolicy = new CampusOperationPolicy();
 
     @Test
