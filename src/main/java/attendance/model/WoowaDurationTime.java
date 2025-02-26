6894a8bf44a0f4bf69698105b7fb9ed49fb25db0
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-26 18:40:17 +0900

    🛠️ refactor: 캠퍼스가 운영시간인지 확인하는 로직 리팩터링

--- src/main/java/attendance/model/WoowaDurationTime.java
+++ src/main/java/attendance/model/WoowaDurationTime.java
@@ -1,11 +1,35 @@
 package attendance.model;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
 
 public enum WoowaDurationTime {
+    MONDAY(DayOfWeek.MONDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
+    TUESDAY(DayOfWeek.TUESDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
+    WEDNESDAY(DayOfWeek.WEDNESDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
+    THURSDAY(DayOfWeek.THURSDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
+    FRIDAY(DayOfWeek.FRIDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
+    SATURDAY(DayOfWeek.SATURDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
+    SUNDAY(DayOfWeek.SUNDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
     ;
 
-    public static boolean isDurationTime(LocalTime time) {
-        return !(time.isBefore(LocalTime.of(8, 0)) || time.isAfter(LocalTime.of(23, 0)));
+    private final DayOfWeek dayOfWeek;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+
+    WoowaDurationTime(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public static boolean isDurationTime(LocalDate localDate, LocalTime time) {
+        WoowaDurationTime durationTime = Arrays.stream(WoowaDurationTime.values())
+                .filter(woowaDurationTime -> woowaDurationTime.dayOfWeek.equals(localDate.getDayOfWeek()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 날짜입니다."));
+        return !(time.isBefore(durationTime.startTime) || time.isAfter(durationTime.endTime));
     }
 }
--- src/test/java/model/WoowaDurationTimeTest.java
+++ src/test/java/model/WoowaDurationTimeTest.java
@@ -3,6 +3,7 @@ package model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import attendance.model.WoowaDurationTime;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,10 +14,11 @@ class WoowaDurationTimeTest {
     @DisplayName("캠퍼스 운영시간이 아닌 경우")
     void 입력한_시간이_운영시간인지_확인한다_1() {
         // given
+        LocalDate date = LocalDate.of(2024, 12, 9);
         LocalTime time = LocalTime.of(7, 0);
 
         // when
-        boolean isDurationTime = WoowaDurationTime.isDurationTime(time);
+        boolean isDurationTime = WoowaDurationTime.isDurationTime(date, time);
 
         // then
         assertThat(isDurationTime).isFalse();
@@ -26,10 +28,11 @@ class WoowaDurationTimeTest {
     @DisplayName("캠퍼스 운영시간인 경우")
     void 입력한_시간이_운영시간인지_확인한다_2() {
         // given
+        LocalDate date = LocalDate.of(2024, 12, 8);
         LocalTime time = LocalTime.of(8, 0);
 
         // when
-        boolean isDurationTime = WoowaDurationTime.isDurationTime(time);
+        boolean isDurationTime = WoowaDurationTime.isDurationTime(date, time);
 
         // then
         assertThat(isDurationTime).isTrue();
