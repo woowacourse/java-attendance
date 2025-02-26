48fb5cb3d308ce17fa4fcebbbec8f336b8a13e49
taek2222 <oot309@daum.net>
2025-02-26 22:45:52 +0900

    feat(AttendanceState): 출결 상황 반환 기능 추가
    - 등교 시간으로 부터 오바되는 시간으로 출석, 지각 기능을 추가했습니다.

--- src/main/java/attendance/domain/AttendanceState.java
+++ src/main/java/attendance/domain/AttendanceState.java
@@ -1,9 +1,21 @@
 package attendance.domain;
 
+import java.util.Arrays;
+
 public enum AttendanceState {
-    TARDY;
+    ATTENDANCE(5),
+    TARDY(30);
+
+    private final int threshold;
+
+    AttendanceState(final int threshold) {
+        this.threshold = threshold;
+    }
 
     public static AttendanceState evaluate(final int overTime) {
-        return TARDY;
+        return Arrays.stream(values())
+                .filter(state -> state.threshold >= overTime)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 서버 에러가 발생했습니다."));
     }
 }
