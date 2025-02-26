1c636123f2993c0155bc118af38cba9b611dd5f2
LeeJaeHoon <dlwogns3413@naver.com>
2025-02-26 16:00:40 +0900

    test: 공휴일에 출석 객체 생성시 예외 발생 기능 테스트 (red)

--- src/test/java/domain/AttendanceTest.java
+++ src/test/java/domain/AttendanceTest.java
@@ -10,11 +10,23 @@ public class AttendanceTest {
     void 주말에_출석을_생성할_경우_예외가_발생한다() {
         //given
         String nickname = "pobi";
-        LocalDateTime attendanceDateTime = LocalDateTime.of(2024, 12, 14, 10, 1); // 토요일
+        LocalDateTime saturday = LocalDateTime.of(2024, 12, 14, 10, 1); // 토요일
 
         //when
-        Assertions.assertThatThrownBy(() -> new Attendance(nickname, attendanceDateTime))
+        Assertions.assertThatThrownBy(() -> new Attendance(nickname, saturday))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("주말에는 출석할 수 없습니다.");
+    }
+
+    @Test
+    void 공휴일에_출석_객체를_생성하는_경우_예외가_발생한다() {
+        //given
+        String nickname = "pobi";
+        LocalDateTime christmas = LocalDateTime.of(2024, 12, 25, 10, 1); // 크리스마스
+
+        //when
+        Assertions.assertThatThrownBy(() -> new Attendance(nickname, christmas))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("공휴일에는 출석할 수 없습니다.");
     }
 }
