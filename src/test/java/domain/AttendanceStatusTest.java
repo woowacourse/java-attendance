769026df85b1f64482099d744c86799b90025d7a
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-26 17:06:58 +0900

    test(domain): 지각 상태 확인 테스트 코드 작성

--- src/test/java/domain/AttendanceStatusTest.java
+++ src/test/java/domain/AttendanceStatusTest.java
@@ -18,8 +18,24 @@ public class AttendanceStatusTest {
     @DisplayName("화~금요일의 출석 상태를 구할 수 있다.")
     @Test
     void test2() {
-        Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 10, 9, 59));
+        Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 10, 10, 5));
 
         assertThat(AttendanceStatus.from(attendance)).isEqualTo(AttendanceStatus.ATTEND);
+    }
+
+    @DisplayName("월요일의 지각 상태를 구할 수 있다.")
+    @Test
+    void test3() {
+        Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 9, 13, 6));
+
+        assertThat(AttendanceStatus.from(attendance)).isEqualTo(AttendanceStatus.LATE);
+    }
+
+    @DisplayName("화~금요일의 지각 상태를 구할 수 있다.")
+    @Test
+    void test4() {
+        Attendance attendance = new Attendance(LocalDateTime.of(2024, 12, 10, 10, 6));
+
+        assertThat(AttendanceStatus.from(attendance)).isEqualTo(AttendanceStatus.LATE);
     }
 }
