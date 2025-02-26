d47dd1aa69ac8dec7de5461f11ea36fac44ed4b4
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-26 22:26:56 +0900

    refactor: 임시 클래스 제거 및 도메인 분리에 따른 테스트 코드 재작성
    - 분리된 도메인으로 테스트를 수행할 수 있도록 테스트 코드 재구성
    - 중복 테스트 제거

--- src/test/java/AttendanceCheckTest.java
+++ src/test/java/AttendanceCheckTest.java
@@ -1,25 +1,40 @@
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import domain.Attendance;
+import domain.AttendanceBook;
+import domain.Day;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class AttendanceCheckTest {
-    final LocalDate today = LocalDate.now();
+    final Day today = new Day(LocalDate.of(2025, 2, 26));
 
     public static Stream<Arguments> provideEachDayOfWeekAttendance() {
         return Stream.of(
                 // 월요일
-                Arguments.of(LocalDate.of(2025, 2, 24), "에드", LocalTime.of(13, 0), "출석"),
-                Arguments.of(LocalDate.of(2025, 2, 24), "제프", LocalTime.of(13, 6), "지각"),
+                Arguments.of(new Day(LocalDate.of(2025, 2, 24)), "에드", LocalTime.of(13, 0), false, false),
+                Arguments.of(new Day(LocalDate.of(2025, 2, 24)), "제프", LocalTime.of(13, 6), true, false),
                 //수요일
-                Arguments.of(LocalDate.of(2025, 2, 25), "율무", LocalTime.of(13, 0), "결석")
-        );
+                Arguments.of(new Day(LocalDate.of(2025, 2, 25)), "율무", LocalTime.of(13, 0), false, true));
+    }
+
+    public static Stream<Arguments> provideDifferentHoursOfAttendance() {
+        return Stream.of(
+                // 출석
+                Arguments.of("에드", LocalTime.of(10, 4), false, false),
+                // 지각
+                Arguments.of("제프", LocalTime.of(10, 6), true, false),
+                Arguments.of("율무", LocalTime.of(10, 30), true, false),
+                // 결석
+                Arguments.of("링크", LocalTime.of(13, 31), false, true));
     }
 
     @Test
@@ -27,109 +42,86 @@ public class AttendanceCheckTest {
         final var nickname = "에드";
         final var attendanceTime = LocalTime.of(9, 59);
 
-        AttendanceCheck attendanceCheck = new AttendanceCheck();
-        attendanceCheck.attend(today, nickname, attendanceTime);
-        final var actual = attendanceCheck.getAttendanceStatus(today, nickname);
-        final var expected = "출석";
-        assertEquals(expected, actual);
+        Attendance attendance = new Attendance(today, attendanceTime);
+        AttendanceBook attendanceBook = new AttendanceBook();
+        attendanceBook.recordAttendance(nickname, attendance);
+        final var attendances = attendanceBook.getAttendances(nickname);
+
+        assertDoesNotThrow(() -> attendances.findByDay(today));
     }
 
-    @Test
-    void 출석_후_출석_기록을_확인한다() {
-        final var nickname = "에드";
-        final var attendanceTime = LocalTime.of(9, 59);
+    @ParameterizedTest
+    @MethodSource("provideDifferentHoursOfAttendance")
+    void 출석_시간에_따라_출석_상태를_다르게_처리한다(String nickname, LocalTime attendanceTime, Boolean isLateExpected,
+                                   Boolean isAbsentExpected) {
+        Attendance attendance = new Attendance(today, attendanceTime);
+        AttendanceBook attendanceBook = new AttendanceBook();
+        attendanceBook.recordAttendance(nickname, attendance);
 
-        AttendanceCheck attendanceCheck = new AttendanceCheck();
-
-        attendanceCheck.attend(today, nickname, attendanceTime);
-
-        final var attendanceTimeRecord = attendanceCheck.getAttendanceTime(today, nickname);
-        final var attendanceStatus = attendanceCheck.getAttendanceStatus(today, nickname);
-
+        final var attendances = attendanceBook.getAttendances(nickname);
+        final var attendanceRecord = attendances.findByDay(today);
+        final var attendanceTimeRecord = attendanceRecord.getTime();
+        final var isLate = attendanceRecord.isLate();
+        final var isAbsent = attendanceRecord.isAbsent();
         assertEquals(attendanceTime, attendanceTimeRecord);
-        assertEquals("출석", attendanceStatus);
+        assertEquals(isLateExpected, isLate);
+        assertEquals(isAbsentExpected, isAbsent);
     }
 
     @Test
     void 이미_출석한_상태에서_출석을_시도하면_예외처리() {
         final var nickname = "에드";
         final var attendanceTime = LocalTime.of(9, 59);
-        AttendanceCheck attendanceCheck = new AttendanceCheck();
+        Attendance attendance = new Attendance(today, attendanceTime);
+        AttendanceBook attendanceBook = new AttendanceBook();
+        attendanceBook.recordAttendance(nickname, attendance);
 
-        attendanceCheck.attend(today, nickname, attendanceTime);
-
-        assertThatThrownBy(() -> attendanceCheck.attend(today, nickname, attendanceTime))
+        assertThatThrownBy(() -> attendanceBook.recordAttendance(nickname, new Attendance(today, attendanceTime)))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("[ERROR] 이미 출석이 완료되었습니다. 수정 기능을 이용하세요.");
     }
 
-    @Test
-    void 시작_시간으로부터_5분_초과는_지각으로_처리한다() {
-        final var nickname = "에드";
-        final var attendanceTime = LocalTime.of(10, 6);
-        AttendanceCheck attendanceCheck = new AttendanceCheck();
-
-        attendanceCheck.attend(today, nickname, attendanceTime);
-        final var expected = "지각";
-        assertEquals(expected, attendanceCheck.getAttendanceStatus(today, nickname));
-    }
-
-    @Test
-    void 시작_시간으로부터_30분_초과는_결석으로_처리한다() {
-        final var nickname = "에드";
-        final var attendanceTime = LocalTime.of(10, 31);
-        AttendanceCheck attendanceCheck = new AttendanceCheck();
-
-        attendanceCheck.attend(today, nickname, attendanceTime);
-        final var expected = "결석";
-        assertEquals(expected, attendanceCheck.getAttendanceStatus(today, nickname));
-    }
 
     @ParameterizedTest
     @MethodSource("provideEachDayOfWeekAttendance")
-    void 요일에_따라_다른_시작_시간을_적용한다(LocalDate today, String nickname, LocalTime attendanceTime, String expected) {
-        AttendanceCheck attendanceCheck = new AttendanceCheck();
-        attendanceCheck.attend(today, nickname, attendanceTime);
-        final var actual = attendanceCheck.getAttendanceStatus(today, nickname);
-        assertEquals(expected, actual);
+    void 요일에_따라_다른_시작_시간을_적용한다(Day today, String nickname, LocalTime attendanceTime, Boolean isLateExpected,
+                               Boolean isAbsentExpected) {
+        Attendance attendance = new Attendance(today, attendanceTime);
+        AttendanceBook attendanceBook = new AttendanceBook();
+        attendanceBook.recordAttendance(nickname, attendance);
+
+        final var attendances = attendanceBook.getAttendances(nickname);
+        final var attendanceRecord = attendances.findByDay(today);
+
+        final var isLate = attendanceRecord.isLate();
+        final var isAbsent = attendanceRecord.isAbsent();
+        assertEquals(isLateExpected, isLate);
+        assertEquals(isAbsentExpected, isAbsent);
     }
 
-    //주말 및 공휴일에는 출석을 받지 않는다.
-    @Test
-    void 주말에는_출석을_받지_않는다() {
+    @ParameterizedTest
+    @CsvSource({
+            "2025-01-01", // 설날
+            "2025-02-22", // 토요일
+            "2025-02-23" // 일요일
+    })
+    void 주말_및_공휴일에는_출석을_받지_않는다(String dateString) {
         // 일요일
-        final var sunday = LocalDate.of(2025, 2, 23);
-
-        final var nickname = "에드";
-        final var attendanceTime = LocalTime.of(10, 31);
-        AttendanceCheck attendanceCheck = new AttendanceCheck();
-
-        assertThatThrownBy(() -> attendanceCheck.attend(sunday, nickname, attendanceTime))
+        assertThatThrownBy(() -> new Day(LocalDate.parse(dateString)))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageStartingWith("[ERROR]");
     }
 
-    @Test
-    void 공휴일에는_출석을_받지_않는다() {
-        //설날
-        final var holiday = LocalDate.of(2025, 1, 1);
-
+    @ParameterizedTest
+    @CsvSource({
+            "07:59",
+            "23:01"
+    })
+    void 운영_시간을_벗어난_출석은_받지_않는다(String timeString) {
         final var nickname = "에드";
-        final var attendanceTime = LocalTime.of(10, 31);
-        AttendanceCheck attendanceCheck = new AttendanceCheck();
+        final var attendanceTime = LocalTime.parse(timeString);
 
-        assertThatThrownBy(() -> attendanceCheck.attend(holiday, nickname, attendanceTime))
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessageStartingWith("[ERROR]");
-    }
-
-    @Test
-    void 운영_시간을_벗어난_출석은_받지_않는다() {
-        final var nickname = "에드";
-        final var attendanceTime = LocalTime.of(7, 59);
-        AttendanceCheck attendanceCheck = new AttendanceCheck();
-
-        assertThatThrownBy(() -> attendanceCheck.attend(today, nickname, attendanceTime))
+        assertThatThrownBy(() -> new Attendance(today, attendanceTime))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageStartingWith("[ERROR]");
     }
