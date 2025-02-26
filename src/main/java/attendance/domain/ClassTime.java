0a6a984da76d62d1c5aee043b48dcd2b965c1a1f
taek2222 <oot309@daum.net>
2025-02-26 23:35:03 +0900

    feat(ClassTime): 출석 차이 시간 반환 메서드 추가

---
+++ src/main/java/attendance/domain/ClassTime.java
@@ -0,0 +1,11 @@
+package attendance.domain;
+
+import java.time.LocalDateTime;
+
+public enum ClassTime {
+    ;
+
+    public static int calculateAttendanceDifference(final LocalDateTime attendanceDateTime) {
+        return 0;
+    }
+}
