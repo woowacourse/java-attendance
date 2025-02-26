3982d3f4eef3156883d2f219f7a30ce938da12f3
taek2222 <oot309@daum.net>
2025-02-26 23:40:04 +0900

    feat(ClassTime): 월요일 출석 차이 반환 기능 추가

--- src/main/java/attendance/domain/ClassTime.java
+++ src/main/java/attendance/domain/ClassTime.java
@@ -1,11 +1,13 @@
 package attendance.domain;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public enum ClassTime {
     ;
 
     public static int calculateAttendanceDifference(final LocalDateTime attendanceDateTime) {
-        return 0;
+        LocalTime monday = LocalTime.of(13, 0);
+        return (attendanceDateTime.toLocalTime().toSecondOfDay() - monday.toSecondOfDay()) / 60;
     }
 }
