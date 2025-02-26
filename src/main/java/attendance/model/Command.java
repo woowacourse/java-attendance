be5e9772cd5662ffe6bb821d1d0b6a0261d6c4c6
Yehyeok Bang <qkddpgur318@gmail.com>
2025-02-26 14:57:37 +0900

    feat: 명령어 모델 구현
    - 각 기능을 대표하는 명령어를 추가합니다.

---
+++ src/main/java/attendance/model/Command.java
@@ -0,0 +1,35 @@
+package attendance.model;
+
+import java.util.Arrays;
+
+public enum Command {
+
+    ATTENDANCE("1", "출석 확인"),
+    EDIT_ATTENDANCE("2", "출석 수정"),
+    ATTENDANCE_LOGS("3", "크루별 출석 기록 확인"),
+    WARNING_LIST("4", "제적 위험자 확인"),
+    QUIT("Q", "종료");
+
+    private final String code;
+    private final String description;
+
+    Command(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public boolean isMatch(String code) {
+        return this.code.equals(code);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Command from(String input) {
+        return Arrays.stream(values())
+                .filter(command -> command.isMatch(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 커맨드입니다. 입력: %s".formatted(input)));
+    }
+}
