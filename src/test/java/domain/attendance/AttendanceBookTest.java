b8b5ac18773a1408de73f2bd3e077fc28879cc03
yeonnhuu <yeonnhuu@gmail.com>
2025-02-26 15:53:15 +0900

    :test_tube: test: 출석 확인 및 수정, 크루별 출결 기록 확인 테스트 추가

--- src/test/java/domain/attendance/AttendanceBookTest.java
+++ src/test/java/domain/attendance/AttendanceBookTest.java
@@ -4,63 +4,69 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import domain.crew.CrewStatus;
+import domain.crew.Crew;
 import exception.ErrorException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-public class AttendanceLogsTest {
+public class AttendanceBookTest {
 
+    private AttendanceBook attendanceBook;
+    private Crew crew;
     private AttendanceLogs attendanceLogs;
 
     @BeforeEach
     void setUp() {
+        crew = new Crew("행성");
         attendanceLogs = new AttendanceLogs();
         attendanceLogs.registerLog(LocalDateTime.of(2025, 2, 24, 10, 0));
         attendanceLogs.registerLog(LocalDateTime.of(2025, 2, 25, 10, 0));
+        attendanceBook = new AttendanceBook(Map.of(crew, attendanceLogs));
     }
 
-    @ParameterizedTest
-    @DisplayName("크루의 전체 출결 기록 상태와 경고 상태 테스트")
-    @CsvSource({
-            "'17', 'PASS'",
-            "'17,18', 'WARNING'",
-            "'17,18,19', 'CONSULT'",
-            "'17,18,19,20,21,24', 'EXPEL'"
+    @Test
+    @DisplayName("크루의 출결 기록 검색 기능 테스트")
+    void 크루의_출결_기록_검색_기능_테스트() {
+        // when
+        String crewName = crew.getName();
+        // then & given
+        assertEquals(attendanceBook.findAttendanceLogs(crewName), attendanceLogs);
+    }
 
-    })
-    void 크루의_전체_출결_기록_상태_테스트(String days, String expectedStatus) {
-        // given
-        AttendanceLogs attendanceLogs = new AttendanceLogs();
-        for (String day : days.split(",")) {
-            attendanceLogs.registerLog(LocalDateTime.of(2025, 2, Integer.parseInt(day), 15, 0));
-        }
-        // when & then
-        assertEquals(CrewStatus.valueOf(expectedStatus), attendanceLogs.calculateCrewStatus());
+    @Test
+    @DisplayName("크루의 출결 기록 검색 예외 테스트")
+    void 크루의_출결_기록_검색_예외_테스트() {
+        // when
+        String crewName = "토성";
+        // then & given
+        assertThatThrownBy(() -> attendanceBook.findAttendanceLogs(crewName))
+                .isInstanceOf(ErrorException.class)
+                .hasMessageContaining("[ERROR]");
     }
 
     @Test
     @DisplayName("출석 확인 기능 테스트")
     void 출석_확인_기능_테스트() {
         // given
+        String crewName = crew.getName();
         LocalDateTime attendDateTime = LocalDateTime.of(2025, 2, 26, 10, 0);
         // when & then
-        assertTrue(attendanceLogs.registerLog(attendDateTime).isAttendDate(attendDateTime.toLocalDate()));
+        assertTrue(attendanceBook.registerAttendanceLog(crewName, attendDateTime).isAttendDate(attendDateTime.toLocalDate()));
     }
 
     @Test
     @DisplayName("출석 확인 예외 테스트")
     void 출석_확인_예외_테스트() {
         // given
-        LocalDateTime attendDateTime = LocalDateTime.of(2025, 2, 25, 10, 0);
+        String crewName = crew.getName();
+        LocalDateTime attendDateTime = LocalDateTime.of(2025, 2, 25, 11, 0);
         // when & then
-        assertThatThrownBy(() -> attendanceLogs.registerLog(attendDateTime))
+        assertThatThrownBy(() -> attendanceBook.registerAttendanceLog(crewName, attendDateTime))
                 .isInstanceOf(ErrorException.class)
                 .hasMessageContaining("[ERROR]");
     }
@@ -69,22 +75,23 @@ public class AttendanceLogsTest {
     @DisplayName("출석 수정 기능 테스트")
     void 출석_수정_기능_테스트() {
         // given
+        String crewName = crew.getName();
         LocalDate editDate = LocalDate.of(2025, 2, 25);
         LocalTime editTime = LocalTime.of(11, 0);
         // when & then
-        assertEquals(attendanceLogs.editLog(editDate, editTime).getAttendanceTime(), editTime);
+        assertEquals(attendanceBook.editAttendanceLog(crewName, editDate, editTime).getAttendanceTime(), editTime);
     }
 
     @Test
     @DisplayName("출석 수정 예외 테스트")
     void 출석_수정_예외_테스트() {
         // given
+        String crewName = crew.getName();
         LocalDate editDate = LocalDate.of(2025, 2, 26);
         LocalTime editTime = LocalTime.of(11, 0);
         // when & then
-        assertThatThrownBy(() -> attendanceLogs.editLog(editDate, editTime))
+        assertThatThrownBy(() -> attendanceBook.editAttendanceLog(crewName, editDate, editTime))
                 .isInstanceOf(ErrorException.class)
                 .hasMessageContaining("[ERROR]");
     }
 }
-
