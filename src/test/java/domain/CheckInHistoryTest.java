e958edb3551876ef0b7f77a23174b73b55786d0f
geonwoo <kysub99@naver.com>
2025-02-26 21:13:28 +0900

    refactor(CheckInHistoryTest): 초기값 BeforeEach로 초기화

--- src/test/java/domain/CheckInHistoryTest.java
+++ src/test/java/domain/CheckInHistoryTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,17 +11,38 @@ import java.util.TreeSet;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CheckInHistoryTest {
+
+    CheckInHistory history;
+
+    @BeforeEach
+    void setUp() {
+        CheckInDateTime checkInDateTime = CheckInDateTime.of(
+                LocalDate.of(2024, 12, 3), LocalTime.of(10, 0)
+        );
+        TreeSet<CheckInDateTime> dateTimes = new TreeSet<>();
+        dateTimes.add(checkInDateTime);
+        history = CheckInHistory.of(dateTimes);
+    }
+
     @Test
     @DisplayName("출석 기록부에 내역을 정상적으로 추가")
     void addCheckInTimeTest() {
         //given
-        CheckInHistory history = CheckInHistory.of(new TreeSet<CheckInDateTime>());
         CheckInDateTime checkInDateTime = CheckInDateTime.of(
-                LocalDate.of(2024, 12, 3), LocalTime.of(10, 0)
+                LocalDate.of(2024, 12, 4), LocalTime.of(10, 0)
         );
         //when
         history.checkIn(checkInDateTime);
         //then
-        assertThat(history.getCheckInCount()).isEqualTo(1);
+        assertThat(history.getCheckInCount()).isEqualTo(2);
     }
+
+/*    @Test
+    @DisplayName("당일에 이미 출석 기록이 있는 경우 예외 발생")
+    void alreadyCheckInExceptionTest() {
+        //given
+        CheckInDateTime checkInDateTime = CheckInDateTime.of()
+        //when
+        //then
+    }*/
 }
