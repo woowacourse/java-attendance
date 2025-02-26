5dfd22843861076aae2a969d779e8fc84411d285
yush <yush2202@gmail.com>
2025-02-26 14:25:55 +0900

    refactor: 정규식 Pattern으로 변경

--- src/main/java/view/dto/CrewAttendancesDTO.java
+++ src/main/java/view/dto/CrewAttendancesDTO.java
@@ -13,10 +13,7 @@ public record CrewAttendancesDTO(String nickName, List<AttendanceLogDTO> attenda
                 .map(AttendanceLogDTO::from)
                 .sorted()
                 .toList();
-
-        // count() 계산 메서드 같은거 말고, get으로 가져오기?
-        // 그럼 attendance에서 지각, 출석, 결석, 현재 출석 상태를 상태로 가지고 있기?
-        // 그럼 필드변수가 너무 많아지는데 괜찮?
+        
         return new CrewAttendancesDTO(crew.getNickname(), sortedAttendanceLogDtos, attendanceStatistics.getPresent(),
                 attendanceStatistics.getLate(), attendanceStatistics.getAbsent(),
                 attendanceStatistics.getAlertLevel().getName());
