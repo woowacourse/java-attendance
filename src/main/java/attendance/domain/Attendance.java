f9c6148d693384c35252532fca3c22b15335a9d5
LeeJaeHoon <dlwogns3413@naver.com>
2025-02-26 15:40:44 +0900

    feat: 주말에 출석을 할 경우 예외 발생 로직 기능 구현 (green)

--- src/main/java/attendance/domain/Attendance.java
+++ src/main/java/attendance/domain/Attendance.java
@@ -1,5 +1,6 @@
 package attendance.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -11,6 +12,7 @@ public class Attendance {
     private final LocalTime attendanceTime;
 
     public Attendance(String nickname, LocalDateTime attendanceDateTime) {
+        validateAttendanceDate(attendanceDateTime.toLocalDate());
         this.nickname = nickname;
         this.attendanceDate = attendanceDateTime.toLocalDate();
         this.attendanceTime = attendanceDateTime.toLocalTime();
@@ -19,5 +21,11 @@ public class Attendance {
     public boolean isAlreadyAttend(Attendance newAttendance) {
         return nickname.equals(newAttendance.nickname) &&
                 attendanceDate.equals(newAttendance.attendanceDate);
+    }
+
+    private void validateAttendanceDate(LocalDate attendanceDate) {
+        if (attendanceDate.getDayOfWeek() == DayOfWeek.SATURDAY || attendanceDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            throw new IllegalArgumentException("주말에는 출석할 수 없습니다.");
+        }
     }
 }
