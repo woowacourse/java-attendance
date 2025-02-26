f1dee911f5c7290f9daa3416624a9582dc026c25
LeeJaeHoon <dlwogns3413@naver.com>
2025-02-26 15:38:53 +0900

    test: 주말에 출석을 할 경우 예외 발생 로직 테스트 (red)

---
+++ src/test/java/domain/AttendanceTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import attendance.domain.Attendance;
+import java.time.LocalDateTime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AttendanceTest {
+    @Test
+    void 주말_또는_공휴일에_출석을_할_경우_예외가_발생한다() {
+        //given
+        String nickname = "pobi";
+        LocalDateTime attendanceDateTime = LocalDateTime.of(2024, 12, 14, 10, 1); // 토요일
+
+        //when
+        Assertions.assertThatThrownBy(() -> new Attendance(nickname, attendanceDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주말에는 출석할 수 없습니다.");
+    }
+}
