279d178c9c68e767d83d542d51876ede60798371
CheChe903 <cheche903@naver.com>
2025-02-26 20:05:44 +0900

    refactor(Time): 깨진 테스트 통과하도록 리팩터링
    기존에 주말 검증 로직으로 인한 깨진 테스트를 모두 성공시킥기 위해 주말 출석 로직을 리팩터링합니다.

--- src/main/java/attendance/domain/Time.java
+++ src/main/java/attendance/domain/Time.java
@@ -1,5 +1,6 @@
 package attendance.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 public class Time {
@@ -7,18 +8,21 @@ public class Time {
     private final LocalDateTime attendanceTime;
 
     public Time(LocalDateTime attendanceTime) {
-        validateHoliday();
+        validateHoliday(attendanceTime);
         validateCampusOperationTime(attendanceTime);
         this.attendanceTime = attendanceTime;
     }
 
     private void validateCampusOperationTime(LocalDateTime attendanceTime) {
         if (attendanceTime.getHour() < 8 || (attendanceTime.getHour() == 23 && attendanceTime.getMinute() > 0)) {
-            throw new IllegalArgumentException("[ERROR] 주말 및 공휴일은 출석할 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 캠퍼스 운영 시간이 아닙니다.");
         }
     }
 
-    private void validateHoliday() {
-        throw new IllegalArgumentException("[ERROR] 주말 및 공휴일은 출석할 수 없습니다.");
+    private void validateHoliday(LocalDateTime attendanceTime) {
+        if (attendanceTime.getDayOfWeek().equals(DayOfWeek.SATURDAY) || attendanceTime.getDayOfWeek()
+                .equals(DayOfWeek.SUNDAY)) {
+            throw new IllegalArgumentException("[ERROR] 주말 및 공휴일은 출석할 수 없습니다.");
+        }
     }
 }
