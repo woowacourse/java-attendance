6203fdc7319fed8132dce82766ead917f45408fa
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-26 17:14:18 +0900

    refactor(domain): 지각 상태 확인 리팩토링
    - 중복 코드 제거
    - 네이밍 수정

--- src/main/java/domain/AttendanceStatus.java
+++ src/main/java/domain/AttendanceStatus.java
@@ -17,18 +17,20 @@ public enum AttendanceStatus {
     }
 
     public static AttendanceStatus from(Attendance attendance) {
-        int attendanceHour = calculateAttendanceHour(attendance.getDayOfWeek());
-        LocalTime lateCondition = LocalTime.of(attendanceHour, START_MINUTE_OF_LATE);
-        if(attendance.getTime().isBefore(lateCondition)) {
+        int endHourOfAttendance = calculateEndHourOfAttendance(attendance.getDayOfWeek());
+        LocalTime endTimeOfAttendance = LocalTime.of(endHourOfAttendance, END_MINUTE_OF_ATTENDANCE);
+        LocalTime checkTime = attendance.getTime();
+
+        if(checkTime.isBefore(endTimeOfAttendance) || checkTime.equals(endTimeOfAttendance)) {
             return AttendanceStatus.ATTEND;
         }
-        if(attendance.getTime().equals(lateCondition) || attendance.getTime().isAfter(lateCondition)) {
+        if(checkTime.isAfter(endTimeOfAttendance)) {
             return AttendanceStatus.LATE;
         }
         return null;
     }
 
-    private static int calculateAttendanceHour(DayOfWeek dayOfWeek) {
+    private static int calculateEndHourOfAttendance(DayOfWeek dayOfWeek) {
         if(dayOfWeek == DayOfWeek.MONDAY) {
             return ATTENDANCE_HOUR_OF_MONDAY;
         }
--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -3,5 +3,5 @@ package util;
 public class Constants {
     public static final int ATTENDANCE_HOUR_OF_MONDAY = 13;
     public static final int ATTENDANCE_HOUR_OF_TUESDAY_TO_FRIDAY = 10;
-    public static final int START_MINUTE_OF_LATE = 6;
+    public static final int END_MINUTE_OF_ATTENDANCE = 5;
 }
