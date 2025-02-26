020dc10bdeb29647a396370ccfd5d5ab030858b4
threepebbles <threepebbles@naver.com>
2025-02-26 15:59:18 +0900

    test: 크루의 출석 기록이 존재하지 않는 경우 테스트 추가

--- src/test/java/repository/AttendanceRecordRepositoryTest.java
+++ src/test/java/repository/AttendanceRecordRepositoryTest.java
@@ -18,15 +18,30 @@ class AttendanceRecordRepositoryTest {
 
     @Test
     @DisplayName("날짜(월일)에 크루의 출석 기록이 있으면 true를 반환한다")
-    void existsTest() {
+    void existsTest_true() {
         // given
         String nickname = "name";
-        LocalDate monday = LocalDate.of(2025, 2, 3);
+        LocalDate checkedDate = LocalDate.of(2025, 2, 3);
         LocalTime time = LocalTime.of(13, 0);
-        AttendanceStatus status = AttendanceStatus.of(monday, time);
-        AttendanceRecordRepository.add(new AttendanceRecord("name", monday, time, status));
+        AttendanceStatus status = AttendanceStatus.of(checkedDate, time);
+        AttendanceRecordRepository.add(new AttendanceRecord("name", checkedDate, time, status));
 
         // when & then
-        Assertions.assertThat(AttendanceRecordRepository.exists(nickname, monday)).isTrue();
+        Assertions.assertThat(AttendanceRecordRepository.exists(nickname, checkedDate)).isTrue();
+    }
+
+    @Test
+    @DisplayName("날짜(월일)에 크루의 출석 기록이 없으면 false를 반환한다")
+    void existsTest_false() {
+        // given
+        String nickname = "name";
+        LocalDate checkedDate = LocalDate.of(2025, 2, 3);
+        LocalTime time = LocalTime.of(13, 0);
+        AttendanceStatus status = AttendanceStatus.of(checkedDate, time);
+        AttendanceRecordRepository.add(new AttendanceRecord("name", checkedDate, time, status));
+
+        LocalDate uncheckedDate = LocalDate.of(2025, 2, 4);
+        // when & then
+        Assertions.assertThat(AttendanceRecordRepository.exists(nickname, uncheckedDate)).isFalse();
     }
 }
