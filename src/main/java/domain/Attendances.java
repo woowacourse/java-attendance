80a061ff9f6b52159dd7a389a8058f456ece4fc5
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-26 22:24:18 +0900

    refactor: Attendance의 일급컬렉션 Attendances 구현
    - 이미 존재하는 Attendance 객체가 추가될 경우 예외처리 메서드 구현

---
+++ src/main/java/domain/Attendances.java
@@ -0,0 +1,28 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Attendances {
+    private final List<Attendance> attendances = new ArrayList<>();
+
+    public void add(Attendance attendance) {
+        validateAttendance(attendance);
+        attendances.add(attendance);
+    }
+
+    private void validateAttendance(Attendance attendance) {
+        if (attendances.contains(attendance)) {
+            throw new IllegalStateException("[ERROR] 이미 출석이 완료되었습니다. 수정 기능을 이용하세요.");
+        }
+    }
+
+    public Attendance findByDay(Day day) {
+        return attendances.stream()
+                .filter(attendance -> attendance.has(day))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("[ERROR] 해당일에 출석 기록이 없습니다."));
+    }
+
+
+}
