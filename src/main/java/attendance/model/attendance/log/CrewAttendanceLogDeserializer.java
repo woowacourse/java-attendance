8f688ea24ed15f80629893015de7f090c7f09acd
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-26 15:09:41 +0900

    feat: 크루 출석 기록 역직렬화기 클래스 작성

---
+++ src/main/java/attendance/model/attendance/log/CrewAttendanceLogDeserializer.java
@@ -0,0 +1,11 @@
+package attendance.model.attendance.log;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class CrewAttendanceLogDeserializer {
+
+    public List<CrewAttendanceLog> deserializeFromCsv(final Path csvFilePath) {
+        return null;
+    }
+}
