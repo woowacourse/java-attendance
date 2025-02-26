881cfd9180210c6070f999b921d50febf3bb2c89
taek2222 <oot309@daum.net>
2025-02-26 23:48:34 +0900

    feat(ClassTime): 화요일 등교 시간도 가능하도록 개선

--- src/main/java/attendance/domain/ClassTime.java
+++ src/main/java/attendance/domain/ClassTime.java
@@ -1,13 +1,27 @@
 package attendance.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Arrays;
 
 public enum ClassTime {
-    ;
+    MONDAY(LocalTime.of(13, 0)),
+    TUESDAY(LocalTime.of(10, 0));
+
+    private final LocalTime startTime;
+
+    ClassTime(final LocalTime startTime) {
+        this.startTime = startTime;
+    }
 
     public static int calculateAttendanceDifference(final LocalDateTime attendanceDateTime) {
-        LocalTime monday = LocalTime.of(13, 0);
-        return (attendanceDateTime.toLocalTime().toSecondOfDay() - monday.toSecondOfDay()) / 60;
+        DayOfWeek dayOfWeek = attendanceDateTime.toLocalDate().getDayOfWeek();
+        ClassTime classTime = Arrays.stream(values())
+                .filter(weekday -> weekday.name().equals(dayOfWeek.name()))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+
+        return (attendanceDateTime.toLocalTime().toSecondOfDay() - classTime.startTime.toSecondOfDay()) / 60;
     }
 }
