f1f632ed8a5f5ba875878f28109ed768cb319956
jhan0121 <jhan0121@gmail.com>
2025-02-26 18:10:01 +0900

    Revert "test(green): HolidayChecker 공휴일 판정 기능 테스트 통과"
    This reverts commit da2ce3b6315de63c3f5ca401fbb0d05d2f2fca08.

--- src/test/java/HolidayChecker.java
+++ src/test/java/HolidayChecker.java
@@ -2,6 +2,6 @@ import java.time.LocalDate;
 
 public class HolidayChecker {
     public boolean isHoliday(final LocalDate targetDate) {
-        return targetDate.isEqual(LocalDate.of(2024, 12, 25));
+        return true;
     }
 }
