7bdbee22ed959f0c33b45609d0718b3911d36f6b
kimdev <kim__dev@hufs.ac.kr>
2025-02-26 16:17:18 +0900

    test: 공휴일인지 확인하는 테스트 작성

---
+++ src/test/java/attendance/domain/HolidayTest.java
@@ -0,0 +1,28 @@
+package attendance.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.time.LocalDateTime;
+import java.time.Year;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HolidayTest {
+
+    @CsvSource({
+            "1,1,true",
+            "1,2,false"
+    })
+    @ParameterizedTest
+    void 주어진_날짜가_공휴일인지_확인한다(int month, int day, boolean expected) {
+        // Given
+        LocalDateTime dateTime = Year.of(2025).atMonth(month).atDay(day).atTime(10, 00);
+
+        // When & Then
+        assertThat(Holiday.isHoliday(dateTime))
+                .isEqualTo(expected);
+    }
+}
