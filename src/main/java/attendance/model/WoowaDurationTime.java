5ba27a54c04db8f711e13f58f248c61f5c8537f9
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-26 18:27:40 +0900

    🔴️ red: 캠퍼스가 운영시간인지 확인하는 테스트 작성

---
+++ src/main/java/attendance/model/WoowaDurationTime.java
@@ -0,0 +1,11 @@
+package attendance.model;
+
+import java.time.LocalTime;
+
+public enum WoowaDurationTime {
+    ;
+
+    public static boolean isDurationTime(LocalTime time) {
+        return true;
+    }
+}
---
+++ src/test/java/model/WoowaDurationTimeTest.java
@@ -0,0 +1,37 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import attendance.model.WoowaDurationTime;
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WoowaDurationTimeTest {
+
+    @Test
+    @DisplayName("캠퍼스 운영시간이 아닌 경우")
+    void 입력한_시간이_운영시간인지_확인한다_1() {
+        // given
+        LocalTime time = LocalTime.of(7, 0);
+
+        // when
+        boolean isDurationTime = WoowaDurationTime.isDurationTime(time);
+
+        // then
+        assertThat(isDurationTime).isFalse();
+    }
+
+    @Test
+    @DisplayName("캠퍼스 운영시간인 경우")
+    void 입력한_시간이_운영시간인지_확인한다_2() {
+        // given
+        LocalTime time = LocalTime.of(8, 0);
+
+        // when
+        boolean isDurationTime = WoowaDurationTime.isDurationTime(time);
+
+        // then
+        assertThat(isDurationTime).isTrue();
+    }
+}
