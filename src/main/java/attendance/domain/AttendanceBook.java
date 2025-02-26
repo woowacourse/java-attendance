5530c0385a3ad313a6bb01b92d9a2e98336c841a
CheChe903 <cheche903@naver.com>
2025-02-26 20:17:27 +0900

    refactor(AttendanceBook): 불변 객체인 string에 의미 없는 final 삭제

--- src/main/java/attendance/domain/AttendanceBook.java
+++ src/main/java/attendance/domain/AttendanceBook.java
@@ -6,11 +6,11 @@ public class AttendanceBook {
 
     private final List<String> crewNames;
 
-    public AttendanceBook(List<String> crewNames) {
+    public AttendanceBook(final List<String> crewNames) {
         this.crewNames = crewNames;
     }
 
-    public void hasCrew(final String crewName) {
+    public void hasCrew(String crewName) {
         if (!crewNames.contains(crewName)) {
             throw new IllegalArgumentException("[ERROR] 출석부에 존재하지 않는 닉네임입니다.");
         }
