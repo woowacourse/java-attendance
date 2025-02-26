3a900ace320c5a2bdc331cf5efeecf094b3873c6
2jin2.1031 <2jin2.1031@gmail.com>
2025-02-26 14:53:15 +0900

    feat: csv 파일을 읽어오는 기능 구현

---
+++ src/test/java/util/CsvReaderTest.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CsvReaderTest {
+
+    @Test
+    @DisplayName("csv 파일을 읽어오는 기능이 잘 작동하는지")
+    void readFile() {
+        // given
+        String FILE_PATH = "src/main/resources/attendances.csv";
+        List<String[]> expected = List.of(
+                new String[]{"쿠키", "2024-12-13 10:08"},
+                new String[]{"빙봉", "2024-12-13 10:07"},
+                new String[]{"빙티", "2024-12-13 10:07"},
+                new String[]{"이든", "2024-12-13 10:07"},
+                new String[]{"빙봉", "2024-12-12 11:11"}
+        );
+
+        // when
+        Stream<String[]> parsedFile = CsvReader.readFile(FILE_PATH).stream()
+                .limit(5);
+
+        // then
+        Assertions.assertThat(parsedFile).containsAll(expected);
+    }
+}
