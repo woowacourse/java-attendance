8d1fae63f03bf13b5c6bdb39c8d5a8f83cf74234
Sung-june27 <kmymy27@gmail.com>
2025-02-26 15:12:06 +0900

    feat: 파일 입출력을 통해 파일 읽기 구현

---
+++ src/main/java/attendance/util/AttendancesFileReader.java
@@ -0,0 +1,26 @@
+package attendance.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AttendancesFileReader {
+
+    public static String read() {
+        try {
+            File file = new File("src/main/resources/attendances.csv");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine())!= null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
