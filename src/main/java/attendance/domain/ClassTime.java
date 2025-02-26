f73daaf10da08e40e1af18bb000e0f189f35d36d
taek2222 <oot309@daum.net>
2025-02-27 00:02:19 +0900

    refactor(ClassTime): 메서드 분리 및 로직 최적화

--- src/main/java/attendance/domain/ClassTime.java
+++ src/main/java/attendance/domain/ClassTime.java
@@ -1,9 +1,11 @@
 package attendance.domain;
 
-import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.TextStyle;
 import java.util.Arrays;
+import java.util.Locale;
 
 public enum ClassTime {
     MONDAY(LocalTime.of(13, 0)),
@@ -16,12 +18,34 @@ public enum ClassTime {
     }
 
     public static int calculateAttendanceDifference(final LocalDateTime attendanceDateTime) {
-        DayOfWeek dayOfWeek = attendanceDateTime.toLocalDate().getDayOfWeek();
-        ClassTime classTime = Arrays.stream(values())
-                .filter(weekday -> weekday.name().equals(dayOfWeek.name()))
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
+        ClassTime classTime = find(attendanceDateTime.toLocalDate());
+        return calculateDifferenceTime(
+                attendanceDateTime.toLocalTime(),
+                classTime.startTime
+        );
+    }
 
-        return (attendanceDateTime.toLocalTime().toSecondOfDay() - classTime.startTime.toSecondOfDay()) / 60;
+    private static ClassTime find(final LocalDate date) {
+        return Arrays.stream(values())
+                .filter(weekday -> isSameDayOfWeek(date, weekday))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(formatErrorMessage(date)));
+    }
+
+    private static boolean isSameDayOfWeek(final LocalDate date, final ClassTime weekday) {
+        return weekday.name().equals(date.getDayOfWeek().name());
+    }
+
+    private static int calculateDifferenceTime(final LocalTime time, final LocalTime comparisonTime) {
+        int differenceInSeconds = time.toSecondOfDay() - comparisonTime.toSecondOfDay();
+        return differenceInSeconds / 60;
+    }
+
+    private static String formatErrorMessage(final LocalDate date) {
+        return String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.",
+                date.getMonthValue(),
+                date.getDayOfMonth(),
+                date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA)
+        );
     }
 }
