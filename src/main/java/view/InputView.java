19720d34417ecbe210be3186c6d4800a4a6c0c02
soeun2537 <soeun2537@gmail.com>
2025-02-26 15:15:57 +0900

    feat: 출석 옵션을 입력 받는 로직 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,10 @@
 package view;
 
+import constant.InputViewMessage;
+import dto.AttendanceOptionRequest;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class InputView {
@@ -7,6 +12,17 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {
+    }
+
+    public static AttendanceOptionRequest readAttendanceOptionRequest(LocalDateTime dateTime) {
+        println(InputViewMessage.ATTENDANCE_OPTION_PROMPT.getMessage(
+                dateTime.getMonth().getValue(),
+                dateTime.getDayOfMonth(),
+                dateTime.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN)));
+        String option = scanner.nextLine();
+
+        printNewLine();
+        return new AttendanceOptionRequest(option);
     }
 
     private static void println(String message) {
