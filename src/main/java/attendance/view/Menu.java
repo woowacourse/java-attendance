6ee22a76c7dc2b716982ef591f42196d69440160
taek2222 <oot309@daum.net>
2025-02-26 21:44:15 +0900

    feat: 메뉴 및 탐색 기능 추가

--- src/main/java/attendance/view/Menu.java
+++ src/main/java/attendance/view/Menu.java
@@ -1,9 +1,20 @@
 package attendance.view;
 
-public enum Menu {
-    ;
+import java.util.Arrays;
 
-    public static void find(final String input) {
-        throw new IllegalArgumentException("[ERROR] 잘못된 메뉴 선택의 입력입니다.");
+public enum Menu {
+    CHECK("1");
+
+    private final String command;
+
+    Menu(final String command) {
+        this.command = command;
+    }
+
+    public static Menu find(final String input) {
+        return Arrays.stream(values())
+                .filter(menu -> menu.command.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 잘못된 메뉴 선택의 입력입니다."));
     }
 }
