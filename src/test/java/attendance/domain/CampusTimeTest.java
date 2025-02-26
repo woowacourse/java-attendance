8305503845c4048e14cde27a04f102b54bc516b5
taek2222 <oot309@daum.net>
2025-02-27 00:29:22 +0900

    test(CampusTimeTest): 캠퍼스 시간 파라미터 수정 및 테스트 추가
    - 파라미터의 잘못된 정의로 다르게 테스트한 부분을 수정했습니다.
    
    - 예외가 발생하지 않는 케이스도 추가해 검증했습니다.

--- src/test/java/attendance/domain/CampusTimeTest.java
+++ src/test/java/attendance/domain/CampusTimeTest.java
@@ -7,8 +7,21 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class CampusTimeTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "07:59",
+            "23:01"
+    })
+    @DisplayName("캠퍼스 운영 시간이 아닌 경우 예외가 발생한다")
+    void 캠퍼스_운영_시간이_아닌_경우_예외가_발생한다(LocalTime attendanceTime) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> CampusTime.validateOperateTime(attendanceTime))
+                .withMessage("[ERROR] 캠퍼스 운영 시간은 08:00 ~ 23:00 입니다.");
+    }
 
     @ParameterizedTest
     @CsvSource({
@@ -17,9 +30,8 @@ class CampusTimeTest {
             "23:00"
     })
     @DisplayName("캠퍼스 운영 시간이 아닌 경우 예외가 발생한다")
-    void 캠퍼스_운영_시간이_아닌_경우_예외가_발생한다(LocalTime attendanceTime) {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> CampusTime.validateOperateTime(attendanceTime))
-                .withMessage("[ERROR] 캠퍼스 운영 시간은 08:00 ~ 23:00 입니다.");
+    void 캠퍼스_운영_시간인_경우_예외가_발생하지_않는다(LocalTime attendanceTime) {
+        assertThatNoException()
+                .isThrownBy(() -> CampusTime.validateOperateTime(attendanceTime));
     }
 }
