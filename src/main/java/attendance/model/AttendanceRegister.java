08190410b68443b42454a1d1300a442aed8c6413
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-26 15:10:13 +0900

    🟢 green: 출석 수정 기능 구현

--- src/main/java/attendance/model/AttendanceRegister.java
+++ src/main/java/attendance/model/AttendanceRegister.java
@@ -32,5 +32,18 @@ public class AttendanceRegister {
     }
 
     public void modify(String crewName, LocalDate modifyDate, LocalTime modifyTime) {
+        validateContainsCrewName(crewName);
+        List<LocalDateTime> attendanceRecord = register.get(crewName);
+        int index = -1;
+        for (int i = 0; i < attendanceRecord.size(); ++i) {
+            if (attendanceRecord.get(i).toLocalDate().equals(modifyDate)) {
+                index = i;
+            }
+        }
+        if (index == -1) {
+            throw new IllegalArgumentException("존재하지 않는 출석일입니다.");
+        }
+        attendanceRecord.remove(index);
+        attendanceRecord.add(LocalDateTime.of(modifyDate, modifyTime));
     }
 }
