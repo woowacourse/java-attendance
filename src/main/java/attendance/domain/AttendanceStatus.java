8d3fe42daa12f968f3b6581cf4d0898ae38cb5f1
Firedrago95 <yonghwa95@naver.com>
2025-02-26 15:23:09 +0900

    feat: 출석 상태 판단 기능 구현

---
+++ src/main/java/attendance/domain/AttendanceStatus.java
@@ -0,0 +1,35 @@
+package attendance.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public enum AttendanceStatus {
+
+    PRESENCE,
+    LATE,
+    ABSENCE;
+
+    public static final LocalTime MONDAY_START_TIME = LocalTime.of(13, 0);
+    public static final LocalTime WEEKDAY_START_TIME = LocalTime.of(10, 0);
+    public static final int ABSENCE_BOUNDARY = 30;
+    public static final int LATE_BOUNDARY = 5;
+
+    public static AttendanceStatus findAttendanceStatus(LocalDate attendanceDate, LocalTime attendanceTime) {
+        DayOfWeek dayOfWeek = attendanceDate.getDayOfWeek();
+        if (dayOfWeek.equals(DayOfWeek.MONDAY)) {
+            return getAttendanceStatus(MONDAY_START_TIME, attendanceTime);
+        }
+        return getAttendanceStatus(WEEKDAY_START_TIME, attendanceTime);
+    }
+
+    private static AttendanceStatus getAttendanceStatus(LocalTime weekdayStartTime, LocalTime attendanceTime) {
+        if (attendanceTime.isAfter(weekdayStartTime.plusMinutes(ABSENCE_BOUNDARY))) {
+            return ABSENCE;
+        }
+        if (attendanceTime.isAfter(weekdayStartTime.plusMinutes(LATE_BOUNDARY))) {
+            return LATE;
+        }
+        return PRESENCE;
+    }
+}
---
+++ src/test/java/attendance/domain/AttendanceStatusTest.java
@@ -0,0 +1,53 @@
+package attendance.domain;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class AttendanceStatusTest {
+
+    @Test
+    void 월요일_지각상태를_찾는다() {
+        // given
+        LocalDate monday = LocalDate.of(2024, 12, 2);
+        LocalTime lateTime = LocalTime.of(13, 6);
+
+        // when
+        AttendanceStatus attendanceStatus = AttendanceStatus.findAttendanceStatus(monday, lateTime);
+
+        // then
+        assertThat(attendanceStatus).isEqualTo(AttendanceStatus.LATE);
+    }
+
+    @Test
+    void 금요일_결석상태를_찾는다() {
+        // given
+        LocalDate friday = LocalDate.of(2024, 12, 6);
+        LocalTime absenceTime = LocalTime.of(10, 31);
+
+        // when
+        AttendanceStatus attendanceStatus = AttendanceStatus.findAttendanceStatus(friday, absenceTime);
+
+        // then
+        assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ABSENCE);
+    }
+
+    @Test
+    void 화요일_출석상태를_찾는다() {
+        // given
+        LocalDate tuesday = LocalDate.of(2024, 12, 3);
+        LocalTime presenceTime = LocalTime.of(10, 2);
+
+        // when
+        AttendanceStatus attendanceStatus = AttendanceStatus.findAttendanceStatus(tuesday, presenceTime);
+
+        // then
+        assertThat(attendanceStatus).isEqualTo(AttendanceStatus.PRESENCE);
+    }
+}
