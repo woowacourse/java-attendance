cf4bceab3e1ecb2f50c5a3bd5b35a92885ba937d
soeun2537 <soeun2537@gmail.com>
2025-02-26 15:00:53 +0900

    feat: InputView 메시지를 관리하는 Enum 구현

---
+++ src/main/java/constant/InputViewMessage.java
@@ -0,0 +1,35 @@
+package constant;
+
+public enum InputViewMessage {
+
+    ATTENDANCE_OPTION_PROMPT("오늘은 %s월 %s일 %s입니다. 기능을 선택해 주세요.%n"
+            + "1. 출석 확인%n"
+            + "2. 출석 수정%n"
+            + "3. 크루별 출석 기록 확인%n"
+            + "4. 제적 위험자 확인%n"
+            + "Q. 종료"),
+
+    ATTENDANCE_CHECK_IN_NICKNAME_PROMPT("닉네임을 입력해 주세요."),
+    ATTENDANCE_CHECK_IN_TIME_PROMPT("등교 시간을 입력해 주세요."),
+
+    ATTENDANCE_UPDATE_NICKNAME_PROMPT("출석을 수정하려는 크루의 닉네임을 입력해 주세요."),
+    ATTENDANCE_UPDATE_DAY_PROMPT("수정하려는 날짜(일)를 입력해 주세요."),
+    ATTENDANCE_UPDATE_TIME_PROMPT("언제로 변경하겠습니까?"),
+
+    ATTENDANCE_HISTORY_NICKNAME_PROMPT("닉네임을 입력해 주세요."),
+    ;
+
+    private final String message;
+
+    InputViewMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return String.format(message);
+    }
+
+    public String getMessage(int args1, int args2, String args3) {
+        return String.format(message, args1, args2, args3);
+    }
+}
