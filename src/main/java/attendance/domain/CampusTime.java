3621710476a4dbd85c41d15281c0d829b87c421d
taek2222 <oot309@daum.net>
2025-02-27 00:35:47 +0900

    refactor(CampusTime): 에러 메시지 개선

--- src/main/java/attendance/domain/CampusTime.java
+++ src/main/java/attendance/domain/CampusTime.java
@@ -14,7 +14,14 @@ public enum CampusTime {
 
     public static void validateOperateTime(final LocalTime time) {
         if (START.time.isAfter(time) || END.time.isBefore(time)) {
-            throw new IllegalArgumentException("[ERROR] 캠퍼스 운영 시간은 08:00 ~ 23:00 입니다.");
+            throw new IllegalArgumentException(formatErrorMessage());
         }
+    }
+
+    private static String formatErrorMessage() {
+        return String.format("[ERROR] 캠퍼스 운영 시간은 %s ~ %s 입니다.",
+                START.time,
+                END.time
+        );
     }
 }
