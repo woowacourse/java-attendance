c4ba3ccae6ea62ff255ab1472b31ae8c95e292c7
LeeJaeHoon <dlwogns3413@naver.com>
2025-02-26 15:07:54 +0900

    feat: 아무 대상자가 아닐 경우 NONE 반환 기능 구현 (green)

--- src/main/java/attendance/domain/WarningLevel.java
+++ src/main/java/attendance/domain/WarningLevel.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.function.Function;
 
 public enum WarningLevel {
-    NONE(totalAbsent -> false),
+    NONE(totalAbsent -> totalAbsent < 2),
     WARNING(totalAbsent -> totalAbsent == 2),
     INTERVIEW(totalAbsent -> 3 <= totalAbsent && totalAbsent <= 5),
     WEEDING(totalAbsent -> totalAbsent > 5);
