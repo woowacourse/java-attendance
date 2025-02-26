d4191dffe19a6f451d0e525a76640b398479a5c9
taek2222 <oot309@daum.net>
2025-02-27 00:32:46 +0900

    test(CampusTimeTest): 메서드 영어 및 위치 변경

--- src/test/java/attendance/domain/CampusTimeTest.java
+++ src/test/java/attendance/domain/CampusTimeTest.java
@@ -9,19 +9,8 @@ import java.time.LocalTime;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
+@DisplayName("캠퍼스 운영 시간 테스트")
 class CampusTimeTest {
-
-    @ParameterizedTest
-    @CsvSource({
-            "07:59",
-            "23:01"
-    })
-    @DisplayName("캠퍼스 운영 시간이 아닌 경우 예외가 발생한다")
-    void 캠퍼스_운영_시간이_아닌_경우_예외가_발생한다(LocalTime attendanceTime) {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> CampusTime.validateOperateTime(attendanceTime))
-                .withMessage("[ERROR] 캠퍼스 운영 시간은 08:00 ~ 23:00 입니다.");
-    }
 
     @ParameterizedTest
     @CsvSource({
@@ -29,9 +18,21 @@ class CampusTimeTest {
             "15:30",
             "23:00"
     })
-    @DisplayName("캠퍼스 운영 시간이 아닌 경우 예외가 발생한다")
-    void 캠퍼스_운영_시간인_경우_예외가_발생하지_않는다(LocalTime attendanceTime) {
+    @DisplayName("캠퍼스_운영_시간인_경우_예외가_발생하지_않는다")
+    void shouldNotThrowExceptionWhenWithinCampusOperatingHours(LocalTime attendanceTime) {
         assertThatNoException()
                 .isThrownBy(() -> CampusTime.validateOperateTime(attendanceTime));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "07:59",
+            "23:01"
+    })
+    @DisplayName("캠퍼스 운영 시간이 아닌 경우 예외가 발생한다")
+    void shouldThrowExceptionWhenNotWithinCampusOperatingHours(LocalTime attendanceTime) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> CampusTime.validateOperateTime(attendanceTime))
+                .withMessage("[ERROR] 캠퍼스 운영 시간은 08:00 ~ 23:00 입니다.");
     }
 }
