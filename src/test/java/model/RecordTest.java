c6119e9557ff5e1c11c6f8c330e8d2a9987e1234
(minjae8563) <(minjae8563@naver.com)>
2025-02-26 11:15:44 +0900

    refactor : 메서드 네임 변경,

--- src/test/java/model/RecordTest.java
+++ src/test/java/model/RecordTest.java
@@ -20,7 +20,7 @@ class RecordTest {
     @DisplayName("찾고자 하는 로컬 데이트 타임을 찾는 메서드 테스트")
     void test2() {
         org.assertj.core.api.Assertions.assertThat(record
-                .isSameDay(LocalDateTime.of(2024,12,12,0,0)))
+                .findSameDay(LocalDateTime.of(2024,12,12,0,0)))
                 .isEqualTo(LocalDateTime.of(2024,12,12,0,0));
     }
 
@@ -37,7 +37,7 @@ class RecordTest {
     @DisplayName("두 로컬 데이트 타임을 일자까지만 비교하여 같은 날인지 확인하는 메서드 테스트")
     void test4() {
         Assertions.assertTrue(
-                record.isSameDay(LocalDateTime.of(2024,12,12,0,0),
+                record.findSameDay(LocalDateTime.of(2024,12,12,0,0),
                         LocalDateTime.of(2024,12,12,13,59))
         );
     }
