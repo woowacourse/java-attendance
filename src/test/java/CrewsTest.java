ae8bad96956807250c468b87437a6ea0f8b6620e
changuii <rhljh7410@gmail.com>
2025-02-26 14:23:37 +0900

    red: 닉네임에 해당하는 크루가 존재하지 않는다면, 예외를 발생시켜라

--- src/test/java/CrewsTest.java
+++ src/test/java/CrewsTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.Crew;
 import domain.Crews;
@@ -31,5 +32,19 @@ public class CrewsTest {
     @Nested
     @DisplayName("실패 테스트")
     class FailCases {
+
+        @DisplayName("닉네임에 해당하는 크루가 존재하지 않는다면, 예외가 발생한다.")
+        @Test
+        public void findByName() throws Exception {
+            // given
+            final var crewName = "우가";
+            final var crews = new Crews(List.of());
+
+            // when & then
+            assertThatThrownBy(() -> {
+                crews.findByName(crewName);
+            }).isInstanceOf(IllegalArgumentException.class);
+
+        }
     }
 }
