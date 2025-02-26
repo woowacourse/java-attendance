864e0aa43320e00de6870abb7457ef0e8a9bb308
threepebbles <threepebbles@naver.com>
2025-02-26 18:10:54 +0900

    remove: 주석 제거

--- src/main/java/domain/LectureTime.java
+++ src/main/java/domain/LectureTime.java
@@ -34,8 +34,7 @@ public enum LectureTime {
                 .map(lectureTime -> lectureTime.dayOfWeek)
                 .anyMatch(dayOfWeek1 -> dayOfWeek1 == date.getDayOfWeek());
     }
-
-    // TODO: 날짜에 맞는 교육 시간을 반환한다, 교육 날이 아니면 예외를 발생시킨다
+    
     public static LectureTime from(LocalDate date) {
         return Arrays.stream(values())
                 .filter(lectureTime -> lectureTime.dayOfWeek == date.getDayOfWeek())
