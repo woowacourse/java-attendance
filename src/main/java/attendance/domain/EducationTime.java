eac6982bc4639e1bd3386779c2332af1971fa300
YuyoungRhee <bbclara@naver.com>
2025-02-27 01:51:08 +0900

    refactor(EducationTime): 사용하지 않는 필드 삭제
    - endTime은 추후 사용될 가능성이 있으므로 삭제하지 않음

--- src/main/java/attendance/domain/EducationTime.java
+++ src/main/java/attendance/domain/EducationTime.java
@@ -7,19 +7,17 @@ import java.time.LocalTime;
 import java.util.Arrays;
 
 public enum EducationTime {
-    MONDAY("월요일", DayOfWeek.MONDAY, LocalTime.of(13, 0), LocalTime.of(18, 0)),
-    TUESDAY("화요일", DayOfWeek.TUESDAY, LocalTime.of(10, 0), LocalTime.of(18, 0)),
-    WEDNESDAY("수요일", DayOfWeek.WEDNESDAY, LocalTime.of(10, 0), LocalTime.of(18, 0)),
-    THURSDAY("목요일", DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(18, 0)),
-    FRIDAY("금요일", DayOfWeek.FRIDAY, LocalTime.of(10, 0), LocalTime.of(18, 0));
+    MONDAY(DayOfWeek.MONDAY, LocalTime.of(13, 0), LocalTime.of(18, 0)),
+    TUESDAY(DayOfWeek.TUESDAY, LocalTime.of(10, 0), LocalTime.of(18, 0)),
+    WEDNESDAY(DayOfWeek.WEDNESDAY, LocalTime.of(10, 0), LocalTime.of(18, 0)),
+    THURSDAY(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(18, 0)),
+    FRIDAY(DayOfWeek.FRIDAY, LocalTime.of(10, 0), LocalTime.of(18, 0));
 
-    private final String name;
     private final DayOfWeek dayOfWeek;
     private final LocalTime startTime;
     private final LocalTime endTime;
 
-    EducationTime(String name, DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
-        this.name = name;
+    EducationTime(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
         this.dayOfWeek = dayOfWeek;
         this.startTime = startTime;
         this.endTime = endTime;
