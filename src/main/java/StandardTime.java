163e3742e8c9d164ebbbd4a3ea56cf8adbeb06dd
BWY <quantumjump10@gmail.com>
2025-02-26 16:49:15 +0900

    feat: 요일에 따라 기준 시간 enum을 반환하는 기능 추가

---
+++ src/main/java/StandardTime.java
@@ -0,0 +1,37 @@
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.Arrays;
+
+public enum StandardTime {
+
+    MONDAY(DayOfWeek.MONDAY, LocalTime.of(13, 5), LocalTime.of(13, 30)),
+    TUESDAY(DayOfWeek.TUESDAY, LocalTime.of(10, 5), LocalTime.of(10, 30)),
+    WEDNESDAY(DayOfWeek.WEDNESDAY, LocalTime.of(10, 5), LocalTime.of(10, 30)),
+    THURSDAY(DayOfWeek.THURSDAY, LocalTime.of(10, 5), LocalTime.of(10, 30)),
+    FRIDAY(DayOfWeek.FRIDAY, LocalTime.of(10, 5), LocalTime.of(10, 30));
+
+    private final DayOfWeek dayOfWeek;
+    private final LocalTime lateTime;
+    private final LocalTime absentTime;
+
+    StandardTime(DayOfWeek dayOfWeek, LocalTime lateTime, LocalTime absentTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.lateTime = lateTime;
+        this.absentTime = absentTime;
+    }
+
+    public static StandardTime findByDayOfWeek(DayOfWeek dayOfWeek) {
+        return Arrays.stream(values())
+                .filter(standardTime -> standardTime.dayOfWeek == dayOfWeek)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 요일의 기준 시간 정보가 없습니다."));
+    }
+
+    public LocalTime getAbsentTime() {
+        return absentTime;
+    }
+
+    public LocalTime getLateTime() {
+        return lateTime;
+    }
+}
