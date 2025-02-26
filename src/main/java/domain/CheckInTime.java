2a1f1a79cd492cb37ed6a97ee1a4b2efc5662823
geonwoo <kysub99@naver.com>
2025-02-26 20:43:41 +0900

    feat(CheckInTime): TreeSet 사용을 위한 기본 함수 작성

--- src/main/java/domain/CheckInTime.java
+++ src/main/java/domain/CheckInTime.java
@@ -4,8 +4,9 @@ import exception.AppException;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
 
-public class CheckInTime {
+public class CheckInTime implements Comparable<CheckInTime> {
     private final LocalDate checkInDate;
     private final LocalTime checkInTime;
 
@@ -26,5 +27,27 @@ public class CheckInTime {
         if (checkInTime.isBefore(CAMPUS_START_TIME) || checkInTime.isAfter(CAMPUS_END_TIME)) {
             throw new AppException("캠퍼스 이용 시간은 08시부터 23시까지 입니다.");
         }
+    }
+
+    @Override
+    public int compareTo(CheckInTime o) {
+        int i = this.checkInDate.compareTo(o.checkInDate);
+        if (i != 0) {
+            return i;
+        }
+        return this.checkInTime.compareTo(o.checkInTime);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CheckInTime that = (CheckInTime) o;
+        return checkInDate.equals(that.checkInDate) && checkInTime.equals(that.checkInTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(checkInDate, checkInTime);
     }
 }
