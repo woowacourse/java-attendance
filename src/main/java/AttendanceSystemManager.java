6495e7f10442496f9e490009be8998738e2602a5
Jin409 <sualng123@gmail.com>
2025-02-26 14:21:47 +0900

    feat: 존재하지 않는 크루이거나 수정하고자 하는 출석 기록이 존재하지 않으면 예외를 던져라

--- src/main/java/AttendanceSystemManager.java
+++ src/main/java/AttendanceSystemManager.java
@@ -23,4 +23,13 @@ public class AttendanceSystemManager {
 
         AttendanceType attendanceType = AttendanceType.findAttendanceTypeByDateTime(requestedAt);
     }
+
+    public void update(String nickname, LocalDateTime requestedAt) {
+        Crew crew = crews.findCrewByName(nickname);
+
+        boolean existedHistory = attendanceHistories.checkExistenceByCrewAndDate(crew, requestedAt.toLocalDate());
+        if (!existedHistory) {
+            throw new IllegalArgumentException("존재하지 않는 출석 기록입니다. 먼저 등록 기능을 이용해주세요.");
+        }
+    }
 }
