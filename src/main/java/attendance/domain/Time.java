09b6bee63f3bc6dc3084390d5c9c642b36d45538
CheChe903 <cheche903@naver.com>
2025-02-26 20:01:53 +0900

    feat(Time): 주말 출석 검증 성공 로직 구현

--- src/main/java/attendance/domain/Time.java
+++ src/main/java/attendance/domain/Time.java
@@ -7,13 +7,18 @@ public class Time {
     private final LocalDateTime attendanceTime;
 
     public Time(LocalDateTime attendanceTime) {
+        validateHoliday();
         validateCampusOperationTime(attendanceTime);
         this.attendanceTime = attendanceTime;
     }
 
     private void validateCampusOperationTime(LocalDateTime attendanceTime) {
         if (attendanceTime.getHour() < 8 || (attendanceTime.getHour() == 23 && attendanceTime.getMinute() > 0)) {
-            throw new IllegalArgumentException("[ERROR] 캠퍼스 운영 시간이 아닙니다.");
+            throw new IllegalArgumentException("[ERROR] 주말 및 공휴일은 출석할 수 없습니다.");
         }
+    }
+
+    private void validateHoliday() {
+        throw new IllegalArgumentException("[ERROR] 주말 및 공휴일은 출석할 수 없습니다.");
     }
 }
