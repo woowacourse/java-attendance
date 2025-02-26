9db39e39fc02c5a9b34b03dc343f258801feb184
LeeJaeHoon <dlwogns3413@naver.com>
2025-02-26 14:24:03 +0900

    refactor: 지각 계산 로직 리팩터링

--- src/main/java/attendance/domain/AttendanceType.java
+++ src/main/java/attendance/domain/AttendanceType.java
@@ -3,18 +3,25 @@ package attendance.domain;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.function.Function;
 
 public enum AttendanceType {
-    LATE;
+    LATE((duration -> duration.toMinutes() > 5));
+
+    private final Function<Duration, Boolean> isMatch;
+
+    AttendanceType(Function<Duration, Boolean> isMatch) {
+        this.isMatch = isMatch;
+    }
 
     public static AttendanceType calculate(DayOfWeek dayOfWeek, LocalTime attendanceTime) {
         LocalTime attendanceStartTime = getAttendanceStartTime(dayOfWeek);
         Duration duration = Duration.between(attendanceStartTime, attendanceTime);
-        long minutes = duration.getSeconds() / 60;
-        if (minutes > 5) {
-            return LATE;
-        }
-        return null;
+        return Arrays.stream(values())
+                .filter(attendanceType -> attendanceType.isMatch.apply(duration))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 출석 타입이 없습니다."));
     }
 
     private static LocalTime getAttendanceStartTime(DayOfWeek dayOfWeek) {
