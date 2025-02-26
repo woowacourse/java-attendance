367bd7b1186b09c5e96e499930621a48a8781abc
changuii <rhljh7410@gmail.com>
2025-02-26 15:43:28 +0900

    green: 존재하지 않는 크루에 대해 AttendanceHistory를 요구하면, 예외가 발생시켜라

--- src/main/java/domain/AttendanceBook.java
+++ src/main/java/domain/AttendanceBook.java
@@ -22,6 +22,9 @@ public class AttendanceBook {
     }
 
     public AttendanceHistory findByCrew(final Crew crew) {
+        if(!attendanceBook.containsKey(crew)){
+            throw new IllegalArgumentException();
+        }
         return attendanceBook.get(crew);
     }
 }
