1078d5036ce7e6eaf3cf6090a247d0a362decbef
taek2222 <oot309@daum.net>
2025-02-27 00:26:05 +0900

    test(CampusTimeTest): 캠퍼스 운영 시간 파라미터 테스트 추가

--- src/test/java/attendance/domain/CampusTimeTest.java
+++ src/test/java/attendance/domain/CampusTimeTest.java
@@ -1,7 +1,8 @@
 package attendance.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalTime;
 
@@ -9,15 +10,16 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 class CampusTimeTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "08:00",
+            "15:30",
+            "23:00"
+    })
     @DisplayName("캠퍼스 운영 시간이 아닌 경우 예외가 발생한다")
-    void 캠퍼스_운영_시간이_아닌_경우_예외가_발생한다() {
-        // given
-        LocalTime time = LocalTime.MAX;
-
-        // when & then
+    void 캠퍼스_운영_시간이_아닌_경우_예외가_발생한다(LocalTime attendanceTime) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> CampusTime.validateOperateTime(time))
+                .isThrownBy(() -> CampusTime.validateOperateTime(attendanceTime))
                 .withMessage("[ERROR] 캠퍼스 운영 시간은 08:00 ~ 23:00 입니다.");
     }
 }
