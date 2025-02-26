6e615dd25a0d10540a278b190740f0cc8d9544ca
Hacanna42 <dtmdgus901@gmail.com>
2025-02-26 16:17:45 +0900

    feat: 출력 관련 기능 구현

---
+++ src/main/java/view/FeatureType.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.util.Arrays;
+
+public enum FeatureType {
+    CHECK_ATTENDANCE("1", "출석 확인"),
+    EDIT_ATTENDANCE("2", "출석 수정"),
+    CHECK_ATTENDANCE_OF_CREW("3", "크루별 출석 기록 확인"),
+    CHECK_CREW_OF_BAN_RISK("4", "제적 위험자 확인"),
+    QUIT("Q", "종료"),
+    ;
+
+    private final String key;
+    private final String name;
+
+    FeatureType(String key, String name) {
+        this.key = key;
+        this.name = name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static FeatureType from(String key) {
+        return Arrays.stream(FeatureType.values())
+                .filter(value -> value.getKey().equals(key))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,40 @@
 package view;
 
-public class InputView {
+import java.util.Scanner;
 
+public class InputView {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static FeatureType askFeature() {
+        for (FeatureType value : FeatureType.values()) {
+            System.out.printf("%s. %s%n", value.getKey(), value.getName());
+        }
+
+        return FeatureType.from(scanner.next());
+    }
+
+    public static String askNickname(boolean isForEdit) {
+        if (isForEdit) {
+            System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+            return scanner.next();
+        }
+
+        System.out.println("닉네임을 입력해 주세요.");
+        return scanner.next();
+    }
+
+    public static String askAttendanceTime(boolean isForEdit) {
+        if (isForEdit) {
+            System.out.println("언제로 변경하겠습니까?");
+            return scanner.next();
+        }
+
+        System.out.println("등교 시간을 입력해 주세요.");
+        return scanner.next();
+    }
+
+    public static String askDayForEdit() {
+        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        return scanner.next();
+    }
 }
