d019d10fc14c2bb6e54cf2fd1be9cf5214701bc1
kimdev <kim__dev@hufs.ac.kr>
2025-02-26 15:05:29 +0900

    feat: 파일 전체 라인을 읽는 FileLineReader의 기능 구현

---
+++ src/main/java/attendance/view/FileLineReader.java
@@ -0,0 +1,16 @@
+package attendance.view;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class FileLineReader {
+    public List<String> readAllLines(final String path, final String fileName) {
+        try {
+            return Files.readAllLines(Path.of(path + fileName));
+        } catch (IOException exception) {
+            throw new IllegalArgumentException("올바른 파일 경로를 입력해 주세요.");
+        }
+    }
+}
