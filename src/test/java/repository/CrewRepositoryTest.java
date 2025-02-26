60c55be923778929f33f27e99e959a4151028d00
threepebbles <threepebbles@naver.com>
2025-02-26 15:56:01 +0900

    test: 매 테스트 실행 시 Crew 저장소 초기화

--- src/test/java/repository/CrewRepositoryTest.java
+++ src/test/java/repository/CrewRepositoryTest.java
@@ -2,10 +2,16 @@ package repository;
 
 import domain.Crew;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CrewRepositoryTest {
+    @AfterEach
+    void clearCrewRepository() {
+        CrewRepository.clear();
+    }
+
     @Test
     @DisplayName("존재하는 크루라면 true를 반환한다")
     void crewExistsTest_true() {
