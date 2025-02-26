a282650e76410727a9090cb2a4229d8e53889e63
kimdev <kim__dev@hufs.ac.kr>
2025-02-26 16:02:55 +0900

    test: 출석시간 객체의 예외 테스트 작성

--- src/test/java/attendance/domain/AttendanceDateTimeTest.java
+++ src/test/java/attendance/domain/AttendanceDateTimeTest.java
@@ -12,10 +12,54 @@ public class AttendanceDateTimeTest {
     @Test
     void 출석_일자와_시간을_알려주면_해당_시간대의_출석시간_객체를_생성한다() {
         // Given
-        LocalDateTime attendanceDateTime = Year.of(2025).atMonth(2).atDay(26).atTime(10, 00);
+        LocalDateTime attendanceDateTime = Year.of(2025).atMonth(2).atDay(26).atTime(10, 0);
 
         // When & Then
         assertThatCode(() -> new AttendanceDateTime(attendanceDateTime))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 주말에는_출석할_수_없다() {
+        // Given
+        LocalDateTime attendanceDateTime = Year.of(2025).atMonth(2).atDay(23).atTime(10, 0);
+
+        // When & Then
+        assertThatThrownBy(() -> new AttendanceDateTime(attendanceDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("2월 23일 일요일은 등교일이 아닙니다.");
+    }
+
+    @Test
+    void 공휴일에는_출석할_수_없다() {
+        // Given
+        LocalDateTime attendanceDateTime = Year.of(2025).atMonth(1).atDay(1).atTime(10, 0);
+
+        // When & Then
+        assertThatThrownBy(() -> new AttendanceDateTime(attendanceDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1월 1일 수요일은 등교일이 아닙니다.");
+    }
+
+    @Test
+    void 캠퍼스_운영시간_외에는_출석할_수_없다() {
+        // Given
+        LocalDateTime attendanceDateTime = Year.of(2025).atMonth(2).atDay(26).atTime(7, 0);
+
+        // When & Then
+        assertThatThrownBy(() -> new AttendanceDateTime(attendanceDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("7시 0분은 캠퍼스 운영 시간이 아닙니다.");
+    }
+
+    @Test
+    void 미래의_시간에는_출석할_수_없다() {
+        // Given
+        LocalDateTime attendanceDateTime = Year.of(2025).atMonth(12).atDay(26).atTime(7, 0);
+
+        // When & Then
+        assertThatThrownBy(() -> new AttendanceDateTime(attendanceDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("미래의 시간으로는 출석할 수 없습니다.");
     }
 }
