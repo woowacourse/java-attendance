95940606580a5cc58012128e05ab0f8c576942e4
jhan0121 <jhan0121@gmail.com>
2025-02-26 17:41:34 +0900

    refactor: OperationTimeChecker 날짜가 주어졌을 때, 주말인지 판정하는 로직 수정

--- src/test/java/OperationTimeChecker.java
+++ src/test/java/OperationTimeChecker.java
@@ -4,9 +4,6 @@ import java.time.LocalDate;
 public class OperationTimeChecker {
 
     public boolean isWeekend(final LocalDate targetDate) {
-        if (targetDate.getDayOfWeek() == DayOfWeek.SUNDAY || targetDate.getDayOfWeek() == DayOfWeek.SATURDAY) {
-            return true;
-        }
-        return false;
+        return targetDate.getDayOfWeek() == DayOfWeek.SUNDAY || targetDate.getDayOfWeek() == DayOfWeek.SATURDAY;
     }
 }
