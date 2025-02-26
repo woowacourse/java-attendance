dc4b457fcdb45ea9db1dd9b3d6a6b1c6cdc7041a
yesjuhee <juhee0924@g.skku.edu>
2025-02-26 15:19:41 +0900

    feat(refactor) : 1.1 닉네임과 등교 시간을 받으면 오늘 날짜로 출석 기록을 생성할 수 있다.
    - 테스트에 중첩 클래스 사용
    - 테스트 메서드 이름 변경
    - 출결 기준 시간 상수화

--- src/main/java/domain/Attendance.java
+++ src/main/java/domain/Attendance.java
@@ -3,11 +3,14 @@ package domain;
 import java.time.LocalTime;
 
 public class Attendance {
-    public String attend(String nickname, LocalTime time) {
-        if (time.isBefore(LocalTime.of(10, 5)) || time.equals(LocalTime.of(10, 5))) {
+    private static final LocalTime PRESENT_THRESHOLD_TIME = LocalTime.of(10, 5);
+    private static final LocalTime TARDY_THRESHOLD_TIME = LocalTime.of(10, 30);
+
+    public String checkAttendance(String nickname, LocalTime time) {
+        if (time.isBefore(PRESENT_THRESHOLD_TIME) || time.equals(PRESENT_THRESHOLD_TIME)) {
             return "출석";
         }
-        if (time.isBefore(LocalTime.of(10, 30)) || time.equals(LocalTime.of(10, 30))) {
+        if (time.isBefore(TARDY_THRESHOLD_TIME) || time.equals(TARDY_THRESHOLD_TIME)) {
             return "지각";
         }
         return "결석";
--- src/test/java/domain/AttendanceTest.java
+++ src/test/java/domain/AttendanceTest.java
@@ -4,46 +4,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("1.1 닉네임과 등교 시간을 받으면 오늘 날짜로 출석 기록을 생성할 수 있다.")
 public class AttendanceTest {
-    @Test
-    @DisplayName("10시 5분에 출석할 경우 출석으로 처리한다.")
-    void attendTest1() {
-        // given
-        Attendance attendance = new Attendance();
-        String nickname = "노랑";
-        LocalTime time = LocalTime.of(10, 5);
-        // when
-        String attendanceRecord = attendance.attend(nickname, time);
-        // then
-        assertThat(attendanceRecord).isEqualTo("출석");
-    }
+    @Nested
+    @DisplayName("1.1 닉네임과 등교 시간을 받으면 오늘 날짜로 출석 기록을 생성할 수 있다.")
+    class AttendanceCheckTest {
+        @Test
+        @DisplayName("10시 5분에 출석할 경우 출석으로 처리한다.")
+        void testPresentAttendance() {
+            // given
+            Attendance attendance = new Attendance();
+            String nickname = "노랑";
+            LocalTime time = LocalTime.of(10, 5);
+            // when
+            String attendanceRecord = attendance.checkAttendance(nickname, time);
+            // then
+            assertThat(attendanceRecord).isEqualTo("출석");
+        }
 
-    @Test
-    @DisplayName("10시 30분에 출석할 경우 지각으로 처리한다.")
-    void attendTest2() {
-        // given
-        Attendance attendance = new Attendance();
-        String nickname = "노랑";
-        LocalTime time = LocalTime.of(10, 30);
-        // when
-        String attendanceRecord = attendance.attend(nickname, time);
-        // then
-        assertThat(attendanceRecord).isEqualTo("지각");
-    }
+        @Test
+        @DisplayName("10시 30분에 출석할 경우 지각으로 처리한다.")
+        void testTardyAttendance() {
+            // given
+            Attendance attendance = new Attendance();
+            String nickname = "노랑";
+            LocalTime time = LocalTime.of(10, 30);
+            // when
+            String attendanceRecord = attendance.checkAttendance(nickname, time);
+            // then
+            assertThat(attendanceRecord).isEqualTo("지각");
+        }
 
-    @Test
-    @DisplayName("10시 30분 1초에 출석할 경우 결석으로 처리한다.")
-    void attendTest3() {
-        // given
-        Attendance attendance = new Attendance();
-        String nickname = "노랑";
-        LocalTime time = LocalTime.of(10, 30, 1);
-        // when
-        String attendanceRecord = attendance.attend(nickname, time);
-        // then
-        assertThat(attendanceRecord).isEqualTo("결석");
+        @Test
+        @DisplayName("10시 30분 1초에 출석할 경우 결석으로 처리한다.")
+        void testAbsentAttendance() {
+            // given
+            Attendance attendance = new Attendance();
+            String nickname = "노랑";
+            LocalTime time = LocalTime.of(10, 30, 1);
+            // when
+            String attendanceRecord = attendance.checkAttendance(nickname, time);
+            // then
+            assertThat(attendanceRecord).isEqualTo("결석");
+        }
     }
 }
