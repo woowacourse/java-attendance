fba80c587adf050c49c8d26462c0709e2d3dd5c8
taek2222 <oot309@daum.net>
2025-02-26 23:35:37 +0900

    test(ClassTimeTest): 월요일 출석 여러 파라미터 추가

--- src/test/java/attendance/domain/ClassTimeTest.java
+++ src/test/java/attendance/domain/ClassTimeTest.java
@@ -1,7 +1,8 @@
 package attendance.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDateTime;
 
@@ -10,17 +11,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("출석 시간 테스트")
 class ClassTimeTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "2024-12-02T13:00, 0",
+            "2024-12-02T13:06, 6",
+            "2024-12-02T13:31, 31",
+    })
     @DisplayName("월요일 등교 시작 시간으로부터 차이를 반환한다")
-    void shouldReturnDifferenceFromMondayStartTime() {
-        // given
-        LocalDateTime attendanceDateTime = LocalDateTime.of(2024, 12, 2, 13, 0);
-
+    void shouldReturnDifferenceFromMondayStartTime(LocalDateTime attendanceDateTime, int excepted) {
         // when
         int result = ClassTime.calculateAttendanceDifference(attendanceDateTime);
 
         // then
-        assertThat(result)
-                .isEqualTo(0);
+        assertThat(result).isEqualTo(excepted);
     }
 }
