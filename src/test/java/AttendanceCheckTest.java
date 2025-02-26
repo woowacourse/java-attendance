58642f6b17bab56283d38372a39e85eb49bde7be
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-26 15:19:06 +0900

    test: 시작 시간으로부터 5분 초과된 출석은 지각 처리 테스트 작성

--- src/test/java/AttendanceCheckTest.java
+++ src/test/java/AttendanceCheckTest.java
@@ -6,16 +6,6 @@ import org.junit.jupiter.api.Test;
 
 public class AttendanceCheckTest {
 
-    //닉네임과 등교 시간을 입력하면 출석할 수 있다.
-    //출석 후 출석 기록을 확인할 수 있다.
-    //이미 출석한 경우, 다시 출석할 수 없으며 수정 기능을 이용하도록 안내한다.
-
-    //닉네임을 입력해 주세요.
-    //이든
-    //등교 시간을 입력해 주세요.
-    //09:59
-    //
-    //12월 05일 화요일 09:59 (출석)
     @Test
     void 닉네임과_등교_시간을_입력하면_출석할_수_있다() {
         final var nickname = "에드";
@@ -51,5 +41,18 @@ public class AttendanceCheckTest {
         assertThatThrownBy(() -> AttendanceCheck.attend(nickname, attendanceTime))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("[ERROR] 이미 출석이 완료되었습니다. 수정 기능을 이용하세요.");
+    }
+
+    //교육 시간은 월요일은 13:00~18:00, 화요일~금요일은 10:00~18:00이다.
+    //해당 요일의 시작 시각으로부터 5분 초과는 지각으로 간주한다.
+    //해당 요일의 시작 시각으로부터 30분 초과는 결석으로 간주한다.
+    @Test
+    void 시작_시간으로부터_5분_초과는_지각으로_처리한다() {
+        final var nickname = "에드";
+        final var attendanceTime = LocalTime.of(10, 6);
+
+        AttendanceCheck.attend(nickname, attendanceTime);
+        final var expected = "지각";
+        assertEquals(expected, AttendanceCheck.getAttendanceStatus(nickname));
     }
 }
