bc0bdc2e7207cf1ee7c7db10ce38ecee5769ad41
(minjae8563) <(minjae8563@naver.com)>
2025-02-26 12:15:06 +0900

    refactor : Controller 생성자를 통해 오늘 날짜 주입

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,10 @@
 import controller.Controller;
+import java.time.LocalDate;
+import model.TodayDate;
 
 public class Application {
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        Controller controller = new Controller(new TodayDate(LocalDate.of(2024, 12, 14)));
         controller.start();
     }
 }
