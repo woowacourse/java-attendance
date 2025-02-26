aea1197ef967aa2c59756abe4453fa3e30ce7fca
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-26 14:21:34 +0900

    🔴 red: 출석 확인 테스트 작성

---
+++ src/main/java/attendance/model/AttendanceRegister.java
@@ -0,0 +1,13 @@
+package attendance.model;
+
+import java.time.LocalDateTime;
+
+public class AttendanceRegister {
+
+    public void attend(String 한스, AttendanceRegister attendanceRegister) {
+    }
+
+    public LocalDateTime findAttendanceByName(String 한스) {
+        return LocalDateTime.now();
+    }
+}
---
+++ src/test/java/model/AttendanceTest.java
@@ -0,0 +1,23 @@
+package model;
+
+import attendance.model.AttendanceRegister;
+import java.time.LocalDateTime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AttendanceTest {
+
+    @Test
+    void 크루가_오늘날짜로_출석부에_출석을_한다() {
+        // given
+        AttendanceRegister attendanceRegister = new AttendanceRegister();
+        LocalDateTime attendanceDateTime = LocalDateTime.of(2024, 12, 10, 10, 5);
+        attendanceRegister.attend("한스", attendanceRegister);
+
+        // when
+        LocalDateTime registryAttendanceDateTime = attendanceRegister.findAttendanceByName("한스");
+
+        // then
+        Assertions.assertThat(registryAttendanceDateTime).isEqualTo(attendanceDateTime);
+    }
+}
