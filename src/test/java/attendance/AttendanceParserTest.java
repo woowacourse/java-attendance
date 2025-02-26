daaa75fb77a9f3f6f1e0b6c7ed12ee5549f4c0f8
songsunkook <songsunkook@gmail.com>
2025-02-26 16:56:00 +0900

    red: 여러 라인을 불러와 크루 정보로 변환한다

--- src/test/java/attendance/AttendanceParserTest.java
+++ src/test/java/attendance/AttendanceParserTest.java
@@ -4,11 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class AttendanceParserTest {
+class AttendanceParserTest {
 
     @Test
     @DisplayName("문자열을 파싱해 크루 정보로 변환한다")
@@ -25,5 +26,26 @@ public class AttendanceParserTest {
 
         // then
         assertThat(crewData).usingRecursiveAssertion().isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("여러 라인을 불러와 크루 정보로 변환한다")
+    void parseLinesTest() {
+        // given
+        String lines = """
+            쿠키,2024-02-03 10:08
+            쿠키,2024-02-03 10:07
+            쿠키,2024-02-03 10:06
+            쿠키,2024-02-03 10:05
+            """;
+
+        // when
+        List<Crew> crews = AttendanceParser.parseLines(lines);
+
+        // then
+        Crew actual = crews.get(0);
+        assertThat(actual.getName()).isEqualTo("쿠키");
+        assertThat(actual.getAttendanceTimeOf(LocalDate.of(2024, 2, 3))).isEqualTo(LocalTime.of(10, 8));
+        assertThat(actual.getAttendanceTimeOf(LocalDate.of(2024, 2, 4))).isEqualTo(LocalTime.of(10, 7));
     }
 }
