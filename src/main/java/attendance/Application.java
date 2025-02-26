2e4840478550c99517dd6d1b5465197ca48173c6
kjyyjk <rlawnsdud920@gmail.com>
2025-02-26 17:40:44 +0900

    feat: 입력에 따른 기능 실행 구현
    - 옵션을 key로 가지고, Runnable 기능을 value로 가지는
    map을 활용한다. 이 덕분에 option별 if 분기문을 작성하지 않아도
    옵션 별 기능을 실행할 수 있다.

--- src/main/java/attendance/Application.java
+++ src/main/java/attendance/Application.java
@@ -2,10 +2,41 @@ package attendance;
 
 import attendance.view.InputView;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
+    private static final Map<String, Runnable> options;
+
+    static {
+        options = new HashMap<>();
+        options.put("1", Application::doAttendance);
+        options.put("2", Application::modifyAttendance);
+        options.put("3", Application::checkAttendanceHistory);
+        options.put("4", Application::checkDangerousCrews);
+        options.put("Q", null);
+    }
+
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
         String option = InputView.readOption(today);
+        Runnable function = options.get(option);
+        function.run();
+    }
+
+    private static void doAttendance() {
+
+    }
+
+    private static void modifyAttendance() {
+
+    }
+
+    private static void checkAttendanceHistory() {
+
+    }
+
+    private static void checkDangerousCrews() {
+
     }
 }
