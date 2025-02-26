ff6e77c77c8ad7a72d79e8b4172cd4e6c5f72bb6
taek2222 <oot309@daum.net>
2025-02-26 21:52:24 +0900

    test(MenuTest): 여러 메뉴 파라미터 테스트 추가

--- src/test/java/attendance/view/MenuTest.java
+++ src/test/java/attendance/view/MenuTest.java
@@ -3,20 +3,24 @@ package attendance.view;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+@DisplayName("메뉴 테스트")
 class MenuTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource
     @DisplayName("입력과 일치한 메뉴를 반환한다")
-    void 입력과_일치한_메뉴를_반환한다() {
-        // given
-        String input = "1";
-
+    void shouldReturnMenuMatchingInput(String input, Menu excepted) {
         // when
         Menu result = Menu.find(input);
 
         // then
-        Assertions.assertThat(result).isEqualTo(Menu.CHECK);
+        Assertions.assertThat(result).isEqualTo(excepted);
     }
 
     @Test
@@ -29,5 +33,15 @@ class MenuTest {
         Assertions.assertThatIllegalArgumentException()
                 .isThrownBy(() -> Menu.find(input))
                 .withMessage("[ERROR] 잘못된 메뉴 선택의 입력입니다.");
+    }
+
+    private static Stream<Arguments> shouldReturnMenuMatchingInput() {
+        return Stream.of(
+                Arguments.of("1", Menu.CHECK),
+                Arguments.of("2", Menu.UPDATE),
+                Arguments.of("3", Menu.RECORD_SEARCH),
+                Arguments.of("4", Menu.RISK_CREW_SEARCH),
+                Arguments.of("Q", Menu.QUIT)
+        );
     }
 }
