863e15c0cfec4073b5d8a9badbfdc9cbc5f4afea
(minjae8563) <(minjae8563@naver.com)>
2025-02-26 15:24:12 +0900

    feat : 멤버 변수로 이름과 출석 기록을 가지고 있는 Student 객체 구현

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,83 @@
+package model;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Student {
+
+    String name;
+
+    List<LocalDateTime> timeRecords;
+
+    public Student(String name, List<LocalDateTime> timeRecords) {
+        this.name = name;
+        this.timeRecords = timeRecords;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<LocalDateTime> getTimeRecords() {
+        return (ArrayList<LocalDateTime>) timeRecords;
+    }
+
+    public void addTime(LocalDateTime localDateTime) {
+        timeRecords.add(localDateTime);
+        Collections.sort(timeRecords);
+    }
+
+    public LocalDateTime findSameDay(LocalDateTime wantToFindLocalDateTime) {
+        for (LocalDateTime localDateTime : timeRecords) {
+            LocalDateTime dayDate1 = localDateTime.truncatedTo(ChronoUnit.DAYS);
+            LocalDateTime dayDate2 = wantToFindLocalDateTime.truncatedTo(ChronoUnit.DAYS);
+            if (dayDate1.compareTo(dayDate2) == 0) {
+                return localDateTime;
+            }
+        }
+        return null;
+    }
+
+    public void modifyRecord(LocalDateTime wantToModifyLocalDateTime) {
+        timeRecords.remove(findSameDay(wantToModifyLocalDateTime));
+        addTime(wantToModifyLocalDateTime);
+        Collections.sort(timeRecords);
+    }
+
+    public boolean isExistSameDay(LocalDateTime localDateTime1, LocalDateTime localDateTime2) {
+        LocalDateTime dayDate1 = localDateTime1.truncatedTo(ChronoUnit.DAYS);
+        LocalDateTime dayDate2 = localDateTime2.truncatedTo(ChronoUnit.DAYS);
+
+        return (dayDate1.compareTo(dayDate2) == 0);
+    }
+
+    public void updateNoInformationInFile(LocalDateTime todayDate) {
+        LocalDateTime standard = LocalDateTime.of(2024, 12, 1, 0, 0);
+        while (!isExistSameDay(standard,todayDate)) {
+            addTimeRecordIfValid(standard);
+            standard = standard.plusDays(1);
+        }
+        Collections.sort(timeRecords);
+    }
+
+    private void addTimeRecordIfValid(LocalDateTime standard) {
+        if (!isWeekend(standard) && findSameDay(standard) == null) {
+            timeRecords.add(standard);
+        }
+    }
+
+    private boolean isWeekend(LocalDateTime localDateTime) {
+        return (localDateTime.getDayOfWeek().equals(DayOfWeek.SUNDAY) || localDateTime.getDayOfWeek().equals(DayOfWeek.SATURDAY));
+    }
+
+    public void isAlreadyAttendanceDate(TodayDate todayDate) {
+        if (timeRecords.contains(todayDate.getTodayDateTIme())) {
+            throw new IllegalArgumentException("[ERROR] 이미 출석한 요일입니다. 수정하고 싶으시면 수정 메뉴를 이용해 주세요.");
+        }
+    }
+
+}
