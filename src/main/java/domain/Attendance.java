e58fc5ebe008707b840232e8a2d60d6693ca7301
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-26 22:23:36 +0900

    refactor: Attendance 도메인 분리
    - 출석일, 출석 시간, 지각, 결석여부를 관리하는 Attendance 클래스 구현
    - day 기반 equals 오버라이딩

---
+++ src/main/java/domain/Attendance.java
@@ -0,0 +1,67 @@
+package domain;
+
+import java.time.LocalTime;
+import java.util.Objects;
+
+public class Attendance {
+    private static final Integer LATE_CRITERIA_MINUTES = 5;
+    private static final Integer ABSENT_CRITERIA_MINUTES = 30;
+    private static final LocalTime OPERATION_START_TIME = LocalTime.of(8, 0);
+    private static final LocalTime OPERATION_END_TIME = LocalTime.of(23, 0);
+    private final Day day;
+    private LocalTime time;
+    private Boolean isLate = false;
+    private Boolean isAbsent = false;
+
+    public Attendance(Day day, LocalTime time) {
+        validateTime(time);
+        this.day = day;
+        this.time = time;
+        updateStatus();
+    }
+
+    private void validateTime(LocalTime time) {
+        if (time.isBefore(OPERATION_START_TIME) || time.isAfter(OPERATION_END_TIME)) {
+            throw new IllegalStateException("[ERROR] 운영 시간이 아닙니다.");
+        }
+    }
+
+    private void updateStatus() {
+        LocalTime criteriaTime = day.getCriteriaTime();
+        if (criteriaTime.plusMinutes(ABSENT_CRITERIA_MINUTES).isBefore(time)) {
+            isAbsent = true;
+            return;
+        }
+        isLate = criteriaTime.plusMinutes(LATE_CRITERIA_MINUTES).isBefore(time);
+    }
+
+    public Boolean isLate() {
+        return isLate;
+    }
+
+    public Boolean isAbsent() {
+        return isAbsent;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public Boolean has(Day day) {
+        return this.day.equals(day);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Attendance that = (Attendance) o;
+        return Objects.equals(day, that.day);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(day);
+    }
+}
