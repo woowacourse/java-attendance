d29c1501293c08e5579e71d0b6368d448708c575
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-02-26 21:07:02 +0900

    test: 유효하지 않은 파일 입력 추가

--- src/test/java/domain/AttendanceReaderTest.java
+++ src/test/java/domain/AttendanceReaderTest.java
@@ -1,6 +1,9 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import except.AttendanceException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import strategy.AttendanceCurrentDateGenerateStrategy;
@@ -15,7 +18,19 @@ public class AttendanceReaderTest {
         CrewAttendances crewAttendances = new CrewAttendances(new AttendanceCurrentDateGenerateStrategy(),
                 attendanceReader.readAttendances());
         CrewAttendanceHistories crewAttendanceHistories = crewAttendances.crewAttendancesHistory("투다");
-        Assertions.assertThat(crewAttendanceHistories.crewAttendanceHistories().size())
+        assertThat(crewAttendanceHistories.crewAttendanceHistories().size())
                 .isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 파일로부터 출석 입력시 예외가 발생한다")
+    void invalidAddAttendanceFromFile() {
+        String fileName = "/invalidTestAttendance.csv";
+        AttendanceReader attendanceReader = new AttendanceReader(fileName);
+        CrewAttendances crewAttendances = new CrewAttendances(new AttendanceCurrentDateGenerateStrategy(),
+                attendanceReader.readAttendances());
+        CrewAttendanceHistories crewAttendanceHistories = crewAttendances.crewAttendancesHistory("투다");
+        assertThatThrownBy(crewAttendanceHistories::crewAttendanceHistories)
+                .isInstanceOf(AttendanceException.class);
     }
 }
