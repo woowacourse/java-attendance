ddb75064fa8d3d9fe4531f1ff56e02b78544dc8d
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-26 20:31:52 +0900

    🔴 red: 날짜로 부터 교육 시간을 구하는 테스트 작성

---
+++ src/main/java/attendance/model/EducationSchedule.java
@@ -0,0 +1,11 @@
+package attendance.model;
+
+import java.time.LocalDate;
+
+public enum EducationSchedule {
+    MONDAY;
+
+    public static EducationSchedule from(LocalDate date) {
+        return null;
+    }
+}
---
+++ src/test/java/model/EducationScheduleTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import attendance.model.EducationSchedule;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+public class EducationScheduleTest {
+
+    @Test
+    void 특정_날짜의_운영시간을_조회한다() {
+        // given
+        LocalDate date = LocalDate.of(2024, 12, 9);
+
+        // when
+        EducationSchedule educationSchedule = EducationSchedule.from(date);
+
+        // then
+        assertThat(educationSchedule).isEqualTo(EducationSchedule.MONDAY);
+    }
+}
