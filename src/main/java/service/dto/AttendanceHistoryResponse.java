ec937950d21675701f1a4e1540ebe850004756e2
minSsan <msp1125@hanyang.ac.kr>
2025-02-26 15:58:59 +0900

    chore(service): 미사용 dto 삭제

--- src/main/java/service/dto/AttendanceHistoryResponse.java
+++
@@ -1,8 +0,0 @@
-package service.dto;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Optional;
-
-public record AttendanceHistoryResponse(LocalDate date, Optional<LocalTime> time, String status) {
-}
