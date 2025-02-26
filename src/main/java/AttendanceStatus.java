d7914a316be8968ebf916273fbd31d0bb6202498
LJM\eueo8 <eueo8259@gmail.com>
2025-02-26 14:48:44 +0900

    refactor : 시간 확인 하는 중복되는 부분 수정

--- src/main/java/AttendanceStatus.java
+++ src/main/java/AttendanceStatus.java
@@ -1,37 +1,21 @@
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public enum AttendanceStatus {
 
-    ATTENDANCE, LATE, ABSENT;
+    ATTENDANCE,
+    LATE,
+    ABSENT;
 
     public static AttendanceStatus attendanceStatusCalculate(LocalDate todayDate ,String input) {
-        DayOfWeek todayDayOfWeek = todayDate.getDayOfWeek();
-        LocalTime localDate = LocalTime.parse(input);
-        AttendanceStatus absent = mondayAttendanceStatusCalculate(todayDayOfWeek, localDate);
-        if (absent != null) {
-            return absent;
-        }
-        if (localDate.isAfter(LocalTime.of(10, 30))){
+        LocalTime attendanceTime = LocalTime.parse(input);
+        LocalTime attendanceStartTime = WeeklyAttendanceSchedule.findAttendanceScheduleByLocalDate(todayDate);
+        if (attendanceTime.isAfter(attendanceStartTime.plusMinutes(30))){
             return ABSENT;
         }
-        if (localDate.isAfter(LocalTime.of(10, 5))){
+        if (attendanceTime.isAfter(attendanceStartTime.plusMinutes(5))){
             return LATE;
         }
         return ATTENDANCE;
-    }
-
-    private static AttendanceStatus mondayAttendanceStatusCalculate(DayOfWeek todayDayOfWeek, LocalTime localDate) {
-        if (todayDayOfWeek.equals(DayOfWeek.MONDAY)){
-            if (localDate.isAfter(LocalTime.of(13, 30))){
-                return ABSENT;
-            }
-            if (localDate.isAfter(LocalTime.of(13, 5))){
-                return LATE;
-            }
-            return ATTENDANCE;
-        }
-        return null;
     }
 }
