966d088f1b7d19762c4f16a19d650497d67b1f4c
Sung-june27 <kmymy27@gmail.com>
2025-02-26 21:44:32 +0900

    feat: 출석 수정 기능 구현

--- src/main/java/attendance/controller/util/DateTimeConverter.java
+++ src/main/java/attendance/controller/util/DateTimeConverter.java
@@ -1,16 +1,35 @@
 package attendance.controller.util;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class DateTimeConverter {
+    public static LocalDateTime convertToDateTime(String inputDay, String inputTime, LocalDate date) {
+        validateDay(inputDay);
+        int day =  Integer.parseInt(inputDay);
+        return LocalDateTime.of(date.withDayOfMonth(day), LocalTime.parse(inputTime));
+    }
+
     public static LocalTime convertToTime(String input) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         try {
             return LocalTime.parse(input, formatter);
         } catch (DateTimeParseException e) {
             throw new IllegalArgumentException("[ERROR] 시간을 올바른 형식으로 입력해주십시오.");
+        }
+    }
+
+    private static void validateDay(String inputDay) {
+        try {
+            int day =  Integer.parseInt(inputDay);
+            if (day < 1 || day > 12) {
+                throw new IllegalArgumentException("[ERROR] 올바른 날짜를 입력해주세요.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 올바른 날짜를 입력해주세요.");
         }
     }
 }
--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -27,4 +27,19 @@ public class InputView {
         System.out.println("등교 시간을 입력해 주세요.");
         return scanner.nextLine();
     }
+
+    public String readUpdateAttendanceNickname() {
+        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public String readUpdateAttendanceDay() {
+        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public String readUpdateAttendanceTime() {
+        System.out.println("언제로 변경하겠습니까?");
+        return scanner.nextLine();
+    }
 }
