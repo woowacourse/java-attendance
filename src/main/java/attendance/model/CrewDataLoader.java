863e9feb999b73e9f291e606796aeea4573fdfbc
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-27 15:36:58 +0900

    🛠️ refactor: 크루 데이터 로더 로직 리팩터링

--- src/main/java/attendance/model/CrewDataLoader.java
+++ src/main/java/attendance/model/CrewDataLoader.java
@@ -1,10 +1,16 @@
 package attendance.model;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class CrewDataLoader {
-
+    public static final String ROW_DELIMITER = ",";
+    public static final DateTimeFormatter CSV_DATE_TIME_FORMATER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    public static final int CREW_NAME_COLUMN_INDEX = 0;
+    public static final int ATTENDANCE_DATE_TIME_COLUMN_INDEX = 1;
     private final AttendanceRegister register;
 
     public CrewDataLoader(AttendanceRegister register) {
@@ -12,31 +18,25 @@ public class CrewDataLoader {
     }
 
     public void load(String path) {
-        register.attend(
-                "1",
-                new AttendanceDate(LocalDate.now()),
-                LocalTime.now()
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(path)))
         );
-        register.attend(
-                "2",
-                new AttendanceDate(LocalDate.now()),
-                LocalTime.now()
-        );
-        register.attend(
-                "3",
-                new AttendanceDate(LocalDate.now()),
-                LocalTime.now()
-        );
-        register.attend(
-                "4",
-                new AttendanceDate(LocalDate.now()),
-                LocalTime.now()
-        );
-        register.attend(
-                "5",
-                new AttendanceDate(LocalDate.now()),
-                LocalTime.now()
-        );
+        bufferedReader.lines().skip(1).forEach(row -> {
+            String[] parsed = parseRow(row);
+            String crewName = parsed[CREW_NAME_COLUMN_INDEX];
+            LocalDateTime dateTime = parseLocalDateTime(parsed[ATTENDANCE_DATE_TIME_COLUMN_INDEX]);
+            register.attend(crewName,
+                    new AttendanceDate(dateTime.toLocalDate()),
+                    dateTime.toLocalTime()
+            );
+        });
+    }
 
+    private String[] parseRow(String row) {
+        return row.split(ROW_DELIMITER);
+    }
+
+    private LocalDateTime parseLocalDateTime(String dateTime) {
+        return LocalDateTime.parse(dateTime, CSV_DATE_TIME_FORMATER);
     }
 }
