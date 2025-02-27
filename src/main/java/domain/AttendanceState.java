327e23947c83130409238f0ac0205e4092555eed
dltmddus <tmddus4671@gmail.com>
2025-02-27 14:51:23 +0900

    feat: 출석 시간에 따라 올바른 출석 상태를 반환하는 기능 추가

---
+++ src/main/java/domain/AttendanceState.java
@@ -0,0 +1,44 @@
+package domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public enum AttendanceState {
+    ATTENDANCE("출석"),
+    LATENESS("지각"),
+    ABSENCE("결석");
+
+    private String state;
+
+    AttendanceState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public static AttendanceState findStateBy(LocalDate localDate, LocalTime localTime) {
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+
+        if (dayOfWeek == DayOfWeek.MONDAY) {
+            if (localTime.isAfter(LocalTime.of(13, 30))) {
+                return ABSENCE;
+            }
+            if (localTime.isAfter(LocalTime.of(13, 5))) {
+                return LATENESS;
+            }
+        }
+        if (dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY) {
+            if (localTime.isAfter(LocalTime.of(10, 30))) {
+                return ABSENCE;
+            }
+            if (localTime.isAfter(LocalTime.of(10, 5))) {
+                return LATENESS;
+            }
+        }
+        return ATTENDANCE;
+    }
+
+}
--- src/test/java/domain/AttendanceTest.java
+++ src/test/java/domain/AttendanceTest.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class AttendanceTest {
@@ -58,6 +60,49 @@ public class AttendanceTest {
             repository.checkIn(name, localDate, firstTime);
             repository.checkIn(name, localDate, secondTime);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Nested
+    @DisplayName("출석_시간으로_알맞은_출석_상태를_반환한다")
+    class AttendanceStateTest {
+        @Test
+        void 출석했을_경우_출석_상태를_반환한다() {
+            // given
+            LocalDate localDate = LocalDate.of(2024, 12, 3);
+            LocalTime localTime = LocalTime.of(9, 55);
+
+            // when
+            AttendanceState state = AttendanceState.findStateBy(localDate, localTime);
+
+            // then
+            Assertions.assertThat(state.getState()).isEqualTo("출석");
+        }
+
+        @Test
+        void 지각했을_경우_지각_상태를_반환한다() {
+            // given
+            LocalDate localDate = LocalDate.of(2024, 12, 3);
+            LocalTime localTime = LocalTime.of(10, 6);
+
+            // when
+            AttendanceState state = AttendanceState.findStateBy(localDate, localTime);
+
+            // then
+            Assertions.assertThat(state.getState()).isEqualTo("지각");
+        }
+
+        @Test
+        void 결석했을_경우_결석_상태를_반환한다() {
+            // given
+            LocalDate localDate = LocalDate.of(2024, 12, 3);
+            LocalTime localTime = LocalTime.of(10, 31);
+
+            // when
+            AttendanceState state = AttendanceState.findStateBy(localDate, localTime);
+
+            // then
+            Assertions.assertThat(state.getState()).isEqualTo("결석");
+        }
 
     }
 }
