88987c711acdffe58d9d8b9cfaff6c869ecc3afd
CheChe903 <cheche903@naver.com>
2025-02-27 13:44:13 +0900

    test(AttendancesTest): 출석 추가 실패하는 테스트 구현

---
+++ src/test/java/attendance/domain/AttendancesTest.java
@@ -0,0 +1,29 @@
+package attendance.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AttendancesTest {
+
+
+    @DisplayName("출석을 추가한다.")
+    @Test
+    void 출석을_추가한다() {
+
+        // given
+        Attendance attendance1 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 27, 10, 0)));
+        Attendances attendances = new Attendances(List.of(attendance1));
+        int previousSize = 1;
+        Attendance attendance2 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 28, 10, 0)));
+
+        // when
+        attendances.add(attendance2);
+
+        // then
+        assertThat(attendances.contains(attendance2)).isTrue();
+    }
+}
