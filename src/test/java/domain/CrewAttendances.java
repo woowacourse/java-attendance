2f27c2f86797f3a4e87514c02e60b4e7c46dbca3
LeeJaeHoon <dlwogns3413@naver.com>
2025-02-27 22:44:47 +0900

    feat: 크루 출석 객체 생성 기능 구현 (green)

--- src/test/java/domain/CrewAttendances.java
+++ src/test/java/domain/CrewAttendances.java
@@ -9,7 +9,19 @@ public class CrewAttendances {
     private final List<Attendance> attendances;
 
     public CrewAttendances(String nickname, List<Attendance> attendances) {
+        validateAttendances(nickname, attendances);
         this.nickname = nickname;
         this.attendances = attendances;
+    }
+
+    private void validateAttendances(String nickname, List<Attendance> attendances) {
+        if (!isAllAttendancesMatchNickname(nickname, attendances)) {
+            throw new IllegalArgumentException(nickname + "의 출석만 이용하여 생성가능합니다.");
+        }
+    }
+
+    private boolean isAllAttendancesMatchNickname(String nickname, List<Attendance> attendances) {
+        return attendances.stream()
+                .allMatch(attendance -> attendance.isEqualNickname(nickname));
     }
 }
