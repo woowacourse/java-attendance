4a6f9f68fc1fbc5507ad18e7ae10cfaee67c0991
bowook <mbosx316@naver.com>
2025-02-27 16:30:20 +0900

    test(CampusOperationTest) : ParmeterizedTest로 수정

--- src/test/java/domain/CampusOperationTimeTest.java
+++ src/test/java/domain/CampusOperationTimeTest.java
@@ -3,16 +3,23 @@ package domain;
 import attendance.domain.CampusOperationTime;
 import java.time.LocalDateTime;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CampusOperationTimeTest {
 
-    @Test
-    void 캠퍼스_운영_중() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "2025, 2, 27, 7, 59, false",
+            "2025, 2, 27, 8, 0, true",
+            "2025, 2, 27, 22, 59, true",
+            "2025, 2, 27, 23, 0, false"
+    })
+    void 캠퍼스_운영_테스트(int year, int month, int day, int hour, int minute, boolean expectedResult) {
         //given
-        LocalDateTime localDateTime = LocalDateTime.of(2025, 2, 27, 9,59);
+        LocalDateTime localDateTime = LocalDateTime.of(year, month, day, hour,minute);
 
         //when & then
-        Assertions.assertThat(CampusOperationTime.isOperation(localDateTime.getHour())).isTrue();
+        Assertions.assertThat(CampusOperationTime.isOperation(localDateTime.getHour())).isEqualTo(expectedResult);
     }
 }
