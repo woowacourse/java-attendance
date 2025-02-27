af6f088c13c40fd2342aa2286d0e4af47f2fe510
jhpark1227 <junhyeok0246@naver.com>
2025-02-27 11:43:31 +0900

    feat: 출석시간을 생성할 때 휴일인 경우 예외를 발생시키는 기능 구현

--- src/main/java/domain/AttendanceTime.java
+++ src/main/java/domain/AttendanceTime.java
@@ -7,6 +7,7 @@ public class AttendanceTime {
     private final LocalDateTime time;
 
     public AttendanceTime(LocalDateTime time) {
+        validateHoliday(time);
         validateOperationTime(time);
         this.time = time;
     }
@@ -18,6 +19,12 @@ public class AttendanceTime {
     private void validateOperationTime(LocalDateTime time) {
         if (!OperationSchedule.isInOperationTime(time)) {
             throw new IllegalArgumentException("운영시간이 아니면 출석할 수 없습니다.");
+        }
+    }
+
+    private void validateHoliday(LocalDateTime time) {
+        if (Holiday.isHoliday(time.toLocalDate())) {
+            throw new IllegalArgumentException("주말과 공휴일에는 출석할 수 없습니다.");
         }
     }
 }
