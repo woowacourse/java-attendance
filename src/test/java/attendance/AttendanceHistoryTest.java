e2a7c148d24403e8bc171951face0d47cd6ef108
kjyyjk <rlawnsdud920@gmail.com>
2025-02-27 11:54:39 +0900

    test(red): AttendanceHistory 출석 수정 테스트
    - AttendanceHistory는 기존 출석을 인자로 받은 수정된 출석으로 변경할
    수 있다.

--- src/test/java/attendance/AttendanceHistoryTest.java
+++ src/test/java/attendance/AttendanceHistoryTest.java
@@ -49,4 +49,32 @@ class AttendanceHistoryTest {
             assertThat(result).isEmpty();
         }
     }
+
+    @DisplayName("출석을_수정하고_새로_추가된_출석을_반환할_수_있다")
+    @Test
+    void 출석을_수정하고_새로_추가된_출석을_반환할_수_있다() {
+        //given
+        AttendanceHistory attendanceHistory = new AttendanceHistory();
+        LocalDate date = LocalDate.of(2024, 12, 26);
+        LocalTime time = LocalTime.of(10, 7);
+        attendanceHistory.addAttendance(new Attendance(date, time, "LATE"));
+
+        LocalTime modificationTime = LocalTime.of(10, 0);
+        String modificationStatus = "ATTENDANCE";
+        Attendance modifiedAttendance = new Attendance(date, modificationTime, modificationStatus);
+
+        //when
+        Attendance result = attendanceHistory.modifyAttendance(modifiedAttendance);
+
+        //then
+        assertAll(
+            () -> assertThat(result.isDateEquals(date)).isTrue(),
+            () -> assertThat(result.getTime()).isEqualTo(modificationTime),
+            () -> assertThat(result.getStatus()).isEqualTo(modificationStatus),
+            () -> {
+                Optional<Attendance> foundedAttendance = attendanceHistory.findAttendance(date);
+                assertThat(result).isEqualTo(foundedAttendance.get());
+            }
+        );
+    }
 }
