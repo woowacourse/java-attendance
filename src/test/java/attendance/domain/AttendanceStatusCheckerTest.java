6ae5cff33c1505fda8ddb47a62c48e31450367af
kimdev <kim__dev@hufs.ac.kr>
2025-02-27 15:25:51 +0900

    test: 출석 시간을 통해 출석 상태를 알려주는 테스트 케이스에 월요일 케이스 추가

--- src/test/java/attendance/domain/AttendanceStatusCheckerTest.java
+++ src/test/java/attendance/domain/AttendanceStatusCheckerTest.java
@@ -5,25 +5,31 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.Year;
 
+import static attendance.domain.AttendanceStatusChecker.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class AttendanceStatusCheckerTest {
 
     @CsvSource({
-            "10, 0, AttendanceStatus.ATTENDANCE",
-            "10, 5, AttendanceStatus.ATTENDANCE",
-            "10, 6, AttendanceStatus.LATE",
-            "10, 30, AttendanceStatus.LATE",
-            "10, 31, AttendanceStatus.ABSENT",
+            "24, 13, 0, ATTENDANCE",
+            "24, 13, 5, ATTENDANCE",
+            "24, 13, 6, LATE",
+            "24, 13, 30, LATE",
+            "24, 13, 31, ABSENT",
+            "25, 10, 0, ATTENDANCE",
+            "25, 10, 5, ATTENDANCE",
+            "25, 10, 6, LATE",
+            "25, 10, 30, LATE",
+            "25, 10, 31, ABSENT",
     })
     @ParameterizedTest
-    void 출석_시간을_주면_출석_상태를_알려준다(int hour, int minute, AttendanceStatus expected) {
+    void 출석_시간을_주면_출석_상태를_알려준다(int day, int hour, int minute, AttendanceStatus expected) {
         // Given
         AttendanceDateTime attendanceDateTime = new AttendanceDateTime(
-                Year.of(2025).atMonth(2).atDay(27).atTime(hour, minute));
+                Year.of(2025).atMonth(2).atDay(day).atTime(hour, minute));
 
         // When
-        AttendanceStatus attendanceStatus = AttendanceStatusChecker.checkStatus(attendanceDateTime);
+        AttendanceStatus attendanceStatus = checkStatus(attendanceDateTime);
 
         // Then
         assertThat(attendanceStatus).isEqualTo(expected);
