99a4c5c2e9b644c88d78da6efc88d06ef81b73a3
(minjae8563) <(minjae8563@naver.com)>
2025-02-27 22:35:03 +0900

    chore : 불필요한 import 삭제

--- src/test/java/model/StudentAttendanceHistoryTest.java
+++ src/test/java/model/StudentAttendanceHistoryTest.java
@@ -2,7 +2,6 @@ package model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
