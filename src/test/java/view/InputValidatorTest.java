b45fda59e3638f3fcc55f0ffc2e552812aceca5d
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-27 16:53:57 +0900

    test(view): 시간 입력 예외 테스트 코드 작성

---
+++ src/test/java/view/InputValidatorTest.java
@@ -0,0 +1,19 @@
+package view;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class InputValidatorTest {
+
+    @DisplayName("시간 형식이 올바르지 않을 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"9:59", "", " ", "a", "1", "10:3", "10;10"})
+    void test1(String invalidTimeFormat) {
+        assertThatThrownBy(Inputvalidator.validateTime(invalidTimeFormat))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
