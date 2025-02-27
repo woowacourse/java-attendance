9d2175885f8e8c5be368b0da5819b19e97abeab2
taek2222 <oot309@daum.net>
2025-02-27 11:56:09 +0900

    feat(Holidays): 여러 휴일을 가지는 객체 필드 추가

--- src/main/java/attendance/domain/Holiday.java
+++ src/main/java/attendance/domain/Holiday.java
@@ -1,24 +1,12 @@
 package attendance.domain;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.format.TextStyle;
-import java.util.Locale;
 
 public class Holiday {
 
-    public static void validateAttendanceDate(final LocalDate attendanceDate) {
-        DayOfWeek dayOfWeek = attendanceDate.getDayOfWeek();
-        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
-            throw new IllegalArgumentException(formatErrorMessage(attendanceDate));
-        }
-    }
+    private final LocalDate holiday;
 
-    private static String formatErrorMessage(final LocalDate date) {
-        return String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.",
-                date.getMonthValue(),
-                date.getDayOfMonth(),
-                date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA)
-        );
+    public Holiday(final LocalDate date) {
+        this.holiday = date;
     }
 }
---
+++ src/main/java/attendance/domain/Holidays.java
@@ -0,0 +1,35 @@
+package attendance.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.List;
+import java.util.Locale;
+
+public class Holidays {
+
+    private final List<Holiday> holidays;
+
+    public Holidays(final List<Holiday> holidays) {
+        this.holidays = holidays;
+    }
+
+    public static void validateAttendanceDate(final LocalDate attendanceDate) {
+        DayOfWeek dayOfWeek = attendanceDate.getDayOfWeek();
+        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+            throw new IllegalArgumentException(formatErrorMessage(attendanceDate));
+        }
+    }
+
+    public List<Holiday> getHolidays() {
+        return holidays;
+    }
+
+    private static String formatErrorMessage(final LocalDate date) {
+        return String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.",
+                date.getMonthValue(),
+                date.getDayOfMonth(),
+                date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA)
+        );
+    }
+}
