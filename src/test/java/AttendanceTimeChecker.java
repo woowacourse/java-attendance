658b7bf3dbee840c49999ff7145525eed15de21b
GiSeok Kang <ime.rokz@gmail.com>
2025-02-27 15:01:59 +0900

    refactor: 출석,지각,결석을 결정하는 로직 순서 변경

--- src/test/java/AttendanceTimeChecker.java
+++ src/test/java/AttendanceTimeChecker.java
@@ -3,13 +3,14 @@ public class AttendanceTimeChecker {
         String[] split = time.split(":");
         int minute = Integer.parseInt(split[1]);
 
-        if (minute < 5) {
-            return AttendPolicy.ATTEND;
-        }
-
         if (minute > 30) {
             return AttendPolicy.ABSENT;
         }
-        return AttendPolicy.LATE;
+
+        if (minute > 5) {
+            return AttendPolicy.LATE;
+        }
+
+        return AttendPolicy.ATTEND;
     }
 }
