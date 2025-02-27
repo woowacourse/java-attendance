af529dcb842636d3cb78aa155165e2620b526908
julie <7jujubuilds@gmail.com>
2025-02-27 14:41:08 +0900

    test(records): 출석 기록 확인 관련 테스트 작성
    테스트 내용:
    - 주어진 날짜에 출석 기록이 존재하는지 여부를 반환한다

--- src/test/java/AttendanceRecordsTest.java
+++ src/test/java/AttendanceRecordsTest.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class AttendanceRecordsTest {
@@ -16,5 +18,20 @@ class AttendanceRecordsTest {
 
         // then
         assertThatNoException().isThrownBy(() -> attendanceRecords.add(attendanceRecord));
+    }
+
+    @DisplayName("주어진 날짜에 출석 기록이 존재하는지 여부를 반환한다.")
+    @Test
+    void hasRecordOnDateTest() {
+        // given
+        AttendanceRecords attendanceRecords = new AttendanceRecords();
+        LocalDateTime dateTime = LocalDateTime.of(2024, 12, 2, 13, 10);
+        AttendanceRecord attendanceRecord = new AttendanceRecord(dateTime);
+
+        // when
+        attendanceRecords.add(attendanceRecord);
+
+        // then
+        assertThat(attendanceRecords.hasRecordOnDate(LocalDate.of(2024, 12, 2))).isTrue();
     }
 }
