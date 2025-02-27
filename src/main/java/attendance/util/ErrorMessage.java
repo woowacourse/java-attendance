1d7bf295b323ad6b8ecea6c168d37ca2b13b747a
ljhee92 <weilainib7@gmail.com>
2025-02-27 16:11:26 +0900

    feat: 파일 내용 읽기 구현

--- src/main/java/attendance/util/ErrorMessage.java
+++ src/main/java/attendance/util/ErrorMessage.java
@@ -3,7 +3,8 @@ package attendance.util;
 public enum ErrorMessage {
 
     NICKNAME_MISSING_ERROR("크루의 닉네임은 반드시 존재해야 합니다."),
-    CREW_DUPLICATE_ERROR("같은 크루를 중복하여 추가할 수 없습니다.")
+    CREW_DUPLICATE_ERROR("같은 크루를 중복하여 추가할 수 없습니다."),
+    FILE_READ_ERROR("파일 읽기 오류가 발생했습니다.")
     ;
 
     private static final String PREFIX = "[ERROR] ";
@@ -15,6 +16,6 @@ public enum ErrorMessage {
     }
 
     public String getMessage() {
-        return message;
+        return PREFIX + message;
     }
 }
