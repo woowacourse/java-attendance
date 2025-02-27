bad54f7dd48db488d99be6b8ea9e63ee2187c26e
jhpark1227 <junhyeok0246@naver.com>
2025-02-27 13:56:28 +0900

    test: Attendance 단위테스트 작성 (특정 크루 및 날짜가 출석기록과 일치하는지 비교한다)

--- src/test/java/AttendanceTest.java
+++ src/test/java/AttendanceTest.java
@@ -3,6 +3,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import domain.Attendance;
 import domain.AttendanceTime;
 import domain.Crew;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -25,6 +26,25 @@ public class AttendanceTest {
         AttendanceTime time2 = new AttendanceTime(LocalDateTime.of(2024, 12, date, 9, 30));
         //when
         boolean actual = attendance.isSameCrewAndTime(new Attendance(crew2, time2));
+        //then
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "쿠키, 2, true",
+            "쿠키, 3, false",
+            "빙봉, 2, false",
+            "빙봉, 3, false"
+    })
+    void 특정_크루및_날짜가_출석기록과_일치하는지_비교한다(String nickname, int date, boolean expected) {
+        Crew crew1 = new Crew("쿠키");
+        AttendanceTime time1 = new AttendanceTime(LocalDateTime.of(2024, 12, 2, 9, 30));
+        Attendance attendance = new Attendance(crew1, time1);
+        Crew crew2 = new Crew(nickname);
+        LocalDate day = LocalDate.of(2024, 12, date);
+        //when
+        boolean actual = attendance.compareByCrewAndTime(crew2, day);
         //then
         assertThat(expected).isEqualTo(actual);
     }
