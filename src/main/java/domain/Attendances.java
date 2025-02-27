f1b5b8705559f6461ff2e139ee635ab3f1081ff8
jhpark1227 <junhyeok0246@naver.com>
2025-02-27 11:05:59 +0900

    feat: 출석기록을 추가할 때 오늘의 출석이 존재하면 예외를 발생시키는 기능 구현

--- src/main/java/domain/Attendances.java
+++ src/main/java/domain/Attendances.java
@@ -12,7 +12,14 @@ public class Attendances {
     }
 
     public void add(Attendance attendance) {
+        validateExistAttendance(attendance);
         attendances.add(attendance);
+    }
+
+    private void validateExistAttendance(Attendance newAttendance) {
+        if (attendances.stream().anyMatch(attendance -> attendance.isSameCrewAndTime(newAttendance))) {
+            throw new IllegalArgumentException("출석이 이미 존재합니다.");
+        }
     }
 
     @Override
