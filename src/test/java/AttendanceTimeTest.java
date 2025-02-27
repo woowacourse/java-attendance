62aefbb5c43aa411acf6b8df7d1b602dc52050c2
jhpark1227 <junhyeok0246@naver.com>
2025-02-27 10:42:47 +0900

    test: AttendanceTime 단위테스트 작성 (다른 출석시간과 같은 날인지 비교한다)

--- src/test/java/AttendanceTimeTest.java
+++ src/test/java/AttendanceTimeTest.java
@@ -1,6 +1,8 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import domain.AttendanceTime;
 import java.time.LocalDateTime;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,8 +16,25 @@ public class AttendanceTimeTest {
         //given
         LocalDateTime checkInTime = LocalDateTime.of(2024, 12, 2, hour, minute);
         //when & then
-        Assertions.assertThatThrownBy(() -> new AttendanceTime(checkInTime))
+        assertThatThrownBy(() -> new AttendanceTime(checkInTime))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("운영시간이 아니면 출석할 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, 9, 30, 2, 10, 30, true",
+            "2, 9, 30, 3, 10, 30, false"
+    })
+    void 다른_출석시간과_같은_날인지_비교한다(int date1, int hour1, int minute1, int date2, int hour2, int minute2, boolean expected) {
+        //given
+        LocalDateTime time1 = LocalDateTime.of(2024, 12, date1, hour1, minute1);
+        LocalDateTime time2 = LocalDateTime.of(2024, 12, date2, hour2, minute2);
+        AttendanceTime attendanceTime1 = new AttendanceTime(time1);
+        AttendanceTime attendanceTime2 = new AttendanceTime(time2);
+        //when
+        boolean isSameDay = attendanceTime1.isSameDay(attendanceTime2);
+        //then
+        assertThat(expected).isEqualTo(isSameDay);
     }
 }
