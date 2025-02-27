638e3bcd81873f0c8c7ea28e254d8a678350fa9c
eoehd1ek <eoehd2ek@gmail.com>
2025-02-27 17:42:44 +0900

    remove: 출석 다시 구현하기 위해 구현한 코드 삭제

--- src/main/java/domain/AttendCount.java
+++
@@ -1,7 +0,0 @@
-package domain;
-
-public record AttendCount(long attend, long late, long absence) {
-    public long calculateRank() {
-        return absence * WarningStatus.LATE_FER_ABSENCE + late;
-    }
-}
--- src/test/java/AttendCountTest.java
+++
@@ -1,8 +0,0 @@
-import domain.AttendCount;
-import domain.WarningStatus;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-public class AttendCountTest {
-}
