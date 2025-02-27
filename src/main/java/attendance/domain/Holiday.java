c3a782c1285f1de00727b5e7340aa7de05ecf4f7
taek2222 <oot309@daum.net>
2025-02-27 10:56:29 +0900

    refactor(Holiday): 등교 일자에 알맞는 예외 메시지 출력 개선

--- src/main/java/attendance/domain/Holiday.java
+++ src/main/java/attendance/domain/Holiday.java
@@ -1,10 +1,20 @@
 package attendance.domain;
 
 import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class Holiday {
 
     public static void validateAttendanceDate(final LocalDate attendanceDate) {
-        throw new IllegalArgumentException("[ERROR] 12월 14일 토요일은 등교일이 아닙니다.");
+        throw new IllegalArgumentException(formatErrorMessage(attendanceDate));
+    }
+
+    private static String formatErrorMessage(final LocalDate date) {
+        return String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.",
+                date.getMonthValue(),
+                date.getDayOfMonth(),
+                date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA)
+        );
     }
 }
