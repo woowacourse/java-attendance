ebd306c9ed19fa531a1ee84e15b6dc1e363b4ed5
JO-eusan <goodsan0330@gmail.com>
2025-02-27 17:18:11 +0900

    feat: 컨트롤러에서 출석부 파일 읽기

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import static util.loader.FileLoader.loadCSV;
+
+import domain.AttendanceBook;
 import view.InputView;
 import view.OutputView;
 
@@ -7,10 +10,14 @@ public class AttendanceController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final AttendanceBook attendanceBook;
 
     public AttendanceController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+
+        this.attendanceBook = new AttendanceBook();
+        attendanceBook.initializeCrewRecords(loadCSV("src/main/resources/attendances.csv"));
     }
 
     public void selectFunction() {
