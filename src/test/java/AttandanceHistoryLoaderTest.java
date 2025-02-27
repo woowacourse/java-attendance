376cf5d6162821db6c3f30d2cefc05a3bff9f135
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-27 16:42:50 +0900

    test: 파일 형식이 잘못되어 출석부 초기화에 예외가 발생하는 테스트 작성

--- src/test/java/AttandanceHistoryLoaderTest.java
+++ src/test/java/AttandanceHistoryLoaderTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import domain.AttendanceBook;
 import java.io.File;
@@ -6,6 +7,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AttandanceHistoryLoaderTest {
     private File createTempFile(String csvData) throws IOException {
@@ -18,6 +21,7 @@ class AttandanceHistoryLoaderTest {
     @Test
     void CSV_파일을_읽어_출석부를_초기화한다() throws IOException {
         String csvData = """
+                nickname,datetime
                 에드, 2025-02-26 10:03
                 제프, 2025-02-26 10:01
                 """;
@@ -27,8 +31,32 @@ class AttandanceHistoryLoaderTest {
 
         assertThat(attendanceBook.getAttendances("에드")).isNotInstanceOf(Exception.class);
         assertThat(attendanceBook.getAttendances("제프")).isNotInstanceOf(Exception.class);
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = {
+            """
+                    nickname,datetime
+                    에드, 2025-02-26-10:03
+                    제프, 2025-02-26 10:01
+                    """,
+            """
+                    nickname,datetime
+                    에드, 2025-02-26
+                    제프, 2025-02-26 10:01
+                    """,
+            """               
+                    nickname,datetime
+                    에드, 2025-02-26 10:03
+                    제프, 10:01 025-02-26
+                    """})
+    void CSV_파일의_형식이_잘못되면_예외를_발생시킨다(String csvData) throws IOException {
+        File file = createTempFile(csvData);
+        AttandanceHistoryLoader loader = new AttandanceHistoryLoader();
 
+        assertThatThrownBy(() -> loader.initializeAttendanceWith(file))
+                .isInstanceOf(IOException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
 }
