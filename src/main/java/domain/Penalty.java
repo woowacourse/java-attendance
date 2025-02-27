05ff9f861b9faad603ee3f41c4aac5e200256933
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-27 11:03:08 +0900

    feat(domain): 제적 확인 기능 구현

--- src/main/java/domain/Penalty.java
+++ src/main/java/domain/Penalty.java
@@ -1,5 +1,7 @@
 package domain;
 
+import dto.AttendanceCount;
+
 public enum Penalty {
     EXPULSION("제적"),
     COUNSELING("면담"),
@@ -9,5 +11,12 @@ public enum Penalty {
 
     Penalty(String expression) {
         this.expression = expression;
+    }
+
+    public static Penalty from(AttendanceCount attendanceCount) {
+        if(attendanceCount.absentCount() >= 6) {
+            return Penalty.EXPULSION;
+        }
+        return null;
     }
 }
