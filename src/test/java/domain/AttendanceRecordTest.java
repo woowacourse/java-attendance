cf7a607f17ab2e5443f5a2f61f156f028ec1ddfe
changuii <rhljh7410@gmail.com>
2025-02-27 15:17:04 +0900

    red: record가 비어있다면, ABSENCE를 반환한다.

--- src/test/java/domain/AttendanceRecordTest.java
+++ src/test/java/domain/AttendanceRecordTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -38,6 +39,19 @@ public class AttendanceRecordTest {
                     Arguments.of(LocalDateTime.of(2024, 12, 3, 10, 30), AttendanceStatus.LATE),
                     Arguments.of(LocalDateTime.of(2024, 12, 3, 10, 31), AttendanceStatus.ABSENCE)
             );
+        }
+
+        @DisplayName("record가 비어있다면, ABSENCE를 반환한다.")
+        @Test
+        public void calculateAttendanceStatusByEmpty() throws Exception {
+            // given
+            final AttendanceRecord empty = AttendanceRecord.empty(LocalDate.of(2024, 12, 2));
+
+            // when
+            final AttendanceStatus actual = empty.calculateAttendanceStatus();
+
+            // then
+            assertThat(actual).isEqualByComparingTo(AttendanceStatus.ABSENCE);
         }
 
     }
