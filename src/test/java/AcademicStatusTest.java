9059acd7de0877a6a4729c9d22ac2f99ea0a4a6d
rladmstn <oz33177351@gmail.com>
2025-02-27 19:58:19 +0900

    test: 학적 상태 판단 메서드 작성

---
+++ src/test/java/AcademicStatusTest.java
@@ -0,0 +1,35 @@
+import attendance.model.AcademicStatus;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class AcademicStatusTest {
+
+    @ParameterizedTest
+    @MethodSource("lateAndAbsentAndResult")
+    void 입력_받은_지각_및_결석_일수로_크루의_학적_상태를_판단한다(final int late, final int absent, final AcademicStatus expectedResult) {
+
+        // given
+
+        // when
+        final AcademicStatus result = AcademicStatus.getAcademicStatus(late, absent);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
+
+
+    public static Stream<Arguments> lateAndAbsentAndResult() {
+
+        return Stream.of(
+                Arguments.of(0, 6, AcademicStatus.EXPELLED),
+                Arguments.of(0, 3, AcademicStatus.WARN),
+                Arguments.of(0, 2, AcademicStatus.INTERVIEW),
+                Arguments.of(0, 1, AcademicStatus.NOT),
+                Arguments.of(3, 5, AcademicStatus.EXPELLED)
+        );
+    }
+
+}
