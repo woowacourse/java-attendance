4d905f6dc16b5b6b1bfecd1cb35ffe6604ce07b2
kimdev <kim__dev@hufs.ac.kr>
2025-02-27 18:11:27 +0900

    test: 크루의 등록 여부, 오늘 출석 여부를 검증하는 테스트 작성

--- src/test/java/attendance/domain/AttendanceBookTest.java
+++ src/test/java/attendance/domain/AttendanceBookTest.java
@@ -1,13 +1,28 @@
 package attendance.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.time.Year;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class AttendanceBookTest {
+
+    private AttendanceBook attendanceBook;
+
+    @BeforeEach
+    void 출석부_초기화() {
+        Crew cookie = new Crew("쿠키");
+        List<AttendanceDateTime> cookieAttendanceDateTimes = List.of(
+                new AttendanceDateTime(Year.of(2025).atMonth(2).atDay(25).atTime(10, 4)));
+        Map<Crew, List<AttendanceDateTime>> crewAttendances = Map.of(cookie, cookieAttendanceDateTimes);
+        this.attendanceBook = new AttendanceBook(crewAttendances);
+    }
 
     @Test
     void 크루들의_이전_출석_기록들을_바탕으로_출석부를_생성한다() {
@@ -22,7 +37,41 @@ public class AttendanceBookTest {
         Map<Crew, List<AttendanceDateTime>> crewAttendances = Map.of(cookie, cookieAttendanceDateTimes, bingbong, bingbongAttendanceDateTimes);
 
         // When & Then
-        Assertions.assertThatCode(() -> new AttendanceBook(crewAttendances))
+        assertThatCode(() -> new AttendanceBook(crewAttendances))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 등록된_크루인지_검증한다() {
+        // Given
+        Crew cookie = new Crew("쿠키");
+
+        // When & Then
+        assertThatCode(() -> attendanceBook.validateRegisteredCrew(cookie))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 등록된_크루가_아닌_경우를_찾아낸다() {
+        // Given
+        Crew unregisteredCrew = new Crew("쿠키2");
+
+        // When & Then
+        assertThatThrownBy(() -> attendanceBook.validateRegisteredCrew(unregisteredCrew))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("등록되지 않은 닉네임입니다.");
+    }
+
+    @Test
+    void 이미_출석된_날짜인지_검사한다() {
+        // Given
+        Crew cookie = new Crew("쿠키");
+        LocalDateTime localDateTime = Year.of(2025).atMonth(2).atDay(25).atTime(10, 0);
+        AttendanceDateTime attendanceDateTime = new AttendanceDateTime(localDateTime);
+
+        // When & Then
+        assertThatThrownBy(() -> attendanceBook.validateDuplicateAttendanceDate(cookie, attendanceDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("오늘은 이미 출석하셨습니다. 출석 수정 기능을 이용해 주세요.");
     }
 }
