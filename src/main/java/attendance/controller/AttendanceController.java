4ff715b8ad384c19dfe3aac378f3e5038c6ecce9
songsunkook <songsunkook@gmail.com>
2025-02-27 13:44:33 +0900

    feat: AttendanceController 작성

---
+++ src/main/java/attendance/controller/AttendanceController.java
@@ -0,0 +1,75 @@
+package attendance.controller;
+
+import java.time.LocalDate;
+import java.util.function.BooleanSupplier;
+
+import attendance.view.InputView;
+import attendance.view.OutputView;
+
+public class AttendanceController {
+
+    private final AttendanceService service = new AttendanceService();
+
+    public void run() {
+        while (process(this::menu)) {
+        }
+    }
+
+    private boolean menu() {
+        switch (InputView.menu(LocalDate.now())) {
+            case "1" -> {
+                process(this::attendance);
+            }
+            case "2" -> {
+                process(this::modifyAttendance);
+            }
+            case "3" -> {
+                process(this::history);
+            }
+            case "4" -> {
+                process(this::riskCrews);
+            }
+            case "q", "Q" -> {
+                return false;
+            }
+            default -> {
+                throw new IllegalArgumentException("잘못된 메뉴 입력입니다.");
+            }
+        }
+        return true;
+    }
+
+    private void attendance() {
+
+    }
+
+    private void modifyAttendance() {
+
+    }
+
+    private void history() {
+
+    }
+
+    private void riskCrews() {
+
+    }
+
+    private void process(Runnable action) {
+        try {
+            action.run();
+        } catch (IllegalArgumentException e) {
+            OutputView.exception(e);
+            process(action);
+        }
+    }
+
+    private boolean process(BooleanSupplier action) {
+        try {
+            return action.getAsBoolean();
+        } catch (IllegalArgumentException e) {
+            OutputView.exception(e);
+            process(action);
+        }
+    }
+}
--- src/main/java/attendance/view/OutputView.java
+++ src/main/java/attendance/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public static void dd() {
 
     }
+
+    public static void exception(Exception e) {
+        System.out.println(e.getMessage() + " 다시 입력하세요.");
+    }
 }
