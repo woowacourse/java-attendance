45bfa2d14615fe8579f4bff614d7eef42b2ce64a
rladmstn <oz33177351@gmail.com>
2025-02-27 17:24:04 +0900

    test: 입력 받은 시간이 캠퍼스 운영 시간이 아닐 경우 테스트 작성

--- src/test/java/AttendanceTimeTest.java
+++ src/test/java/AttendanceTimeTest.java
@@ -2,6 +2,8 @@ import attendance.model.AttendanceTime;
 import java.time.LocalDate;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class AttendanceTimeTest {
 
@@ -16,5 +18,22 @@ public class AttendanceTimeTest {
         Assertions.assertThatThrownBy(() -> new AttendanceTime(localdate, hour, minute))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 등교 날짜가 아닙니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "7,59",
+            "23,1"
+    })
+    void 입력_받은_시간이_캠퍼스_운영_시간이_아니면_예외를_발생한다(final int hour, final int minute) {
+
+        // given
+        final LocalDate localdate = LocalDate.of(2025, 2, 27);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new AttendanceTime(localdate, hour, minute))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 캠퍼스 운영 시간이 아닙니다.");
+
     }
 }
