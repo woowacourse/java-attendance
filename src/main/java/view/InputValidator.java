65b02791b6d610e52b3919c0ead1e367c3a29b43
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-27 17:07:41 +0900

    refactor(view): 시간 입력 예외 리팩토링
    - 상수 분리

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -5,12 +5,15 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class InputValidator {
+    private static final String TIME_FORMAT_ERROR = "[ERROR] 시간 형식(HH:mm)이 올바르지 않습니다.";
+    private static final String VALID_TIME_FORMAT = "HH:mm";
+
     public static void validateTime(String time) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(VALID_TIME_FORMAT);
         try {
             LocalTime.parse(time, dateTimeFormatter);
         } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("[ERROR] 시간 형식(HH:mm)이 올바르지 않습니다.");
+            throw new IllegalArgumentException(TIME_FORMAT_ERROR);
         }
     }
 }
