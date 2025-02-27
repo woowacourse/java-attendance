68b0d6a24c3055c638d88eccef430b5b7a295f66
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-27 17:51:21 +0900

    test(util): 파일 읽기 테스트 코드 작성

---
+++ src/test/java/util/FileReaderTest.java
@@ -0,0 +1,21 @@
+package util;
+
+import static org.assertj.core.api.Assertions.*;
+
+import domain.CrewName;
+import dto.InitialInformation;
+import java.io.FileReader;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FileReaderTest {
+    @DisplayName("닉네임과 출석 일시가 기록된 파일을 조회한다.")
+    @Test
+    void test1() {
+        InitialInformation initialInformation = FileReader.readAttendanceInfo();
+        Set<CrewName> crewNames = initialInformation.value().keySet();
+
+        assertThat(crewNames).hasSize(5);
+    }
+}
