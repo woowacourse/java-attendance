7bf23fed6375ea88593625633bf41496fb908f34
CheChe903 <cheche903@naver.com>
2025-02-27 09:38:30 +0900

    feat(AttendanceStatus): 출석 상태 도메인 작성

---
+++ src/main/java/attendance/domain/AttendanceStatus.java
@@ -0,0 +1,45 @@
+package attendance.domain;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
+public enum AttendanceStatus {
+    ATTEND("출석", (arrivalTime, startTime) -> {
+        Duration duration = Duration.between(arrivalTime, startTime);
+        return duration.isZero() || duration.isNegative() && duration.abs().toMinutes() <= 5;
+    }),
+    LATE("지각", (arrivalTime, startTime) -> {
+        Duration duration = Duration.between(arrivalTime, startTime);
+        return duration.isNegative() && duration.abs().toMinutes() > 5 && duration.abs().toMinutes() <= 30;
+    }),
+    ABSENT("결석", (arrivalTime, startTime) -> {
+        Duration duration = Duration.between(arrivalTime, startTime);
+        return duration.isNegative() && duration.abs().toMinutes() > 30;
+    });
+
+    private final String status;
+    private final BiPredicate<LocalTime, LocalTime> timeCondition;
+
+    AttendanceStatus(String status, BiPredicate<LocalTime, LocalTime> timeCondition) {
+        this.status = status;
+        this.timeCondition = timeCondition;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public static AttendanceStatus getStatusByTime(LocalTime arrivalTime, LocalTime startTime) {
+
+        return Arrays.stream(AttendanceStatus.values())
+                .filter(attendanceStatus -> attendanceStatus.matches(arrivalTime, startTime))
+                .findFirst()
+                .orElse(ABSENT);
+    }
+
+    private boolean matches(LocalTime arrivalTime, LocalTime startTime) {
+        return timeCondition.test(arrivalTime, startTime);
+    }
+}
