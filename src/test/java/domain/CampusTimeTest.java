49731edc43c223a3d24bbfa45665ff1978175764
AppleMint98 <applemint9821@gmail.com>
2025-02-27 15:32:02 +0900

    feat : 지정된 형식을 벗어나는 입력값 예외 구현
    [x] 지정된 형식 HH:MM  이 아닌 문자열 입력시, 예외를 발생시킨다.

--- src/test/java/domain/CampusTimeTest.java
+++ src/test/java/domain/CampusTimeTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CampusTimeTest {
 
@@ -27,6 +29,31 @@ class CampusTimeTest {
 
         // when // then
         assertThatThrownBy(() -> CampusTime.from(givenData))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효한 범위의 숫자를 입력해 주세요.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1a:03",
+            "12:ㅇㅇ",
+            "ab:4d"
+    })
+    void 시간_및_분으로_숫자가_아닌_값이_들어오는_경우_예외를_발생시킨다(String inputTime) {
+        // when // then
+        assertThatThrownBy(() -> CampusTime.from(inputTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효한 범위의 숫자를 입력해 주세요.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "12::03",
+            "03;24",
+    })
+    void 구분자가_콜론이_아니거나_두개이상_들어오는_경우_예외를_발생시킨다(String inputTime) {
+        // when // then
+        assertThatThrownBy(() -> CampusTime.from(inputTime))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 유효한 범위의 숫자를 입력해 주세요.");
     }
