3f595145c965b08d16e979b101901aaf708a88b4
julie <7jujubuilds@gmail.com>
2025-02-27 14:19:07 +0900

    test(records): 출석 기록 저장 관련 테스트 작성
    테스트 내용:
    - 새 출석 기록을 저장할 수 있다

---
+++ src/test/java/AttendanceRecordsTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+class AttendanceRecordsTest {
+    @DisplayName("새 출석 기록을 저장할 수 있다.")
+    @Test
+    void test() {
+        // given
+        AttendanceRecords attendanceRecords = new AttendanceRecords();
+        LocalDateTime dateTime = LocalDateTime.of(2024, 12, 2, 13, 10);
+        AttendanceRecord attendanceRecord = new AttendanceRecord(dateTime);
+
+        // then
+        assertThatNoException().isThrownBy(() -> attendanceRecords.add(attendanceRecord));
+    }
+}
