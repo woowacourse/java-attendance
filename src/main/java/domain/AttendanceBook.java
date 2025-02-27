de97ee911ab9b4871a2752ed39de4bd3696178f2
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-27 17:44:16 +0900

    feat: 출석부 추가

--- src/main/java/domain/AttendanceBook.java
+++ src/main/java/domain/AttendanceBook.java
@@ -1,72 +1,60 @@
 package domain;
 
-import domain.rule.AbsentRule;
-import util.FormatUtil;
-import util.TimeMachine;
+import domain.policy.AttendancePolicy;
+import reader.AttendanceFileReader;
+import reader.FileReadException;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class AttendanceBook {
 
-    private final Map<String, Attendances> attendanceBook;
+    private final Map<Nickname, Attendances> nicknameToAttendances;
+    private final AttendancePolicy attendancePolicy;
 
-    private AttendanceBook(Map<String, Attendances> attendanceBook) {
-        this.attendanceBook = attendanceBook;
+    private AttendanceBook(Map<Nickname, Attendances> nicknameToAttendances, AttendancePolicy attendancePolicy) {
+        this.nicknameToAttendances = nicknameToAttendances;
+        this.attendancePolicy = attendancePolicy;
     }
 
-    public static AttendanceBook initialize(List<String> attendanceData) {
-        Map<String, Attendances> nicknameToAttendances = new HashMap<>();
-        loadData(attendanceData, nicknameToAttendances);
-        return new AttendanceBook(nicknameToAttendances);
+    public static AttendanceBook initialize(AttendancePolicy attendancePolicy) {
+        return new AttendanceBook(new HashMap<>(), attendancePolicy);
     }
 
-    private static void loadData(List<String> attendanceData,
-                                 Map<String, Attendances> nicknameToAttendances) {
-        attendanceData.stream()
-                .map(attendance -> attendance.split(FormatUtil.ATTENDANCE_DATA_DELIMITER))
-                .forEach(data -> writeData(data, nicknameToAttendances));
+    public void loadAttendance(AttendanceFileReader attendanceFileReader,
+                               String filePath) throws FileReadException {
+
+        Map<String, Map<LocalDate, LocalTime>> rawAttendances = attendanceFileReader.read(filePath).parseData();
+
+        Map<Nickname, Attendances> nicknameToAttendances = new HashMap<>();
+        writeNickname(rawAttendances, nicknameToAttendances);
+        writeAttendances(rawAttendances, nicknameToAttendances);
     }
 
-    private static void writeData(String[] data,
-                                  Map<String, Attendances> nicknameToAttendances) {
-        String nickname = data[FormatUtil.NICKNAME_INDEX];
-        LocalDateTime attendanceDateTime = FormatUtil.parseLocalDateTIme(data[FormatUtil.DATE_TIME_INDEX]);
-
-        AttendanceDate attendanceDate = AttendanceDate.from(attendanceDateTime.toLocalDate());
-        AttendanceTime attendanceTime = AttendanceTime.from(attendanceDateTime.toLocalTime());
-
-        nicknameToAttendances.computeIfAbsent(nickname, incomingNickname -> Attendances.create());
-        nicknameToAttendances.get(nickname).add(attendanceDate, attendanceTime);
+    private void writeNickname(Map<String, Map<LocalDate, LocalTime>> rawAttendances,
+                               Map<Nickname, Attendances> nicknameToAttendances) {
+        rawAttendances.keySet().stream()
+                .map(Nickname::from)
+                .forEach(nickname -> nicknameToAttendances.put(nickname, Attendances.create()));
     }
 
-    public Attendance add(String nickname, Attendance attendance) {
-        if (!existsByNickname(nickname)) {
-            attendanceBook.put(nickname, Attendances.create());
-        }
-
-        Attendances attendances = attendanceBook.get(nickname);
-        return attendances.add(attendance.attendanceDate(), attendance.attendanceTime());
+    private void writeAttendances(Map<String, Map<LocalDate, LocalTime>> rawAttendances,
+                                  Map<Nickname, Attendances> nicknameToAttendances) {
+        rawAttendances.forEach((nickname, dateTime) ->
+                writeAttendance(nicknameToAttendances, nickname, dateTime)
+        );
     }
 
-    public Attendances findAllByNickname(String nickname) {
-        if (existsByNickname(nickname)) {
-            return attendanceBook.get(nickname);
-        }
-        throw new IllegalArgumentException("해당 닉네임으로 출석된 기록이 없습니다.");
-    }
-
-    public ExpulsionCandidates findExpulsionCandidates() {
-        return ExpulsionCandidates.from(attendanceBook.entrySet().stream()
-                .map(expulsionCandidateAttendances ->
-                        expulsionCandidateAttendances.getValue().calculateStatistics(expulsionCandidateAttendances.getKey(), TimeMachine.dateOfNow()))
-                .filter(attendanceStatistics -> AbsentRule.calculateAbsentPolicy(attendanceStatistics).isRiskOfExpulsion())
-                .toList());
-    }
-
-    private boolean existsByNickname(String nickname) {
-        return attendanceBook.containsKey(nickname);
+    private void writeAttendance(Map<Nickname, Attendances> nicknameToAttendances,
+                                 String nickname,
+                                 Map<LocalDate, LocalTime> dateTime) {
+        dateTime.forEach((date, time) -> {
+            Attendances attendances = nicknameToAttendances.get(Nickname.from(nickname));
+            attendances.add(Attendance.of(
+                    AttendanceDate.of(date, attendancePolicy),
+                    AttendanceTime.of(time, attendancePolicy)));
+        });
     }
 }
--- src/test/java/domain/AttendanceBookTest.java
+++ src/test/java/domain/AttendanceBookTest.java
@@ -1,113 +1,41 @@
 package domain;
 
-import org.junit.jupiter.api.BeforeEach;
+import domain.policy.AttendancePolicy;
+import domain.policy.date.AttendanceDatePolicy;
+import domain.policy.time.AttendanceTimePolicy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import util.FormatUtil;
-import util.TimeMachine;
+import reader.AttendanceFileReader;
 
-import java.time.LocalTime;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 class AttendanceBookTest {
 
-    private AttendanceBook attendanceBook;
-
-    @BeforeEach
-    void setUp() {
-        TimeMachine.timeTravelAt(5);
-
-        List<String> attendanceData = List.of(
-                "강산" + FormatUtil.ATTENDANCE_DATA_DELIMITER + "2024-12-02 09:00",
-                "강산" + FormatUtil.ATTENDANCE_DATA_DELIMITER + "2024-12-03 09:00",
-                "강산" + FormatUtil.ATTENDANCE_DATA_DELIMITER + "2024-12-04 09:00",
-                "고양이" + FormatUtil.ATTENDANCE_DATA_DELIMITER + "2024-12-02 09:10",
-                "고양이" + FormatUtil.ATTENDANCE_DATA_DELIMITER + "2024-12-03 09:10",
-                "고양이" + FormatUtil.ATTENDANCE_DATA_DELIMITER + "2024-12-04 09:10",
-                "결석이" + FormatUtil.ATTENDANCE_DATA_DELIMITER + "2024-12-02 12:00",
-                "결석이" + FormatUtil.ATTENDANCE_DATA_DELIMITER + "2024-12-03 12:00",
-                "결석이" + FormatUtil.ATTENDANCE_DATA_DELIMITER + "2024-12-04 12:00"
-        );
-
-        attendanceBook = AttendanceBook.initialize(attendanceData);
-    }
+    private final AttendancePolicy attendancePolicy = new AttendancePolicy(
+            new AttendanceDatePolicy(),
+            new AttendanceTimePolicy()
+    );
 
     @Test
-    @DisplayName("출석 데이터를 이용해 출석부를 초기화할 수 있다")
-    void initialize() {
+    @DisplayName("출석부는 출석 정책을 통해서 초기 상태로 생성할 수 있다.")
+    void canInitialize() {
         // given
         // when
         // then
-        assertAll(
-                () -> assertThat(attendanceBook.findAllByNickname("강산")).isNotNull(),
-                () -> assertThat(attendanceBook.findAllByNickname("고양이")).isNotNull()
-        );
-    }
-
-    @Test
-    @DisplayName("기존에 없던 닉네임의 크루도 출석 시간 이용해 출석을 추가할 수 있다")
-    void addWhenExists() {
-        // given
-        AttendanceDate attendanceDate = AttendanceDate.from(TimeMachine.dateOfNow());
-        AttendanceTime attendanceTime = AttendanceTime.from(LocalTime.of(10, 0));
-
-        // when
-        // then
-        assertThatCode(() -> attendanceBook.add("이런이름은위에서정말없었습니다", Attendance.from(attendanceDate, attendanceTime)))
+        assertThatCode(() -> AttendanceBook.initialize(attendancePolicy))
                 .doesNotThrowAnyException();
     }
 
     @Test
-    @DisplayName("닉네임과 출석 시간 이용해 출석을 추가할 수 있다")
-    void addWhenNotExists() {
+    @DisplayName("출석부는 원시 값(문자열 포함)으로 구성된 출석 데이터들을 올바르게 그룹화 할 수 있다.")
+    void canGroupRawAttendancesData() {
         // given
-        AttendanceDate attendanceDate = AttendanceDate.from(TimeMachine.dateOfNow());
-        AttendanceTime attendanceTime = AttendanceTime.from(LocalTime.of(10, 0));
+        AttendanceBook attendanceBook = AttendanceBook.initialize(attendancePolicy);
+        AttendanceFileReader attendanceFileReader = new AttendanceFileReader();
 
         // when
         // then
-        assertThatCode(() -> attendanceBook.add("강산", Attendance.from(attendanceDate, attendanceTime)))
+        assertThatCode(() -> attendanceBook.loadAttendance(attendanceFileReader, AttendanceFileReader.DEFAULT_ATTENDANCE_DATA_PATH))
                 .doesNotThrowAnyException();
-    }
-
-    @Test
-    @DisplayName("닉네임을 이용해 출석 기록을 조회할 수 있다")
-    void findAllByNickname() {
-        // given
-        // when
-        Attendances attendance = attendanceBook.findAllByNickname("강산");
-
-        // then
-        assertThat(attendance).isNotNull();
-    }
-
-    @Test
-    @DisplayName("존재하지 않는 닉네임을 조회하면 예외가 발생한다")
-    void findAllByNickname_ShouldThrowException_WhenNicknameNotFound() {
-        // given
-        // when
-        // then
-        assertThatThrownBy(() -> attendanceBook.findAllByNickname("없음이"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("해당 닉네임으로 출석된 기록이 없습니다.");
-    }
-
-    @Test
-    @DisplayName("제적 후보자를 조회할 수 있다")
-    void findExpulsionCandidates() {
-        // given
-        // when
-        ExpulsionCandidates candidates = attendanceBook.findExpulsionCandidates();
-
-        // then
-        assertThat(candidates).isNotNull();
-        assertThat(candidates.attendanceStatistics().size()).isEqualTo(1);
-        assertThat(candidates.attendanceStatistics().getFirst().nickname()).isEqualTo("결석이");
-        assertThat(candidates.attendanceStatistics().getFirst().absentCount()).isEqualTo(2);
     }
 }
