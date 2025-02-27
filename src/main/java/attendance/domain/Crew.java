0ea85ad2de9ff012b2a9f664a5b699195c0726a0
bowook <mbosx316@naver.com>
2025-02-27 21:53:19 +0900

    feat(Crew) : Crew 기능 추가 (GREEN)

---
+++ src/main/java/attendance/domain/Crew.java
@@ -0,0 +1,13 @@
+package attendance.domain;
+
+public class Crew {
+    private final String name;
+
+    public Crew(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
