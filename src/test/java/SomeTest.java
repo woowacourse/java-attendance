8c5bfc79d8a6c95f78ee393c482b1e4a495cdd41
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-27 14:41:29 +0900

    GREEN) test(domain) : AttendanceStatus
    - 화,수,목,금요일 각 상황별 테스트 추가
     - 출석, 지각, 결석

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class SomeTest {
-    // 월요일의 경우, 출석, 지각, 결석 상태 반환
-
     @Nested
     @DisplayName("월요일 정상 출석, 지각, 결석 시 출석 상태 결정 테스트")
     class MondayAttendanceTest {
@@ -38,6 +36,54 @@ public class SomeTest {
         void test3() {
             // given
             LocalDateTime attendedTime =  LocalDateTime.of(2024, 12, 2, 13, 31);
+
+            // when & then
+            Assertions.assertThat(AttendanceStatus.getStatusByAttendedTime(attendedTime))
+                    .isEqualTo(AttendanceStatus.ABSENT);
+        }
+    }
+
+    @Nested
+    @DisplayName("화수목금 정상 출석, 지각, 결석 시 출석 상태 결정 테스트")
+    class NonMondayAttendanceTest {
+        @DisplayName("정상 출석")
+        @Test
+        void test1() {
+            // given
+            LocalDateTime attendedTime =  LocalDateTime.of(2024, 12, 3, 10, 0);
+
+            // when & then
+            Assertions.assertThat(AttendanceStatus.getStatusByAttendedTime(attendedTime))
+                    .isEqualTo(AttendanceStatus.ATTEND);
+        }
+
+        @DisplayName("지각")
+        @Test
+        void test2() {
+            // given
+            LocalDateTime attendedTime =  LocalDateTime.of(2024, 12, 3, 10, 6);
+
+            // when & then
+            Assertions.assertThat(AttendanceStatus.getStatusByAttendedTime(attendedTime))
+                    .isEqualTo(AttendanceStatus.LATE);
+        }
+
+        @DisplayName("결석")
+        @Test
+        void test3() {
+            // given
+            LocalDateTime attendedTime =  LocalDateTime.of(2024, 12, 4, 10, 31);
+
+            // when & then
+            Assertions.assertThat(AttendanceStatus.getStatusByAttendedTime(attendedTime))
+                    .isEqualTo(AttendanceStatus.ABSENT);
+        }
+
+        @DisplayName("결석 - 월요일이었으면 출석처리")
+        @Test
+        void test4() {
+            // given
+            LocalDateTime attendedTime =  LocalDateTime.of(2024, 12, 5, 13, 0);
 
             // when & then
             Assertions.assertThat(AttendanceStatus.getStatusByAttendedTime(attendedTime))
