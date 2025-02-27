a2835a35573944902b5cac7de892fdc4c022c02f
rladmstn <oz33177351@gmail.com>
2025-02-27 20:55:17 +0900

    test: 입력 받은 이름이 크루 목록에 존재하는지 판단 테스트 작성

--- src/test/java/AttendanceBookTest.java
+++ src/test/java/AttendanceBookTest.java
@@ -3,7 +3,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import attendance.model.AttendanceBook;
 import attendance.model.AttendanceTime;
 import java.time.LocalDate;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class AttendanceBookTest {
 
@@ -22,4 +25,21 @@ public class AttendanceBookTest {
         assertThat(result).isEqualTo(attendanceTime);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {
+            "이름1, true",
+            "이름2, false"
+    })
+    void 입력_받은_이름이_크루_목록에_존재하는지_판단한다(final String name, final boolean expectedResult) {
+
+        // given
+        final AttendanceBook attendanceBook = new AttendanceBook();
+        attendanceBook.add("이름1", new AttendanceTime(LocalDate.of(2025, 2, 27), 10, 10));
+
+        // when
+        final boolean result = attendanceBook.isCrewExists(name);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
 }
