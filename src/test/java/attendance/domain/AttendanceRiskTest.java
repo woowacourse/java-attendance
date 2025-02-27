5ff55f006c464c89b6995c980143231fa5a8e752
taek2222 <oot309@daum.net>
2025-02-27 14:39:32 +0900

    test(AttendanceRiskTest): 면담 대상자 파악 검증 테스트 추가

--- src/test/java/attendance/domain/AttendanceRiskTest.java
+++ src/test/java/attendance/domain/AttendanceRiskTest.java
@@ -19,4 +19,17 @@ class AttendanceRiskTest {
         // then
         Assertions.assertThat(result).isEqualTo(AttendanceRisk.WARNING);
     }
+
+    @Test
+    @DisplayName("결석 3회 이상은 면담 대상자이다")
+    void 결석_3회_이상은_면담_대상자이다() {
+        // given
+        int absence = 3;
+
+        // when
+        AttendanceRisk result = AttendanceRisk.evaluate(absence);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(AttendanceRisk.INTERVIEW);
+    }
 }
