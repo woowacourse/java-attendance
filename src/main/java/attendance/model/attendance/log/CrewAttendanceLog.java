1435398045f52da5d67792d2f1398c2549d86149
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-27 20:24:38 +0900

    feat: 지각, 결석 횟수 반환 기능 추가

--- src/main/java/attendance/model/attendance/log/CrewAttendanceLog.java
+++ src/main/java/attendance/model/attendance/log/CrewAttendanceLog.java
@@ -20,4 +20,24 @@ public class CrewAttendanceLog {
     public List<AttendanceLog> getAttendanceLogs() {
         return attendanceLogs.getValues();
     }
+
+    public int getAbsenceCount() {
+        return attendanceLogs.getAbsenceCount();
+    }
+
+    public int getLateCount() {
+        return attendanceLogs.getLateCount();
+    }
+
+    public int getPolicyAppliedAbsenceCount() {
+        return attendanceLogs.getPolicyAppliedAbsenceCount();
+    }
+
+    public int getPolicyAppliedLateCount() {
+        return attendanceLogs.getPolicyAppliedLateCount();
+    }
+
+    public String getCrewNickname() {
+        return crew.getNickName();
+    }
 }
