423ac63b8bf2dcb2bfacd70d32b300a45a3a477a
kjyyjk <rlawnsdud920@gmail.com>
2025-02-27 16:05:49 +0900

    feat(red): AttendanceStatistics가 위험 상태 계산

--- src/main/java/attendance/domain/AttendanceStatistics.java
+++ src/main/java/attendance/domain/AttendanceStatistics.java
@@ -1,11 +1,27 @@
 package attendance.domain;
 
 public class AttendanceStatistics {
+    private final int attendanceCount;
+    private final int lateCount;
+    private final int absenceCount;
     public AttendanceStatistics(final int attendanceCount, final int lateCount, final int absenceCount) {
-
+        this.attendanceCount = attendanceCount;
+        this.lateCount = lateCount;
+        this.absenceCount = absenceCount;
     }
 
     public String calculateDangerousStatus() {
-        return "null";
+        int totalAbsence = absenceCount;
+        totalAbsence += (lateCount / 3);
+        if (totalAbsence > 5) {
+            return "FIRE";
+        }
+        if (totalAbsence > 2) {
+            return "INTERVIEW";
+        }
+        if (totalAbsence > 1) {
+            return "WARNING";
+        }
+        return "NONE";
     }
 }
--- src/test/java/attendance/domain/AttendanceStatisticsTest.java
+++ src/test/java/attendance/domain/AttendanceStatisticsTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class AttendanceStatisticsTest {
     @DisplayName("d")
-    @CsvSource(value = {"1:3:0:NONE", "1:3:1:WARNING", "4:0:3:INTERVIEW", "2:7:3:FIRE"}, delimiterString = ":")
+    @CsvSource(value = {"1:3:0:NONE", "1:3:1:WARNING", "4:0:3:INTERVIEW", "2:10:3:FIRE"}, delimiterString = ":")
     @ParameterizedTest
     void should_ReturnDangerousStatus(int attendanceCount, int lateCount, int absenceCount, String dangerousStatus) {
         //given
