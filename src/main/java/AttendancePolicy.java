4437886a75d5aa4e679afa3714b83142d6010f0e
byesol <bsj5967@naver.com>
2025-02-27 21:14:52 +0900

    feat : 영업일 체크 기능 추가(green)

--- src/main/java/AttendancePolicy.java
+++ src/main/java/AttendancePolicy.java
@@ -1,5 +1,8 @@
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.MonthDay;
+import java.util.Set;
 
 public class AttendancePolicy {
     private static final LocalTime DEFAULT_START_TIME = LocalTime.of(10, 0);
@@ -7,6 +10,9 @@ public class AttendancePolicy {
     private static final int ABSENT_STANDARD = 30;
     private static final LocalTime OPERATING_START_TIME = LocalTime.of(8, 0);
     private static final LocalTime OPERATING_END_TIME = LocalTime.of(23, 0);
+
+    private static final Set<DayOfWeek> WEEKENDS = Set.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+    private static final Set<MonthDay> HOLIDAYS = Set.of(MonthDay.of(12, 25));
 
 
     public static AttendanceStatus checkAttendanceStatus(LocalDateTime attendanceTime) {
@@ -22,4 +28,12 @@ public class AttendancePolicy {
     public static boolean isOperatingTime(LocalTime attendanceTime) {
         return !attendanceTime.isBefore(OPERATING_START_TIME) && !attendanceTime.isAfter(OPERATING_END_TIME);
     }
+
+    public static boolean isOperatingDay(LocalDateTime attendanceTime) {
+        if (WEEKENDS.contains(DayOfWeek.from(attendanceTime)) || HOLIDAYS.contains(MonthDay.from(attendanceTime))) {
+            return false;
+        }
+        return true;
+    }
+
 }
