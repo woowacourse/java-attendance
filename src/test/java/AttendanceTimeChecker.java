6298ac94d94767c357e2cb53a8a0293423d15d57
GiSeok Kang <ime.rokz@gmail.com>
2025-02-27 15:04:51 +0900

    green: 시작 시간 이전에 출석해도 출석 기능 구현

--- src/test/java/AttendanceTimeChecker.java
+++ src/test/java/AttendanceTimeChecker.java
@@ -1,7 +1,12 @@
 public class AttendanceTimeChecker {
     public AttendPolicy attendanceCheck(String time) {
         String[] split = time.split(":");
+        int hour = Integer.parseInt(split[0]);
         int minute = Integer.parseInt(split[1]);
+
+        if (hour < 10) {
+            return AttendPolicy.ATTEND;
+        }
 
         if (minute > 30) {
             return AttendPolicy.ABSENT;
