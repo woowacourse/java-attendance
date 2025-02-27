cf1f1893f7352ec25168fa2e2d9d7e6b43b47a8f
ryan-dia <cjfdnjs158@naver.com>
2025-02-27 15:16:09 +0900

    feat: 출결 관련 counts 객체 추가

---
+++ src/main/java/domain/AttendanceRecords.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.stream.Collectors;
+
+public class AttendanceRecords {
+    private final SortedSet<AttendanceRecord> attendanceRecords;
+    private final AttendanceStatusCounts attendanceStatusCounts;
+
+    public AttendanceRecords(final SortedSet<AttendanceRecord> attendanceRecords) {
+        this.attendanceRecords = attendanceRecords;
+        this.attendanceStatusCounts = this.countAttendanceStatus();
+    }
+
+    private AttendanceStatusCounts countAttendanceStatus() {
+        final Map<AttendanceStatus, Long> counts = attendanceRecords.stream()
+                .collect(Collectors.groupingBy(
+                        AttendanceRecord::getAttendanceStatus,
+                        () -> new EnumMap<>(AttendanceStatus.class),
+                        Collectors.counting()
+                ));
+        final int attendanceCount = counts.getOrDefault(AttendanceStatus.PRESENT, 0L).intValue();
+        final int lateCount = counts.getOrDefault(AttendanceStatus.LATE, 0L).intValue();
+        final int absenceCount = counts.getOrDefault(AttendanceStatus.ABSENT, 0L).intValue();
+
+        return new AttendanceStatusCounts(absenceCount, lateCount, attendanceCount);
+    }
+
+    public AttendanceStatusCounts getAttendanceStatusCounts() {
+        return attendanceStatusCounts;
+    }
+}
