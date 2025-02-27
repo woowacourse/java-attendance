8d110c6deb6273333e57398527c2367d956bbf44
taek2222 <oot309@daum.net>
2025-02-28 00:36:31 +0900

    feat(CrewAttendanceManager): 크루 출석 등록 로직 추가

---
+++ src/main/java/attendance/domain/CrewAttendanceManager.java
@@ -0,0 +1,22 @@
+package attendance.domain;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CrewAttendanceManager {
+
+    private final Map<String, Attendances> crewAttendance = new HashMap<>();
+
+    public void addNewCrew(String nickname, Attendances attendances) {
+        crewAttendance.put(nickname, attendances);
+    }
+
+    public Attendance processAttendanceCheck(final String nickname, final LocalDateTime dateTime) {
+        Attendances attendances = crewAttendance.get(nickname);
+        Attendances newAttendances = attendances.registerAttendance(dateTime);
+        crewAttendance.put(nickname, newAttendances);
+
+        return newAttendances.findAttendanceByDate(dateTime.toLocalDate());
+    }
+}
