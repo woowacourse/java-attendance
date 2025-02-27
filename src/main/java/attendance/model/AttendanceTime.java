2b60927361cc3d40b82fab49c4c688e83d6dfa31
rladmstn <oz33177351@gmail.com>
2025-02-27 17:36:09 +0900

    feat: 입력 시간에 대해 캠퍼스 운영 시간 검증 로직 구현

--- src/main/java/attendance/model/AttendanceTime.java
+++ src/main/java/attendance/model/AttendanceTime.java
@@ -2,14 +2,19 @@ package attendance.model;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class AttendanceTime {
     private final LocalDate date;
     private final int hour;
     private final int minute;
 
+    private static final LocalTime CAMPUS_START_TIME = LocalTime.of(8, 0);
+    private static final LocalTime CAMPUS_END_TIME = LocalTime.of(23, 0);
+
     public AttendanceTime(final LocalDate date, final int hour, final int minute) {
         validateAttendDate(date);
+        validateOperatingTime(hour, minute);
         this.date = date;
         this.hour = hour;
         this.minute = minute;
@@ -23,5 +28,13 @@ public class AttendanceTime {
 
     private boolean isWeekend(final DayOfWeek day) {
         return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
+    }
+
+    private void validateOperatingTime(final int hour, final int minute) {
+        LocalTime inputTime = LocalTime.of(hour, minute);
+
+        if (inputTime.isBefore(CAMPUS_START_TIME) || inputTime.isAfter(CAMPUS_END_TIME)) {
+            throw new IllegalArgumentException("[ERROR] 캠퍼스 운영 시간이 아닙니다.");
+        }
     }
 }
