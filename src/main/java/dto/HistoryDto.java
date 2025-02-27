1d11075943bbf048cc395fbd7e1140528832d4ca
byesol <bsj5967@naver.com>
2025-02-27 16:20:25 +0900

    set : 2단계를 위한 파일 제거

--- src/main/java/dto/HistoryDto.java
+++
@@ -1,9 +0,0 @@
-package dto;
-
-import java.time.LocalDateTime;
-
-public record HistoryDto(
-        LocalDateTime attendanceTime,
-        String attendanceResult
-) {
-}
--- src/main/java/util/AttendanceDataReader.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-
-public interface AttendanceDataReader {
-    Map<String, List<LocalDateTime>> loadAttendanceData();
-}
