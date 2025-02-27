5270720c9d0fcca0cf9839b3658ddd34d899a539
Mint <audwl03071@gmail.com>
2025-02-27 13:49:55 +0900

    test(SubjectType): 테스트 추가

--- src/test/java/attendance/model/SubjectTypeTest.java
+++ src/test/java/attendance/model/SubjectTypeTest.java
@@ -1,10 +1,15 @@
 package attendance.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import attendance.domain.model.SubjectType;
-import org.assertj.core.api.Assertions;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SubjectTypeTest {
 
@@ -30,6 +35,39 @@ class SubjectTypeTest {
         SubjectType expected = SubjectType.from(absentCount, lateCount);
 
         // Then
-        Assertions.assertThat(expected.getName()).isEqualTo(subjectTypeName);
+        assertThat(expected.getName()).isEqualTo(subjectTypeName);
+    }
+
+    @Test
+    @DisplayName("지각 횟수를 계산한다")
+    void 지각_횟수를_계산한다() {
+        // Given
+        int lateCount = 2;
+        int absentCount = 3;
+
+        // When
+        int totalLateCount = SubjectType.calculateTotalLateCount(lateCount, absentCount);
+
+        // Then
+        assertThat(totalLateCount).isEqualTo(11);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("적용 가능한 타입인지 확인한다")
+    void 적용_가능한_타입인지_확인한다(final SubjectType subjectType, final boolean expected) {
+        // Given
+
+        // When & Then
+        assertThat(subjectType.isApplicable()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> 적용_가능한_타입인지_확인한다() {
+        return Stream.of(
+                Arguments.of(SubjectType.WARNING, true),
+                Arguments.of(SubjectType.INTERVIEW, true),
+                Arguments.of(SubjectType.EXPULSION, true),
+                Arguments.of(SubjectType.NOT_APPLICABLE, false)
+        );
     }
 }
