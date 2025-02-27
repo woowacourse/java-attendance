7c2cad7637fc00d280d6dfd7dd98fcc55f67c0f8
GiSeok Kang <ime.rokz@gmail.com>
2025-02-27 14:58:35 +0900

    green: 30분 초과 출석 시 결석 기능 구현

--- src/test/java/AttendanceTimeChecker.java
+++ src/test/java/AttendanceTimeChecker.java
@@ -1,5 +1,11 @@
 public class AttendanceTimeChecker {
     public AttendPolicy attendanceCheck(String time) {
+        String[] split = time.split(":");
+        int minute = Integer.parseInt(split[1]);
+
+        if (minute > 30) {
+            return AttendPolicy.ABSENT;
+        }
         return AttendPolicy.LATE;
     }
 }
