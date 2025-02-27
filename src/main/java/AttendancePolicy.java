226b9f9a2b0da42389a628f93dd46ab1aeb8468f
byesol <bsj5967@naver.com>
2025-02-27 17:26:26 +0900

    feat(green test) :  지각 출석 확인 성공
    - 단순 시간에 의해서만 판별(월요일 x)

--- src/main/java/AttendancePolicy.java
+++ src/main/java/AttendancePolicy.java
@@ -1,9 +1,16 @@
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class AttendancePolicy {
+    private static final LocalTime DEFAULT_START_TIME = LocalTime.of(10, 0);
+    private static final int LATE_STANDARD = 5;
 
     public static AttendanceStatus checkAttendanceStatus(LocalDateTime attendanceTime) {
-        return AttendanceStatus.ATTEND;
+        if (LocalTime.from(attendanceTime).isBefore(DEFAULT_START_TIME.plusMinutes(LATE_STANDARD))) {
+            return AttendanceStatus.ATTEND;
+        }
+        return AttendanceStatus.LATE;
+
 
     }
 }
--- src/test/java/CheckAttendanceTest.java
+++ src/test/java/CheckAttendanceTest.java
@@ -6,19 +6,27 @@ import org.junit.jupiter.api.Test;
 
 public class CheckAttendanceTest {
 
-    // 1. 시간을 주면 상태를 반환한다.
+    // 1. 시간에 따른 출석 상태를 반환한다. 처음엔 단순히 10시 출석 기준 날짜와 관개없이 시간만으로 판단한다.
+    // 2. 날짜에 따라 휴일이나 공휴일일 겨우 오류처리한다
+    // 3. 날짜(월요일)에 따라 다른 시간 정책을 적용한다.
 
+    //1번 과정
+    //출석, 결석, 지각,
     @Test
     public void checkAttendanceStatusTest() {
         // given
         LocalDateTime attendanceTime = LocalDateTime.of(2024, 12, 17, 10, 0);
-        LocalDateTime lateTime = LocalDateTime.of(2024, 12, 17, 10, 30);
+        LocalDateTime lateTime = LocalDateTime.of(2024, 12, 17, 10, 6);
+
         // when
         AttendanceStatus attendanceStatus = AttendancePolicy.checkAttendanceStatus(attendanceTime);
-        AttendanceStatus lateStatus = AttendancePolicy.checkAttendanceStatus(attendanceTime);
+        AttendanceStatus lateStatus = AttendancePolicy.checkAttendanceStatus(lateTime);
+
         // then
         assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ATTEND);
-        assertThat(attendanceStatus).isEqualTo(AttendanceStatus.LATE);
-
+        assertThat(lateStatus).isEqualTo(AttendanceStatus.LATE);
     }
+    //운영시간 외 처리
+
+
 }
