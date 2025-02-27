9b12101c56aca2612f383b729b8d2df4df0af44f
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-27 14:12:54 +0900

    refactor: 임시 클래스 제거 및 테스트 코드 재구성
    - 이미 분리된 도메인에 기능 구현 했기에 별도의 도메인 분리 없음

--- src/test/java/AttendanceUpdateTest.java
+++ src/test/java/AttendanceUpdateTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class AttendanceUpdateTest {
-    //String nickname, LocalTime orginTime, LocalTime modifiedTime, Boolean isLateExpected, Boolean isAbsentExpected) {
     public static Stream<Arguments> provideModificationInformation() {
         return Stream.of(
                 Arguments.of("에드", LocalTime.of(10, 0), LocalTime.of(10, 6), true, false),
@@ -20,17 +19,6 @@ class AttendanceUpdateTest {
                 Arguments.of("율무", LocalTime.of(10, 0), LocalTime.of(10, 31), false, true),
                 Arguments.of("링크", LocalTime.of(10, 31), LocalTime.of(10, 30), true, false));
     }
-
-    //출석 확인을 수정하려면 닉네임, 수정하려는 날짜, 등교 시간을 입력하여 기록을 수정할 수 있다.
-    //수정 후에는 변경 전과 변경 후의 출석 기록을 확인할 수 있다.
-    //출석을 수정하려는 크루의 닉네임을 입력해 주세요.
-    //빙티
-    //수정하려는 날짜(일)를 입력해 주세요.
-    //3
-    //언제로 변경하겠습니까?
-    //09:58
-    //
-    //12월 03일 화요일 10:07 (지각) -> 09:58 (출석) 수정 완료!
 
     @Test
     void 닉네임_수정날짜_등교시간을_입력하여_기록을_수정한다() {
@@ -42,8 +30,7 @@ class AttendanceUpdateTest {
         attendanceBook.recordAttendance(nickname, attendance);
 
         final var modifiedTime = LocalTime.of(10, 5);
-        AttendanceUpdate attendanceUpdate = new AttendanceUpdate();
-        attendanceUpdate.updateAttendanceTime(attendanceBook, nickname, day, modifiedTime);
+        attendance.modifyTimeTo(modifiedTime);
 
         assertEquals(modifiedTime, attendance.getTime());
     }
@@ -58,8 +45,7 @@ class AttendanceUpdateTest {
         AttendanceBook attendanceBook = new AttendanceBook();
         attendanceBook.recordAttendance(nickname, attendance);
 
-        AttendanceUpdate attendanceUpdate = new AttendanceUpdate();
-        attendanceUpdate.updateAttendanceTime(attendanceBook, nickname, day, modifiedTime);
+        attendance.modifyTimeTo(modifiedTime);
 
         Boolean isLate = attendance.isLate();
         Boolean isAbsent = attendance.isAbsent();
