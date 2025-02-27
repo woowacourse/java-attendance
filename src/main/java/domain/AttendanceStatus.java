42903bc953b6ab22c0253774c9fd3b8ce2529928
changuii <rhljh7410@gmail.com>
2025-02-27 14:36:14 +0900

    green: 출석의 상태를 계산하라

--- src/main/java/domain/AttendanceStatus.java
+++ src/main/java/domain/AttendanceStatus.java
@@ -2,15 +2,37 @@ package domain;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public enum AttendanceStatus {
 
-    ATTENDANCE,
-    LATE,
-    ABSENCE;
+    ATTENDANCE(0),
+    LATE(5),
+    ABSENCE(30);
 
+    private static final LocalTime MONDAY_START_CAMPUS = LocalTime.of(13, 0);
+    private static final LocalTime BESIDE_MONDAY_START_CAMPUS = LocalTime.of(10, 0);
+    private final int boundaryMinute;
 
-    public static AttendanceStatus calculateStatus(final LocalTime time, final DayOfWeek tuesday) {
-        return null;
+    AttendanceStatus(final int boundaryMinute) {
+        this.boundaryMinute = boundaryMinute;
+    }
+
+    public static AttendanceStatus calculateStatus(final LocalTime time, final DayOfWeek dayOfWeek) {
+        return Arrays.stream(values())
+                .sorted(Comparator.comparingInt(AttendanceStatus::getBoundaryMinute).reversed())
+                .filter(status -> {
+                    if (dayOfWeek == DayOfWeek.MONDAY) {
+                        return time.isAfter(MONDAY_START_CAMPUS.plusMinutes(status.boundaryMinute));
+                    }
+                    return time.isAfter(BESIDE_MONDAY_START_CAMPUS.plusMinutes(status.boundaryMinute));
+                })
+                .findFirst()
+                .orElse(ATTENDANCE);
+    }
+
+    public int getBoundaryMinute() {
+        return boundaryMinute;
     }
 }
