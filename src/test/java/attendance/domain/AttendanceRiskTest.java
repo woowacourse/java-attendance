d95e5a8f0a2c0c250f652a47fb08b37468f0388c
taek2222 <oot309@daum.net>
2025-02-27 15:38:54 +0900

    test(AttendanceRiskTest): 파라미터 테스트로 리팩토링

--- src/test/java/attendance/domain/AttendanceRiskTest.java
+++ src/test/java/attendance/domain/AttendanceRiskTest.java
@@ -2,47 +2,31 @@ package attendance.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 @DisplayName("출결 위험도 테스트")
 class AttendanceRiskTest {
 
-    @Test
-    @DisplayName("결석 2회 이상은 경고 대상자이다")
-    void 결석_2회_이상은_경고_대상자이다() {
-        // given
-        int absence = 2;
-
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("결석 횟수로 위험도를 판단해 반환한다")
+    void 결석_횟수로_위험도를_판단해_반환한다(int absence, AttendanceRisk excepted) {
         // when
         AttendanceRisk result = AttendanceRisk.evaluate(absence);
 
         // then
-        Assertions.assertThat(result).isEqualTo(AttendanceRisk.WARNING);
+        Assertions.assertThat(result).isEqualTo(excepted);
     }
 
-    @Test
-    @DisplayName("결석 3회 이상은 면담 대상자이다")
-    void 결석_3회_이상은_면담_대상자이다() {
-        // given
-        int absence = 3;
-
-        // when
-        AttendanceRisk result = AttendanceRisk.evaluate(absence);
-
-        // then
-        Assertions.assertThat(result).isEqualTo(AttendanceRisk.INTERVIEW);
-    }
-
-    @Test
-    @DisplayName("결석 5회 초과는 제적 대상자이다")
-    void 결석_5회_초과는_제적_대상자이다() {
-        // given
-        int absence = 6;
-
-        // when
-        AttendanceRisk result = AttendanceRisk.evaluate(absence);
-
-        // then
-        Assertions.assertThat(result).isEqualTo(AttendanceRisk.WEEDING);
+    private static Stream<Arguments> 결석_횟수로_위험도를_판단해_반환한다() {
+        return Stream.of(
+                Arguments.of(2, AttendanceRisk.WARNING),
+                Arguments.of(3, AttendanceRisk.INTERVIEW),
+                Arguments.of(6, AttendanceRisk.WEEDING)
+        );
     }
 }
