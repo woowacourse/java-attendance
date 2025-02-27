4a055e670120be0a2f991c293f94f791f496746a
GiSeok Kang <ime.rokz@gmail.com>
2025-02-27 19:31:09 +0900

    green: 결석 3회 이상 면담 대상 기능 구현

--- src/test/java/AbsentPenaltyChecker.java
+++ src/test/java/AbsentPenaltyChecker.java
@@ -1,6 +1,9 @@
 public class AbsentPenaltyChecker {
 
     public AbsentPenalty check(int absentCount) {
+        if (absentCount >= 3) {
+            return AbsentPenalty.INTERVIEW;
+        }
         return AbsentPenalty.WARNING;
     }
 }
