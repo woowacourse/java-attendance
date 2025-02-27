3cf296cbfcae51eb4bbbeb6a11405da0b0d47ec1
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-27 17:22:42 +0900

    RED) test(domain) : Crew, Attendance
    - 출석 상태별 갯수 반환 테스트 작성

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -1,17 +1,19 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import domain.Attendance;
 import domain.Crew;
 import domain.ERROR_MESSAGE;
 import java.time.LocalDateTime;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class SomeTest {
     @Nested
-    @DisplayName("출석 기록 저장 오류 테스트")
+    @DisplayName("크루 출석 기록 저장 테스트")
     class AddAttendanceTest {
 
         @DisplayName("정상 출석 확인")
@@ -46,7 +48,7 @@ public class SomeTest {
     }
 
     @Nested
-    @DisplayName("출석 기록 저장 오류 테스트")
+    @DisplayName("크루 출석 기록 저장 오류 테스트")
     class AbnormalAddAttendanceTest {
 
         @DisplayName("이미 출석한 경우")
@@ -64,8 +66,31 @@ public class SomeTest {
                     .hasMessage(ERROR_MESSAGE.ALREADY_ATTENDED.getMessage());
         }
     }
+
+    @Nested
+    @DisplayName("크루 출석 상태 갯수")
+    class CountAttendanceStatusTest {
+
+        @DisplayName("출석 상태별 개수 반환")
+        @Test
+        void test1() {
+            // given
+            Crew crew = new Crew("빙봉");
+
+            // when
+            crew.addAttendanceWithDateTime(LocalDateTime.of(2024, 12, 2, 8, 25)); // 출석
+            crew.addAttendanceWithDateTime(LocalDateTime.of(2024, 12, 3, 8, 25)); // 출석
+            crew.addAttendanceWithDateTime(LocalDateTime.of(2024, 12, 4, 8, 25)); // 출석
+            crew.addAttendanceWithDateTime(LocalDateTime.of(2024, 12, 5, 10, 25)); // 지각
+            crew.addAttendanceWithDateTime(LocalDateTime.of(2024, 12, 6, 10, 25)); // 지각
+            crew.addAttendanceWithDateTime(LocalDateTime.of(2024, 12, 9, 15, 0)); // 결석
+
+            // then
+            assertAll(
+                    () -> assertThat(crew.getAttendCount()).isEqualTo(3),
+                    () -> assertThat(crew.getLateCount()).isEqualTo(2),
+                    () -> assertThat(crew.getAbsentCount()).isEqualTo(1)
+            );
+        }
+    }
 }
-
-
-
-
