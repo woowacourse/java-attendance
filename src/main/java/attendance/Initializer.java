da8751bb9f96381d0ca4ad926a3ed70f526ebbd4
ljhee92 <weilainib7@gmail.com>
2025-02-27 18:34:26 +0900

    feat: 필요한 정보 초기화 구현
    - step1에서 Controller가 했던 초기화 기능을 Initializer에게 부여했습니다.

---
+++ src/main/java/attendance/Initializer.java
@@ -0,0 +1,52 @@
+package attendance;
+
+import attendance.domain.Attendance;
+import attendance.domain.AttendanceBook;
+import attendance.domain.Crew;
+import attendance.domain.Crews;
+import attendance.domain.Nickname;
+import attendance.util.FileResourceReader;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Initializer {
+
+    private static final String PATH = "src/main/resources/attendances.csv";
+    private static final String DELIMITER = ",";
+    private static final int NICKNAME_INDEX = 0;
+    private static final int DATE_INDEX = 1;
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    private final Crews crews;
+    private final AttendanceBook attendanceBook;
+
+    public Initializer() {
+        this.crews = new Crews(new ArrayList<>());
+        this.attendanceBook = new AttendanceBook(new HashMap<>());
+        initAll();
+    }
+
+    private void initAll() {
+        for (String line : FileResourceReader.read(PATH)) {
+            String nickname = line.split(DELIMITER)[NICKNAME_INDEX];
+            Crew crew = new Crew(new Nickname(nickname));
+            crews.add(crew);
+            LocalDate attendDate = LocalDate.parse(line.split(DELIMITER)[DATE_INDEX], DATE_TIME_FORMATTER);
+            LocalTime attendTime = LocalTime.parse(line.split(DELIMITER)[DATE_INDEX], DATE_TIME_FORMATTER);
+            Attendance attendance = new Attendance(attendDate, attendTime);
+            attendanceBook.add(crew, attendance);
+        }
+    }
+
+    public AttendanceBook initAttendanceBook() {
+        return attendanceBook;
+    }
+
+    public Crews initCrews() {
+        return crews;
+    }
+}
