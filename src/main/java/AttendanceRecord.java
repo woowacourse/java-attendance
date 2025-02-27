a091a2aa13bd01533723ed7954829d763d241454
julie <7jujubuilds@gmail.com>
2025-02-27 11:19:24 +0900

    feat(record): 출석 기록 클래스 생성
    - 출석 날짜와 시간을 저장하는 클래스 구현

---
+++ src/main/java/AttendanceRecord.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class AttendanceRecord {
+    private final LocalDateTime dateTime;
+
+    public AttendanceRecord(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
