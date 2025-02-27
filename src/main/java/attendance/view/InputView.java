0d7f4399d3fe84b121e77206518c893dcc59af6b
KJungW <sagakkb0707@naver.com>
2025-02-27 09:19:55 +0900

    test(input): 입력값 검증 (구현)
    구현내용
    - 입력값에 대한 기본 검증 구현
    - 검증을 입력뷰에 적용

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -3,6 +3,7 @@ package attendance.view;
 import attendance.controller.MenuCommand;
 import attendance.utility.DateTimeUtility;
 import attendance.view.message.InputMessage;
+import attendance.view.validator.InputValidator;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Scanner;
@@ -19,32 +20,44 @@ public class InputView {
         String dateContent = DateTimeUtility.formatDateTime(now);
         String menuContent = String.format(InputMessage.MENU.getContent(), dateContent);
         System.out.println(menuContent);
-        String input = scanner.nextLine();
+        String input = readNotBlankInput();
         return MenuCommand.parse(input);
     }
 
     public String readNickname() {
         System.out.println(InputMessage.NICKNAME.getContent());
-        return scanner.nextLine();
+        String input = readNotBlankInput();
+        return input;
     }
 
     public LocalTime readArrivalTime() {
         System.out.println(InputMessage.ARRIVAL_TIME.getContent());
-        return DateTimeUtility.parseTimeByDefault(scanner.nextLine());
+        String input = readNotBlankInput();
+        return DateTimeUtility.parseTimeByDefault(input);
     }
 
     public String readNicknameForUpdate() {
         System.out.println(InputMessage.NICKNAME_FOR_UPDATE.getContent());
-        return scanner.nextLine();
+        String input = readNotBlankInput();
+        return input;
     }
 
     public int readDayForUpdate() {
         System.out.println(InputMessage.DAY_FOR_UPDATE.getContent());
-        return Integer.parseInt(scanner.nextLine());
+        String input = readNotBlankInput();
+        InputValidator.validateNonNumeric(input);
+        return Integer.parseInt(input);
     }
 
     public LocalTime readArrivalTimeForUpdate() {
         System.out.println(InputMessage.TIME_FOR_UPDATE.getContent());
-        return DateTimeUtility.parseTimeByDefault(scanner.nextLine());
+        String input = readNotBlankInput();
+        return DateTimeUtility.parseTimeByDefault(input);
+    }
+
+    private String readNotBlankInput() {
+        String input = scanner.nextLine();
+        InputValidator.validateBlank(input);
+        return input;
     }
 }
--- src/main/java/attendance/view/validator/InputValidator.java
+++ src/main/java/attendance/view/validator/InputValidator.java
@@ -1,11 +1,20 @@
 package attendance.view.validator;
 
+import attendance.exception.ExceptionMessage;
+
 public class InputValidator {
 
     public static void validateBlank(String input) {
-
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(ExceptionMessage.BLANK_INPUT.getMessage());
+        }
     }
 
     public static void validateNonNumeric(String input) {
+        try {
+            int numeric = Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMERIC_INPUT.getMessage());
+        }
     }
 }
