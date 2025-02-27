a7773301f685ea2b75a21831ff3489bca952643f
AppleMint98 <applemint9821@gmail.com>
2025-02-27 15:27:25 +0900

    feat : 시간 파싱 범위 벗어나는 경우 예외 구현
    [x] 시가 0~24, 분이 0~60 범위를 초과할 경우, 예외를 발생시킨다.

--- src/main/java/domain/CampusTime.java
+++ src/main/java/domain/CampusTime.java
@@ -1,18 +1,27 @@
 package domain;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 
 public class CampusTime {
 
     private final LocalTime time;
 
-    private CampusTime(LocalTime time) {
-        this.time = time;
+    private CampusTime(String inputTime) {
+        validateTimeRangeAndType(inputTime);
+        this.time = LocalTime.parse(inputTime);
     }
 
-    public static CampusTime from(String hourColonMinute) {
-        LocalTime time = LocalTime.parse(hourColonMinute);
-        return new CampusTime(time);
+    public static CampusTime from(String inputTime) {
+        return new CampusTime(inputTime);
+    }
+
+    private void validateTimeRangeAndType(String hourColonMinute) {
+        try {
+            LocalTime.parse(hourColonMinute);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("[ERROR] 유효한 범위의 숫자를 입력해 주세요.");
+        }
     }
 
     public int getHour() {
--- src/test/java/domain/CampusTimeTest.java
+++ src/test/java/domain/CampusTimeTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,17 @@ class CampusTimeTest {
         // then
         assertThat(campusTime.getHour()).isEqualTo(10);
         assertThat(campusTime.getMinute()).isEqualTo(30);
+    }
+
+    @Test
+    void 시간_및_분_범위를_초과하는_숫자_입력시_예외를_발생시킨다() {
+        // given
+        String givenData = "24:01";
+
+        // when // then
+        assertThatThrownBy(() -> CampusTime.from(givenData))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효한 범위의 숫자를 입력해 주세요.");
     }
 
 }
