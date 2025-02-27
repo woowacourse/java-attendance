311fb61e935cf2f8ef6ae6fcfd340f610b521326
changuii <rhljh7410@gmail.com>
2025-02-27 15:36:32 +0900

    green: 제적 위험자인지 판단하라

--- src/main/java/domain/RiskOfExpulsionStatus.java
+++ src/main/java/domain/RiskOfExpulsionStatus.java
@@ -1,8 +1,29 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public enum RiskOfExpulsionStatus {
-    NORMAL,
-    WARNING,
-    INTERVIEW,
-    EXPULSION;
+    NORMAL(0),
+    WARNING(2),
+    INTERVIEW(3),
+    EXPULSION(6);
+
+    private final int absenceCountBoundary;
+
+    RiskOfExpulsionStatus(final int absenceCountBoundary) {
+        this.absenceCountBoundary = absenceCountBoundary;
+    }
+
+    public static RiskOfExpulsionStatus calculateRiskOfExpulsionStatus(final int absenceCount) {
+        return Arrays.stream(values())
+                .sorted(Comparator.comparingInt(RiskOfExpulsionStatus::getAbsenceCountBoundary).reversed())
+                .filter(status -> absenceCount >= status.absenceCountBoundary)
+                .findFirst()
+                .orElseThrow(IllegalAccessError::new);
+    }
+
+    public int getAbsenceCountBoundary() {
+        return absenceCountBoundary;
+    }
 }
