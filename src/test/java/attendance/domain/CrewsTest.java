36f745764dcb549497caf7c0a983492c2339c115
ljhee92 <weilainib7@gmail.com>
2025-02-27 15:37:55 +0900

    test(red): 크루 리스트 추가, 중복 검증 테스트

---
+++ src/test/java/attendance/domain/CrewsTest.java
@@ -0,0 +1,33 @@
+package attendance.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("크루 리스트 테스트")
+public class CrewsTest {
+
+    @Test
+    void 크루_리스트에_크루를_추가할_수_있다() {
+        Crews crews = new Crews(new ArrayList<>());
+        Crew crew = new Crew(new Nickname("듀이"));
+        crews.add(crew);
+
+        assertThat(crews.size()).isEqualTo(1);
+    }
+
+    @Test
+    void 크루_리스트에_크루가_이미존재한다면_예외를_반환한다() {
+        Crew duei = new Crew(new Nickname("듀이"));
+        Crews crews = new Crews(List.of(duei));
+
+        assertThatThrownBy(() -> crews.add(duei))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 같은 크루를 중복하여 추가할 수 없습니다.");
+    }
+}
