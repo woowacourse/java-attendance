da406249c9d1de7882c882ef7b625ba07d96dac0
julie <7jujubuilds@gmail.com>
2025-02-27 11:48:49 +0900

    test(record): 출석 기록 생성 관련 실패 테스트 작성
    테스트 내용:
    - 출석을 기록하려는 날짜가 등교일이 아닐 경우 예외가 발생한다

--- src/test/java/AttendanceRecordTest.java
+++ src/test/java/AttendanceRecordTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AttendanceRecordTest {
     @DisplayName("LocalDateTime을 받아 기록 객체를 생성할 수 있다.")
@@ -14,5 +15,15 @@ class AttendanceRecordTest {
 
         // then
         assertThat(new AttendanceRecord(dateTime)).isInstanceOf(AttendanceRecord.class);
+    }
+
+    @DisplayName("출석을 기록하려는 날짜가 등교일이 아닐 경우 예외가 발생한다.")
+    @Test
+    void validateDateTest() {
+        // given
+        LocalDateTime dateTime = LocalDateTime.of(2024, 12, 25, 10, 0);
+
+        // then
+        assertThatThrownBy(() -> new AttendanceRecord(dateTime)).isInstanceOf(IllegalArgumentException.class);
     }
 }
