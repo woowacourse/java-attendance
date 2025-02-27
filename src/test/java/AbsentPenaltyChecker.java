0abf06580606b480f285a32f5d19a33a38acbe8c
GiSeok Kang <ime.rokz@gmail.com>
2025-02-27 19:34:26 +0900

    green: 결석 5회 초과 제적 대상 기능 구현

--- src/test/java/AbsentPenaltyChecker.java
+++ src/test/java/AbsentPenaltyChecker.java
@@ -1,9 +1,14 @@
 public class AbsentPenaltyChecker {
 
     public AbsentPenalty check(int absentCount) {
+        if (absentCount > 5) {
+            return AbsentPenalty.DISMISSAL;
+        }
+
         if (absentCount >= 3) {
             return AbsentPenalty.INTERVIEW;
         }
+
         return AbsentPenalty.WARNING;
     }
 }
