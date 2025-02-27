ba62d7372d4fbdcd5e6f64423ffe2ccbc848c5bd
kjyyjk <rlawnsdud920@gmail.com>
2025-02-28 00:48:27 +0900

    test: Attendance의 isYearMonthDate 테스트

--- src/test/java/attendance/domain/AttendanceTest.java
+++ src/test/java/attendance/domain/AttendanceTest.java
@@ -8,6 +8,8 @@ import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class AttendanceTest {
     @Nested
@@ -16,12 +18,12 @@ class AttendanceTest {
         @Test
         void should_ReturnTrue_WhenDateIsSame() {
             //given
-            LocalDate date = LocalDate.of(2024, 12, 26);
-            LocalTime time = LocalTime.of(10, 0);
-            Attendance attendance = new Attendance(date, time, ATTENDANCE);
+            LocalDate attendanceDate = LocalDate.of(2024, 12, 26);
+            LocalTime attendanceTime = LocalTime.of(10, 0);
+            Attendance attendance = new Attendance(attendanceDate, attendanceTime, ATTENDANCE);
 
             //when
-            boolean result = attendance.isDateEquals(date);
+            boolean result = attendance.isDateEquals(attendanceDate);
 
             //then
             assertThat(result).isTrue();
@@ -31,15 +33,31 @@ class AttendanceTest {
         @Test
         void should_ReturnTrue_WhenDateIsNotSame() {
             //given
-            LocalDate date = LocalDate.of(2024, 12, 26);
-            LocalTime time = LocalTime.of(10, 0);
-            Attendance attendance = new Attendance(date, time, ATTENDANCE);
+            LocalDate attendanceDate = LocalDate.of(2024, 12, 26);
+            LocalTime attendanceTime = LocalTime.of(10, 0);
+            Attendance attendance = new Attendance(attendanceDate, attendanceTime, ATTENDANCE);
 
             //when
-            boolean result = attendance.isDateEquals(date.plusDays(1));
+            boolean result = attendance.isDateEquals(attendanceDate.plusDays(1));
 
             //then
             assertThat(result).isFalse();
         }
+    }
+
+    @DisplayName("주어진_날짜가_출석_날짜와_같은_연도_월인지_여부를_반환할_수_있다")
+    @CsvSource(value = {"2024-12-02:True", "2024-11-02:False", "2025-12-22:False"}, delimiterString = ":")
+    @ParameterizedTest
+    void isYearMonthEquals(LocalDate date, boolean expected) {
+        //given
+        LocalDate attendanceDate = LocalDate.of(2024, 12, 26);
+        LocalTime attendanceTime = LocalTime.of(10, 0);
+        Attendance attendance = new Attendance(attendanceDate, attendanceTime, ATTENDANCE);
+
+        //when
+        boolean result = attendance.isYearMonthEquals(date);
+
+        //then
+        assertThat(result).isEqualTo(expected);
     }
 }
