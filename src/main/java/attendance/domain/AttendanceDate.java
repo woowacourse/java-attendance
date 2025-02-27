ace7d69a46dc5e6279bb635647267261d8a0515c
jeyong <joon6093@naver.com>
2025-02-27 17:38:26 +0900

    feat: 출석 날짜가 주말 또는 공휴일이라면 기록하지 않도록 구현

--- src/main/java/attendance/domain/AttendanceDate.java
+++ src/main/java/attendance/domain/AttendanceDate.java
@@ -1,5 +1,8 @@
 package attendance.domain;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class AttendanceDate {
 
     private final int year;
@@ -11,13 +14,14 @@ public class AttendanceDate {
         final int month,
         final int day
     ) {
-        validateDate(year, month, day);
+        validate2024December(year, month, day);
+        validateHoliday(year, month, day);
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
-    private void validateDate(
+    private void validate2024December(
         final int year,
         final int month,
         final int day
@@ -25,6 +29,42 @@ public class AttendanceDate {
         if (year != 2024 || month != 12 || day > 31) {
             throw new IllegalArgumentException("출석 날짜는 2024년 12월만 지원합니다.");
         }
+    }
+
+    private void validateHoliday(
+        final int year,
+        final int month,
+        final int day
+    ) {
+        if (isHoliday(year, month, day)) {
+            throw new IllegalArgumentException("출석 날짜는 주말 또는 공휴일은 지원하지 않습니다.");
+        }
+    }
+
+    private boolean isHoliday(
+        final int year,
+        final int month,
+        final int day
+    ) {
+        return isWeekend(year, month, day) || isPublicHoliday(month, day);
+    }
+
+    private boolean isWeekend(
+        final int year,
+        final int month,
+        final int day
+    ) {
+        return DayOfWeek.of(LocalDate.of(year, month, day)
+                .getDayOfWeek()
+                .getValue())
+            .getValue() > 5;
+    }
+
+    private boolean isPublicHoliday(
+        final int month,
+        final int day
+    ) {
+        return month == 12 && day == 25;
     }
 
     public int getYear() {
