e1b5f9313f0e1180854e8d539c66dae7c64ef546
ykmxxi <ykmxxi97@gmail.com>
2025-02-27 16:11:15 +0900

    feat: 출석 확인을 위한 등교 시간을 입력 받는 기능 구현

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -1,9 +1,11 @@
 package attendance.view;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class InputView {
 
+    private static final Pattern TIME_INPUT_PATTERN = Pattern.compile("\\d{2}:\\d{2}");
     private final Scanner scanner = new Scanner(System.in);
 
     public String readAttendanceConfirmNickname() {
@@ -13,9 +15,25 @@ public class InputView {
         return nickname;
     }
 
+    public String readAttendanceConfirmTime() {
+        System.out.println("등교 시간을 입력해 주세요.");
+        String attendanceTime = scanner.nextLine();
+        validateBlank(attendanceTime);
+        validateTimeInputPattern(attendanceTime);
+        return attendanceTime;
+    }
+
     private void validateBlank(final String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("값을 입력해 주세요.");
+        }
+    }
+
+    private void validateTimeInputPattern(final String timeInput) {
+        if (!TIME_INPUT_PATTERN.matcher(timeInput)
+                .matches()
+        ) {
+            throw new IllegalArgumentException("시간은 다음과 같이 입력해주세요. ex) 01:23");
         }
     }
 
