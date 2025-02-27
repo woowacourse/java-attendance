3ea83ef7c39963584f36d5dbca3a1d3e362bf570
(minjae8563) <(minjae8563@naver.com)>
2025-02-27 22:04:03 +0900

    test : LocalDateTime 을 AttendanceDateTime 으로 변경

--- src/test/java/model/AttendanceCalculatorTest.java
+++ src/test/java/model/AttendanceCalculatorTest.java
@@ -12,41 +12,61 @@ class AttendanceCalculatorTest {
     @Test
     @DisplayName("월요일과 시간을 받아 출석 판단하는 메서드 테스트")
     void test1() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(LocalDateTime.of(2024,12,2,13,0), LocalTime.of(13,0)))
+        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+                new AttendanceDateTime(LocalDateTime.of(2024,12,2,13,0))
+                , LocalTime.of(13,0)))
                 .isEqualTo(AttendanceStatus.ATTENDANCE);
     }
 
     @Test
     @DisplayName("월요일과 시간을 받아 결석 판단하는 메서드 테스트")
     void test2() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(LocalDateTime.of(2024,12,2,13,0), LocalTime.of(13,59)))
+        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+                new AttendanceDateTime(LocalDateTime.of(2024,12,2,13,0))
+                ,
+                        LocalTime.of(13,59)))
                 .isEqualTo(AttendanceStatus.ABSENT);
     }
 
     @Test
     @DisplayName("월요일과 시간을 받아 지각을 판단하는 메서드 테스트")
     void test3() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(LocalDateTime.of(2024,12,2,13,0), LocalTime.of(13,6)))
+        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+                new AttendanceDateTime(LocalDateTime.of(2024,12,2,13,0))
+                ,
+                        LocalTime.of(13,6)))
                 .isEqualTo(AttendanceStatus.LATE);
     }@Test
     @DisplayName("화요일과 시간을 받아 출석 판단하는 메서드 테스트")
     void test4() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(LocalDateTime.of(2024,12,3,13,0), LocalTime.of(10,0)))
+        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+                new AttendanceDateTime(LocalDateTime.of(2024,12,3,13,0))
+                ,
+                        LocalTime.of(10,0)))
                 .isEqualTo(AttendanceStatus.ATTENDANCE);
     }@Test
     @DisplayName("화요일과 시간을 받아 결석 판단하는 메서드 테스트")
     void test5() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(LocalDateTime.of(2024,12,3,13,0), LocalTime.of(13,59)))
+        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+                new AttendanceDateTime(LocalDateTime.of(2024,12,3,13,0))
+                ,
+                        LocalTime.of(13,59)))
                 .isEqualTo(AttendanceStatus.ABSENT);
     }@Test
     @DisplayName("화요일과 시간을 받아 지각을 판단하는 메서드 테스트")
     void test6() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(LocalDateTime.of(2024,12,3,13,0), LocalTime.of(10,6)))
+        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+                new AttendanceDateTime(LocalDateTime.of(2024,12,3,13,0))
+                ,
+                        LocalTime.of(10,6)))
                 .isEqualTo(AttendanceStatus.LATE);
     }@Test
     @DisplayName("0시 0분에 입실한 학생은 결석 처리하는 메서드 테스트")
     void test7() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(LocalDateTime.of(2024,12,3,13,0), LocalTime.of(0,0)))
+        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+                new AttendanceDateTime(LocalDateTime.of(2024,12,3,13,0))
+                ,
+                        LocalTime.of(0,0)))
                 .isEqualTo(AttendanceStatus.ABSENT);
     }
     @ParameterizedTest
@@ -59,7 +79,7 @@ class AttendanceCalculatorTest {
     })
     void testCheckHoliday(int year, int month, int day, boolean expected) {
         LocalDateTime date = LocalDateTime.of(year, month, day, 0, 0);
-        org.junit.jupiter.api.Assertions.assertEquals(expected, AttendanceCalculator.checkHoliday(date));
+        org.junit.jupiter.api.Assertions.assertEquals(expected, AttendanceCalculator.checkHoliday(new AttendanceDateTime(date)));
     }
 
 }
