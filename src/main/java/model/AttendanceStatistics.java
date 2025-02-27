72147743c0198fed8af9a059ad7200ca0fc54e5a
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-27 10:00:07 +0900

    green test : 크루별 특정 날짜 전까지의 패널티 여부 계산 TDD

--- src/main/java/model/AttendanceStatistics.java
+++ src/main/java/model/AttendanceStatistics.java
@@ -16,15 +16,32 @@ public class AttendanceStatistics {
 
     public Map<AttendanceStatus, Integer> calculateStatusCountUntilBefore(LocalDate limitDate) {
         List<Attendance> attendances = target.sliceByDateUntilBefore(limitDate);
-        Map<AttendanceStatus, Integer> statusStatistics = new HashMap<>();
+        Map<AttendanceStatus, Integer> statusStatistics = new HashMap<>(Map.of(
+                AttendanceStatus.NORMAL, 0,
+                AttendanceStatus.LATE, 0,
+                AttendanceStatus.ABSENCE, 0
+        ));
         for (Attendance attendance : attendances) {
             AttendanceStatus attendanceStatus = attendance.findStatus();
-            statusStatistics.merge(attendanceStatus, 1, (countTotal, addingCount) -> countTotal + 1);
+            statusStatistics.replace(attendanceStatus, statusStatistics.get(attendanceStatus) + 1);
         }
         return statusStatistics;
     }
 
     public String calculatePenaltyUntilBefore(LocalDate requestDate) {
-        return null;
+        Map<AttendanceStatus, Integer> statusStatistics = calculateStatusCountUntilBefore(requestDate);
+        int lateCount = statusStatistics.get(AttendanceStatus.LATE);
+        int absenceCount = statusStatistics.get(AttendanceStatus.ABSENCE);
+        absenceCount += (lateCount / 3);
+        if (absenceCount >= 6) {
+            return "제적";
+        }
+        if (absenceCount >= 3) {
+            return "면담";
+        }
+        if (absenceCount >= 2) {
+            return "경고";
+        }
+        return "없음";
     }
 }
