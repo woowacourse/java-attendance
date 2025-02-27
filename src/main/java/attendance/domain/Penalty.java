51cbe231a0022cd80f2580583f0fc3fa76dc5311
ljhee92 <weilainib7@gmail.com>
2025-02-27 15:01:23 +0900

    feat(green): 징계 판단 구현

---
+++ src/main/java/attendance/domain/Penalty.java
@@ -0,0 +1,41 @@
+package attendance.domain;
+
+public enum Penalty {
+    REMOVAL("제적", 5),
+    INTERVIEW("면담", 3),
+    WARNING("경고", 2),
+    NONE(null, 0)
+    ;
+
+    private static final int LATE_PER_ABSENT = 3;
+
+    private String name;
+    private int boundary;
+
+    Penalty(String name, int boundary) {
+        this.name = name;
+        this.boundary = boundary;
+    }
+
+    public static int calculateTotalAbsent(int late, int absent) {
+        return absent + (late / LATE_PER_ABSENT);
+    }
+
+    public static Penalty determine(int late, int absent) {
+        int totalAbsent = calculateTotalAbsent(late, absent);
+
+        if (totalAbsent == WARNING.boundary) {
+            return WARNING;
+        }
+
+        if (totalAbsent >= INTERVIEW.boundary && totalAbsent <= REMOVAL.boundary) {
+            return INTERVIEW;
+        }
+
+        if (totalAbsent > REMOVAL.boundary) {
+            return REMOVAL;
+        }
+
+        return NONE;
+    }
+}
