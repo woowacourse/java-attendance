80944ad80b029e2ff55f5c3ec8c773113ed68933
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-27 11:53:43 +0900

    🟢 green: 출석날짜를 캠퍼스 운영일이 아닌 날짜로 지정한 경우 예외가 발생하는 로직 작성

--- src/main/java/attendance/model/AttendanceDate.java
+++ src/main/java/attendance/model/AttendanceDate.java
@@ -1,10 +1,14 @@
 package attendance.model;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public record AttendanceDate(LocalDate date) {
 
     public AttendanceDate(int year, int month, int day) {
         this(LocalDate.of(year, month, day));
+        if (!EducationDay.isDuringEducationDay(date)) {
+            throw new IllegalArgumentException(date.format(DateTimeFormatter.ofPattern("MM월 dd일 EEE은 등교일이 아닙니다.")));
+        }
     }
 }
