a0b11e97aaf9d81c36054be580f48310bb20147b
ljhee92 <weilainib7@gmail.com>
2025-02-27 15:05:35 +0900

    refactor: 징계 테스트 중복 제거 및 테스트 케이스 추가

--- src/test/java/attendance/domain/PenaltyTest.java
+++ src/test/java/attendance/domain/PenaltyTest.java
@@ -1,37 +1,35 @@
 package attendance.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("징계 테스트")
 public class PenaltyTest {
 
-    @Test
-    void 지각이_3회이면_결석1회로_간주하여_결석횟수를_반환한다() {
-        int late = 6;
-        int absent = 1;
-
-        assertThat(Penalty.calculateTotalAbsent(late, absent)).isEqualTo(3);
+    @ParameterizedTest
+    @CsvSource(value = {"6:1:3", "3:0:1", "9:2:5"}, delimiterString = ":")
+    void 지각이_3회이면_결석1회로_간주하여_결석횟수를_반환한다(int late, int absent, int expectedAbsent) {
+        assertThat(Penalty.calculateTotalAbsent(late, absent)).isEqualTo(expectedAbsent);
     }
 
-    @Test
-    void 총결석횟수가_5회를_초과하면_제적을_반환한다() {
-        assertThat(Penalty.determine(0, 6)).isEqualTo(Penalty.REMOVAL);
+    @ParameterizedTest
+    @CsvSource(value = {"0:6", "3:5", "6:4"}, delimiterString = ":")
+    void 총결석횟수가_5회를_초과하면_제적을_반환한다(int late, int absent) {
+        assertThat(Penalty.determine(late, absent)).isEqualTo(Penalty.REMOVAL);
     }
 
-    @Test
-    void 총결석횟수가_3회이상이고_5회이하이면_면담을_반환한다() {
-        assertAll(
-                () -> assertThat(Penalty.determine(0, 3)).isEqualTo(Penalty.INTERVIEW),
-                () -> assertThat(Penalty.determine(0, 5)).isEqualTo(Penalty.INTERVIEW)
-        );
+    @ParameterizedTest
+    @CsvSource(value = {"0:3", "0:5", "3:2", "6:3"}, delimiterString = ":")
+    void 총결석횟수가_3회이상이고_5회이하이면_면담을_반환한다(int late, int absent) {
+        assertThat(Penalty.determine(late, absent)).isEqualTo(Penalty.INTERVIEW);
     }
 
-    @Test
-    void 총결석횟수가_2회라면_경고를_반환한다() {
-        assertThat(Penalty.determine(0, 2)).isEqualTo(Penalty.WARNING);
+    @ParameterizedTest
+    @CsvSource(value = {"0:2", "3:1", "6:0"}, delimiterString = ":")
+    void 총결석횟수가_2회라면_경고를_반환한다(int late, int absent) {
+        assertThat(Penalty.determine(late, absent)).isEqualTo(Penalty.WARNING);
     }
 }
