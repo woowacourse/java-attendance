e7ae4e15f83f8bb61976a2df0f008bbedf21f8f9
bowook <mbosx316@naver.com>
2025-02-27 20:21:13 +0900

    refactor(AttendanceStatus) : 메서드 분리

--- src/main/java/attendance/domain/AttendanceStatus.java
+++ src/main/java/attendance/domain/AttendanceStatus.java
@@ -5,30 +5,44 @@ import java.time.LocalDateTime;
 
 public enum AttendanceStatus {
     ATTENDANCE("출석"),
-    LATE("지각"),
+    LATE("지각" ),
     ABSENCE("결석");
 
     private final String name;
 
+    private static final int MONDAY_HOUR_LIMIT = 13;
+    private static final int GENERAL_HOUR_LIMIT = 10;
+    private static final int LATE_LIMIT = 5;
+    private static final int ABSENCE_LIMIT = 30;
     AttendanceStatus(final String name) {
         this.name = name;
     }
 
     public static AttendanceStatus fetchUserAttendanceStatus(final LocalDateTime localDateTime) {
         DayOfWeek dayOfWeek = localDateTime.getDayOfWeek();
+        int hour = localDateTime.getHour();
+        int minute = localDateTime.getMinute();
         if (dayOfWeek.getValue() == 1) {
-            if (localDateTime.getHour() > 13 || (localDateTime.getHour() == 13 && localDateTime.getMinute() > 30)) {
-                return ABSENCE;
-            }
-            if (localDateTime.getHour() == 13 && localDateTime.getMinute() > 5) {
-                return LATE;
-            }
-            return ATTENDANCE;
+            return findMondayAttendanceStatus(hour, minute);
         }
-        if (localDateTime.getHour() > 10 || (localDateTime.getHour() == 10 && localDateTime.getMinute() > 30)) {
+        return findOtherDayAttendanceStatus(hour, minute);
+    }
+
+    private static AttendanceStatus findOtherDayAttendanceStatus(int hour, int minute) {
+        if (hour > GENERAL_HOUR_LIMIT || (hour == GENERAL_HOUR_LIMIT && minute > ABSENCE_LIMIT)) {
             return ABSENCE;
         }
-        if (localDateTime.getHour() == 10 && localDateTime.getMinute() > 5) {
+        if (hour == GENERAL_HOUR_LIMIT && minute > LATE_LIMIT) {
+            return LATE;
+        }
+        return ATTENDANCE;
+    }
+
+    private static AttendanceStatus findMondayAttendanceStatus(int hour, int minute) {
+        if (hour > MONDAY_HOUR_LIMIT || (hour == MONDAY_HOUR_LIMIT && minute > ABSENCE_LIMIT)) {
+            return ABSENCE;
+        }
+        if (hour == MONDAY_HOUR_LIMIT && minute > LATE_LIMIT) {
             return LATE;
         }
         return ATTENDANCE;
