8f9b20375ad51a37f72375ae3e9db64c904a0f2b
GiSeok Kang <ime.rokz@gmail.com>
2025-02-27 18:03:02 +0900

    test: 월요일과 다른 평일 테스트 각각 @Nested로 묶음

--- src/test/java/AttendanceDateTimeCheckTest.java
+++ src/test/java/AttendanceDateTimeCheckTest.java
@@ -4,158 +4,155 @@ import java.time.format.TextStyle;
 import java.util.Locale;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class AttendanceDateTimeCheckTest {
 
-    @DisplayName("시작 시간 5분 초과 출석은 지각이다.")
-    @Test
-    void check() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
+    @Nested
+    class ExcludeMonday {
+        @DisplayName("시작 시간 5분 초과 출석은 지각이다.")
+        @Test
+        void start_time_over_five_minute() {
+            // given
             AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
 
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(10, 6));
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(10, 6));
 
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.LATE);
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.LATE);
+        }
+
+        @DisplayName("시작 시간 30분 초과 출석은 결석이다.")
+        @Test
+        void start_time_over_thirty() {
+            // given
+            AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
+
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(10, 31));
+
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.ABSENT);
+        }
+
+        @DisplayName("시작 시간으로부터 5분까지는 출석이다.")
+        @Test
+        void start_time_under_five_minute() {
+            // given
+            AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
+
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(10, 1));
+
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.ATTEND);
+        }
+
+        @DisplayName("시작 시간 이전에 출석해도 출석이다.")
+        @Test
+        void under_start_time() {
+            // given
+            AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
+
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(9, 57));
+
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.ATTEND);
+        }
+
+        @DisplayName("시작 시간이 한참지나 출석하면 결석이다.")
+        @Test
+        void start_time_too_far() {
+            // given
+            AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
+
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(12, 0));
+
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.ABSENT);
+        }
     }
 
-    @DisplayName("시작 시간 30분 초과 출석은 결석이다.")
-    @Test
-    void check2() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
+    @Nested
+    class Monday {
+        @DisplayName("월요일은 13시 5분 초과 시 지각이다.")
+        @Test
+        void start_time_over_five_minute() {
+            // given
             AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
 
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(10, 31));
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(13, 6));
 
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.ABSENT);
-    }
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.LATE);
+        }
 
-    @DisplayName("시작 시간으로부터 5분까지는 출석이다.")
-    @Test
-    void check3() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
+        @DisplayName("월요일은 13시 30분 초과 시 결석이다.")
+        @Test
+        void start_time_over_thirty() {
+            // given
             AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
 
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(10, 1));
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(13, 31));
 
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.ATTEND);
-    }
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.ABSENT);
+        }
 
-    @DisplayName("시작 시간 이전에 출석해도 출석이다.")
-    @Test
-    void check4() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
+        @DisplayName("월요일은 13시로부터 5분까지는 출석이다.")
+        @Test
+        void start_time_under_five_minute() {
+            // given
             AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
 
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(9, 57));
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(13, 5));
 
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.ATTEND);
-    }
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.ATTEND);
+        }
 
-    @DisplayName("시작 시간이 한참지나 출석하면 결석이다.")
-    @Test
-    void check5() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
+        @DisplayName("월요일은 13시 이전에 출석해도 출석이다.")
+        @Test
+        void under_start_time() {
+            // given
             AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
 
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 3), LocalTime.of(12, 0));
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(11, 31));
 
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.ABSENT);
-    }
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.ATTEND);
+        }
 
-    @DisplayName("월요일은 13시 5분 초과 시 지각이다.")
-    @Test
-    void check6() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
+        @DisplayName("월요일은 13시가 한참지나 출석하면 결석이다.")
+        @Test
+        void start_time_too_far() {
+            // given
             AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
 
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(13, 6));
+            // when
+            AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(14, 0));
 
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.LATE);
-    }
-
-    @DisplayName("월요일은 13시 30분 초과 시 결석이다.")
-    @Test
-    void check7() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
-            AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
-
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(13, 31));
-
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.ABSENT);
-    }
-
-    @DisplayName("월요일은 13시로부터 5분까지는 출석이다.")
-    @Test
-    void check8() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
-            AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
-
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(13, 5));
-
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.ATTEND);
-    }
-
-    @DisplayName("월요일은 13시 이전에 출석해도 출석이다.")
-    @Test
-    void check9() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
-            AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
-
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(11, 31));
-
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.ATTEND);
-    }
-
-    @DisplayName("월요일은 13시가 한참지나 출석하면 결석이다.")
-    @Test
-    void check10() {
-        // given
-        AttendanceTimeChecker checker = new AttendanceTimeChecker();
-            AttendanceDateTimeChecker checker = new AttendanceDateTimeChecker();
-
-        // when
-        AttendPolicy policy = checker.attendanceCheck(LocalDate.of(2024, 12, 2), LocalTime.of(14, 0));
-
-        // then
-        Assertions.assertThat(policy)
-                .isEqualTo(AttendPolicy.ABSENT);
+            // then
+            Assertions.assertThat(policy)
+                    .isEqualTo(AttendPolicy.ABSENT);
+        }
     }
 
     @DisplayName("운영시간 전에는 출석할 수 없다.")
