7535a58ba4a4613147e5c21aee5dd2ec16b0dadd
(minjae8563) <(minjae8563@naver.com)>
2025-02-27 15:14:53 +0900

    refactor : 일급 컬렉션 getter 메서드에 unmodifiableList 추가

--- src/main/java/model/Students.java
+++ src/main/java/model/Students.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Students {
@@ -22,13 +23,13 @@ public class Students {
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 학생입니다."));
     }
 
-    public void updateEveryStudentNoInformationInFile(TodayDate todayDate) {
+    public void updateMissingAttendanceRecords(TodayDate todayDate) {
         for (Student student : students) {
             student.updateNoInformationInFile(todayDate.getTodayDateTIme());
         }
     }
 
     public List<Student> getStudents() {
-        return students;
+        return Collections.unmodifiableList(students);
     }
 }
