208376c5a0a17162bf8f9559f5b1b48d29438e6d
JO-eusan <goodsan0330@gmail.com>
2025-02-27 11:23:47 +0900

    remove all of files for TDD

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,6 @@
-import controller.AttendanceController;
-import controller.FileController;
-import domain.Crews;
-
 public class Application {
 
     public static void main(String[] args) {
-        Crews crews = new Crews();
 
-        FileController fileController = new FileController(crews);
-        AttendanceController attendanceController = new AttendanceController(crews);
-
-        fileController.initializeFile("src/main/resources/attendances.csv");
-        attendanceController.run();
     }
 }
