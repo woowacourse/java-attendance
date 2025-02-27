4ac787313bb7b671d0889b59185d0c5be0a35b75
julie <7jujubuilds@gmail.com>
2025-02-27 13:57:52 +0900

    test(attendance_status): `AttendanceStatus` 테스트 수정
    - 클래스 리팩터링으로 인한 변동사항 적용
    - 테스트 메소드명 개선
    - 가독성을 위한 주석 추가

--- src/test/java/AttendanceStatusTest.java
+++ src/test/java/AttendanceStatusTest.java
@@ -9,28 +9,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 class AttendanceStatusTest {
     @DisplayName("출석 시간이 교육 시작 시간으로부터 5분 이내일 경우 출석을 반환한다.")
     @Test
-    void test() {
-        AttendanceStatus attendanceStatus = new AttendanceStatus();
+    void presentReturnTest() {
+        // given
         DayOfWeek dayOfWeek = DayOfWeek.MONDAY;
-        LocalTime time = LocalTime.of(13, 05);
-        assertThat(attendanceStatus.getStatus(dayOfWeek, time)).isEqualTo("출석");
+        LocalTime time = LocalTime.of(13, 5);
+
+        // then
+        assertThat(AttendanceStatus.getStatus(dayOfWeek, time)).isEqualTo(AttendanceStatus.PRESENT);
     }
 
     @DisplayName("출석 시간이 교육 시작 시간으로부터 5분 초과 30분 이내일 경우 지각을 반환한다.")
     @Test
-    void test2() {
-        AttendanceStatus attendanceStatus = new AttendanceStatus();
+    void tardyReturnTest() {
+        // given
         DayOfWeek dayOfWeek = DayOfWeek.MONDAY;
         LocalTime time = LocalTime.of(13, 10);
-        assertThat(attendanceStatus.getStatus(dayOfWeek, time)).isEqualTo("지각");
+
+        // then
+        assertThat(AttendanceStatus.getStatus(dayOfWeek, time)).isEqualTo(AttendanceStatus.TARDY);
     }
 
     @DisplayName("출석 시간이 교육 시작 시간으로부터 30분 초과할 경우 결석을 반환한다.")
     @Test
-    void test3() {
-        AttendanceStatus attendanceStatus = new AttendanceStatus();
+    void absentReturnTest() {
+        // given
         DayOfWeek dayOfWeek = DayOfWeek.MONDAY;
         LocalTime time = LocalTime.of(13, 35);
-        assertThat(attendanceStatus.getStatus(dayOfWeek, time)).isEqualTo("결석");
+
+        // then
+        assertThat(AttendanceStatus.getStatus(dayOfWeek, time)).isEqualTo(AttendanceStatus.ABSENT);
     }
 }
