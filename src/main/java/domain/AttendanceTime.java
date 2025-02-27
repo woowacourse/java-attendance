3bf18b90ea7be7aecb10f4054a5fa055b36fe026
jhpark1227 <junhyeok0246@naver.com>
2025-02-27 10:43:10 +0900

    feat: 다른 출석시간과 같은 날인지 비교하는 기능 구현

--- src/main/java/domain/AttendanceTime.java
+++ src/main/java/domain/AttendanceTime.java
@@ -11,6 +11,10 @@ public class AttendanceTime {
         this.time = time;
     }
 
+    public boolean isSameDay(AttendanceTime otherTime) {
+        return time.toLocalDate().equals(otherTime.time.toLocalDate());
+    }
+
     private void validateOperationTime(LocalDateTime time) {
         if (!OperationSchedule.isInOperationTime(time)) {
             throw new IllegalArgumentException("운영시간이 아니면 출석할 수 없습니다.");
