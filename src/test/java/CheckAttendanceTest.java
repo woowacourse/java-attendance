a9657271a3899f4d70e77d8262ac6369505b56e5
byesol <bsj5967@naver.com>
2025-02-27 16:56:37 +0900

    test(red) :  지각 출석 확인
    - 레드 테스트

--- src/test/java/CheckAttendanceTest.java
+++ src/test/java/CheckAttendanceTest.java
@@ -10,8 +10,15 @@ public class CheckAttendanceTest {
 
     @Test
     public void checkAttendanceStatusTest() {
+        // given
         LocalDateTime attendanceTime = LocalDateTime.of(2024, 12, 17, 10, 0);
+        LocalDateTime lateTime = LocalDateTime.of(2024, 12, 17, 10, 30);
+        // when
         AttendanceStatus attendanceStatus = AttendancePolicy.checkAttendanceStatus(attendanceTime);
+        AttendanceStatus lateStatus = AttendancePolicy.checkAttendanceStatus(attendanceTime);
+        // then
         assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ATTEND);
+        assertThat(attendanceStatus).isEqualTo(AttendanceStatus.LATE);
+
     }
 }
