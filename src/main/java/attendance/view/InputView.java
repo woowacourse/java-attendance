667ef276509eed1edbda9260c6900ecd24e7e7ab
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-27 14:46:02 +0900

    feat: inputView 구현

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -1,51 +1,50 @@
-//package attendance.view;
-//
-//import attendance.model.CustomLocalDateTime;
-//import java.time.format.DateTimeFormatter;
-//import java.util.Scanner;
-//
-//public class InputView {
-//
-//    private final DateTimeFormatter normalFormatter = DateTimeFormatter.ofPattern("MM월 dd일 EEEE");
-//    private static Scanner scanner = new Scanner(System.in);
-//
-//    public String inputCommand() {
-//        CustomLocalDateTime customLocalDateTime = new CustomLocalDateTime();
-//        System.out.println(
-//                String.format("""
-//                        오늘은 %s입니다. 기능을 선택해 주세요.
-//                        1. 출석 확인
-//                        2. 출석 수정
-//                        3. 크루별 출석 기록 확인
-//                        4. 제적 위험자 확인
-//                        Q. 종료
-//                        """, customLocalDateTime.now().format(normalFormatter))
-//        );
-//        return scanner.nextLine();
-//    }
-//
-//    public String inputCrewName() {
-//        System.out.println("닉네임을 입력해 주세요.");
-//        return scanner.nextLine();
-//    }
-//
-//    public String inputModifyAttendanceCrewName() {
-//        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
-//        return scanner.nextLine();
-//    }
-//
-//    public String inputModifyAttendanceDate() {
-//        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
-//        return scanner.nextLine();
-//    }
-//
-//    public String inputModifyAttendanceTime() {
-//        System.out.println("언제로 변경하겠습니까?");
-//        return scanner.nextLine();
-//    }
-//
-//    public String inputEntryTime() {
-//        System.out.println("등교 시간을 입력해 주세요.");
-//        return scanner.nextLine();
-//    }
-//}
+package attendance.view;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class InputView {
+
+    private final DateTimeFormatter normalFormatter = DateTimeFormatter.ofPattern("MM월 dd일 EEEE");
+    private static Scanner scanner = new Scanner(System.in);
+
+    public String inputCommand() {
+        System.out.println(
+                String.format("""
+                        오늘은 %s입니다. 기능을 선택해 주세요.
+                        1. 출석 확인
+                        2. 출석 수정
+                        3. 크루별 출석 기록 확인
+                        4. 제적 위험자 확인
+                        Q. 종료
+                        """, LocalDate.now().format(normalFormatter))
+        );
+        return scanner.nextLine();
+    }
+
+    public String inputCrewName() {
+        System.out.println("닉네임을 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public String inputModifyAttendanceCrewName() {
+        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public String inputModifyAttendanceDate() {
+        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public String inputModifyAttendanceTime() {
+        System.out.println("언제로 변경하겠습니까?");
+        return scanner.nextLine();
+    }
+
+    public String inputEntryTime() {
+        System.out.println("등교 시간을 입력해 주세요.");
+        return scanner.nextLine();
+    }
+}
