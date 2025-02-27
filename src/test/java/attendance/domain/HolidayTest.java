7196c34700e0de9bfebfee9c62d2f723c6d2c7ff
taek2222 <oot309@daum.net>
2025-02-27 10:55:31 +0900

    test(HolidayTest): 등교 일자가 아닌 경우 테스트 파라미터 추가

--- src/test/java/attendance/domain/HolidayTest.java
+++ src/test/java/attendance/domain/HolidayTest.java
@@ -1,22 +1,35 @@
 package attendance.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class HolidayTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "2024-12-01",
+            "2024-12-07",
+            "2024-12-14"
+    })
     @DisplayName("등교 일자가 아닌 경우 예외가 발생한다")
-    void shouldThrowExceptionWhenNotASchoolDay() {
-        // given
-        LocalDate attendanceDate = LocalDate.of(2024, 12, 14);
-
-        // when & then
-        Assertions.assertThatIllegalArgumentException()
+    void shouldThrowExceptionWhenNotASchoolDay(LocalDate attendanceDate) {
+        assertThatIllegalArgumentException()
                 .isThrownBy(() -> Holiday.validateAttendanceDate(attendanceDate))
-                .withMessage("[ERROR] 12월 14일 토요일은 등교일이 아닙니다.");
+                .withMessage(formatErrorMessage(attendanceDate));
+    }
+
+    private static String formatErrorMessage(final LocalDate date) {
+        return String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.",
+                date.getMonthValue(),
+                date.getDayOfMonth(),
+                date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA)
+        );
     }
 }
