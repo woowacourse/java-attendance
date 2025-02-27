1854fde218d65ca05c0472da95c9bfb0b9942a8b
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-27 16:58:08 +0900

    refactor : 날짜 관리 객체 December로 통일, 관련 내용 수정

--- src/main/java/model/Attendance.java
+++ src/main/java/model/Attendance.java
@@ -1,17 +1,40 @@
 package model;
 
+import common.Common;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Objects;
+import model.exception.CampusUnavailableException;
+import model.exception.FutureAttendanceModifyException;
+import model.exception.HolidayAttendanceException;
 
 public class Attendance {
     private final LocalDate date;
     private final LocalTime time;
 
     public Attendance(LocalDate date, LocalTime time) {
-        //TODO : 검증로직
+        validateDate(date);
+        validateTime(time);
         this.date = date;
         this.time = time; //TODO : null로 해보기
+    }
+
+    private void validateTime(LocalTime time) {
+        if (time.equals(Common.noneAttendanceTime)) {
+            return;
+        }
+        if (time.isBefore(Common.campusOpenTime) || time.isAfter(Common.campusCloseTime)) {
+            throw new CampusUnavailableException();
+        }
+    }
+
+    private void validateDate(LocalDate date) {
+        if (date.isAfter(December.now())) {
+            throw new FutureAttendanceModifyException();
+        }
+        if (December.isHolidayAt(date)) {
+            throw new HolidayAttendanceException(date);
+        }
     }
 
     public boolean isSameDateWith(LocalDate date) {
