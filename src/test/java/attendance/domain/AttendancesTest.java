dbdb74073124ea2efa0a528d23294e9fc05d9fde
taek2222 <oot309@daum.net>
2025-02-27 16:33:50 +0900

    test(AttendancesTest): 날짜, 시간으로 출석 등록 테스트 추가

---
+++ src/test/java/attendance/domain/AttendancesTest.java
@@ -0,0 +1,31 @@
+package attendance.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AttendancesTest {
+
+    @Test
+    @DisplayName("날짜와 시간으로 출석을 등록한다")
+    void 날짜와_시간으로_출석을_등록한다() {
+        // given
+        Attendance defaultAttendance = new Attendance(LocalDateTime.of(LocalDate.now(), LocalTime.MAX));
+        Attendances attendances = new Attendances(List.of(defaultAttendance));
+
+        LocalDateTime attendanceDateTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 0));
+
+        // when
+        Attendance attendance = attendances.processCheck(attendanceDateTime);
+
+        // then
+        assertThat(attendance.getDateTime())
+                .isEqualTo(attendanceDateTime);
+    }
+}
