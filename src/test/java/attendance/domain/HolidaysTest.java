1d2ee006beb2b626e9c2f0381b59b2f1bb3705d7
taek2222 <oot309@daum.net>
2025-02-27 11:52:33 +0900

    test(HolidaysTest): 휴일 테스트 변경 및 추가
    - 여러 휴일을 보관하는 형태로 s 복수형을 네이밍으로 수정했습니다.
    
    - 휴일을 추가해 검증하는 테스트도 추가했습니다.

--- src/test/java/attendance/domain/HolidaysTest.java
+++ src/test/java/attendance/domain/HolidaysTest.java
@@ -7,12 +7,30 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 import java.time.format.TextStyle;
+import java.util.List;
 import java.util.Locale;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
-class HolidayTest {
+@DisplayName("휴일 테스트")
+class HolidaysTest {
+
+    @Test
+    @DisplayName("휴일에 공휴일을 추가한다")
+    void 휴일에_공휴일을_추가한다() {
+        // given
+        LocalDate addHoliday = LocalDate.of(2024, 12, 25);
+        Holiday holiday = new Holiday(addHoliday);
+        Holidays holidays = new Holidays(List.of(holiday));
+
+        // when
+        List<Holiday> result = holidays.getHolidays();
+
+        // then
+        assertThat(result).contains(holiday);
+    }
 
     @ParameterizedTest
     @CsvSource({
@@ -23,7 +41,7 @@ class HolidayTest {
     @DisplayName("등교 일자가 아닌 경우 예외가 발생한다")
     void shouldThrowExceptionWhenNotASchoolDay(LocalDate attendanceDate) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> Holiday.validateAttendanceDate(attendanceDate))
+                .isThrownBy(() -> Holidays.validateAttendanceDate(attendanceDate))
                 .withMessage(formatErrorMessage(attendanceDate));
     }
 
@@ -35,7 +53,7 @@ class HolidayTest {
 
         // when & then
         assertThatNoException()
-                .isThrownBy(() -> Holiday.validateAttendanceDate(attendanceDate));
+                .isThrownBy(() -> Holidays.validateAttendanceDate(attendanceDate));
     }
 
     private static String formatErrorMessage(final LocalDate date) {
