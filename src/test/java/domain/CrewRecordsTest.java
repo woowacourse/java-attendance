9cd269b9bb95ff0c7de484c1529c5345bce3fe65
julie <7jujubuilds@gmail.com>
2025-02-28 16:58:40 +0900

    test(crew_records): 출석 기록 저장 관련 테스트 작성
    테스트 내용:
    - 입력 받은 닉네임과 LocalDateTime으로 새 출석 기록을 저장할 수 있다

--- src/test/java/domain/CrewRecordsTest.java
+++ src/test/java/domain/CrewRecordsTest.java
@@ -3,7 +3,10 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class CrewRecordsTest {
     @DisplayName("입력한 닉네임에 대한 출석 기록이 없을 경우 예외를 발생시킨다.")
@@ -19,5 +22,20 @@ class CrewRecordsTest {
 
         // then
         assertThatThrownBy(() -> crewRecords.validateCrew(excludedCrew)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력 받은 닉네임과 LocalDateTime으로 새 출석 기록을 저장할 수 있다.")
+    @Test
+    void test() {
+        // given
+        CrewRecords crewRecords = new CrewRecords();
+        Crew crew = new Crew("브리");
+        AttendanceRecord record = new AttendanceRecord(LocalDateTime.parse("2024-12-03T13:00"));
+
+        // when
+        crewRecords.addCrewRecords(crew, new AttendanceRecords());
+
+        // then
+        assertDoesNotThrow(() -> crewRecords.addRecord(crew, record));
     }
 }
