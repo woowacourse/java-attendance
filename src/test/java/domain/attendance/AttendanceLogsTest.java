c6ab564df59d2fa6df6db62c133d5b9b4633b344
yeonnhuu <yeonnhuu@gmail.com>
2025-02-28 14:16:15 +0900

    :bug: fix: 크루의 전체 출결 기록 관련 테스트 수정

--- src/test/java/domain/attendance/AttendanceLogsTest.java
+++ src/test/java/domain/attendance/AttendanceLogsTest.java
@@ -22,25 +22,25 @@ public class AttendanceLogsTest {
     @BeforeEach
     void setUp() {
         attendanceLogs = new AttendanceLogs();
-        attendanceLogs.registerLog(LocalDateTime.of(2025, 2, 24, 10, 0));
-        attendanceLogs.registerLog(LocalDateTime.of(2025, 2, 25, 10, 0));
+        attendanceLogs.registerLog(LocalDateTime.of(2024, 12, 10, 10, 0));
+        attendanceLogs.registerLog(LocalDateTime.of(2024, 12, 11, 10, 0));
     }
 
     @ParameterizedTest
-    @DisplayName("크루의 전체 출결 기록 상태와 경고 상태 테스트")
+    @DisplayName("크루의 전체 출결 기록 상태 테스트")
     @CsvSource({
-            "'2', 'PASS'",
-            "'2,3', 'WARNING'",
-            "'2,3,4', 'CONSULT'",
-            "'2,3,4,5,6,9', 'EXPEL'"
+            "'2', 'EXPEL'",
+            "'2,3', 'CONSULT'",
+            "'2,3,4,5,6', 'WARNING'",
+            "'2,3,4,5,6,9', 'PASS'"
 
     })
     void 크루의_전체_출결_기록_상태_테스트(String days, String expectedStatus) {
         // given
-        LocalDate todayDate = LocalDate.of(2024, 12, 10);
+        LocalDate todayDate = LocalDate.of(2024, 12, 11);
         AttendanceLogs attendanceLogs = new AttendanceLogs();
         for (String day : days.split(",")) {
-            attendanceLogs.registerLog(LocalDateTime.of(2024, 12, Integer.parseInt(day), 15, 0));
+            attendanceLogs.registerLog(LocalDateTime.of(2024, 12, Integer.parseInt(day), 10, 0));
         }
         // when & then
         assertEquals(CrewStatus.valueOf(expectedStatus), attendanceLogs.calculateCrewStatus(todayDate));
@@ -50,7 +50,7 @@ public class AttendanceLogsTest {
     @DisplayName("출석 확인 기능 테스트")
     void 출석_확인_기능_테스트() {
         // given
-        LocalDateTime attendDateTime = LocalDateTime.of(2024, 12, 26, 10, 0);
+        LocalDateTime attendDateTime = LocalDateTime.of(2024, 12, 12, 10, 0);
         // when & then
         assertTrue(attendanceLogs.registerLog(attendDateTime).isAttendDate(attendDateTime.toLocalDate()));
     }
@@ -59,7 +59,7 @@ public class AttendanceLogsTest {
     @DisplayName("출석 확인 예외 테스트")
     void 출석_확인_예외_테스트() {
         // given
-        LocalDateTime attendDateTime = LocalDateTime.of(2024, 12, 25, 10, 0);
+        LocalDateTime attendDateTime = LocalDateTime.of(2024, 12, 11, 10, 0);
         // when & then
         assertThatThrownBy(() -> attendanceLogs.registerLog(attendDateTime))
                 .isInstanceOf(ErrorException.class)
@@ -70,7 +70,7 @@ public class AttendanceLogsTest {
     @DisplayName("출석 수정 기능 테스트")
     void 출석_수정_기능_테스트() {
         // given
-        LocalDate editDate = LocalDate.of(2025, 2, 25);
+        LocalDate editDate = LocalDate.of(2024, 12, 10);
         LocalTime editTime = LocalTime.of(11, 0);
         // when & then
         assertEquals(attendanceLogs.editLog(editDate, editTime).getAttendanceTime(), editTime);
@@ -80,7 +80,7 @@ public class AttendanceLogsTest {
     @DisplayName("출석 수정 예외 테스트")
     void 출석_수정_예외_테스트() {
         // given
-        LocalDate editDate = LocalDate.of(2025, 2, 26);
+        LocalDate editDate = LocalDate.of(2024, 12, 12);
         LocalTime editTime = LocalTime.of(11, 0);
         // when & then
         assertThatThrownBy(() -> attendanceLogs.editLog(editDate, editTime))
