3bf03648ddb4d4996f23eea099d31458724d1d1d
AppleMint98 <applemint9821@gmail.com>
2025-02-28 17:56:55 +0900

    feat(AttendanceRiskLevel) : Level 계산 로직 이전

--- src/main/java/domain/AttendanceRiskLevel.java
+++ src/main/java/domain/AttendanceRiskLevel.java
@@ -8,18 +8,5 @@ public enum AttendanceRiskLevel {
     ;
 
     AttendanceRiskLevel(String status) {}
-
-    public static AttendanceRiskLevel calculateByAbsenceCount(final int count) {
-        if (count > 5) {
-            return AttendanceRiskLevel.EXPULSION;
-        }
-        if (count >= 3) {
-            return AttendanceRiskLevel.COUNSELING;
-        }
-        if (count >= 2) {
-            return AttendanceRiskLevel.WARNING;
-        }
-        return AttendanceRiskLevel.NORMAL;
-    }
 }
 
