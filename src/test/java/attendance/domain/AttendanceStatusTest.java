a00137ad7e1247896f26a2cc9a6a16fd14e2a1d4
taek2222 <oot309@daum.net>
2025-02-28 17:15:36 +0900

    test(AttendanceStatusTest): 파라미터 테스트 케이스 검증 추가

--- src/test/java/attendance/domain/AttendanceStatusTest.java
+++ src/test/java/attendance/domain/AttendanceStatusTest.java
@@ -1,36 +1,56 @@
 package attendance.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class AttendanceStatusTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource
     @DisplayName("출석 목록으로 출결 상황을 종합한다")
-    void 출석_목록으로_출결_상황을_종합한다() {
-        // given
-        LocalDate nowDate = LocalDate.now();
-        List<Attendance> attendances = List.of(
-                Attendance.fromDateTime(LocalDateTime.of(nowDate, LocalTime.of(10, 0))),
-                Attendance.fromDateTime(LocalDateTime.of(nowDate.minusDays(1), LocalTime.of(10, 0))),
-                Attendance.fromDateTime(LocalDateTime.of(nowDate.minusDays(2), LocalTime.of(10, 0)))
-        );
-
+    void 출석_목록으로_출결_상황을_종합한다(
+            List<Attendance> attendances,
+            int attendanceExcepted,
+            int tardyExcepted,
+            int absenceExcepted
+    ) {
         // when
         AttendanceStatus result = AttendanceStatus.fromAttendances(attendances);
 
         // then
         assertAll(
-                () -> assertThat(result.getStateCount(AttendanceState.ATTENDANCE)).isEqualTo(3),
-                () -> assertThat(result.getStateCount(AttendanceState.TARDY)).isEqualTo(0),
-                () -> assertThat(result.getStateCount(AttendanceState.ABSENCE)).isEqualTo(0)
+                () -> assertThat(result.getStateCount(AttendanceState.ATTENDANCE)).isEqualTo(attendanceExcepted),
+                () -> assertThat(result.getStateCount(AttendanceState.TARDY)).isEqualTo(tardyExcepted),
+                () -> assertThat(result.getStateCount(AttendanceState.ABSENCE)).isEqualTo(absenceExcepted)
+        );
+    }
+
+    private static Stream<Arguments> 출석_목록으로_출결_상황을_종합한다() {
+        LocalDate nowDate = LocalDate.now();
+        return Stream.of(
+                Arguments.of(List.of(
+                        Attendance.fromDateTime(LocalDateTime.of(nowDate, LocalTime.of(10, 0)))
+                ), 1, 0, 0),
+                Arguments.of(List.of(
+                        Attendance.fromDateTime(LocalDateTime.of(nowDate, LocalTime.of(10, 0))),
+                        Attendance.fromDateTime(LocalDateTime.of(nowDate.minusDays(1), LocalTime.of(10, 6)))
+                ), 1, 1, 0),
+                Arguments.of(List.of(
+                        Attendance.fromDateTime(LocalDateTime.of(nowDate, LocalTime.of(10, 0))),
+                        Attendance.fromDateTime(LocalDateTime.of(nowDate.minusDays(1), LocalTime.of(10, 6))),
+                        Attendance.fromDateTime(LocalDateTime.of(nowDate.minusDays(1), LocalTime.of(10, 31)))
+                ), 1, 1, 1)
         );
     }
 }
