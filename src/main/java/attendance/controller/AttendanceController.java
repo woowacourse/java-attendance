e95433c301038e086d7ec6dadf26260faf2e7c2f
rladmstn <oz33177351@gmail.com>
2025-02-28 16:39:54 +0900

    feat: 잘못된 기능 입력 시 재입력 기능 추가

--- src/main/java/attendance/controller/AttendanceController.java
+++ src/main/java/attendance/controller/AttendanceController.java
@@ -1,9 +1,11 @@
 package attendance.controller;
 
 import attendance.model.AttendanceBook;
+import attendance.model.Function;
 import attendance.util.AttendanceReader;
 import attendance.view.InputView;
 import attendance.view.OutputView;
+import java.util.function.Supplier;
 
 public class AttendanceController {
 
@@ -19,11 +21,53 @@ public class AttendanceController {
 
         AttendanceBook attendanceBook = new AttendanceBook();
         initAttendances(attendanceBook);
+        runSystem(attendanceBook);
     }
 
     private static void initAttendances(AttendanceBook attendanceBook) {
 
         AttendanceReader.initAttendances(attendanceBook);
         attendanceBook.initCrewsAbsence();
+    }
+
+    private void runSystem(final AttendanceBook attendanceBook) {
+
+        Function function = inputFunction();
+        if (function == Function.QUIT) {
+            return;
+        }
+
+        doFunction(function, attendanceBook);
+        runSystem(attendanceBook);
+    }
+
+    private void doFunction(final Function function, final AttendanceBook attendanceBook) {
+        if (function == Function.ADD_ATTENDANCE) {
+
+        }
+        if (function == Function.MODIFY_ATTENDANCE) {
+
+        }
+        if (function == Function.GET_CREW_ATTENDANCES) {
+
+        }
+        if (function == Function.GET_EXPULSION_CANDIDATES) {
+
+        }
+    }
+
+    private Function inputFunction() {
+
+        return retryInput(() -> Function.getFunction(inputView.inputFunction()));
+    }
+
+    private <T> T retryInput(Supplier<T> supplier) {
+
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return retryInput(supplier);
+        }
     }
 }
--- src/main/java/attendance/view/OutputView.java
+++ src/main/java/attendance/view/OutputView.java
@@ -1,4 +1,8 @@
 package attendance.view;
 
 public class OutputView {
+
+    public void printErrorMessage(final String message) {
+        System.out.println(message);
+    }
 }
