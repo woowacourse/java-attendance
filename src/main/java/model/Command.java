05bcd7876084496b67ddae2c57fe0b991baf36c9
2jin2.1031 <2jin2.1031@gmail.com>
2025-02-28 17:26:51 +0900

    feat: 명령어 필드 추가

---
+++ src/main/java/model/Command.java
@@ -0,0 +1,26 @@
+package model;
+
+enum Command {
+
+    ATTENDANCE_CHECK("1", "출석 확인"),
+    ATTENDANCE_CORRECTION("2", "출석 수정"),
+    CHECK_ATTENDANCE_RECORDS_BY_CREW("3", "크루별 출석 기록 확인"),
+    IDENTIFICATION_OF_THE_RISK_OF_EXPULSION("4", "제적 위험자 확인"),
+    QUIT("Q", "종료");
+
+    private final String symbol;
+    private final String displayName;
+
+    Command(final String symbol, final String displayName) {
+        this.symbol = symbol;
+        this.displayName = displayName;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
