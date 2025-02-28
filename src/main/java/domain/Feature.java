1fa82245d60b585b59e11b524e6e76c4077c671d
JO-eusan <goodsan0330@gmail.com>
2025-02-28 14:36:00 +0900

    feat: 제공하는 기능 목록 확인

--- src/main/java/domain/Feature.java
+++ src/main/java/domain/Feature.java
@@ -1,11 +1,15 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum Feature {
     ATTENDANCE_CHECK("1", "출석 확인"),
     ATTENDANCE_EDIT("2", "출석 수정"),
     CREW_RECORDS_CHECK("3", "크루별 출석 기록 확인"),
     EXPELLED_WARNING_CHECK("4", "제적 위험자 확인"),
     EXIT("Q", "종료");
+
+    private static final String NOT_PROVIDED_ERROR_MESSAGE = "제공하지 않는 기능입니다.";
 
     private final String functionNumber;
     private final String functionName;
@@ -15,7 +19,10 @@ public enum Feature {
         this.functionName = functionName;
     }
 
-    public static boolean isProvided(String input) {
-        return false;
+    public static void isProvided(String input) {
+        Arrays.stream(values())
+            .filter(feature -> feature.functionNumber.equals(input))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(NOT_PROVIDED_ERROR_MESSAGE));
     }
 }
--- src/test/java/domain/FeatureTest.java
+++ src/test/java/domain/FeatureTest.java
@@ -1,6 +1,7 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,15 +13,13 @@ public class FeatureTest {
     @ValueSource(strings = {"1", "2", "3", "4", "Q"})
     @DisplayName("제공되는 기능을 확인한다.")
     void checkProvided(String input) {
-        boolean actual = Feature.isProvided(input);
-        assertThat(actual).isTrue();
+        assertThatNoException().isThrownBy(() -> Feature.isProvided(input));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"6", "ok", "go", "y"})
     @DisplayName("제공되지 않는 기능을 확인한다.")
     void checkNotProvided(String input) {
-        boolean actual = Feature.isProvided(input);
-        assertThat(actual).isFalse();
+        assertThatThrownBy(() -> Feature.isProvided(input));
     }
 }
