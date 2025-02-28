847e162373b1f0b3a73a66c0c67d2cd6fe0adb70
jhpark1227 <junhyeok0246@naver.com>
2025-02-28 15:39:08 +0900

    test: OperationSchedule 단위테스트 작성 (특정시간과 출석인정시간의 차이를 계산한다)

--- src/test/java/OperationScheduleTest.java
+++ src/test/java/OperationScheduleTest.java
@@ -1,6 +1,7 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
 import domain.OperationSchedule;
 import java.time.LocalDateTime;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -19,6 +20,21 @@ public class OperationScheduleTest {
         //when
         boolean isInOperationTIme = OperationSchedule.isInOperationTime(time);
         //then
-        Assertions.assertThat(isInOperationTIme).isEqualTo(expected);
+        assertThat(isInOperationTIme).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3, 10, 0, 0, 0, 0",
+            "3, 10, 0, 0, 1, 1",
+            "3, 9, 59, 59, 999999999, -1"
+    })
+    void 특정시간과_출석인정시간의_차이를_계산한다(int date, int hour, int minute, int second, int nano, long expected) {
+        //given
+        LocalDateTime time = LocalDateTime.of(2024, 12, date, hour, minute, second, nano);
+        //when
+        long difference = OperationSchedule.calculateDifferenceFromStartTime(time);
+        //then
+        assertThat(difference).isEqualTo(expected);
     }
 }
