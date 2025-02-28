e9dca64fc3435876f542d7be99a06fca9eda40de
KJungW <sagakkb0707@naver.com>
2025-02-28 17:49:29 +0900

    test(crew): 크루 닉네임 전처리 (실패 테스트 작성)
    테스트내용
    - 크루를 생성할 때 입력된 닉네임의 양사이드 공백을 제거한다

--- src/test/java/attendance/domain/crew/CrewTest.java
+++ src/test/java/attendance/domain/crew/CrewTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CrewTest {
 
@@ -19,6 +20,14 @@ class CrewTest {
         assertThatCode(() -> new Crew(input))
                 .isInstanceOf(AttendanceException.class)
                 .hasMessage(ExceptionMessage.BLANK_NICKNAME.getMessage());
+    }
+
+    @DisplayName("크루를 생성할 때 입력된 닉네임의 양사이드 공백을 제거한다")
+    @ParameterizedTest
+    @ValueSource(strings = {" 쿠키", "쿠키 ", " 쿠키 "})
+    void 크루를_생성할_때_입력된_닉네임의_양사이드_공백을_제거한다(String nickname) {
+        Crew crew = new Crew(nickname);
+        assertThat(crew.getNickname()).isEqualTo("쿠키");
     }
 
     @DisplayName("닉네임이 동일한지 체크할 수 있다")
