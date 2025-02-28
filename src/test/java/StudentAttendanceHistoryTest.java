c6f167cf4b1e2ed85a02cd3dd34feb9f1ce8783e
(minjae8563) <(minjae8563@naver.com)>
2025-02-28 18:40:15 +0900

    test : 생성자에 List 넣어주는 식으로 테스트 수정

--- src/test/java/StudentAttendanceHistoryTest.java
+++ src/test/java/StudentAttendanceHistoryTest.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.util.List;
 import model.AttendanceDateTime;
 import model.StudentAttendanceHistory;
 import org.junit.jupiter.api.Assertions;
@@ -6,16 +7,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StudentAttendanceHistoryTest {
-    StudentAttendanceHistory studentAttendanceHistory = new StudentAttendanceHistory();
     AttendanceDateTime addAttendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 12, 12));
-    AttendanceDateTime deleteAttendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 12, 12));
+    AttendanceDateTime deleteAttendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 13, 12, 12));
+    StudentAttendanceHistory studentAttendanceHistory = new StudentAttendanceHistory(
+            List.of(new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 12, 12)),
+                    new AttendanceDateTime(LocalDateTime.of(2024, 12, 13, 12, 12)))
+    );
+
 
     @Test
     @DisplayName("날짜를 추가하는 메서드 테스트")
     void test1() {
         studentAttendanceHistory.addAttendanceDateTime(addAttendanceDateTime);
         Assertions.assertTrue(
-                studentAttendanceHistory.isContatinAttendanceDateTime(addAttendanceDateTime)
+                studentAttendanceHistory.isContainsAttendanceDateTime(addAttendanceDateTime)
         );
     }
 
@@ -24,7 +29,7 @@ public class StudentAttendanceHistoryTest {
     void test2() {
         studentAttendanceHistory.removeAttendanceDateTime(deleteAttendanceDateTime);
         Assertions.assertFalse(
-                studentAttendanceHistory.isContatinAttendanceDateTime(deleteAttendanceDateTime)
+                studentAttendanceHistory.isContainsAttendanceDateTime(deleteAttendanceDateTime)
         );
     }
 }
