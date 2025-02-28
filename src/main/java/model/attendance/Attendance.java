de47714d5c5efbaef7a0129c04efde350d835231
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-28 13:28:51 +0900

    refactor : Attendance 도메인 검증 로직은 도메인 내부에서 행하도록 리팩토링

--- src/main/java/model/attendance/Attendance.java
+++ src/main/java/model/attendance/Attendance.java
@@ -4,25 +4,25 @@ import common.Common;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Objects;
+import model.date.December;
 import model.exception.CampusUnavailableException;
+import model.exception.FutureAttendanceException;
+import model.exception.HolidayAttendanceException;
 
 public class Attendance {
     private final LocalDate date;
     private final LocalTime time;
 
+    public Attendance(LocalDate date) {
+        this.date = date;
+        this.time = Common.noneAttendanceTime;
+    }
+
     public Attendance(LocalDate date, LocalTime time) {
+        validateDate(date);
         validateTime(time);
         this.date = date;
         this.time = time;
-    }
-
-    private void validateTime(LocalTime time) {
-        if (time.equals(Common.noneAttendanceTime)) {
-            return;
-        }
-        if (time.isBefore(Common.campusOpenTime) || time.isAfter(Common.campusCloseTime)) {
-            throw new CampusUnavailableException();
-        }
     }
 
     public boolean isSameDateWith(LocalDate date) {
@@ -39,6 +39,24 @@ public class Attendance {
 
     public LocalTime getTime() {
         return time;
+    }
+
+    private void validateDate(LocalDate date) {
+        if (date.isAfter(December.now())) {
+            throw new FutureAttendanceException();
+        }
+        if (December.isHolidayAt(date)) {
+            throw new HolidayAttendanceException(date);
+        }
+    }
+
+    private void validateTime(LocalTime time) {
+        if (time.equals(Common.noneAttendanceTime)) {
+            return;
+        }
+        if (time.isBefore(Common.campusOpenTime) || time.isAfter(Common.campusCloseTime)) {
+            throw new CampusUnavailableException();
+        }
     }
 
     @Override
