29f6152dbbcb2f17f74e78b2cece0c08c09481c3
kimdev <kim__dev@hufs.ac.kr>
2025-02-28 21:00:55 +0900

    test: 제적 상태를 보고 해당하는 문자열을 생성하는 테스트 작성

---
+++ src/test/java/attendance/view/ExpulsionStatusTextMakerTest.java
@@ -0,0 +1,27 @@
+package attendance.view;
+
+import attendance.domain.ExpulsionStatus;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ExpulsionStatusTextMakerTest {
+
+    @CsvSource({
+            "WARNING, 경고",
+            "INTERVIEW, 면담",
+            "EXPULSION, 제적",
+    })
+    @ParameterizedTest
+    void 제적상태를_알려주면_그에_해당하는_문자열을_알려준다(ExpulsionStatus expulsionStatus, String expected) {
+        // Given
+        ExpulsionStatusTextMaker expulsionStatusTextMaker = new ExpulsionStatusTextMaker();
+
+        // When
+        String actual = expulsionStatusTextMaker.make(expulsionStatus);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+}
