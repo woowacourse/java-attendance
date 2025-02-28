357bb31d4979676a978dd78fe7e70699c66c2607
taek2222 <oot309@daum.net>
2025-03-01 01:56:07 +0900

    test(CrewAttendanceManagerTest): 초기 파일 셋팅 테스트 및 날짜 생성 구현체 적용

--- src/test/java/attendance/domain/CrewAttendanceManagerTest.java
+++ src/test/java/attendance/domain/CrewAttendanceManagerTest.java
@@ -1,5 +1,6 @@
 package attendance.domain;
 
+import attendance.util.DateGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,18 +10,27 @@ import java.time.LocalTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("출석 매니저 테스트")
 class CrewAttendanceManagerTest {
 
-    private final CrewAttendanceManager attendanceManager = new CrewAttendanceManager();
+    private final DateGenerator dateGenerator = new TestDateGenerator();
+    private final CrewAttendanceManager attendanceManager = new CrewAttendanceManager(dateGenerator);
+
+    @Test
+    @DisplayName("출석 파일을 읽어 출석 매니저를 생성한다")
+    void 출석_파일을_읽어_출석_매니저를_생성한다() {
+        assertThatNoException()
+                .isThrownBy(attendanceManager::initAttendanceFromFile);
+    }
 
     @Test
     @DisplayName("닉네임과 입력 시간으로 크루의 출석을 등록한다")
     void 닉네임과_입력_시간으로_크루의_출석을_등록한다() {
         // given
-        LocalDate nowDate = LocalDate.now();
+        LocalDate nowDate = dateGenerator.generate();
 
         Attendance attendance = Attendance.fromDateTime(LocalDateTime.of(nowDate, LocalTime.MAX));
         Attendances attendances = new Attendances(List.of(attendance));
@@ -44,7 +54,7 @@ class CrewAttendanceManagerTest {
     @DisplayName("닉네임과 입력 일자와 시간으로 크루의 출석을 수정한다")
     void 닉네임과_입력_일자와_시간으로_크루의_출석을_수정한다() {
         // given
-        LocalDate nowDate = LocalDate.now();
+        LocalDate nowDate = dateGenerator.generate();
         LocalDateTime baseDateTime = LocalDateTime.of(nowDate, LocalTime.of(18, 0));
 
         Attendance attendance = Attendance.fromDateTime(baseDateTime);
@@ -72,7 +82,7 @@ class CrewAttendanceManagerTest {
     @DisplayName("닉네임의 전날 출석 날짜, 시간과 출결 상황을 반환한다")
     void 닉네임의_전날_출석_날짜_시간과_출결_상황을_반환한다() {
         // given
-        LocalDate nowDate = LocalDate.now();
+        LocalDate nowDate = dateGenerator.generate();
 
         Attendances baseRecord = new Attendances(List.of(
                 Attendance.fromDateTime(LocalDateTime.of(nowDate, LocalTime.of(13, 0))),
@@ -103,10 +113,9 @@ class CrewAttendanceManagerTest {
     @DisplayName("정렬된 출결 상황 위험자를 반환한다")
     void 정렬된_출결_상황_위험자를_반환한다() {
         // given
-        LocalDate nowDate = LocalDate.now();
-
         List<String> nicknames = List.of("비타", "레오", "듀이", "꾹이", "몽이");
 
+        LocalDate nowDate = dateGenerator.generate();
         List<Attendances> attendances = List.of(
                 new Attendances(List.of(
                         Attendance.fromDateTime(LocalDateTime.of(nowDate, LocalTime.MAX)),
@@ -156,5 +165,13 @@ class CrewAttendanceManagerTest {
                 () -> assertThat(records.records().get(3).getNickname()).isEqualTo("레오"),
                 () -> assertThat(records.records().get(4).getNickname()).isEqualTo("비타")
         );
+    }
+
+    static class TestDateGenerator implements DateGenerator {
+
+        @Override
+        public LocalDate generate() {
+            return LocalDate.of(2025, 2, 28);
+        }
     }
 }
