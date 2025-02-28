2ecc857acc0a5d170ee24b182408cd54e55e1b7b
threepebbles <threepebbles@naver.com>
2025-02-28 19:15:58 +0900

    feat(Validator): null 검증 기능 추가

---
+++ src/main/java/util/Validator.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.Objects;
+
+public class Validator {
+    public static void validateNull(Object o) {
+        if (Objects.isNull(o)) {
+            throw new IllegalArgumentException("null이 입력되었습니다");
+        }
+    }
+}
