8f2d1167e3272556112086800b23bd8adc5a1267
RaZel713 <razel7130@gmail.com>
2025-02-28 16:00:08 +0900

    feat: InputView 와 InputPrompt 에 기능별 사용자의 입력을 받는 로직 구현

--- src/main/java/view/input/InputPrompt.java
+++ src/main/java/view/input/InputPrompt.java
@@ -1,7 +1,16 @@
 package view.input;
 
 public enum InputPrompt {
-    GUIDE_FUNCTION_INPUT("기능을 선택해 주세요.");
+    GUIDE_FUNCTION_INPUT("기능을 선택해 주세요."),
+
+    GUIDE_INPUT_NAME_TO_CHECK_ATTENDANCE("출석 확인을 위한 닉네임을 입력해 주세요."),
+    GUIDE_INPUT_TIME_TO_CHECK_ATTENDANCE("출석 확인을 위한 등교 시간을 입력해 주세요."),
+
+    GUIDE_INPUT_NAME_TO_MODIFY_ATTENDANCE("출석을 수정하려는 크루의 닉네임을 입력해 주세요."),
+    GUIDE_INPUT_DATE_TO_MODIFY_ATTENDANCE("수정하려는 날짜(일)를 입력해 주세요."),
+    GUIDE_INPUT_TIME_TO_MODIFY_ATTENDANCE("언제로 변경하겠습니까?"),
+
+    GUIDE_INPUT_NAME_TO_CHECK_ATTENDANCE_RECORD("출석 기록 확인을 위한 닉네임을 입력해 주세요.");
 
     private final String format;
 
--- src/main/java/view/input/InputView.java
+++ src/main/java/view/input/InputView.java
@@ -1,12 +1,48 @@
 package view.input;
 
 import static view.input.InputPrompt.GUIDE_FUNCTION_INPUT;
+import static view.input.InputPrompt.GUIDE_INPUT_DATE_TO_MODIFY_ATTENDANCE;
+import static view.input.InputPrompt.GUIDE_INPUT_NAME_TO_CHECK_ATTENDANCE;
+import static view.input.InputPrompt.GUIDE_INPUT_NAME_TO_CHECK_ATTENDANCE_RECORD;
+import static view.input.InputPrompt.GUIDE_INPUT_NAME_TO_MODIFY_ATTENDANCE;
+import static view.input.InputPrompt.GUIDE_INPUT_TIME_TO_CHECK_ATTENDANCE;
+import static view.input.InputPrompt.GUIDE_INPUT_TIME_TO_MODIFY_ATTENDANCE;
 
 import java.util.Scanner;
 import view.output.OutputView;
 
 public class InputView {
     private final Scanner scanner = new Scanner(System.in);
+
+    public String askNameToCheckAttendance() {
+        System.out.println(GUIDE_INPUT_NAME_TO_CHECK_ATTENDANCE);
+        return getUserInput();
+    }
+
+    public String askTimeToCheckAttendance() {
+        System.out.println(GUIDE_INPUT_TIME_TO_CHECK_ATTENDANCE);
+        return getUserInput();
+    }
+
+    public String askNameToModifyAttendance() {
+        System.out.println(GUIDE_INPUT_NAME_TO_MODIFY_ATTENDANCE);
+        return getUserInput();
+    }
+
+    public String askDateToModifyAttendance() {
+        System.out.println(GUIDE_INPUT_DATE_TO_MODIFY_ATTENDANCE);
+        return getUserInput();
+    }
+
+    public String askTimeToModifyAttendance() {
+        System.out.println(GUIDE_INPUT_TIME_TO_MODIFY_ATTENDANCE);
+        return getUserInput();
+    }
+
+    public String askNameToCheckAttendanceRecord() {
+        System.out.println(GUIDE_INPUT_NAME_TO_CHECK_ATTENDANCE_RECORD);
+        return getUserInput();
+    }
 
     public String askFunctionSelection() {
         System.out.println(GUIDE_FUNCTION_INPUT.getFormat());
