b8c418ebdefd182c399e45c0c8530862bdaea037
geonwoo <kysub99@naver.com>
2025-02-28 14:01:04 +0900

    feat(PenaltyStatus): 페널티 enum 및 상태 판단 로직 구현

---
+++ src/main/java/domain/PenaltyStatus.java
@@ -0,0 +1,34 @@
+package domain;
+
+public enum PenaltyStatus {
+    WARNING(2),
+    INTERVIEW(3),
+    EXPULSION(6),
+    NONE(0);;
+
+    private final int absenceCountLimit;
+
+    private final static int LATE_TO_ABSENCE_RATIO = 3;
+
+    PenaltyStatus(int absenceCountLimit) {
+        this.absenceCountLimit = absenceCountLimit;
+    }
+
+    public static PenaltyStatus determinePenalty(int lateCount, int absenceCount) {
+        int convertedAbsenceCount = convertLateToAbsence(lateCount, absenceCount);
+        if (convertedAbsenceCount >= EXPULSION.absenceCountLimit) {
+            return EXPULSION;
+        }
+        if (convertedAbsenceCount >= INTERVIEW.absenceCountLimit) {
+            return INTERVIEW;
+        }
+        if (convertedAbsenceCount >= WARNING.absenceCountLimit) {
+            return WARNING;
+        }
+        return NONE;
+    }
+
+    public static int convertLateToAbsence(int lateCount, int absenceCount) {
+        return absenceCount + (lateCount / LATE_TO_ABSENCE_RATIO);
+    }
+}
