344e328b94923a1e03788e23f23f7e49620860d7
jeyong <joon6093@naver.com>
2025-02-28 17:32:02 +0900

    feat: 출석부는 크루와 출석 기록을 가지도록 구현

---
+++ src/main/java/attendance/domain/AttendanceBook.java
@@ -0,0 +1,25 @@
+package attendance.domain;
+
+public class AttendanceBook {
+
+    private final Crew crew;
+    private final AttendanceRecord attendanceRecord;
+
+    public AttendanceBook(
+        Crew crew,
+        AttendanceRecord attendanceRecord
+    ) {
+        validateNotNull(crew, attendanceRecord);
+        this.crew = crew;
+        this.attendanceRecord = attendanceRecord;
+    }
+
+    private void validateNotNull(
+        final Crew crew,
+        final AttendanceRecord attendanceRecord
+    ) {
+        if (crew == null || attendanceRecord == null) {
+            throw new IllegalArgumentException("출석부는 크루와 출석 기록을 가지고 있어야 합니다.");
+        }
+    }
+}
