bb6c0f30ba451fac198d5067a33a604080703282
rladmstn <oz33177351@gmail.com>
2025-02-28 16:17:37 +0900

    feat: 출석 기록을 초기화하는 객체 생성
    파일을 읽어와 출석 기록을 초기화하는 클래스를 생성합니다.

---
+++ src/main/java/attendance/util/AttendanceReader.java
@@ -0,0 +1,35 @@
+package attendance.util;
+
+import attendance.model.AttendanceBook;
+import attendance.model.AttendanceTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class AttendanceReader {
+
+    private static final String ATTENDANCE_FILE_PATH = "src/main/resources/attendances.csv";
+    private static final String DELIMITER = ",";
+    private static final DateTimeFormatter ATTENDANCE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static void initAttendances(AttendanceBook attendanceBook) {
+
+        List<String> contents = Reader.getContents(ATTENDANCE_FILE_PATH);
+        contents.removeFirst();
+
+        for (String content : contents) {
+            String[] split = content.split(DELIMITER);
+            addAttendance(attendanceBook, split);
+        }
+    }
+
+    private static void addAttendance(AttendanceBook attendanceBook, String[] split) {
+        
+        String crewName = split[0];
+        LocalDateTime attendTime = LocalDateTime.parse(split[1], ATTENDANCE_TIME_FORMATTER);
+        int hour = attendTime.getHour();
+        int minute = attendTime.getMinute();
+
+        attendanceBook.add(crewName, new AttendanceTime(attendTime.toLocalDate(), hour, minute));
+    }
+}
