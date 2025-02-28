a15a79b360a1ef0daab2c6e62fb228ccdb4a1b1a
rladmstn <oz33177351@gmail.com>
2025-02-28 16:04:24 +0900

    feat: 기능 enum 추가

---
+++ src/main/java/attendance/model/Function.java
@@ -0,0 +1,25 @@
+package attendance.model;
+
+import java.util.Arrays;
+
+public enum Function {
+    ADD_ATTENDANCE("1"),
+    MODIFY_ATTENDANCE("2"),
+    GET_CREW_ATTENDANCES("3"),
+    GET_EXPULSION_CANDIDATES("4"),
+    QUIT("Q");
+
+    private final String value;
+
+    Function(String value) {
+        this.value = value;
+    }
+
+    public static Function getFunction(final String input) {
+
+        return Arrays.stream(Function.values())
+                .filter(function -> function.value.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 올바른 기능을 입력해 주세요."));
+    }
+}
