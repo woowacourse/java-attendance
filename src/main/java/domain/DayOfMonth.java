2131ed718d2b3bc0e586b6aa0ec2953377f9bd18
yush <yush2202@gmail.com>
2025-02-28 16:23:13 +0900

    feat: 공휴일 판단 기능 구현

--- src/main/java/domain/DayOfMonth.java
+++ src/main/java/domain/DayOfMonth.java
@@ -1,9 +1,20 @@
 package domain;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public record DayOfMonth(int dayOfMonth) {
 
     public DayOfMonth {
         isInvalidDayOfMonth(dayOfMonth);
+    }
+
+    public boolean isHoliday(List<Integer> holidays, LocalDate today) {
+        if (holidays.contains(dayOfMonth)) {
+            return true;
+        }
+        LocalDate newDay = LocalDate.of(today.getYear(), today.getMonth().getValue(), dayOfMonth);
+        return newDay.getDayOfWeek().getValue() == 6 || newDay.getDayOfWeek().getValue() == 7;
     }
 
     private void isInvalidDayOfMonth(int dayOfMonth) {
--- src/test/java/domain/DayOfMonthTest.java
+++ src/test/java/domain/DayOfMonthTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.time.LocalDate;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,5 +13,14 @@ class DayOfMonthTest {
     @ValueSource(ints = {-1, 0, 32})
     void InvalidDayOfMonthTest(int value) {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new DayOfMonth(value));
+    }
+
+    @ParameterizedTest
+    @DisplayName("해당 날짜가 공휴일인지 확인합니다.")
+    @ValueSource(ints = {1, 7, 8, 14, 15, 21, 22, 28, 25, 29})
+    void isHolidayTest(int value) {
+        List<Integer> holidays = List.of(25);
+        LocalDate today = LocalDate.of(2024, 12, 2);
+        Assertions.assertTrue(new DayOfMonth(value).isHoliday(holidays, today));
     }
 }
