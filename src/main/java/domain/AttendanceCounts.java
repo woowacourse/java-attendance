1cbbc69d8b6a7b142f247f00ed4f152617553847
AppleMint98 <applemint9821@gmail.com>
2025-02-28 18:01:29 +0900

    feat(AttendanceCounts) : 출결 횟수를 입력받아 상태를 반환하는 로직 구현

---
+++ src/main/java/domain/AttendanceCounts.java
@@ -0,0 +1,44 @@
+package domain;
+
+public class AttendanceCounts {
+
+    private final int attendanceCount;
+    private final int tardinessCount;
+    private final int absenceCount;
+
+    private AttendanceCounts(final int attendanceCount, final int tardinessCount, final int absenceCount) {
+        this.attendanceCount = attendanceCount;
+        this.tardinessCount = tardinessCount;
+        this.absenceCount = absenceCount;
+    }
+
+    public static AttendanceCounts ofStatusCounts(final int attendanceCount, final int tardinessCount, final int absenceCount) {
+        return new AttendanceCounts(attendanceCount, tardinessCount, absenceCount);
+    }
+
+    public AttendanceRiskLevel calculateAttendanceRiskLevel() {
+        int totalRiskLimitCount = absenceCount + tardinessCount / 3;
+        if (totalRiskLimitCount > 5) {
+            return AttendanceRiskLevel.EXPULSION;
+        }
+        if (totalRiskLimitCount >= 3) {
+            return AttendanceRiskLevel.COUNSELING;
+        }
+        if (totalRiskLimitCount >= 2) {
+            return AttendanceRiskLevel.WARNING;
+        }
+        return AttendanceRiskLevel.NORMAL;
+    }
+
+    public int getAttendanceCount() {
+        return attendanceCount;
+    }
+
+    public int getTardinessCount() {
+        return tardinessCount;
+    }
+
+    public int getAbsenceCount() {
+        return absenceCount;
+    }
+}
