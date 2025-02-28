5f522ae62b4050b4e6a18f30032e0c6bccc9be16
junhaa <2171326@hansung.ac.kr>
2025-02-28 09:44:27 +0900

    feat: 해당 날짜에 출석 기록이 있는 경우 예외가 발생하는 기능 구현

--- src/main/java/attendance/domain/AttendanceBook.java
+++ src/main/java/attendance/domain/AttendanceBook.java
@@ -1,21 +1,39 @@
 package attendance.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AttendanceBook {
     private final String crewName;
-    private final Map<LocalDate, LocalTime> timestamps;
+    private final Map<LocalDate, Attendance> timestamps;
 
     public AttendanceBook(String crewName) {
         this.crewName = crewName;
         this.timestamps = new HashMap<>();
     }
 
+    public LocalDateTime attend(LocalDate date, LocalTime time){
+        if(isAttendedDate(date)){
+            throw new IllegalArgumentException("해당 날짜에 출석 기록이 있습니다. 출석 수정 기능을 이용해주세요.");
+        }
+        AttendanceChecker.checkCampusOpen(date, time);
+
+        Attendance attendance = new Attendance(time, date);
+        timestamps.put(date, attendance);
+        return LocalDateTime.of(date, time);
+    }
+
     public boolean isNameMatched(String crewName) {
         return this.crewName.equals(crewName);
+    }
+
+    private boolean isAttendedDate(LocalDate attendDate){
+        return timestamps.keySet()
+                .stream()
+                .anyMatch(date -> date.equals(attendDate));
     }
 
 }
--- src/test/java/attendance/domain/AttendanceBookTest.java
+++ src/test/java/attendance/domain/AttendanceBookTest.java
@@ -1,7 +1,13 @@
 package attendance.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import attendance.domain.fixture.LocalDateTestFixture;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -37,5 +43,57 @@ public class AttendanceBookTest {
         boolean result = attendanceBook.isNameMatched(findName);
 
         assertThat(result).isFalse();
+    }
+
+    @Test
+    void 출석_저장_시_출석_시각을_반환한다(){
+        String crewName = "빙티";
+        AttendanceBook attendanceBook = new AttendanceBook(crewName);
+        LocalDate attendDate = LocalDateTestFixture.createRegularDate();
+        LocalTime attendTime = LocalTime.of(10, 0);
+
+        LocalDateTime result = attendanceBook.attend(attendDate, attendTime);
+
+        assertThat(result.getHour()).isEqualTo(10);
+        assertThat(result.getMinute()).isEqualTo(0);
+        assertThat(result.toLocalDate()).isEqualTo(attendDate);
+    }
+
+    @Test
+    void 출석_기록이_있는_경우_예외가_발생한다(){
+        String crewName = "빙티";
+        AttendanceBook attendanceBook = new AttendanceBook(crewName);
+        LocalDate attendDate = LocalDateTestFixture.createRegularDate();
+        LocalTime attendTime = LocalTime.of(10, 0);
+        attendanceBook.attend(attendDate, attendTime);
+
+        LocalTime newAttendTime = LocalTime.of(10,4);
+        assertThatThrownBy(() -> attendanceBook.attend(attendDate, newAttendTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 날짜에 출석 기록이 있습니다. 출석 수정 기능을 이용해주세요.");
+    }
+
+    @Test
+    void 캠퍼스_운영시간이_아니면_예외가_발생한다(){
+        String crewName = "빙티";
+        AttendanceBook attendanceBook = new AttendanceBook(crewName);
+        LocalDate attendDate = LocalDateTestFixture.createRegularDate();
+        LocalTime attendTime = LocalTime.of(23, 59);
+
+        assertThatThrownBy(() -> attendanceBook.attend(attendDate, attendTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("현재 캠퍼스 운영시간이 아닙니다.");
+    }
+
+    @Test
+    void 캠퍼스_등교일이_아니면_예외가_발생한다(){
+        String crewName = "빙티";
+        AttendanceBook attendanceBook = new AttendanceBook(crewName);
+        LocalDate attendDate = LocalDateTestFixture.createWeekendDate();
+        LocalTime attendTime = LocalTime.of(10, 0);
+
+        assertThatThrownBy(() -> attendanceBook.attend(attendDate, attendTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("등교일이 아닙니다.");
     }
 }
