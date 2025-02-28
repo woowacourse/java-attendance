efd4233a0491a79456cb573dbfa5358c51727488
LeeJaeHoon <dlwogns3413@naver.com>
2025-02-28 22:48:46 +0900

    test: 출석 결과 객체 비교시 전체 결석 횟수가 같다면 지각 횟수로 비교 기능 테스트 (red)

--- src/test/java/attendance/domain/AttendanceResultTest.java
+++ src/test/java/attendance/domain/AttendanceResultTest.java
@@ -73,4 +73,27 @@ public class AttendanceResultTest {
         //then
         assertThat(result).isEqualTo(-1);
     }
+
+    @Test
+    void 전체_결석_횟수가_같다면_지각_횟수로_비교한다() {
+        //given
+        AttendanceResult attendanceResult1 = new AttendanceResult(
+                "pobi",
+                Map.of(
+                        AttendanceStatus.ABSENT, 4,
+                        AttendanceStatus.LATE, 2
+                ));
+        AttendanceResult attendanceResult2 = new AttendanceResult(
+                "neo",
+                Map.of(
+                        AttendanceStatus.ABSENT, 4,
+                        AttendanceStatus.LATE, 1
+                ));
+
+        //when
+        int result = attendanceResult1.compareTo(attendanceResult2);
+
+        //then
+        assertThat(result).isEqualTo(-1);
+    }
 }
