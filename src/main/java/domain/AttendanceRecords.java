3f1fc81c45e07db2781af89ea696d9c8f7f55f68
threepebbles <threepebbles@naver.com>
2025-02-28 21:19:58 +0900

    refactor(AttendanceRecords): 출석 기록 저장소 자료구조 수정(Set -> List)

--- src/main/java/domain/AttendanceRecords.java
+++ src/main/java/domain/AttendanceRecords.java
@@ -2,12 +2,10 @@ package domain;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class AttendanceRecords {
-    private final Set<AttendanceRecord> attendanceRecords = new HashSet<>();
+    private final List<AttendanceRecord> attendanceRecords = new ArrayList<>();
 
     public AttendanceRecords() {
     }
