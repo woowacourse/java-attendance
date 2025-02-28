45524672a7671ce79e4f47350854b4f81ebd31ec
byesol <bsj5967@naver.com>
2025-02-28 11:15:24 +0900

    feat : 월요일 출석 처리

--- src/main/java/AttendancePolicy.java
+++ src/main/java/AttendancePolicy.java
@@ -6,20 +6,26 @@ import java.util.Set;
 
 public class AttendancePolicy {
     private static final LocalTime DEFAULT_START_TIME = LocalTime.of(10, 0);
+    private static final LocalTime MONDAY_START_TIME = LocalTime.of(13, 0);
     private static final int LATE_STANDARD = 5;
     private static final int ABSENT_STANDARD = 30;
     private static final LocalTime OPERATING_START_TIME = LocalTime.of(8, 0);
     private static final LocalTime OPERATING_END_TIME = LocalTime.of(23, 0);
+
 
     private static final Set<DayOfWeek> WEEKENDS = Set.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
     private static final Set<MonthDay> HOLIDAYS = Set.of(MonthDay.of(12, 25));
 
 
     public static AttendanceStatus checkAttendanceStatus(LocalDateTime attendanceTime) {
-        if (LocalTime.from(attendanceTime).isBefore(DEFAULT_START_TIME.plusMinutes(LATE_STANDARD))) {
+        LocalTime startTime = DEFAULT_START_TIME; // 기본 시작 시간
+        if (attendanceTime.getDayOfWeek() == DayOfWeek.MONDAY) {
+            startTime = MONDAY_START_TIME; // 월요일 시작 시간 적용
+        }
+        if (LocalTime.from(attendanceTime).isBefore(startTime.plusMinutes(LATE_STANDARD))) {
             return AttendanceStatus.ATTEND;
         }
-        if (LocalTime.from(attendanceTime).isBefore(DEFAULT_START_TIME.plusMinutes(ABSENT_STANDARD))) {
+        if (LocalTime.from(attendanceTime).isBefore(startTime.plusMinutes(ABSENT_STANDARD))) {
             return AttendanceStatus.LATE;
         }
         return AttendanceStatus.ABSENT;
