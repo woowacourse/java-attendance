b4d6ad81441b73f11056398c6a1cab9ccf189c46
taek2222 <oot309@daum.net>
2025-02-28 13:47:00 +0900

    feat(AttendanceRecord): 전날 출석 반환 기능 추가

---
+++ src/main/java/attendance/domain/AttendanceRecord.java
@@ -0,0 +1,19 @@
+package attendance.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class AttendanceRecord {
+
+    private final List<Attendance> record;
+
+    public AttendanceRecord(final List<Attendance> record) {
+        this.record = record;
+    }
+
+    public List<Attendance> getRecordExcludingToday() {
+        return record.stream()
+                .filter(attendance -> !attendance.isSameDate(LocalDate.now()))
+                .toList();
+    }
+}
