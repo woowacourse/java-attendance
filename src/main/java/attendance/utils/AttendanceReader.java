4492708238dcebf9ee51c27f1a8990b3799ed62c
CheChe903 <cheche903@naver.com>
2025-02-28 15:32:18 +0900

    feat(AttendanceReader): 출석부 및 출석 기록 파싱 및 정제해내는 로직 구현

---
+++ src/main/java/attendance/utils/AttendanceReader.java
@@ -0,0 +1,52 @@
+package attendance.utils;
+
+import attendance.domain.Attendance;
+import attendance.domain.Time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public final class AttendanceReader {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    private AttendanceReader() {
+    }
+
+    public static Set<Attendance> getAttendancesOnFile(List<String> attendanceContents) {
+        attendanceContents.removeFirst();
+
+        Set<Attendance> attendances = new HashSet<>();
+
+        for (String content : attendanceContents) {
+            String[] split = content.split(",");
+
+            String crewName = split[0];
+            LocalDateTime attendanceTime = LocalDateTime.parse(split[1], formatter);
+
+            attendances.add(new Attendance(crewName, new Time(attendanceTime)));
+        }
+        return attendances;
+    }
+
+    public static Set<String> getCrewNamesOnFile(String filePath) {
+
+        List<String> attendanceContents = FileReader.parseToFile(filePath);
+        attendanceContents.removeFirst();
+
+        Set<String> crewNames = new HashSet<>();
+
+        for (String content : attendanceContents) {
+            String[] split = content.split(",");
+
+            String crewName = split[0];
+
+            crewNames.add(crewName);
+
+        }
+        return crewNames;
+
+    }
+}
