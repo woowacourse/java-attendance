ce5f1d5a48829b1c3db9e891d03ae4f7e1217470
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-28 13:53:49 +0900

    GREEN) feat(domain) : Crew, Penalty
    - 각 패널티 상태값 확인
    - 지각 3회는 결석 1회로 간주

--- src/main/java/domain/Crew.java
+++ src/main/java/domain/Crew.java
@@ -1,6 +1,10 @@
 package domain;
 
 import static domain.AttendanceStatus.isWeekendOrChristmas;
+import static domain.Penalty.COUNSELLING;
+import static domain.Penalty.EXPEL;
+import static domain.Penalty.NONE;
+import static domain.Penalty.WARNING;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -62,13 +66,31 @@ public class Crew {
 
     public void fillEmptyDateWithAbsent(LocalDate localDate) {
         for (int day = 1; day <= localDate.getDayOfMonth(); day++) {
-            if (isEmptyDay(day) && !isWeekendOrChristmas(LocalDate.of(2024,12,day))) {
+            if (isEmptyDay(day) && !isWeekendOrChristmas(LocalDate.of(2024, 12, day))) {
                 addDummyAbsent(day);
             }
         }
     }
 
     private void addDummyAbsent(int day) {
-        attendanceHistory.add(new Attendance(LocalDateTime.of(2024, 12,day,22,59,59)));
+        attendanceHistory.add(new Attendance(LocalDateTime.of(2024, 12, day, 22, 59, 59)));
+    }
+
+    // 경고 대상자: 결석 2회 이상
+    //  - 면담 대상자: 결석 3회 이상
+    //  - 제적 대상자: 결석 5회 초과
+
+    public Penalty getPenalty() {
+        int penaltyStandard = getAbsentCount() + (getLateCount() / 3);
+        if (penaltyStandard > EXPEL.getCount()) {
+            return EXPEL;
+        }
+        if (penaltyStandard >= COUNSELLING.getCount()) {
+            return COUNSELLING;
+        }
+        if (penaltyStandard >= WARNING.getCount()) {
+            return WARNING;
+        }
+        return NONE;
     }
 }
---
+++ src/main/java/domain/Penalty.java
@@ -0,0 +1,25 @@
+package domain;
+
+public enum Penalty {
+    WARNING("경고", 2),
+    COUNSELLING("면담", 3),
+    EXPEL("제적", 5),
+
+    NONE("", 0)
+    ;
+
+    private final String status;
+    private final int count;
+
+    Penalty(String status, int count) {
+        this.status = status;
+        this.count = count;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public int getCount() {
+        return count;
+    }
+}
