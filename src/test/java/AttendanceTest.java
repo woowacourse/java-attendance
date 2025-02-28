4b2f7c7a47e21889b83df8f6f07fd02f542b2b0d
GiSeok Kang <ime.rokz@gmail.com>
2025-02-28 22:36:16 +0900

    red: 이미 출석한 경우 출석 시 예외 테스트 작성

--- src/test/java/AttendanceTest.java
+++ src/test/java/AttendanceTest.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,24 @@ public class AttendanceTest {
         // then
         Assertions.assertThat(attendance)
                 .isEqualTo(new Attendance(nickname, date, time));
+    }
+
+    @DisplayName("이미 출석했으면 출석할 수 없다.")
+    @Test
+    void already_attend() {
+        // given
+        List<Attendance> attendances = List.of(
+                new Attendance("율무", LocalDate.of(2024, 12, 3), LocalTime.of(10, 0)),
+                new Attendance("율무", LocalDate.of(2024, 12, 4), LocalTime.of(10, 4))
+        );
+        AttendanceBook attendanceBook = new AttendanceBook(attendances);
+        String nickname = "율무";
+        LocalDate date = LocalDate.of(2024, 12, 3);
+        LocalTime time = LocalTime.of(10, 0);
+
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> attendanceBook.check(nickname, date, time))
+                        .isInstanceOf(IllegalArgumentException.class);
     }
 }
