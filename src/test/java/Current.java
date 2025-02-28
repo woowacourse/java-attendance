6b1be352319a541bfdca3bde72e6444142c3614e
jhan0121 <jhan0121@gmail.com>
2025-02-28 18:20:06 +0900

    feat: 당일 데이터 제공 기능 추가

---
+++ src/test/java/Current.java
@@ -0,0 +1,28 @@
+import domain.OperationTime;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public enum Current {
+    TODAY(13);
+
+    private final int day;
+    private final LocalDate date;
+
+    Current(final int day) {
+        this.day = day;
+        this.date = LocalDate.of(2024, 12, this.day);
+    }
+
+    public int getYear() {
+        return this.date.getYear();
+    }
+
+    public int getMonth() {
+        return this.date.getMonth().getValue();
+    }
+
+    public int getDay() {
+        return this.day;
+    }
+}
