99203624cb1e2f171cb931df6f06556789601226
ryan-dia <cjfdnjs158@naver.com>
2025-02-28 13:41:18 +0900

    style: 필드명 value로 변경

--- src/main/java/domain/Nickname.java
+++ src/main/java/domain/Nickname.java
@@ -3,14 +3,18 @@ package domain;
 public class Nickname {
     private static final String NICKNAME_REGEX = "^[가-힣]{2,4}$";
 
-    private final String displayName;
+    private final String value;
 
-    public Nickname(final String displayName) {
-        validateNickname(displayName);
-        this.displayName = displayName;
+    public Nickname(final String nickname) {
+        validate(nickname);
+        this.value = nickname;
     }
 
-    private void validateNickname(final String nickname) {
+    public String getValue() {
+        return value;
+    }
+
+    private void validate(final String nickname) {
         if (!nickname.matches(NICKNAME_REGEX)) {
             throw new IllegalArgumentException("닉네임은 2~4글자이며 한글로만 작성이 가능합니다.");
         }
