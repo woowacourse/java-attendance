4e4859a9bf5a5ee7bc32f45bbd4cf167ab0e8278
jeyong <joon6093@naver.com>
2025-03-01 01:44:51 +0900

    chore: 출석 일시에 대한 테스트 명을 명시적으로 변경

--- src/test/java/attendance/domain/AttendanceDateTimeTest.java
+++ src/test/java/attendance/domain/AttendanceDateTimeTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-class AttendanceTest {
+class AttendanceDateTimeTest {
 
     @Nested
     class InvalidCases {
