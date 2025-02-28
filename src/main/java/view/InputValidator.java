e970da791952f26ec7b8686745d263e0eb9de602
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-28 19:58:29 +0900

    refactor(view): 선택한 메뉴 예외 리팩토링
    - 입력값 정상 검증 케이스 테스트 코드 추가
    - 상수 분리

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -1,6 +1,7 @@
 package view;
 
 import static util.Constants.ERROR_HEADER;
+import static util.Constants.TODAY;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
@@ -11,8 +12,11 @@ import java.time.format.DateTimeParseException;
 public class InputValidator {
     private static final String TIME_FORMAT_ERROR = "시간 형식(HH:mm)이 올바르지 않습니다.";
     private static final String NAME_FORMAT_ERROR = "닉네임은 2자 이상 4자 이하로 입력해주세요.";
+    private static final String INVALID_DATE_ERROR = "유효하지 않은 날짜입니다.";
+    private static final String INVALID_MENU_ERROR = "유효하지 않은 메뉴입니다.";
 
     private static final String VALID_TIME_FORMAT = "HH:mm";
+    private static final String MENU_PATTERN = "[1234Qq]";
     private static final int MIN_NAME_LENGTH = 2;
     private static final int MAX_NAME_LENGTH = 4;
 
@@ -31,18 +35,18 @@ public class InputValidator {
         }
     }
 
-    public static void validateDay(LocalDate today, String day) {
+    public static void validateDay( String day) {
         try {
             int dayNumber = Integer.parseInt(day);
-            LocalDate.of(today.getYear(), today.getMonth(), dayNumber);
+            LocalDate.of(TODAY.getYear(), TODAY.getMonth(), dayNumber);
         } catch (NumberFormatException | DateTimeException e) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다.");
+            throw new IllegalArgumentException(ERROR_HEADER + INVALID_DATE_ERROR);
         }
     }
 
     public static void validateSelectedMenu(String selectedMenu) {
-        if(!selectedMenu.matches("1234Qq")) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 메뉴입니다.");
+        if(!selectedMenu.matches(MENU_PATTERN)) {
+            throw new IllegalArgumentException(ERROR_HEADER + INVALID_MENU_ERROR);
         }
     }
 }
--- src/test/java/view/InputValidatorTest.java
+++ src/test/java/view/InputValidatorTest.java
@@ -1,14 +1,21 @@
 package view;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static util.Constants.ERROR_HEADER;
 
-import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputValidatorTest {
+    @DisplayName("선택한 메뉴가 존재할 경우 정상적으로 검증을 마친다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "4", "Q", "q"})
+    void test5(String validSelectedMenu) {
+        assertDoesNotThrow(() -> InputValidator.validateSelectedMenu(validSelectedMenu));
+    }
+
     @DisplayName("선택한 메뉴가 존재하지 않을 경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"0", "" , " "})
@@ -18,32 +25,52 @@ public class InputValidatorTest {
                 .hasMessageContaining(ERROR_HEADER);
     }
 
+    @DisplayName("시간 형식이 올바를 경우 정상적으로 검증을 마친다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"09:59", "10:00"})
+    void test6(String validTime) {
+        assertDoesNotThrow(() -> InputValidator.validateTime(validTime));
+    }
+
 
     @DisplayName("시간 형식이 올바르지 않을 경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"9:59", "", " ", "a", "1", "10:3", "10;10"})
-    void test1(String invalidTimeFormat) {
-        assertThatThrownBy(() -> InputValidator.validateTime(invalidTimeFormat))
+    void test1(String invalidTime) {
+        assertThatThrownBy(() -> InputValidator.validateTime(invalidTime))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_HEADER);
+    }
+
+    @DisplayName("닉네임 자릿수가 유효할 경우 정상적으로 검증을 마친다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"미미", "미미미미"})
+    void test7(String validName) {
+        assertDoesNotThrow(() -> InputValidator.validateName(validName));
     }
 
     @DisplayName("닉네임이 1자 이하, 5자 이상일 경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"미", "미미미미미", "", " "})
-    void test2(String invalidNameFormat) {
-        assertThatThrownBy(() -> InputValidator.validateName(invalidNameFormat))
+    void test2(String invalidName) {
+        assertThatThrownBy(() -> InputValidator.validateName(invalidName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_HEADER);
     }
 
+    @DisplayName("날짜 형식이 올바를 경우 정상적으로 검증을 마친다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "31"})
+    void test8(String validDay) {
+        assertDoesNotThrow(() -> InputValidator.validateDay(validDay));
+    }
+
+
     @DisplayName("날짜 형식이 올바르지 않을 경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "32", "0"})
-    void test3(String invalidDayFormat) {
-        LocalDate today = LocalDate.of(2024, 12, 13);
-
-        assertThatThrownBy(() -> InputValidator.validateDay(today, invalidDayFormat))
+    void test3(String invalidDay) {
+        assertThatThrownBy(() -> InputValidator.validateDay(invalidDay))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_HEADER);
     }
