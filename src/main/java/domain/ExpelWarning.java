0be4ebf9e5893049cf0df8a89066fcd3c09d7a7e
Dompoo <dlckdrms0517@gmail.com>
2025-02-28 16:02:06 +0900

    refactor: ExpelWarning의 열거형 상수가 의미있는 값을 가지도록 리팩터링

--- src/main/java/domain/ExpelWarning.java
+++ src/main/java/domain/ExpelWarning.java
@@ -1,17 +1,29 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public enum ExpelWarning {
-    정상,
-    경고,
-    면담,
-    제적,
+    정상(1, 0),
+    경고(2, 2),
+    면담(3, 3),
+    제적(4, 6),
     ;
+    
+    private final int importance;
+    private final int minExpelWarningPoint;
+    
+    ExpelWarning(final int importance, final int minExpelWarningPoint) {
+        this.importance = importance;
+        this.minExpelWarningPoint = minExpelWarningPoint;
+    }
     
     public static ExpelWarning of(final int lateCount, final int absentCount) {
         int expelWarningPoint = (lateCount / 3) + absentCount;
-        if (expelWarningPoint > 5) return 제적;
-        if (expelWarningPoint >= 3) return 면담;
-        if (expelWarningPoint == 2) return 경고;
-        return 정상;
+        
+        return Arrays.stream(ExpelWarning.values())
+                .filter(expelWarning -> expelWarning.minExpelWarningPoint <= expelWarningPoint)
+                .max(Comparator.comparingInt(expelWarning -> expelWarning.importance))
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 제적 위험치가 없습니다."));
     }
 }
