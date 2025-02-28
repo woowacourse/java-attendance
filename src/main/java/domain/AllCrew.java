1069b25f045dc69fc2bdca01aa852cb61f964003
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-28 16:24:06 +0900

    feat(domain) : AllCrew
    - Crew 리스트 클래스 생성

---
+++ src/main/java/domain/AllCrew.java
@@ -0,0 +1,68 @@
+package domain;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class AllCrew {
+    private final List<Crew> allCrew;
+
+    public AllCrew() {
+        allCrew = new ArrayList<Crew>();
+    }
+
+
+    // 리팩토링, 뷰 분리
+    public AllCrew(File file) throws FileNotFoundException {
+        allCrew = new ArrayList<>();
+        Scanner fileScanner = new Scanner(file);
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+            List<String> crewNameAndAttendanceTime = List.of(line.split("-"));
+
+            String rawDateTime = crewNameAndAttendanceTime.get(1);
+            // 예외 처리
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd HH mm");
+
+            LocalDateTime dateTime = LocalDateTime.parse(rawDateTime, formatter);
+            Attendance attendance = new Attendance(dateTime);
+
+            addCrewInfo(crewNameAndAttendanceTime.getFirst(), attendance);
+        }
+    }
+
+    public void addCrewInfo(String crewName, Attendance attendance) {
+        if (containsCrewName(crewName)) {
+            findCrewByName(crewName).addAttendance(attendance);
+            return;
+        }
+        Crew crew = new Crew(crewName);
+        crew.addAttendance(attendance);
+        allCrew.add(crew);
+    }
+
+    // 테스트 어케하지
+    public boolean containsCrewName(String crewName) {
+        return allCrew.stream().anyMatch(crew -> crew.isSameName(crewName));
+    }
+    // 테스트 어케하지
+    public Crew findCrewByName(String crewName) {
+        for (Crew crew : allCrew) {
+            if (crew.isSameName(crewName))
+                return crew;
+        }
+        throw new NoSuchElementException("[ERROR] 등록되지 않은 닉네임입니다.");
+    }
+
+    public void fillAllCrewsEmptyDateWithAbsent(LocalDate date) {
+        for (Crew crew : allCrew) {
+            crew.fillEmptyDateWithAbsent(date);
+        }
+    }
+}
