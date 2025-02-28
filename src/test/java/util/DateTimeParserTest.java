d2499a571f1b93e219b08448c129dc71b31888f8
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-28 18:22:49 +0900

    🟢green(DateTimeParser): 날짜 변환기 구현

---
+++ src/test/java/util/DateTimeParserTest.java
@@ -0,0 +1,28 @@
+package util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DateTimeParserTest {
+
+
+    @Test
+    @DisplayName("yyyy-MM-dd HH:mm 형태의 문자열을 LocalDateTime으로 파싱한다")
+    void test1() {
+        //given
+        final String dateTime = "2024-12-13 10:08";
+
+        //when
+        final LocalDateTime localDateTime = DateTimeParser.parseToLocalDateTime(dateTime);
+
+        //then
+        assertThat(localDateTime.getYear()).isEqualTo(2024);
+        assertThat(localDateTime.getMonth().getValue()).isEqualTo(12);
+        assertThat(localDateTime.getDayOfMonth()).isEqualTo(13);
+
+    }
+
+}
