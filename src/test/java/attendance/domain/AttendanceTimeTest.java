0381624ccb4d245acb4689b318f49e0eab672c62
jeyong <joon6093@naver.com>
2025-02-28 13:19:41 +0900

    test: 출석 시간의 시와 분이 범위를 벗어나지 않음을 검증

--- src/test/java/attendance/domain/AttendanceTimeTest.java
+++ src/test/java/attendance/domain/AttendanceTimeTest.java
@@ -5,11 +5,30 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AttendanceTimeTest {
 
     @Nested
     class InvalidCases {
+
+        @ParameterizedTest
+        @ValueSource(ints = {-1, 24})
+        void 출석시간은_0시부터_23시까지만_지원한다(int hour) {
+            // when & then
+            assertThatThrownBy(() -> new AttendanceTime(hour, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시간은 0 이상 23 이하여야 합니다.");
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {-1, 60})
+        void 출석시간은_0분부터_59분까지만_지원한다(int minute) {
+            // when & then
+            assertThatThrownBy(() -> new AttendanceTime(7, minute))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("분은 0 이상 59 이하여야 합니다.");
+        }
 
         @ParameterizedTest
         @CsvSource(
