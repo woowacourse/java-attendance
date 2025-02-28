df041686e51056d6ba0825da7e3b69fc05f121df
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-28 16:25:01 +0900

    feat(controller) : 컨트롤러 기능 추가
    - 출석 확인 기능 구현

--- src/main/java/controller/AttendanceSystem.java
+++ src/main/java/controller/AttendanceSystem.java
@@ -7,7 +7,12 @@ import domain.AllCrew;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import view.UserInputView;
 
 public class AttendanceSystem {
     private final LocalDate today;
@@ -16,13 +21,44 @@ public class AttendanceSystem {
     public AttendanceSystem() throws FileNotFoundException {
         today = LocalDate.now();
         allCrew = new AllCrew(new File("src/main/resources/attendances.csv"));
+        allCrew.fillAllCrewsEmptyDateWithAbsent(today);
     }
 
     public void run() throws FileNotFoundException {
-        System.out.println("오늘은 " + getFormattedDayInfo(today) + "입니다. 기능을 선택해 주세요.");
-        // 오늘 기준으로 모든 크루의 빈 결석 정보 업데이트
-        printMenu();
-        String option = new Scanner(System.in).next();
+        while (true) {
+            System.out.println("오늘은 " + getFormattedDayInfo(today) + "입니다. 기능을 선택해 주세요.");
+            printMenu();
+            String option = new Scanner(System.in).nextLine();
+            if (option.equals("1")) {
+                checkAttendance();
+            }
+            if (option.equals("2")) {
+//                modifyAttendance();
+            }
+            if (option.equals("3")) {
+//                checkCrewAttendanceHistory();
+            }
+            if (option.equals("4")) {
+//                checkPenaltyReceivedCrew();
+            }
+            if (option.equals("q")) {
+                return;
+            }
+        }
+
+
+
+    }
+
+    private void checkAttendance() {
+        String crewName = UserInputView.askCrewName();
+        if (allCrew.containsCrewName(crewName)) {
+            List<String> hourAndMinute = List.of(UserInputView.askAttendanceTime().split(":"));
+            LocalTime time = LocalTime.of(Integer.parseInt(hourAndMinute.getFirst()), Integer.parseInt(hourAndMinute.getLast()));
+            allCrew.findCrewByName(crewName).addAttendanceWithDateTime(LocalDateTime.of(today, time));
+            return;
+        }
+        throw new NoSuchElementException("[ERROR] 등록되지 않은 닉네임입니다.");
     }
 
 }
