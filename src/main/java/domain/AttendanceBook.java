31b32a41f678b58222ea21c9883dfcb83cd2ae56
ppparkta <pppppark2@gmail.com>
2025-02-28 14:14:11 +0900

    feat: 중복된 크루가 있다면 예외를 뱉는다 (그린)

--- src/main/java/domain/AttendanceBook.java
+++ src/main/java/domain/AttendanceBook.java
@@ -1,8 +1,27 @@
 package domain;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AttendanceBook {
-    public AttendanceBook(List<String> crewNames) {
+    private final Set<Crew> crews;
+
+    public AttendanceBook(List<String> crews) {
+        validate(crews);
+        this.crews = convertNameToCrew(crews);
+    }
+
+    private void validate(List<String> crewNames) {
+        List<String> distinctCrews = crewNames.stream().distinct().toList();
+        if (distinctCrews.size() != crewNames.size()) {
+            throw new IllegalArgumentException("중복된 닉네임의 크루는 존재할 수 없습니다");
+        }
+    }
+
+    private Set<Crew> convertNameToCrew(List<String> crewNames) {
+        return crewNames.stream()
+                .map(crewName -> new Crew(crewName))
+                .collect(Collectors.toSet());
     }
 }
---
+++ src/main/java/domain/Crew.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Crew {
+    private final String nickname;
+
+    public Crew(String nickname) {
+        this.nickname = nickname;
+    }
+}
