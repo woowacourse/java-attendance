5358772e77da49f71eeef10dc62d15148d249451
jhpark1227 <junhyeok0246@naver.com>
2025-02-28 14:38:56 +0900

    feat: 출석시간을 변경하는 기능 구현

--- src/main/java/domain/AttendanceTime.java
+++ src/main/java/domain/AttendanceTime.java
@@ -2,6 +2,8 @@ package domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Objects;
 
 public class AttendanceTime {
 
@@ -21,6 +23,10 @@ public class AttendanceTime {
         return time.toLocalDate().equals(day);
     }
 
+    public AttendanceTime changeTime(LocalTime newTime) {
+        return new AttendanceTime(LocalDateTime.of(time.toLocalDate(), newTime));
+    }
+
     private void validateOperationTime(LocalDateTime time) {
         if (!OperationSchedule.isInOperationTime(time)) {
             throw new IllegalArgumentException("운영시간이 아니면 출석할 수 없습니다.");
@@ -31,5 +37,22 @@ public class AttendanceTime {
         if (Holiday.isHoliday(time.toLocalDate())) {
             throw new IllegalArgumentException("주말과 공휴일에는 출석할 수 없습니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AttendanceTime that = (AttendanceTime) o;
+        return Objects.equals(time, that.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(time);
     }
 }
