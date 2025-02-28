fd6e6eeda9863267d1dcade5bcecb9e8aeba0aab
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-02-28 14:40:28 +0900

    feat: 2024-12-02가 주어졌을 때, 월요일을 반환한다

---
+++ src/main/java/attendance/AttendanceCalculator.java
@@ -0,0 +1,11 @@
+package attendance;
+
+import java.time.LocalDate;
+
+public class AttendanceCalculator {
+
+    public static String findDayOfWeek(LocalDate currentDate) {
+        return "월요일";
+    }
+
+}
---
+++ src/test/java/attendance/AttendanceCalculatorTest.java
@@ -0,0 +1,20 @@
+package attendance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AttendanceCalculatorTest {
+
+    @DisplayName("2024-12-2가 주어졌을 때, 월요일을 반환해야 한다.")
+    @Test
+    void given_2024_12_2_then_return_monday() {
+        String expectedDayOfWeek = "월요일";
+        LocalDate findDate = LocalDate.of(2024, 12, 2);
+        String dayOfWeek = AttendanceCalculator.findDayOfWeek(findDate);
+        assertThat(dayOfWeek).isEqualTo(expectedDayOfWeek);
+    }
+
+}
