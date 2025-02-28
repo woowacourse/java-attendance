607d9a8c83391badc5ba97372c2179b165a97def
taek2222 <oot309@daum.net>
2025-03-01 02:07:07 +0900

    feat(CrewAttendanceManager): 크루 파일 초기화 기능 추가

--- src/main/java/attendance/domain/CrewAttendanceManager.java
+++ src/main/java/attendance/domain/CrewAttendanceManager.java
@@ -1,7 +1,13 @@
 package attendance.domain;
 
+import attendance.util.DateGenerator;
+import attendance.util.DateTimeParser;
+import attendance.util.FileUtil;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +15,43 @@ import java.util.Map;
 public class CrewAttendanceManager {
 
     private final Map<String, Attendances> crewAttendance = new HashMap<>();
+    private final DateGenerator dateGenerator;
+    private final Holidays holidays;
+
+    public CrewAttendanceManager(DateGenerator dateGenerator) {
+        this.dateGenerator = dateGenerator;
+        holidays = new Holidays();
+    }
+
+    public void initAttendanceFromFile() {
+        List<String> readLines = FileUtil.readFile("attendances.csv");
+
+        for (String readLine : readLines) {
+            String[] split = readLine.split(",");
+
+            String nickname = split[0];
+            LocalDateTime dateTime = DateTimeParser.parseDateTime(split[1]);
+
+            if (!crewAttendance.containsKey(nickname)) {
+                LocalDate nowDate = dateGenerator.generate();
+                int lengthOfMonth = nowDate.lengthOfMonth();
+
+                List<Attendance> defaultAttendances = new ArrayList<>();
+                for (int day = 1; day < lengthOfMonth; day++) {
+                    LocalDate date = nowDate.withDayOfMonth(day);
+                    if (holidays.isHoliday(date)) {
+                        continue;
+                    }
+
+                    LocalDateTime defaultDateTime = LocalDateTime.of(date, LocalTime.MAX);
+                    defaultAttendances.add(Attendance.fromDateTime(defaultDateTime));
+                }
+                addNewCrew(nickname, new Attendances(defaultAttendances));
+            }
+
+            processAttendanceCheck(nickname, dateTime);
+        }
+    }
 
     public void addNewCrew(String nickname, Attendances attendances) {
         crewAttendance.put(nickname, attendances);
@@ -34,7 +77,7 @@ public class CrewAttendanceManager {
 
     public AttendanceRecord getAttendanceRecord(String nickname) {
         Attendances attendances = crewAttendance.get(nickname);
-        List<Attendance> excludingToday = attendances.getAttendancesBefore(LocalDate.now());
+        List<Attendance> excludingToday = attendances.getAttendancesBefore(dateGenerator.generate());
         return AttendanceRecord.fromNicknameAndAttendances(nickname, excludingToday);
     }
 
