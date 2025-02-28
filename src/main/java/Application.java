c22caea0457673850b86b337ce5500788404bbf9
RaZel713 <razel7130@gmail.com>
2025-02-28 17:08:19 +0900

    feat(Controller): 출석부 초기화 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,12 @@
 import controller.Controller;
+import service.CrewRegistrationService;
 import view.input.InputView;
 import view.output.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO : 작성할 것
-        Controller controller = new Controller(new InputView(), new OutputView());
+        Controller controller = new Controller(new InputView(), new OutputView(), new CrewRegistrationService());
         controller.start();
     }
 }
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,7 @@
 package controller;
 
+import domain.AttendanceBook;
+import service.CrewRegistrationService;
 import view.Function;
 import view.input.InputView;
 import view.output.OutputView;
@@ -7,13 +9,18 @@ import view.output.OutputView;
 public class Controller {
     private final InputView inputView;
     private final OutputView outputView;
+    private final CrewRegistrationService registration;
 
-    public Controller(InputView inputView, OutputView outputView) {
+    public Controller(InputView inputView, OutputView outputView, CrewRegistrationService registration) {
         this.outputView = outputView;
         this.inputView = inputView;
+        this.registration = registration;
     }
 
     public void start() {
+        String filePath = "src/main/java/resources/attendances.csv";
+        AttendanceBook attendanceBook = registration.registerCrews(filePath); // 초기화
+
         outputView.displayFunctionPrompt();
         Function function = Function.checkFunctionNumber(inputView.askFunctionSelection());
 
