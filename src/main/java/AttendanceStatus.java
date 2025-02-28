830b397e29df3ac18b688cc34fb43491f9eb51f0
minSsan <msp1125@hanyang.ac.kr>
2025-02-28 15:12:38 +0900

    green: 출석 상태 생성자 기능 추가

--- src/main/java/AttendanceStatus.java
+++ src/main/java/AttendanceStatus.java
@@ -1,5 +1,29 @@
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public enum AttendanceStatus {
-    ATTENDANCE,
-    LATE,
-    ABSENCE;
+    ATTENDANCE(0),
+    LATE(5),
+    ABSENCE(30),
+    ;
+
+    private final int upperBoundMinute;
+
+    AttendanceStatus(int upperBoundMinute) {
+        this.upperBoundMinute = upperBoundMinute;
+    }
+
+    public static AttendanceStatus from(LocalTime startTime, LocalTime enterTime) {
+        List<AttendanceStatus> descendingValues = Arrays.stream(values()).sorted(Comparator.reverseOrder()).toList();
+        return descendingValues.stream()
+                .filter(status -> enterTime.isAfter(status.getBoundaryTimeWith(startTime)))
+                .findFirst()
+                .orElse(ATTENDANCE);
+    }
+
+    public LocalTime getBoundaryTimeWith(LocalTime time) {
+        return time.plusMinutes(upperBoundMinute);
+    }
 }
