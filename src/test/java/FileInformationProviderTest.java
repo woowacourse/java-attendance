338204e7582e37c3dd968eac3e2b7719539d2a8a
LJM\eueo8 <eueo8259@gmail.com>
2025-02-28 14:34:03 +0900

    test : 파일 정보를 가져온 후 제대로 가져왔는지 확인 테스트

---
+++ src/test/java/FileInformationProviderTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FileInformationProviderTest {
+    @Test
+    @DisplayName("출석부 기록 전환 후 학생 수 확인 테스트")
+    void 출석부_기록_전환_후_학생_수_확인_테스트() throws IOException {
+        FileInformationProvider fileProvider = new FileInformationProvider();
+        Map<String, List<LocalDateTime>> studentRecord = fileProvider.loadStudentAttendance();
+        int expect = 5;
+        int result = studentRecord.size();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("출석부 기록 전환 후 학생 기록 확인 테스트")
+    void 출석부_기록_전환_후_학생_기록_확인_테스트() throws IOException {
+        FileInformationProvider fileProvider = new FileInformationProvider();
+        Map<String, List<LocalDateTime>> studentRecord = fileProvider.loadStudentAttendance();
+        List<LocalDateTime> attendanceRecord = studentRecord.get("빙티");
+        int expect = 2;
+        int result = attendanceRecord.size();
+        assertEquals(expect, result);
+    }
+
+}
