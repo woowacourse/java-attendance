3ac32eb9c716e0c2dbb19262dc098f37a4501e45
CheChe903 <cheche903@naver.com>
2025-02-28 14:24:03 +0900

    test(AttendanceBookTest): 특정 출석 상태의 횟수 구하기 및 제적 상태 구하는 실패하는 테스트 작성

--- src/test/java/attendance/domain/AttendanceBookTest.java
+++ src/test/java/attendance/domain/AttendanceBookTest.java
@@ -1,10 +1,13 @@
 package attendance.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,7 @@ class AttendanceBookTest {
 
     private List<String> names;
     private String name;
+    private Attendances attendances;
     private AttendanceBook attendanceBook;
 
     @BeforeEach
@@ -21,7 +25,19 @@ class AttendanceBookTest {
         name = "체체";
         names.add(name);
 
-        attendanceBook = new AttendanceBook(names);
+        Attendance attendance1 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 24, 10, 6)));
+        Attendance attendance2 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 25, 10, 6)));
+        Attendance attendance3 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 26, 10, 6)));
+        Attendance attendance4 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 27, 10, 6)));
+        Attendance attendance5 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 28, 10, 6)));
+        Attendance attendance6 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 17, 10, 31)));
+        Attendance attendance7 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 18, 10, 31)));
+        Attendance attendance8 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 19, 10, 31)));
+
+        attendances = new Attendances(
+                Set.of(attendance1, attendance2, attendance3, attendance4, attendance5, attendance6, attendance7,
+                        attendance8));
+        attendanceBook = new AttendanceBook(names, attendances);
     }
 
     @DisplayName("입력된 이름이 출석부에 없다면 예외를 발생한다.")
@@ -46,5 +62,31 @@ class AttendanceBookTest {
         assertThatCode(() -> {
             attendanceBook.hasCrew("체체");
         }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("특정 크루의 특정 출석 상태의 횟수를 구한다.")
+    @Test
+    void 특정_크루의_특정_출석_상태의_횟수를_구한다() {
+
+        // given
+
+        // when
+        long totalCount = attendanceBook.getCountAcademicStatus(AttendanceStatus.LATE, "체체", 2025, 2, 10, 0);
+
+        // then
+        assertThat(totalCount).isEqualTo(5);
+    }
+
+    @DisplayName("특정 크루의 제적 상태를 구한다.")
+    @Test
+    void 특정_크루의_제적_상태를_구한다() {
+
+        // given
+
+        // when
+        AcademicStatus academicStatus = attendanceBook.getAcademicStatusByCrewName("체체", 2025, 2, 10, 0);
+
+        // then
+        assertThat(academicStatus).isEqualTo(AcademicStatus.INTERVIEW);
     }
 }
