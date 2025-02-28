558eaf4c3d3b0d5e43fac01f2fc7954e23194a98
(minjae8563) <(minjae8563@naver.com)>
2025-02-28 18:09:33 +0900

    test : 테스트 코드 수정

--- src/test/java/AttendanceDateTimeTest.java
+++ src/test/java/AttendanceDateTimeTest.java
@@ -1,7 +1,5 @@
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import model.AttendanceDateTime;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -12,30 +10,26 @@ public class AttendanceDateTimeTest {
     @Test
     @DisplayName("AttendanceTime 을 통해 오늘이 주말인지 공휴일인지 확인하는 메서드 테스트")
     void test1() {
-        AttendanceDateTime saturdayAttendanceDateTime = new AttendanceDateTime();
-        AttendanceDateTime sundayAttendanceDateTime = new AttendanceDateTime();
-        AttendanceDateTime christmasAttendanceDateTime = new AttendanceDateTime();
-        Assertions.assertTrue(saturdayAttendanceDateTime.toLocalDate().getDayOfWeek().equals.(DayOfWeek.SUNDAY));
-        Assertions.assertTrue(sundayAttendanceDateTime.toLocalDate().getDayOfWeek().equals.(DayOfWeek.SATURDAY));
-        Assertions.assertTrue(christmasAttendanceDateTime.toLocalDate().getDayOfMonth() == 25);
+        AttendanceDateTime saturdayAttendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 7, 0, 0));
+        AttendanceDateTime sundayAttendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 8, 0, 0));
+        AttendanceDateTime christmasAttendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 25, 0, 0));
+        Assertions.assertTrue(saturdayAttendanceDateTime.isWeekend());
+        Assertions.assertTrue(sundayAttendanceDateTime.isWeekend());
+        Assertions.assertTrue(christmasAttendanceDateTime.isChristmas());
     }
 
     @Test
     @DisplayName("AttendanceTime 멤버 변수를 LocalDate 로 바꾸는 메서드 테스트")
     void test2() {
         AttendanceDateTime attendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 12, 0));
-        Assertions.assertTrue(
-                attendanceDateTime.toLocalDate().equals(LocalTime.of(12, 0))
-        );
+        Assertions.assertEquals(attendanceDateTime.toLocalDate(), LocalDate.of(2024, 12, 12));
     }
 
     @Test
     @DisplayName("AttendanceTime 멤버 변수를 LocalTime 으로 바꾸는 메서드 테스트")
     void test3() {
         AttendanceDateTime attendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 12, 0));
-        Assertions.assertTrue(
-                attendanceDateTime.toLocalDate().equals(LocalDate.of(2024, 12, 12))
-        );
+        Assertions.assertEquals(attendanceDateTime.toLocalDate(), LocalDate.of(2024, 12, 12));
     }
 
     @Test
@@ -43,16 +37,16 @@ public class AttendanceDateTimeTest {
     void test4() {
         AttendanceDateTime attendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 7, 0));
         Assertions.assertTrue(
-                attendanceDateTime.toLocalTime().isNotOpeningTime()
+                attendanceDateTime.isNotOpeningTime()
         );
     }
 
     @Test
     @DisplayName("AttendanceTime 의 멤버 변수를 LocalTime 으로 바꾸고, 08:00 ~ 23:00 사이에 있는지 확인하는 메서드")
-    void test4() {
+    void test5() {
         AttendanceDateTime attendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 23, 10));
         Assertions.assertTrue(
-                attendanceDateTime.toLocalTime().isNotOpeningTime()
+                attendanceDateTime.isNotOpeningTime()
         );
     }
 }
