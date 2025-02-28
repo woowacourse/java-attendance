8f6c9cd2d30b2a9f62c7cba44446b1a014da34ce
jhpark1227 <junhyeok0246@naver.com>
2025-02-28 15:39:47 +0900

    feat: 특정시간과 출석인정시간의 차이를 계산하는 기능 구현

--- src/main/java/domain/OperationSchedule.java
+++ src/main/java/domain/OperationSchedule.java
@@ -6,23 +6,25 @@ import java.time.LocalTime;
 
 public enum OperationSchedule {
 
-    MONDAY(DayOfWeek.MONDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
-    TUESDAY(DayOfWeek.TUESDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
-    WEDNESDAY(DayOfWeek.WEDNESDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
-    THURSDAY(DayOfWeek.THURSDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
-    FRIDAY(DayOfWeek.FRIDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
-    SATURDAY(DayOfWeek.SATURDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
-    SUNDAY(DayOfWeek.SUNDAY, LocalTime.of(8, 0), LocalTime.of(23, 0)),
-    NONE(null, LocalTime.of(0, 0), LocalTime.of(0, 0));;
+    MONDAY(DayOfWeek.MONDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(13, 0)),
+    TUESDAY(DayOfWeek.TUESDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    WEDNESDAY(DayOfWeek.WEDNESDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    THURSDAY(DayOfWeek.THURSDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    FRIDAY(DayOfWeek.FRIDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    SATURDAY(DayOfWeek.SATURDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    SUNDAY(DayOfWeek.SUNDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    NONE(null, LocalTime.of(0, 0), LocalTime.of(0, 0), LocalTime.of(10, 0));
 
     private final DayOfWeek dayOfWeek;
     private final LocalTime startTime;
     private final LocalTime endTime;
+    private final LocalTime attendanceStandard;
 
-    OperationSchedule(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+    OperationSchedule(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime, LocalTime attendanceStandard) {
         this.dayOfWeek = dayOfWeek;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.attendanceStandard = attendanceStandard;
     }
 
     public static boolean isInOperationTime(LocalDateTime datetime) {
@@ -38,5 +40,10 @@ public enum OperationSchedule {
             }
         }
         return NONE;
+    }
+
+    public static long calculateDifferenceFromStartTime(LocalDateTime time) {
+        OperationSchedule schedule = findSchedule(time);
+        return time.toLocalTime().toNanoOfDay() - schedule.attendanceStandard.toNanoOfDay();
     }
 }
