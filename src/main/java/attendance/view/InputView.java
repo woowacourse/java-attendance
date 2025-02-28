996289c3231f0bd94a716dcb50534bd8712a938e
rladmstn <oz33177351@gmail.com>
2025-02-28 17:02:16 +0900

    refactor: 기능 목록 출력 시 마지막 개행 문자 제거

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
         final String day = LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN);
         System.out.printf("오늘은 %d월 %d일 %s입니다. 기능을 선택해 주세요.\n", month, date, day);
 
-        System.out.println("""
+        System.out.print("""
                 1. 출석 확인
                 2. 출석 수정
                 3. 크루별 출석 기록 확인
