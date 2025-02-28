c9b199b3316a1015230f514a4a42ce8397ccd091
KJungW <sagakkb0707@naver.com>
2025-02-28 19:37:58 +0900

    test(record): 변수명 수정
    구현내용
    - 가독성을 위해 상수명과 변수명 수정

--- src/test/java/attendance/domain/checker/AttendanceTypeTest.java
+++ src/test/java/attendance/domain/checker/AttendanceTypeTest.java
@@ -11,24 +11,24 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class AttendanceTypeTest {
 
-    final static LocalTime START_TIME = LocalTime.of(10, 0);
+    final static LocalTime EDUCATION_START_TIME = LocalTime.of(10, 0);
 
     @DisplayName("기준을 넘긴 시간에 맞춰 출석 상태를 판별할 수 있다")
     @ParameterizedTest
     @MethodSource
-    void 기준을_넘긴_시간에_맞춰_출석_상태를_판별할_수_있다(LocalTime time, AttendanceType expectedType) {
-        AttendanceType actualType = AttendanceType.parse(START_TIME, time);
+    void 기준을_넘긴_시간에_맞춰_출석_상태를_판별할_수_있다(LocalTime arrivalTime, AttendanceType expectedType) {
+        AttendanceType actualType = AttendanceType.parse(EDUCATION_START_TIME, arrivalTime);
         assertThat(actualType).isEqualTo(expectedType);
     }
 
     static Stream<Arguments> 기준을_넘긴_시간에_맞춰_출석_상태를_판별할_수_있다() {
         return Stream.of(
-                Arguments.of(START_TIME, AttendanceType.ATTENDANCE),
-                Arguments.of(START_TIME.plusMinutes(4).plusSeconds(59), AttendanceType.ATTENDANCE),
-                Arguments.of(START_TIME.plusMinutes(5), AttendanceType.LATE),
-                Arguments.of(START_TIME.plusMinutes(29).plusSeconds(59), AttendanceType.LATE),
-                Arguments.of(START_TIME.plusMinutes(30), AttendanceType.ABSENCE),
-                Arguments.of(START_TIME.plusMinutes(30).plusSeconds(1), AttendanceType.ABSENCE)
+                Arguments.of(EDUCATION_START_TIME, AttendanceType.ATTENDANCE),
+                Arguments.of(EDUCATION_START_TIME.plusMinutes(4).plusSeconds(59), AttendanceType.ATTENDANCE),
+                Arguments.of(EDUCATION_START_TIME.plusMinutes(5), AttendanceType.LATE),
+                Arguments.of(EDUCATION_START_TIME.plusMinutes(29).plusSeconds(59), AttendanceType.LATE),
+                Arguments.of(EDUCATION_START_TIME.plusMinutes(30), AttendanceType.ABSENCE),
+                Arguments.of(EDUCATION_START_TIME.plusMinutes(30).plusSeconds(1), AttendanceType.ABSENCE)
         );
     }
 }
