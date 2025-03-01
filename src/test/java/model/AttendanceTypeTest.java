e7a8e5b3504d07037e506f80257863129cf8d3b4
soeun2537 <soeun2537@gmail.com>
2025-03-01 22:02:07 +0900

    test: 출석 타입 테스트 세분화

--- src/test/java/model/AttendanceTypeTest.java
+++ src/test/java/model/AttendanceTypeTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 class AttendanceTypeTest {
 
     @Test
-    @DisplayName("출석 타입을 반환한다.")
+    @DisplayName("금요일 출석 타입을 반환한다.")
     void test1() {
         // given
         LocalDate localDate = LocalDate.of(2025, 2, 28);
@@ -24,7 +24,7 @@ class AttendanceTypeTest {
     }
 
     @Test
-    @DisplayName("지각 타입을 반환한다.")
+    @DisplayName("금요일 지각 타입을 반환한다.")
     void test2() {
         // given
         LocalDate localDate = LocalDate.of(2025, 2, 28);
@@ -38,11 +38,53 @@ class AttendanceTypeTest {
     }
 
     @Test
-    @DisplayName("결석 타입을 반환한다.")
+    @DisplayName("금요일 결석 타입을 반환한다.")
     void test3() {
         // given
         LocalDate localDate = LocalDate.of(2025, 2, 28);
         LocalTime localTime = LocalTime.of(10, 31);
+
+        // when
+        AttendanceType result = AttendanceType.calculate(localDate, localTime);
+
+        // then
+        assertThat(result).isEqualTo(AttendanceType.ABSENCE);
+    }
+
+    @Test
+    @DisplayName("월요일 출석 타입을 반환한다.")
+    void test4() {
+        // given
+        LocalDate localDate = LocalDate.of(2025, 2, 24);
+        LocalTime localTime = LocalTime.of(12, 30);
+
+        // when
+        AttendanceType result = AttendanceType.calculate(localDate, localTime);
+
+        // then
+        assertThat(result).isEqualTo(AttendanceType.SUCCESS);
+    }
+
+    @Test
+    @DisplayName("월요일 지각 타입을 반환한다.")
+    void test5() {
+        // given
+        LocalDate localDate = LocalDate.of(2025, 2, 24);
+        LocalTime localTime = LocalTime.of(13, 6);
+
+        // when
+        AttendanceType result = AttendanceType.calculate(localDate, localTime);
+
+        // then
+        assertThat(result).isEqualTo(AttendanceType.BE_LATE);
+    }
+
+    @Test
+    @DisplayName("월요일 결석 타입을 반환한다.")
+    void test6() {
+        // given
+        LocalDate localDate = LocalDate.of(2025, 2, 24);
+        LocalTime localTime = LocalTime.of(13, 31);
 
         // when
         AttendanceType result = AttendanceType.calculate(localDate, localTime);
