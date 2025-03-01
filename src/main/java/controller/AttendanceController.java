b9de2f1a1e6e7c2db7c9677ef877d6b74a242c6e
BWY <quantumjump10@gmail.com>
2025-03-01 15:30:28 +0900

    feat: AttendanceController 추가

---
+++ src/main/java/controller/AttendanceController.java
@@ -0,0 +1,91 @@
+package controller;
+
+import domain.Attendance;
+import domain.Attendances;
+import domain.Command;
+import domain.constant.StandardDate;
+import view.InputView;
+import view.OutputView;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Map;
+
+public class AttendanceController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Attendances attendances;
+    private final Map<Command, Runnable> commandOption = Map.of(
+            Command.ATTENDANCE_REGISTER, this::registerAttendance,
+            Command.ATTENDANCE_UPDATE, this::updateAttendance,
+            Command.ATTENDANCE_CHECK, this::identifyAttendanceLogsWithCrew,
+            Command.PENALTY_CHECK, this::identifyPenaltyCrews
+    );
+
+    public AttendanceController(InputView inputView, OutputView outputView, Attendances attendances) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.attendances = attendances;
+    }
+
+    public void run() {
+        String command = "";
+        while (!command.equals("Q")) {
+            outputView.printOptionMessage();
+            command = inputView.readOptionNumber();
+            runCommand(command);
+        }
+    }
+
+    public void runCommand(String inputOption) {
+        Command command = Command.identify(inputOption);
+        commandOption.get(command).run();
+    }
+
+    private void registerAttendance() {
+        String nickname = inputView.readNickname();
+        LocalTime attendanceTime = inputView.readAttendanceTime();
+        LocalDateTime localDateTime = LocalDateTime.of(StandardDate.TODAY, attendanceTime);
+        attendances.addAttendanceLog(nickname, localDateTime);
+        outputView.printAttendanceCheckMessage(localDateTime);
+    }
+
+    private void updateAttendance() {
+        String nickname = inputView.readUpdateNickname();
+        int updateDayOfMonth = inputView.readUpdateDayOfMonth();
+        validateDayOfMonth(updateDayOfMonth);
+        LocalDate updateDate = getUpdateDate(updateDayOfMonth);
+
+        Attendance originalAttendance = attendances.findLogWithNameAndDate(nickname, updateDate);
+        LocalDateTime originalDateTime = originalAttendance.getLocalDateTime();
+
+        LocalDateTime updateDateTime = LocalDateTime.of(updateDate, inputView.readUpdateAttendanceTime());
+        attendances.updateAttendance(nickname, updateDateTime);
+        outputView.printUpdatedAttendanceMessage(originalDateTime, updateDateTime);
+    }
+
+    private LocalDate getUpdateDate(int updateDayOfMonth) {
+        LocalDate today = StandardDate.TODAY;
+        return LocalDate.of(today.getYear(), today.getMonthValue(), updateDayOfMonth);
+    }
+
+    private void identifyAttendanceLogsWithCrew() {
+        String nickname = inputView.readNickname();
+        outputView.printAttendanceLogsWithCrew(nickname, attendances);
+
+    }
+
+    private void identifyPenaltyCrews() {
+        outputView.printPenaltyCrews(attendances);
+    }
+
+    private void validateDayOfMonth(int updateDate) {
+        LocalDate today = StandardDate.TODAY;
+        int endDayOfMonth = today.lengthOfMonth();
+        if (1 > updateDate || endDayOfMonth < updateDate) {
+            throw new IllegalArgumentException("[ERROR] 수정 일자가 해당 월의 일자 범위를 벗어납니다.");
+        }
+    }
+}
