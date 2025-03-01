fe668c51d992dbdf193745327877e389c108aa90
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-01 19:45:13 +0900

    🛠️refactor(Attendance): of() 메서드 매개변수 시그니처 변경 및 내부 로직 수정

--- src/main/java/domain/Attendance.java
+++ src/main/java/domain/Attendance.java
@@ -1,13 +1,22 @@
 package domain;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public record Attendance(
         AttendanceDate attendanceDate,
         AttendanceTime attendanceTime,
         AttendanceStatus status
 ) {
-    public static Attendance of(final AttendanceDate attendanceDate, final AttendanceTime attendanceTime) {
-        final AttendanceStatus status = AttendanceStatus.findByTime(attendanceDate.getDayOfWeek(),
-                attendanceTime.getLocaltime());
+    public static Attendance of(final LocalDateTime localDateTime) {
+        final LocalDate date = localDateTime.toLocalDate();
+        final LocalTime time = localDateTime.toLocalTime();
+        final DayOfWeek dayOfWeek = date.getDayOfWeek();
+        final AttendanceStatus status = AttendanceStatus.findByTime(dayOfWeek, time);
+        final AttendanceDate attendanceDate = new AttendanceDate(date);
+        final AttendanceTime attendanceTime = AttendanceTime.of(dayOfWeek, time);
         return new Attendance(attendanceDate, attendanceTime, status);
     }
 }
--- src/test/java/domain/AttendanceTest.java
+++ src/test/java/domain/AttendanceTest.java
@@ -1,9 +1,11 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,6 +26,39 @@ public class AttendanceTest {
         //should
         assertThatCode(() -> new Attendance(attendanceDate1, attendanceTime1, status1)).doesNotThrowAnyException();
         assertThatCode(() -> new Attendance(attendanceDate2, attendanceTime2, status2)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("유효한 등교일시 이므로 출석 객체를 성공적으로 생성한다.")
+    void test2() {
+        //given
+        final LocalDateTime localDateTime1 = LocalDateTime.of(2024, 12, 13, 13, 1);
+        final LocalDateTime localDateTime2 = LocalDateTime.of(2024, 12, 9, 10, 1);
+
+        //should
+        assertThatCode(() -> Attendance.of(localDateTime1)).doesNotThrowAnyException();
+        assertThatCode(() -> Attendance.of(localDateTime2)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("유요한 등교일시가 아니므로 출석 객체 생성시 예외가 발생한다.")
+    void test3() {
+        //given
+        final LocalDateTime localDateTime1 = LocalDateTime.of(2024, 12, 13, 6, 59);
+        final LocalDateTime localDateTime2 = LocalDateTime.of(2024, 12, 9, 23, 1);
+        final LocalDateTime localDateTime3 = LocalDateTime.of(2024, 12, 14, 10, 1);
+        final LocalDateTime localDateTime4 = LocalDateTime.of(2024, 12, 15, 10, 1);
+        final LocalDateTime localDateTime5 = LocalDateTime.of(2024, 12, 15, 10, 1);
+
+        //should
+        assertAll(
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> Attendance.of(localDateTime1)),
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> Attendance.of(localDateTime2)),
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> Attendance.of(localDateTime3)),
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> Attendance.of(localDateTime4)),
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> Attendance.of(localDateTime5))
+        );
+
     }
 
 }
