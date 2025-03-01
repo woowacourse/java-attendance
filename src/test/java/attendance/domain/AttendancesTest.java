7b287e61b290e9a8d81fa1daea9c4d46ed7d9103
ljhee92 <weilainib7@gmail.com>
2025-03-01 14:27:14 +0900

    test(red): 출석리스트 출석 수정 테스트

--- src/test/java/attendance/domain/AttendancesTest.java
+++ src/test/java/attendance/domain/AttendancesTest.java
@@ -8,9 +8,9 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("출석 리스트 테스트")
 public class AttendancesTest {
@@ -33,5 +33,37 @@ public class AttendancesTest {
         assertThatThrownBy(() -> attendances.add(sameDateAttendance))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.ATTENDANCE_ALREADY_EXIST_ERROR.getMessage());
+    }
+
+    @Test
+    void 출석_리스트에서_원하는_날짜의_출석을_반환한다() {
+        Attendance attendance = new Attendance(LocalDate.of(2024, 12, 12), LocalTime.of(13, 0));
+        Attendances attendances = new Attendances(new ArrayList<>(List.of(attendance)));
+
+        Attendance oldAttendance = attendances.findByDate(LocalDate.of(2024, 12, 12));
+
+        assertThat(oldAttendance).isEqualTo(attendance);
+    }
+
+    @Test
+    void 출석_리스트에_원하는_날짜의_출석이_없다면_예외가_발생한다() {
+        Attendance attendance = new Attendance(LocalDate.of(2024, 12, 12), LocalTime.of(13, 0));
+        Attendances attendances = new Attendances(new ArrayList<>(List.of(attendance)));
+
+        assertThatThrownBy(() -> attendances.findByDate(LocalDate.of(2024, 12, 13)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 수정하려는 날짜에 출석 기록이 없습니다.");
+    }
+
+    @Test
+    void 출석_리스트에서_원하는_날짜의_출석을_수정한다() {
+        Attendance attendance = new Attendance(LocalDate.of(2024, 12, 12), LocalTime.of(13, 0));
+        Attendances attendances = new Attendances(new ArrayList<>(List.of(attendance)));
+
+        Attendance oldAttendance = attendances.findByDate(LocalDate.of(2024, 12, 12));
+        Attendance newAttendance = new Attendance(LocalDate.of(2024, 12, 12), LocalTime.of(13, 30));
+        attendances.update(oldAttendance, newAttendance);
+
+        assertThat(attendances.size()).isEqualTo(1);
     }
 }
