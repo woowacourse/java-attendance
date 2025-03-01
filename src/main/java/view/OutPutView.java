5213eaa8ff0fb8447056d42d17d7ed8638436259
LJM\eueo8 <eueo8259@gmail.com>
2025-03-01 18:45:57 +0900

    feat : 출석 기록 확인 출력 메서드 구현

--- src/main/java/view/OutPutView.java
+++ src/main/java/view/OutPutView.java
@@ -1,8 +1,12 @@
 package view;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.util.Locale;
+import java.util.Map;
+import model.AttendanceStatus;
 
 public class OutPutView {
     public static void displayAttendanceMenu(LocalDate localDate){
@@ -20,5 +24,16 @@ public class OutPutView {
 
     public static void requestNickName(){
         System.out.println("닉네임을 입력해 주세요.");
+    }
+
+    public static void displayAttendanceRecord(LocalTime localTime, AttendanceStatus attendanceStatus, LocalDate today){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        String attendanceTime = localTime.format(formatter);
+        System.out.printf("%d월 %d일 %s %s (%s)\n",
+                today.getMonthValue(),
+                today.getDayOfMonth(),
+                today.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA),
+                attendanceTime,
+                attendanceStatus.getAttendanceStatus());
     }
 }
