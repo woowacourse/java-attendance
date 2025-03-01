3aa3db005e0da12e905eb10a0433bf8f6f342a4b
LJM\eueo8 <eueo8259@gmail.com>
2025-03-01 16:28:55 +0900

    feat : 메서드 읽기 쉽게 수정 및 제적 위험 관련 계산 메서드 구현

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -17,34 +17,36 @@ public class Student {
         this.name = name;
     }
 
-    public LocalTime findAttendanceLocalTimeByLocalDate(LocalDate localDate){
+    public LocalTime findAttendanceLocalTimeByLocalDate(LocalDate localDate) {
         return attendanceTimeRecord.getAttendanceTimeRecords().get(localDate);
     }
 
-    public AttendanceStatus findAttendanceStatusByLocalDate(LocalDate localDate){
+    public AttendanceStatus findAttendanceStatusByLocalDate(LocalDate localDate) {
         return attendanceStatusRecord.getAttendanceStatusRecords().get(localDate);
     }
 
     public void registerAttendanceRecord(LocalDate todayDate, LocalTime attendanceTime) {
         validateDuplicateAttendance(todayDate);
-        attendanceTimeRecord.registerAttendanceTimeRecord(todayDate,attendanceTime);
-        AttendanceStatus attendanceStatus = AttendanceStatus.attendanceStatusCalculate(todayDate, attendanceTime);
-        attendanceStatusRecord.registerAttendanceStatusRecord(todayDate, attendanceStatus);
+        attendanceTimeRecord.registerAttendanceTimeRecord(todayDate, attendanceTime);
+        attendanceStatusRecord.registerAttendanceStatusRecord(
+                todayDate, AttendanceStatus.attendanceStatusCalculate(todayDate, attendanceTime)
+        );
     }
 
     public void modifyAttendanceRecord(String modifyDate, LocalTime modifyTime) {
-        LocalDate localDate = LocalDate.of(2024,12,Integer.parseInt(modifyDate));
+        LocalDate localDate = LocalDate.of(2024, 12, Integer.parseInt(modifyDate));
         attendanceTimeRecord.modifyAttendanceTimeRecord(localDate, modifyTime);
-        AttendanceStatus attendanceStatus = AttendanceStatus.attendanceStatusCalculate(localDate, modifyTime);
-        attendanceStatusRecord.modifyAttendanceStatusRecord(localDate, attendanceStatus);
+        attendanceStatusRecord.modifyAttendanceStatusRecord(
+                localDate, AttendanceStatus.attendanceStatusCalculate(localDate, modifyTime)
+        );
     }
 
     public long convertTardiesToAbsence() {
-        return attendanceStatusRecord.findAttendanceStatusCount(AttendanceStatus.LATE)/3;
+        return attendanceStatusRecord.findAttendanceStatusCount(AttendanceStatus.LATE) / 3;
     }
 
     public void nonAttendanceRecordStatusIsAbsent(LocalDate today) {
-        if (!attendanceTimeRecord.checkAttendanceRecordByLocalDate(today)){
+        if (!attendanceTimeRecord.checkAttendanceRecordByLocalDate(today)) {
             attendanceTimeRecord.putNullLocalTime(today);
             attendanceStatusRecord.putAttendanceStateToAbsent(today);
         }
@@ -54,17 +56,23 @@ public class Student {
         attendanceStatusCount.updateAttendanceCount(attendanceStatusRecord);
     }
 
-    private void validateDuplicateAttendance(LocalDate today){
-        if (attendanceTimeRecord.checkAttendanceRecordByLocalDate(today)){
-            throw new IllegalArgumentException("[ERROR] 출석기록이 존재합니다.");
-        }
-    }
-
     public Map<LocalDate, LocalTime> findAttendanceTimeRecordMap() {
         return attendanceTimeRecord.getAttendanceTimeRecords();
+    }
+
+    public long calculateAbsentCount(){
+        updateAttendanceCount();
+        return attendanceStatusCount.getAbsentCount() + convertTardiesToAbsence();
+    }
+
+    private void validateDuplicateAttendance(LocalDate today) {
+        if (attendanceTimeRecord.checkAttendanceRecordByLocalDate(today)) {
+            throw new IllegalArgumentException("[ERROR] 출석기록이 존재합니다.");
+        }
     }
 
     public String getName() {
         return name;
     }
+
 }
