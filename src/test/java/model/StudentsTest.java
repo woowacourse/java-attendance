7b6aa2378cf6d3bb636911166cff6d9a84e3e58d
(minjae8563) <(minjae8563@naver.com)>
2025-03-01 14:17:03 +0900

    test : List<Student> 를 멤버 변수로 가지는 객체의 테스트 작성

--- src/test/java/model/StudentsTest.java
+++ src/test/java/model/StudentsTest.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,40 +31,16 @@ public class StudentsTest {
     @DisplayName("같은 이름의 학생 찾기")
     void test1() {
         String studentName = "이든";
-        Assertions.assertTrue(
-                students.findStudentByName(studentName).equlas(student)
-        );
+        Assertions.assertEquals(students.findStudentByName(studentName), student);
     }
 
     @Test
-    @DisplayName("같은 이름의 학생을 찾고, 새로운 출석 기록 삽입 메서드 테스트")
+    @DisplayName("없는 이름을 입력하면 예외 발생 테스트")
     void test2() {
-        String studentName = "이든";
-        students.addAttendanceDateTime(addAttendanceDateTime);
-        Assertions.assertTrue(
-                students.findStudentByName(studentName).isExistSameAttendanceDateTime(addAttendanceDateTime)
-        );
-    }
-
-    @Test
-    @DisplayName("같은 이름의 학생을 찾고, 수정하고 싶은 기록 삽입 메서드 테스트")
-    void test3() {
-        String studentName = "이든";
-        students.modifyDateTime(addAttendanceDateTime);
-        Assertions.assertTrue(
-                students.findStudentByName(studentName).isExistSameAttendanceDateTime(addAttendanceDateTime)
-        );
-    }
-
-    @Test
-    @DisplayName("같은 이름의 학생을 찾고, 수정하고 싶은 날짜와 같은 기록을 삭제하는 메서드 테스트")
-    void test4() {
-        String studentName = "이든";
-        students.modifyDateTime(addAttendanceDateTime);
-        Assertions.assertFalse(
-                students.findStudentByName(studentName).isExistSameAttendanceDateTime(
-                        LocalDateTime.of(2024, 12, 12, 12, 12)
-                )
-        );
+        String studentName = "말론";
+        org.assertj.core.api.Assertions.assertThatThrownBy(
+                () -> students.findStudentByName(studentName)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 존재하지 않는 학생의 이름입니다.");
     }
 }
