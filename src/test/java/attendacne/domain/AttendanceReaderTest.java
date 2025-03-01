5fb83790400c7c921fa74eaad82ddf8d62d8f1e8
황재웅 <wodnd0131@naver.com>
2025-03-01 16:59:02 +0900

    test: "csv 정보를 불러온다" 관련 예외 처리 테스트
    - 잘못된 주소의 csv 정보를 불러올때, 예외가 발생한다
    - 파일을 읽어올 수 없을 경우, 예외가 발생한다

--- src/test/java/attendacne/domain/AttendanceReaderTest.java
+++ src/test/java/attendacne/domain/AttendanceReaderTest.java
@@ -2,6 +2,9 @@ package attendacne.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileNotFoundException;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +20,26 @@ public class AttendanceReaderTest {
         var attendanceReader = new AttendanceReader(ATTENDANCE_CSV);
 
         assertDoesNotThrow(attendanceReader::load);
+    }
+
+    @Test
+    @DisplayName("잘못된 주소의 csv 정보를 불러올때, 예외가 발생한다.")
+    void error_LoadingCsvWithWrongFileName() {
+        var attendanceReader = new AttendanceReader("/ErrorFile.csv");
+
+        Assertions.assertThatThrownBy(attendanceReader::load)
+            .isInstanceOf(FileNotFoundException.class)
+            .hasMessageContaining("파일을 찾을 수 없습니다")
+            .hasMessageContaining("ErrorFile");
+    }
+
+    @Test
+    @DisplayName("파일을 읽어올 수 없을 경우, 예외가 발생한다.")
+    void error_CantLoadingFile() {
+        var attendanceReader = new AttendanceReader("ErrorFile");
+
+        Assertions.assertThatThrownBy(attendanceReader::load)
+            .isInstanceOf(FileNotFoundException.class)
+            .hasMessageContaining("파일을 읽을 수 없습니다");
     }
 }
