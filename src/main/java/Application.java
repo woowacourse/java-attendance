86a4c4f261bfca5b0d98b141150cc28b0db14e19
(minjae8563) <(minjae8563@naver.com)>
2025-03-01 22:12:26 +0900

    feat : 기능이 실행되는 클래스 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import controller.AttendanceManagementController;
+
+public class Application {
+    public static void main(String[] args) {
+        AttendanceManagementController attendanceManagementController = new AttendanceManagementController();
+        attendanceManagementController.attendanceManagementStart();
+    }
+}
