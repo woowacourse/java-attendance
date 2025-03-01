4f82492af4795151f8bda7b24396bae3bd84cafe
(minjae8563) <(minjae8563@naver.com)>
2025-03-01 17:14:32 +0900

    test : 시간 관련 테스트 삭제

--- src/test/java/model/AttendanceDateTimeTest.java
+++ src/test/java/model/AttendanceDateTimeTest.java
@@ -28,31 +28,6 @@ public class AttendanceDateTimeTest {
     }
 
     @Test
-    @DisplayName("AttendanceTime 멤버 변수를 LocalTime 으로 바꾸는 메서드 테스트")
-    void test3() {
-        AttendanceDateTime attendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 12, 0));
-        Assertions.assertEquals(attendanceDateTime.toLocalTime(), LocalTime.of(12, 0));
-    }
-
-    @Test
-    @DisplayName("AttendanceTime 의 멤버 변수를 LocalTime 으로 바꾸고, 08:00 ~ 23:00 사이에 있는지 확인하는 메서드")
-    void test4() {
-        AttendanceDateTime attendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 7, 0));
-        Assertions.assertTrue(
-                attendanceDateTime.isNotOpeningTime()
-        );
-    }
-
-    @Test
-    @DisplayName("AttendanceTime 의 멤버 변수를 LocalTime 으로 바꾸고, 08:00 ~ 23:00 사이에 있는지 확인하는 메서드")
-    void test5() {
-        AttendanceDateTime attendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 23, 10));
-        Assertions.assertTrue(
-                attendanceDateTime.isNotOpeningTime()
-        );
-    }
-
-    @Test
     @DisplayName("다른 객체이지만 LocalDateTime 값을 비교하여 같은지 확인하는 메서드 테스트")
     void test6() {
         AttendanceDateTime attendanceDateTime = new AttendanceDateTime(LocalDateTime.of(2024, 12, 12, 12, 12));
