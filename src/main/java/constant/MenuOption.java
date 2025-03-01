33b358ad1212c05353770e7e1f0644f6fd08f7b5
LJM\eueo8 <eueo8259@gmail.com>
2025-03-01 17:51:10 +0900

    feat : MenuOption enum 생성 및 입력값 validate 메서드 구현

---
+++ src/main/java/constant/MenuOption.java
@@ -0,0 +1,28 @@
+package constant;
+
+import java.util.Arrays;
+
+public enum MenuOption {
+    ATTENDANCE_REGISTER("1"),
+    ATTENDANCE_MODIFY("2"),
+    CREW_ATTENDANCE_CHECK("3"),
+    EXPULSION_RISK("4"),
+    QUIT("Q");
+
+    private final String inputMenuValue;
+
+    MenuOption(String inputMenuValue) {
+        this.inputMenuValue = inputMenuValue;
+    }
+
+    public static MenuOption validateSelectMenuOption(String input){
+        return Arrays.stream(MenuOption.values())
+                .filter(option -> option.getInputMenuValue().equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 유효하지 않은 선택입니다."+ input));
+    }
+
+    public String getInputMenuValue() {
+        return inputMenuValue;
+    }
+}
