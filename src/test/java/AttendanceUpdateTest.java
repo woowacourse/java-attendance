1478f4977e071b83a248034ed31bd8019724261f
GiSeok Kang <ime.rokz@gmail.com>
2025-03-01 12:15:20 +0900

    red: 출석 기록이 없는 날 수정 시 예외 테스트 작성

--- src/test/java/AttendanceUpdateTest.java
+++ src/test/java/AttendanceUpdateTest.java
@@ -26,4 +26,21 @@ public class AttendanceUpdateTest {
         Assertions.assertThat(updateAttendance)
                 .isEqualTo(attendanceBook.findAttendance(updateNickname, updateDate, updateTime));
     }
+
+    @DisplayName("출석 기록이 없는 날은 수정할 수 없다.")
+    @Test
+    void update_none_attendance() {
+        // given
+        AttendanceBook attendanceBook = new AttendanceBook(List.of(
+                new Attendance("율무", LocalDate.of(2024, 12, 4), LocalTime.of(10, 4))
+        ));
+        final var updateNickname = "율무";
+        final var updateDate = LocalDate.of(2024, 12, 3);
+        final var updateTime = LocalTime.of(9, 58);
+
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> attendanceBook.update(updateNickname, updateDate, updateTime))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
