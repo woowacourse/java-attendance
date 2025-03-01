e0677ce4d4bf992759dfe5009e72d5d1538f3bc4
LeeJaeHoon <dlwogns3413@naver.com>
2025-03-01 17:15:08 +0900

    feat: 출석 프로그램 시작 코드 뼈대 구축

---
+++ src/main/java/attendance/AttendanceProgram.java
@@ -0,0 +1,12 @@
+package attendance;
+
+import attendance.controller.AttendanceController;
+import attendance.view.InputView;
+import attendance.view.OutputView;
+
+public class AttendanceProgram {
+    public static void main(String[] args) {
+        AttendanceController attendanceController = new AttendanceController(new InputView(), new OutputView());
+        attendanceController.run();
+    }
+}
---
+++ src/main/java/attendance/controller/AttendanceController.java
@@ -0,0 +1,19 @@
+package attendance.controller;
+
+import attendance.view.InputView;
+import attendance.view.OutputView;
+
+public class AttendanceController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public AttendanceController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+}
