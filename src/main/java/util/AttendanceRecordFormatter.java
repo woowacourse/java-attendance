2ae9fa052eb28c7384295a84ced7bb39ebd8650b
LJM\eueo8 <eueo8259@gmail.com>
2025-03-01 21:56:51 +0900

    feat : 제적 위험자 출력 로직 구현

--- src/main/java/util/AttendanceRecordFormatter.java
+++ src/main/java/util/AttendanceRecordFormatter.java
@@ -5,18 +5,32 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.util.Locale;
+import model.AttendancePenalty;
 import model.AttendanceStatus;
+import model.Student;
 
 public class AttendanceRecordFormatter {
-    public static String attendanceRecordFormatter(LocalTime localTime,
-                                                   AttendanceStatus attendanceStatus,
-                                                   LocalDate localDate) {
+    public static String attendanceRecordFormatter(Student student, LocalDate localDate) {
+        LocalTime localTime = student.findAttendanceLocalTimeByLocalDate(localDate);
+        AttendanceStatus attendanceStatus = student.findAttendanceStatusByLocalDate(localDate);
         String attendanceTime = localTimeFormatter(localTime);
-        return localDate.getMonthValue() + "월" +
-                localDate.getDayOfMonth() + "일" +
-                localDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA) +
+        return localDate.getMonthValue() + "월 " +
+                localDate.getDayOfMonth() + "일 " +
+                localDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA) + " " +
                 attendanceTime +
                 " (" + attendanceStatus.getAttendanceStatus() + ")";
+    }
+
+    public static String expulsionRiskRecordFormatter(Student student){
+        long absent = student.calculateAbsentCount();
+        long late = student.calculateLateCount();
+        long totalAbsent = student.calculateTotalAbsentCount();
+        AttendancePenalty attendancePenalty = AttendancePenalty.findPenaltyByAbsentCount(totalAbsent);
+        return String.format("- %s: 결석 %d회, 지각 %d회 (%s)",
+                student.getName(),
+                absent,
+                late,
+                attendancePenalty.getPenalty());
     }
 
     private static String localTimeFormatter(LocalTime localTime){
