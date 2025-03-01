855bc98ecab7ef2a554a921454b6d57bde2fb682
(minjae8563) <(minjae8563@naver.com)>
2025-03-01 18:41:11 +0900

    test : 학생의 출석 정보를 키 값에 날짜, 벨류 값에 시간을 넣어 보관하는 클래스에 대한 테스트 작성

---
+++ src/test/java/model/StudentAttendanceHistoryMapTest.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StudentAttendanceHistoryMapTest {
+    StudentAttendanceHistoryMap studentAttendanceHistoryMap;
+
+    @BeforeEach
+    void set() {
+        studentAttendanceHistoryMap = new StudentAttendanceHistoryMap(
+                Map.of(
+                        new AttendanceDate(LocalDate.of(2024, 12, 12)), new AttendanceTime(LocalTime.of(8, 0))
+                )
+        );
+    }
+
+    @Test
+    @DisplayName("출석 요일, 출석 시간 추가 기능 테스트")
+    void test1() {
+        studentAttendanceHistoryMap.addStudentAttendanceHistory(
+                new AttendanceDate(LocalDate.of(2024, 12, 13)),
+                new AttendanceTime(LocalTime.of(10, 31))
+        );
+        Assertions.assertTrue(
+                studentAttendanceHistoryMap.getStudentAttendanceHistory().containsKey(new AttendanceDate(LocalDate.of(2024, 12, 13)))
+        );
+    }
+
+    @Test
+    @DisplayName("출석 요일, 출석 시간 수정 기능 테스트")
+    void test2() {
+        studentAttendanceHistoryMap.modifyStudentAttendanceHistory(
+                new AttendanceDate(LocalDate.of(2024, 12, 12)),
+                new AttendanceTime(LocalTime.of(9, 0))
+        );
+
+        Assertions.assertTrue(studentAttendanceHistoryMap.getStudentAttendanceHistory().containsKey(
+                new AttendanceDate(LocalDate.of(2024, 12, 12))
+        ));
+
+        Assertions.assertEquals(studentAttendanceHistoryMap.getStudentAttendanceHistory().get(
+                new AttendanceDate(LocalDate.of(2024, 12, 12))
+        ), new AttendanceTime(LocalTime.of(9, 0)));
+    }
+
+}
