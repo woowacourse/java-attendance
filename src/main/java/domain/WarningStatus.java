8c5cec2e5732f0ba3025dc645279b8b5c0795c97
jhan0121 <jhan0121@gmail.com>
2025-03-01 13:59:14 +0900

    feat: 총 결석 횟수를 기반으로 경고 유형을 판정하는 기능 추가

--- src/main/java/domain/WarningStatus.java
+++ src/main/java/domain/WarningStatus.java
@@ -1,14 +1,34 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 public enum WarningStatus {
-    PASS,
-    WARNING,
-    INTERVIEW,
-    EXPEL;
+    PASS(totalCount -> totalCount < WarningStatus.WARNING_BOUND),
+    WARNING(totalCount -> totalCount == WarningStatus.WARNING_BOUND),
+    INTERVIEW(totalCount -> WarningStatus.INTERVIEW_BOUND <= totalCount && totalCount <= WarningStatus.EXPEL_BOUND),
+    EXPEL(totalCount -> totalCount > WarningStatus.EXPEL_BOUND);
 
     private static final long LATE_ABSENCE_RATIO = 3;
+    private static final long WARNING_BOUND = 2;
+    private static final long INTERVIEW_BOUND = 3;
+    private static final long EXPEL_BOUND = 5;
 
-    public static long calculateTotalAbsence(final AttendCount attendCount) {
+    private Predicate<Long> condition;
+
+    WarningStatus(Predicate<Long> condition) {
+        this.condition = condition;
+    }
+
+    public static WarningStatus judgeWarningStatus(final AttendCount attendCount) {
+        long totalAbsence = calculateTotalAbsence(attendCount);
+        return Arrays.stream(WarningStatus.values())
+                .filter(warningStatus -> warningStatus.condition.test(totalAbsence))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("경고 판정 실패"));
+    }
+
+    private static long calculateTotalAbsence(final AttendCount attendCount) {
         return attendCount.lateCount() / LATE_ABSENCE_RATIO + attendCount.absenceCount();
     }
 }
--- src/test/java/WarningStatusTest.java
+++ src/test/java/WarningStatusTest.java
@@ -2,21 +2,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.AttendCount;
 import domain.WarningStatus;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class WarningStatusTest {
 
-    @Test
-    @DisplayName("출석 상태 횟수를 기반으로 총 결석 횟수를 계산해야 한다")
-    void calculateTotalAbsenceCount() {
-        //given
-        AttendCount attendCount = new AttendCount(0, 4, 1);
+    private static Arguments createAttendCountArgument(long attendCount, long lateCount,
+                                                       long absenceCount, WarningStatus warningStatus) {
+        return Arguments.of(new AttendCount(attendCount, lateCount, absenceCount), warningStatus);
+    }
 
+    private static Stream<Arguments> provideAttendCount() {
+        return Stream.of(
+                createAttendCountArgument(0, 0, 1, WarningStatus.PASS),
+                createAttendCountArgument(0, 3, 0, WarningStatus.PASS),
+                createAttendCountArgument(0, 0, 2, WarningStatus.WARNING),
+                createAttendCountArgument(0, 6, 0, WarningStatus.WARNING),
+                createAttendCountArgument(0, 0, 3, WarningStatus.INTERVIEW),
+                createAttendCountArgument(0, 4, 4, WarningStatus.INTERVIEW),
+                createAttendCountArgument(0, 0, 6, WarningStatus.EXPEL),
+                createAttendCountArgument(0, 6, 4, WarningStatus.EXPEL)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideAttendCount")
+    @DisplayName("총 결석 횟수를 기반으로 경고 유형을 판정해야 한다")
+    void judgeWarningStatusUsingTotalAbsenceCount(AttendCount attendCount, WarningStatus expected) {
         //when
-        long actual = WarningStatus.calculateTotalAbsence(attendCount);
+        WarningStatus actual = WarningStatus.judgeWarningStatus(attendCount);
 
         //then
-        assertThat(actual).isEqualTo(2);
+        assertThat(actual).isEqualTo(expected);
     }
 }
