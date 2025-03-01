388e6f245dc41cc0344708269ebe3809a36a4fe4
soeun2537 <soeun2537@gmail.com>
2025-03-01 16:49:22 +0900

    feat: Controller 및 Main 클래스에 의존성 추가 및 로직 적용

--- src/main/java/AttendanceMachine.java
+++ src/main/java/AttendanceMachine.java
@@ -1,9 +1,15 @@
 import controller.AttendanceController;
+import util.DateTimeGenerator;
+import util.DateTimeStrategy;
+import util.SystemDateTimeStrategy;
 
 public class AttendanceMachine {
 
     public static void main(String[] args) {
-        AttendanceController attendanceController = new AttendanceController();
+        DateTimeStrategy dateTimeStrategy = new SystemDateTimeStrategy();
+        DateTimeGenerator dateTimeGenerator = new DateTimeGenerator(dateTimeStrategy);
+
+        AttendanceController attendanceController = new AttendanceController(dateTimeGenerator);
         attendanceController.run();
     }
 }
--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -5,18 +5,25 @@ import dto.AttendanceHistoryRequest;
 import dto.AttendanceOptionRequest;
 import dto.AttendanceUpdateRequest;
 import java.time.LocalDateTime;
+import util.DateTimeGenerator;
 import view.InputView;
 
 public class AttendanceController {
 
+    private final DateTimeGenerator dateTimeGenerator;
+
+    public AttendanceController(DateTimeGenerator dateTimeGenerator) {
+        this.dateTimeGenerator = dateTimeGenerator;
+    }
+
     public void run() {
         LocalDateTime now = LocalDateTime.now();
 
-        AttendanceOptionRequest attendanceOptionRequest = InputView.readAttendanceOptionRequest(now);
+        AttendanceOptionRequest attendanceOptionRequest = InputView.readAttendanceOptionRequest(dateTimeGenerator);
 
         AttendanceCheckInRequest attendanceCheckInRequest = InputView.readAttendanceCheckInRequest();
 
-        AttendanceUpdateRequest attendanceUpdateRequest = InputView.readAttendanceUpdateRequest(now);
+        AttendanceUpdateRequest attendanceUpdateRequest = InputView.readAttendanceUpdateRequest(dateTimeGenerator);
 
         AttendanceHistoryRequest attendanceHistoryRequest = InputView.readAttendanceHistoryRequest();
     }
