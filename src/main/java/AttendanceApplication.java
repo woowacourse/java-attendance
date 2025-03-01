15be6ebb34980ceea936ece9238ae9b02c15561d
jumdo12 <jumdo12@gmail.com>
2025-03-01 15:26:13 +0900

    feat: Main 실행문 추가

--- src/main/java/AttendanceApplication.java
+++ src/main/java/AttendanceApplication.java
@@ -1,5 +1,8 @@
+import controller.AttendanceController;
+
 public class AttendanceApplication {
     public static void main(String[] args) {
-
+        AttendanceController attendanceController = new AttendanceController();
+        attendanceController.run();
     }
 }
