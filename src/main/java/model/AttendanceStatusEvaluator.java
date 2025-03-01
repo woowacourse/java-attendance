b6b1357bea02c108b43ee07244a185809fe788a9
(minjae8563) <(minjae8563@naver.com)>
2025-03-01 17:34:43 +0900

    feat : 요일, 시간을 받아 출석, 결석, 지각 중 하나를 계산하는 클래스의 구현

---
+++ src/main/java/model/AttendanceStatusEvaluator.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.time.LocalTime;
+
+public enum AttendanceStatusEvaluator {
+    MONDAY(new AttendanceTime(LocalTime.of(13, 5)), new AttendanceTime(LocalTime.of(13, 30))),
+    TUESDAY_TO_FRIDAY(new AttendanceTime(LocalTime.of(10, 5)), new AttendanceTime(LocalTime.of(10, 30)));
+
+    private final AttendanceTime lateTime;
+    private final AttendanceTime absentTime;
+
+    AttendanceStatusEvaluator(AttendanceTime lateTime, AttendanceTime absentTime) {
+        this.lateTime = lateTime;
+        this.absentTime = absentTime;
+    }
+
+    public static AttendanceStatus calculateAttendanceStatus(AttendanceDateTime attendanceDateTime, AttendanceTime attendanceTime) {
+        if (attendanceDateTime.isMonday()) {
+            if (attendanceTime.isAfter(MONDAY.absentTime)) {
+                return AttendanceStatus.ABSENT;
+            }
+            if (attendanceTime.isAfter(MONDAY.lateTime)) {
+                return AttendanceStatus.LATE;
+            }
+            return AttendanceStatus.ATTENDANCE;
+        }
+
+        if (attendanceTime.isAfter(TUESDAY_TO_FRIDAY.absentTime)) {
+            return AttendanceStatus.ABSENT;
+        }
+        if (attendanceTime.isAfter(TUESDAY_TO_FRIDAY.lateTime)) {
+            return AttendanceStatus.LATE;
+        }
+        return AttendanceStatus.ATTENDANCE;
+    }
+
+}
