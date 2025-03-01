7eb28014c2382bd7d7544b163ba17b1cc7a345c3
jhpark1227 <junhyeok0246@naver.com>
2025-03-01 22:52:30 +0900

    test: AttendanceType 단위테스트 작성 (결석기준시간을 계산한다)

--- src/test/java/AttendanceTypeTest.java
+++ src/test/java/AttendanceTypeTest.java
@@ -1,7 +1,11 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
 import domain.AttendanceTime;
 import domain.AttendanceType;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import org.assertj.core.api.Assertions;
+import java.time.LocalTime;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -25,6 +29,17 @@ public class AttendanceTypeTest {
         //when
         AttendanceType type = AttendanceType.calculateType(attendanceTime);
         //then
-        Assertions.assertThat(type).isEqualTo(expected);
+        assertThat(type).isEqualTo(expected);
+    }
+
+    @Test
+    void 결석기준시간을_계산한다() {
+        //given
+        LocalDate day = LocalDate.of(2024, 12, 3);
+        LocalTime expected = LocalTime.of(10, 30);
+        //when
+        LocalTime actual = AttendanceType.calculateAbsenceStandardTime(day);
+        //then
+        assertThat(actual).isEqualTo(expected);
     }
 }
