216ac6e82d7191ba638d37494df7e7cc63293872
(minjae8563) <(minjae8563@naver.com)>
2025-03-02 00:52:37 +0900

    test : 필요없는 테스트 삭제

--- src/test/java/util/FileInputTest.java
+++ src/test/java/util/FileInputTest.java
@@ -14,15 +14,6 @@ class FileInputTest {
     String regex = "[가-힣]+,\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}";
 
     @Test
-    @DisplayName("파일에 있는 자료의 형식이 맞지 않으면 예외 던지는 메서드 테스트")
-    void test1() {
-        String information = "쿠키,,2024-12-02 13:01";
-        org.assertj.core.api.Assertions.assertThatThrownBy(() -> information.matches(regex))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 파일 자료 형식에 맞지 않습니다.");
-    }
-
-    @Test
     @DisplayName("파일에 있는 자료의 형식이 맞으면 통과하는 테스트")
     void test2() {
         String information = "쿠키,2024-12-02 13:01";
