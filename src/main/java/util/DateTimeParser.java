db390a5e498e59184f4fd94335455d24e38d71eb
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-01 20:12:47 +0900

    🛠️refactor(DateTimeParser): LocalDate를 M월 d일 E요일로 파싱하는 메서드 추가

--- src/main/java/util/DateTimeParser.java
+++ src/main/java/util/DateTimeParser.java
@@ -1,17 +1,24 @@
 package util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class DateTimeParser {
 
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter FORMATTER_1 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter FORMATTER_2 = DateTimeFormatter.ofPattern("M월 d일 E요일", Locale.KOREAN);
 
     private DateTimeParser() {
     }
 
     public static LocalDateTime parseToLocalDateTime(final String dateTime) {
-        return LocalDateTime.parse(dateTime, FORMATTER);
+        return LocalDateTime.parse(dateTime, FORMATTER_1);
+    }
+
+    public static String parseToLocalDateKoreanFormat(final LocalDate localDate) {
+        return localDate.format(FORMATTER_2);
     }
 }
--- src/test/java/util/DateTimeParserTest.java
+++ src/test/java/util/DateTimeParserTest.java
@@ -2,6 +2,7 @@ package util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,20 @@ public class DateTimeParserTest {
         assertThat(localDateTime.getYear()).isEqualTo(2024);
         assertThat(localDateTime.getMonth().getValue()).isEqualTo(12);
         assertThat(localDateTime.getDayOfMonth()).isEqualTo(13);
+
+    }
+
+    @Test
+    @DisplayName("LocalDate를 M월 d월 E요일 형식으로 파싱한다.")
+    void test2() {
+        //given
+        final LocalDate localDate = LocalDate.of(2024, 12, 13);
+
+        //when
+        final String koreanFormat = DateTimeParser.parseToLocalDateKoreanFormat(localDate);
+
+        //then
+        assertThat(koreanFormat).isEqualTo("12월 13일 금요일");
 
     }
 
