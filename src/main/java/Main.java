1247f5aabc32e091dddc3e580c072ca8c134be90
LJM\eueo8 <eueo8259@gmail.com>
2025-03-01 21:58:16 +0900

    feat : Main 클래스 추가

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import controller.Controller;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Controller controller = new Controller();
+        controller.run();
+    }
+}
