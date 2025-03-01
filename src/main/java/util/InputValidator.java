0f78ff16e36f80a9fb561b27fd35e07748b5dd35
soeun2537 <soeun2537@gmail.com>
2025-03-01 15:44:34 +0900

    feat: 정수를 검증하는 로직 구현

--- src/main/java/util/InputValidator.java
+++ src/main/java/util/InputValidator.java
@@ -1,6 +1,7 @@
 package util;
 
 import static constant.ErrorMessage.INVALID_INPUT_NULL_OR_BLANK;
+import static constant.ErrorMessage.INVALID_INTEGER_FORMAT;
 
 public class InputValidator {
 
@@ -10,6 +11,14 @@ public class InputValidator {
     public static void validateNullOrBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(INVALID_INPUT_NULL_OR_BLANK.getMessage());
+        }
+    }
+
+    public static void validateInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INTEGER_FORMAT.getMessage());
         }
     }
 }
