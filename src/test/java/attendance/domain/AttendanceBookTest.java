5b3ece8de2baf72b8538f8fce0a71491e2f36dc2
ljhee92 <weilainib7@gmail.com>
2025-03-01 15:41:52 +0900

    test(red): 출석부에서 크루의 출석리스트 수정 테스트

--- src/test/java/attendance/domain/AttendanceBookTest.java
+++ src/test/java/attendance/domain/AttendanceBookTest.java
@@ -8,6 +8,7 @@ import java.time.LocalTime;
 import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayName("출석부 테스트")
 public class AttendanceBookTest {
@@ -20,5 +21,33 @@ public class AttendanceBookTest {
         attendanceBook.add(crew, attendance);
 
         assertThat(attendanceBook.size()).isEqualTo(1);
+    }
+
+    @Test
+    void 출석부의_크루별_출석리스트중_원하는날짜의_출석을_가져올수_있다() {
+        AttendanceBook attendanceBook = new AttendanceBook(new HashMap<>());
+        Crew crew = new Crew(new Nickname("듀이"));
+        Attendance attendance = new Attendance(LocalDate.of(2024, 12, 12), LocalTime.of(10, 0));
+        attendanceBook.add(crew, attendance);
+
+        LocalDate inputDate = LocalDate.of(2024, 12, 12);
+        Attendance oldAttendance = attendanceBook.findAttendanceByCrew(crew, inputDate);
+
+        assertThat(oldAttendance).isEqualTo(attendance);
+    }
+
+    @Test
+    void 출석부에서_크루별_출석을_수정할수_있다() {
+        AttendanceBook attendanceBook = new AttendanceBook(new HashMap<>());
+        Crew crew = new Crew(new Nickname("듀이"));
+        Attendance attendance = new Attendance(LocalDate.of(2024, 12, 12), LocalTime.of(10, 0));
+        attendanceBook.add(crew, attendance);
+
+        LocalDate inputDate = LocalDate.of(2024, 12, 12);
+        Attendance oldAttendance = attendanceBook.findAttendanceByCrew(crew, inputDate);
+        Attendance newAttendance = new Attendance(inputDate, LocalTime.of(10, 30));
+
+        assertThatCode(() -> attendanceBook.update(crew, oldAttendance, newAttendance))
+                .doesNotThrowAnyException();
     }
 }
