303d0e2fdbce3c6203479673b5ad6ed46a094fd9
soeun2537 <soeun2537@gmail.com>
2025-03-02 03:03:21 +0900

    feat: 특정 크루의 출석 기록을 출력하는 로직 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,14 +1,23 @@
 package view;
 
+import static constant.OutputViewMessage.ATTENDANCE_ABSENCE_TYPE_RESPONSE;
+import static constant.OutputViewMessage.ATTENDANCE_BE_LATE_TYPE_RESPONSE;
 import static constant.OutputViewMessage.ATTENDANCE_CHECK_IN_RESPONSE;
+import static constant.OutputViewMessage.ATTENDANCE_HISTORY_NULL_RESPONSE;
+import static constant.OutputViewMessage.ATTENDANCE_HISTORY_RESPONSE;
+import static constant.OutputViewMessage.ATTENDANCE_PUNISHMENT_TYPE_RESPONSE;
+import static constant.OutputViewMessage.ATTENDANCE_SUCCESS_TYPE_RESPONSE;
 import static constant.OutputViewMessage.ATTENDANCE_UPDATE_NULL_RESPONSE;
 import static constant.OutputViewMessage.ATTENDANCE_UPDATE_RESPONSE;
 
 import dto.AttendanceCheckInResponse;
+import dto.AttendanceHistoryResponse;
 import dto.AttendanceUpdateResponse;
 import java.time.format.TextStyle;
 import java.util.Locale;
+import model.Attendance;
 import model.AttendanceType;
+import model.PunishmentType;
 
 public class OutputView {
 
@@ -55,6 +64,43 @@ public class OutputView {
         printNewLine();
     }
 
+    public static void printAttendanceHistory(AttendanceHistoryResponse response) {
+        for (Attendance attendance : response.attendances()) {
+            if (attendance.getCheckInTime() == null) {
+                println(String.format(ATTENDANCE_HISTORY_NULL_RESPONSE.getMessage(),
+                        attendance.getCheckInDate().getMonthValue(),
+                        attendance.getCheckInDate().getDayOfMonth(),
+                        attendance.getCheckInDate().getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN),
+                        parseAttendanceType(attendance.getAttendanceType())
+                ));
+                continue;
+            }
+            println(String.format(ATTENDANCE_HISTORY_RESPONSE.getMessage(),
+                    attendance.getCheckInDate().getMonthValue(),
+                    attendance.getCheckInDate().getDayOfMonth(),
+                    attendance.getCheckInDate().getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN),
+                    attendance.getCheckInTime().getHour(),
+                    attendance.getCheckInTime().getMinute(),
+                    parseAttendanceType(attendance.getAttendanceType())
+            ));
+        }
+        printNewLine();
+
+        println(String.format(ATTENDANCE_SUCCESS_TYPE_RESPONSE.getMessage(),
+                response.attendanceTotal().get(AttendanceType.SUCCESS)));
+        println(String.format(ATTENDANCE_BE_LATE_TYPE_RESPONSE.getMessage(),
+                response.attendanceTotal().get(AttendanceType.BE_LATE)));
+        println(String.format(ATTENDANCE_ABSENCE_TYPE_RESPONSE.getMessage(),
+                response.attendanceTotal().get(AttendanceType.ABSENCE)));
+        printNewLine();
+
+        if (!response.punishmentType().equals(PunishmentType.NONE)) {
+            println(String.format(ATTENDANCE_PUNISHMENT_TYPE_RESPONSE.getMessage(),
+                    parsePunishmentType(response.punishmentType())));
+            printNewLine();
+        }
+    }
+
     private static String parseAttendanceType(AttendanceType attendanceType) {
         if (attendanceType.equals(AttendanceType.SUCCESS)) {
             return "출석";
@@ -63,6 +109,19 @@ public class OutputView {
             return "지각";
         }
         return "결석";
+    }
+
+    private static String parsePunishmentType(PunishmentType punishmentType) {
+        if (punishmentType.equals(PunishmentType.WARNING)) {
+            return "경고";
+        }
+        if (punishmentType.equals(PunishmentType.MEETING)) {
+            return "면담";
+        }
+        if (punishmentType.equals(PunishmentType.EXPULSION)) {
+            return "제적";
+        }
+        return "";
     }
 
     private static void println(String message) {
