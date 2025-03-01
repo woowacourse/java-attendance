3fe2d2a18d0c0d44dd7d9eb54e3126aa10029582
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-02 01:13:00 +0900

    🟢green(CrewsGenerator): csv파일을 읽어 크루를 생성하는 기능 그린 테스트

--- src/test/java/domain/CrewsGenerator.java
+++ src/test/java/domain/CrewsGenerator.java
@@ -1,15 +1,40 @@
 package domain;
 
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import util.DateTimeParser;
+import util.FileManager;
 
 public class CrewsGenerator {
+
+    private static final String FILE_NAME = "attendances.csv";
+    private static Long initialId = 1L;
 
     private CrewsGenerator() {
 
     }
 
     public static Map<String, Crew> generate() {
-        return new HashMap<String, Crew>();
+        final Map<String, Crew> crews = new LinkedHashMap<>();
+        List<String> lines = FileManager.readFileLines(FILE_NAME);
+        lines.removeFirst();
+        for (String line : lines) {
+            processAttendanceRecord(line, crews);
+        }
+        return crews;
+    }
+
+    private static void processAttendanceRecord(final String line, final Map<String, Crew> crews) {
+        final String[] split = line.split(",");
+        final String name = split[0];
+        final LocalDateTime localDateTime = DateTimeParser.parseToLocalDateTime(split[1]);
+        crews.computeIfAbsent(name, CrewsGenerator::createCrew).putAttendance(localDateTime);
+    }
+
+
+    private static Crew createCrew(final String name) {
+        return new Crew(initialId++,name, new LinkedHashMap<>());
     }
 }
--- src/test/java/domain/CrewsGeneratorTest.java
+++ src/test/java/domain/CrewsGeneratorTest.java
@@ -1,22 +1,38 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CrewsGeneratorTest {
 
+    //쿠키,2024-12-13 10:08
     @Test
     @DisplayName("csv파일을 읽어 크루 출석 기록 초기화 한다")
     void test1() {
         //given
+        final LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 13, 10, 8);
+        final Attendance attendance = Attendance.of(localDateTime);
         //when
         final Map<String, Crew> crews = CrewsGenerator.generate();
+        final Set<String> crewsName = crews.keySet();
 
         //then
-        assertThat(crews).isNotEmpty();
-
+        assertAll(
+                () -> assertThat(crewsName).contains("쿠키")
+                        .contains("빙티")
+                        .contains("빙봉")
+                        .contains("이든")
+                        .contains("짱수"),
+                () -> assertThat(crews.get("쿠키").getAttendanceMap()).containsEntry(localDateTime.toLocalDate(), attendance),
+                () -> assertThat(crews.get("빙봉").getAttendanceMap()).containsEntry(localDateTime.toLocalDate(), attendance),
+                () -> assertThat(crews.get("빙티").getAttendanceMap()).containsEntry(localDateTime.toLocalDate(), attendance),
+                () -> assertThat(crews.get("이든").getAttendanceMap()).containsEntry(localDateTime.toLocalDate(), attendance)
+        );
     }
 }
