b466ce5573f9bcab7529a48151285a216a2e1c8a
jumdo12 <jumdo12@gmail.com>
2025-03-02 00:34:10 +0900

    fix: 출결 데이터가 없는 경우 결석이 출력 안되는 문제 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,20 +1,21 @@
 package view;
 
 import domain.Crew;
-import domain.attendance.Attendance;
-import domain.attendance.AttendanceDate;
-import domain.attendance.AttendanceStatus;
-import domain.attendance.StudentStatus;
+import domain.attendance.*;
+import util.DateTimeUtils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.TextStyle;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.IntStream;
 
+import static domain.attendance.AttendanceStatus.*;
 import static util.DateTimeUtils.*;
 
 public class OutputView {
-    private static final String doubleNewLine = "\n\n";
+    private static final String newLine = "\n";
 
     public static void printWelcomeMessage() {
         String welcomeMessage = "오늘은 " +
@@ -34,26 +35,45 @@ public class OutputView {
 
     public static void printCrewAttendance(String crewName ,Attendance crewAttendanceResult){
         StringBuilder attendResult = new StringBuilder();
+        attendResult.append("이번 달 ").append(crewName).append("의 출석 기록입니다.").append(newLine);
 
-        List<AttendanceDate> sortedRecord = crewAttendanceResult.getSortedAttendanceResult();
-        attendResult.append("이번 달 ").append(crewName).append("의 출석 기록입니다.").append(doubleNewLine);
-        sortedRecord.forEach(attendanceDate -> attendResult
-                .append(convertToLocalDayFormat(attendanceDate.getAttendanceAt()))
-                .append(convertToDateTimeFormat(attendanceDate.getAttendanceAt()))
-                .append("\n"));
-        attendResult.append(doubleNewLine);
+        IntStream.range(1,NOW_DAY).forEach(day -> addCrewAttendanceResult(attendResult,day,crewAttendanceResult));
 
-        attendResult.append("출석: ").append(crewAttendanceResult.getAttendanceCount()).append("회\n")
-                .append("지각: ").append(crewAttendanceResult.getTardyCount()).append("회\n")
-                .append("결석: ").append(crewAttendanceResult.getAbsenceCount()).append("회\n\n");
+        attendResult.append("출석: ").append(crewAttendanceResult.getAttendanceCount()).append("회").append(newLine)
+                .append("지각: ").append(crewAttendanceResult.getTardyCount()).append("회").append(newLine)
+                .append("결석: ").append(crewAttendanceResult.getAbsenceCount()).append("회").append(newLine);
 
         System.out.println(attendResult);
         printStudentStatus(crewAttendanceResult);
     }
 
+    private static void addCrewAttendanceResult(StringBuilder attendResult, int day, Attendance crewAttendanceResult) {
+        LocalDate findDate = LocalDate.of(NOW_YEAR,NOW_MONTH,day);
+        if (crewAttendanceResult.has(findDate)) {
+            processAttendanceRecord(attendResult,crewAttendanceResult,day);
+            return;
+        }
+        if(TimeTable.isAttendanceDay(findDate)){
+            processAbsenceDay(attendResult,day);
+        }
+    }
+
+    private static void processAttendanceRecord(StringBuilder attendResult, Attendance crewAttendanceResult, int day){
+        AttendanceDate findAttendance = crewAttendanceResult.findByLocalDate(LocalDate.of(NOW_YEAR, NOW_MONTH, day));
+        attendResult.append(convertToLocalDayFormat(findAttendance.getAttendanceAt()))
+                .append(convertToDateTimeFormat(findAttendance.getAttendanceAt()))
+                .append(convertToAttendanceStatus(findAttendance))
+                .append(newLine);
+    }
+
+    private static void processAbsenceDay(StringBuilder attendResult, int day){
+        attendResult.append(convertToLocalDayFormat(LocalDate.of(NOW_YEAR, NOW_MONTH, day)))
+                .append("--:--").append(" (" + ABSENCE.getStatus() + ")").append(newLine);
+    }
+
     private static void printStudentStatus(Attendance crewAttendanceResult){
         if(crewAttendanceResult.getStudentStatus().equals(StudentStatus.NONE)) return;
-        System.out.println(crewAttendanceResult.getStudentStatus().getDescription() + " 대상자 입니다.\n");
+        System.out.println(crewAttendanceResult.getStudentStatus().getDescription() + " 대상자 입니다.");
     }
 
     public static void printEditResult(AttendanceDate oldRecord, AttendanceDate newRecord){
@@ -88,6 +108,10 @@ public class OutputView {
         return attendanceAt.format(localDayFormatter);
     }
 
+    private static String convertToLocalDayFormat(LocalDate localDate){
+        return localDate.format(localDayFormatter);
+    }
+
     private static String convertToDateTimeFormat(LocalDateTime attendanceAt){
         return attendanceAt.format(dateTimeFormatter);
     }
@@ -97,6 +121,7 @@ public class OutputView {
     }
 
     private static String convertToAttendanceStatus(AttendanceDate attendanceRecord){
-        return " ( " + attendanceRecord.getStatus() + " ) ";
+
+        return " (" + attendanceRecord.getStatus().getStatus() + ") ";
     }
 }
