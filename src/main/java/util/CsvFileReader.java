3a3422b11325e720689c87774d28941adbd4a402
DongchannN <ldc991104@gmail.com>
2025-03-01 21:20:14 +0900

    feat: csv 파일을 읽는 기능 구현

---
+++ src/main/java/util/CsvFileReader.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvFileReader {
+    public static List<String[]> readCsvFile(String filePath) throws IOException {
+        List<String[]> records = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                records.add(line.split(","));
+            }
+        }
+        return records;
+    }
+}
---
+++ src/test/java/util/CsvFileReaderTest.java
@@ -0,0 +1,23 @@
+package util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CsvFileReaderTest {
+    private static final String CSV_FILE_PATH = "src/test/resources/attendances.csv";
+
+    @Test
+    @DisplayName("파일 이름을 통해 해당 파일의 값을 List<String[]>형태로 읽음")
+    void readCsvFileTest() throws IOException {
+        // when
+        List<String[]> read = CsvFileReader.readCsvFile(CSV_FILE_PATH);
+
+        // then
+        assertThat(read).contains(new String[]{"차니", "2024-12-02 13:00"})
+                .contains(new String[]{"포비", "2024-12-02 13:06"});
+    }
+}
