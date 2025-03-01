00136ebb1dd33753a7c85b1e1db4f9d624588a9c
AppleMint98 <applemint9821@gmail.com>
2025-03-01 10:05:30 +0900

    feat(Crew) : 크루 이름에 한글 글자 이외의 값이 들어오면 예외 발생
    [x] 크루 이름에 가-힣 이외의 문자가 들어올 경우, 예외를 발생시킨다.

--- src/main/java/domain/Crew.java
+++ src/main/java/domain/Crew.java
@@ -1,15 +1,27 @@
 package domain;
 
+import java.util.regex.Pattern;
+
 public class Crew {
+
+    private static final String KOREAN_WORDS_REGEX = "^[가-힣]+$";
 
     private final String name;
 
     private Crew(String name) {
+        validateCrewNameIsKoreanWords(name);
         this.name = name;
     }
 
     public static Crew fromName(String name) {
         return new Crew(name);
+    }
+
+    private void validateCrewNameIsKoreanWords(String name) {
+        Pattern pattern = Pattern.compile(KOREAN_WORDS_REGEX);
+        if (!pattern.matcher(name).matches()) {
+            throw new IllegalArgumentException("[ERROR] 크루 이름을 정상적으로 입력해 주세요.");
+        }
     }
 
     public String getName() {
--- src/test/java/domain/CrewTest.java
+++ src/test/java/domain/CrewTest.java
@@ -15,4 +15,37 @@ class CrewTest {
         Assertions.assertThat(crew.getName()).isEqualTo("제프리");
     }
 
+    @Test
+    void 크루_이름에_숫자가_들어오면_예외를_발생시킨다() {
+        // given
+        String name = "제프리1";
+
+        // when // then
+        Assertions.assertThatThrownBy(() -> Crew.fromName(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 크루 이름을 정상적으로 입력해 주세요.");
+    }
+
+    @Test
+    void 크루_이름에_단일_문자가_들어오면_예외를_발생시킨다() {
+        // given
+        String name = "제프ㅇ리";
+
+        // when // then
+        Assertions.assertThatThrownBy(() -> Crew.fromName(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 크루 이름을 정상적으로 입력해 주세요.");
+    }
+
+    @Test
+    void 크루_이름에_구분자가_들어오면_예외를_발생시킨다() {
+        // given
+        String name = "제프:리";
+
+        // when // then
+        Assertions.assertThatThrownBy(() -> Crew.fromName(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 크루 이름을 정상적으로 입력해 주세요.");
+    }
+
 }
