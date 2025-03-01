ccd53e201aac639fcce8fb993acfc448b95afe1d
CheChe903 <cheche903@naver.com>
2025-03-01 12:55:16 +0900

    tesT(AttendanceBook): 단위 테스트 추가

--- src/test/java/attendance/domain/AttendanceBookTest.java
+++ src/test/java/attendance/domain/AttendanceBookTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -33,10 +34,15 @@ class AttendanceBookTest {
         Attendance attendance6 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 17, 10, 31)));
         Attendance attendance7 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 18, 10, 31)));
         Attendance attendance8 = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 19, 10, 31)));
-
-        attendances = new Attendances(
-                Set.of(attendance1, attendance2, attendance3, attendance4, attendance5, attendance6, attendance7,
-                        attendance8));
+        attendances = new Attendances(new HashSet<>());
+        attendances.add(attendance1);
+        attendances.add(attendance2);
+        attendances.add(attendance3);
+        attendances.add(attendance4);
+        attendances.add(attendance5);
+        attendances.add(attendance6);
+        attendances.add(attendance7);
+        attendances.add(attendance8);
         attendanceBook = new AttendanceBook(names, attendances);
     }
 
@@ -88,5 +94,48 @@ class AttendanceBookTest {
 
         // then
         assertThat(academicStatus).isEqualTo(AcademicStatus.INTERVIEW);
+    }
+
+    @DisplayName("출석을 추가한다.")
+    @Test
+    void 출석을_추가한다() {
+
+        // given
+        Attendance attendance = new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 3, 10, 0)));
+
+        // when
+        attendanceBook.addAttendance(attendance);
+        List<Attendance> resultAttendances = attendanceBook.findAttendancesByCrewNameAndYearAndMonth("체체", 2025, 2);
+
+        // then
+        assertThat(resultAttendances.size()).isEqualTo(9);
+    }
+
+    @DisplayName("크루 이름, 년,월,일로 출석 기록 하나를 가져온다.")
+    @Test
+    void 크루_이름_년월일로_출석_기록_하나를_가져온다() {
+
+        // given
+        attendances.add(new Attendance("체체", new Time(LocalDateTime.of(2025, 2, 5, 10, 0))));
+
+        // when
+        Attendance attendance = attendanceBook.findAttendanceByCrewNameAndLocalDate("체체", LocalDate.of(2025, 2, 5));
+
+        // then
+        assertThat(attendanceBook.findAttendanceByCrewNameAndLocalDate("체체", LocalDate.of(2025, 2, 5))
+                .checkStatus()).isEqualTo(AttendanceStatus.ATTEND);
+    }
+
+    @DisplayName("크루 이름, 년,월로 해당 월의 출석 기록들을 가져온다.")
+    @Test
+    void 크루_이름_년월로_해당_월의_출석_기록들을_가져온다() {
+
+        // given
+
+        // when
+        List<Attendance> resultAttendances = attendanceBook.findAttendancesByCrewNameAndYearAndMonth("체체", 2025, 2);
+
+        // then
+        assertThat(resultAttendances.size()).isEqualTo(8);
     }
 }
