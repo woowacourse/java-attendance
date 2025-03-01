b09b54e0481aeb4cbe4d8ed647cbd623a9b97c47
(minjae8563) <(minjae8563@naver.com)>
2025-03-01 21:21:54 +0900

    feat : 학생의 제적, 면담, 경고를 결정하는 Enum 클래스 구현

---
+++ src/main/java/model/StudentPunishment.java
@@ -0,0 +1,27 @@
+package model;
+
+public enum StudentPunishment {
+    ABSENT(5),
+    INTERVIEW(3),
+    WARNING(2),
+    SAFE(1);
+
+    private final int absenceCount;
+
+    StudentPunishment(int absenceCount) {
+        this.absenceCount = absenceCount;
+    }
+
+    public static StudentPunishment calculatePunishment(int riskLevel) {
+        if (riskLevel >= ABSENT.absenceCount) {
+            return StudentPunishment.ABSENT;
+        }
+        if (riskLevel >= INTERVIEW.absenceCount) {
+            return StudentPunishment.INTERVIEW;
+        }
+        if (riskLevel >= WARNING.absenceCount) {
+            return StudentPunishment.WARNING;
+        }
+        return StudentPunishment.SAFE;
+    }
+}
