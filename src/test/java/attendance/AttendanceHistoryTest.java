7673731874b44f62fc15188e2dae2aedc4c9dae1
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-03-01 14:45:27 +0900

    test: 중복 테스트 코드 분리

--- src/test/java/attendance/AttendanceHistoryTest.java
+++ src/test/java/attendance/AttendanceHistoryTest.java
@@ -14,20 +14,23 @@ import org.junit.jupiter.api.Test;
 
 public class AttendanceHistoryTest {
 
+    String nicknameJenson = "젠슨";
     AttendanceHistory attendanceHistory;
     AttendanceTimes attendanceTimes;
+    AttendanceTime attendanceTime1 = AttendanceTime.from(LocalDateTime.of(2024, 12, 3, 10, 0));
 
     @BeforeEach
     void init() {
         attendanceTimes = AttendanceTimes.create();
         attendanceHistory = AttendanceHistory.create();
+        attendanceHistory.add(nicknameJenson, attendanceTime1);
     }
 
     @DisplayName("닉네임과 출석 기록이 주어졌을 때, 저장할 수 있어야 한다.")
     @Test
     void given_nickname_and_attendance_then_save() {
         //given
-        String nickname = "젠슨";
+        String nickname = "포비";
         AttendanceTime attendanceTime = AttendanceTime.from(LocalDateTime.of(2024, 12, 3, 10, 0));
 
         //when
@@ -42,31 +45,29 @@ public class AttendanceHistoryTest {
     @Test
     void nickname_already_exist_then_add_exising_attendance() {
         //given
-        String nickname = "젠슨";
-        AttendanceTime attendanceTime = AttendanceTime.from(LocalDateTime.of(2024, 12, 3, 10, 0));
-        attendanceHistory.add(nickname, attendanceTime);
-        AttendanceTime anotherAttendanceTime = AttendanceTime.from(LocalDateTime.of(2024, 12, 4, 10, 0));
+        AttendanceTime anotherAttendanceTime = AttendanceTime.from(
+            LocalDateTime.of(2024, 12, 4, 10, 0));
 
         // when
-        attendanceHistory.add(nickname, anotherAttendanceTime);
+        attendanceHistory.add(nicknameJenson, anotherAttendanceTime);
 
         //then
         Map<String, AttendanceTimes> attendanceHistory1 = attendanceHistory.getAttendanceHistory();
-        assertThat(attendanceHistory1.get(nickname).getAttendanceTimes().size()).isEqualTo(2);
-        assertThat(attendanceHistory1.get(nickname).getAttendanceTimes()).containsExactlyInAnyOrder(attendanceTime, anotherAttendanceTime);
+        assertThat(attendanceHistory1.get(nicknameJenson).getAttendanceTimes().size()).isEqualTo(2);
+        assertThat(
+            attendanceHistory1.get(nicknameJenson).getAttendanceTimes()).containsExactlyInAnyOrder(
+            attendanceTime1, anotherAttendanceTime);
     }
 
     @DisplayName("이미 해당 날짜에 출석 기록이 존재한다면, 예외를 던진다")
     @Test
     void already_attendance_history_in_date_then_throw_exception() {
         //given
-        String nickname = "젠슨";
-        AttendanceTime attendanceTime = AttendanceTime.from(LocalDateTime.of(2024, 12, 3, 10, 0));
-        attendanceHistory.add(nickname, attendanceTime);
-        AttendanceTime duplicateAttendanceTime = AttendanceTime.from(LocalDateTime.of(2024, 12, 3, 11, 0));
+        AttendanceTime duplicateAttendanceTime = AttendanceTime.from(
+            LocalDateTime.of(2024, 12, 3, 11, 0));
 
         //when, then
-        assertThatThrownBy(() -> attendanceHistory.add(nickname, duplicateAttendanceTime))
+        assertThatThrownBy(() -> attendanceHistory.add(nicknameJenson, duplicateAttendanceTime))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("해당 날짜에 이미 출석했습니다. 수정 기능을 이용해주세요.");
     }
@@ -74,11 +75,9 @@ public class AttendanceHistoryTest {
     @DisplayName("날짜를 입력받고, 해당 날짜의 크루에 대한 출석 기록이 존재하지 않는다면, 예외를 발생시켜야 한다.")
     @Test
     void given_date_and_find_attendance_but_not_exist_then_throw_exception() {
-        String nickname = "젠슨";
-        int findDate = 3;
-        AttendanceTime attendanceTime = AttendanceTime.from(LocalDateTime.of(2024, 12, 4, 10, 0));
-        attendanceHistory.add(nickname, attendanceTime);
-        assertThatThrownBy(() -> attendanceHistory.getAttendanceTimeByDate(nickname, findDate))
+        int findDate = 4;
+        assertThatThrownBy(
+            () -> attendanceHistory.getAttendanceTimeByDate(nicknameJenson, findDate))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("해당 날짜에는 출석 기록이 없습니다.");
     }
@@ -86,27 +85,24 @@ public class AttendanceHistoryTest {
     @DisplayName("크루의 닉네임과 날짜(date)를 입력받았을 때, 해당하는 출석 기록을 가져온다")
     @Test
     void given_nickname_and_date_then_return_attendance_time() {
-        String nickname = "젠슨";
         int findDate = 3;
-        AttendanceTime attendanceTime = AttendanceTime.from(LocalDateTime.of(2024, 12, 3, 10, 0));
-        attendanceHistory.add(nickname, attendanceTime);
         LocalDate expectedAttendanceDate = LocalDate.of(2024, 12, 3);
-        AttendanceTime foundAttendanceTime = attendanceHistory.getAttendanceTimeByDate(nickname, findDate);
+        AttendanceTime foundAttendanceTime = attendanceHistory.getAttendanceTimeByDate(
+            nicknameJenson, findDate);
         assertThat(foundAttendanceTime.getDate()).isEqualTo(expectedAttendanceDate);
     }
 
     @DisplayName("기존 시간과 수정 시간이 주어졌을 때, 기존 출석 기록을 제거 후, 새로운 출석 기록을 삽입한다")
     @Test
     void remove_before_attendance_and_insert_new_attendance() {
-        String nickname = "젠슨";
         int findDate = 3;
-        AttendanceTime attendanceTime = AttendanceTime.from(LocalDateTime.of(2024, 12, 3, 10, 0));
-        attendanceHistory.add(nickname, attendanceTime);
         LocalDateTime modifyTime = LocalDateTime.of(2024, 12, 3, 10, 0);
-        AttendanceTime modifyAttendanceTime = attendanceHistory.modifyAttendance(nickname, attendanceTime, modifyTime);
-        assertThat(modifyAttendanceTime.getAttendanceDateTime().toLocalTime()).isEqualTo(LocalTime.of(10, 0));
-        AttendanceTime attendanceTimeByDate = attendanceHistory.getAttendanceTimeByDate(nickname,
-            findDate);
+        AttendanceTime modifyAttendanceTime = attendanceHistory.modifyAttendance(nicknameJenson,
+            attendanceTime1, modifyTime);
+        AttendanceTime attendanceTimeByDate = attendanceHistory.getAttendanceTimeByDate(
+            nicknameJenson, findDate);
+        assertThat(modifyAttendanceTime.getAttendanceDateTime().toLocalTime()).isEqualTo(
+            LocalTime.of(10, 0));
         assertThat(attendanceTimeByDate.getAttendanceDateTime()).isEqualTo(modifyTime);
     }
 }
