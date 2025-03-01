fc4d3f43d1018f8338a06a7be4fc5f2db4279f33
LJM\eueo8 <eueo8259@gmail.com>
2025-03-01 16:28:11 +0900

    feat : 제적 위험 학생 찾는 메서드 구현

--- src/main/java/AttendanceBook.java
+++ src/main/java/AttendanceBook.java
@@ -1,12 +1,17 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class AttendanceBook {
-    List<Student> attendanceBook;
+    private final List<Student> attendanceBook;
+
+    public AttendanceBook(List<Student> attendanceBook) {
+        this.attendanceBook = attendanceBook;
+    }
 
     public AttendanceBook(Map<String, List<LocalDateTime>> fileAttendanceRecord){
         attendanceBook = fileAttendanceRecord.entrySet().stream()
@@ -20,5 +25,12 @@ public class AttendanceBook {
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 찾는 학생이 존재하지 않습니다."));
         return student.findAttendanceTimeRecordMap();
+    }
+
+    public List<Student> findExpulsionRiskStudents() {
+        return attendanceBook.stream()
+                .filter(student -> student.calculateAbsentCount() >= 3)
+                .sorted(Comparator.comparing(Student::calculateAbsentCount))
+                .collect(Collectors.toList());
     }
 }
