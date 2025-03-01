1e5c3c3944507ebefee60ee7d93ba446a33a09d3
Yehyeok Bang <qkddpgur318@gmail.com>
2025-03-01 17:56:13 +0900

    refactor: 닉네임 길이 조건 상수화

--- src/main/java/attendance/model/Nickname.java
+++ src/main/java/attendance/model/Nickname.java
@@ -6,6 +6,8 @@ import java.util.regex.Pattern;
 public class Nickname implements Comparable<Nickname> {
 
     private static final Pattern KOREAN_ONLY_PATTERN = Pattern.compile("^[가-힣]+$");
+    private static final int MIN_NICKNAME_LENGTH = 2;
+    private static final int MAX_NICKNAME_LENGTH = 4;
 
     private final String value;
 
@@ -21,8 +23,9 @@ public class Nickname implements Comparable<Nickname> {
         if (nickname.isBlank()) {
             throw new IllegalArgumentException("닉네임이 공백으로만 구성되어 있습니다.");
         }
-        if (nickname.length() < 2 || 4 < nickname.length()) {
-            throw new IllegalArgumentException("닉네임 길이는 2자 이상 4자 이하여야 합니다. 입력: %s".formatted(nickname));
+        if (nickname.length() < MIN_NICKNAME_LENGTH || MAX_NICKNAME_LENGTH < nickname.length()) {
+            throw new IllegalArgumentException("닉네임 길이는 %d자 이상 %d자 이하여야 합니다. 입력: %s"
+                    .formatted(MIN_NICKNAME_LENGTH, MAX_NICKNAME_LENGTH, nickname));
         }
         if (!KOREAN_ONLY_PATTERN.matcher(nickname).matches()) {
             throw new IllegalArgumentException("닉네임은 한글만 사용할 수 있습니다. 입력: %s".formatted(nickname));
