3d7a6f09edfcd79d8690862fdf4f4f92bfd742bc
soeun2537 <soeun2537@gmail.com>
2025-03-01 15:53:07 +0900

    feat: 시간 형식을 검증하는 로직 구현

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -3,6 +3,8 @@ package constant;
 public enum ErrorMessage {
 
     INVALID_INPUT_NULL_OR_BLANK("입력 값은 null이거나 공백일 수 없습니다."),
+    INVALID_INTEGER_FORMAT("해당 입력 값은 정수이어야 합니다."),
+    INVALID_TIME_FORMAT("해당 입력 값은 시간 형식이어야 합니다. (ex. 09:24)"),
     ;
 
     private static final String prefix = "[ERROR] ";
--- src/main/java/util/InputValidator.java
+++ src/main/java/util/InputValidator.java
@@ -2,6 +2,7 @@ package util;
 
 import static constant.ErrorMessage.INVALID_INPUT_NULL_OR_BLANK;
 import static constant.ErrorMessage.INVALID_INTEGER_FORMAT;
+import static constant.ErrorMessage.INVALID_TIME_FORMAT;
 
 public class InputValidator {
 
@@ -19,6 +20,14 @@ public class InputValidator {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_INTEGER_FORMAT.getMessage());
+        }
+    }
+
+    public static void validateTime(String input) {
+        String timeRegex = "^([0-1]\\d|2[0-3]):[0-5]\\d$";
+
+        if (!input.matches(timeRegex)) {
+            throw new IllegalArgumentException(INVALID_TIME_FORMAT.getMessage());
         }
     }
 }
