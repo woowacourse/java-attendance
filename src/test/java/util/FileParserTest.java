22aa5ae93262e652bc966cb0e57d6849702e996d
soeun2537 <soeun2537@gmail.com>
2025-03-01 18:47:17 +0900

    test: 파일을 불러오는 단위 테스트 추가

---
+++ src/test/java/util/FileParserTest.java
@@ -0,0 +1,23 @@
+package util;
+
+import static constant.PathConstant.ATTENDANCE_FILE_PATH;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FileParserTest {
+
+    @Test
+    @DisplayName("파일을 불러온다.")
+    void test() {
+        // given
+
+        // when
+        List<String> lines = FileParser.readLines(ATTENDANCE_FILE_PATH.getPath());
+
+        // then
+        assertThat(lines).hasSize(4);
+    }
+}
