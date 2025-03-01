38fbe5e86ece4141eb79c1545d3c5b1660c6d51b
yush <yush2202@gmail.com>
2025-03-02 03:01:17 +0900

    feat: 시간 비교 기능 구현

--- src/main/java/domain/Time.java
+++ src/main/java/domain/Time.java
@@ -15,4 +15,18 @@ public class Time {
         int minute = Integer.parseInt(splittedTime[1]);
         return LocalTime.of(hour, minute);
     }
+
+    private boolean isBeforeAndSame(LocalTime startTime) {
+        LocalTime currentTime = convertTime();
+        return startTime.isBefore(currentTime) || startTime.equals(currentTime);
+    }
+
+    private boolean isAfterAndSame(LocalTime endTime) {
+        LocalTime currentTime = convertTime();
+        return endTime.isAfter(currentTime) || endTime.equals(currentTime);
+    }
+
+    public boolean isBetweenTime(LocalTime startTime, LocalTime endTime) {
+        return isBeforeAndSame(startTime) && isAfterAndSame(endTime);
+    }
 }
--- src/test/java/domain/TimeTest.java
+++ src/test/java/domain/TimeTest.java
@@ -5,6 +5,8 @@ import java.time.LocalTime;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class TimeTest {
     @Test
@@ -15,5 +17,29 @@ class TimeTest {
         Time time = new Time(rawTime);
 
         Assertions.assertEquals(time.convertTime(), LocalTime.of(11, 0));
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력된 시간이 지정된 시간 사이에 있으면 참을 반환합니다.")
+    @ValueSource(strings = {"08:00", "12:00", "23:00"})
+    void betweenTimeTrueTest(String value) {
+        LocalTime startTime = LocalTime.of(8, 0);
+        LocalTime endTime = LocalTime.of(23, 0);
+
+        Time time = new Time(value);
+
+        Assertions.assertTrue(time.isBetweenTime(startTime, endTime));
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력된 시간이 지정된 시간 사이에 없으면 거짓을 반환합니다.")
+    @ValueSource(strings = {"00:00", "07:59", "23:01"})
+    void betweenTimeFalseTest(String value) {
+        LocalTime startTime = LocalTime.of(8, 0);
+        LocalTime endTime = LocalTime.of(23, 0);
+
+        Time time = new Time(value);
+
+        Assertions.assertFalse(time.isBetweenTime(startTime, endTime));
     }
 }
