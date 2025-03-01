11156d071a42e39f0ac82abb76349411ed58d6e6
jhpark1227 <junhyeok0246@naver.com>
2025-03-02 02:06:10 +0900

    test: Crews 단위테스트 작성 (닉네임을 찾을 수 없으면 예외를 발생시킨다)

--- src/test/java/domain/CrewsTest.java
+++ src/test/java/domain/CrewsTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Set;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,16 @@ class CrewsTest {
         Crew actual = crews.findByNickname("쿠키").get();
         //then
         assertThat(actual).isEqualTo(crew);
+    }
+
+    @Test
+    void 닉네임을_찾을수_없으면_예외를_발생시킨다() {
+        //given
+        Crew crew = new Crew("쿠키");
+        Crews crews = new Crews(Set.of(crew));
+        //when & then
+        assertThatThrownBy(() -> crews.findByNickname("쿠기"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 닉네임의 크루가 존재하지 않습니다.");
     }
 }
