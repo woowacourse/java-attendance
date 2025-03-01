ead482eb25040f3bc696493ea79f446192f43b39
jumdo12 <jumdo12@gmail.com>
2025-03-01 22:07:06 +0900

    feat: 파일 입력 기능 추가

---
+++ src/test/java/view/FileInputViewTest.java
@@ -0,0 +1,28 @@
+package view;
+
+import domain.CrewGroup;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+
+class FileInputViewTest {
+    @DisplayName("파일 입력 테스트")
+    @Test
+    void FilesTest(){
+        try {
+            CrewGroup crews= FileInputView.loadInitFileData();
+
+            Assertions.assertThat(crews.has("빙봉")).isTrue();
+            Assertions.assertThat(crews.has("빙티")).isTrue();
+            Assertions.assertThat(crews.has("이든")).isTrue();
+            Assertions.assertThat(crews.has("가이온")).isFalse();
+
+            Assertions.assertThat(crews.findByName("빙봉").getAttendanceRecord().getAbsenceCount()).isEqualTo(2);
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
