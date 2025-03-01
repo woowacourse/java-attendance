e1148dedd1fe14a76fe6541b95a008bef9c5673c
jhpark1227 <junhyeok0246@naver.com>
2025-03-02 00:43:24 +0900

    test: Attendances 단위테스트 작성 (출석 유형별 횟수를 조회한다)

--- src/test/java/AttendancesTest.java
+++ src/test/java/AttendancesTest.java
@@ -3,12 +3,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.Attendance;
 import domain.AttendanceTime;
+import domain.AttendanceType;
 import domain.Attendances;
 import domain.Crew;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class AttendancesTest {
@@ -74,5 +76,27 @@ public class AttendancesTest {
 
         //then
         assertThat(filteredAttendances).isEqualTo(expected);
+    }
+
+    @Test
+    void 출석유형별_횟수를_조회한다() {
+        //given
+        Crew crew = new Crew("쿠키");
+        AttendanceTime attendanceTime1 = new AttendanceTime(LocalDateTime.of(2024, 12, 3, 9, 30));
+        AttendanceTime attendanceTime2 = new AttendanceTime(LocalDateTime.of(2024, 12, 4, 10, 6));
+        AttendanceTime attendanceTime3 = new AttendanceTime(LocalDateTime.of(2024, 12, 5, 10, 31));
+        Attendance attendance1 = Attendance.of(crew, attendanceTime1);
+        Attendance attendance2 = Attendance.of(crew, attendanceTime2);
+        Attendance attendance3 = Attendance.of(crew, attendanceTime3);
+
+        Attendances attendances = new Attendances(List.of(attendance1, attendance2, attendance3));
+        Map<AttendanceType, Integer> expected = Map.of(AttendanceType.SUCCESS, 1, AttendanceType.LATE, 1,
+                AttendanceType.ABSENCE, 1);
+
+        //when
+        Map<AttendanceType, Integer> actual = attendances.countAttendanceType();
+
+        //then
+        assertThat(actual).isEqualTo(expected);
     }
 }
