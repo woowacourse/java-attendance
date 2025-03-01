50af79eefb410cea1559a0d6f37b12cfe4e03968
soeun2537 <soeun2537@gmail.com>
2025-03-01 21:18:47 +0900

    feat: 지각인 경우를 확인하는 로직 구현

---
+++ src/main/java/model/AttendanceTime.java
@@ -0,0 +1,45 @@
+package model;
+
+import static constant.ErrorMessage.CANNOT_CHECK_IN_ON_WEEKEND;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+
+public enum AttendanceTime {
+
+    MONDAY(DayOfWeek.MONDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(13, 0)),
+    TUESDAY(DayOfWeek.TUESDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    WEDNESDAY(DayOfWeek.WEDNESDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    THURSDAY(DayOfWeek.THURSDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    FRIDAY(DayOfWeek.FRIDAY, LocalTime.of(8, 0), LocalTime.of(23, 0), LocalTime.of(10, 0)),
+    ;
+
+    private final DayOfWeek dayOfWeek;
+    private final LocalTime operationStartTime;
+    private final LocalTime operationEndTime;
+    private final LocalTime educationStartTime;
+
+    AttendanceTime(DayOfWeek dayOfWeek, LocalTime operationStartTime,
+                   LocalTime operationEndTime, LocalTime educationStartTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.operationStartTime = operationStartTime;
+        this.operationEndTime = operationEndTime;
+        this.educationStartTime = educationStartTime;
+    }
+
+    public static boolean isLate(LocalDate localDate, LocalTime localTime, int beLateTime, int absenceTime) {
+        AttendanceTime attendanceTime = find(localDate);
+
+        return localTime.isAfter(attendanceTime.educationStartTime.plusMinutes(beLateTime)) &&
+                localTime.isBefore(attendanceTime.educationStartTime.plusMinutes(absenceTime));
+    }
+
+    private static AttendanceTime find(LocalDate localDate) {
+        return Arrays.stream(AttendanceTime.values())
+                .filter(attendanceTime -> attendanceTime.dayOfWeek.equals(localDate.getDayOfWeek()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(CANNOT_CHECK_IN_ON_WEEKEND.getMessage()));
+    }
+}
