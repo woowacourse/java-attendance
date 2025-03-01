92ce917c921279ee31c1268e0656b1bfa6dc8b6a
황재웅 <wodnd0131@naver.com>
2025-03-01 23:55:53 +0900

    test: 출석 상태 통계 반환

--- src/test/java/attendance/domain/AttendancesTest.java
+++ src/test/java/attendance/domain/AttendancesTest.java
@@ -1,8 +1,10 @@
 package attendance.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
 import java.time.LocalDateTime;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +19,7 @@ class AttendancesTest {
         LocalDateTime weekend = LocalDateTime.of(2024, 12, 14, 10, 0);
         var attendances = new Attendances(systemDateTime);
 
-        Assertions.assertThatThrownBy(() -> attendances.addAttendance(weekend))
+        assertThatThrownBy(() -> attendances.addAttendance(weekend))
             .isInstanceOf(AttendanceArgumentException.class)
             .hasMessageContaining("12월 14일 토요일은 등교일이 아닙니다.");
     }
@@ -28,7 +30,7 @@ class AttendancesTest {
         LocalDateTime holiday = LocalDateTime.of(2024, 12, 25, 10, 0);
         var attendances = new Attendances(systemDateTime);
 
-        Assertions.assertThatThrownBy(() -> attendances.addAttendance(holiday))
+        assertThatThrownBy(() -> attendances.addAttendance(holiday))
             .isInstanceOf(AttendanceArgumentException.class)
             .hasMessageContaining("12월 25일 수요일은 등교일이 아닙니다.");
     }
@@ -40,8 +42,28 @@ class AttendancesTest {
         var attendances = new Attendances(systemDateTime);
         attendances.addAttendance(duplicate);
 
-        Assertions.assertThatThrownBy(() -> attendances.addAttendance(duplicate))
+        assertThatThrownBy(() -> attendances.addAttendance(duplicate))
             .isInstanceOf(AttendanceArgumentException.class)
             .hasMessageContaining("이미 출석하였습니다. 수정 기능을 이용해주세요.");
+    }
+
+    @Test
+    @DisplayName("출석 상태 통계를 반환한다.")
+    void test_returnAttendanceStatusStatistic() {
+        var attendance = LocalDateTime.of(2024, 12, 10, 10, 0);
+        var late = LocalDateTime.of(2024, 12, 11, 10, 10);
+        var absence = LocalDateTime.of(2024, 12, 12, 10, 30);
+        var attendances = new Attendances(systemDateTime);
+        attendances.addAttendance(attendance);
+        attendances.addAttendance(late);
+        attendances.addAttendance(absence);
+
+        AttendanceStatusStatistics statusStatics = attendances.calculateStatics();
+        Assertions.assertAll(
+            () -> assertThat(statusStatics.get(AttendanceStatus.ATTENDANCE)).isEqualTo(1),
+            () -> assertThat(statusStatics.get(AttendanceStatus.LATE)).isEqualTo(1),
+            () -> assertThat(statusStatics.get(AttendanceStatus.ABSENCE)).isEqualTo(1)
+        );
+
     }
 }
