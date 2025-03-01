f4910ea093aad37527cabaadf2310968b080e250
soeun2537 <soeun2537@gmail.com>
2025-03-02 05:00:01 +0900

    feat: 공휴일 출석 에러 메시지 상수 추출

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -12,6 +12,7 @@ public enum ErrorMessage {
     INVALID_OPTION_FORMAT("유효한 옵션이 아닙니다."),
 
     CANNOT_CHECK_IN_ON_WEEKEND("주말에는 출석할 수 없습니다."),
+    CANNOT_CHECK_IN_ON_HOLIDAY("공휴일에는 출석할 수 없습니다."),
     NOT_FOUND_CREW("존재하지 않는 크루입니다."),
     NOT_FOUND_ATTENDANCE("존재하지 않는 출석 내역입니다."),
     OUT_OF_OPERATION_HOURS("지금은 운영 시간이 아닙니다."),
