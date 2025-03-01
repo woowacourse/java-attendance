35f9eb3e33a48a637f12b0a0a57a0c1ac6b77c3e
junhaa <2171326@hansung.ac.kr>
2025-03-01 21:53:12 +0900

    feat: 사용자로부터 실행할 기능 번호를 입력받는 기능 구현

---
+++ src/main/java/attendance/view/InputView.java
@@ -0,0 +1,63 @@
+package attendance.view;
+
+import attendance.controller.AttendanceCommand;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class InputView {
+    private static final String COMMAND_MESSAGE_HEADER_FORMAT = "오늘은 %02d월 %02d일 %s입니다. 기능을 선택해 주세요.\n";
+    private static final String COMMAND_PROMPT_FORMAT = "%s. %s\n";
+
+
+    private final BufferedReader bufferedReader;
+
+
+    public InputView() {
+        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public String inputCommand() {
+        System.out.print(createCommandMessage());
+        return readLine();
+    }
+
+    private String createCommandMessage() {
+        LocalDate now = LocalDate.now();
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format(COMMAND_MESSAGE_HEADER_FORMAT, now.getMonthValue(), now.getDayOfMonth(),
+                getDisplayName(now)));
+        Arrays.stream(AttendanceCommand.values())
+                .forEach(command -> {
+                    builder.append(
+                            String.format(COMMAND_PROMPT_FORMAT, command.getCommand(), command.getDescription()));
+                });
+
+        return builder.toString();
+    }
+
+    private static String getDisplayName(LocalDate date) {
+        return date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA);
+    }
+
+    private String readLine() {
+        try {
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException("입력 중 오류가 발생했습니다.", e);
+        }
+    }
+
+    public void close() {
+        try {
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException("입력 스트림을 닫는데 실패했습니다.", e);
+        }
+    }
+
+}
