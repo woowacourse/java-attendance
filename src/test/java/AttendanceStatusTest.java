7af885213927d7c96371e48604aa604d64714e1a
LJM\eueo8 <eueo8259@gmail.com>
2025-03-01 17:24:43 +0900

    refactor : 메서드 수정에 따른 테스트 수정

--- src/test/java/AttendanceStatusTest.java
+++ src/test/java/AttendanceStatusTest.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,8 @@ public class AttendanceStatusTest {
     @DisplayName("월요일을 제외한 입력 받은 출석 시간 전에 도착한 경우 출석 상태 가져오기")
     void 입력_받은_출석_시간으로_출석_상태_가져오기(){
         LocalDate today = LocalDate.of(2024,12,13);
-        String input = "09:03";
+        LocalTime input = LocalTime.parse("09:03");
+
         AttendanceStatus expect = AttendanceStatus.ATTENDANCE;
         AttendanceStatus result = AttendanceStatus.attendanceStatusCalculate(today, input);
         Assertions.assertEquals(expect, result);
@@ -18,7 +20,8 @@ public class AttendanceStatusTest {
     @DisplayName("월요일을 제외한 입력 받은 출석 시간 5분 이후 도착한 경우 지각 상태 가져오기")
     void 입력_받은_지각_시간으로_지각_상태_가져오기(){
         LocalDate today = LocalDate.of(2024,12,13);
-        String input = "10:06";
+        LocalTime input = LocalTime.parse("10:06");
+
         AttendanceStatus expect = AttendanceStatus.LATE;
         AttendanceStatus result = AttendanceStatus.attendanceStatusCalculate(today, input);
         Assertions.assertEquals(expect, result);
@@ -28,7 +31,7 @@ public class AttendanceStatusTest {
     @DisplayName("월요일을 제외한 입력 받은 출석 시간을 30분 이후 도착한 경우 결석 상태 가져오기")
     void 입력_받은_결석_시간으로_결석_상태_가져오기(){
         LocalDate today = LocalDate.of(2024,12,13);
-        String input = "10:31";
+        LocalTime input = LocalTime.parse("10:31");
         AttendanceStatus expect = AttendanceStatus.ABSENT;
         AttendanceStatus result = AttendanceStatus.attendanceStatusCalculate(today, input);
         Assertions.assertEquals(expect, result);
@@ -38,7 +41,8 @@ public class AttendanceStatusTest {
     @DisplayName("월요일인 경우 입력 받은 출석 시간으로 출석 상태 가져오기")
     void 월요일의_경우_입력_받은_출석_시간으로_상태_가져오기(){
         LocalDate today = LocalDate.of(2024,12,9);
-        String input = "13:00";
+        LocalTime input = LocalTime.parse("13:00");
+
         AttendanceStatus expect = AttendanceStatus.ATTENDANCE;
         AttendanceStatus result = AttendanceStatus.attendanceStatusCalculate(today, input);
         Assertions.assertEquals(expect, result);
