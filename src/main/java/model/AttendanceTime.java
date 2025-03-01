431b2db09359f1168cf1229f8505acf2faf046be
(minjae8563) <(minjae8563@naver.com)>
2025-03-01 17:15:18 +0900

    feat : 멤버 변수로 LocalTime 을 가지고 있는 클래스 구현

---
+++ src/main/java/model/AttendanceTime.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.time.LocalTime;
+
+public class AttendanceTime {
+    private final LocalTime attendanceTime;
+    private static final LocalTime OPEN_TIME = LocalTime.of(8, 0);
+    private static final LocalTime END_TIME = LocalTime.of(23, 0);
+
+    public AttendanceTime(LocalTime attendanceTime) {
+        this.attendanceTime = attendanceTime;
+    }
+
+    public boolean isAfter(AttendanceTime wantToCompareAttendanceTime) {
+        return attendanceTime.isAfter(wantToCompareAttendanceTime.toLocalTime());
+    }
+
+    public boolean isBefore(AttendanceTime wantToCompareAttendanceTime){
+        return attendanceTime.isBefore(wantToCompareAttendanceTime.toLocalTime());
+    }
+
+    public boolean isEqual(AttendanceTime wantToCompareAttendanceTime) {
+        return attendanceTime.equals(wantToCompareAttendanceTime.toLocalTime());
+    }
+
+    public boolean isNotOpeningTime() {
+        return attendanceTime.isBefore(OPEN_TIME) || attendanceTime.isAfter(END_TIME);
+    }
+
+    public LocalTime toLocalTime() {
+        return this.attendanceTime;
+    }
+}
