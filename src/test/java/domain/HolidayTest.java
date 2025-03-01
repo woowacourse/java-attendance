e738ec91fbbc9f2689fcfabff957694010ec3a58
threepebbles <threepebbles@naver.com>
2025-03-01 17:57:25 +0900

    refactor: 테스트 픽스처 사용하도록 수정

--- src/test/java/domain/HolidayTest.java
+++ src/test/java/domain/HolidayTest.java
@@ -1,6 +1,6 @@
 package domain;
 
-import java.time.LocalDate;
+import fixture.LocalDateFixture;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,14 +9,12 @@ class HolidayTest {
     @Test
     @DisplayName("공휴일이면 true를 반환한다")
     void isHoliday_true() {
-        LocalDate christmas = LocalDate.of(2025, 12, 25);
-        Assertions.assertThat(Holiday.isHoliday(christmas)).isTrue();
+        Assertions.assertThat(Holiday.isHoliday(LocalDateFixture.CHRISTMAS)).isTrue();
     }
 
     @Test
     @DisplayName("공휴일이 아니면 false를 반환한다")
     void isHoliday_false() {
-        LocalDate christmas = LocalDate.of(2025, 12, 10);
-        Assertions.assertThat(Holiday.isHoliday(christmas)).isFalse();
+        Assertions.assertThat(Holiday.isHoliday(LocalDateFixture.CHRISTMAS)).isFalse();
     }
 }
