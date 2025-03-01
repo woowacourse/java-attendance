28683043e21e1a8ff826b341276a62c26bfd35f1
julie <7jujubuilds@gmail.com>
2025-03-02 00:10:00 +0900

    test(crew_records): 출석 기록 수정 관련 테스트 작성
    테스트 내용:
    - 입력 받은 날짜의 출석 시간을 수정할 수 있다

--- src/test/java/domain/CrewRecordsTest.java
+++ src/test/java/domain/CrewRecordsTest.java
@@ -3,9 +3,13 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class CrewRecordsTest {
@@ -37,5 +41,28 @@ class CrewRecordsTest {
 
         // then
         assertDoesNotThrow(() -> crewRecords.addRecord(crew, record));
+    }
+
+    @DisplayName("입력 받은 날짜의 출석 시간을 수정할 수 있다.")
+    @Test
+    void editRecordTest() {
+        // given
+        CrewRecords crewRecords = new CrewRecords();
+        Crew crew = new Crew("저스틴");
+        AttendanceRecord oldRecord = new AttendanceRecord(LocalDateTime.parse("2024-12-03T13:10"));
+        LocalTime newTime = LocalTime.of(13, 0);
+        LocalDate oldDate = LocalDate.of(2024, 12, 3);
+
+        // when
+        crewRecords.addCrewRecords(crew, new AttendanceRecords());
+        crewRecords.addRecord(crew, oldRecord);
+        crewRecords.editRecord(oldDate, newTime);
+
+        // then
+        assertAll(
+                () -> assertThat(oldRecord.getAttendanceStatus()).isEqualTo(AttendanceStatus.TARDY),
+                () -> assertThat(crewRecords.getRecordOnDate(crew, oldDate).getAttendanceStatus()).isEqualTo(AttendanceStatus.PRESENT)
+        );
+
     }
 }
