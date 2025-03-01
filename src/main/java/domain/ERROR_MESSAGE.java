6c70133886f5c63fc184c051bb2b81e4d09fff9a
jye326 <jye326@naver.com>
2025-03-01 20:25:58 +0900

    feat(view) : UserInputView
    - 메뉴 옵션 기능 추가

--- src/main/java/domain/ERROR_MESSAGE.java
+++ src/main/java/domain/ERROR_MESSAGE.java
@@ -4,7 +4,10 @@ public enum ERROR_MESSAGE {
     CLOSED_DAY("오늘은 캠퍼스 휴장일입니다."),
     CLOSED_TIME("지금은 캠퍼스 휴장시간입니다."),
     ALREADY_ATTENDED("이미 출석하였습니다. 수정을 원하시면 출석 수정 기능을 이용하세요."),
-    EMPTY_DATE("등록되지 않은 날짜입니다.")
+    EMPTY_DATE("등록되지 않은 날짜입니다."),
+    INVALID_INPUT("올바르지 않은 입력값입니다."),
+    NO_ATTENDANCES_FILE("출석 기록 파일을 찾을 수 없습니다"),
+    INVALID_FILE_TIME_FORMAT("출석 정보 저장 형식이 잘못되었습니다. attendances.csv 파일을 확인해주세요")
     ;
 
     private final String message;
--- src/main/java/view/UserInputView.java
+++ src/main/java/view/UserInputView.java
@@ -1,12 +1,19 @@
 package view;
 
+import constant.MenuOption;
+
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import static constant.MenuOption.*;
+import static domain.ERROR_MESSAGE.*;
 
 public class UserInputView {
     public static String askCrewName() {
         System.out.println("닉네임을 입력해 주세요.");
         return new Scanner(System.in).nextLine();
     }
+
     public static String askAttendanceTime() {
         System.out.println("등교 시간을 입력해 주세요");
         return new Scanner(System.in).nextLine();
@@ -20,5 +27,21 @@ public class UserInputView {
     public static String askTimeForModify() {
         System.out.println("언제로 변경하겠습니까?");
         return new Scanner(System.in).nextLine();
+    }
+
+    public static MenuOption askMenuOption() {
+        OutputView.printMenu();
+        String option = (new Scanner(System.in).nextLine());
+        if (option.equals("1"))
+            return CHECK_ATTENDANCE;
+        if (option.equals("2"))
+            return MODIFY_ATTENDANCE;
+        if (option.equals("3"))
+            return CHECK_CREW_ATTENDANCE_HISTORY;
+        if (option.equals("4"))
+            return CHECK_PENALTY_RECEIVED_CREW;
+        if (Pattern.matches(QUIT.getOption(), option))
+                return QUIT;
+        throw new IllegalArgumentException(INVALID_INPUT.getMessage());
     }
 }
