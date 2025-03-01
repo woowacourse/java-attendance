7c552d220354e87554d24e81795204230d15d986
jumdo12 <jumdo12@gmail.com>
2025-03-01 22:07:16 +0900

    feat: 포메터 추가

--- src/main/java/util/DateTimeUtils.java
+++ src/main/java/util/DateTimeUtils.java
@@ -4,6 +4,6 @@ import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtils {
     public static final DateTimeFormatter localDayFormatter = DateTimeFormatter.ofPattern("MM월 dd일 ");
-    public static final DateTimeFormatter dateTimeformatter = DateTimeFormatter.ofPattern("HH:mm");
-
+    public static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+    public static final DateTimeFormatter dateTimeInputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 }
