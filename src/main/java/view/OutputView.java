0133b8701dde4bca92d5c533dc56846138c71ea5
jhan0121 <jhan0121@gmail.com>
2025-03-02 00:22:13 +0900

    refactor: 시간 출력 형식 설정 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,12 +2,22 @@ package view;
 
 import domain.Attend;
 import domain.AttendStatus;
+import java.time.LocalTime;
 
 public class OutputView {
     public void printAttendResult(final Attend attend, final AttendStatus attendStatus) {
         String dateFormat = DateTimeFormat.DATE.formatDate(attend.getDate());
-        String timeFormat = DateTimeFormat.TIME.formatTime(attend.getTime());
+        String timeFormat = formatAttendTime(attend);
         String attendStatusFormat = AttendStatusFormat.findStatusFormat(attendStatus);
         System.out.printf("%s %s (%s)%n%n", dateFormat, timeFormat, attendStatusFormat);
+    }
+
+    private String formatAttendTime(Attend attend) {
+        String result = "--:--";
+        if (attend.checkTimeNull()) {
+            LocalTime time = attend.getTime();
+            result = DateTimeFormat.TIME.formatTime(time);
+        }
+        return result;
     }
 }
