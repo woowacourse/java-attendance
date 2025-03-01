7013e7293d26bde54f908efaa00b9142234fbbce
LJM\eueo8 <eueo8259@gmail.com>
2025-03-01 21:57:28 +0900

    feat : 출결 패널티 관리하는 클래스 추가

---
+++ src/main/java/model/AttendancePenalty.java
@@ -0,0 +1,32 @@
+package model;
+
+public enum AttendancePenalty {
+    WARNING("경고", 2),
+    COUNSELING("면담", 3),
+    EXPULSION("제적", 5);
+
+    private final String penalty;
+    private final int penaltyCount;
+
+    AttendancePenalty(String penalty, int penaltyCount) {
+        this.penalty = penalty;
+        this.penaltyCount = penaltyCount;
+    }
+
+    public static AttendancePenalty findPenaltyByAbsentCount(long count){
+        if (count > AttendancePenalty.EXPULSION.penaltyCount){
+            return AttendancePenalty.EXPULSION;
+        }
+        if (count >= AttendancePenalty.COUNSELING.penaltyCount){
+            return AttendancePenalty.COUNSELING;
+        }
+        if (count >= AttendancePenalty.WARNING.penaltyCount){
+            return AttendancePenalty.WARNING;
+        }
+        return null;
+    }
+
+    public String getPenalty() {
+        return penalty;
+    }
+}
