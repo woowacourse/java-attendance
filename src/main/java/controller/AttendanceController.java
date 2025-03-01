2a6785171774c40832e36e3b6685dfe118f56c0d
soeun2537 <soeun2537@gmail.com>
2025-03-01 18:07:05 +0900

    feat: Controller 재시도 로직 추가

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -4,7 +4,8 @@ import dto.AttendanceCheckInRequest;
 import dto.AttendanceHistoryRequest;
 import dto.AttendanceOptionRequest;
 import dto.AttendanceUpdateRequest;
-import java.time.LocalDateTime;
+import java.util.function.Supplier;
+import model.Option;
 import util.DateTimeGenerator;
 import view.InputView;
 
@@ -17,14 +18,66 @@ public class AttendanceController {
     }
 
     public void run() {
-        LocalDateTime now = LocalDateTime.now();
 
+        while (true) {
+            Option option = processWithRetry(this::option);
+            
+            if (option.equals(Option.ONE)) {
+                processWithRetry(this::checkIn);
+            }
+            if (option.equals(Option.TWO)) {
+                processWithRetry(this::checkIn);
+            }
+            if (option.equals(Option.THREE)) {
+                processWithRetry(this::update);
+            }
+            if (option.equals(Option.FOUR)) {
+                processWithRetry(this::history);
+            }
+            if (option.equals(Option.QUIT)) {
+                break;
+            }
+        }
+    }
+
+    private Option option() {
         AttendanceOptionRequest attendanceOptionRequest = InputView.readAttendanceOptionRequest(dateTimeGenerator);
+        return Option.find(attendanceOptionRequest.option());
+    }
 
+    private void checkIn() {
         AttendanceCheckInRequest attendanceCheckInRequest = InputView.readAttendanceCheckInRequest();
 
+    }
+
+    private void update() {
         AttendanceUpdateRequest attendanceUpdateRequest = InputView.readAttendanceUpdateRequest(dateTimeGenerator);
 
+    }
+
+    private void history() {
         AttendanceHistoryRequest attendanceHistoryRequest = InputView.readAttendanceHistoryRequest();
+
+    }
+
+    private <T> T processWithRetry(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void processWithRetry(Runnable runnable) {
+        while (true) {
+            try {
+                runnable.run();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
