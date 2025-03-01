dfb79e61d87911ebb21a905be527952e7f3b932c
황재웅 <wodnd0131@naver.com>
2025-03-01 16:39:13 +0900

    feat: csv 정보 불러오기

--- src/main/java/attendance/domain/AttendanceReader.java
+++ src/main/java/attendance/domain/AttendanceReader.java
@@ -1,4 +1,32 @@
 package attendance.domain;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class AttendanceReader {
+    private String fileName;
+
+    public AttendanceReader(String fileName) {
+        this.fileName = fileName;
+
+    }
+
+    public void load() {
+        InputStream inputStream = getClass().getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalArgumentException("파일을 찾을 수 없습니다: " + fileName);
+        }
+        try (
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader)
+        ) {
+            var lines = bufferedReader.lines()
+                .skip(1)
+                .toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/attendacne/domain/AttendanceReaderTest.java
+++ src/test/java/attendacne/domain/AttendanceReaderTest.java
@@ -1,6 +1,6 @@
 package attendacne.domain;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,13 +9,13 @@ import attendance.domain.AttendanceReader;
 
 public class AttendanceReaderTest {
 
-    private static final String ATTENDANCE_CSV = "/attendance.csv";
+    private static final String ATTENDANCE_CSV = "/attendances.csv";
 
     @Test
     @DisplayName("csv 정보를 불러온다.")
     void test_LoadingCsvWithNoException() {
         var attendanceReader = new AttendanceReader(ATTENDANCE_CSV);
 
-        assertThatNoException(attendanceReader.load());
+        assertDoesNotThrow(attendanceReader::load);
     }
 }
