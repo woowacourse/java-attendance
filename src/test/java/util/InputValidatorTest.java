6d84dc274ad7128fe3d798b2d60c2e006af34953
soeun2537 <soeun2537@gmail.com>
2025-03-01 15:51:59 +0900

    test: 시간 형식을 검증하는 단위 테스트 추가

--- src/test/java/util/InputValidatorTest.java
+++ src/test/java/util/InputValidatorTest.java
@@ -2,6 +2,7 @@ package util;
 
 import static constant.ErrorMessage.INVALID_INPUT_NULL_OR_BLANK;
 import static constant.ErrorMessage.INVALID_INTEGER_FORMAT;
+import static constant.ErrorMessage.INVALID_TIME_FORMAT;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -55,5 +56,40 @@ class InputValidatorTest {
         assertThatThrownBy(() -> InputValidator.validateInteger(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INVALID_INTEGER_FORMAT.getMessage());
+    }
+
+    @Test
+    @DisplayName("시간 형식 입력 시 예외가 발생하지 않는다.")
+    void test5() {
+        // given
+        String input = "09:59";
+
+        // when & then
+        assertThatCode(() -> InputValidator.validateTime(input))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("시간 형식이 아닌 입력 시 예외가 발생한다.")
+    void test6() {
+        // given
+        String input = "09:60";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateTime(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_TIME_FORMAT.getMessage());
+    }
+
+    @Test
+    @DisplayName("시간 형식이 아닌 입력 시 예외가 발생한다.")
+    void test7() {
+        // given
+        String input = "9:00";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateTime(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_TIME_FORMAT.getMessage());
     }
 }
