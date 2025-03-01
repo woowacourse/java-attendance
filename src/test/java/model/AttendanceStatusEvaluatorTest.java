f7d1f8dab540371ef125026fbccffb4ea0f7facb
(minjae8563) <(minjae8563@naver.com)>
2025-03-01 17:35:37 +0900

    test : 메서드, 클래스 명 변경

--- src/test/java/model/AttendanceStatusEvaluatorTest.java
+++ src/test/java/model/AttendanceStatusEvaluatorTest.java
@@ -10,16 +10,16 @@ public class AttendanceStatusEvaluatorTest {
     @Test
     @DisplayName("월요일과 시간을 받아 출석 판단하는 메서드 테스트")
     void test1() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+        Assertions.assertThat(AttendanceStatusEvaluator.calculateAttendanceStatus(
                         new AttendanceDateTime(LocalDateTime.of(2024,12,2,13,0))
-                        , LocalTime.of(13,0)))
+                        , new AttendanceTime(LocalTime.of(13,0))))
                 .isEqualTo(AttendanceStatus.ATTENDANCE);
     }
 
     @Test
     @DisplayName("월요일과 시간을 받아 결석 판단하는 메서드 테스트")
     void test2() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+        Assertions.assertThat(AttendanceStatusEvaluator.calculateAttendanceStatus(
                         new AttendanceDateTime(LocalDateTime.of(2024,12,2,13,0))
                         ,
                         new AttendanceTime(LocalTime.of(13,59))
@@ -30,7 +30,7 @@ public class AttendanceStatusEvaluatorTest {
     @Test
     @DisplayName("월요일과 시간을 받아 지각을 판단하는 메서드 테스트")
     void test3() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+        Assertions.assertThat(AttendanceStatusEvaluator.calculateAttendanceStatus(
                         new AttendanceDateTime(LocalDateTime.of(2024,12,2,13,0))
                         ,
                         new AttendanceTime(LocalTime.of(13,6))
@@ -39,7 +39,7 @@ public class AttendanceStatusEvaluatorTest {
     }@Test
     @DisplayName("화요일과 시간을 받아 출석 판단하는 메서드 테스트")
     void test4() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+        Assertions.assertThat(AttendanceStatusEvaluator.calculateAttendanceStatus(
                         new AttendanceDateTime(LocalDateTime.of(2024,12,3,13,0))
                         ,
                         new AttendanceTime(LocalTime.of(10,0))
@@ -48,7 +48,7 @@ public class AttendanceStatusEvaluatorTest {
     }@Test
     @DisplayName("화요일과 시간을 받아 결석 판단하는 메서드 테스트")
     void test5() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+        Assertions.assertThat(AttendanceStatusEvaluator.calculateAttendanceStatus(
                         new AttendanceDateTime(LocalDateTime.of(2024,12,3,13,0))
                         ,
                         new AttendanceTime(LocalTime.of(13,59))
@@ -57,7 +57,7 @@ public class AttendanceStatusEvaluatorTest {
     }@Test
     @DisplayName("화요일과 시간을 받아 지각을 판단하는 메서드 테스트")
     void test6() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+        Assertions.assertThat(AttendanceStatusEvaluator.calculateAttendanceStatus(
                         new AttendanceDateTime(LocalDateTime.of(2024,12,3,13,0))
                         ,
                         new AttendanceTime(LocalTime.of(10,6))
@@ -66,7 +66,7 @@ public class AttendanceStatusEvaluatorTest {
     }@Test
     @DisplayName("0시 0분에 입실한 학생은 결석 처리하는 메서드 테스트")
     void test7() {
-        Assertions.assertThat(AttendanceCalculator.calculateAttendance(
+        Assertions.assertThat(AttendanceStatusEvaluator.calculateAttendanceStatus(
                         new AttendanceDateTime(LocalDateTime.of(2024,12,3,13,0))
                         ,
                         new AttendanceTime(LocalTime.of(0,0))
