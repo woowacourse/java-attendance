e05f6a2acd15ff9f3b30ebec578d8f0dac116b69
julie <7jujubuilds@gmail.com>
2025-03-02 23:29:11 +0900

    test(attendance_status): 테스트 코드 리팩터링
    - 정확한 테스트를 위한 입력 값 추가
    - when 단계 구분

--- src/test/java/domain/AttendanceStatusTest.java
+++ src/test/java/domain/AttendanceStatusTest.java
@@ -2,6 +2,8 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
@@ -16,19 +18,28 @@ class AttendanceStatusTest {
         DayOfWeek dayOfWeek = DayOfWeek.MONDAY;
         LocalTime time = LocalTime.of(13, 5);
 
+        // when
+        AttendanceStatus expectedValue = AttendanceStatus.PRESENT;
+        AttendanceStatus actualValue = AttendanceStatus.getStatus(dayOfWeek, time);
+
         // then
-        assertThat(AttendanceStatus.getStatus(dayOfWeek, time)).isEqualTo(AttendanceStatus.PRESENT);
+        assertThat(actualValue).isEqualTo(expectedValue);
     }
 
     @DisplayName("출석 시간이 교육 시작 시간으로부터 5분 초과 30분 이내일 경우 지각을 반환한다.")
-    @Test
-    void tardyReturnTest() {
+    @ParameterizedTest
+    @ValueSource(ints = {6, 30})
+    void tardyReturnTest(int minute) {
         // given
         DayOfWeek dayOfWeek = DayOfWeek.MONDAY;
-        LocalTime time = LocalTime.of(13, 10);
+        LocalTime time = LocalTime.of(13, minute);
+
+        // when
+        AttendanceStatus expectedValue = AttendanceStatus.TARDY;
+        AttendanceStatus actualValue = AttendanceStatus.getStatus(dayOfWeek, time);
 
         // then
-        assertThat(AttendanceStatus.getStatus(dayOfWeek, time)).isEqualTo(AttendanceStatus.TARDY);
+        assertThat(actualValue).isEqualTo(expectedValue);
     }
 
     @DisplayName("출석 시간이 교육 시작 시간으로부터 30분 초과할 경우 결석을 반환한다.")
@@ -36,9 +47,13 @@ class AttendanceStatusTest {
     void absentReturnTest() {
         // given
         DayOfWeek dayOfWeek = DayOfWeek.MONDAY;
-        LocalTime time = LocalTime.of(13, 35);
+        LocalTime time = LocalTime.of(13, 31);
+
+        // when
+        AttendanceStatus expectedValue = AttendanceStatus.ABSENT;
+        AttendanceStatus actualValue = AttendanceStatus.getStatus(dayOfWeek, time);
 
         // then
-        assertThat(AttendanceStatus.getStatus(dayOfWeek, time)).isEqualTo(AttendanceStatus.ABSENT);
+        assertThat(actualValue).isEqualTo(expectedValue);
     }
 }
