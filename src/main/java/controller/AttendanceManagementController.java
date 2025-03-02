78a148475f04fede538d5623b6178357dfbc0857
(minjae8563) <(minjae8563@naver.com)>
2025-03-02 11:36:47 +0900

    feat : 오늘 날짜 주입하는 메서드 호출, 1번 기능을 담당하는 메서드 호출

--- src/main/java/controller/AttendanceManagementController.java
+++ src/main/java/controller/AttendanceManagementController.java
@@ -1,18 +1,29 @@
 package controller;
 
-import java.util.Map;
+import java.time.LocalDate;
+import model.AttendanceDate;
 import model.Students;
 import view.InputView;
 
 public class AttendanceManagementController {
+    private final TodayDateGenerator todayDateGenerator;
+
+    public AttendanceManagementController(final TodayDateGenerator todayDateGenerator) {
+        this.todayDateGenerator = todayDateGenerator;
+    }
+
     public void attendanceManagementStart() {
+        Students students = StudentLoaderController.readFileAndMakeStudents();
+        AttendanceDate attendanceStartDate = new AttendanceDate(LocalDate.of(2024, 12, 1));
+        AttendanceDate today = todayDateGenerator.generate();
+        students.updateMissingAttendanceRecords(attendanceStartDate, today);
 
         String userInput = "";
 
         while (!userInput.equals(MenuOption.QUIT.getOption())){
             userInput = InputView.getUserWantMenuUntilValidate();
             if (userInput.equals(MenuOption.ATTENDANCE_CHECK.getOption())) {
-
+                AttendanceCheckController.attendanceCheck(students, today);
             }
             if (userInput.equals(MenuOption.ATTENDANCE_MODIFY.getOption())) {
             }
@@ -22,7 +33,4 @@ public class AttendanceManagementController {
             }
         }
     }
-
-
-
 }
