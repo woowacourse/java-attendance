462f1a4ec404119e215995d91582be4977ba9da0
ppparkta <pppppark2@gmail.com>
2025-03-02 18:22:45 +0900

    style: 불필요한 개행 제거

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,8 +11,7 @@ public class InputView {
 
     public String inputCommand(String today) {
         System.out.printf("오늘은 %s입니다. 기능을 선택해 주세요.\n", today);
-        System.out.println(""
-                + "1. 출석 확인\n"
+        System.out.println("1. 출석 확인\n"
                 + "2. 출석 수정\n"
                 + "3. 크루별 출석 기록 확인\n"
                 + "4. 제적 위험자 확인\n"
@@ -23,7 +22,6 @@ public class InputView {
     public String inputValue() {
         return scanner.next().trim();
     }
-
 
     public void close() {
         this.scanner.close();
