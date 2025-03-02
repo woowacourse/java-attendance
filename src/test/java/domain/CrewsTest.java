d2c499fc76d742c1a3a88f94d020dcdb71ab5450
changuii <rhljh7410@gmail.com>
2025-03-02 14:40:20 +0900

    refactor(CrewsTest): 행위를 기준으로 계층화하여 작성하라

--- src/test/java/domain/CrewsTest.java
+++ src/test/java/domain/CrewsTest.java
@@ -2,8 +2,10 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,8 +13,9 @@ import org.junit.jupiter.api.Test;
 public class CrewsTest {
 
     @Nested
-    @DisplayName("성공 테스트")
-    class SuccessCases {
+    @DisplayName("닉네임에 해당하는 크루를 반환한다.")
+    class FindCrewByName {
+
         @DisplayName("닉네임에 해당하는 크루를 반환한다.")
         @Test
         public void findByName() throws Exception {
@@ -27,15 +30,10 @@ public class CrewsTest {
             // then
             assertThat(actual.getName()).isEqualTo(crewName);
         }
-    }
-
-    @Nested
-    @DisplayName("실패 테스트")
-    class FailCases {
 
         @DisplayName("닉네임에 해당하는 크루가 존재하지 않는다면, 예외가 발생한다.")
         @Test
-        public void findByName() throws Exception {
+        public void findByNameInNotContainsCrew() throws Exception {
             // given
             final var crewName = "우가";
             final var crews = new Crews(List.of());
@@ -46,5 +44,35 @@ public class CrewsTest {
             }).isInstanceOf(IllegalArgumentException.class);
 
         }
+
     }
+
+    @Nested
+    @DisplayName("AttendanceBook에 현재 등록된 크루를 등록한다.")
+    class RegisterCrewsToAttendanceBook {
+
+        @DisplayName("Crews에 등록된 크루들을 올바르게 AttendanceBook에 등록한다.")
+        @Test
+        public void registerCrewsToAttendanceBook() throws Exception {
+            // given
+            final List<String> crewNameList = List.of(
+                    "헤일러", "우가", "포스티", "미소", "부기"
+            );
+            final List<Crew> crewList = crewNameList.stream()
+                    .map(Crew::new)
+                    .collect(Collectors.toList());
+            final var crews = new Crews(crewList);
+            final AttendanceBook attendanceBook = new AttendanceBook();
+
+            // when
+            crews.registerCrewsToAttendanceBook(attendanceBook);
+
+            // then
+            assertSoftly(s -> {
+                crewList.forEach(crew -> s.assertThat(attendanceBook.containsCrew(crew)).isTrue());
+            });
+        }
+
+    }
+
 }
