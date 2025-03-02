86b731d560c82801f67262bf4fe88304034ba330
saera <yook.saera@gmail.com>
2025-03-02 15:54:26 +0900

    test: 출석부에 크루 추가 테스트 작성

---
+++ src/test/java/AttendanceBookTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AttendanceBookTest {
+    @DisplayName("출석부에 신규 크루 추가")
+    @Test
+    void test1() {
+        AttendanceBook attendanceBook = new AttendanceBook();
+
+        attendanceBook.addCrew("빙봉");
+
+        assertThat(attendanceBook.countCrews()).isEqualTo(1);
+        assertThat(attendanceBook.isCrew("빙봉")).isTrue;
+    }
+
+    @DisplayName("출석부에 추가하려는 크루가 존재하면 해당 크루의 CrewAttendance 반환")
+    @Test
+    void test2() {
+        AttendanceBook attendanceBook = new AttendanceBook();
+        attendanceBook.addCrew("빙봉");
+
+        CrewAttendance crewAttendance = attendanceBook.addCrew("빙봉");
+
+        assertThat(attendanceBook.countCrews()).isEqualTo(1);
+        assertThat(attendanceBook.isCrew("빙봉")).isTrue;
+        assertThat(crewAttendance).isInstanceOf(CrewAttendance.class);
+    }
+}
