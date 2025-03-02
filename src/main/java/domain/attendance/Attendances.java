33ff8c92761b3dd02cba4062c1e2c7388ac9e333
ppparkta <pppppark2@gmail.com>
2025-03-03 02:26:16 +0900

    feat: 출석부 빈 날짜 초기화 로직 추가

--- src/main/java/domain/attendance/Attendances.java
+++ src/main/java/domain/attendance/Attendances.java
@@ -1,8 +1,8 @@
 package domain.attendance;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -14,6 +14,7 @@ public class Attendances {
     public Attendances(List<LocalDateTime> attendances, LocalDate startDate, LocalDate endDate) {
         validate(attendances);
         initAttendanceDate(startDate, endDate);
+        attendances.forEach(this::editAttendanceDate);
         this.attendances.addAll(attendances.stream()
                 .map(Attendance::new)
                 .toList());
@@ -21,11 +22,34 @@ public class Attendances {
 
     private void initAttendanceDate(LocalDate startDate, LocalDate endDate) {
         if (startDate.isAfter(endDate)) {
-            throw new IllegalArgumentException("출석부 시작일이 기준일보다 뒤일 수 없습니다");
+            throw new IllegalArgumentException("출석부 시작 날짜가 마지막 날짜보다 늦을 수 없습니다");
         }
         while (startDate.isBefore(endDate)) {
-            attendances.add(new Attendance(startDate));
+            startDate = updateAttendanceDate(startDate);
         }
+    }
+
+    private LocalDate updateAttendanceDate(LocalDate startDate) {
+        if (Holiday.isHoliday(startDate)
+                || startDate.getDayOfWeek().compareTo(DayOfWeek.FRIDAY) > Attendance.WEEKDAY) {
+            startDate = startDate.plusDays(1);
+            return startDate;
+        }
+        attendances.add(new Attendance(startDate));
+        startDate = startDate.plusDays(1);
+        return startDate;
+    }
+
+    private void editAttendanceDate(LocalDateTime dateTime) {
+        deleteAttendanceDate(dateTime.toLocalDate());
+        attendances.add(new Attendance(dateTime));
+    }
+
+    private void deleteAttendanceDate(LocalDate date) {
+        attendances.remove(attendances.stream()
+                .filter(attendance -> attendance.has(date))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 날짜의 출석이 존재하지 않습니다")));
     }
 
     public boolean has(LocalDate day) {
@@ -45,16 +69,6 @@ public class Attendances {
         return attendance.getStatus();
     }
 
-    private void validate(List<LocalDateTime> dateTimes) {
-        Set<LocalDate> dates = dateTimes
-                .stream()
-                .map(LocalDateTime::toLocalDate)
-                .collect(Collectors.toSet());
-        if (dateTimes.size() != dates.size()) {
-            throw new IllegalArgumentException("동일한 날짜의 출석 기록은 등록할 수 없습니다");
-        }
-    }
-
     public int countAttendance() {
         return (int) attendances.stream()
                 .filter(attendance -> attendance.getStatus() == AttendanceStatus.ATTENDANCE)
@@ -71,5 +85,15 @@ public class Attendances {
         return (int) attendances.stream()
                 .filter(attendance -> attendance.getStatus() == AttendanceStatus.ABSENCE)
                 .count();
+    }
+
+    private void validate(List<LocalDateTime> dateTimes) {
+        Set<LocalDate> dates = dateTimes
+                .stream()
+                .map(LocalDateTime::toLocalDate)
+                .collect(Collectors.toSet());
+        if (dateTimes.size() != dates.size()) {
+            throw new IllegalArgumentException("동일한 날짜의 출석 기록은 등록할 수 없습니다");
+        }
     }
 }
