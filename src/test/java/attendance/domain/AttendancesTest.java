34fc5b9178719386c26fb9273926fe69e3dd083d
황재웅 <wodnd0131@naver.com>
2025-03-02 20:50:57 +0900

    test: 출석하지 않은 날을 포함한 출석 목록을 반환

--- src/test/java/attendance/domain/AttendancesTest.java
+++ src/test/java/attendance/domain/AttendancesTest.java
@@ -1,8 +1,10 @@
 package attendance.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -44,5 +46,26 @@ class AttendancesTest {
         assertThatThrownBy(() -> attendances.addAttendance(duplicate))
             .isInstanceOf(AttendanceArgumentException.class)
             .hasMessageContaining("이미 출석하였습니다. 수정 기능을 이용해주세요.");
+    }
+
+    @Test
+    @DisplayName("출석하지 않은 날도 포함한 출석 목록을 반환한다.")
+    void test_getAttendancesWithTruancy() {
+        var attendances = new Attendances(systemDateTime);
+        attendances.addAttendance(LocalDateTime.of(2024, 12, 11, 10, 0));
+        List<Attendance> attendancesWithTruancy = attendances.getAttendancesWithTruancy();
+
+        var attendanceCount = attendancesWithTruancy.stream()
+            .filter(attendance -> !attendance.isTruancy())
+            .count();
+        var truancyCount = attendancesWithTruancy.stream()
+            .filter(Attendance::isTruancy)
+            .count();
+
+        assertAll(
+            () -> assertThat(attendanceCount).isEqualTo(1),
+            () -> assertThat(truancyCount).isEqualTo(16),
+            () -> assertThat(attendanceCount + truancyCount).isEqualTo(attendancesWithTruancy.size())
+        );
     }
 }
