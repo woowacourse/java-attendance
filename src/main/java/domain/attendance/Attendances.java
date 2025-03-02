cc213eda06308f2a945b1249877189a8e745ac98
ppparkta <pppppark2@gmail.com>
2025-03-02 18:40:41 +0900

    feat: 초기화 시 동일한 날짜의 출석 기록 등록 불가(그린)

--- src/main/java/domain/attendance/Attendances.java
+++ src/main/java/domain/attendance/Attendances.java
@@ -2,17 +2,15 @@ package domain.attendance;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Attendances {
     private final List<Attendance> attendances;
 
-    public Attendances() {
-        this.attendances = new ArrayList<>();
-    }
-
     public Attendances(List<LocalDateTime> attendances) {
+        validate(attendances);
         this.attendances = attendances.stream()
                 .map(Attendance::new)
                 .toList();
@@ -21,6 +19,16 @@ public class Attendances {
     public boolean has(LocalDate day) {
         return attendances.stream()
                 .anyMatch(attendance -> attendance.has(day));
+    }
+
+    private void validate(List<LocalDateTime> dateTimes) {
+        Set<LocalDate> dates = dateTimes
+                .stream()
+                .map(LocalDateTime::toLocalDate)
+                .collect(Collectors.toSet());
+        if (dateTimes.size() != dates.size()) {
+            throw new IllegalArgumentException("동일한 날짜의 출석 기록은 등록할 수 없습니다");
+        }
     }
 
     private Attendance findAttendanceByDate(LocalDate date) {
