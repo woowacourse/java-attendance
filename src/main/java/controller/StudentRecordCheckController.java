af3476b196a4fe538a0c54b9ecd0746481b3f960
(minjae8563) <(minjae8563@naver.com)>
2025-03-02 16:23:38 +0900

    refactor : 이름도 인자로 넘겨주는 방식으로 수정

--- src/main/java/controller/StudentRecordCheckController.java
+++ src/main/java/controller/StudentRecordCheckController.java
@@ -9,7 +9,7 @@ public class StudentRecordCheckController {
     public static void studentRecordCheck(Students students) {
         String studentName = InputView.getStudentNameUntilValidateToAttendance(students);
         Student wantToCheckStudent = students.findStudentByName(studentName);
-        OutputView.printRecordCheck(wantToCheckStudent.getStudentAttendanceHistory().getAttendanceHistory());
+        OutputView.printRecordCheck(wantToCheckStudent.getStudentAttendanceHistory().getAttendanceHistory(), studentName);
         OutputView.printResult(wantToCheckStudent.calculateStudentAttendanceResult());
     }
 }
