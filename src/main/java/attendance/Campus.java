4d574531d325464dc0fbeaae57b1ca42d345f26c
saera <yook.saera@gmail.com>
2025-03-02 18:23:56 +0900

    refactor(Campus): 메서드 분리 및 코드 중복 개선

--- src/main/java/attendance/Campus.java
+++ src/main/java/attendance/Campus.java
@@ -1,6 +1,7 @@
 package attendance;
 
-import java.time.DayOfWeek;
+import static java.time.DayOfWeek.MONDAY;
+
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
@@ -8,22 +9,21 @@ public class Campus {
     private static final LocalTime REGULAR_START_TIME = LocalTime.of(10, 0, 0);
     private static final LocalTime MONDAY_START_TIME = LocalTime.of(13, 0, 0);
     private static final int PRESENT_LIMIT_MINUTES = 5;
-    private static final long LATE_LIMIT_MINUTES = 30;
+    private static final int LATE_LIMIT_MINUTES = 30;
 
     public static AttendanceStatus calculateAttendanceStatus(final LocalDateTime attendance) {
-        if (attendance.getDayOfWeek() == DayOfWeek.MONDAY) {
-            if (attendance.toLocalTime().isAfter(MONDAY_START_TIME.plusMinutes(LATE_LIMIT_MINUTES))) {
-                return AttendanceStatus.ABSENT;
-            }
-            if (attendance.toLocalTime().isAfter(MONDAY_START_TIME.plusMinutes(PRESENT_LIMIT_MINUTES))) {
-                return AttendanceStatus.LATE;
-            }
-            return AttendanceStatus.PRESENT;
+        final LocalTime attendanceTime = LocalTime.from(attendance);
+        if (attendance.getDayOfWeek() == MONDAY) {
+            return calculateAttendanceStatus(attendanceTime, MONDAY_START_TIME);
         }
-        if (attendance.toLocalTime().isAfter(REGULAR_START_TIME.plusMinutes(LATE_LIMIT_MINUTES))) {
+        return calculateAttendanceStatus(attendanceTime, REGULAR_START_TIME);
+    }
+
+    private static AttendanceStatus calculateAttendanceStatus(final LocalTime attendance, final LocalTime startTime) {
+        if (attendance.isAfter(startTime.plusMinutes(LATE_LIMIT_MINUTES))) {
             return AttendanceStatus.ABSENT;
         }
-        if (attendance.toLocalTime().isAfter(REGULAR_START_TIME.plusMinutes(PRESENT_LIMIT_MINUTES))) {
+        if (attendance.isAfter(startTime.plusMinutes(PRESENT_LIMIT_MINUTES))) {
             return AttendanceStatus.LATE;
         }
         return AttendanceStatus.PRESENT;
