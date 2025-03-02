71a68f2d3bed1d1878df4e2b0f2b9d19ee6cae18
julie <7jujubuilds@gmail.com>
2025-03-02 23:21:09 +0900

    test(record): 결석 기록 생성 관련 테스트 작성
    테스트 내용:
    - LocalDate을 받아 결석으로 처리된 객체를 생성할 수 있다

--- src/test/java/domain/AttendanceRecordTest.java
+++ src/test/java/domain/AttendanceRecordTest.java
@@ -3,9 +3,11 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class AttendanceRecordTest {
     @DisplayName("LocalDateTime을 받아 기록 객체를 생성할 수 있다.")
@@ -16,6 +18,22 @@ class AttendanceRecordTest {
 
         // then
         assertThatNoException().isThrownBy(() -> new AttendanceRecord(dateTime));
+    }
+
+    @DisplayName("LocalDate을 받아 결석으로 처리된 객체를 생성할 수 있다.")
+    @Test
+    void absentInstanceTest() {
+        // given
+        LocalDate date = LocalDate.of(2024, 12, 2);
+
+        // when
+        AttendanceStatus expectedValue = AttendanceStatus.ABSENT;
+
+        // then
+        assertAll(
+                () -> assertThatNoException().isThrownBy(() -> new AttendanceRecord(date)),
+                () -> assertThat(new AttendanceRecord(date).getAttendanceStatus()).isEqualTo(expectedValue)
+        );
     }
 
     @DisplayName("출석을 기록하려는 날짜가 등교일이 아닐 경우 예외가 발생한다.")
