cae643f215c193ae092f48a7ba09527e00253078
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-03 00:00:09 +0900

    🟢green(Penalty): 패널티 그린 테스트

--- src/main/java/domain/Penalty.java
+++ src/main/java/domain/Penalty.java
@@ -1,10 +1,13 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum Penalty {
 
+    NONE(0, 0),
     WARNING(1, 2),
     INTERVIEW(2, 3),
-    EXPLUSION(3, 5);
+    EXPLUSION(3, 6);
 
     private final int code;
     private final int limit;
@@ -15,6 +18,14 @@ public enum Penalty {
     }
 
     public static Penalty findByAbsenceCount(final int count) {
-        return null;
+        return Arrays.stream(Penalty.values())
+                .sorted((p1,p2) -> p2.limit - p1.limit)
+                .filter(penalty -> count >= penalty.limit)
+                .findAny()
+                .orElse(Penalty.NONE);
+    }
+
+    public int getCode() {
+        return code;
     }
 }
--- src/test/java/domain/PenaltyTest.java
+++ src/test/java/domain/PenaltyTest.java
@@ -2,22 +2,30 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class PenaltyTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource
     @DisplayName("결석 횟수 기준으로 패널티를 찾는다.")
-    void test1() {
-        //given
-        final int count = 5;
+    void test1(final int count, final Penalty penalty) {
+        //should
+        assertThat(Penalty.findByAbsenceCount(count)).isEqualTo(penalty);
+    }
 
-        //when
-        final Penalty penalty = Penalty.findByAbsenceCount(count);
-
-        //then
-        assertThat(penalty).isEqualTo(Penalty.INTERVIEW);
-
+    private static Stream<Arguments> test1() {
+        return Stream.of(
+                Arguments.of(5, Penalty.INTERVIEW),
+                Arguments.of(6, Penalty.EXPLUSION),
+                Arguments.of(2, Penalty.WARNING),
+                Arguments.of(1, Penalty.NONE),
+                Arguments.of(0, Penalty.NONE)
+        );
     }
 }
