4bff602551a9eba0ac792dd3e2f77c13732d932a
dltmddus <tmddus4671@gmail.com>
2025-03-02 15:01:22 +0900

    feat: 등교 시간으로 24시간 형식만 입력받기 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.regex.Pattern;
 import util.Console;
 
 public class InputView {
@@ -9,7 +10,41 @@ public class InputView {
                 + "2. 출석 수정\n"
                 + "3. 크루별 출석 기록 확인\n"
                 + "4. 제적 위험자 확인\n"
-                + "Q. 종료\n");
+                + "Q. 종료");
+        String featureNumber = Console.readLine();
+        validateFeatureNumber(featureNumber);
+        return featureNumber;
+    }
+
+    private static void validateFeatureNumber(final String featureNumber) {
+        if (!featureNumber.equals("1") && !featureNumber.equals("2") && !featureNumber.equals("3")
+                && !featureNumber.equals("4") && !featureNumber.equals("Q")) {
+            throw new IllegalArgumentException("잘못된 기능 번호입니다.");
+        }
+    }
+
+    public static String inputNickname() {
+        System.out.println("닉네임을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public static String inputTime() {
+        System.out.println("등교 시간을 입력해 주세요.");
+        String time = Console.readLine();
+        validateTimeFormat(time);
+        return time;
+    }
+
+    private static void validateTimeFormat(final String input) {
+        if (!isCorrectFormat(input)) {
+            throw new IllegalArgumentException("시간은 24시간 형식만 사용합니다.");
+        }
+    }
+
+    private static boolean isCorrectFormat(final String input) {
+        String datePattern = "2[0-3]|[01][0-9]:[0-5][0-9]";
+        Pattern correctPattern = Pattern.compile(datePattern);
+
+        return correctPattern.matcher(input).find();
     }
 }
