452d0af0c20b5383115851d73fdb75fa406f544e
Mint <audwl03071@gmail.com>
2025-03-02 17:25:14 +0900

    refactor(StringParser): 사용하지 않은 import문 제거

--- src/main/java/attendance/util/StringParser.java
+++ src/main/java/attendance/util/StringParser.java
@@ -2,7 +2,6 @@ package attendance.util;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.MonthDay;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
