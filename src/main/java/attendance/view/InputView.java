de066ca9f68e5003392306279ad86464f4874bbd
taek2222 <oot309@daum.net>
2025-03-02 22:41:00 +0900

    feat(InputView): 출석 수정 기능 입력 추가

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -19,4 +19,19 @@ public class InputView {
         System.out.println("등교 시간을 입력해 주세요.");
         return console.nextLine();
     }
+
+    public String readNicknameForUpdate() {
+        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        return console.nextLine();
+    }
+
+    public int readAttendanceDay() {
+        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        return Integer.parseInt(console.nextLine());
+    }
+
+    public String readAttendanceTimeForUpdate() {
+        System.out.println("언제로 변경하겠습니까?");
+        return console.nextLine();
+    }
 }
