e349af3742d0299a6bd4e295841bac13723feb8b
saera <yook.saera@gmail.com>
2025-03-02 19:59:36 +0900

    feat: 출석 기록 여러 개 불러오기 기능 구현

--- src/main/java/attendance/AttendanceLoader.java
+++ src/main/java/attendance/AttendanceLoader.java
@@ -14,4 +14,15 @@ public class AttendanceLoader {
 
         return attendanceBook;
     }
+    
+    public static AttendanceBook load(final List<String> data) {
+        AttendanceBook attendanceBook = new AttendanceBook();
+        for (String line : data) {
+            List<String> contents = Arrays.asList(line.split(","));
+            LocalDateTime attendance = LocalDateTime.parse(contents.getLast().replace(" ", "T"));
+
+            attendanceBook.addAttendance(contents.getFirst(), attendance);
+        }
+        return attendanceBook;
+    }
 }
