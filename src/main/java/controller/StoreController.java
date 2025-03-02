860eb033f6e6653fa7b60ba71437020f1fa00cf5
minSsan <msp1125@hanyang.ac.kr>
2025-03-02 19:41:08 +0900

    green: 파일 내용 저장 기능 구현

--- src/main/java/controller/StoreController.java
+++ src/main/java/controller/StoreController.java
@@ -1,16 +1,24 @@
-//package controller;
-//
-//import domain.AttendanceStoreManager;
-//
-//public class StoreController implements Controller {
-//    private final AttendanceStoreManager attendanceStoreManager;
-//
-//    public StoreController(AttendanceStoreManager attendanceStoreManager) {
-//        this.attendanceStoreManager = attendanceStoreManager;
-//    }
-//
-//    @Override
-//    public void run() {
-//        attendanceStoreManager.save("src/main/resources/attendances.csv");
-//    }
-//}
+package controller;
+
+import domain.CrewAttendanceStorage;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class StoreController implements Controller {
+    private final String FILE = "";
+    private final CrewAttendanceStorage crewAttendanceStorage;
+
+    public StoreController(CrewAttendanceStorage crewAttendanceStorage) {
+        this.crewAttendanceStorage = crewAttendanceStorage;
+    }
+
+    @Override
+    public void run() {
+        save();
+    }
+
+    private void save() {
+
+    }
+}
--- src/main/java/domain/FileStoreManager.java
+++ src/main/java/domain/FileStoreManager.java
@@ -1,5 +1,15 @@
 package domain;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class FileStoreManager {
     private final CrewAttendanceStorage storage;
 
@@ -7,5 +17,49 @@ public class FileStoreManager {
         this.storage = storage;
     }
     public void save(String file) {
+        List<String> lines = readLines(file);
+        Set<String> crews = getCrews(lines);
+        for (String crew : crews) {
+            storage.create(crew);
+        }
+        registerAttendances(lines);
+    }
+
+    private List<String> readLines(String file) {
+        List<String> lines = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            reader.readLine();
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+                lines.add(line);
+            }
+            return lines;
+        } catch (IOException e) {
+            throw new RuntimeException("파일 로드 중에 오류가 발생했습니다.");
+        }
+    }
+
+
+    private Set<String> getCrews(List<String> lines) {
+        Set<String> crews = new HashSet<>();
+        for (String line : lines) {
+            String[] split = line.split(",");
+            crews.add(split[0].trim());
+        }
+        return crews;
+    }
+
+    private void registerAttendances(List<String> lines) {
+        for (String line : lines) {
+            String[] split = line.split(",");
+            String crew = split[0].trim();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            LocalDateTime attendanceTime = LocalDateTime.parse(split[1], formatter);
+            storage.register(crew, attendanceTime.toLocalDate(), attendanceTime.toLocalTime());
+        }
     }
 }
