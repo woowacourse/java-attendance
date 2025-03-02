a1bc3731549a7ff3ccbe3199a3f544d924b02f63
Yeongjae Kim <sees77770@gmail.com>
2025-03-02 15:43:33 +0900

    test(green): 월요일의 지각 결석 여부 판단 테스트

--- src/test/java/AttendancePolicy.java
+++ src/test/java/AttendancePolicy.java
@@ -6,11 +6,14 @@ import java.util.Locale;
 
 public class AttendancePolicy {
 
+    public static final int MONDAY_VALUE = 1;
     private static final int SATURDAY_VALUE = 6;
     private static final LocalTime CAMPUS_OPEN_TIME = LocalTime.of(8, 0);
     private static final LocalTime CAMPUS_CLOSE_TIME = LocalTime.of(23, 0);
     private static final LocalTime LATE_TIME = LocalTime.of(10, 5);
     private static final LocalTime ABSENCE_TIME = LocalTime.of(10, 30);
+    private static final LocalTime MONDAY_LATE_TIME = LocalTime.of(13, 5);
+    private static final LocalTime MONDAY_ABSENCE_TIME = LocalTime.of(13, 30);
 
     public void validateIsWeekDays(LocalDate today) {
         if (today.getDayOfWeek().getValue() >= SATURDAY_VALUE) {
@@ -28,12 +31,29 @@ public class AttendancePolicy {
     }
 
     public AttendanceStatus getAttendanceStatus(LocalDateTime attendanceTime) {
-        if (attendanceTime.toLocalTime().isAfter(ABSENCE_TIME)) {
+        LocalTime lateTime = getLateTime(attendanceTime);
+        LocalTime absenceTime = getAbsenceTime(attendanceTime);
+        LocalTime currentTime = attendanceTime.toLocalTime();
+
+        if (currentTime.isAfter(absenceTime)) {
             return AttendanceStatus.ABSENCE;
         }
-        if (attendanceTime.toLocalTime().isAfter(LATE_TIME)) {
+        if (currentTime.isAfter(lateTime)) {
             return AttendanceStatus.LATE;
         }
         return AttendanceStatus.ATTENDANCE;
     }
+
+    private boolean isMonday(LocalDateTime attendanceTime) {
+        return attendanceTime.getDayOfWeek().getValue() == MONDAY_VALUE;
+    }
+
+    private LocalTime getLateTime(LocalDateTime attendanceTime) {
+        return isMonday(attendanceTime) ? MONDAY_LATE_TIME : LATE_TIME;
+    }
+
+    private LocalTime getAbsenceTime(LocalDateTime attendanceTime) {
+        return isMonday(attendanceTime) ? MONDAY_ABSENCE_TIME : ABSENCE_TIME;
+    }
+
 }
