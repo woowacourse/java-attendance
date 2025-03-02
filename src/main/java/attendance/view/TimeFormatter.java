a6619cb049fd4ae19a5972b582a6e80c06e0291c
Mint <audwl03071@gmail.com>
2025-03-02 13:39:05 +0900

    fix(TimeFormatter): 요일 형식 한글로만 지원되도록 수정

--- src/main/java/attendance/view/TimeFormatter.java
+++ src/main/java/attendance/view/TimeFormatter.java
@@ -3,10 +3,12 @@ package attendance.view;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class TimeFormatter {
 
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("MM월 dd일 E요일");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("MM월 dd일 E요일")
+            .withLocale(Locale.KOREAN);
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
     public static String makeDateMessage(final LocalDate localDate) {
