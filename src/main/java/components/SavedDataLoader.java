46d064a27773c35deeaa036a5ae20594cae2b74f
changuii <rhljh7410@gmail.com>
2025-03-02 11:22:04 +0900

    refactor(SavedDataLoader): 변수명을 축약하지 않고 자세하게 작성하라

--- src/main/java/components/SavedDataLoader.java
+++ src/main/java/components/SavedDataLoader.java
@@ -11,22 +11,23 @@ import java.util.stream.Collectors;
 import utils.FileReader;
 
 public class SavedDataLoader {
-    private static final String FILE_PATH = "./src/main/resources/attendances.csv";
-    private static final int CREW = 0;
-    private static final int ATTENDANCE = 1;
-    private static final String DELIMITER = ",";
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final String LOAD_FILE_PATH = "./src/main/resources/attendances.csv";
+    private static final int CREW_INDEX = 0;
+    private static final int ATTENDANCE_INDEX = 1;
+    private static final String SAVED_ATTENDANCE_LINE_DELIMITER = ",";
+    private static final DateTimeFormatter SAVED_ATTENDANCE_DATE_TIME_FORMATTER = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd HH:mm");
     private final List<String[]> loadedData;
 
     public SavedDataLoader() {
-        final List<String> readData = FileReader.read(FILE_PATH);
+        final List<String> readData = FileReader.read(LOAD_FILE_PATH);
         readData.removeFirst();
         loadedData = parsedData(readData);
     }
 
     private List<String[]> parsedData(final List<String> readData) {
         return readData.stream()
-                .map(line -> line.split(DELIMITER))
+                .map(line -> line.split(SAVED_ATTENDANCE_LINE_DELIMITER))
                 .collect(Collectors.toList());
     }
 
@@ -35,10 +36,10 @@ public class SavedDataLoader {
     }
 
     private void loadAttendance(final AttendanceBook attendanceBook, final Crews crews, final String[] line) {
-        final Crew crew = crews.findByName(line[CREW]);
+        final Crew crew = crews.findByName(line[CREW_INDEX]);
         registerCrew(attendanceBook, crew);
         final AttendanceHistory attendanceHistory = attendanceBook.findByCrew(crew);
-        attendanceHistory.attendance(parseDateTime(line[ATTENDANCE]));
+        attendanceHistory.attendance(parseDateTime(line[ATTENDANCE_INDEX]));
     }
 
     private void registerCrew(final AttendanceBook attendanceBook, final Crew crew) {
@@ -48,12 +49,12 @@ public class SavedDataLoader {
     }
 
     private LocalDateTime parseDateTime(final String dateTime) {
-        return LocalDateTime.parse(dateTime, FORMATTER);
+        return LocalDateTime.parse(dateTime, SAVED_ATTENDANCE_DATE_TIME_FORMATTER);
     }
 
     public Crews loadCrews() {
         return new Crews(loadedData.stream()
-                .map(line -> line[CREW])
+                .map(line -> line[CREW_INDEX])
                 .map(Crew::new)
                 .collect(Collectors.toList()));
     }
