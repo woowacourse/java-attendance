ccbb4af967f2b558333b1e471faa536f2a2f0f21
DongchannN <ldc991104@gmail.com>
2025-03-02 13:46:21 +0900

    refactor: text block 사용으로 가독성 향상

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,11 +9,12 @@ public class InputView {
 
     public String readCommandCode(LocalDate date) {
         System.out.printf("오늘은 %s입니다. 기능을 선택해 주세요.\n", CustomDateTimeFormatter.dateToString(date));
-        System.out.println("1. 출석 확인\n"
-                + "2. 출석 수정\n"
-                + "3. 크루별 출석 기록 확인\n"
-                + "4. 제적 위험자 확인\n"
-                + "Q. 종료");
+        System.out.println("""
+                1. 출석 확인
+                2. 출석 수정
+                3. 크루별 출석 기록 확인
+                4. 제적 위험자 확인
+                Q. 종료""");
         return scanner.next();
     }
 
