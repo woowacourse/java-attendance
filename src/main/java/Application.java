747c1d1b24f7f9609e797e8d698817530101f99e
Yeongjae Kim <sees77770@gmail.com>
2025-03-02 23:32:26 +0900

    feat: 컨트롤러, 뷰 클래스 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,16 @@
+import controller.AttendanceController;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        AttendanceController attendanceController = new AttendanceController(
+                inputView, outputView
+        );
+        attendanceController.run();
     }
 }
