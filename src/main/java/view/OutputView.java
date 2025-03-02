f9fef7a63fc5cf39f96d14bac0ddd6473960de45
ppparkta <pppppark2@gmail.com>
2025-03-02 21:31:00 +0900

    feat: 출석하기 커멘드 인터페이스 연결

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,16 @@
 package view;
 
+import controller.DateTimeConverter;
+import java.time.LocalDateTime;
+
 public class OutputView {
     public void printError(String message) {
         System.out.println("[ERROR] " + message);
+    }
+
+    public void printAttend(LocalDateTime attendedTime, String attendanceStatus) {
+        System.out.println(DateTimeConverter.convertLocalDateToString(attendedTime.toLocalDate()) + " "
+                + DateTimeConverter.convertLocalTimeToString(attendedTime.toLocalTime())
+                + " (" + attendanceStatus + ")");
     }
 }
