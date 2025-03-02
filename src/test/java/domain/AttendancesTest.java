96d5392371c1a5a3f0c5027bb161082cba9f2cdd
eoehd1ek <eoehd2ek@gmail.com>
2025-03-03 01:19:22 +0900

    refector: 테스트를 기능 단위로 묶도록 inner class 추가

--- src/test/java/domain/AttendancesTest.java
+++ src/test/java/domain/AttendancesTest.java
@@ -4,35 +4,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class AttendancesTest {
-    @Test
-    @DisplayName("출석 시간을 가지고 출석을 기록한다")
-    void should_attend_by_attending_time() {
-        // given
-        LocalTime attendingTime = LocalTime.parse("10:00");
-        Attendances attendances = new Attendances();
+    @Nested
+    @DisplayName("출석 등록 테스트")
+    class AttendTest {
+        @Test
+        @DisplayName("출석 시간을 가지고 출석을 기록한다")
+        void should_attend_by_attending_time() {
+            // given
+            LocalTime attendingTime = LocalTime.parse("10:00");
+            Attendances attendances = new Attendances();
 
-        // when
-        attendances.attend(attendingTime);
+            // when
+            attendances.attend(attendingTime);
 
-        // then
-        assertThat(attendances).isNotEqualTo(new Attendances());
-    }
+            // then
+            assertThat(attendances).isNotEqualTo(new Attendances());
+        }
 
-    @Test
-    @DisplayName("이미 출석한 경우를 알 수 있다")
-    void should_return_true_when_already_attended() {
-        // given
-        LocalTime attendingTime = LocalTime.parse("10:00");
-        Attendances attendances = new Attendances();
-        attendances.attend(attendingTime);
+        @Test
+        @DisplayName("이미 출석한 경우를 알 수 있다")
+        void should_return_true_when_already_attended() {
+            // given
+            LocalTime attendingTime = LocalTime.parse("10:00");
+            Attendances attendances = new Attendances();
+            attendances.attend(attendingTime);
 
-        // when
-        boolean result = attendances.isAttended();
+            // when
+            boolean result = attendances.isAttended();
 
-        // then
-        assertThat(result).isEqualTo(true);
+            // then
+            assertThat(result).isEqualTo(true);
+        }
     }
 }
