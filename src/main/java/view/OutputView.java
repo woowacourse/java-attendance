1e390dcd64afd1514317b90f7a1881769f6387ca
ppparkta <pppppark2@gmail.com>
2025-03-03 03:03:43 +0900

    feat: 출석 수정 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.DateTimeConverter;
+import domain.attendance.AttendanceStatus;
 import java.time.LocalDateTime;
 
 public class OutputView {
@@ -12,5 +13,16 @@ public class OutputView {
         System.out.println(DateTimeConverter.convertLocalDateToString(attendedTime.toLocalDate()) + " "
                 + DateTimeConverter.convertLocalTimeToString(attendedTime.toLocalTime())
                 + " (" + attendanceStatus + ")");
+    }
+
+    public void printEdit(LocalDateTime beforeDateTime,
+                          String beforeStatus,
+                          LocalDateTime afterDateTime,
+                          String afterStatus) {
+        System.out.println(DateTimeConverter.convertLocalDateToString(beforeDateTime.toLocalDate()) + " "
+                + DateTimeConverter.convertLocalTimeToString(beforeDateTime.toLocalTime())
+                + " (" + beforeStatus + ") -> "
+                + DateTimeConverter.convertLocalTimeToString(afterDateTime.toLocalTime())
+                + " (" + afterStatus + ") 수정 완료!");
     }
 }
