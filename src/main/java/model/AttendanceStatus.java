4adde341f375217924d495d4e51366ff9264ef5f
LJM\eueo8 <eueo8259@gmail.com>
2025-03-02 20:55:38 +0900

    refactor : 각 상태별 기준값을 enum에 추가하고 메서드 명 변경 및 로직 수정

--- src/main/java/model/AttendanceStatus.java
+++ src/main/java/model/AttendanceStatus.java
@@ -2,31 +2,36 @@ package model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public enum AttendanceStatus {
-
-    ATTENDANCE("출석"),
-    LATE("지각"),
-    ABSENT("결석");
+    ATTENDANCE("출석", 0),
+    LATE("지각", 5),
+    ABSENT("결석", 30);
 
     private final String attendanceStatus;
+    private final int thresholdMinutes;
 
-    AttendanceStatus(String attendanceStatus) {
+    AttendanceStatus(String attendanceStatus, int thresholdMinutes) {
         this.attendanceStatus = attendanceStatus;
+        this.thresholdMinutes = thresholdMinutes;
     }
 
-    public static AttendanceStatus attendanceStatusCalculate(LocalDate todayDate, LocalTime attendanceTime) {
+    public static AttendanceStatus calculateAttendanceStatus(LocalDate todayDate, LocalTime attendanceTime) {
         LocalTime attendanceStartTime = WeeklyAttendanceSchedule.findAttendanceScheduleByLocalDate(todayDate);
-        if (attendanceTime.isAfter(attendanceStartTime.plusMinutes(30))) {
-            return ABSENT;
-        }
-        if (attendanceTime.isAfter(attendanceStartTime.plusMinutes(5))) {
-            return LATE;
-        }
-        return ATTENDANCE;
+        return Arrays.stream(AttendanceStatus.values())
+                .sorted(Comparator.comparingInt(AttendanceStatus::getThresholdMinutes).reversed())
+                .filter(status -> attendanceTime.isAfter(attendanceStartTime.plusMinutes(status.getThresholdMinutes())))
+                .findFirst()
+                .orElse(AttendanceStatus.ATTENDANCE);
     }
 
     public String getAttendanceStatus() {
         return attendanceStatus;
+    }
+
+    public int getThresholdMinutes() {
+        return thresholdMinutes;
     }
 }
