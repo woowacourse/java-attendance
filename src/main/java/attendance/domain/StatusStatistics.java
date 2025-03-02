18e8aa23dd449308ed101e082f6ee39954fed445
황재웅 <wodnd0131@naver.com>
2025-03-02 14:13:57 +0900

    feat: 출석 상태 통계는 제적 상태로 비교

--- src/main/java/attendance/domain/StatusStatistics.java
+++ src/main/java/attendance/domain/StatusStatistics.java
@@ -4,7 +4,8 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
-public record StatusStatistics(Nickname nickname, Map<AttendanceStatus, Integer> statistics) {
+public record StatusStatistics(Nickname nickname, Map<AttendanceStatus, Integer> statistics)
+    implements Comparable<StatusStatistics> {
     private static final int WEIGHT_DIVIDER_FOR_LATE = 3;
 
     public StatusStatistics(Nickname nickname) {
@@ -18,15 +19,27 @@ public record StatusStatistics(Nickname nickname, Map<AttendanceStatus, Integer>
     }
 
     public void put(AttendanceStatus state) {
-        statistics.put(state, this.get(state) + 1);
+        statistics.put(state, this.getCount(state) + 1);
     }
 
-    public int get(AttendanceStatus state) {
+    public int getCount(AttendanceStatus state) {
         return statistics.getOrDefault(state, 0);
     }
 
     public int getWeight() {
-        return statistics.get(AttendanceStatus.LATE) / WEIGHT_DIVIDER_FOR_LATE
-            + statistics.get(AttendanceStatus.ABSENCE);
+        return getCount(AttendanceStatus.LATE) / WEIGHT_DIVIDER_FOR_LATE
+            + getCount(AttendanceStatus.ABSENCE);
+    }
+
+    public int compareWeight() {
+        return getCount(AttendanceStatus.LATE) + getCount(AttendanceStatus.ABSENCE);
+    }
+
+    @Override
+    public int compareTo(StatusStatistics o) {
+        var sanctionLevel = SanctionLevel.matchLevel(getWeight());
+        var otherSanctionLevel = SanctionLevel.matchLevel(o.getWeight());
+
+        return otherSanctionLevel.compareTo(sanctionLevel);
     }
 }
