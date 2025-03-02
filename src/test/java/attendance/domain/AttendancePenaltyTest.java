1a2903ac8f75084df1ac9f198edb2bdc677e2d31
jeyong <joon6093@naver.com>
2025-03-02 19:52:17 +0900

    test: 결석이 1회 이상일 시 패널티 없음을 검증

--- src/test/java/attendance/domain/AttendancePenaltyTest.java
+++ src/test/java/attendance/domain/AttendancePenaltyTest.java
@@ -86,5 +86,30 @@ class AttendancePenaltyTest {
             // then
             assertThat(penalty).isEqualTo(AttendancePenalty.WARNING);
         }
+
+        @ParameterizedTest
+        @CsvSource(
+            {
+                "3, 0",
+                "1, 0"
+            }
+        )
+        void 지각3회를_결석1회로_간주하여_1회이상일시_패널티없음으로_간주한다(
+            int lateCount,
+            int absenceCount
+        ) {
+            // given
+            Map<AttendanceStatus, Integer> attendanceStatusCount = Map.of(
+                AttendanceStatus.LATE, lateCount,
+                AttendanceStatus.ABSENCE, absenceCount
+            );
+
+            // when
+            AttendancePenalty penalty = AttendancePenalty.from(
+                attendanceStatusCount);
+
+            // then
+            assertThat(penalty.isNoPenalty()).isTrue();
+        }
     }
 }
