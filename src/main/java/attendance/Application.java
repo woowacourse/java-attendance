bfa0c7a3943f84261dce8c47ff6baff8e220bf0f
kjyyjk <rlawnsdud920@gmail.com>
2025-03-02 21:27:33 +0900

    refactor: Options의 도메인 로직 Application으로 이동
    - 기존에는 Options가 일종의 Controller 역할을 하였으나,
    이는 Options의 책임이 아니라고 판단하여 Application으로 이동하였다.
    따라서 Options는 입력 받은 Option에 따라 Application의 메서드를
    호출한다.

--- src/main/java/attendance/Application.java
+++ src/main/java/attendance/Application.java
@@ -1,29 +1,53 @@
 package attendance;
 
+import static attendance.domain.CampusManager.validateOperationDate;
+import static attendance.domain.CampusManager.validateOperationTime;
+import static attendance.view.InputView.readAttendanceDateToModify;
+import static attendance.view.InputView.readAttendanceModificationTime;
+import static attendance.view.InputView.readAttendanceTime;
+import static attendance.view.InputView.readCrewNickname;
+import static attendance.view.InputView.readCrewNicknameToModify;
 import static attendance.view.InputView.readOption;
+import static attendance.view.OutputView.printAttendance;
+import static attendance.view.OutputView.printAttendanceModificationResult;
+import static attendance.view.OutputView.printAttendanceStatistics;
+import static attendance.view.OutputView.printDangerousCrewsInformation;
+import static attendance.view.OutputView.printMonthlyAttendances;
+import static attendance.view.OutputView.printNoAttendanceToModify;
 
+import attendance.domain.Attendance;
 import attendance.domain.AttendanceManager;
+import attendance.domain.AttendanceStatistics;
+import attendance.domain.Nickname;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class Application {
+    private static final AttendanceManager attendanceManager = new AttendanceManager();
+
     public static void main(String[] args) {
-        AttendanceManager attendanceManager = new AttendanceManager();
         initAttendances(attendanceManager);
-        Options options = new Options(attendanceManager, now());
         while (true) {
             LocalDate today = now();
             String option = readOption(today);
-            boolean isExitOption = options.isExitOption(option);
+            boolean isExitOption = Options.isExitOption(option);
             if (isExitOption) {
                 break;
             }
-            try {
-                options.run(option);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+            run(option);
+        }
+    }
+
+    private static void run(final String option) {
+        try {
+            Options.run(option);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -34,6 +58,47 @@ public class Application {
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
         }
+    }
+
+    public static void doAttendance() {
+        validateOperationDate(now());
+        Nickname crewNickname = new Nickname(readCrewNickname());
+        attendanceManager.validateExistingCrew(crewNickname);
+        attendanceManager.validateDuplicatedAttendance(crewNickname, now());
+        LocalTime attendanceTime = readAttendanceTime();
+        validateOperationTime(attendanceTime);
+        Attendance attendance = attendanceManager.addAttendance(crewNickname, now(), attendanceTime);
+        printAttendance(attendance);
+    }
+
+    public static void modifyAttendance() {
+        Nickname crewNickname = new Nickname(readCrewNicknameToModify());
+        attendanceManager.validateExistingCrew(crewNickname);
+        LocalDate dateToModify = readAttendanceDateToModify(now());
+        validateOperationDate(dateToModify);
+        Optional<Attendance> existingAttendance = attendanceManager.findAttendance(crewNickname, dateToModify);
+        if (existingAttendance.isEmpty()) {
+            printNoAttendanceToModify();
+            return;
+        }
+        LocalTime modificationTime = readAttendanceModificationTime();
+        validateOperationTime(modificationTime);
+        Attendance modifiedAttendance = attendanceManager.modifyAttendance(crewNickname, dateToModify, modificationTime);
+        printAttendanceModificationResult(existingAttendance.get(), modifiedAttendance);
+    }
+
+    public static void checkAttendanceHistory() {
+        Nickname crewNickname = new Nickname(readCrewNickname());
+        attendanceManager.validateExistingCrew(crewNickname);
+        List<Attendance> monthlyAttendances = attendanceManager.getMonthlyAttendances(now(), crewNickname);
+        printMonthlyAttendances(now(), crewNickname, monthlyAttendances);
+        AttendanceStatistics attendanceStatistics = attendanceManager.getAttendanceStatistics(now(), crewNickname);
+        printAttendanceStatistics(attendanceStatistics);
+    }
+
+    public static void checkDangerousCrews() {
+        Map<Nickname, AttendanceStatistics> dangerousCrewsInformation = attendanceManager.getDangerousCrewsInformation(now());
+        printDangerousCrewsInformation(dangerousCrewsInformation);
     }
 
     private static LocalDate now() {
