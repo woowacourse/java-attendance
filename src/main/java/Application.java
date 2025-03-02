16570a80f1671f0f76818fb955f9a768d45a82bc
DongYeop <s01054830455@gmail.com>
2025-03-02 17:25:55 +0900

    feat: 프로그램 실행을 위한 Application 클래스 추가

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import controller.AttendanceController;
+
+public class Application {
+
+    public static void main(String[] args) {
+        AttendanceController attendanceController = new AttendanceController();
+
+        attendanceController.run();
+    }
+}
