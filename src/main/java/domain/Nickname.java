2c6981edde2605f5d6646fbe90d59f37f45d05ca
ryan-dia <cjfdnjs158@naver.com>
2025-03-02 16:23:17 +0900

    feat: equals, hashCod 재정의

--- src/main/java/domain/Nickname.java
+++ src/main/java/domain/Nickname.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Nickname {
     private static final String NICKNAME_REGEX = "^[가-힣]{2,4}$";
 
@@ -18,5 +20,19 @@ public class Nickname {
         if (!nickname.matches(NICKNAME_REGEX)) {
             throw new IllegalArgumentException("닉네임은 2~4글자이며 한글로만 작성이 가능합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Nickname nickname = (Nickname) o;
+        return Objects.equals(value, nickname.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
     }
 }
