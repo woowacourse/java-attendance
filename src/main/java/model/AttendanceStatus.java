74e39643122b98238208e091a341b2557e08c11f
LJM\eueo8 <eueo8259@gmail.com>
2025-03-03 00:15:57 +0900

    refactor : 출석 상태 로직 수정 및 출석 기록을 기반으로 특정 출석 상태의 개수를 계산하는 메서드 추가

--- src/main/java/model/AttendanceStatus.java
+++ src/main/java/model/AttendanceStatus.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Map;
 
 public enum AttendanceStatus {
     ATTENDANCE("출석", 0),
@@ -19,12 +20,24 @@ public enum AttendanceStatus {
     }
 
     public static AttendanceStatus calculateAttendanceStatus(LocalDate todayDate, LocalTime attendanceTime) {
+        if (attendanceTime == null) {
+            return AttendanceStatus.ABSENT;
+        }
         LocalTime attendanceStartTime = WeeklyAttendanceSchedule.findAttendanceScheduleByLocalDate(todayDate);
         return Arrays.stream(AttendanceStatus.values())
                 .sorted(Comparator.comparingInt(AttendanceStatus::getThresholdMinutes).reversed())
                 .filter(status -> attendanceTime.isAfter(attendanceStartTime.plusMinutes(status.getThresholdMinutes())))
                 .findFirst()
                 .orElse(AttendanceStatus.ATTENDANCE);
+    }
+
+    public static long calculateAttendanceStatusCount(Map<LocalDate, LocalTime> attendanceRecord,
+                                                      AttendanceStatus attendanceStatus) {
+        return attendanceRecord.entrySet().stream()
+                .filter(entry -> AttendanceStatus.calculateAttendanceStatus(
+                        entry.getKey(),
+                        entry.getValue()).equals(attendanceStatus))
+                .count();
     }
 
     public String getAttendanceStatus() {
