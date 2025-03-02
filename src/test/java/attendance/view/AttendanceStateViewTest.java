947ed6eae069ca921dd718f3c26d95793eab0318
taek2222 <oot309@daum.net>
2025-03-02 21:37:56 +0900

    test(AttendanceStateViewTest): 상태 출력 Enum 정의

---
+++ src/test/java/attendance/view/AttendanceStateViewTest.java
@@ -0,0 +1,22 @@
+package attendance.view;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AttendanceStateViewTest {
+
+    @Test
+    @DisplayName("이름과 동일한 출력문을 반환한다")
+    void 이름과_동일한_출력문을_반환한다() {
+        // given
+        String name = "ATTENDANCE";
+
+        // when
+        AttendanceStateView result = AttendanceStateView.find(name);
+
+        // then
+        Assertions.assertThat(result.getName())
+                .isEqualTo("출석");
+    }
+}
