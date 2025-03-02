c2f26a7cd8b31504390176a81d530dd56fac4a10
LJM\eueo8 <eueo8259@gmail.com>
2025-03-03 00:18:31 +0900

    refactor : findExpulsionRiskStudents 메서드의 filter 기준 오류 수정

--- src/main/java/model/AttendanceBook.java
+++ src/main/java/model/AttendanceBook.java
@@ -2,14 +2,12 @@ package model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class AttendanceBook {
-    private static final int LATE_COUNT_FOR_ONE_ABSENCE = 3;
     private final List<Student> students;
 
     public AttendanceBook(List<Student> attendanceBook) {
@@ -22,13 +20,13 @@ public class AttendanceBook {
                 .collect(Collectors.toList());
     }
 
-    public void updateNonExistentAttendanceRecords(LocalDate today){
-        for (Student student : students){
+    public void updateNonExistentAttendanceRecords(LocalDate today) {
+        for (Student student : students) {
             student.updateNonAttendanceRecordStatusIsAbsent(today);
         }
     }
 
-    public Student findStudentByNickName(String name){
+    public Student findStudentByNickName(String name) {
         return students.stream()
                 .filter(stu -> stu.getName().equals(name))
                 .findFirst()
@@ -37,7 +35,8 @@ public class AttendanceBook {
 
     public List<Student> findExpulsionRiskStudents() {
         return students.stream()
-                .filter(student -> student.calculateTotalAbsentCount() >= LATE_COUNT_FOR_ONE_ABSENCE)
+                .filter(student -> student.calculateTotalAbsentCount()
+                        >= AttendancePenalty.COUNSELING.getThresholdAbsenceCount())
                 .sorted(Comparator.comparing(Student::calculateTotalAbsentCount))
                 .collect(Collectors.toList());
     }
