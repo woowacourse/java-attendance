98786e4f4bd935273703e8cfb16500ea98b01417
saera <yook.saera@gmail.com>
2025-03-02 19:36:44 +0900

    test: 출석 기록 1개 불러오기 테스트 작성

---
+++ src/test/java/attendance/AttendanceLoaderTest.java
@@ -0,0 +1,21 @@
+package attendance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AttendanceLoaderTest {
+    @DisplayName("크루의 출석 기록 1개 저장 성공")
+    @Test
+    void test1() {
+        String data = "쿠키,2024-12-13 10:08";
+
+        AttendanceBook attendanceBook = AttendanceLoader.load(data);
+
+        assertThat(attendanceBook)
+                .isNotNull()
+                .isInstanceOf(AttendanceBook.class);
+        assertThat(attendanceBook.countCrews()).isEqualTo(1);
+    }
+}
