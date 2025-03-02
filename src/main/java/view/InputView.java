d6651ca022bdefa4a8aac3871017f108ece76f81
ppparkta <pppppark2@gmail.com>
2025-03-03 03:03:35 +0900

    feat: 출석 수정 사용자 입력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -19,13 +19,28 @@ public class InputView {
         return inputValue();
     }
 
-    public String inputNickname() {
+    public String inputAttendNickname() {
         System.out.println("닉네임을 입력해 주세요.");
+        return inputValue();
+    }
+
+    public String inputEditNickname() {
+        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
         return inputValue();
     }
 
     public String getAttendTime() {
         System.out.println("등교 시간을 입력해 주세요.");
+        return inputValue();
+    }
+
+    public String getEditDate() {
+        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        return inputValue();
+    }
+
+    public String getEditTime() {
+        System.out.println("언제로 변경하겠습니까?");
         return inputValue();
     }
 
