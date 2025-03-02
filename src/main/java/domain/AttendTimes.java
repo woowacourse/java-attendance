dde4344fa48f76b1237c8a4f631155f026ef87c0
supernovaMK <minkikim2002@gmail.com>
2025-03-02 17:56:59 +0900

    refactor: 인덴트 깊이(2->1) 수정

--- src/main/java/domain/AttendTimes.java
+++ src/main/java/domain/AttendTimes.java
@@ -1,8 +1,10 @@
 package domain;
 
+import controller.AttendanceController;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -22,50 +24,53 @@ public class AttendTimes {
     }
 
     public Optional<AttendTime> findAttendanceByDate(int date) {
-        return attendTimes.stream()
-                .filter(attendTime -> attendTime.checkSameDate(date))
-                .findAny();
+        return attendTimes.stream().filter(attendTime -> attendTime.checkSameDate(date)).findAny();
     }
 
     public List<AttendTime> getAttendTimeline() {
         List<AttendTime> allAttendanceFromToday = new ArrayList<>();
 
-        LocalDate todayLocalDate = LocalDate.parse(AttendanceController.TODAY_LOCAL_DATE, AttendanceController.TODAY_FORMATTER);
+        LocalDate todayLocalDate = LocalDate.parse(AttendanceController.TODAY_LOCAL_DATE,
+                AttendanceController.TODAY_FORMATTER);
         int todayDayOfMonth = todayLocalDate.getDayOfMonth();
         int todayMonthValue = todayLocalDate.getMonthValue();
         int todayYear = todayLocalDate.getYear();
 
-        for (int i = 1; i < todayDayOfMonth; i++) {
-            AttendTime attendTime = findAttendanceByDate(i).orElse(null);
-            if (attendTime != null) {
-                allAttendanceFromToday.add(attendTime);
-                continue;
+        IntStream.range(1, todayDayOfMonth).mapToObj(i -> {
+            Optional<AttendTime> optionalAttendTime = findAttendanceByDate(i);
+            if (optionalAttendTime.isPresent()) {
+                return optionalAttendTime.get();
             }
+            if (December.isWeekDay(i)) {
+                return new AttendTime(LocalDate.of(todayYear, todayMonthValue, i), null);
+            }
+            return null;
+        }).filter(Objects::nonNull).forEach(allAttendanceFromToday::add);
 
-            if (December.checkWeekDay(i)) {
-                AttendTime absentAttendance = new AttendTime(LocalDate.of(todayYear, todayMonthValue, i), null);
-                allAttendanceFromToday.add(absentAttendance);
-            }
-        }
         return allAttendanceFromToday;
     }
 
     public int calculateAttendedCount() {
-        return (int) attendTimes.stream().filter(attendTime -> attendTime.checkAttendanceStatus().equals(AttendanceStatus.ATTENDED)).count();
+        return (int) attendTimes.stream()
+                .filter(attendTime -> attendTime.checkAttendanceStatus().equals(AttendanceStatus.ATTENDED)).count();
     }
 
     public int calculateLateCount() {
-        return (int) attendTimes.stream().filter(attendTime -> attendTime.checkAttendanceStatus().equals(AttendanceStatus.LATE)).count();
+        return (int) attendTimes.stream()
+                .filter(attendTime -> attendTime.checkAttendanceStatus().equals(AttendanceStatus.LATE)).count();
     }
 
     public int calculateAbsentCount() {
         int total = 0;
-        LocalDate todayLocalDate = LocalDate.parse(AttendanceController.TODAY_LOCAL_DATE, AttendanceController.TODAY_FORMATTER);
+        LocalDate todayLocalDate = LocalDate.parse(AttendanceController.TODAY_LOCAL_DATE,
+                AttendanceController.TODAY_FORMATTER);
         int todayDayOfMonth = todayLocalDate.getDayOfMonth();
 
-        total += (int) IntStream.range(1, todayDayOfMonth).filter(i -> findAttendanceByDate(i).isEmpty() && December.checkWeekDay(i)).count();
+        total += (int) IntStream.range(1, todayDayOfMonth)
+                .filter(i -> findAttendanceByDate(i).isEmpty() && December.isWeekDay(i)).count();
 
-        return total + (int) attendTimes.stream().filter(attendTime -> attendTime.checkAttendanceStatus().equals(AttendanceStatus.ABSENT)).count();
+        return total + (int) attendTimes.stream()
+                .filter(attendTime -> attendTime.checkAttendanceStatus().equals(AttendanceStatus.ABSENT)).count();
     }
 
     public List<AttendTime> getAttendTimes() {
