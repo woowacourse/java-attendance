1b7c498a11c2a23c4fba30eae4027e10b969e5c4
soeun2537 <soeun2537@gmail.com>
2025-03-03 15:53:37 +0900

    refactor: InputViewMessage에 Text Blocks 적용

--- src/main/java/constant/InputViewMessage.java
+++ src/main/java/constant/InputViewMessage.java
@@ -2,12 +2,13 @@ package constant;
 
 public enum InputViewMessage {
 
-    ATTENDANCE_OPTION_PROMPT("오늘은 %s월 %s일 %s입니다. 기능을 선택해 주세요.%n"
-            + "1. 출석 확인%n"
-            + "2. 출석 수정%n"
-            + "3. 크루별 출석 기록 확인%n"
-            + "4. 제적 위험자 확인%n"
-            + "Q. 종료"),
+    ATTENDANCE_OPTION_PROMPT("""
+            오늘은 %s월 %s일 %s입니다. 기능을 선택해 주세요.
+            1. 출석 확인
+            2. 출석 수정
+            3. 크루별 출석 기록 확인
+            4. 제적 위험자 확인
+            Q. 종료"""),
 
     ATTENDANCE_CHECK_IN_NICKNAME_PROMPT("닉네임을 입력해 주세요."),
     ATTENDANCE_CHECK_IN_TIME_PROMPT("등교 시간을 입력해 주세요."),
