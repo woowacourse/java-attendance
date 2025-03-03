3386da3688a8b93f7a0663ce074d93f09be7407d
yesjuhee <juhee0924@g.skku.edu>
2025-03-03 13:02:35 +0900

    feat : 6.1 프로그램 시작 시 출석 기록 파일에서 출석 기록을 읽어올 수 있다.

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -1,8 +1,17 @@
 package controller;
 
+import domain.AttendanceDateTime;
+import domain.AttendanceDateTimes;
+import domain.AttendanceHistories;
+import domain.Crew;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import view.FileInputView;
 import view.InputView;
 import view.Menu;
 import view.OutputView;
@@ -10,6 +19,7 @@ import view.OutputView;
 public class AttendanceController {
     private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
+    private final FileInputView fileInputView = new FileInputView();
     private final Map<Menu, Runnable> menuTable = new EnumMap<>(Menu.class);
 
     {
@@ -21,9 +31,25 @@ public class AttendanceController {
     }
 
     public void run(LocalDate today) {
-        outputView.displayMenu(today);
-        Menu menuInput = inputView.readMenu();
-        menuTable.get(menuInput).run();
+        AttendanceHistories attendanceHistories = loadCsvData();
+        do {
+            outputView.displayMenu(today);
+            Menu menuInput = inputView.readMenu();
+            menuTable.get(menuInput).run();
+        } while (true);
+    }
+
+    private AttendanceHistories loadCsvData() {
+        Map<String, List<LocalDateTime>> rawAttendanceData = fileInputView.readAttendanceFile();
+        Map<Crew, AttendanceDateTimes> rawAttendanceHistoryData = new HashMap<>();
+        for (Entry<String, List<LocalDateTime>> attendanceEntry : rawAttendanceData.entrySet()) {
+            Crew crew = new Crew(attendanceEntry.getKey());
+            AttendanceDateTimes attendanceDateTimes = new AttendanceDateTimes(attendanceEntry.getValue().stream()
+                    .map(AttendanceDateTime::new)
+                    .toList());
+            rawAttendanceHistoryData.put(crew, attendanceDateTimes);
+        }
+        return new AttendanceHistories(rawAttendanceHistoryData);
     }
 
     private void checkIn() {
@@ -45,4 +71,13 @@ public class AttendanceController {
     private void quit() {
         System.exit(0);
     }
+
+//    private void retryUntilSuccess(Runnable runnable) {
+//        try {
+//            runnable.run();
+//        } catch (IllegalArgumentException e) {
+//            System.out.printf("%s%n", e.getMessage());
+//            retryUntilSuccess(runnable);
+//        }
+//    }
 }
