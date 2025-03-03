5e9dd52b385e2bade386b445d5365f92bf9f83ea
jye326 <jye326@naver.com>
2025-03-03 12:20:26 +0900

    refactor(constant) : MenuOption 코드 컨벤션 적용
    - 줄 바꿈

--- src/main/java/constant/MenuOption.java
+++ src/main/java/constant/MenuOption.java
@@ -5,13 +5,14 @@ public enum MenuOption {
     MODIFY_ATTENDANCE("2"),
     CHECK_CREW_ATTENDANCE_HISTORY("3"),
     CHECK_PENALTY_RECEIVED_CREW("4"),
-
     QUIT("[qQ]");
 
     private final String option;
-    MenuOption(String option){
+
+    MenuOption(String option) {
         this.option = option;
     }
+
     public String getOption() {
         return option;
     }
