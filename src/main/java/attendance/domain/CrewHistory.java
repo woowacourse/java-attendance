b5a128a7aa14e8cc8e9d52b3e16499aa4cb4cf31
Mint <audwl03071@gmail.com>
2025-03-03 13:32:41 +0900

    refactor(CrewHistory): 불필요한 import문 제거

--- src/main/java/attendance/domain/CrewHistory.java
+++ src/main/java/attendance/domain/CrewHistory.java
@@ -2,7 +2,6 @@ package attendance.domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
