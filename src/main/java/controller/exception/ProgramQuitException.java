f4c87837ed9daef75c50c9e54b2dd7d542dc51ea
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-03-03 23:24:31 +0900

    refactor: 패키지 이동

--- src/main/java/controller/exception/ProgramQuitException.java
+++ src/main/java/controller/exception/ProgramQuitException.java
@@ -1,4 +1,4 @@
-package controller;
+package controller.exception;
 
 public class ProgramQuitException extends RuntimeException {
     public static final String MESSAGE = "프로그램을 종료합니다.";
--- src/main/java/reader/exception/FileReadException.java
+++ src/main/java/reader/exception/FileReadException.java
@@ -1,4 +1,4 @@
-package reader;
+package reader.exception;
 
 import java.io.IOException;
 
