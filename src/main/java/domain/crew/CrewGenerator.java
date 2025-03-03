82e09cd8b199aed7e26de82b4ee03c9a0863384d
ryan-dia <cjfdnjs158@naver.com>
2025-03-04 01:51:22 +0900

    refactor: 메서드 분리

--- src/main/java/domain/crew/CrewGenerator.java
+++ src/main/java/domain/crew/CrewGenerator.java
@@ -22,6 +22,18 @@ public class CrewGenerator {
 
     public Crews generate(final String filePath) {
         final List<String[]> crews = CsvReader.readFile(filePath);
+        final Map<Nickname, TreeSet<AttendanceRecord>> crewData = generateCrewData(crews);
+        final List<Crew> crewGroup = new ArrayList<>();
+        crewData.forEach((nickname, attendanceRecordGroup) -> {
+            final AttendanceRecords attendanceRecords = new AttendanceRecords(attendanceRecordGroup);
+            processAbsence(attendanceRecords);
+            final Crew crew = generateCrew(nickname, attendanceRecords);
+            crewGroup.add(crew);
+        });
+        return new Crews(crewGroup);
+    }
+
+    private static Map<Nickname, TreeSet<AttendanceRecord>> generateCrewData(final List<String[]> crews) {
         final Map<Nickname, TreeSet<AttendanceRecord>> crewData = new HashMap<>();
         for (final String[] crew : crews) {
             final String inputNickname = crew[0];
@@ -31,34 +43,32 @@ public class CrewGenerator {
             final AttendanceRecord attendanceRecord = new AttendanceRecord(attendanceDateTime);
             crewData.computeIfAbsent(nickname, k -> new TreeSet<>()).add(attendanceRecord);
         }
+        return crewData;
+    }
 
-        final List<Crew> crewGroup = new ArrayList<>();
-        crewData.forEach((nickname, attendanceRecordGroup) -> {
-            final AttendanceRecords attendanceRecords = new AttendanceRecords(attendanceRecordGroup);
-            final List<Integer> validDays = DateUtil.calculateValidDays(
-                    REFERENCE_YEAR,
-                    REFERENCE_MONTH,
-                    REFERENCE_DAY - 1
-            );
+    private static void processAbsence(final AttendanceRecords attendanceRecords) {
+        final List<Integer> validDays = DateUtil.calculateValidDays(
+                REFERENCE_YEAR,
+                REFERENCE_MONTH,
+                REFERENCE_DAY - 1
+        );
+        validDays.forEach(day -> {
+            final LocalDate localDate = LocalDate.of(REFERENCE_YEAR, REFERENCE_MONTH, day);
+            final AttendanceRecord attendanceRecord = attendanceRecords.findByDate(localDate);
 
-            validDays.forEach(day -> {
-                final LocalDate localDate = LocalDate.of(REFERENCE_YEAR, REFERENCE_MONTH, day);
-                final AttendanceRecord attendanceRecord = attendanceRecords.findByDate(localDate);
-
-                if (attendanceRecord == null) {
-                    final AttendanceDateTime attendanceDateTime = AttendanceDateTime.createAbsence(localDate);
-                    attendanceRecords.add(new AttendanceRecord(attendanceDateTime));
-                }
-            });
-
-            final AttendanceStatusCounts attendanceStatusCounts = attendanceRecords.getAttendanceStatusCounts();
-            final int absence = attendanceStatusCounts.getAbsence();
-            final int late = attendanceStatusCounts.getLate();
-            final Crew crew = new Crew(nickname, attendanceRecords,
-                    DisciplinaryStatus.findByAbsenceAndLatenessCount(absence, late));
-            crewGroup.add(crew);
+            if (attendanceRecord == null) {
+                final AttendanceDateTime attendanceDateTime = AttendanceDateTime.createAbsence(localDate);
+                attendanceRecords.add(new AttendanceRecord(attendanceDateTime));
+            }
         });
+    }
 
-        return new Crews(crewGroup);
+    private static Crew generateCrew(final Nickname nickname, final AttendanceRecords attendanceRecords) {
+        final AttendanceStatusCounts attendanceStatusCounts = attendanceRecords.getAttendanceStatusCounts();
+        final int absence = attendanceStatusCounts.getAbsence();
+        final int late = attendanceStatusCounts.getLate();
+        final DisciplinaryStatus disciplinaryStatus = DisciplinaryStatus.findByAbsenceAndLatenessCount(absence,
+                late);
+        return new Crew(nickname, attendanceRecords, disciplinaryStatus);
     }
 }
