9dc8f13bca02d4562d2b313fcd09cabe46103149
LeeJaeHoon <dlwogns3413@naver.com>
2025-03-04 01:01:18 +0900

    refactor: InputView 입력 포맷 Text Blocks 활용

--- src/main/java/attendance/io/view/InputView.java
+++ src/main/java/attendance/io/view/InputView.java
@@ -8,13 +8,14 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String INPUT_COMMAND_FORMAT =
-            "오늘은 %s입니다. 기능을 선택해 주세요.\n"
-                    + "1. 출석 확인\n"
-                    + "2. 출석 수정\n"
-                    + "3. 크루별 출석 기록 확인\n"
-                    + "4. 제적 위험자 확인\n"
-                    + "Q. 종료\n";
+    private static final String INPUT_COMMAND_FORMAT = """
+            오늘은 %s입니다. 기능을 선택해 주세요.
+            1. 출석 확인
+            2. 출석 수정
+            3. 크루별 출석 기록 확인
+            4. 제적 위험자 확인
+            Q. 종료
+            """;
 
     private final Scanner scanner = new Scanner(System.in);
 
