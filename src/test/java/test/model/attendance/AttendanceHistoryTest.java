a7ee538c23e9418ada5169760e635c7dd2d354d4
Chaeyoung714 <202102760@hufs.ac.kr>
2025-03-03 12:31:58 +0900

    test : 출석 기록 조회 관련 단위테스트 추가

--- src/test/java/test/model/attendance/AttendanceHistoryTest.java
+++ src/test/java/test/model/attendance/AttendanceHistoryTest.java
@@ -3,17 +3,23 @@ package test.model.attendance;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import common.Campus;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import model.attendance.Attendance;
 import model.attendance.AttendanceHistory;
 import model.exception.FutureAttendanceException;
 import model.exception.HolidayAttendanceException;
+import net.bytebuddy.asm.Advice.Local;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AttendanceHistoryTest {
 
+    /**
+     * 출석 수정 관련
+     */
     @DisplayName("수정하려는 날짜에 맞는 수정전 Attendance 객체를 찾는다.")
     @Test
     void success_findOldAttendanceByModifyDate() {
@@ -74,6 +80,34 @@ public class AttendanceHistoryTest {
         assertThatThrownBy(() -> {
             attendanceHistory.modifyFrom(oldAttendance, modifyTime);
         }).isInstanceOf(FutureAttendanceException.class);
+    }
+
+    /**
+     * 출석 기록 조회 관련
+     */
+    @DisplayName("한 크루의 요청 일자 전날까지의 출석 기록을 반환한다.")
+    @Test
+    void success_sliceAttendanceDatesUntilBeforeRequestDate() {
+        //given
+        LocalDate requestDate = LocalDate.of(2024, 12, 13);
+        AttendanceHistory attendanceHistory = new AttendanceHistory();
+        attendanceHistory.register(LocalDate.of(2024, 12, 10), LocalTime.of(10, 0));
+        attendanceHistory.register(LocalDate.of(2024, 12, 11), LocalTime.of(10, 6));
+        attendanceHistory.register(LocalDate.of(2024, 12, 12), LocalTime.of(10, 31));
+
+        //when
+        List<Attendance> attendanceHistories = attendanceHistory.sliceByDateUntilBefore(requestDate);
+
+        //then
+        assertThat(attendanceHistories.size()).isEqualTo(9);
+        assertThat(attendanceHistories.get(5)).isEqualTo(
+                new Attendance(LocalDate.of(2024, 12, 9), Campus.NONE_ATTENDANCE_TIME));
+        assertThat(attendanceHistories.get(6)).isEqualTo(
+                new Attendance(LocalDate.of(2024, 12, 10), LocalTime.of(10, 0)));
+        assertThat(attendanceHistories.get(7)).isEqualTo(
+                new Attendance(LocalDate.of(2024, 12, 11), LocalTime.of(10, 6)));
+        assertThat(attendanceHistories.get(8)).isEqualTo(
+                new Attendance(LocalDate.of(2024, 12, 12), LocalTime.of(10, 31)));
     }
 
 
