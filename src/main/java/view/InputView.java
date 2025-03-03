4b809bcb1b8f0bc22768f63f9ed66a8c26f12120
jbilee <julishahn@gmail.com>
2025-03-03 11:30:37 +0900

    refactor(view): 콘솔 가독성을 위한 line break 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -15,7 +15,7 @@ public class InputView {
     }
 
     public String readMenuSelection(LocalDate currentDate) {
-        System.out.printf("오늘은 %s입니다. 기능을 선택해 주세요." + System.lineSeparator() +
+        System.out.printf("%n오늘은 %s입니다. 기능을 선택해 주세요." + System.lineSeparator() +
                 "1. 출석 확인" + System.lineSeparator() +
                 "2. 출석 수정" + System.lineSeparator() +
                 "3. 크루별 출석 기록 확인" + System.lineSeparator() +
