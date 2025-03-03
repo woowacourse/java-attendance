e8ca5cadd09f9f155309b11d254618fccd0d5ea7
ryan-dia <cjfdnjs158@naver.com>
2025-03-03 14:06:49 +0900

    style: 오타 수정

--- src/main/java/domain/AttendanceTime.java
+++ src/main/java/domain/AttendanceTime.java
@@ -33,7 +33,7 @@ public class AttendanceTime {
 
     private void validateTime(final LocalTime time) {
         if (time.isBefore(START_TIME) || time.isAfter(END_TIME)) {
-            throw new IllegalArgumentException("운영 시간에만 출석이 가능합니다. (운영 시간: 08:00 ~ 23:00");
+            throw new IllegalArgumentException("운영 시간에만 출석이 가능합니다. (운영 시간: 08:00 ~ 23:00)");
         }
     }
 
