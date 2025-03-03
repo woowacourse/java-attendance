0566f1de84348c93ff279c14805ea9adbbb8edac
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-03-03 13:57:06 +0900

    refactor: CrewTest에 ParameterizedTest 적용

--- src/test/java/domain/CrewTest.java
+++ src/test/java/domain/CrewTest.java
@@ -2,31 +2,32 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CrewTest {
-    @Test
-    void 같은_nickname_멤버를_가지면_같은_객체로_취급한다() {
-        Crew crew1 = new Crew("에드");
 
-        Crew crew2 = new Crew("에드");
+    @ParameterizedTest
+    @CsvSource({
+            "에드, 에드, true",
+            "에드, 제프, false"
+    })
+    void 같은_nickname_멤버를_가지면_같은_객체로_취급한다(String nickname1, String nickname2, boolean expected) {
+        Crew crew1 = new Crew(nickname1);
+        Crew crew2 = new Crew(nickname2);
 
-        Crew crew3 = new Crew("제프");
-
-        assertThat(crew1).isEqualTo(crew2);
-        assertThat(crew1).isNotEqualTo(crew3);
+        assertThat(crew1.equals(crew2)).isEqualTo(expected);
     }
 
-    @Test
-    void 같은_nickname_멤버를_가지면_같은_해쉬코드를_반환한다() {
-        Crew crew1 = new Crew("에드");
+    @ParameterizedTest
+    @CsvSource({
+            "에드, 에드, true",
+            "에드, 제프, false"
+    })
+    void 같은_nickname_멤버를_가지면_같은_해쉬코드를_반환한다(String nickname1, String nickname2, boolean expected) {
+        Crew crew1 = new Crew(nickname1);
+        Crew crew2 = new Crew(nickname2);
 
-        Crew crew2 = new Crew("에드");
-
-        Crew crew3 = new Crew("제프");
-
-        assertThat(crew1.hashCode()).isEqualTo(crew2.hashCode());
-        assertThat(crew1.hashCode()).isNotEqualTo(crew3.hashCode());
+        assertThat(crew1.hashCode() == crew2.hashCode()).isEqualTo(expected);
     }
-
 }
