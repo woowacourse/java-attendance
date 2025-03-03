73e0b198f2db702e336e4f97155a76ee214d3ce0
[CLOUD4] 백승주 <bsj5967@naver.com>
2025-03-03 14:29:43 +0900

    red:  이름을 기준으로 기존 출석 기록에 추가하기 테스트

--- src/test/java/AttendanceHistoriesTest.java
+++ src/test/java/AttendanceHistoriesTest.java
@@ -28,4 +28,28 @@ public class AttendanceHistoriesTest {
 
         assertThat(histories.findByName(name).getName()).isEqualTo(name);
     }
+
+    @Test
+    @DisplayName("이름을 기준으로 기존 출석 기록에 추가하기")
+    public void addAttendanceHistoriesByName() {
+        // given
+        String name = "쿠키";
+        LocalDateTime attendanceTime1 = LocalDateTime.of(2024, 12, 17, 10, 0);
+        LocalDateTime attendanceTime2 = LocalDateTime.of(2024, 12, 18, 10, 1);
+        LocalDateTime attendanceTime3 = LocalDateTime.of(2024, 12, 19, 10, 2);
+        List<Attendance> attendances = new ArrayList<>();
+        attendances.add(new Attendance(attendanceTime1));
+        attendances.add(new Attendance(attendanceTime2));
+        attendances.add(new Attendance(attendanceTime3));
+        AttendanceHistory attendanceHistory = new AttendanceHistory(name, attendances);
+
+        List<AttendanceHistory> attendanceHistories = new ArrayList<>();
+        attendanceHistories.add(attendanceHistory);
+        AttendanceHistories histories = new AttendanceHistories(attendanceHistories);
+        histories.addAttendanceHistory(name, new Attendance(LocalDateTime.of(2024, 12, 20, 10, 3));
+
+        assertThat(histories.findByName(name).getAttendances().size()).isEqualTo(4);
+
+
+    }
 }
