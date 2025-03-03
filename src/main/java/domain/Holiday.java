bd3549e94090d953704beb2ea8d8ad66f87338b2
jhan0121 <jhan0121@gmail.com>
2025-03-03 15:41:26 +0900

    refactor: Localdate -> MonthDay 멤버 변수 타입 변경

--- src/main/java/domain/Holiday.java
+++ src/main/java/domain/Holiday.java
@@ -1,19 +1,21 @@
 package domain;
 
 import java.time.LocalDate;
+import java.time.MonthDay;
 import java.util.Arrays;
 
 public enum Holiday {
     CHRISTMAS(12, 25);
 
-    private final LocalDate date;
+    private final MonthDay date;
 
     Holiday(final int month, final int day) {
-        this.date = LocalDate.of(Current.TODAY.getYear(), month, day);
+        this.date = MonthDay.of(month, day);
     }
 
     public static boolean isHoliday(final LocalDate targetDate) {
+        MonthDay targetDay = MonthDay.from(targetDate);
         return Arrays.stream(Holiday.values())
-                .anyMatch(holiday -> holiday.date.equals(targetDate));
+                .anyMatch(holiday -> holiday.date.equals(targetDay));
     }
 }
