c8f51c4517734947001f1f215677efea81565386
LJM\eueo8 <eueo8259@gmail.com>
2025-03-03 16:02:35 +0900

    refactor: attendanceTimeRecord의 메서드 삭제에의한 테스트 삭제

--- src/test/java/model/AttendanceTimeRecordTest.java
+++ src/test/java/model/AttendanceTimeRecordTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,24 +16,6 @@ class AttendanceTimeRecordTest {
             LocalDateTime.of(2024, 12, 10, 10, 0),
             LocalDateTime.of(2024, 12, 9, 13, 0)
     ));
-
-    @Test
-    void modifyAttendanceTimeRecord() {
-        LocalDate testDate = LocalDate.of(2024,12,13);
-        LocalTime expect = LocalTime.of(13,0);
-        attendanceTimeRecord.modifyAttendanceTimeRecord(testDate, expect);
-        LocalTime result = attendanceTimeRecord.getAttendanceTimeRecords().get(testDate);
-        assertEquals(expect, result);
-    }
-
-    @Test
-    void registerAttendanceTimeRecord() {
-        LocalDate testDate = LocalDate.of(2024,12,20);
-        LocalTime expect = LocalTime.of(13,0);
-        attendanceTimeRecord.registerAttendanceTimeRecord(testDate, expect);
-        LocalTime result = attendanceTimeRecord.getAttendanceTimeRecords().get(testDate);
-        assertEquals(expect, result);
-    }
 
     @Test
     @DisplayName("테스트 날짜가 저장되어있지 않는지 테스트")
@@ -50,14 +31,5 @@ class AttendanceTimeRecordTest {
         LocalDate testDate = LocalDate.of(2024,12,13);
         boolean result = attendanceTimeRecord.checkAttendanceRecordByLocalDate(testDate);
         assertTrue(result);
-    }
-
-
-    @Test
-    @DisplayName("테스트 날짜에 LocalTime이 null인지 확인 테스트")
-    void putNullLocalTime() {
-        LocalDate testDate = LocalDate.of(2024,12,13);
-        attendanceTimeRecord.putNullLocalTime(testDate);
-        assertNull(attendanceTimeRecord.getAttendanceTimeRecords().get(testDate));
     }
 }
