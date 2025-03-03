d40f25dd9ed95de9a44fc7d90da0b6f703c979e6
geonwoo <kysub99@naver.com>
2025-03-03 17:17:28 +0900

    refactor: main 함수 오버로딩

--- src/main/java/AttendanceApplication.java
+++ src/main/java/AttendanceApplication.java
@@ -1,9 +1,16 @@
 import controller.AttendanceController;
 import domain.DateProvider;
+import domain.SystemDateProvider;
 import view.InputView;
 import view.OutputView;
 
 public class AttendanceApplication {
+    // 기존 main 메서드
+    public static void main(String[] args) {
+        DateProvider dateProvider = new SystemDateProvider();
+        main(args, dateProvider);
+    }
+
     public static void main(String[] args, DateProvider dateProvider) {
         final InputView inputView = new InputView();
         final OutputView outputView = new OutputView();
