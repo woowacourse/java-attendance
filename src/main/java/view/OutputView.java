d4fa967659c987a296409a227b842e08856a1562
jye326 <jye326@naver.com>
2025-03-03 16:04:57 +0900

    refactor(view) : OutputView 출력문 가독성 향상
    - 메뉴 안내문 텍스트 블록 적용
    - 출석 완료 및 수정, 출석 기록, 제적 위험자 출력 로직 String.format 적용

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -18,11 +18,13 @@ public class OutputView {
     }
 
     public static void printMenu() {
-        System.out.println("1. 출석 확인\n"
-                + "2. 출석 수정\n"
-                + "3. 크루별 출석 기록 확인\n"
-                + "4. 제적 위험자 확인\n"
-                + "Q. 종료");
+        System.out.println("""
+                1. 출석 확인
+                2. 출석 수정
+                3. 크루별 출석 기록 확인
+                4. 제적 위험자 확인
+                Q. 종료
+                """);
     }
 
     public static void printCheckedAttendance(Attendance attendance) {
@@ -30,28 +32,23 @@ public class OutputView {
     }
 
     public static String getFormattedCheckedAttendance(Attendance attendance) {
-        String formattedCheckedAttendance = "";
-        formattedCheckedAttendance += getFormattedDayInfo(attendance.getLocalDate()) + " ";
-        if (attendance.getAttendanceStatus() == AttendanceStatus.ABSENT) {
-            formattedCheckedAttendance += "--:--" + " (" + attendance.getAttendanceStatus().getValue() + ")";
-            return formattedCheckedAttendance;
-        }
-        formattedCheckedAttendance += attendance.getLocalTime()
-                .format(DateTimeFormatter.ofPattern("HH:mm", Locale.KOREA))
-                + " (" + attendance.getAttendanceStatus().getValue() + ")";
-        return formattedCheckedAttendance;
+        String dayInfo = getFormattedDayInfo(attendance.getLocalDate());
+        AttendanceStatus status = attendance.getAttendanceStatus();
+        if (status == AttendanceStatus.ABSENT)
+            return String.format("%s --:-- (%s)", dayInfo, status.getValue());
+        String dateTime = attendance.getLocalTime()
+                .format(DateTimeFormatter.ofPattern("HH:mm", Locale.KOREA));
+        return String.format("%s %s (%s)", dayInfo,dateTime ,status.getValue());
     }
 
     public static void printModifyResult(ModifyResult modifyResult) {
-        System.out.println(
-                getFormattedCheckedAttendance(modifyResult.getOldAttendance()) + " -> "
-                        + modifyResult.getNewAttendance()
-                        .getLocalTime()
-                        .format(DateTimeFormatter.ofPattern("HH:mm", Locale.KOREA))
-                        + " (" + modifyResult.getNewAttendance()
-                        .getAttendanceStatus()
-                        .getValue() + ") 수정 완료!"
-        );
+        String dateTime = modifyResult.getNewAttendance()
+                .getLocalTime()
+                .format(DateTimeFormatter.ofPattern("HH:mm", Locale.KOREA));
+        String status = modifyResult.getNewAttendance()
+                .getAttendanceStatus()
+                .getValue();
+        System.out.println(String.format("%s -> %s (%s) 수정 완료!",getFormattedCheckedAttendance(modifyResult.getOldAttendance()), dateTime, status));
     }
 
     public static void printCrewAttendanceHistory(Crew crew) {
@@ -68,12 +65,9 @@ public class OutputView {
     }
 
     private static String getFormattedPenaltyStatusCount(Crew crew) {
-        String formattedPenaltyStatusCount = "";
-        formattedPenaltyStatusCount += "출석: " + crew.getAttendCount() + "회\n"
-                + "지각: " + crew.getLateCount() + "회\n"
-                + "결석: " + crew.getAbsentCount() + "회\n\n";
-        if (crew.getPenalty() != Penalty.NONE)
-            formattedPenaltyStatusCount += crew.getPenalty().getStatus() + " 대상자입니다.";
+        String formattedPenaltyStatusCount = String.format("출석: %d회\n지각: %d회\n결석: %d회\n\n",crew.getAttendCount(),crew.getLateCount(),crew.getAbsentCount());
+        if(crew.getPenalty()!=Penalty.NONE)
+            formattedPenaltyStatusCount += String.format("%s 대상자입니다.", crew.getPenalty().getStatus());
         return formattedPenaltyStatusCount;
     }
 
@@ -81,12 +75,9 @@ public class OutputView {
         System.out.println("제적 위험자 조회 결과");
         StringBuilder formattedPenaltyReceivedCrew = new StringBuilder();
         for (Crew crew : penaltyReceivedCrew) {
-            formattedPenaltyReceivedCrew.append("- ")
-                    .append(crew.getName())
-                    .append(": 결석 ")
-                    .append(crew.getAbsentCount()).append("회, 지각 ")
-                    .append(crew.getLateCount()).append("회 (")
-                    .append(crew.getPenalty().getStatus()).append(")\n");
+            formattedPenaltyReceivedCrew.append(
+                    String.format("- %s: 결석 %d회, 지각 %d회 (%s)\n",crew.getName(),crew.getAbsentCount(),crew.getLateCount(),crew.getPenalty().getStatus())
+            );
         }
         System.out.println(formattedPenaltyReceivedCrew);
     }
