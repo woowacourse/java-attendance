bec57335ecf469c352823c17cdfce68e65f62194
saera <yook.saera@gmail.com>
2025-03-03 10:57:10 +0900

    test: 크루의 제적, 면담, 경고 대상자 여부 확인 테스트 추가

--- src/test/java/attendance/domain/AttendanceBookTest.java
+++ src/test/java/attendance/domain/AttendanceBookTest.java
@@ -55,10 +55,38 @@ public class AttendanceBookTest {
             attendanceBook.addAttendance(nickname, attendance);
         }
 
-        Map<AttendanceStatus, Integer> attendanceStatusCounts = attendanceBook.getAttendanceStatusCounts(nickname, today);
+        Map<AttendanceStatus, Integer> attendanceStatusCounts =
+                attendanceBook.getAttendanceStatusCounts(nickname, today);
 
         assertThat(attendanceStatusCounts.get(AttendanceStatus.PRESENT)).isEqualTo(5);
         assertThat(attendanceStatusCounts.get(AttendanceStatus.LATE)).isEqualTo(2);
         assertThat(attendanceStatusCounts.get(AttendanceStatus.ABSENT)).isEqualTo(3);
+    }
+
+    @DisplayName("크루의 제적, 면담, 경고 대상자 확인 테스트")
+    @Test
+    void test4() {
+        LocalDateTime today = LocalDateTime.of(2024, 12, 16, 13, 0);
+        String nickname = "빙티";
+        List<LocalDateTime> attendances = List.of(
+                LocalDateTime.of(2024, 12, 2, 13, 0), //출석
+                LocalDateTime.of(2024, 12, 3, 10, 7), //지각
+                LocalDateTime.of(2024, 12, 4, 10, 2), //출석
+                LocalDateTime.of(2024, 12, 5, 10, 6), //지각
+                LocalDateTime.of(2024, 12, 6, 10, 1), //출석
+                LocalDateTime.of(2024, 12, 10, 10, 3), //출석
+                LocalDateTime.of(2024, 12, 13, 10, 2), //출석
+                today
+        ); //결석 3회
+
+        AttendanceBook attendanceBook = new AttendanceBook();
+        attendanceBook.addCrew(nickname);
+        for (LocalDateTime attendance : attendances) {
+            attendanceBook.addAttendance(nickname, attendance);
+        }
+
+        WarningLevel actual = attendanceBook.getWarningLevelOf(nickname, today);
+
+        assertThat(actual).isEqualTo(WarningLevel.ONE_ON_ONE);
     }
 }
