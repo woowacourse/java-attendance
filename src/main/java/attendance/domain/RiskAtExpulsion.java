516019a00ed4e3ff126e499124cc9f1a895dfdad
Mint <audwl03071@gmail.com>
2025-03-03 11:01:21 +0900

    feat(RiskAtExpulsion): 지각을 결석으로 간주하여 제적 위험자를 판별하는 기능 구현

---
+++ src/main/java/attendance/domain/RiskAtExpulsion.java
@@ -0,0 +1,31 @@
+package attendance.domain;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public enum RiskAtExpulsion {
+
+    NOT_APPLICABLE(0), WARNING(2), INTERVIEW(3), EXPULSION(6);
+
+    private static final List<RiskAtExpulsion> SORTED_VALUES = Arrays.stream(RiskAtExpulsion.values())
+            .sorted(Comparator.comparingInt(RiskAtExpulsion::getThreshold).reversed())
+            .toList();
+    private final int threshold;
+
+    RiskAtExpulsion(final int threshold) {
+        this.threshold = threshold;
+    }
+
+    public static RiskAtExpulsion of(final int absentCount, final int lateCount) {
+        int totalAbsentCount = absentCount + lateCount / 3;
+        return SORTED_VALUES.stream()
+                .filter(riskAtExpulsion -> riskAtExpulsion.threshold <= totalAbsentCount)
+                .findAny()
+                .orElse(NOT_APPLICABLE);
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+}
