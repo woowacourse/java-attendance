ec19a519c1b13da0c8fc9907129b9e47e3000649
황재웅 <wodnd0131@naver.com>
2025-03-03 23:50:01 +0900

    refactor: 불필요한 Schedule 제거
    - 관련 상수에 대한 책임을 attendance에 할당

--- src/main/java/attendance/domain/Attendance.java
+++ src/main/java/attendance/domain/Attendance.java
@@ -1,5 +1,6 @@
 package attendance.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -7,6 +8,11 @@ import java.time.LocalTime;
 import attendance.exception.AttendanceArgumentException;
 
 public record Attendance(LocalDateTime dateTime, AttendanceStatus status) {
+    private static final LocalTime CAMPUS_OPEN = LocalTime.of(8, 0);
+    private static final LocalTime CAMPUS_CLOSE = LocalTime.of(23, 0);
+    private static final LocalTime LESSON_MONDAY = LocalTime.of(13, 0);
+    private static final LocalTime LESSON_DEFAULT_DAY = LocalTime.of(10, 0);
+
     private static final String OUT_OF_SCHEDULE = "캠퍼스 운영시간 외에 출석할 수 없습니다.";
 
     public Attendance(LocalDateTime dateTime) {
@@ -19,7 +25,7 @@ public record Attendance(LocalDateTime dateTime, AttendanceStatus status) {
     }
 
     private static LocalDateTime validateDateTime(LocalDateTime dateTime) {
-        if (Schedule.isDuringCampus(dateTime.toLocalTime())) {
+        if (isDuringCampus(dateTime.toLocalTime())) {
             throw new AttendanceArgumentException(OUT_OF_SCHEDULE);
         }
         return dateTime;
@@ -28,8 +34,19 @@ public record Attendance(LocalDateTime dateTime, AttendanceStatus status) {
     private static AttendanceStatus judgeStatus(LocalDateTime dateTime) {
         LocalDate date = dateTime.toLocalDate();
         LocalTime time = dateTime.toLocalTime();
-        LocalTime schedule = Schedule.getSchedule(date);
+        LocalTime schedule = getSchedule(date);
         return AttendanceStatus.judgeStatus(time, schedule);
+    }
+
+    private static boolean isDuringCampus(LocalTime time) {
+        return time.isBefore(CAMPUS_OPEN) || time.isAfter(CAMPUS_CLOSE);
+    }
+
+    private static LocalTime getSchedule(LocalDate date) {
+        if (date.getDayOfWeek() == DayOfWeek.MONDAY) {
+            return LESSON_MONDAY;
+        }
+        return LESSON_DEFAULT_DAY;
     }
 
     public String getConvertedStatus() {
