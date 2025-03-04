f0b4fe1cb41b1332d7963089b04ce59b003b20cf
ryan-dia <cjfdnjs158@naver.com>
2025-03-04 22:42:51 +0900

    feat: printF로 통일
    Co-authored-by:cheche903<cheche903@naver.com>

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -18,36 +18,36 @@ public final class InputView {
         final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("오늘은 MM월 dd일 EEEE", Locale.KOREAN);
 
         printF("%n%s입니다. 기능을 선택해 주세요.%n", referenceDateTime.format(dateTimeFormatter));
-        print("1. 출석 확인");
-        print("2. 출석 수정");
-        print("3. 크루별 출석 기록 확인");
-        print("4. 제적 위험자 확인");
-        print("Q. 종료");
+        printF("1. 출석 확인%n");
+        printF("2. 출석 수정%n");
+        printF("3. 크루별 출석 기록 확인%n");
+        printF("4. 제적 위험자 확인%n");
+        printF("Q. 종료%n");
         return readInput();
     }
 
     public static String readNickname() {
-        print("닉네임을 입력해 주세요.");
+        printF("닉네임을 입력해 주세요.%n");
         return readInput();
     }
 
     public static String readTime() {
-        print("등교 시간을 입력해 주세요.");
+        printF("등교 시간을 입력해 주세요.%n");
         return readInput();
     }
 
     public static String readNicknameForEdit() {
-        print("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        printF("출석을 수정하려는 크루의 닉네임을 입력해 주세요.%n");
         return readInput();
     }
 
     public static String readEditDate() {
-        print("수정하려는 날짜(일)를 입력해 주세요.");
+        printF("수정하려는 날짜(일)를 입력해 주세요.%n");
         return readInput();
     }
 
     public static String readTimeForEdit() {
-        print("언제로 변경하겠습니까?");
+        printF("언제로 변경하겠습니까?%n");
         return readInput();
     }
 
@@ -62,9 +62,5 @@ public final class InputView {
 
     private static void printF(final String message, final Object... args) {
         System.out.printf(message, args);
-    }
-
-    private static void print(final String message) {
-        System.out.println(message);
     }
 }
