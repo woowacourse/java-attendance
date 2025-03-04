3516be69eeca8986dd621bae8810fb5cc08bc335
bowook <mbosx316@naver.com>
2025-03-04 20:46:44 +0900

    refactor(RiskCrew) : 정렬 책임 갖기

--- src/main/java/attendance/domain/AttendanceBook.java
+++ src/main/java/attendance/domain/AttendanceBook.java
@@ -11,6 +11,8 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class AttendanceBook {
     private final Map<Crew, AttendanceRecord> attendanceBook;
@@ -69,38 +71,12 @@ public class AttendanceBook {
     }
 
     public List<RiskCrew> findPenaltyCrews() {
-        List<RiskCrew> riskCrews = new ArrayList<>();
-        for (Crew crew : attendanceBook.keySet()) {
-            AttendanceRecord attendanceRecord = attendanceBook.get(crew);
-            PenaltyType penaltyType = attendanceRecord.checkPenaltyStatus();
-            findRiskCrews(crew, penaltyType, attendanceRecord, riskCrews);
-        }
-        return sortRiskCrews(riskCrews);
-    }
-
-    private void findRiskCrews(Crew crew, PenaltyType penaltyType, AttendanceRecord attendanceRecord,
-                               List<RiskCrew> riskCrews) {
-        if (!penaltyType.equals(PenaltyType.NONE)) {
-            int lateCounts = attendanceRecord.checkLateCounts();
-            int absenceCounts = attendanceRecord.checkAbsenceCounts();
-            riskCrews.add(new RiskCrew(crew.getName(), absenceCounts, lateCounts, penaltyType));
-        }
-    }
-
-    private List<RiskCrew> sortRiskCrews(List<RiskCrew> riskCrews) {
-        riskCrews.sort((r1, r2) -> {
-            int absenceComparison = Integer.compare(r2.getAbsenceCount(), r1.getAbsenceCount());
-            if (absenceComparison == 0) {
-                int lateComparison = Integer.compare(r2.getLateCount(), r1.getLateCount());
-                if (lateComparison == 0) {
-                    return r1.getName().compareTo(r2.getName());
-                }
-                return lateComparison;
-            }
-            return absenceComparison;
-        });
-
-        return riskCrews;
+        return attendanceBook.entrySet()
+                .stream()
+                .map(entry -> RiskCrew.from(entry.getKey(), entry.getValue()))
+                .filter(Objects::nonNull)
+                .sorted(RiskCrew.RISK_COMPARATOR)
+                .collect(Collectors.toList());
     }
 
     private Crew findRegisteredCrew(String inputCrewName) {
--- src/main/java/attendance/domain/RiskCrew.java
+++ src/main/java/attendance/domain/RiskCrew.java
@@ -1,5 +1,7 @@
 package attendance.domain;
 
+import java.util.Comparator;
+
 public class RiskCrew {
     private final String name;
     private final int absenceCount;
@@ -12,6 +14,30 @@ public class RiskCrew {
         this.lateCount = lateCount;
         this.penaltyType = penaltyType;
     }
+
+    public static RiskCrew from(Crew crew, AttendanceRecord attendanceRecord) {
+        PenaltyType penaltyType = attendanceRecord.checkPenaltyStatus();
+        if (!penaltyType.equals(PenaltyType.NONE)) {
+            return new RiskCrew(crew.getName(), attendanceRecord.checkAbsenceCounts(),
+                    attendanceRecord.checkLateCounts(), penaltyType);
+        }
+        return null;
+    }
+
+    public static final Comparator<RiskCrew> RISK_COMPARATOR = new Comparator<RiskCrew>() {
+        @Override
+        public int compare(RiskCrew r1, RiskCrew r2) {
+            int absenceComparison = Integer.compare(r2.getAbsenceCount(), r1.getAbsenceCount());
+            if (absenceComparison == 0) {
+                int lateComparison = Integer.compare(r2.getLateCount(), r1.getLateCount());
+                if (lateComparison == 0) {
+                    return r1.getName().compareTo(r2.getName());
+                }
+                return lateComparison;
+            }
+            return absenceComparison;
+        }
+    };
 
     public String getName() {
         return name;
@@ -28,6 +54,4 @@ public class RiskCrew {
     public PenaltyType getPenaltyType() {
         return penaltyType;
     }
-    
 }
-
