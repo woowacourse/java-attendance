fbdc55607218ed76261ee8092a2e6a25701885fc
(minjae8563) <(minjae8563@naver.com)>
2025-03-04 11:15:05 +0900

    refactor : 학생을 찾아서 메서드 호출하는 방식에서 바로 호출하는 방식으로 수정

--- src/main/java/controller/AttendanceCheckController.java
+++ src/main/java/controller/AttendanceCheckController.java
@@ -15,7 +15,7 @@ public class AttendanceCheckController {
             String studentName = InputView.getStudentNameUntilValidateToAttendance(students);
             AttendanceTime attendanceTime = InputView.getUserAttendanceTimeUntilValidateToAttendance();
             students.validateAlreadyExistAttendanceDate(studentName, today);
-            students.findStudentByName(studentName).addAttendanceDateTime(today, attendanceTime);
+            students.addAttendanceDateTime(studentName, today, attendanceTime);
             AttendanceStatus studentAttendanceStatus = AttendanceStatusEvaluator.calculateAttendanceStatus(today, attendanceTime);
             OutputView.printAttendanceResult(today, attendanceTime, studentAttendanceStatus);
         } catch (IllegalArgumentException e) {
