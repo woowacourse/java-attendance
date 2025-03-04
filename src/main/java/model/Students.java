e3703dbac0d9eb4f9523b85bed5c49ec74f5d7b3
(minjae8563) <(minjae8563@naver.com)>
2025-03-04 11:24:46 +0900

    feat : 학생 이름, 일자를 받아 해당 일자에 입실한 시간 찾는 메서드 테스트 구현

--- src/main/java/model/Students.java
+++ src/main/java/model/Students.java
@@ -11,6 +11,13 @@ public class Students {
         this.students = new ArrayList<>(students);
     }
 
+    public void findAttendanceTimeByAttendanceDate(String studentName, AttendanceDate attendanceDate) {
+        students.stream()
+                .filter(student -> student.isSameName(studentName))
+                .findFirst()
+                .ifPresent(student -> student.findAttendanceTimeByAttendanceDate(attendanceDate));
+    }
+
     public void modifyAttendanceDateTime(String studentName, AttendanceDate attendanceDate, AttendanceTime attendanceTime) {
         students.stream()
                 .filter(student -> student.isSameName(studentName))
