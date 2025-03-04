4d6d8d059de824700c1c98592a4e128acac5dc69
BWY <quantumjump10@gmail.com>
2025-03-04 20:19:31 +0900

    refactor: named를 사용하여 테스트 명칭 부여 후 중복 제거

--- src/test/java/domain/StandardTimeTest.java
+++ src/test/java/domain/StandardTimeTest.java
@@ -1,7 +1,7 @@
 package domain;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -30,6 +30,21 @@ class StandardTimeTest {
         );
     }
 
+    static Stream<Arguments> dateTimeAndDayOfWeekParam() {
+        return Stream.of(
+                Arguments.arguments(Named.named("월요일 출석 테스트", LocalDateTime.of(2024, 12, 16, 13, 0)), AttendanceStatus.ATTENDANCE),
+                Arguments.arguments(Named.named("월요일 출석 경계값 테스트", LocalDateTime.of(2024, 12, 16, 13, 5)), AttendanceStatus.ATTENDANCE),
+                Arguments.arguments(Named.named("월요일 지각 테스트", LocalDateTime.of(2024, 12, 16, 13, 6)), AttendanceStatus.LATENESS),
+                Arguments.arguments(Named.named("월요일 지각 경계값 테스트", LocalDateTime.of(2024, 12, 16, 13, 30)), AttendanceStatus.LATENESS),
+                Arguments.arguments(Named.named("월요일 결석 테스트", LocalDateTime.of(2024, 12, 16, 13, 31)), AttendanceStatus.ABSENCE),
+                Arguments.arguments(Named.named("화요일 출석 테스트", LocalDateTime.of(2024, 12, 17, 10, 0)), AttendanceStatus.ATTENDANCE),
+                Arguments.arguments(Named.named("화요일 출석 경계값 테스트", LocalDateTime.of(2024, 12, 17, 10, 5)), AttendanceStatus.ATTENDANCE),
+                Arguments.arguments(Named.named("화요일 지각 테스트", LocalDateTime.of(2024, 12, 17, 10, 6)), AttendanceStatus.LATENESS),
+                Arguments.arguments(Named.named("화요일 지각 경계값 테스트", LocalDateTime.of(2024, 12, 17, 10, 30)), AttendanceStatus.LATENESS),
+                Arguments.arguments(Named.named("화요일 결석 테스트", LocalDateTime.of(2024, 12, 17, 10, 31)), AttendanceStatus.ABSENCE)
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("dayOfWeekAndStandardTimes")
     void 요일에_대한_출석기준시간을_가져온다(DayOfWeek dayOfWeek, LocalTime lateTime, LocalTime absentTime) {
@@ -40,93 +55,13 @@ class StandardTimeTest {
         assertThat(standardTime.getAbsentTime()).isEqualTo(absentTime);
     }
 
-    @Test
-    void 월요일_출석_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 16, 13, 0);
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("dateTimeAndDayOfWeekParam")
+    void 요일_출석_테스트(LocalDateTime dateTime, AttendanceStatus expectedStatus) {
+        LocalDateTime localDateTime = dateTime;
 
         AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
 
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ATTENDANCE);
-    }
-
-    @Test
-    void 월요일_출석_경계값_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 16, 13, 5);
-
-        AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
-
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ATTENDANCE);
-    }
-
-    @Test
-    void 월요일_지각_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 16, 13, 6);
-
-        AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
-
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.LATENESS);
-    }
-
-    @Test
-    void 월요일_지각_경계값_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 16, 13, 30);
-
-        AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
-
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.LATENESS);
-    }
-
-    @Test
-    void 월요일_결석_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 16, 13, 31);
-
-        AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
-
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ABSENCE);
-    }
-
-    @Test
-    void 화요일_출석_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 17, 10, 0);
-
-        AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
-
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ATTENDANCE);
-    }
-
-    @Test
-    void 화요일_출석_경계값_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 17, 10, 5);
-
-        AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
-
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ATTENDANCE);
-    }
-
-    @Test
-    void 화요일_지각_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 17, 10, 6);
-
-        AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
-
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.LATENESS);
-    }
-
-    @Test
-    void 화요일_지각_경계값_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 17, 10, 30);
-
-        AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
-
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.LATENESS);
-    }
-
-    @Test
-    void 화요일_결석_테스트() {
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 17, 10, 31);
-
-        AttendanceStatus attendanceStatus = StandardTime.judge(localDateTime);
-
-        Assertions.assertThat(attendanceStatus).isEqualTo(AttendanceStatus.ABSENCE);
+        Assertions.assertThat(attendanceStatus).isEqualTo(expectedStatus);
     }
 }
