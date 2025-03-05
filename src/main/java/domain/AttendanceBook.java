5b0a391fce5201ca36e2808786527f9b919c9a58
geonwoo <kysub99@naver.com>
2025-03-05 20:03:39 +0900

    refactor(AttendanceBook): 누락된 접근 제어자 지정

--- src/main/java/domain/AttendanceBook.java
+++ src/main/java/domain/AttendanceBook.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Optional;
 
 public class AttendanceBook {
-    Map<Crew, CheckInHistory> book;
+    private final Map<Crew, CheckInHistory> book;
 
     private AttendanceBook(Map<Crew, CheckInHistory> book) {
         this.book = book;
