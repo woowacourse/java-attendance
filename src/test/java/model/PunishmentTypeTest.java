19eadbc05f58a18d42246d6b590d8b095992f290
soeun2537 <soeun2537@gmail.com>
2025-03-05 19:09:01 +0900

    test: 반환 타입 변경으로 인한 테스트 로직 수정

--- src/test/java/model/PunishmentTypeTest.java
+++ src/test/java/model/PunishmentTypeTest.java
@@ -3,6 +3,7 @@ package model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.EnumMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,7 @@ class PunishmentTypeTest {
     @DisplayName("징계 타입을 반환한다. (경고)")
     void test1() {
         // given
-        EnumMap<AttendanceType, Integer> attendanceTotal = new EnumMap<>(AttendanceType.class);
+        Map<AttendanceType, Integer> attendanceTotal = new EnumMap<>(AttendanceType.class);
         attendanceTotal.put(AttendanceType.SUCCESS, 5);
         attendanceTotal.put(AttendanceType.BE_LATE, 5);
         attendanceTotal.put(AttendanceType.ABSENCE, 2);
@@ -28,7 +29,7 @@ class PunishmentTypeTest {
     @DisplayName("징계 타입을 반환한다. (경고)")
     void test2() {
         // given
-        EnumMap<AttendanceType, Integer> attendanceTotal = new EnumMap<>(AttendanceType.class);
+        Map<AttendanceType, Integer> attendanceTotal = new EnumMap<>(AttendanceType.class);
         attendanceTotal.put(AttendanceType.SUCCESS, 5);
         attendanceTotal.put(AttendanceType.BE_LATE, 5);
         attendanceTotal.put(AttendanceType.ABSENCE, 5);
@@ -44,7 +45,7 @@ class PunishmentTypeTest {
     @DisplayName("징계 타입을 반환한다. (제적)")
     void test3() {
         // given
-        EnumMap<AttendanceType, Integer> attendanceTotal = new EnumMap<>(AttendanceType.class);
+        Map<AttendanceType, Integer> attendanceTotal = new EnumMap<>(AttendanceType.class);
         attendanceTotal.put(AttendanceType.SUCCESS, 5);
         attendanceTotal.put(AttendanceType.BE_LATE, 5);
         attendanceTotal.put(AttendanceType.ABSENCE, 6);
@@ -60,7 +61,7 @@ class PunishmentTypeTest {
     @DisplayName("징계 타입을 반환한다. (해당 없음)")
     void test4() {
         // given
-        EnumMap<AttendanceType, Integer> attendanceTotal = new EnumMap<>(AttendanceType.class);
+        Map<AttendanceType, Integer> attendanceTotal = new EnumMap<>(AttendanceType.class);
         attendanceTotal.put(AttendanceType.SUCCESS, 5);
         attendanceTotal.put(AttendanceType.BE_LATE, 5);
         attendanceTotal.put(AttendanceType.ABSENCE, 1);
